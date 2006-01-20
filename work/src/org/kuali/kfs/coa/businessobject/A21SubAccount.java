76834d70c46e7e199140b424afb10f1f27637fa2
Ralph	Olstad <rolstad@deltacollege.edu>
2006-01-20 18:27:45 +0000

    A21 Sub Account Maintenance Objects.  RO 1-20-06

--- work/src/org/kuali/kfs/coa/businessobject/A21SubAccount.java
+++ work/src/org/kuali/kfs/coa/businessobject/A21SubAccount.java
@@ -25,6 +25,7 @@ package org.kuali.module.chart.bo;
 import java.util.LinkedHashMap;
 
 import org.kuali.core.bo.BusinessObjectBase;
+import org.kuali.module.chart.bo.codes.ICRTypeCode;
 
 /**
  * @author jsissom
@@ -46,6 +47,17 @@ public class A21SubAccount extends BusinessObjectBase {
   private String costSharingAccountNumber;
   private String costSharingSubAccountNumber;
 
+  private Chart chartOfAccounts;
+  private Account account;
+  private SubAccount subAccount;
+  private ICRTypeCode icrTypeCode;
+  private Chart indirectCostRecoveryChartOfAccounts;
+  private Account indirectCostRecoveryAccount;
+  private Campus offsetCampus;
+  private Chart costSharingChartOfAccounts;
+  private Account costSharingAccount;
+  private SubAccount costSharingSubAccount;
+  
   /**
    * 
    */
@@ -53,6 +65,321 @@ public class A21SubAccount extends BusinessObjectBase {
     super();
   }
 
+/**
+ * Gets the serialVersionUID attribute. 
+ * @return Returns the serialVersionUID.
+ */
+public static long getSerialVersionUID() {
+    return serialVersionUID;
+}
+/**
+ * Gets the account attribute. 
+ * @return Returns the account.
+ */
+public Account getAccount() {
+    return account;
+}
+/**
+ * Sets the account attribute value.
+ * @param account The account to set.
+ */
+public void setAccount(Account account) {
+    this.account = account;
+}
+/**
+ * Gets the accountNumber attribute. 
+ * @return Returns the accountNumber.
+ */
+public String getAccountNumber() {
+    return accountNumber;
+}
+/**
+ * Sets the accountNumber attribute value.
+ * @param accountNumber The accountNumber to set.
+ */
+public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+}
+/**
+ * Gets the chartOfAccounts attribute. 
+ * @return Returns the chartOfAccounts.
+ */
+public Chart getChartOfAccounts() {
+    return chartOfAccounts;
+}
+/**
+ * Sets the chartOfAccounts attribute value.
+ * @param chartOfAccounts The chartOfAccounts to set.
+ */
+public void setChartOfAccounts(Chart chartOfAccounts) {
+    this.chartOfAccounts = chartOfAccounts;
+}
+/**
+ * Gets the chartOfAccountsCode attribute. 
+ * @return Returns the chartOfAccountsCode.
+ */
+public String getChartOfAccountsCode() {
+    return chartOfAccountsCode;
+}
+/**
+ * Sets the chartOfAccountsCode attribute value.
+ * @param chartOfAccountsCode The chartOfAccountsCode to set.
+ */
+public void setChartOfAccountsCode(String chartOfAccountsCode) {
+    this.chartOfAccountsCode = chartOfAccountsCode;
+}
+/**
+ * Gets the costSharingAccount attribute. 
+ * @return Returns the costSharingAccount.
+ */
+public Account getCostSharingAccount() {
+    return costSharingAccount;
+}
+/**
+ * Sets the costSharingAccount attribute value.
+ * @param costSharingAccount The costSharingAccount to set.
+ */
+public void setCostSharingAccount(Account costSharingAccount) {
+    this.costSharingAccount = costSharingAccount;
+}
+/**
+ * Gets the costSharingAccountNumber attribute. 
+ * @return Returns the costSharingAccountNumber.
+ */
+public String getCostSharingAccountNumber() {
+    return costSharingAccountNumber;
+}
+/**
+ * Sets the costSharingAccountNumber attribute value.
+ * @param costSharingAccountNumber The costSharingAccountNumber to set.
+ */
+public void setCostSharingAccountNumber(String costSharingAccountNumber) {
+    this.costSharingAccountNumber = costSharingAccountNumber;
+}
+/**
+ * Gets the costSharingChartOfAccounts attribute. 
+ * @return Returns the costSharingChartOfAccounts.
+ */
+public Chart getCostSharingChartOfAccounts() {
+    return costSharingChartOfAccounts;
+}
+/**
+ * Sets the costSharingChartOfAccounts attribute value.
+ * @param costSharingChartOfAccounts The costSharingChartOfAccounts to set.
+ */
+public void setCostSharingChartOfAccounts(Chart costSharingChartOfAccounts) {
+    this.costSharingChartOfAccounts = costSharingChartOfAccounts;
+}
+/**
+ * Gets the costSharingChartOfAccountsCode attribute. 
+ * @return Returns the costSharingChartOfAccountsCode.
+ */
+public String getCostSharingChartOfAccountsCode() {
+    return costSharingChartOfAccountsCode;
+}
+/**
+ * Sets the costSharingChartOfAccountsCode attribute value.
+ * @param costSharingChartOfAccountsCode The costSharingChartOfAccountsCode to set.
+ */
+public void setCostSharingChartOfAccountsCode(String costSharingChartOfAccountsCode) {
+    this.costSharingChartOfAccountsCode = costSharingChartOfAccountsCode;
+}
+/**
+ * Gets the costSharingSubAccount attribute. 
+ * @return Returns the costSharingSubAccount.
+ */
+public SubAccount getCostSharingSubAccount() {
+    return costSharingSubAccount;
+}
+/**
+ * Sets the costSharingSubAccount attribute value.
+ * @param costSharingSubAccount The costSharingSubAccount to set.
+ */
+public void setCostSharingSubAccount(SubAccount costSharingSubAccount) {
+    this.costSharingSubAccount = costSharingSubAccount;
+}
+/**
+ * Gets the costSharingSubAccountNumber attribute. 
+ * @return Returns the costSharingSubAccountNumber.
+ */
+public String getCostSharingSubAccountNumber() {
+    return costSharingSubAccountNumber;
+}
+/**
+ * Sets the costSharingSubAccountNumber attribute value.
+ * @param costSharingSubAccountNumber The costSharingSubAccountNumber to set.
+ */
+public void setCostSharingSubAccountNumber(String costSharingSubAccountNumber) {
+    this.costSharingSubAccountNumber = costSharingSubAccountNumber;
+}
+/**
+ * Gets the icrTypeCode attribute. 
+ * @return Returns the icrTypeCode.
+ */
+public ICRTypeCode getIcrTypeCode() {
+    return icrTypeCode;
+}
+/**
+ * Sets the icrTypeCode attribute value.
+ * @param icrTypeCode The icrTypeCode to set.
+ */
+public void setIcrTypeCode(ICRTypeCode icrTypeCode) {
+    this.icrTypeCode = icrTypeCode;
+}
+/**
+ * Gets the indirectCostRecoveryAccount attribute. 
+ * @return Returns the indirectCostRecoveryAccount.
+ */
+public Account getIndirectCostRecoveryAccount() {
+    return indirectCostRecoveryAccount;
+}
+/**
+ * Sets the indirectCostRecoveryAccount attribute value.
+ * @param indirectCostRecoveryAccount The indirectCostRecoveryAccount to set.
+ */
+public void setIndirectCostRecoveryAccount(Account indirectCostRecoveryAccount) {
+    this.indirectCostRecoveryAccount = indirectCostRecoveryAccount;
+}
+/**
+ * Gets the indirectCostRecoveryAccountNumber attribute. 
+ * @return Returns the indirectCostRecoveryAccountNumber.
+ */
+public String getIndirectCostRecoveryAccountNumber() {
+    return indirectCostRecoveryAccountNumber;
+}
+/**
+ * Sets the indirectCostRecoveryAccountNumber attribute value.
+ * @param indirectCostRecoveryAccountNumber The indirectCostRecoveryAccountNumber to set.
+ */
+public void setIndirectCostRecoveryAccountNumber(String indirectCostRecoveryAccountNumber) {
+    this.indirectCostRecoveryAccountNumber = indirectCostRecoveryAccountNumber;
+}
+/**
+ * Gets the indirectCostRecoveryChartOfAccounts attribute. 
+ * @return Returns the indirectCostRecoveryChartOfAccounts.
+ */
+public Chart getIndirectCostRecoveryChartOfAccounts() {
+    return indirectCostRecoveryChartOfAccounts;
+}
+/**
+ * Sets the indirectCostRecoveryChartOfAccounts attribute value.
+ * @param indirectCostRecoveryChartOfAccounts The indirectCostRecoveryChartOfAccounts to set.
+ */
+public void setIndirectCostRecoveryChartOfAccounts(Chart indirectCostRecoveryChartOfAccounts) {
+    this.indirectCostRecoveryChartOfAccounts = indirectCostRecoveryChartOfAccounts;
+}
+/**
+ * Gets the indirectCostRecoveryChartOfAccountsCode attribute. 
+ * @return Returns the indirectCostRecoveryChartOfAccountsCode.
+ */
+public String getIndirectCostRecoveryChartOfAccountsCode() {
+    return indirectCostRecoveryChartOfAccountsCode;
+}
+/**
+ * Sets the indirectCostRecoveryChartOfAccountsCode attribute value.
+ * @param indirectCostRecoveryChartOfAccountsCode The indirectCostRecoveryChartOfAccountsCode to set.
+ */
+public void setIndirectCostRecoveryChartOfAccountsCode(String indirectCostRecoveryChartOfAccountsCode) {
+    this.indirectCostRecoveryChartOfAccountsCode = indirectCostRecoveryChartOfAccountsCode;
+}
+/**
+ * Gets the indirectCostRecoveryTypeCode attribute. 
+ * @return Returns the indirectCostRecoveryTypeCode.
+ */
+public String getIndirectCostRecoveryTypeCode() {
+    return indirectCostRecoveryTypeCode;
+}
+/**
+ * Sets the indirectCostRecoveryTypeCode attribute value.
+ * @param indirectCostRecoveryTypeCode The indirectCostRecoveryTypeCode to set.
+ */
+public void setIndirectCostRecoveryTypeCode(String indirectCostRecoveryTypeCode) {
+    this.indirectCostRecoveryTypeCode = indirectCostRecoveryTypeCode;
+}
+/**
+ * Gets the offsetCampus attribute. 
+ * @return Returns the offsetCampus.
+ */
+public Campus getOffsetCampus() {
+    return offsetCampus;
+}
+/**
+ * Sets the offsetCampus attribute value.
+ * @param offsetCampus The offsetCampus to set.
+ */
+public void setOffsetCampus(Campus offsetCampus) {
+    this.offsetCampus = offsetCampus;
+}
+/**
+ * Gets the offsetCampusCode attribute. 
+ * @return Returns the offsetCampusCode.
+ */
+public String getOffsetCampusCode() {
+    return offsetCampusCode;
+}
+/**
+ * Sets the offsetCampusCode attribute value.
+ * @param offsetCampusCode The offsetCampusCode to set.
+ */
+public void setOffsetCampusCode(String offsetCampusCode) {
+    this.offsetCampusCode = offsetCampusCode;
+}
+/**
+ * Gets the seriesId attribute. 
+ * @return Returns the seriesId.
+ */
+public String getSeriesId() {
+    return seriesId;
+}
+/**
+ * Sets the seriesId attribute value.
+ * @param seriesId The seriesId to set.
+ */
+public void setSeriesId(String seriesId) {
+    this.seriesId = seriesId;
+}
+/**
+ * Gets the subAccount attribute. 
+ * @return Returns the subAccount.
+ */
+public SubAccount getSubAccount() {
+    return subAccount;
+}
+/**
+ * Sets the subAccount attribute value.
+ * @param subAccount The subAccount to set.
+ */
+public void setSubAccount(SubAccount subAccount) {
+    this.subAccount = subAccount;
+}
+/**
+ * Gets the subAccountNumber attribute. 
+ * @return Returns the subAccountNumber.
+ */
+public String getSubAccountNumber() {
+    return subAccountNumber;
+}
+/**
+ * Sets the subAccountNumber attribute value.
+ * @param subAccountNumber The subAccountNumber to set.
+ */
+public void setSubAccountNumber(String subAccountNumber) {
+    this.subAccountNumber = subAccountNumber;
+}
+/**
+ * Gets the subAccountTypeCode attribute. 
+ * @return Returns the subAccountTypeCode.
+ */
+public String getSubAccountTypeCode() {
+    return subAccountTypeCode;
+}
+/**
+ * Sets the subAccountTypeCode attribute value.
+ * @param subAccountTypeCode The subAccountTypeCode to set.
+ */
+public void setSubAccountTypeCode(String subAccountTypeCode) {
+    this.subAccountTypeCode = subAccountTypeCode;
+}
   /* (non-Javadoc)
    * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
    */
@@ -64,76 +391,5 @@ public class A21SubAccount extends BusinessObjectBase {
     return map;
   }
 
-  public String getAccountNumber() {
-    return accountNumber;
-  }
-  public void setAccountNumber(String accountNumber) {
-    this.accountNumber = accountNumber;
-  }
-  public String getChartOfAccountsCode() {
-    return chartOfAccountsCode;
-  }
-  public void setChartOfAccountsCode(String chartOfAccountsCode) {
-    this.chartOfAccountsCode = chartOfAccountsCode;
-  }
-  public String getCostSharingAccountNumber() {
-    return costSharingAccountNumber;
-  }
-  public void setCostSharingAccountNumber(String costSharingAccountNumber) {
-    this.costSharingAccountNumber = costSharingAccountNumber;
-  }
-  public String getCostSharingChartOfAccountsCode() {
-    return costSharingChartOfAccountsCode;
-  }
-  public void setCostSharingChartOfAccountsCode(String costSharingChartOfAccountsCode) {
-    this.costSharingChartOfAccountsCode = costSharingChartOfAccountsCode;
-  }
-  public String getCostSharingSubAccountNumber() {
-    return costSharingSubAccountNumber;
-  }
-  public void setCostSharingSubAccountNumber(String costSharingSubAccountNumber) {
-    this.costSharingSubAccountNumber = costSharingSubAccountNumber;
-  }
-  public String getIndirectCostRecoveryAccountNumber() {
-    return indirectCostRecoveryAccountNumber;
-  }
-  public void setIndirectCostRecoveryAccountNumber(String indirectCostRecoveryAccountNumber) {
-    this.indirectCostRecoveryAccountNumber = indirectCostRecoveryAccountNumber;
-  }
-  public String getIndirectCostRecoveryChartOfAccountsCode() {
-    return indirectCostRecoveryChartOfAccountsCode;
-  }
-  public void setIndirectCostRecoveryChartOfAccountsCode(String indirectCostRecoveryChartOfAccountsCode) {
-    this.indirectCostRecoveryChartOfAccountsCode = indirectCostRecoveryChartOfAccountsCode;
-  }
-  public String getIndirectCostRecoveryTypeCode() {
-    return indirectCostRecoveryTypeCode;
-  }
-  public void setIndirectCostRecoveryTypeCode(String indirectCostRecoveryTypeCode) {
-    this.indirectCostRecoveryTypeCode = indirectCostRecoveryTypeCode;
-  }
-  public String getOffsetCampusCode() {
-    return offsetCampusCode;
-  }
-  public void setOffsetCampusCode(String offsetCampusCode) {
-    this.offsetCampusCode = offsetCampusCode;
-  }
-  public String getSeriesId() {
-    return seriesId;
-  }
-  public void setSeriesId(String seriesId) {
-    this.seriesId = seriesId;
-  }
-  public String getSubAccountNumber() {
-    return subAccountNumber;
-  }
-  public void setSubAccountNumber(String subAccountNumber) {
-    this.subAccountNumber = subAccountNumber;
-  }
-  public String getSubAccountTypeCode() {
-    return subAccountTypeCode;
-  }
-  public void setSubAccountTypeCode(String subAccountTypeCode) {
-    this.subAccountTypeCode = subAccountTypeCode;
-  }
+  
 }

babca8e6c375a8750a2ab41e61d8618480cd9ad2
Ralph	Olstad <rolstad@deltacollege.edu>
2006-02-02 01:20:25 +0000

    ICR Exclusion Account maintenance objects.  RO 2-1-06.

--- work/src/org/kuali/kfs/coa/businessobject/IndirectCostRecoveryExclusionAccount.java
+++ work/src/org/kuali/kfs/coa/businessobject/IndirectCostRecoveryExclusionAccount.java
@@ -31,54 +31,117 @@ import org.kuali.core.bo.BusinessObjectBase;
  *
  */
 public class IndirectCostRecoveryExclusionAccount extends BusinessObjectBase {
-  static final long serialVersionUID = -7894071504928822853L;
-
-  private String chartOfAccountsCode;
-  private String accountNumber;
-  private String objectChartOfAccountsCode;
-  private String objectCode;
-
-  /**
-   * 
-   */
-  public IndirectCostRecoveryExclusionAccount() {
-    super();
-  }
-
-  /* (non-Javadoc)
-   * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
-   */
-  protected LinkedHashMap toStringMapper() {
-    LinkedHashMap map = new LinkedHashMap();
-    map.put("chartOfAccountsCode", getChartOfAccountsCode());
-    map.put("accountNumber", getAccountNumber());
-    map.put("objectChartOfAccountsCode", getObjectChartOfAccountsCode());
-    map.put("objectCode", getObjectCode());
-    return map;
-  }
-
-  public String getAccountNumber() {
-    return accountNumber;
-  }
-  public void setAccountNumber(String accountNumber) {
-    this.accountNumber = accountNumber;
-  }
-  public String getChartOfAccountsCode() {
-    return chartOfAccountsCode;
-  }
-  public void setChartOfAccountsCode(String chartOfAccountsCode) {
-    this.chartOfAccountsCode = chartOfAccountsCode;
-  }
-  public String getObjectChartOfAccountsCode() {
-    return objectChartOfAccountsCode;
-  }
-  public void setObjectChartOfAccountsCode(String objectChartOfAccountsCode) {
-    this.objectChartOfAccountsCode = objectChartOfAccountsCode;
-  }
-  public String getObjectCode() {
-    return objectCode;
-  }
-  public void setObjectCode(String objectCode) {
-    this.objectCode = objectCode;
-  }
+    static final long serialVersionUID = -7894071504928822853L;
+    
+    private String chartOfAccountsCode;
+    private String accountNumber;
+    private String objectChartOfAccountsCode;
+    private String objectCode;
+    
+    private Chart chartOfAccounts;
+    private Account account;
+    private Chart objectChartOfAccounts;
+    private ObjectCode object;
+    
+    /**
+     * 
+     */
+    public IndirectCostRecoveryExclusionAccount() {
+        super();
+    }
+    
+    /* (non-Javadoc)
+     * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
+     */
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap map = new LinkedHashMap();
+        map.put("chartOfAccountsCode", getChartOfAccountsCode());
+        map.put("accountNumber", getAccountNumber());
+        map.put("objectChartOfAccountsCode", getObjectChartOfAccountsCode());
+        map.put("objectCode", getObjectCode());
+        return map;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public String getChartOfAccountsCode() {
+        return chartOfAccountsCode;
+    }
+    public void setChartOfAccountsCode(String chartOfAccountsCode) {
+        this.chartOfAccountsCode = chartOfAccountsCode;
+    }
+    public String getObjectChartOfAccountsCode() {
+        return objectChartOfAccountsCode;
+    }
+    public void setObjectChartOfAccountsCode(String objectChartOfAccountsCode) {
+        this.objectChartOfAccountsCode = objectChartOfAccountsCode;
+    }
+    public String getObjectCode() {
+        return objectCode;
+    }
+    public void setObjectCode(String objectCode) {
+        this.objectCode = objectCode;
+    }
+    
+    
+    /**
+     * Gets the account attribute. 
+     * @return Returns the account.
+     */
+    public Account getAccount() {
+        return account;
+    }
+    /**
+     * Sets the account attribute value.
+     * @param account The account to set.
+     */
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+    /**
+     * Gets the chartOfAccounts attribute. 
+     * @return Returns the chartOfAccounts.
+     */
+    public Chart getChartOfAccounts() {
+        return chartOfAccounts;
+    }
+    /**
+     * Sets the chartOfAccounts attribute value.
+     * @param chartOfAccounts The chartOfAccounts to set.
+     */
+    public void setChartOfAccounts(Chart chartOfAccounts) {
+        this.chartOfAccounts = chartOfAccounts;
+    }
+    /**
+     * Gets the object attribute. 
+     * @return Returns the object.
+     */
+    public ObjectCode getObject() {
+        return object;
+    }
+    /**
+     * Sets the object attribute value.
+     * @param object The object to set.
+     */
+    public void setObject(ObjectCode object) {
+        this.object = object;
+    }
+    /**
+     * Gets the objectChartOfAccounts attribute. 
+     * @return Returns the objectChartOfAccounts.
+     */
+    public Chart getObjectChartOfAccounts() {
+        return objectChartOfAccounts;
+    }
+    /**
+     * Sets the objectChartOfAccounts attribute value.
+     * @param objectChartOfAccounts The objectChartOfAccounts to set.
+     */
+    public void setObjectChartOfAccounts(Chart objectChartOfAccounts) {
+        this.objectChartOfAccounts = objectChartOfAccounts;
+    }
 }

d44df1e8aae9bf7b19b9a4d6eb7da91dc55c0582
Ralph	Olstad <rolstad@deltacollege.edu>
2006-02-07 18:44:00 +0000

    ICR Maint Docs and misc maintenance stuff.  RO 2-7-06.

--- work/src/org/kuali/kfs/coa/businessobject/IndirectCostRecoveryExclusionType.java
+++ work/src/org/kuali/kfs/coa/businessobject/IndirectCostRecoveryExclusionType.java
@@ -25,52 +25,113 @@ package org.kuali.module.chart.bo;
 import java.util.LinkedHashMap;
 
 import org.kuali.core.bo.BusinessObjectBase;
+import org.kuali.module.chart.bo.codes.ICRTypeCode;
 
 /**
  * @author jsissom
- *
+ *  
  */
 public class IndirectCostRecoveryExclusionType extends BusinessObjectBase {
-  static final long serialVersionUID = -5958353188483465053L;
+    static final long serialVersionUID = -5958353188483465053L;
 
-  private String accountIndirectCostRecoveryTypeCode;
-  private String chartOfAccountsCode;
-  private String objectCode;
+    private String accountIndirectCostRecoveryTypeCode;
+    private String chartOfAccountsCode;
+    private String objectCode;
 
-  /**
-   * 
-   */
-  public IndirectCostRecoveryExclusionType() {
-    super();
-  }
+    private ICRTypeCode icrTypeCode;
+    private Chart chartOfAccounts;
+    private ObjectCode object;
 
-  /* (non-Javadoc)
-   * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
-   */
-  protected LinkedHashMap toStringMapper() {
-    LinkedHashMap map = new LinkedHashMap();
-    map.put("accountIndirectCostRecoveryTypeCode", getAccountIndirectCostRecoveryTypeCode());
-    map.put("chartOfAccountsCode", getChartOfAccountsCode());
-    map.put("objectCode", getObjectCode());
-    return map;
-  }
 
-  public String getAccountIndirectCostRecoveryTypeCode() {
-    return accountIndirectCostRecoveryTypeCode;
-  }
-  public void setAccountIndirectCostRecoveryTypeCode(String accountIndirectCostRecoveryTypeCode) {
-    this.accountIndirectCostRecoveryTypeCode = accountIndirectCostRecoveryTypeCode;
-  }
-  public String getChartOfAccountsCode() {
-    return chartOfAccountsCode;
-  }
-  public void setChartOfAccountsCode(String chartOfAccountsCode) {
-    this.chartOfAccountsCode = chartOfAccountsCode;
-  }
-  public String getObjectCode() {
-    return objectCode;
-  }
-  public void setObjectCode(String objectCode) {
-    this.objectCode = objectCode;
-  }
+    /**
+     *  
+     */
+    public IndirectCostRecoveryExclusionType() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
+     */
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap map = new LinkedHashMap();
+        map.put("accountIndirectCostRecoveryTypeCode", getAccountIndirectCostRecoveryTypeCode());
+        map.put("chartOfAccountsCode", getChartOfAccountsCode());
+        map.put("objectCode", getObjectCode());
+        return map;
+    }
+
+    public String getAccountIndirectCostRecoveryTypeCode() {
+        return accountIndirectCostRecoveryTypeCode;
+    }
+
+    public void setAccountIndirectCostRecoveryTypeCode(String accountIndirectCostRecoveryTypeCode) {
+        this.accountIndirectCostRecoveryTypeCode = accountIndirectCostRecoveryTypeCode;
+    }
+
+    public String getChartOfAccountsCode() {
+        return chartOfAccountsCode;
+    }
+
+    public void setChartOfAccountsCode(String chartOfAccountsCode) {
+        this.chartOfAccountsCode = chartOfAccountsCode;
+    }
+
+    public String getObjectCode() {
+        return objectCode;
+    }
+
+    public void setObjectCode(String objectCode) {
+        this.objectCode = objectCode;
+    }
+
+    /**
+     * Gets the chartOfAccounts attribute.
+     * @return Returns the chartOfAccounts.
+     */
+    public Chart getChartOfAccounts() {
+        return chartOfAccounts;
+    }
+
+    /**
+     * Sets the chartOfAccounts attribute value.
+     * @param chartOfAccounts The chartOfAccounts to set.
+     */
+    public void setChartOfAccounts(Chart chartOfAccounts) {
+        this.chartOfAccounts = chartOfAccounts;
+    }
+
+    /**
+     * Gets the object attribute.
+     * @return Returns the object.
+     */
+    public ObjectCode getObject() {
+        return object;
+    }
+
+    /**
+     * Sets the object attribute value.
+     * @param object The object to set.
+     */
+    public void setObject(ObjectCode object) {
+        this.object = object;
+    }
+
+
+    /**
+     * Gets the icrTypeCode attribute.
+     * @return Returns the icrTypeCode.
+     */
+    public ICRTypeCode getIcrTypeCode() {
+        return icrTypeCode;
+    }
+
+    /**
+     * Sets the icrTypeCode attribute value.
+     * @param icrTypeCode The icrTypeCode to set.
+     */
+    public void setIcrTypeCode(ICRTypeCode icrTypeCode) {
+        this.icrTypeCode = icrTypeCode;
+    }
 }

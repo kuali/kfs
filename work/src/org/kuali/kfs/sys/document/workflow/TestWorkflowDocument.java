340dd23c6c55395792b07a3412d951ea0134e650
Ryan	Kirkendall <rkirkend@indiana.edu>
2006-02-08 20:50:12 +0000

    TestWorkflowDocument moved to src so as not to confound DD on startup.

---
+++ work/src/org/kuali/kfs/sys/document/workflow/TestWorkflowDocument.java
@@ -0,0 +1,9 @@
+package org.kuali.workflow;
+
+import org.kuali.module.financial.document.InternalBillingDocument;
+
+public class TestWorkflowDocument extends InternalBillingDocument {
+    
+    private static final long serialVersionUID = 5256426601708960189L;
+    
+}

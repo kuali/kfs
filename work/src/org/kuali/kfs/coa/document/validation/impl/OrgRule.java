7f9dd59b59fc7e33a71633d6e459e0bb90f434e8
Andrew Hollamon <andrew@dmentionsystems.com>
2006-02-20 05:27:55 +0000

    KULCOA-397 Initial work on Org business rules. Setup rule class, cleaned xml files, setup defaultExistenceChecks did not implement any business rules yet

---
+++ work/src/org/kuali/kfs/coa/document/validation/impl/OrgRule.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2004, 2005 The National Association of College and University Business Officers,
+ * Cornell University, Trustees of Indiana University, Michigan State University Board of Trustees,
+ * Trustees of San Joaquin Delta College, University of Hawai'i, The Arizona Board of Regents on
+ * behalf of the University of Arizona, and the r*smart group.
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License"); By obtaining,
+ * using and/or copying this Original Work, you agree that you have read, understand, and will
+ * comply with the terms and conditions of the Educational Community License.
+ * 
+ * You may obtain a copy of the License at:
+ * 
+ * http://kualiproject.org/license.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.kuali.module.chart.rules;
+
+import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.util.SpringServiceLocator;
+import org.kuali.module.chart.bo.Org;
+
+public class OrgRule extends MaintenanceDocumentRuleBase {
+
+    protected static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(OrgRule.class);
+    
+    private Org oldOrg;
+    private Org newOrg;
+
+    private KualiConfigurationService configService;
+    
+    public OrgRule() {
+        super();
+
+        // Pseudo-inject some services.
+        //
+        // This approach is being used to make it simpler to convert the Rule classes 
+        // to spring-managed with these services injected by Spring at some later date.  
+        // When this happens, just remove these calls to the setters with 
+        // SpringServiceLocator, and configure the bean defs for spring.
+        this.setConfigService(SpringServiceLocator.getKualiConfigurationService());
+    }
+
+    /**
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomApproveDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */
+    protected boolean processCustomApproveDocumentBusinessRules(MaintenanceDocument document) {
+
+        LOG.info("Entering processCustomApproveDocumentBusinessRules()");
+    
+        //  check that all sub-objects whose keys are specified have matching objects in the db
+        checkExistenceAndActive();
+
+        return true;
+    }
+    
+    /**
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomRouteDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */
+    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+
+        boolean success = true;
+        
+        LOG.info("Entering processCustomRouteDocumentBusinessRules()");
+
+        //  check that all sub-objects whose keys are specified have matching objects in the db
+        success &= checkExistenceAndActive();
+
+        return success;
+    }
+    
+    /**
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomSaveDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */
+    protected boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
+
+        boolean success = true;
+        
+        LOG.info("Entering processCustomSaveDocumentBusinessRules()");
+
+        //  check that all sub-objects whose keys are specified have matching objects in the db
+        success &= checkExistenceAndActive();
+
+        return success;
+    }
+    
+    /**
+     * 
+     * This method sets the convenience objects like newAccount and oldAccount, so you
+     * have short and easy handles to the new and old objects contained in the 
+     * maintenance document.
+     * 
+     * It also calls the BusinessObjectBase.refresh(), which will attempt to load 
+     * all sub-objects from the DB by their primary keys, if available.
+     * 
+     * @param document - the maintenanceDocument being evaluated
+     * 
+     */
+    public void setupConvenienceObjects() {
+        
+        //  setup oldAccount convenience objects, make sure all possible sub-objects are populated
+        oldOrg = (Org) super.oldBo;
+
+        //  setup newAccount convenience objects, make sure all possible sub-objects are populated
+        newOrg = (Org) super.newBo;
+    }
+    
+    protected boolean checkExistenceAndActive() {
+        
+        LOG.info("Entering checkExistenceAndActive()");
+        boolean success = true;
+        
+        return success;
+    }
+
+    /**
+     * Sets the configService attribute value.
+     * @param configService The configService to set.
+     */
+    public void setConfigService(KualiConfigurationService configService) {
+        this.configService = configService;
+    }
+
+}

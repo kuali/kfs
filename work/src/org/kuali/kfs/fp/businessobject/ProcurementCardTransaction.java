0c4b8ea9c07923494d2d72ac4bc27ccd18b034b9
Teresa May <temay@indiana.edu>
2006-04-14 14:50:33 +0000

    KULDBA-121; redo procurement tables

--- work/src/org/kuali/kfs/fp/businessobject/ProcurementCardTransaction.java
+++ work/src/org/kuali/kfs/fp/businessobject/ProcurementCardTransaction.java
@@ -1,31 +1,31 @@
 /*
- * Copyright (c) 2004, 2005 The National Association of College and University
- * Business Officers, Cornell University, Trustees of Indiana University,
- * Michigan State University Board of Trustees, Trustees of San Joaquin Delta
- * College, University of Hawai'i, The Arizona Board of Regents on behalf of the
+ * Copyright (c) 2004, 2005 The National Association of College and University 
+ * Business Officers, Cornell University, Trustees of Indiana University, 
+ * Michigan State University Board of Trustees, Trustees of San Joaquin Delta 
+ * College, University of Hawai'i, The Arizona Board of Regents on behalf of the 
  * University of Arizona, and the r*smart group.
- *
- * Licensed under the Educational Community License Version 1.0 (the "License");
- * By obtaining, using and/or copying this Original Work, you agree that you
- * have read, understand, and will comply with the terms and conditions of the
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License"); 
+ * By obtaining, using and/or copying this Original Work, you agree that you 
+ * have read, understand, and will comply with the terms and conditions of the 
  * Educational Community License.
- *
+ * 
  * You may obtain a copy of the License at:
- *
+ * 
  * http://kualiproject.org/license.html
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  DAMAGES OR OTHER
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  DAMAGES OR OTHER 
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
  * THE SOFTWARE.
  */
 
 package org.kuali.module.financial.bo;
 
-import java.sql.Timestamp;
+import java.sql.Date;
 import java.util.LinkedHashMap;
 
 import org.kuali.core.bo.BusinessObjectBase;
@@ -47,18 +47,19 @@ public class ProcurementCardTransaction extends BusinessObjectBase {
 	private String accountNumber;
 	private String accountTypeCode;
 	private String subAccountNumber;
-	private Timestamp transactionCycleStartDate;
-	private Timestamp transactionCycleEndDate;
-	private String fdocCardHolderName;
-	private Timestamp transactionDate;
+	private Date transactionCycleStartDate;
+	private Date transactionCycleEndDate;
+	private String financialDocumentCardHolderName;
+	private Date transactionDate;
 	private String transactionReferenceNumber;
 	private String transactionVendorName;
-	private String transactionMerchantCategoryCd;
+	private String transactionMerchantCategoryCode;
+
+    private Account account;
 	private Chart chartOfAccounts;
-	private Account subAccount;
 
 	/**
-	 * Default no-arg constructor.
+	 * Default constructor.
 	 */
 	public ProcurementCardTransaction() {
 
@@ -66,354 +67,373 @@ public class ProcurementCardTransaction extends BusinessObjectBase {
 
 	/**
 	 * Gets the transactionIdentifier attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionIdentifier
-	 *
+	 * 
 	 */
-	public String getTransactionIdentifier() {
+	public String getTransactionIdentifier() { 
 		return transactionIdentifier;
 	}
 
-
 	/**
 	 * Sets the transactionIdentifier attribute.
-	 *
+	 * 
 	 * @param - transactionIdentifier The transactionIdentifier to set.
-	 *
+	 * 
 	 */
 	public void setTransactionIdentifier(String transactionIdentifier) {
 		this.transactionIdentifier = transactionIdentifier;
 	}
 
+
 	/**
 	 * Gets the transactionSequenceRowNumber attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionSequenceRowNumber
-	 *
+	 * 
 	 */
-	public Integer getTransactionSequenceRowNumber() {
+	public Integer getTransactionSequenceRowNumber() { 
 		return transactionSequenceRowNumber;
 	}
 
-
 	/**
 	 * Sets the transactionSequenceRowNumber attribute.
-	 *
+	 * 
 	 * @param - transactionSequenceRowNumber The transactionSequenceRowNumber to set.
-	 *
+	 * 
 	 */
 	public void setTransactionSequenceRowNumber(Integer transactionSequenceRowNumber) {
 		this.transactionSequenceRowNumber = transactionSequenceRowNumber;
 	}
 
+
 	/**
 	 * Gets the transactionCreditCardNumber attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionCreditCardNumber
-	 *
+	 * 
 	 */
-	public String getTransactionCreditCardNumber() {
+	public String getTransactionCreditCardNumber() { 
 		return transactionCreditCardNumber;
 	}
 
-
 	/**
 	 * Sets the transactionCreditCardNumber attribute.
-	 *
+	 * 
 	 * @param - transactionCreditCardNumber The transactionCreditCardNumber to set.
-	 *
+	 * 
 	 */
 	public void setTransactionCreditCardNumber(String transactionCreditCardNumber) {
 		this.transactionCreditCardNumber = transactionCreditCardNumber;
 	}
 
+
 	/**
 	 * Gets the financialDocumentTotalAmount attribute.
-	 *
+	 * 
 	 * @return - Returns the financialDocumentTotalAmount
-	 *
+	 * 
 	 */
-	public KualiDecimal getFinancialDocumentTotalAmount() {
+	public KualiDecimal getFinancialDocumentTotalAmount() { 
 		return financialDocumentTotalAmount;
 	}
 
-
 	/**
 	 * Sets the financialDocumentTotalAmount attribute.
-	 *
+	 * 
 	 * @param - financialDocumentTotalAmount The financialDocumentTotalAmount to set.
-	 *
+	 * 
 	 */
 	public void setFinancialDocumentTotalAmount(KualiDecimal financialDocumentTotalAmount) {
 		this.financialDocumentTotalAmount = financialDocumentTotalAmount;
 	}
 
+
 	/**
 	 * Gets the transactionDebitCreditCode attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionDebitCreditCode
-	 *
+	 * 
 	 */
-	public String getTransactionDebitCreditCode() {
+	public String getTransactionDebitCreditCode() { 
 		return transactionDebitCreditCode;
 	}
 
-
 	/**
 	 * Sets the transactionDebitCreditCode attribute.
-	 *
+	 * 
 	 * @param - transactionDebitCreditCode The transactionDebitCreditCode to set.
-	 *
+	 * 
 	 */
 	public void setTransactionDebitCreditCode(String transactionDebitCreditCode) {
 		this.transactionDebitCreditCode = transactionDebitCreditCode;
 	}
 
+
 	/**
 	 * Gets the chartOfAccountsCode attribute.
-	 *
+	 * 
 	 * @return - Returns the chartOfAccountsCode
-	 *
+	 * 
 	 */
-	public String getChartOfAccountsCode() {
+	public String getChartOfAccountsCode() { 
 		return chartOfAccountsCode;
 	}
 
-
 	/**
 	 * Sets the chartOfAccountsCode attribute.
-	 *
+	 * 
 	 * @param - chartOfAccountsCode The chartOfAccountsCode to set.
-	 *
+	 * 
 	 */
 	public void setChartOfAccountsCode(String chartOfAccountsCode) {
 		this.chartOfAccountsCode = chartOfAccountsCode;
 	}
 
+
 	/**
 	 * Gets the accountNumber attribute.
-	 *
+	 * 
 	 * @return - Returns the accountNumber
-	 *
+	 * 
 	 */
-	public String getAccountNumber() {
+	public String getAccountNumber() { 
 		return accountNumber;
 	}
 
-
 	/**
 	 * Sets the accountNumber attribute.
-	 *
+	 * 
 	 * @param - accountNumber The accountNumber to set.
-	 *
+	 * 
 	 */
 	public void setAccountNumber(String accountNumber) {
 		this.accountNumber = accountNumber;
 	}
 
+
 	/**
 	 * Gets the accountTypeCode attribute.
-	 *
+	 * 
 	 * @return - Returns the accountTypeCode
-	 *
+	 * 
 	 */
-	public String getAccountTypeCode() {
+	public String getAccountTypeCode() { 
 		return accountTypeCode;
 	}
 
-
 	/**
 	 * Sets the accountTypeCode attribute.
-	 *
+	 * 
 	 * @param - accountTypeCode The accountTypeCode to set.
-	 *
+	 * 
 	 */
 	public void setAccountTypeCode(String accountTypeCode) {
 		this.accountTypeCode = accountTypeCode;
 	}
 
+
 	/**
 	 * Gets the subAccountNumber attribute.
-	 *
+	 * 
 	 * @return - Returns the subAccountNumber
-	 *
+	 * 
 	 */
-	public String getSubAccountNumber() {
+	public String getSubAccountNumber() { 
 		return subAccountNumber;
 	}
 
-
 	/**
 	 * Sets the subAccountNumber attribute.
-	 *
+	 * 
 	 * @param - subAccountNumber The subAccountNumber to set.
-	 *
+	 * 
 	 */
 	public void setSubAccountNumber(String subAccountNumber) {
 		this.subAccountNumber = subAccountNumber;
 	}
 
+
 	/**
 	 * Gets the transactionCycleStartDate attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionCycleStartDate
-	 *
+	 * 
 	 */
-	public Timestamp getTransactionCycleStartDate() {
+	public Date getTransactionCycleStartDate() { 
 		return transactionCycleStartDate;
 	}
 
-
 	/**
 	 * Sets the transactionCycleStartDate attribute.
-	 *
+	 * 
 	 * @param - transactionCycleStartDate The transactionCycleStartDate to set.
-	 *
+	 * 
 	 */
-	public void setTransactionCycleStartDate(Timestamp transactionCycleStartDate) {
+	public void setTransactionCycleStartDate(Date transactionCycleStartDate) {
 		this.transactionCycleStartDate = transactionCycleStartDate;
 	}
 
+
 	/**
 	 * Gets the transactionCycleEndDate attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionCycleEndDate
-	 *
+	 * 
 	 */
-	public Timestamp getTransactionCycleEndDate() {
+	public Date getTransactionCycleEndDate() { 
 		return transactionCycleEndDate;
 	}
 
-
 	/**
 	 * Sets the transactionCycleEndDate attribute.
-	 *
+	 * 
 	 * @param - transactionCycleEndDate The transactionCycleEndDate to set.
-	 *
+	 * 
 	 */
-	public void setTransactionCycleEndDate(Timestamp transactionCycleEndDate) {
+	public void setTransactionCycleEndDate(Date transactionCycleEndDate) {
 		this.transactionCycleEndDate = transactionCycleEndDate;
 	}
 
-	/**
-	 * Gets the fdocCardHolderName attribute.
-	 *
-	 * @return - Returns the fdocCardHolderName
-	 *
-	 */
-	public String getFdocCardHolderName() {
-		return fdocCardHolderName;
-	}
-
 
 	/**
-	 * Sets the fdocCardHolderName attribute.
-	 *
-	 * @param - fdocCardHolderName The fdocCardHolderName to set.
-	 *
+	 * Gets the financialDocumentCardHolderName attribute.
+	 * 
+	 * @return - Returns the financialDocumentCardHolderName
+	 * 
 	 */
-	public void setFdocCardHolderName(String fdocCardHolderName) {
-		this.fdocCardHolderName = fdocCardHolderName;
+	public String getFinancialDocumentCardHolderName() { 
+		return financialDocumentCardHolderName;
 	}
+
+	/**
+	 * Sets the financialDocumentCardHolderName attribute.
+	 * 
+	 * @param - financialDocumentCardHolderName The financialDocumentCardHolderName to set.
+	 * 
+	 */
+	public void setFinancialDocumentCardHolderName(String financialDocumentCardHolderName) {
+		this.financialDocumentCardHolderName = financialDocumentCardHolderName;
+	}
+
 
 	/**
 	 * Gets the transactionDate attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionDate
-	 *
+	 * 
 	 */
-	public Timestamp getTransactionDate() {
+	public Date getTransactionDate() { 
 		return transactionDate;
 	}
 
-
 	/**
 	 * Sets the transactionDate attribute.
-	 *
+	 * 
 	 * @param - transactionDate The transactionDate to set.
-	 *
+	 * 
 	 */
-	public void setTransactionDate(Timestamp transactionDate) {
+	public void setTransactionDate(Date transactionDate) {
 		this.transactionDate = transactionDate;
 	}
 
+
 	/**
 	 * Gets the transactionReferenceNumber attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionReferenceNumber
-	 *
+	 * 
 	 */
-	public String getTransactionReferenceNumber() {
+	public String getTransactionReferenceNumber() { 
 		return transactionReferenceNumber;
 	}
 
-
 	/**
 	 * Sets the transactionReferenceNumber attribute.
-	 *
+	 * 
 	 * @param - transactionReferenceNumber The transactionReferenceNumber to set.
-	 *
+	 * 
 	 */
 	public void setTransactionReferenceNumber(String transactionReferenceNumber) {
 		this.transactionReferenceNumber = transactionReferenceNumber;
 	}
 
+
 	/**
 	 * Gets the transactionVendorName attribute.
-	 *
+	 * 
 	 * @return - Returns the transactionVendorName
-	 *
+	 * 
 	 */
-	public String getTransactionVendorName() {
+	public String getTransactionVendorName() { 
 		return transactionVendorName;
 	}
 
-
 	/**
 	 * Sets the transactionVendorName attribute.
-	 *
+	 * 
 	 * @param - transactionVendorName The transactionVendorName to set.
-	 *
+	 * 
 	 */
 	public void setTransactionVendorName(String transactionVendorName) {
 		this.transactionVendorName = transactionVendorName;
 	}
 
+
 	/**
-	 * Gets the transactionMerchantCategoryCd attribute.
-	 *
-	 * @return - Returns the transactionMerchantCategoryCd
-	 *
+	 * Gets the transactionMerchantCategoryCode attribute.
+	 * 
+	 * @return - Returns the transactionMerchantCategoryCode
+	 * 
 	 */
-	public String getTransactionMerchantCategoryCd() {
-		return transactionMerchantCategoryCd;
+	public String getTransactionMerchantCategoryCode() { 
+		return transactionMerchantCategoryCode;
+	}
+
+	/**
+	 * Sets the transactionMerchantCategoryCode attribute.
+	 * 
+	 * @param - transactionMerchantCategoryCode The transactionMerchantCategoryCode to set.
+	 * 
+	 */
+	public void setTransactionMerchantCategoryCode(String transactionMerchantCategoryCode) {
+		this.transactionMerchantCategoryCode = transactionMerchantCategoryCode;
 	}
 
 
 	/**
-	 * Sets the transactionMerchantCategoryCd attribute.
-	 *
-	 * @param - transactionMerchantCategoryCd The transactionMerchantCategoryCd to set.
-	 *
+	 * Gets the account attribute.
+	 * 
+	 * @return - Returns the account
+	 * 
 	 */
-	public void setTransactionMerchantCategoryCd(String transactionMerchantCategoryCd) {
-		this.transactionMerchantCategoryCd = transactionMerchantCategoryCd;
+	public Account getAccount() { 
+		return account;
+	}
+
+	/**
+	 * Sets the account attribute.
+	 * 
+	 * @param - account The account to set.
+	 * @deprecated
+	 */
+	public void setAccount(Account account) {
+		this.account = account;
 	}
 
 	/**
 	 * Gets the chartOfAccounts attribute.
-	 *
+	 * 
 	 * @return - Returns the chartOfAccounts
-	 *
+	 * 
 	 */
-	public Chart getChartOfAccounts() {
+	public Chart getChartOfAccounts() { 
 		return chartOfAccounts;
 	}
 
-
 	/**
 	 * Sets the chartOfAccounts attribute.
-	 *
+	 * 
 	 * @param - chartOfAccounts The chartOfAccounts to set.
 	 * @deprecated
 	 */
@@ -422,33 +442,14 @@ public class ProcurementCardTransaction extends BusinessObjectBase {
 	}
 
 	/**
-	 * Gets the subAccount attribute.
-	 *
-	 * @return - Returns the subAccount
-	 *
-	 */
-	public Account getSubAccount() {
-		return subAccount;
-	}
-
-
-	/**
-	 * Sets the subAccount attribute.
-	 *
-	 * @param - subAccount The subAccount to set.
-	 * @deprecated
-	 */
-	public void setSubAccount(Account subAccount) {
-		this.subAccount = subAccount;
-	}
-
-	/**
 	 * @see org.kuali.bo.BusinessObjectBase#toStringMapper()
 	 */
 	protected LinkedHashMap toStringMapper() {
 	    LinkedHashMap m = new LinkedHashMap();
-        m.put("transactionIdentifier", getTransactionIdentifier());
-        m.put("transactionSequenceRowNumber", getTransactionSequenceRowNumber().toString());
+        m.put("transactionIdentifier", this.transactionIdentifier);
+        if (this.transactionSequenceRowNumber != null) {
+            m.put("transactionSequenceRowNumber", this.transactionSequenceRowNumber.toString());
+        }
 	    return m;
-	}
+    }
 }

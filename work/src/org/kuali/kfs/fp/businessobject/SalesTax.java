e0e6ede3f965e3adae5c435470474b03a80a4565
Teresa May <temay@indiana.edu>
2006-05-17 16:53:44 +0000

    converted FP_SALES_TAX_T for the Kuali project

---
+++ work/src/org/kuali/kfs/fp/businessobject/SalesTax.java
@@ -0,0 +1,280 @@
+/*
+ * Copyright (c) 2004, 2005 The National Association of College and University 
+ * Business Officers, Cornell University, Trustees of Indiana University, 
+ * Michigan State University Board of Trustees, Trustees of San Joaquin Delta 
+ * College, University of Hawai'i, The Arizona Board of Regents on behalf of the 
+ * University of Arizona, and the r*smart group.
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License"); 
+ * By obtaining, using and/or copying this Original Work, you agree that you 
+ * have read, understand, and will comply with the terms and conditions of the 
+ * Educational Community License.
+ * 
+ * You may obtain a copy of the License at:
+ * 
+ * http://kualiproject.org/license.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ */
+
+package org.kuali.module.financial.bo;
+
+import java.sql.Date;
+import java.util.LinkedHashMap;
+
+import org.kuali.core.bo.BusinessObjectBase;
+import org.kuali.core.util.KualiDecimal;
+import org.kuali.module.chart.bo.Account;
+import org.kuali.module.chart.bo.Chart;
+
+/**
+ * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
+ */
+public class SalesTax extends BusinessObjectBase {
+
+	private String financialDocumentNumber;
+	private String financialDocumentLineTypeCode;
+	private Integer financialDocumentLineNumber;
+	private String chartOfAccountsCode;
+	private String accountNumber;
+	private KualiDecimal financialDocumentGrossSalesAmount;
+	private KualiDecimal financialDocumentTaxableSalesAmount;
+	private Date financialDocumentSaleDate;
+
+    private Account account;
+	private Chart chartOfAccounts;
+
+	/**
+	 * Default constructor.
+	 */
+	public SalesTax() {
+
+	}
+
+	/**
+	 * Gets the financialDocumentNumber attribute.
+	 * 
+	 * @return - Returns the financialDocumentNumber
+	 * 
+	 */
+	public String getFinancialDocumentNumber() { 
+		return financialDocumentNumber;
+	}
+
+	/**
+	 * Sets the financialDocumentNumber attribute.
+	 * 
+	 * @param - financialDocumentNumber The financialDocumentNumber to set.
+	 * 
+	 */
+	public void setFinancialDocumentNumber(String financialDocumentNumber) {
+		this.financialDocumentNumber = financialDocumentNumber;
+	}
+
+
+	/**
+	 * Gets the financialDocumentLineTypeCode attribute.
+	 * 
+	 * @return - Returns the financialDocumentLineTypeCode
+	 * 
+	 */
+	public String getFinancialDocumentLineTypeCode() { 
+		return financialDocumentLineTypeCode;
+	}
+
+	/**
+	 * Sets the financialDocumentLineTypeCode attribute.
+	 * 
+	 * @param - financialDocumentLineTypeCode The financialDocumentLineTypeCode to set.
+	 * 
+	 */
+	public void setFinancialDocumentLineTypeCode(String financialDocumentLineTypeCode) {
+		this.financialDocumentLineTypeCode = financialDocumentLineTypeCode;
+	}
+
+
+	/**
+	 * Gets the financialDocumentLineNumber attribute.
+	 * 
+	 * @return - Returns the financialDocumentLineNumber
+	 * 
+	 */
+	public Integer getFinancialDocumentLineNumber() { 
+		return financialDocumentLineNumber;
+	}
+
+	/**
+	 * Sets the financialDocumentLineNumber attribute.
+	 * 
+	 * @param - financialDocumentLineNumber The financialDocumentLineNumber to set.
+	 * 
+	 */
+	public void setFinancialDocumentLineNumber(Integer financialDocumentLineNumber) {
+		this.financialDocumentLineNumber = financialDocumentLineNumber;
+	}
+
+
+	/**
+	 * Gets the chartOfAccountsCode attribute.
+	 * 
+	 * @return - Returns the chartOfAccountsCode
+	 * 
+	 */
+	public String getChartOfAccountsCode() { 
+		return chartOfAccountsCode;
+	}
+
+	/**
+	 * Sets the chartOfAccountsCode attribute.
+	 * 
+	 * @param - chartOfAccountsCode The chartOfAccountsCode to set.
+	 * 
+	 */
+	public void setChartOfAccountsCode(String chartOfAccountsCode) {
+		this.chartOfAccountsCode = chartOfAccountsCode;
+	}
+
+
+	/**
+	 * Gets the accountNumber attribute.
+	 * 
+	 * @return - Returns the accountNumber
+	 * 
+	 */
+	public String getAccountNumber() { 
+		return accountNumber;
+	}
+
+	/**
+	 * Sets the accountNumber attribute.
+	 * 
+	 * @param - accountNumber The accountNumber to set.
+	 * 
+	 */
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+
+	/**
+	 * Gets the financialDocumentGrossSalesAmount attribute.
+	 * 
+	 * @return - Returns the financialDocumentGrossSalesAmount
+	 * 
+	 */
+	public KualiDecimal getFinancialDocumentGrossSalesAmount() { 
+		return financialDocumentGrossSalesAmount;
+	}
+
+	/**
+	 * Sets the financialDocumentGrossSalesAmount attribute.
+	 * 
+	 * @param - financialDocumentGrossSalesAmount The financialDocumentGrossSalesAmount to set.
+	 * 
+	 */
+	public void setFinancialDocumentGrossSalesAmount(KualiDecimal financialDocumentGrossSalesAmount) {
+		this.financialDocumentGrossSalesAmount = financialDocumentGrossSalesAmount;
+	}
+
+
+	/**
+	 * Gets the financialDocumentTaxableSalesAmount attribute.
+	 * 
+	 * @return - Returns the financialDocumentTaxableSalesAmount
+	 * 
+	 */
+	public KualiDecimal getFinancialDocumentTaxableSalesAmount() { 
+		return financialDocumentTaxableSalesAmount;
+	}
+
+	/**
+	 * Sets the financialDocumentTaxableSalesAmount attribute.
+	 * 
+	 * @param - financialDocumentTaxableSalesAmount The financialDocumentTaxableSalesAmount to set.
+	 * 
+	 */
+	public void setFinancialDocumentTaxableSalesAmount(KualiDecimal financialDocumentTaxableSalesAmount) {
+		this.financialDocumentTaxableSalesAmount = financialDocumentTaxableSalesAmount;
+	}
+
+
+	/**
+	 * Gets the financialDocumentSaleDate attribute.
+	 * 
+	 * @return - Returns the financialDocumentSaleDate
+	 * 
+	 */
+	public Date getFinancialDocumentSaleDate() { 
+		return financialDocumentSaleDate;
+	}
+
+	/**
+	 * Sets the financialDocumentSaleDate attribute.
+	 * 
+	 * @param - financialDocumentSaleDate The financialDocumentSaleDate to set.
+	 * 
+	 */
+	public void setFinancialDocumentSaleDate(Date financialDocumentSaleDate) {
+		this.financialDocumentSaleDate = financialDocumentSaleDate;
+	}
+
+
+	/**
+	 * Gets the account attribute.
+	 * 
+	 * @return - Returns the account
+	 * 
+	 */
+	public Account getAccount() { 
+		return account;
+	}
+
+	/**
+	 * Sets the account attribute.
+	 * 
+	 * @param - account The account to set.
+	 * @deprecated
+	 */
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	/**
+	 * Gets the chartOfAccounts attribute.
+	 * 
+	 * @return - Returns the chartOfAccounts
+	 * 
+	 */
+	public Chart getChartOfAccounts() { 
+		return chartOfAccounts;
+	}
+
+	/**
+	 * Sets the chartOfAccounts attribute.
+	 * 
+	 * @param - chartOfAccounts The chartOfAccounts to set.
+	 * @deprecated
+	 */
+	public void setChartOfAccounts(Chart chartOfAccounts) {
+		this.chartOfAccounts = chartOfAccounts;
+	}
+
+	/**
+	 * @see org.kuali.bo.BusinessObjectBase#toStringMapper()
+	 */
+	protected LinkedHashMap toStringMapper() {
+	    LinkedHashMap m = new LinkedHashMap();	    
+        m.put("financialDocumentNumber", this.financialDocumentNumber);
+        m.put("financialDocumentLineTypeCode", this.financialDocumentLineTypeCode);
+        if (this.financialDocumentLineNumber != null) {
+            m.put("financialDocumentLineNumber", this.financialDocumentLineNumber.toString());
+        }
+	    return m;
+    }
+}

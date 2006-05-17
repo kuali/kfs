a3205d43c6387287c6cb759a1663eff5441d3c52
Geoff McGregor <gmcgrego@iu.edu>
2006-05-17 14:49:55 +0000

    KULERA-133: Added class to encapsulate audit error information

---
+++ work/src/org/kuali/kfs/module/cg/document/validation/impl/AuditError.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2004, 2005 The National Association of College and University Business Officers,
+ * Cornell University, Trustees of Indiana University, Michigan State University Board of Trustees,
+ * Trustees of San Joaquin Delta College, University of Hawai'i, The Arizona Board of Regents on
+ * behalf of the University of Arizona, and the r*smart group.
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License"); By obtaining,
+ * using and/or copying this Original Work, you agree that you have read, understand, and will
+ * comply with the terms and conditions of the Educational Community License.
+ * 
+ * You may obtain a copy of the License at:
+ * 
+ * http://kualiproject.org/license.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.kuali.module.kra.util;
+
+public class AuditError {
+    
+    private String key;
+    private String link;
+    
+    public AuditError(String key, String link) {
+        this.setKey(key);
+        this.setLink(link);
+    }
+
+    /**
+     * Gets the link attribute. 
+     * @return Returns the link.
+     */
+    public String getLink() {
+        return link;
+    }
+
+    /**
+     * Sets the link attribute value.
+     * @param link The link to set.
+     */
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    /**
+     * Gets the key attribute. 
+     * @return Returns the key.
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the key attribute value.
+     * @param key The key to set.
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+}

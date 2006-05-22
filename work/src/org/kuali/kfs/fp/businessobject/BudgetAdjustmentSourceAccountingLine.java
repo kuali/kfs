232a816b87d037897a41aaf9b17b820152428366
Teresa May <temay@indiana.edu>
2006-05-22 21:07:14 +0000

    combining FP_BDGT_ADJ_DTL_T and FP_BADJ_MNTH_DTL_T into one table named FP_BDGT_ADJ_DTL_T

--- work/src/org/kuali/kfs/fp/businessobject/BudgetAdjustmentSourceAccountingLine.java
+++ work/src/org/kuali/kfs/fp/businessobject/BudgetAdjustmentSourceAccountingLine.java
@@ -34,9 +34,19 @@ public class BudgetAdjustmentSourceAccountingLine extends SourceAccountingLine {
 	private String budgetAdjustmentPeriodCode;
     private KualiDecimal currentBudgetAdjustmentAmount;
     private KualiDecimal baseBudgetAdjustmentAmount;
+    private KualiDecimal financialDocumentMonth1LineAmount;
+    private KualiDecimal financialDocumentMonth2LineAmount;
+    private KualiDecimal financialDocumentMonth3LineAmount;
+    private KualiDecimal financialDocumentMonth4LineAmount;
+    private KualiDecimal financialDocumentMonth5LineAmount;
+    private KualiDecimal financialDocumentMonth6LineAmount;
+    private KualiDecimal financialDocumentMonth7LineAmount;
+    private KualiDecimal financialDocumentMonth8LineAmount;
+    private KualiDecimal financialDocumentMonth9LineAmount;
+    private KualiDecimal financialDocumentMonth10LineAmount;
+    private KualiDecimal financialDocumentMonth11LineAmount;
+    private KualiDecimal financialDocumentMonth12LineAmount;
     
-    private BudgetAdjustmentSourceMonth budgetAdjustmentSourceMonth;
-	
     /**
 	 * This constructor needs to initialize the ojbConcreteClass attribute such
 	 * that it sets it to its class name. This is how OJB knows what grouping of
@@ -73,12 +83,255 @@ public class BudgetAdjustmentSourceAccountingLine extends SourceAccountingLine {
 		this.currentBudgetAdjustmentAmount = currentBudgetAdjustmentAmount;
 	}
 
-    public BudgetAdjustmentSourceMonth getBudgetAdjustmentSourceMonth() {
-        return budgetAdjustmentSourceMonth;
+    /**
+     * Gets the financialDocumentMonth1LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth1LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth1LineAmount() { 
+        return financialDocumentMonth1LineAmount;
+    }   
+    
+    /**
+     * Sets the financialDocumentMonth1LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth1LineAmount The financialDocumentMonth1LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth1LineAmount(KualiDecimal financialDocumentMonth1LineAmount) {
+        this.financialDocumentMonth1LineAmount = financialDocumentMonth1LineAmount;
     }
 
-    public void setBudgetAdjustmentSourceMonth(BudgetAdjustmentSourceMonth budgetAdjustmentSourceMonth) {
-        this.budgetAdjustmentSourceMonth = budgetAdjustmentSourceMonth;
+
+    /**
+     * Gets the financialDocumentMonth2LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth2LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth2LineAmount() { 
+        return financialDocumentMonth2LineAmount;
     }
-	
+
+    /**
+     * Sets the financialDocumentMonth2LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth2LineAmount The financialDocumentMonth2LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth2LineAmount(KualiDecimal financialDocumentMonth2LineAmount) {
+        this.financialDocumentMonth2LineAmount = financialDocumentMonth2LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth3LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth3LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth3LineAmount() { 
+        return financialDocumentMonth3LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth3LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth3LineAmount The financialDocumentMonth3LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth3LineAmount(KualiDecimal financialDocumentMonth3LineAmount) {
+        this.financialDocumentMonth3LineAmount = financialDocumentMonth3LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth4LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth4LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth4LineAmount() { 
+        return financialDocumentMonth4LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth4LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth4LineAmount The financialDocumentMonth4LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth4LineAmount(KualiDecimal financialDocumentMonth4LineAmount) {
+        this.financialDocumentMonth4LineAmount = financialDocumentMonth4LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth5LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth5LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth5LineAmount() { 
+        return financialDocumentMonth5LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth5LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth5LineAmount The financialDocumentMonth5LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth5LineAmount(KualiDecimal financialDocumentMonth5LineAmount) {
+        this.financialDocumentMonth5LineAmount = financialDocumentMonth5LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth6LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth6LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth6LineAmount() { 
+        return financialDocumentMonth6LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth6LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth6LineAmount The financialDocumentMonth6LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth6LineAmount(KualiDecimal financialDocumentMonth6LineAmount) {
+        this.financialDocumentMonth6LineAmount = financialDocumentMonth6LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth7LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth7LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth7LineAmount() { 
+        return financialDocumentMonth7LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth7LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth7LineAmount The financialDocumentMonth7LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth7LineAmount(KualiDecimal financialDocumentMonth7LineAmount) {
+        this.financialDocumentMonth7LineAmount = financialDocumentMonth7LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth8LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth8LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth8LineAmount() { 
+        return financialDocumentMonth8LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth8LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth8LineAmount The financialDocumentMonth8LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth8LineAmount(KualiDecimal financialDocumentMonth8LineAmount) {
+        this.financialDocumentMonth8LineAmount = financialDocumentMonth8LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth9LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth9LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth9LineAmount() { 
+        return financialDocumentMonth9LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth9LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth9LineAmount The financialDocumentMonth9LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth9LineAmount(KualiDecimal financialDocumentMonth9LineAmount) {
+        this.financialDocumentMonth9LineAmount = financialDocumentMonth9LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth10LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth10LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth10LineAmount() { 
+        return financialDocumentMonth10LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth10LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth10LineAmount The financialDocumentMonth10LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth10LineAmount(KualiDecimal financialDocumentMonth10LineAmount) {
+        this.financialDocumentMonth10LineAmount = financialDocumentMonth10LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth11LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth11LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth11LineAmount() { 
+        return financialDocumentMonth11LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth11LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth11LineAmount The financialDocumentMonth11LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth11LineAmount(KualiDecimal financialDocumentMonth11LineAmount) {
+        this.financialDocumentMonth11LineAmount = financialDocumentMonth11LineAmount;
+    }
+
+
+    /**
+     * Gets the financialDocumentMonth12LineAmount attribute.
+     * 
+     * @return - Returns the financialDocumentMonth12LineAmount
+     * 
+     */
+    public KualiDecimal getFinancialDocumentMonth12LineAmount() { 
+        return financialDocumentMonth12LineAmount;
+    }
+
+    /**
+     * Sets the financialDocumentMonth12LineAmount attribute.
+     * 
+     * @param - financialDocumentMonth12LineAmount The financialDocumentMonth12LineAmount to set.
+     * 
+     */
+    public void setFinancialDocumentMonth12LineAmount(KualiDecimal financialDocumentMonth12LineAmount) {
+        this.financialDocumentMonth12LineAmount = financialDocumentMonth12LineAmount;
+    }
+
 }

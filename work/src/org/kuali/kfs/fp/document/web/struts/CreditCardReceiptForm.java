dd8d77f936aa6df2e667bde9c149c20bed7612b7
Dennis Friends <dwf5@cornell.edu>
2006-06-23 17:17:26 +0000

    KULEDOCS-1325
    Added an override of the parent's populate() method and used it to traverse through the associated credit card receipts and apply all the forceUppercase rules to each line, as were defined in the CreditCardDetail.xml file.

--- work/src/org/kuali/kfs/fp/document/web/struts/CreditCardReceiptForm.java
+++ work/src/org/kuali/kfs/fp/document/web/struts/CreditCardReceiptForm.java
@@ -22,7 +22,13 @@
  */
 package org.kuali.module.financial.web.struts.form;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.kuali.core.util.SpringServiceLocator;
 import org.kuali.core.web.struts.form.KualiTransactionalDocumentFormBase;
+import org.kuali.module.financial.bo.AdvanceDepositDetail;
 import org.kuali.module.financial.bo.CreditCardDetail;
 import org.kuali.module.financial.document.CreditCardReceiptDocument;
 
@@ -62,5 +68,26 @@ public class CreditCardReceiptForm extends KualiTransactionalDocumentFormBase {
      */
     public void setNewCreditCardReceipt(CreditCardDetail newCreditCardReceipt) {
         this.newCreditCardReceipt = newCreditCardReceipt;
+    }
+    
+    /**
+     * Overrides the parent to call super.populate and then tells each line to check the associated data dictionary
+     * and modify the values entered to follow all the attributes set for the values of the accounting line.
+     * 
+     * @see org.kuali.core.web.struts.form.KualiTransactionalDocumentFormBase#populate(javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    public void populate(HttpServletRequest request) {
+        super.populate(request);
+
+        //
+        // now run through all of the accounting lines and make sure they've been uppercased and populated appropriately
+        SpringServiceLocator.getBusinessObjectDictionaryService().performForceUppercase(getNewCreditCardReceipt());
+        
+        List<CreditCardDetail> creditCardReceipts = getCreditCardReceiptDocument().getCreditCardReceipts();
+        for (CreditCardDetail detail : creditCardReceipts) {
+            SpringServiceLocator.getBusinessObjectDictionaryService().performForceUppercase(detail);
+       }
+
     }
 }

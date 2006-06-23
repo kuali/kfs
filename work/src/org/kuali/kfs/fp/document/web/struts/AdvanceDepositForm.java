47e437cd8053f9781b97c0c646aaca9ff1ae6ef3
Dennis Friends <dwf5@cornell.edu>
2006-06-23 15:32:12 +0000

    KULEDOCS-1363
    Added an override of the parent's populate() method and used it to traverse through the associated advance deposits and apply all the forceUppercase rules to each line, as were defined in the AdvanceDeposit.xml file.

--- work/src/org/kuali/kfs/fp/document/web/struts/AdvanceDepositForm.java
+++ work/src/org/kuali/kfs/fp/document/web/struts/AdvanceDepositForm.java
@@ -22,6 +22,11 @@
  */
 package org.kuali.module.financial.web.struts.form;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.kuali.core.util.SpringServiceLocator;
 import org.kuali.core.web.struts.form.KualiTransactionalDocumentFormBase;
 import org.kuali.module.financial.bo.AdvanceDepositDetail;
 import org.kuali.module.financial.document.AdvanceDepositDocument;
@@ -62,5 +67,23 @@ public class AdvanceDepositForm extends KualiTransactionalDocumentFormBase {
      */
     public void setNewAdvanceDeposit(AdvanceDepositDetail newAdvanceDeposit) {
         this.newAdvanceDeposit = newAdvanceDeposit;
+    }
+    
+    /**
+     * Overrides the parent to call super.populate and then tells each line to check the associated data dictionary
+     * and modify the values entered to follow all the attributes set for the values of the accounting line.
+     * 
+     * @see org.kuali.core.web.struts.form.KualiTransactionalDocumentFormBase#populate(javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    public void populate(HttpServletRequest request) {
+        super.populate(request);
+
+        SpringServiceLocator.getBusinessObjectDictionaryService().performForceUppercase(getNewAdvanceDeposit());
+        
+        List<AdvanceDepositDetail> advancedDeposits = getAdvanceDepositDocument().getAdvanceDeposits();
+        for (AdvanceDepositDetail detail : advancedDeposits) {
+            SpringServiceLocator.getBusinessObjectDictionaryService().performForceUppercase(detail);
+       }
     }
 }

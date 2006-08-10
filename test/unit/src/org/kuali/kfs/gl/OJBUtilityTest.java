8277a9fc9070fbee831feac71e893501268fdccb
Bin Gao <gaob@msu.edu>
2006-08-10 15:42:18 +0000

    Utilities that are used to build OJB criteria and query based on the given field-value pairs

---
+++ test/unit/src/org/kuali/kfs/gl/OJBUtilityTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2004, 2005 The National Association of College and University Business Officers,
+ * Cornell University, Trustees of Indiana University, Michigan State University Board of Trustees,
+ * Trustees of San Joaquin Delta College, University of Hawai'i, The Arizona Board of Regents on
+ * behalf of the University of Arizona, and the r*smart group.
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License"); By obtaining,
+ * using and/or copying this Original Work, you agree that you have read, understand, and will
+ * comply with the terms and conditions of the Educational Community License.
+ * 
+ * You may obtain a copy of the License at:
+ * 
+ * http://kualiproject.org/license.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.kuali.module.gl.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ojb.broker.query.Criteria;
+import org.kuali.PropertyConstants;
+import org.kuali.module.gl.bo.AccountBalance;
+import org.kuali.module.gl.bo.DummyBusinessObject;
+import org.kuali.module.gl.web.Constant;
+import org.kuali.test.KualiTestBaseWithSpringOnly;
+
+/**
+ * This class is the JUnit test case applied on the BusinessObjectHandler class
+ * 
+ * @author Bin Gao from Michigan State University
+ */
+public class OJBUtilityTest extends KualiTestBaseWithSpringOnly {
+
+    // test cases for buildPropertyMap method
+    public void testBuildPropertyMap() throws Exception{
+        DummyBusinessObject dummyBusinessObject = new DummyBusinessObject();
+
+        dummyBusinessObject.setAmountViewOption(Constant.ACCUMULATE);
+        Map propertyMap = OJBUtility.buildPropertyMap(dummyBusinessObject);
+
+        String amountViewOption = (String) propertyMap.get("amountViewOption");
+        assertEquals(amountViewOption, Constant.ACCUMULATE);
+    }
+
+    // test cases for buildCriteriaFromMap method
+    public void testBuildCriteriaFromMap() throws Exception{
+        AccountBalance accountBalance = new AccountBalance();
+
+        Map propertyMap1 = new HashMap();
+        propertyMap1.put("accountNumber", "2004");
+        propertyMap1.put("chartOfAccountsCode", "");
+        propertyMap1.put("objectCode", null);
+        Criteria criteria1 = OJBUtility.buildCriteriaFromMap(propertyMap1, accountBalance);
+
+        String criteria1Value = criteria1.toString();
+        assertTrue(criteria1Value.indexOf("accountNumber") >= 0);
+        assertTrue(criteria1Value.indexOf("chartOfAccountsCode") < 0);
+        assertTrue(criteria1Value.indexOf("objectCode") < 0);
+
+        accountBalance.setAccountNumber("2004");
+        accountBalance.setChartOfAccountsCode("");
+        accountBalance.setObjectCode(null);
+        // Map propertyMap2 = BusinessObjectHandler.buildPropertyMap(accountBalance);
+        // Criteria criteria2 = BusinessObjectHandler.buildCriteriaFromMap(propertyMap1, accountBalance);
+
+        String criteria2Value = criteria1.toString();
+        assertTrue(criteria2Value.indexOf("accountNumber") >= 0);
+        assertTrue(criteria2Value.indexOf("chartOfAccountsCode") < 0);
+        assertTrue(criteria2Value.indexOf("objectCode") < 0);
+        assertTrue(criteria2Value.indexOf("subObjectCode") < 0);
+    }
+    
+    public void testGetResultSizeFromMap() throws Exception{
+        Map propertyMap = new HashMap();
+        propertyMap.put(PropertyConstants.UNIVERSITY_FISCAL_YEAR, "2007");
+        
+        Long resultSize = OJBUtility.getResultSizeFromMap(propertyMap, new AccountBalance());      
+        assertTrue("Should be greater than 0 if there are account balance records", resultSize.intValue() > 0);       
+    }
+    
+    public void testGetResultLimit(){
+        Integer limit = OJBUtility.getResultLimit();      
+        assertTrue("Should be greater than 0", limit.intValue() == 200);        
+    }
+}
---
+++ work/src/org/kuali/kfs/gl/OJBUtility.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2004, 2005 The National Association of College and University Business Officers,
+ * Cornell University, Trustees of Indiana University, Michigan State University Board of Trustees,
+ * Trustees of San Joaquin Delta College, University of Hawai'i, The Arizona Board of Regents on
+ * behalf of the University of Arizona, and the r*smart group.
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License"); By obtaining,
+ * using and/or copying this Original Work, you agree that you have read, understand, and will
+ * comply with the terms and conditions of the Educational Community License.
+ * 
+ * You may obtain a copy of the License at:
+ * 
+ * http://kualiproject.org/license.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.kuali.module.gl.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.beanutils.DynaClass;
+import org.apache.commons.beanutils.DynaProperty;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.beanutils.WrapDynaClass;
+import org.apache.commons.lang.StringUtils;
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryFactory;
+import org.kuali.Constants;
+import org.kuali.KeyConstants;
+import org.kuali.core.dao.LookupDao;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.service.impl.LookupServiceImpl;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.util.ObjectUtils;
+import org.kuali.core.util.SpringServiceLocator;
+import org.kuali.core.util.TypeUtils;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.ojb.OjbOperationException;
+
+/**
+ * This class provides a set of utilities that can handle common tasks related to business objects.
+ * 
+ * @author Bin Gao from Michigan State University
+ */
+public class OJBUtility {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(OJBUtility.class);
+    private static BeanFactory beanFactory = SpringServiceLocator.getBeanFactory();
+
+    /**
+     * This method builds a map of business object with its property names and values
+     * 
+     * @param businessObject the given business object
+     * @return the map of business object with its property names and values
+     */
+    public static LinkedHashMap buildPropertyMap(Object businessObject) {
+        DynaClass dynaClass = WrapDynaClass.createDynaClass(businessObject.getClass());
+        DynaProperty[] properties = dynaClass.getDynaProperties();
+        LinkedHashMap propertyMap = new LinkedHashMap();
+
+        try {
+            for (int numOfProperty = 0; numOfProperty < properties.length; numOfProperty++) {
+                String propertyName = properties[numOfProperty].getName();
+                if (PropertyUtils.isWriteable(businessObject, propertyName)) {
+                    Object propertyValue = PropertyUtils.getProperty(businessObject, propertyName);
+                    propertyMap.put(propertyName, propertyValue);
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return propertyMap;
+    }
+
+    /**
+     * This method builds an OJB query criteria based on the input field map
+     * 
+     * @param fieldValues the input field map
+     * @param businessObject the given business object
+     * @return an OJB query criteria
+     */
+    public static Criteria buildCriteriaFromMap(Map fieldValues, Object businessObject) {
+        Criteria criteria = new Criteria();
+
+        Iterator propsIter = fieldValues.keySet().iterator();
+        while (propsIter.hasNext()) {
+            String propertyName = (String) propsIter.next();
+            String propertyValue = (String) fieldValues.get(propertyName);
+            propertyValue = (propertyValue != null) ? propertyValue.trim() : "";
+
+            // if searchValue is empty and the key is not a valid property ignore
+            boolean isCreated = createCriteria(businessObject, propertyValue, propertyName, criteria);
+            if (!isCreated) {
+                continue;
+            }
+        }
+        return criteria;
+    }
+    
+    /**
+     * limit the size of the result set from the given query operation
+     * @param query the given query operation
+     */
+    public static void limitResultSize(Query query){
+        int startingIndex = 1;
+        int endingIndex = getResultLimit().intValue();
+        
+        query.setStartAtIndex(startingIndex);
+        query.setEndAtIndex(endingIndex);
+    }
+    
+    /**
+     * This method calculates the actual size of given selection results
+     * @param result the given selection results
+     * @param recordCount the possible number of the given results
+     * @param fieldValues the input field map
+     * @param businessObject the given business object
+     * @return the actual size of given selection results
+     */
+    public static Long getResultActualSize(Collection result, Integer recordCount, Map fieldValues, Object businessObject){
+        int resultSize = result.size();
+        Integer limit = getResultLimit();       
+        Long resultActualSize = new Long(resultSize);
+        
+        if (recordCount > limit) {
+            long actualCount = recordCount.longValue() + resultSize - limit.longValue();
+            resultActualSize = new Long(actualCount);
+        }
+        return resultActualSize;
+    }
+    
+    /**
+     * This method gets the size of a result set from the given search criteria
+     * @param fieldValues the input field map
+     * @param businessObject the given business object
+     * @return the size of a result set from the given search criteria
+     */
+    public static Long getResultSizeFromMap(Map fieldValues, Object businessObject){
+        LookupDao lookupDao = (LookupDao)beanFactory.getBean("lookupDao"); 
+        return lookupDao.findCountByMap(businessObject, fieldValues);
+    }
+       
+    /**
+     * This method gets the limit of the selection results
+     * @return the limit of the selection results
+     */
+    public static Integer getResultLimit(){
+        // get the result limit number from configuration
+        KualiConfigurationService kualiConfigurationService = (KualiConfigurationService)beanFactory.getBean("kualiConfigurationService");
+        String limitConfig = kualiConfigurationService.getPropertyString("lookup.results.limit");
+        
+        Integer limit = Integer.MAX_VALUE;
+        if (limitConfig != null) {
+            limit = Integer.valueOf(limitConfig);
+        }        
+        return limit;
+    }
+
+    /**
+     * This method build OJB criteria from the given property value and name
+     */
+    public static boolean createCriteria(Object businessObject, String propertyValue, String propertyName, Criteria criteria) {
+        LookupDao lookupDao = (LookupDao)beanFactory.getBean("lookupDao");       
+        return lookupDao.createCriteria(businessObject, propertyValue, propertyName, criteria);
+    }
+}

23ab73c3c43a694a45fa158b057bd1040fd61bba
Wes Price <wprice@kuali.org>
2006-08-14 17:53:30 +0000

    Changed case-sensitive username comparisons to case-insensitive, since that was causing an error in CashManagementDocumentRule and might eventually cause an error in ProcurementCardDocumentAuthorizer

--- work/src/org/kuali/kfs/fp/document/validation/impl/CashManagementDocumentRule.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/CashManagementDocumentRule.java
@@ -92,7 +92,7 @@ public class CashManagementDocumentRule extends FinancialDocumentRuleBase implem
         KualiUser currentUser = GlobalVariables.getUserSession().getKualiUser();
         if (cmd.getDocumentHeader() != null && cmd.getDocumentHeader().getWorkflowDocument() != null) {
             String cmdInitiatorNetworkId = cmd.getDocumentHeader().getWorkflowDocument().getInitiatorNetworkId();
-            if (!cmdInitiatorNetworkId.equals(currentUser.getPersonUserIdentifier())) {
+            if (!cmdInitiatorNetworkId.equalsIgnoreCase(currentUser.getPersonUserIdentifier())) {
                 throw new IllegalStateException("The current user (" + currentUser.getPersonUserIdentifier() + ") is not the individual (" + cmdInitiatorNetworkId + ") that initiated this document.");
             }
         }

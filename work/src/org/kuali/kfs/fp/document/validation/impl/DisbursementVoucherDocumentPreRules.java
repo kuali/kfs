324433181d7f1bc32cea626fe6131c16038bbabf
Dennis Friends <dwf5@cornell.edu>
2006-08-24 18:12:48 +0000

    KULEDOCS-1530: Added code to notify user if the Non-Employee Travel tab contains data and the payment reason isn't an N.

--- work/src/org/kuali/kfs/fp/document/validation/impl/DisbursementVoucherDocumentPreRules.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/DisbursementVoucherDocumentPreRules.java
@@ -22,10 +22,22 @@
  */
 package org.kuali.module.financial.rules;
 
+import java.text.MessageFormat;
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
+import org.kuali.Constants;
+import org.kuali.KeyConstants;
+import org.kuali.core.bo.BusinessObjectBase;
 import org.kuali.core.document.Document;
+import org.kuali.core.document.TransactionalDocumentBase;
 import org.kuali.core.rules.PreRulesContinuationBase;
+import org.kuali.core.rules.RulesUtils;
 import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.util.KualiDecimal;
+import org.kuali.core.util.ObjectUtils;
+import org.kuali.core.util.SpringServiceLocator;
+import org.kuali.module.financial.bo.DisbursementVoucherNonEmployeeTravel;
 import org.kuali.module.financial.document.DisbursementVoucherDocument;
 
 /**
@@ -46,6 +58,8 @@ public class DisbursementVoucherDocumentPreRules extends PreRulesContinuationBas
         DisbursementVoucherDocument dvDocument = (DisbursementVoucherDocument) document;
         checkSpecialHandlingIndicator(dvDocument);
 
+        preRulesOK &= checkNonEmployeeTravelTabState(dvDocument);
+        
         return preRulesOK;
     }
 
@@ -60,4 +74,129 @@ public class DisbursementVoucherDocumentPreRules extends PreRulesContinuationBas
         }
     }
 
+    /**
+     * 
+     * This method...
+     * @param dvDocument
+     * @return Returns true if the state of all the tabs is valid, false otherwise.
+     */
+    private boolean checkNonEmployeeTravelTabState(DisbursementVoucherDocument dvDocument) {
+        boolean tabStatesOK = true; 
+        
+        DisbursementVoucherNonEmployeeTravel dvNonEmplTrav = dvDocument.getDvNonEmployeeTravel();
+        
+        String[] travelNonEmplPaymentReasonCodes = SpringServiceLocator.getKualiConfigurationService().getApplicationParameterValues(DV_DOCUMENT_PARAMETERS_GROUP_NM, NONEMPLOYEE_TRAVEL_PAY_REASONS_PARM_NM);
+        
+        if(hasValues(dvNonEmplTrav) && !RulesUtils.makeSet(travelNonEmplPaymentReasonCodes).contains(dvDocument.getDvPayeeDetail().getDisbVchrPaymentReasonCode())) {
+            String questionText = SpringServiceLocator.getKualiConfigurationService().getPropertyString(KeyConstants.QUESTION_CLEAR_NON_EMPLOYEE_TRAVEL_TAB);
+
+            Object[] args = { dvDocument.getDvPayeeDetail().getDisbVchrPaymentReasonCode(), travelNonEmplPaymentReasonCodes[0] };
+            questionText = MessageFormat.format(questionText, args);
+            
+            boolean clearTab = super.askOrAnalyzeYesNoQuestion(Constants.DisbursementVoucherDocumentConstants.CLEAR_NON_EMPLOYEE_TAB_QUESTION_ID, questionText);
+            if (clearTab) {
+                DisbursementVoucherNonEmployeeTravel blankDvNonEmplTrav = new DisbursementVoucherNonEmployeeTravel();
+                blankDvNonEmplTrav.setFinancialDocumentNumber(dvNonEmplTrav.getFinancialDocumentNumber());
+                blankDvNonEmplTrav.setVersionNumber(dvNonEmplTrav.getVersionNumber());
+                dvDocument.setDvNonEmployeeTravel(blankDvNonEmplTrav);
+            } else {
+                // return to document if the user doesn't want to clear the Non Employee Travel tab
+                super.event.setActionForwardName(Constants.MAPPING_BASIC);
+                tabStatesOK = false;
+            }
+        }
+        
+        return tabStatesOK;
+    }
+    
+    /**
+     * 
+     * This method...
+     * @param dvNonEmplTrav
+     * @return True if non employee travel tab contains any data in any fields.
+     */
+    private boolean hasValues(DisbursementVoucherNonEmployeeTravel dvNonEmplTrav) {
+        boolean hasValues = false;
+
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrNonEmpTravelerName());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrServicePerformedDesc());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDvServicePerformedLocName());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDvServiceRegularEmprName());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrTravelFromCityName());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrTravelFromStateCode());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDvTravelFromCountryCode());
+        hasValues |= ObjectUtils.isNotNull(dvNonEmplTrav.getDvPerdiemStartDttmStamp());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrTravelToCityName());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrTravelToStateCode());
+        hasValues |= StringUtils.isNotBlank(dvNonEmplTrav.getDisbVchrTravelToCountryCode());
+        hasValues |= ObjectUtils.isNotNull(dvNonEmplTrav.getDvPerdiemEndDttmStamp());
+        
+        if(!hasValues) {
+            hasValues = hasPerDiemValues(dvNonEmplTrav);
+        }
+        
+        if(!hasValues) {
+            hasValues = hasPersonalVehicleValues(dvNonEmplTrav);
+        }
+        
+        if(!hasValues) {
+            hasValues = hasNonEmployeeTravelExpenses(dvNonEmplTrav);
+        }
+        
+        if(!hasValues) {
+            hasValues = hasNonEmployeeTravelPrepaidExpenses(dvNonEmplTrav);
+        }
+        
+        return hasValues;
+    }
+
+    /**
+     * 
+     * This method...
+     * @param dvNonEmplTrav
+     * @return True if non employee travel tab contains data in any of the fields in the per diem section
+     */
+    private boolean hasPerDiemValues(DisbursementVoucherNonEmployeeTravel dvNonEmplTrav) {        
+        return !StringUtils.isBlank(dvNonEmplTrav.getDisbVchrPerdiemCategoryName()) || 
+               !ObjectUtils.isNull(dvNonEmplTrav.getDisbVchrPerdiemRate()) || 
+               !ObjectUtils.isNull(dvNonEmplTrav.getDisbVchrPerdiemCalculatedAmt()) || 
+               !ObjectUtils.isNull(dvNonEmplTrav.getDisbVchrPerdiemActualAmount()) ||
+               !StringUtils.isBlank(dvNonEmplTrav.getDvPerdiemChangeReasonText());
+    }
+
+    /**
+     * 
+     * This method...
+     * @param dvNonEmplTrav
+     * @return True if non employee travel tab contains data in any of the fields in the personal vehicle section
+     */
+    private boolean hasPersonalVehicleValues(DisbursementVoucherNonEmployeeTravel dvNonEmplTrav) {        
+        return !StringUtils.isBlank(dvNonEmplTrav.getDisbVchrAutoFromCityName()) || 
+               !StringUtils.isBlank(dvNonEmplTrav.getDisbVchrAutoFromStateCode()) || 
+               !StringUtils.isBlank(dvNonEmplTrav.getDisbVchrAutoToCityName()) ||
+               !StringUtils.isBlank(dvNonEmplTrav.getDisbVchrAutoToStateCode()) ||
+               !ObjectUtils.isNull(dvNonEmplTrav.getDisbVchrMileageCalculatedAmt()) ||
+               !ObjectUtils.isNull(dvNonEmplTrav.getDisbVchrPersonalCarAmount());
+    }
+
+    /**
+     * 
+     * This method...
+     * @return
+     */
+    private boolean hasNonEmployeeTravelExpenses(DisbursementVoucherNonEmployeeTravel dvNonEmplTrav) {
+        return dvNonEmplTrav.getDvNonEmployeeExpenses().size() > 0;
+    }
+
+
+    /**
+     * 
+     * This method...
+     * @param dvNonEmplTrav
+     * @return
+     */
+    private boolean hasNonEmployeeTravelPrepaidExpenses(DisbursementVoucherNonEmployeeTravel dvNonEmplTrav) {
+        return dvNonEmplTrav.getDvPrePaidEmployeeExpenses().size() > 0;
+    }
+    
 }

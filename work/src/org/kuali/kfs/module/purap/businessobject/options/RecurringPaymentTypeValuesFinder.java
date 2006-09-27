676d5387173acdf17149de546c7b7dcef5d36006
Naser Alavi <alavinas@ais.msu.edu>
2006-09-27 17:33:53 +0000

    added this new file

--- work/src/org/kuali/kfs/module/purap/businessobject/options/RecurringPaymentTypeValuesFinder.java
+++ work/src/org/kuali/kfs/module/purap/businessobject/options/RecurringPaymentTypeValuesFinder.java
@@ -3,15 +3,15 @@
  * Cornell University, Trustees of Indiana University, Michigan State University Board of Trustees,
  * Trustees of San Joaquin Delta College, University of Hawai'i, The Arizona Board of Regents on
  * behalf of the University of Arizona, and the r*smart group.
- * 
+ *
  * Licensed under the Educational Community License Version 1.0 (the "License"); By obtaining,
  * using and/or copying this Original Work, you agree that you have read, understand, and will
  * comply with the terms and conditions of the Educational Community License.
- * 
+ *
  * You may obtain a copy of the License at:
- * 
+ *
  * http://kualiproject.org/license.html
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
  * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
  * AND NONINFRINGEMENT.
@@ -31,14 +31,14 @@ import org.kuali.core.lookup.keyvalues.KeyValuesBase;
 import org.kuali.core.service.KeyValuesService;
 import org.kuali.core.util.SpringServiceLocator;
 import org.kuali.core.web.uidraw.KeyLabelPair;
-import org.kuali.module.purap.bo.PurchaseOrderTransmissionMethod;
+import org.kuali.module.purap.bo.RecurringPaymentType;
 
 /**
- * This class...
+ * This class returns list containg A = Active or I = Inactive
  * 
- * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
+ * @author Nervous System Team (kualidev@oncourse.iu.edu)
  */
-public class PurchaseOrderTransmissionMethodValuesFinder extends KeyValuesBase {
+public class RecurringPaymentTypeValuesFinder extends KeyValuesBase {
 
     /*
      * @see org.kuali.keyvalues.KeyValuesFinder#getKeyValues()
@@ -46,12 +46,12 @@ public class PurchaseOrderTransmissionMethodValuesFinder extends KeyValuesBase {
     public List getKeyValues() {
 
         KeyValuesService boService = SpringServiceLocator.getKeyValuesService();
-        Collection codes = boService.findAll(PurchaseOrderTransmissionMethod.class);
+        Collection codes = boService.findAll(RecurringPaymentType.class);
         List labels = new ArrayList();
         labels.add(new KeyLabelPair("", ""));
         for (Iterator iter = codes.iterator(); iter.hasNext();) {
-            PurchaseOrderTransmissionMethod transmissionMethod = (PurchaseOrderTransmissionMethod) iter.next();
-            labels.add(new KeyLabelPair(transmissionMethod.getPurchaseOrderTransmissionMethodCode(), transmissionMethod.getPurchaseOrderTransmissionMethodDescription()));
+            RecurringPaymentType rpt = (RecurringPaymentType) iter.next();
+            labels.add(new KeyLabelPair(rpt.getRecurringPaymentTypeCode(), rpt.getRecurringPaymentTypeDescription()));
         }
 
         return labels;

3b6bb6da9dabe9368394032b83f27d080c5c9927
Laran Evans <laran@laranevans.com>
2006-10-11 16:23:24 +0000

    Corrected a capitalization error.

--- test/unit/src/org/kuali/kfs/coa/service/MandatoryTransferEliminationCodeServiceTest.java
+++ test/unit/src/org/kuali/kfs/coa/service/MandatoryTransferEliminationCodeServiceTest.java
@@ -38,10 +38,9 @@ public class MandatoryTransferEliminationCodeServiceTest extends KualiTestBase {
 
     private MandatoryTransferEliminationCode knowGood;
     private static final String GOOD_CODE="N";
-    private static final String GOOD_NAME="None";
+    private static final String GOOD_NAME="NONE";
     private static final String BAD_CODE = "X";
     private static final String BAD_NAME = "BAD";
-
 
     /**
      * @see org.kuali.test.KualiTestBase#setUp()

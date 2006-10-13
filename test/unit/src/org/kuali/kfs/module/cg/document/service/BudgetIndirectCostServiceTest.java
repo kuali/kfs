542b3dd4cc481643a684814fccf6cfec4fec1fb3
Laran Evans <laran@laranevans.com>
2006-10-13 15:18:49 +0000

    Added a catch for a budget with no periods.

--- test/unit/src/org/kuali/kfs/module/cg/document/service/BudgetIndirectCostServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/cg/document/service/BudgetIndirectCostServiceTest.java
@@ -138,43 +138,44 @@ public class BudgetIndirectCostServiceTest extends KualiTestBase {
 
     public void testRefreshIndirectCost() {
         BudgetDocument budgetDocument = new BudgetDocument();
-         Budget budget = budgetDocument.getBudget();
-        
-         String[] categories = { "CO", "CO", "FL", "SC" };
-         String[] subCategories = { "C1", "C1", "F5", "R2" };
-         String[] subcontractorNumber = { "", "", "", "1" };
-         List nonpersonnelItems = BudgetNonpersonnelTest.createBudgetNonpersonnel(categories, subCategories, subcontractorNumber);
-         for (Iterator iter = nonpersonnelItems.iterator(); iter.hasNext();) {
-         BudgetNonpersonnel nonpersonnel = (BudgetNonpersonnel) iter.next();
-         nonpersonnel.setBudgetPeriodSequenceNumber(new Integer(2));
-         }
-         budget.setNonpersonnelItems(nonpersonnelItems);
-        
-         List userAppointmentTaskPeriods = new ArrayList();
-        
-         BudgetPeriod period1 = (BudgetPeriod) budget.getPeriods().get(0);
-        
-         UserAppointmentTaskPeriod taskPeriod = new UserAppointmentTaskPeriod();
-         taskPeriod.setBudgetPeriodSequenceNumber(period1.getBudgetPeriodSequenceNumber());
-         taskPeriod.setAgencyRequestTotalAmount(new KualiInteger(39000));
-         taskPeriod.setAgencyFringeBenefitTotalAmount(new KualiInteger(13000));
-         userAppointmentTaskPeriods.add(taskPeriod);
-        
-         UserAppointmentTaskPeriod taskPeriod2 = new UserAppointmentTaskPeriod();
-         taskPeriod2.setBudgetPeriodSequenceNumber(period1.getBudgetPeriodSequenceNumber());
-         taskPeriod2.setAgencyRequestTotalAmount(new KualiInteger(43000));
-         taskPeriod2.setAgencyFringeBenefitTotalAmount(new KualiInteger(8500));
-         userAppointmentTaskPeriods.add(taskPeriod2);
-        
-         BudgetPeriod period2 = (BudgetPeriod) budget.getPeriods().get(1);
-        
-         UserAppointmentTaskPeriod taskPeriod3 = new UserAppointmentTaskPeriod();
-         taskPeriod3.setBudgetPeriodSequenceNumber(period2.getBudgetPeriodSequenceNumber());
-         taskPeriod3.setAgencyRequestTotalAmount(new KualiInteger(74000));
-         taskPeriod3.setAgencyFringeBenefitTotalAmount(new KualiInteger(21500));
-         userAppointmentTaskPeriods.add(taskPeriod3);
-        
-         budget.setAllUserAppointmentTaskPeriods(userAppointmentTaskPeriods);
+        Budget budget = budgetDocument.getBudget();
+
+        String[] categories = { "CO", "CO", "FL", "SC" };
+        String[] subCategories = { "C1", "C1", "F5", "R2" };
+        String[] subcontractorNumber = { "", "", "", "1" };
+        List nonpersonnelItems = BudgetNonpersonnelTest.createBudgetNonpersonnel(categories, subCategories, subcontractorNumber);
+        for (Iterator iter = nonpersonnelItems.iterator(); iter.hasNext();) {
+            BudgetNonpersonnel nonpersonnel = (BudgetNonpersonnel) iter.next();
+            nonpersonnel.setBudgetPeriodSequenceNumber(new Integer(2));
+        }
+        budget.setNonpersonnelItems(nonpersonnelItems);
+
+        List userAppointmentTaskPeriods = new ArrayList();
+
+        assertFalse("Budget periods should not be empty.", budget.getPeriods().isEmpty());
+        BudgetPeriod period1 = (BudgetPeriod) budget.getPeriods().get(0);
+
+        UserAppointmentTaskPeriod taskPeriod = new UserAppointmentTaskPeriod();
+        taskPeriod.setBudgetPeriodSequenceNumber(period1.getBudgetPeriodSequenceNumber());
+        taskPeriod.setAgencyRequestTotalAmount(new KualiInteger(39000));
+        taskPeriod.setAgencyFringeBenefitTotalAmount(new KualiInteger(13000));
+        userAppointmentTaskPeriods.add(taskPeriod);
+
+        UserAppointmentTaskPeriod taskPeriod2 = new UserAppointmentTaskPeriod();
+        taskPeriod2.setBudgetPeriodSequenceNumber(period1.getBudgetPeriodSequenceNumber());
+        taskPeriod2.setAgencyRequestTotalAmount(new KualiInteger(43000));
+        taskPeriod2.setAgencyFringeBenefitTotalAmount(new KualiInteger(8500));
+        userAppointmentTaskPeriods.add(taskPeriod2);
+
+        BudgetPeriod period2 = (BudgetPeriod) budget.getPeriods().get(1);
+
+        UserAppointmentTaskPeriod taskPeriod3 = new UserAppointmentTaskPeriod();
+        taskPeriod3.setBudgetPeriodSequenceNumber(period2.getBudgetPeriodSequenceNumber());
+        taskPeriod3.setAgencyRequestTotalAmount(new KualiInteger(74000));
+        taskPeriod3.setAgencyFringeBenefitTotalAmount(new KualiInteger(21500));
+        userAppointmentTaskPeriods.add(taskPeriod3);
+
+        budget.setAllUserAppointmentTaskPeriods(userAppointmentTaskPeriods);
 
     }
 }

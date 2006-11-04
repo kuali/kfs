3197cb4e8604bff9ca6e35af7a31224c668fadae
Rich Pierce <ripierce@indiana.edu>
2006-11-04 14:01:47 +0000

    KULPURAP-124: Design and Implement Assign a Contract Manager.

--- work/src/org/kuali/kfs/module/purap/document/service/PurapServiceImpl.java
+++ work/src/org/kuali/kfs/module/purap/document/service/PurapServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2006 The Kuali Foundation.
+ * Copyright 2006 The Kuali Foundation.
  * 
  * $Source: /opt/cvs/kfs/work/src/org/kuali/kfs/module/purap/document/service/PurapServiceImpl.java,v $
  * 
@@ -17,29 +17,68 @@
  */
 package org.kuali.module.purap.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.util.GlobalVariables;
 import org.kuali.core.util.ObjectUtils;
+import org.kuali.core.util.SpringServiceLocator;
+import org.kuali.module.purap.PurapKeyConstants;
+import org.kuali.module.purap.PurapPropertyConstants;
+import org.kuali.module.purap.bo.ContractManager;
+import org.kuali.module.purap.bo.StatusHistory;
 import org.kuali.module.purap.document.PurchasingAccountsPayableDocument;
-import org.kuali.module.purap.document.PurchasingDocument;
+import org.kuali.module.purap.document.RequisitionDocument;
 import org.kuali.module.purap.service.PurapService;
 
 public class PurapServiceImpl implements PurapService {
-// TODO: make the boService available, also need to add to the PurapService section of KualiSpringBeansPurap.xml
-//    private BusinessObjectService boService;
-//
-//    protected BusinessObjectService getBOService() {
-//        if( ObjectUtils.isNull( this.boService ) ) {
-//            this.boService = SpringServiceLocator.getBusinessObjectService();
-//        }
-//        return this.boService;
-//    }
-    
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PurapServiceImpl.class);
+
+    private BusinessObjectService businessObjectService;
+    protected BusinessObjectService getBOService() {
+        if( ObjectUtils.isNull( this.businessObjectService ) ) {
+            this.businessObjectService = SpringServiceLocator.getBusinessObjectService();
+        }
+        return this.businessObjectService;
+    }
+    public void setBusinessObjectService(BusinessObjectService boService) {
+        this.businessObjectService = boService;    
+    }
+
     /**
      * This method updates the status and status history for a purap document.
      *
     */
-    public boolean updateStatusAndStatusHistory(PurchasingAccountsPayableDocument document, String statusToSet) {
+    public boolean updateStatusAndStatusHistory(PurchasingAccountsPayableDocument document,String statusToSet) {
+        LOG.debug("updateStatusAndStatusHistory(): entered method.");
         
-        boolean success = false; // TODO: is this needed?
+        boolean success = false;
+        
+        if ( ObjectUtils.isNull(document) || ObjectUtils.isNull(statusToSet) ) {
+            return success;
+        }
+
+        success = this.updateStatus(document, statusToSet);
+
+// TODO: make this work.
+//        success = this.updateStatusHistory(document, statusToSet);
+
+        LOG.debug("updateStatusAndStatusHistory(): leaving method.");
+        return success;
+    }
+
+    /**
+     * This method updates the status for a purap document.
+     *
+    */
+    public boolean updateStatus(PurchasingAccountsPayableDocument document,String statusToSet) {
+        LOG.debug("updateStatus(): entered method.");
+        
+        boolean success = false;
         
         if ( ObjectUtils.isNull(document) || ObjectUtils.isNull(statusToSet) ) {
             return success;
@@ -47,13 +86,36 @@ public class PurapServiceImpl implements PurapService {
 
         document.setStatusCode(statusToSet);
         
-        // TODO: update the status history.
-        
-        // TODO: if/else that checks to see if the document's status and status history is correct,
-        //   then set success = true if so.
-        
         success = true;
+        
+        LOG.debug("updateStatus(): leaving method.");
         return success;
     }
-    
+
+    /**
+     * This method updates the status history for a purap document.
+     *
+    */
+    public boolean updateStatusHistory(PurchasingAccountsPayableDocument document,String statusToSet) {
+        LOG.debug("updateStatusHistory(): entered method.");
+        
+        boolean success = false;
+        
+        if ( ObjectUtils.isNull(document) || ObjectUtils.isNull(statusToSet) ) {
+            return success;
+        }
+
+        StatusHistory statusHistory = new StatusHistory();
+        statusHistory.setOldStatusCode(document.getStatusCode());
+        statusHistory.setNewStatusCode(statusToSet);
+        // TODO: add note, what other fields need to be filled?
+        
+        document.getStatusHistories().add(statusHistory);
+
+        success = true;
+        
+        LOG.debug("updateStatusHistory(): leaving method.");
+        return success;
+    }
+
 }

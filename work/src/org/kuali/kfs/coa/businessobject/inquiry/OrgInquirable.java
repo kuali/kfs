17040a53efc191fb5ff43297f63e339731bfcc89
Jerry Neal <jkneal@iu.edu>
2006-11-07 14:34:26 +0000

    Inquiry Sections

--- work/src/org/kuali/kfs/coa/businessobject/inquiry/OrgInquirable.java
+++ work/src/org/kuali/kfs/coa/businessobject/inquiry/OrgInquirable.java
@@ -17,30 +17,38 @@
  */
 package org.kuali.module.chart.maintenance;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.kuali.core.bo.BusinessObject;
 import org.kuali.core.inquiry.KualiInquirableImpl;
 import org.kuali.core.web.uidraw.Column;
+import org.kuali.core.web.uidraw.Section;
 import org.kuali.module.chart.bo.Org;
 
 public class OrgInquirable extends KualiInquirableImpl {
 
     
-    public void addAdditionalColumns(List columns, BusinessObject bo) {
+    public void addAdditionalSections(List sections, BusinessObject bo) {
         if (bo instanceof Org) {
             Org org = (Org) bo;
-
+            
+            List rows = new ArrayList();
             Column c = new Column();
             c.setColumnTitle("Organization Hierarchy");
             c.setPropertyValue(org.getOrganizationHierarchy());
-            columns.add(c);
+            rows.add(c);
 
             c = new Column();
             c.setColumnTitle("Organization Review Hierarchy");
             c.setPropertyValue("run search");
             c.setPropertyURL(org.getOrganizationReviewHierarchy());
-            columns.add(c);
+            rows.add(c);
+            
+            Section section = new Section();
+            section.setRows(rows);
+            section.setSectionTitle("Organization Hierarchy");
+            sections.add(section);
         }
     }
 

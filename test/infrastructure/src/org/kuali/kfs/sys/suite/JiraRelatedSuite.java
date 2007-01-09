5f0fb46c58c0eb37537748a4569611cbf18705e3
J. David Buetel <jbeutel@hawaii.edu>
2007-01-09 21:09:39 +0000

    KULCG-20 - trailing comma convention does not avoid CVS conflicts on update, so never mind it

--- test/infrastructure/src/org/kuali/kfs/sys/suite/JiraRelatedSuite.java
+++ test/infrastructure/src/org/kuali/kfs/sys/suite/JiraRelatedSuite.java
@@ -43,10 +43,7 @@ public abstract class JiraRelatedSuite {
     public static enum State {
         IN_PROGRESS("status=3&tempMax=1000"),
         OPEN_OR_IN_PROGRESS("status=1&status=3&tempMax=9999"),
-        OPEN_OR_IN_PROGRESS_OR_REOPENED("status=1&status=3&status=4&tempMax=9999"),
-        TEST1("foo"),
-        TEST2(""),
-        ;
+        OPEN_OR_IN_PROGRESS_OR_REOPENED("status=1&status=3&status=4&tempMax=9999");
 
         public final String filterUrl;
 

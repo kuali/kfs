918f22eaca7d892cda948c5f7f7629cb11471669
Warren	Liang <wliang@uci.edu>
2007-01-11 02:42:47 +0000

    KULRNE-4297: Change edits on ICR automated entry table to allow @ and # in certain accounting string fields https://test.kuali.org/jira/browse/KULRNE-4297

--- work/src/org/kuali/kfs/coa/document/validation/impl/IcrAutomatedEntryRule.java
+++ work/src/org/kuali/kfs/coa/document/validation/impl/IcrAutomatedEntryRule.java
@@ -26,7 +26,9 @@ import org.kuali.KeyConstants;
 import org.kuali.core.bo.PersistableBusinessObject;
 import org.kuali.core.document.MaintenanceDocument;
 import org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.core.util.GlobalVariables;
 import org.kuali.core.util.KualiPercent;
+import org.kuali.core.util.SpringServiceLocator;
 import org.kuali.module.chart.bo.Account;
 import org.kuali.module.chart.bo.Chart;
 import org.kuali.module.chart.bo.IcrAutomatedEntry;
@@ -76,74 +78,91 @@ public class IcrAutomatedEntryRule extends MaintenanceDocumentRuleBase {
         String transactionDebitIndicator = newIcrAutomatedEntry.getTransactionDebitIndicator();
         KualiPercent awardIndrCostRcvyRatePct = newIcrAutomatedEntry.getAwardIndrCostRcvyRatePct();
 
-        // Chart Code Rule
-        if (chartOfAccountsCode != null) {
-            if (StringUtils.contains(chartOfAccountsCode, "@") || StringUtils.contains(chartOfAccountsCode, "#")) {
-
+        
+        success &= checkCorrectWildcards(newIcrAutomatedEntry);
+        
+        if (success) {
+            // because of check above, we know that:
+            // if any of these are wildcards: chart, account, or subaccount, then they are all wildcards (except for subaccount, which may be dashes)
+            // if any of these are wildcards: chart, object, or subobject, then they are all wildcards (except for subobject, which may be dashes)
+            
+            Class icrClazz = newIcrAutomatedEntry.getClass();
+            
+            // Chart Code Rule
+            if (chartOfAccountsCode != null) {
+                if (isWildcard(chartOfAccountsCode)) {
+    
+                }
+                else {
+                    Map pkMap = new HashMap();
+                    pkMap.put(Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, chartOfAccountsCode);
+                    success &= checkExistenceFromTable(Chart.class, pkMap, Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME,
+                            SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrClazz, Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME));
+                }
             }
-            else {
-                Map pkMap = new HashMap();
-                pkMap.put("chartOfAccountsCode", chartOfAccountsCode);
-                success &= checkExistenceFromTable(Chart.class, pkMap, "chartOfAccountsCode", "Chart Code");
+    
+    
+            // Account Number Rule
+            if (accountNumber != null) {
+                if (isWildcard(accountNumber)) {
+    
+                }
+                else {
+                    Map pkMap = new HashMap();
+                    pkMap.put(Constants.ACCOUNT_NUMBER_PROPERTY_NAME, accountNumber);
+                    pkMap.put(Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, chartOfAccountsCode);
+                    success &= checkExistenceFromTable(Account.class, removeWildcardForPkMap(pkMap), Constants.ACCOUNT_NUMBER_PROPERTY_NAME,
+                            SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrClazz, Constants.ACCOUNT_NUMBER_PROPERTY_NAME));
+                }
             }
-        }
-
-
-        // Account Number Rule
-        if (accountNumber != null) {
-            if (StringUtils.contains(accountNumber, "@") || StringUtils.contains(accountNumber, "#")) {
-
+    
+    
+            // Sub-Account Number Rule
+            if (subAccountNumber != null) {
+                // checkCorrectWildcards makes sure that the wildcard is appropriate for the sub account number
+                if (isWildcard(subAccountNumber) || StringUtils.equals(subAccountNumber, Constants.DASHES_SUB_ACCOUNT_NUMBER)) {
+    
+                }
+                else {
+                    Map pkMap = new HashMap();
+                    pkMap.put(Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, chartOfAccountsCode);
+                    pkMap.put(Constants.ACCOUNT_NUMBER_PROPERTY_NAME, accountNumber);
+                    pkMap.put(Constants.SUB_ACCOUNT_NUMBER_PROPERTY_NAME, subAccountNumber);
+                    success &= checkExistenceFromTable(SubAccount.class, removeWildcardForPkMap(pkMap), Constants.SUB_ACCOUNT_NUMBER_PROPERTY_NAME,
+                            SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrClazz, Constants.SUB_ACCOUNT_NUMBER_PROPERTY_NAME));
+                }
             }
-            else {
-                Map pkMap = new HashMap();
-                pkMap.put("accountNumber", accountNumber);
-                pkMap.put("chartOfAccountsCode", chartOfAccountsCode);
-                success &= checkExistenceFromTable(Account.class, removeWildcardForPkMap(pkMap), "accountNumber", "Account");
+    
+            // Financial ObjectCode Rule
+            if (financialObjectCode != null) {
+                if (isWildcard(financialObjectCode)) {
+    
+                }
+                else {
+                    Map pkMap = new HashMap();
+                    pkMap.put(Constants.UNIVERSITY_FISCAL_YEAR_PROPERTY_NAME, universityFiscalYear);
+                    pkMap.put(Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, chartOfAccountsCode);
+                    pkMap.put(Constants.FINANCIAL_OBJECT_CODE_PROPERTY_NAME, financialObjectCode);
+                    success &= checkExistenceFromTable(ObjectCode.class, removeWildcardForPkMap(pkMap), Constants.FINANCIAL_OBJECT_CODE_PROPERTY_NAME,
+                            SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrClazz, Constants.FINANCIAL_OBJECT_CODE_PROPERTY_NAME));
+                }
             }
-        }
-
-
-        // Sub-Account Number Rule
-        if (subAccountNumber != null) {
-            if (StringUtils.contains(subAccountNumber, "@") || StringUtils.contains(subAccountNumber, Constants.DASHES_SUB_ACCOUNT_NUMBER)) {
-
-            }
-            else {
-                Map pkMap = new HashMap();
-                pkMap.put("chartOfAccountsCode", chartOfAccountsCode);
-                pkMap.put("accountNumber", accountNumber);
-                pkMap.put("subAccountNumber", subAccountNumber);
-                success &= checkExistenceFromTable(SubAccount.class, removeWildcardForPkMap(pkMap), "subAccountNumber", "Sub-Account");
-            }
-        }
-
-        // Financial ObjectCode Rule
-        if (financialObjectCode != null) {
-            if (StringUtils.contains(financialObjectCode, "@") || StringUtils.contains(financialObjectCode, "#")) {
-
-            }
-            else {
-                Map pkMap = new HashMap();
-                pkMap.put("universityFiscalYear", universityFiscalYear);
-                pkMap.put("chartOfAccountsCode", chartOfAccountsCode);
-                pkMap.put("financialObjectCode", financialObjectCode);
-                success &= checkExistenceFromTable(ObjectCode.class, removeWildcardForPkMap(pkMap), "financialObjectCode", "Object Code");
-            }
-        }
-
-        // Financial SubObjectCode Rule
-        if (financialSubObjectCode != null) {
-            if (StringUtils.contains(financialSubObjectCode, Constants.DASHES_SUB_OBJECT_CODE)) {
-
-            }
-            else {
-                Map pkMap = new HashMap();
-                pkMap.put("universityFiscalYear", universityFiscalYear);
-                pkMap.put("chartOfAccountsCode", chartOfAccountsCode);
-                pkMap.put("accountNumber", accountNumber);
-                pkMap.put("financialObjectCode", financialObjectCode);
-                pkMap.put("financialSubObjectCode", financialSubObjectCode);
-                success = checkExistenceFromTable(SubObjCd.class, removeWildcardForPkMap(pkMap), "financialSubObjectCode", "SubObject Code");
+    
+            // Financial SubObjectCode Rule
+            if (financialSubObjectCode != null) {
+                if (isWildcard(financialSubObjectCode) || StringUtils.contains(financialSubObjectCode, Constants.DASHES_SUB_OBJECT_CODE)) {
+    
+                }
+                else {
+                    Map pkMap = new HashMap();
+                    pkMap.put(Constants.UNIVERSITY_FISCAL_YEAR_PROPERTY_NAME, universityFiscalYear);
+                    pkMap.put(Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, chartOfAccountsCode);
+                    pkMap.put(Constants.ACCOUNT_NUMBER_PROPERTY_NAME, accountNumber);
+                    pkMap.put(Constants.FINANCIAL_OBJECT_CODE_PROPERTY_NAME, financialObjectCode);
+                    pkMap.put(Constants.FINANCIAL_SUB_OBJECT_CODE_PROPERTY_NAME, financialSubObjectCode);
+                    success = checkExistenceFromTable(SubObjCd.class, removeWildcardForPkMap(pkMap), Constants.FINANCIAL_SUB_OBJECT_CODE_PROPERTY_NAME,
+                            SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrClazz, Constants.FINANCIAL_SUB_OBJECT_CODE_PROPERTY_NAME));
+                }
             }
         }
 
@@ -194,16 +213,15 @@ public class IcrAutomatedEntryRule extends MaintenanceDocumentRuleBase {
 
 
     // Check existence of each field from table.
-    private boolean checkExistenceFromTable(Class clazz, Map map, String errorField, String errorMessage) {
+    private boolean checkExistenceFromTable(Class clazz, Map fieldValues, String errorField, String errorMessage) {
         boolean success = true;
-        PersistableBusinessObject findByChartCode = null;
-        findByChartCode = getBoService().findByPrimaryKey(clazz, map);
-        if (findByChartCode == null) {
-            putFieldError(errorField, KeyConstants.ERROR_EXISTENCE, errorMessage);
-            success = false;
+        success = getBoService().countMatching(clazz, fieldValues) != 0;
+        if (!success) {
+            GlobalVariables.getErrorMap().putErrorWithoutFullErrorPath(Constants.MAINTENANCE_NEW_MAINTAINABLE + errorField, KeyConstants.ERROR_EXISTENCE, errorMessage);
         }
         return success;
     }
+    
     private Map removeWildcardForPkMap(Map pkMap){
         Map returnMap = new HashMap();
         Iterator keyIter = pkMap.keySet().iterator(); 
@@ -211,11 +229,153 @@ public class IcrAutomatedEntryRule extends MaintenanceDocumentRuleBase {
         while(keyIter.hasNext()){
             Object key = keyIter.next();
             Object value = pkMap.get(key);
-            if (!StringUtils.contains(value.toString(), "@")){
+            if (!isWildcard(value.toString())){
                 returnMap.put(key, value);
             }
         }
         
         return returnMap;
+    }
+    
+    protected boolean checkCorrectWildcards(IcrAutomatedEntry newIcrAutomatedEntry) {
+        String chartOfAccountsCode = newIcrAutomatedEntry.getChartOfAccountsCode();
+        String accountNumber = newIcrAutomatedEntry.getAccountNumber();
+        String subAccountNumber = newIcrAutomatedEntry.getSubAccountNumber();
+        String financialObjectCode = newIcrAutomatedEntry.getFinancialObjectCode();
+        String financialSubObjectCode = newIcrAutomatedEntry.getFinancialSubObjectCode();
+        
+        boolean success = true;
+        
+        // first check that each of the above fields has an appropriate wildcard/field value
+        // @ should be valid for chart, account, sub account, object code, and sub object code.
+        // # should be valid for chart, account, sub account, and object code. Sub object code is not needed here. 
+        
+        // TODO: make these into app parameters?
+        success &= isValidWildcard(newIcrAutomatedEntry, Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, chartOfAccountsCode, "@", "#");
+        success &= isValidWildcard(newIcrAutomatedEntry, Constants.ACCOUNT_NUMBER_PROPERTY_NAME, accountNumber, "@", "#");
+        if (!Constants.DASHES_SUB_ACCOUNT_NUMBER.equals(subAccountNumber)) {
+            success &= isValidWildcard(newIcrAutomatedEntry, Constants.SUB_ACCOUNT_NUMBER_PROPERTY_NAME, subAccountNumber, "@", "#");
+        }
+        success &= isValidWildcard(newIcrAutomatedEntry, Constants.FINANCIAL_OBJECT_CODE_PROPERTY_NAME, financialObjectCode, "@", "#");
+        if (!Constants.DASHES_SUB_OBJECT_CODE.equals(financialSubObjectCode)) {
+            success &= isValidWildcard(newIcrAutomatedEntry, Constants.FINANCIAL_SUB_OBJECT_CODE_PROPERTY_NAME, financialSubObjectCode, "@");
+        }
+        
+        if (!success) {
+            // invalid wildcards, don't bother validating the rest
+            return false;
+        }
+        
+        success &= checkWildcardsForChartAccountSubAccount(newIcrAutomatedEntry);
+        success &= checkWildcardsForChartObjectSubObject(newIcrAutomatedEntry);
+        
+        return success;
+        
+    }
+    
+    private boolean isValidWildcard(IcrAutomatedEntry newIcrAutomatedEntry, String fieldName, String fieldValue, String... allowedWildcards) {
+        if (!StringUtils.isBlank(fieldValue)) {
+            if (!StringUtils.isAlphanumeric(fieldValue)) {
+                for (String wildcard : allowedWildcards) {
+                    if (wildcard.equals(fieldValue)) {
+                        // wildcard validation passed
+                        return true;
+                    }
+                }
+                // validation didn't pass against allowed list of wildcards
+                putInvalidWildcardError(newIcrAutomatedEntry, fieldName, allowedWildcards);
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void putInvalidWildcardError(IcrAutomatedEntry instance, String fieldName, String... allowedWildcards) {
+        StringBuilder sb = new StringBuilder();
+        // build a human readable string listing all the possible values for the allowed values string.
+        for (int i = 0; i < allowedWildcards.length; i++) {
+            if (i == 0) {
+                sb.append(allowedWildcards[i]);
+            }
+            else if (i == allowedWildcards.length - 1) {
+                // last element
+                // if there are 2 elements, then no comma (e.g. A or B), but if there are more than 2, than we need a comma (e.g. A, B, or C)
+                sb.append(i == 1 ? "" : ",").append(" or ").append(allowedWildcards[i]);
+            }
+            else {
+                sb.append(", ").append(allowedWildcards[i]);
+            }
+        }
+        putFieldError(fieldName, KeyConstants.IndirectCostRecovery.ERROR_DOCUMENT_ICR_INVALID_WILDCARD,
+                new String[] {SpringServiceLocator.getDataDictionaryService().getAttributeLabel(instance.getClass(), fieldName), sb.toString()});
+    }
+    
+    /**
+     * Checks whether if at least one of these attributes has a wildcard, that they all have a wildcard: chart, account, and subaccount 
+     * 
+     * @param chartOfAccountsCode
+     * @param accountNumber
+     * @param subAccountNumber
+     * @return
+     */
+    protected boolean checkWildcardsForChartAccountSubAccount(IcrAutomatedEntry icrAutomatedEntry) {
+        String chartOfAccountsCode = icrAutomatedEntry.getChartOfAccountsCode();
+        String accountNumber = icrAutomatedEntry.getAccountNumber();
+        String subAccountNumber = icrAutomatedEntry.getSubAccountNumber();
+        
+        if (isWildcard(chartOfAccountsCode) || isWildcard(accountNumber) || isWildcard(subAccountNumber)) {
+            // these should never be null
+            // chart needs same wildcard as account, and if chart is wildcard, then then subaccount must be the same wildcard or dashes
+            boolean success = chartOfAccountsCode.equals(accountNumber) && 
+                (chartOfAccountsCode.equals(subAccountNumber) || Constants.DASHES_SUB_ACCOUNT_NUMBER.equals(subAccountNumber));
+            if (!success) {
+                String chartDesc = SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrAutomatedEntry.getClass(), Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME);
+                String accountDesc = SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrAutomatedEntry.getClass(), Constants.ACCOUNT_NUMBER_PROPERTY_NAME);
+                String subAccountDesc = SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrAutomatedEntry.getClass(), Constants.SUB_ACCOUNT_NUMBER_PROPERTY_NAME);
+                
+                String groupDesc = chartDesc + ", " + accountDesc + ", and " + subAccountDesc;
+                GlobalVariables.getErrorMap().putErrorWithoutFullErrorPath(Constants.MAINTENANCE_NEW_MAINTAINABLE + Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, 
+                        KeyConstants.IndirectCostRecovery.ERROR_DOCUMENT_ICR_WILDCARDS_MUST_MATCH, new String[] {groupDesc, subAccountDesc});
+            }
+            return success;
+        }
+        // if no wildcards in this group, then this check is fine
+        return true;
+    }
+    
+    /**
+     * Checks whether if at least one of these attributes has a wildcard, that they all have a wildcard: chart, object, and subobject
+     * 
+     * @param chartOfAccountsCode
+     * @param financialObjectCode
+     * @param financialSubObjectCode
+     * @return
+     */
+    protected boolean checkWildcardsForChartObjectSubObject(IcrAutomatedEntry icrAutomatedEntry) {
+        String chartOfAccountsCode = icrAutomatedEntry.getChartOfAccountsCode();
+        String financialObjectCode = icrAutomatedEntry.getFinancialObjectCode();
+        String financialSubObjectCode = icrAutomatedEntry.getFinancialSubObjectCode();
+
+        if (isWildcard(chartOfAccountsCode) || isWildcard(financialObjectCode) || isWildcard(financialSubObjectCode)) {
+            // these should never be null
+            boolean success = chartOfAccountsCode.equals(financialObjectCode) && 
+                    (chartOfAccountsCode.equals(financialSubObjectCode) || Constants.DASHES_SUB_OBJECT_CODE.equals(financialSubObjectCode));
+            if (!success) {
+                String chartDesc = SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrAutomatedEntry.getClass(), Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME);
+                String objectDesc = SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrAutomatedEntry.getClass(), Constants.FINANCIAL_OBJECT_CODE_PROPERTY_NAME);
+                String subObjectDesc = SpringServiceLocator.getDataDictionaryService().getAttributeLabel(icrAutomatedEntry.getClass(), Constants.FINANCIAL_SUB_OBJECT_CODE_PROPERTY_NAME);
+                
+                String groupDesc = chartDesc + ", " + objectDesc + ", and " + subObjectDesc;
+                GlobalVariables.getErrorMap().putErrorWithoutFullErrorPath(Constants.MAINTENANCE_NEW_MAINTAINABLE + Constants.CHART_OF_ACCOUNTS_CODE_PROPERTY_NAME, 
+                        KeyConstants.IndirectCostRecovery.ERROR_DOCUMENT_ICR_WILDCARDS_MUST_MATCH, new String[] {groupDesc, subObjectDesc});
+            }
+            return success;
+        }
+        // if no wildcards in this group, then this check is fine
+        return true;
+    }
+    
+    protected boolean isWildcard(String fieldValue) {
+        return StringUtils.equals(fieldValue, "@")  || StringUtils.equals(fieldValue, "#");
     }
 }

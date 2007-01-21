1ebe0984eb1416f8767b927e225d033201dd9d35
Shawn Choo <schoo@vivantech.com>
2007-01-21 22:12:59 +0000

    LaborScrubberServiceImpl

---
+++ work/src/org/kuali/kfs/module/ld/batch/service/impl/LaborScrubberServiceImpl.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2005-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.service.impl;
+
+import org.kuali.core.service.DateTimeService;
+import org.kuali.core.service.DocumentTypeService;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.service.PersistenceService;
+import org.kuali.core.util.SpringServiceLocator;
+import org.kuali.module.chart.service.ObjectCodeService;
+import org.kuali.module.chart.service.OffsetDefinitionService;
+import org.kuali.module.financial.service.FlexibleOffsetAccountService;
+import org.kuali.module.gl.bo.OriginEntryGroup;
+import org.kuali.module.gl.dao.UniversityDateDao;
+import org.kuali.module.gl.service.OriginEntryGroupService;
+import org.kuali.module.gl.service.OriginEntryService;
+import org.kuali.module.gl.service.ReportService;
+import org.kuali.module.gl.service.ScrubberService;
+import org.kuali.module.gl.service.ScrubberValidator;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class LaborScrubberServiceImpl implements ScrubberService, BeanFactoryAware {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(LaborScrubberServiceImpl.class);
+
+    private FlexibleOffsetAccountService flexibleOffsetAccountService;
+    private DocumentTypeService documentTypeService;
+    private BeanFactory beanFactory;
+    private OriginEntryService originEntryService;
+    private OriginEntryGroupService originEntryGroupService;
+    private DateTimeService dateTimeService;
+    private OffsetDefinitionService offsetDefinitionService;
+    private ObjectCodeService objectCodeService;
+    private KualiConfigurationService kualiConfigurationService;
+    private UniversityDateDao universityDateDao;
+    private PersistenceService persistenceService;
+    private ReportService reportService;
+    private ScrubberValidator scrubberValidator;
+
+    /**
+     * 
+     * @see org.kuali.module.gl.service.ScrubberService#scrubGroupReportOnly(org.kuali.module.gl.bo.OriginEntryGroup)
+     */
+    public void scrubGroupReportOnly(OriginEntryGroup group,String documentNumber) {
+        LOG.debug("scrubGroupReportOnly() started");
+
+        // The logic for this was moved into another object because the process was written using
+        // many instance variables which shouldn't be used for Spring services
+
+        LaborScrubberProcess sp = new LaborScrubberProcess(flexibleOffsetAccountService, documentTypeService, beanFactory, originEntryService, originEntryGroupService, dateTimeService, offsetDefinitionService, objectCodeService, kualiConfigurationService, universityDateDao, persistenceService, reportService, scrubberValidator);
+        sp.scrubGroupReportOnly(group,documentNumber);
+    }
+
+    /**
+     * 
+     * @see org.kuali.module.gl.service.ScrubberService#scrubEntries()
+     */
+    public void scrubEntries() {
+        LOG.debug("scrubEntries() started");
+
+        // The logic for this was moved into another object because the process was written using
+        // many instance variables which shouldn't be used for Spring services
+
+        LaborScrubberProcess sp = new LaborScrubberProcess(flexibleOffsetAccountService, documentTypeService, beanFactory, originEntryService, originEntryGroupService, dateTimeService, offsetDefinitionService, objectCodeService, kualiConfigurationService, universityDateDao, persistenceService, reportService, scrubberValidator);
+        sp.scrubEntries();
+    }
+
+    public void setFlexibleOffsetAccountService(FlexibleOffsetAccountService flexibleOffsetAccountService) {
+        this.flexibleOffsetAccountService = flexibleOffsetAccountService;
+    }
+
+    public void setDocumentTypeService(DocumentTypeService documentTypeService) {
+        this.documentTypeService = documentTypeService;
+    }
+
+    public void setScrubberValidator(ScrubberValidator sv) {
+        scrubberValidator = sv;
+    }
+
+    public void setOriginEntryService(OriginEntryService oes) {
+        this.originEntryService = oes;
+    }
+
+    public void setOriginEntryGroupService(OriginEntryGroupService groupService) {
+        this.originEntryGroupService = groupService;
+    }
+
+    public void setDateTimeService(DateTimeService dts) {
+        this.dateTimeService = dts;
+    }
+
+    public void setUniversityDateDao(UniversityDateDao universityDateDao) {
+        this.universityDateDao = universityDateDao;
+    }
+
+    public void setPersistenceService(PersistenceService ps) {
+        persistenceService = ps;
+    }
+
+    public void setOffsetDefinitionService(OffsetDefinitionService offsetDefinitionService) {
+        this.offsetDefinitionService = offsetDefinitionService;
+    }
+
+    public void setObjectCodeService(ObjectCodeService objectCodeService) {
+        this.objectCodeService = objectCodeService;
+    }
+
+    public void setKualiConfigurationService(KualiConfigurationService kualiConfigurationService) {
+        this.kualiConfigurationService = kualiConfigurationService;
+    }
+
+    public void setReportService(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    public void setBeanFactory(BeanFactory bf) throws BeansException {
+        beanFactory = bf;
+    }
+}

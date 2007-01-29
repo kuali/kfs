de9fac9c0c0458634a6e910b38f46fc6be5277a5
Naser Alavi <alavinas@ais.msu.edu>
2007-01-29 20:19:04 +0000

    KULPURAP-350

--- work/src/org/kuali/kfs/module/purap/PurapKeyConstants.java
+++ work/src/org/kuali/kfs/module/purap/PurapKeyConstants.java
@@ -63,6 +63,7 @@ public class PurapKeyConstants {
     public static final String ERROR_US_REQUIRES_ZIP = "error.vendorMaint.vendorAddress.USRequiresZip";
     public static final String ERROR_FAX_NUMBER = "error.vendorMaint.vendorAddress.faxNumber";
     public static final String ERROR_ADDRESS_TYPE = "error.vendorMaint.vendorAddress.addressType";
+    public static final String ERROR_ADDRESS_TYPE_DIVISIONS = "error.vendorMaint.vendorAddress.addressType.divisions";
     public static final String ERROR_ADDRESS_DEFAULT_CAMPUS = "error.vendorMaint.vendorDefaultAddress.campusCode";
     public static final String ERROR_ADDRESS_DEFAULT_INDICATOR = "error.vendorMaint.vendorAddress.vendorDefaultAddressIndicator";
     public static final String ERROR_ADDRESS_DEFAULT_CAMPUS_NOT_ALLOWED = "error.vendorMaint.vendorDefaultCampus.notAllowed";

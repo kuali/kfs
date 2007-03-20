d5eaf08805d2dcc3e961fb1d97a929427b9eeaba
Henry Shatavsky <shatavsky@gmail.com>
2007-03-20 00:43:30 +0000

    KULLAB-102: Develop CSF Tracker View Balance Inquiry

--- work/src/org/kuali/kfs/module/ld/dataaccess/impl/LaborDaoOjb.java
+++ work/src/org/kuali/kfs/module/ld/dataaccess/impl/LaborDaoOjb.java
@@ -36,72 +36,12 @@ import org.springmodules.orm.ojb.support.PersistenceBrokerDaoSupport;
  */
 public class LaborDaoOjb extends PersistenceBrokerDaoSupport implements LaborDao {
 
-
     public Collection getCSFTrackerData(Map fieldValues) {
-        Criteria criteria = buildCriteriaFromMap(fieldValues, new CalculatedSalaryFoundationTracker());
-
+        
+        Criteria criteria = new Criteria();        
+        criteria.addAndCriteria(OJBUtility.buildCriteriaFromMap(fieldValues, new CalculatedSalaryFoundationTracker()));
         LookupUtils.applySearchResultsLimit(criteria);
-
         QueryByCriteria query = QueryFactory.newQuery(CalculatedSalaryFoundationTracker.class, criteria);
         return getPersistenceBrokerTemplate().getCollectionByQuery(query);
     }
-
-    /**
-     * This method builds an OJB query criteria based on the input field map
-     * 
-     * @param fieldValues the input field map
-     * @param businessObject the given business object
-     * @return an OJB query criteria
-     */
-    public Criteria buildCriteriaFromMap(Map fieldValues, Object businessObject) {
-        Criteria criteria = new Criteria();
-
-        UniversityDate currentUniversityDate = SpringServiceLocator.getUniversityDateService().getCurrentUniversityDate();
-        String currentFiscalPeriodCode = currentUniversityDate.getUniversityFiscalAccountingPeriod();
-        Integer currentFiscalYear = currentUniversityDate.getUniversityFiscalYear();
-
-        // deal with null fiscal year and fiscal period code as current fiscal year and period code respectively
-        String fiscalPeriodFromForm = null;
-        if (fieldValues.containsKey(PropertyConstants.UNIVERSITY_FISCAL_PERIOD_CODE)) {
-            fiscalPeriodFromForm = (String) fieldValues.get(PropertyConstants.UNIVERSITY_FISCAL_PERIOD_CODE);
-        }
-
-        String fiscalYearFromForm = null;
-        if (fieldValues.containsKey(PropertyConstants.UNIVERSITY_FISCAL_YEAR)) {
-            fiscalYearFromForm = (String) fieldValues.get(PropertyConstants.UNIVERSITY_FISCAL_YEAR);
-        }
-
-        boolean includeNullFiscalPeriodCodeInLookup = null != currentFiscalPeriodCode && currentFiscalPeriodCode.equals(fiscalPeriodFromForm);
-        boolean includeNullFiscalYearInLookup = null != currentFiscalYear && currentFiscalYear.toString().equals(fiscalYearFromForm);
-
-        if (includeNullFiscalPeriodCodeInLookup) {
-            Criteria apValueCriteria = new Criteria();
-            apValueCriteria.addLike(PropertyConstants.UNIVERSITY_FISCAL_PERIOD_CODE, fiscalPeriodFromForm);
-
-            Criteria apNullCriteria = new Criteria();
-            apNullCriteria.addIsNull(PropertyConstants.UNIVERSITY_FISCAL_PERIOD_CODE);
-
-            apValueCriteria.addOrCriteria(apNullCriteria);
-            criteria.addAndCriteria(apValueCriteria);
-
-            fieldValues.remove(PropertyConstants.UNIVERSITY_FISCAL_PERIOD_CODE);
-        }
-
-        if (includeNullFiscalYearInLookup) {
-            Criteria fyValueCriteria = new Criteria();
-            fyValueCriteria.addEqualTo(PropertyConstants.UNIVERSITY_FISCAL_YEAR, fiscalYearFromForm);
-
-            Criteria fyNullCriteria = new Criteria();
-            fyNullCriteria.addIsNull(PropertyConstants.UNIVERSITY_FISCAL_YEAR);
-
-            fyValueCriteria.addOrCriteria(fyNullCriteria);
-            criteria.addAndCriteria(fyValueCriteria);
-
-            fieldValues.remove(PropertyConstants.UNIVERSITY_FISCAL_YEAR);
-        }
-
-        criteria.addAndCriteria(OJBUtility.buildCriteriaFromMap(fieldValues, businessObject));
-        return criteria;
-    }
-
 }

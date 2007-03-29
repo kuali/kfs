f3466c2de5098cc1726dc289fb3ad66781de9794
Henry Shatavsky <shatavsky@gmail.com>
2007-03-29 15:32:19 +0000

    KULLAB-91: Develop the BaseFundsAccountStatus Balance Inquiry

--- work/src/org/kuali/kfs/module/ld/dataaccess/impl/LaborDaoOjb.java
+++ work/src/org/kuali/kfs/module/ld/dataaccess/impl/LaborDaoOjb.java
@@ -40,79 +40,69 @@ import org.springmodules.orm.ojb.support.PersistenceBrokerDaoSupport;
 public class LaborDaoOjb extends PersistenceBrokerDaoSupport implements LaborDao {
     private LaborDaoOjb dao;
 
-    // CSF Tracker Inquiry
-    public Collection getCSFTrackerData(Map fieldValues) {        
-        Criteria criteria = new Criteria();        
+    public Collection getCSFTrackerData(Map fieldValues) {
+        Criteria criteria = new Criteria();
         criteria.addAndCriteria(OJBUtility.buildCriteriaFromMap(fieldValues, new CalculatedSalaryFoundationTracker()));
         LookupUtils.applySearchResultsLimit(criteria);
         QueryByCriteria query = QueryFactory.newQuery(CalculatedSalaryFoundationTracker.class, criteria);
         return getPersistenceBrokerTemplate().getCollectionByQuery(query);
     }
-    
-    // CSF Current Year Funds Inquiry
-    public Collection getCurrentYearFunds(Map fieldValues) {        
-        Criteria criteria = new Criteria();        
+
+    public Collection getCurrentYearFunds(Map fieldValues) {
+        Criteria criteria = new Criteria();
         criteria.addAndCriteria(OJBUtility.buildCriteriaFromMap(fieldValues, new BalanceByGeneralLedgerKey()));
         LookupUtils.applySearchResultsLimit(criteria);
         QueryByCriteria query = QueryFactory.newQuery(BalanceByGeneralLedgerKey.class, criteria);
         return getPersistenceBrokerTemplate().getCollectionByQuery(query);
     }
-    
-    // CSF Base Funds Inquiry
-    public Collection getBaseFunds1(Map fieldValues) {
-        
-        Criteria criteria = new Criteria();        
-        criteria.addAndCriteria(OJBUtility.buildCriteriaFromMap(fieldValues, new AccountStatus()));
-        System.out.println("Criteria:" + criteria);
-    //    LookupUtils.applySearchResultsLimit(criteria);
-      //  QueryByCriteria query = QueryFactory.newQuery(AccountStatus.class, criteria);
-        
-//        dao.sqlCommand("DELETE fp_bal_by_obj_t WHERE person_sys_id = USERENV('SESSIONID')");        
-        
-//        Criteria criteria = OJBUtility.buildCriteriaFromMap(fieldValues, new AccountStatus());
-        ReportQueryByCriteria query = QueryFactory.newReportQuery(AccountStatus.class, criteria);
-        return getPersistenceBrokerTemplate().getCollectionByQuery(query);
 
-    }
-    
-    // CSF Base Funds Inquiry
+    /**
+     * This method retrieves information for the Base Funds balance inquiry
+     * 
+     * @param fieldValues
+     * @return
+     */
     public Collection getBaseFunds(Map fieldValues) {
-    Statement stmt = null;
-    ArrayList baseFundList = new ArrayList();
-    String selectStatement = "SELECT distinct A1.UNIV_FISCAL_YR, A0.PERSON_UNVL_ID, A0.SUB_ACCT_NBR, A0.FIN_OBJECT_CD, A0.FIN_SUB_OBJ_CD, A0.OBJ_ID, A0.VER_NBR, A0.BUDGET_AMT, A0.CSF_AMT ";
-    String fromStatement = "FROM LD_BAL_GLBL_CSF_T A0, LD_LDGR_ENTR_T A1 WHERE ";
-    try {
-        Criteria criteria = new Criteria();
-        
-        String accountNumber = fieldValues.get("accountNumber").toString();
-        String universityFiscalYear = fieldValues.get("universityFiscalYear").toString();
-        
-        String whereStatement = "ACCOUNT_NBR like'" + accountNumber + "%' and UNIV_FISCAL_YR ='" + universityFiscalYear + "'";
-        Connection connection = getPersistenceBroker(true).serviceConnectionManager().getConnection();
-        stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery(selectStatement + fromStatement + whereStatement); 
-        while (rs.next()) {
-            System.out.println("#1");
-            AccountStatus as = new AccountStatus();
-            as.setUniversityFiscalYear(Integer.valueOf(rs.getString("UNIV_FISCAL_YR")));                    
-            as.setAccountNumber(rs.getString("ACCOUNT_NBR"));
-            baseFundList.add(as);        
-            System.out.println("#2");
-        }        
-    }
-    catch (Exception e) {
-        throw new RuntimeException("Unable to execute: " + e.getMessage());
-    }
-    finally {
+        Statement stmt = null;
+        ArrayList baseFundList = new ArrayList();
+        String selectStatement = "SELECT distinct A1.UNIV_FISCAL_YR, A1.FIN_COA_CD, A0.PERSON_UNVL_ID, A0.SUB_ACCT_NBR, A0.FIN_OBJECT_CD, A0.FIN_SUB_OBJ_CD, A0.OBJ_ID, A0.VER_NBR, A0.BUDGET_AMT, A0.CSF_AMT ";
+        String fromStatement = "FROM LD_BAL_GLBL_CSF_T A0, LD_LDGR_ENTR_T A1 WHERE ";
         try {
-            if (stmt != null) {
-                stmt.close();
+
+            // Parse the UI for parameters
+            String accountNumber = fieldValues.get("accountNumber").toString();
+            String universityFiscalYear = fieldValues.get("universityFiscalYear").toString();
+            String charOfAccountsCode = fieldValues.get("charOfAccountsCode").toString();
+            String financialObjectCode = fieldValues.get("financialObjectCode").toString();
+
+            // Build the where statement
+            String whereStatement = "ACCOUNT_NBR like'" + accountNumber + "%' and UNIV_FISCAL_YR ='" + universityFiscalYear + "'";
+                        
+            // Use the KFS Connection String to retrieve data from the database
+            Connection connection = getPersistenceBroker(true).serviceConnectionManager().getConnection();
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(selectStatement + fromStatement + whereStatement);
+            while (rs.next()) {
+                AccountStatus as = new AccountStatus();
+                as.setUniversityFiscalYear(Integer.valueOf(rs.getString("UNIV_FISCAL_YR")));
+                as.setAccountNumber(rs.getString("ACCOUNT_NBR"));
+                baseFundList.add(as);
             }
         }
         catch (Exception e) {
-            throw new RuntimeException("Unable to close connection: " + e.getMessage());
+            throw new RuntimeException("Unable to execute: " + e.getMessage());
         }
-    }
-    return baseFundList;    
+        finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            }
+            catch (Exception e) {
+                throw new RuntimeException("Unable to close database connection: " + e.getMessage());
+            }
+        }
+        // Return the query
+        return baseFundList;
     }
 }

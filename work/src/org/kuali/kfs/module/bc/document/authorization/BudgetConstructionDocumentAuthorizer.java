e546b818f12e95134a6cfce1a0e5e1d2200004d3
Gary	Palmer <gwpalmer@iu.edu>
2007-03-30 16:15:17 +0000

    overrode getEditMode() and getDocumentActionFlags()

--- work/src/org/kuali/kfs/module/bc/document/authorization/BudgetConstructionDocumentAuthorizer.java
+++ work/src/org/kuali/kfs/module/bc/document/authorization/BudgetConstructionDocumentAuthorizer.java
@@ -15,10 +15,68 @@
  */
 package org.kuali.module.budget.document.authorization;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kuali.Constants;
+import org.kuali.core.authorization.AuthorizationConstants;
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.Document;
+import org.kuali.core.document.authorization.DocumentActionFlags;
 import org.kuali.core.document.authorization.DocumentAuthorizerBase;
+import org.kuali.core.exceptions.GroupNotFoundException;
+import org.kuali.core.workflow.service.KualiWorkflowDocument;
 import org.kuali.kfs.document.authorization.AccountingDocumentAuthorizerBase;
+import org.kuali.rice.KNSServiceLocator;
+
+import edu.iu.uis.eden.EdenConstants;
+import edu.iu.uis.eden.clientapp.vo.ValidActionsVO;
 
 //public class BudgetConstructionDocumentAuthorizer extends AccountingDocumentAuthorizerBase {
 public class BudgetConstructionDocumentAuthorizer extends DocumentAuthorizerBase {
-    
+    private static Log LOG = LogFactory.getLog(BudgetConstructionDocumentAuthorizer.class);
+
+    /**
+     * This inits and returns an editModeMap based on the BC security model
+     * 
+     * @see org.kuali.core.document.authorization.DocumentAuthorizerBase#getEditMode(org.kuali.core.document.Document, org.kuali.core.bo.user.UniversalUser)
+     */
+    @Override
+    public Map getEditMode(Document d, UniversalUser u) {
+
+//        return super.getEditMode(d, u);
+
+        /*
+         * TODO eventually this needs to call service methods that implements the BC security model
+         */ 
+        Map editModeMap = new HashMap();
+        String editMode = AuthorizationConstants.EditMode.FULL_ENTRY;
+        editModeMap.put(editMode, "TRUE");
+
+        return editModeMap;
+    }
+
+    /**
+     * @see org.kuali.core.document.authorization.DocumentAuthorizerBase#getDocumentActionFlags(org.kuali.core.document.Document, org.kuali.core.bo.user.UniversalUser)
+     */
+    @Override
+    public DocumentActionFlags getDocumentActionFlags(Document document, UniversalUser user) {
+//TODO this needs to call service methods that implements the BC security model
+//        return super.getDocumentActionFlags(document, user);
+            LOG.debug("calling BudgetConstructionDocumentAuthorizer.getDocumentActionFlags for document '" + document.getDocumentNumber() + "'. user '" + user.getPersonUserIdentifier() + "'");
+
+            DocumentActionFlags flags = new DocumentActionFlags(); // all flags default to false
+
+            flags.setCanClose(true);
+            flags.setCanSave(true);
+
+            //TODO is this needed for BC??
+            setAnnotateFlag(flags);
+
+            return flags;
+    }
+
+
 }

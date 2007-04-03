c1b8e738ce778ebb79d4605bf89a21a54834ada4
Shawn Choo <schoo@vivantech.com>
2007-04-03 02:51:17 +0000

    Created Labor Specified Transaction BO class

---
+++ work/src/org/kuali/kfs/module/ld/businessobject/LaborTransaction.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.bo;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import org.kuali.core.bo.DocumentHeader;
+import org.kuali.core.bo.DocumentType;
+import org.kuali.kfs.bo.OriginationCode;
+import org.kuali.module.chart.bo.Account;
+import org.kuali.module.chart.bo.AccountingPeriod;
+import org.kuali.module.chart.bo.Chart;
+import org.kuali.module.chart.bo.ObjectCode;
+import org.kuali.module.chart.bo.SubAccount;
+import org.kuali.module.chart.bo.SubObjCd;
+import org.kuali.module.gl.bo.Transaction;
+
+public interface LaborTransaction extends Transaction {
+    
+    public String getPositionNumber();
+    public Date getTransactionPostingDate();
+    public Date getPayPeriodEndDate();
+    public BigDecimal getTransactionTotalHours();
+    public Integer getPayrollEndDateFiscalYear();
+    public String getPayrollEndDateFiscalPeriodCode();
+    public String getFinancialDocumentApprovedCode();
+    public String getTransactionEntryOffsetCode();
+    public Date getTransactionEntryProcessedTimestamp();
+    public String getEmplid();
+    public Integer getEmployeeRecord();
+    public String getEarnCode();
+    public String getPayGroup();
+    public String getSalaryAdministrationPlan();
+    public String getGrade();
+    public String getRunIdentifier();
+    
+    public String getLaborLedgerOriginalChartOfAccountsCode();
+    public String getLaborLedgerOriginalAccountNumber();
+    public String getLaborLedgerOriginalSubAccountNumber();
+    public String getLaborLedgerOriginalFinancialObjectCode();
+    public String getLaborLedgerOriginalFinancialSubObjectCode();
+    public String getHrmsCompany();
+    public String getSetid();
+    //public Date transactionDateTimeStamp;
+
+    public ObjectCode getLaborLedgerOriginalFinancialObject();
+    public Chart getLaborLedgerOriginalChartOfAccounts();
+    public Account getLaborLedgerOriginalAccount();
+    
+    public DocumentHeader getFinancialDocument();
+    public DocumentType getReferenceFinancialDocumentType();
+    public OriginationCode getReferenceFinancialSystemOrigination();
+    public AccountingPeriod getPayrollEndDateFiscalPeriod();
+    public SubAccount getLaborLedgerOriginalSubAccount();
+    public SubObjCd getLaborLedgerOriginalFinancialSubObject();
+    
+    
+    
+    
+}

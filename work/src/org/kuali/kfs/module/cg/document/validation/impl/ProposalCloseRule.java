49b1c10bdcdd4e5c169110d62e6d7a6c9fd91d8a
Laran Evans <laran@laranevans.com>
2007-04-06 21:30:37 +0000

    KULCG-241 Implemented maintenance document for proposal close. Created a business rule that requires that the proposal close date be greater than or equal to the date on which the document is created.

---
+++ work/src/org/kuali/kfs/module/cg/document/validation/impl/ProposalCloseRule.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2005-2006 The Kuali Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.cg.rules;
+
+import org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.core.document.Document;
+import org.kuali.module.cg.bo.ProposalClose;
+import org.kuali.kfs.util.SpringServiceLocator;
+import org.kuali.KeyConstants;
+
+import java.sql.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: evans
+ * Date: Apr 6, 2007
+ * Time: 1:25:50 PM
+ */
+public class ProposalCloseRule extends MaintenanceDocumentRuleBase {
+
+    /**
+     *
+     * @param document
+     * @return true if
+     */
+    @Override
+    public boolean processSaveDocument(Document document) {
+        boolean isOk = super.processSaveDocument(document);
+        if(!isOk) {
+            return isOk;
+        }
+        ProposalClose close = (ProposalClose) document.getDocumentBusinessObject();
+        Date userDate = close.getUserInitiatedCloseDate();
+        Date today = SpringServiceLocator.getDateTimeService().getCurrentSqlDateMidnight();
+        isOk = today.getTime() <= userDate.getTime();
+        if(!isOk) {
+            putFieldError("userInitiatedCloseDate", KeyConstants.ContractsAndGrants.USER_INITIATED_DATE_TOO_EARLY, userDate.toString());
+        }
+        return isOk;
+    }
+}

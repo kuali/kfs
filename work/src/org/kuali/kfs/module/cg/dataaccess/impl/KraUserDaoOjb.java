5e3fc0eade08d9ba0b70229a9c0b00907dcc0c74
Jonathan Keller <keller.jonathan@gmail.com>
2007-04-16 22:39:41 +0000

    KULRNE-4530 : adapted user active logic for lookups to use OJB criteria instead of straight SQL

---
+++ work/src/org/kuali/kfs/module/cg/dataaccess/impl/KraUserDaoOjb.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.kra.budget.dao.ojb;
+
+import java.util.Arrays;
+
+import org.apache.ojb.broker.query.Criteria;
+import org.kuali.core.bo.BusinessRule;
+import org.kuali.core.dao.ojb.KualiModuleUserDaoOjb;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.module.kra.KraConstants;
+
+public class KraUserDaoOjb extends KualiModuleUserDaoOjb {
+
+	KualiConfigurationService configService;
+	
+	@Override
+	public Object getActiveUserQueryCriteria(String moduleId) {
+        BusinessRule rule = getConfigService().getApplicationRule( KraConstants.KRA_ADMIN_GROUP_NAME, KraConstants.ALLOWED_EMPLOYEE_STATUS_RULE  );
+        String[] values = rule.getRuleText().split( ";" );
+
+		Criteria criteria = new Criteria();
+		criteria.addEqualTo("staff", "Y");
+		Criteria isFacultyCriteria = new Criteria();
+		isFacultyCriteria.addEqualTo("faculty", "Y");
+		criteria.addOrCriteria(isFacultyCriteria);
+		criteria.addIn("employeeStatusCode", Arrays.asList(values));
+		return criteria; 
+        
+	}
+
+	public KualiConfigurationService getConfigService() {
+		return configService;
+	}
+
+	public void setConfigService(KualiConfigurationService configService) {
+		this.configService = configService;
+	}
+	
+}

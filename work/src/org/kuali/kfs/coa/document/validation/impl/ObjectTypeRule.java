b8f3dcb4075e76cc1a939b0f76d4d5307e2b1adb
James Smith <smith750@iu.edu>
2007-04-17 20:53:00 +0000

    KULRNE-4342
    Account Category now maintainable; proper maintenance for Account Category field added to Object Type maintenance document plus a rule to make sure that Object Type's Account Category is valid.

---
+++ work/src/org/kuali/kfs/coa/document/validation/impl/ObjectTypeRule.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.chart.rules;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.kuali.KeyConstants;
+import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.module.chart.bo.AccountCategory;
+import org.kuali.module.chart.bo.ObjectType;
+
+public class ObjectTypeRule extends MaintenanceDocumentRuleBase {
+
+    /**
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomRouteDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */
+    @Override
+    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+        boolean success = true;
+        success &= checkAccountingCategory(document);
+        return success;
+    }
+
+    /**
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomSaveDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */
+    @Override
+    protected boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
+        checkAccountingCategory(document);
+        return true; // it's a save, always return true
+    }
+
+    protected boolean checkAccountingCategory(MaintenanceDocument document) {
+        boolean result = true;
+        ObjectType objectType = (ObjectType)document.getNewMaintainableObject().getBusinessObject();
+        Map pkMap = new HashMap();
+        pkMap.put("accountCategoryCode", objectType.getAccountCategoryCode());
+        AccountCategory accountCategory = (AccountCategory)this.getBoService().findByPrimaryKey(AccountCategory.class, pkMap);
+        if (accountCategory == null) {
+            result = false;
+            putFieldError("accountCategoryCode", KeyConstants.ERROR_DOCUMENT_OBJTYPE_INVALID_ACCT_CTGRY, new String[] { objectType.getAccountCategoryCode() });
+        }
+        return result;
+    }
+}

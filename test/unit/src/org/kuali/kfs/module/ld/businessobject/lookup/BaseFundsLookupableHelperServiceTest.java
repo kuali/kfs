a7a9b9c148668dc207accd78e070c131c3f2b66d
Henry Shatavsky <shatavsky@gmail.com>
2007-04-17 22:37:41 +0000

    KULLAB-91: Develop the BaseFundsAccountStatus Balance Inquiry Added unit testing code. This currently has no asserts, so will not mess up anthill.

--- test/unit/src/org/kuali/kfs/module/ld/businessobject/lookup/BaseFundsLookupableHelperServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/ld/businessobject/lookup/BaseFundsLookupableHelperServiceTest.java
@@ -24,16 +24,15 @@ import java.util.Properties;
 import org.kuali.PropertyConstants;
 import org.kuali.core.lookup.LookupableHelperService;
 import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.service.PersistenceService;
 import org.kuali.kfs.util.SpringServiceLocator;
-import org.kuali.module.gl.bo.AccountBalance;
 import org.kuali.module.gl.web.TestDataGenerator;
 import org.kuali.module.labor.bo.AccountStatusBaseFunds;
 import org.kuali.module.labor.bo.LedgerBalance;
-import org.kuali.module.labor.dao.LaborDao;
+import org.kuali.module.labor.service.LaborNightlyOutService;
 import org.kuali.module.labor.util.ObjectUtil;
 import org.kuali.test.KualiTestBase;
 import org.kuali.test.WithTestSpringContext;
-import org.kuali.test.suite.RelatesTo;
 import org.springframework.beans.factory.BeanFactory;
 
 /**
@@ -43,57 +42,75 @@ import org.springframework.beans.factory.BeanFactory;
 public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
     private BusinessObjectService businessObjectService;
     private LookupableHelperService lookupableHelperService;
+    private PersistenceService persistenceService;
+
     private BeanFactory beanFactory;
-    
+    private Properties properties;
+    private String fieldNames, documentFieldNames;
+    private String deliminator;
+
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        
-        // TODO: put your test data file name here
-        String messageFileName = "test/src/org/kuali/module/gl/web/message.properties";
-        String propertiesFileName = "test/src/org/kuali/module/gl/web/data.properties";
+
+        String messageFileName = "test/src/org/kuali/module/labor/web/lookupable/testdata/message.properties";
+        String propertiesFileName = "test/src/org/kuali/module/labor/web/lookupable/testdata/accountStatusBaseFunds.properties";
+
+        properties = (new TestDataGenerator(propertiesFileName, messageFileName)).getProperties();
+        fieldNames = properties.getProperty("fieldNames");
+        documentFieldNames = properties.getProperty("fieldNames");
+        deliminator = properties.getProperty("deliminator");
+
         TestDataGenerator testDataGenerator = new TestDataGenerator(propertiesFileName, messageFileName);
-                
+
+        BeanFactory beanFactory = SpringServiceLocator.getBeanFactory();
+        businessObjectService = (BusinessObjectService) beanFactory.getBean("businessObjectService");
+        persistenceService = (PersistenceService) beanFactory.getBean("persistenceService");
+
+        int numberOfDocuments = Integer.valueOf(properties.getProperty("getAccountStatusBaseFunds.numOfData"));
+        List<LedgerBalance> inputDataList = this.getInputDocumentList("getAccountStatusBaseFunds.testData", numberOfDocuments);
+        businessObjectService.save(inputDataList);
+
         beanFactory = SpringServiceLocator.getBeanFactory();
         businessObjectService = (BusinessObjectService) beanFactory.getBean("businessObjectService");
-        
-        lookupableHelperService = (LookupableHelperService) beanFactory.getBean("BaseFundsLookupableHelperService"); 
+
+        lookupableHelperService = (LookupableHelperService) beanFactory.getBean("BaseFundsLookupableHelperService");
         lookupableHelperService.setBusinessObjectClass(AccountStatusBaseFunds.class);
-        
+
         // TODO: clear up the database so that any existing data cannot affact your test result
         // businessObjectService.deleteMatching(clazz, fieldValues);
     }
 
-    public void testGetSearchResults() throws Exception {       
-        // TODO: Insert test data into the ledger balance table
+    public void testGetSearchResults() throws Exception {
         // insertNewRecord(ledgerBalance);
-        
+
         AccountStatusBaseFunds accountStatusBaseFunds = new AccountStatusBaseFunds();
         accountStatusBaseFunds.setAccountNumber("1031400");
-        accountStatusBaseFunds.setUniversityFiscalYear(2004);
+        accountStatusBaseFunds.setUniversityFiscalYear(2007);
 
         // test the search results before the specified entry is inserted into the database
         Map fieldValues = buildFieldValues(accountStatusBaseFunds, this.getLookupFields(false));
         List searchResults = lookupableHelperService.getSearchResults(fieldValues);
-        
-        if(searchResults != null){
-            System.out.println(searchResults.size());
+
+        if (searchResults != null) {
+            System.out.println("Results Size:" + searchResults.size());
         }
-        
+
         // TODO: construct your expected results into a list of AccountStatusBaseFunds
         List<AccountStatusBaseFunds> expectedAccountStatusBaseFunds = null;
-        
+
         // TODO: compare the search results with the expected and see if they match with each other
-        //assertTrue(!contains(expectedAccountStatusBaseFunds, searchResults));
+        // assertTrue(!contains(expectedAccountStatusBaseFunds, searchResults));
     }
-    
-    private Map<String, String> buildFieldValues(AccountStatusBaseFunds accountStatusBaseFunds, List<String> lookupFields){
+
+    private Map<String, String> buildFieldValues(AccountStatusBaseFunds accountStatusBaseFunds, List<String> lookupFields) {
         Map<String, String> fieldValues = new HashMap<String, String>();
-        
+
         Map<String, Object> tempFieldValues = ObjectUtil.buildPropertyMap(accountStatusBaseFunds, lookupFields);
-        for(String key : tempFieldValues.keySet()){
-            fieldValues.put(key, tempFieldValues.get(key).toString());          
-        }       
+        for (String key : tempFieldValues.keySet()) {
+            fieldValues.put(key, tempFieldValues.get(key).toString());
+        }
         return fieldValues;
     }
 
@@ -107,7 +124,7 @@ public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
     }
 
     /**
-     * This method inserts a new account balance record into database
+     * This method inserts a new Base Funds account balance record into database
      * 
      * @param accounBalance the given account balance
      */
@@ -117,5 +134,25 @@ public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
         }
         catch (Exception e) {
         }
+    }
+
+    /**
+     * 
+     * This method generates ledger balance data objects and returns them for a temporary update.
+     * @param propertyKeyPrefix
+     * @param numberOfInputData
+     * @return
+     */
+    private List<LedgerBalance> getInputDocumentList(String propertyKeyPrefix, int numberOfInputData) {
+        List<LedgerBalance> inputDataList = new ArrayList<LedgerBalance>();
+        for (int i = 1; i <= numberOfInputData; i++) {
+            String propertyKey = propertyKeyPrefix + i;
+            System.out.println("propertyKey:" + propertyKey);
+            System.out.println("documentFieldNames:" + documentFieldNames);
+            LedgerBalance inputData = new LedgerBalance();
+            ObjectUtil.populateBusinessObject(inputData, properties, propertyKey, documentFieldNames, deliminator);
+            inputDataList.add(inputData);
+        }
+        return inputDataList;
     }
 }

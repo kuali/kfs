ac10b401858a91c166e769856850955ec92bf949
Henry Shatavsky <shatavsky@gmail.com>
2007-04-18 22:02:38 +0000

    KULLAB-91 Develop BaseFundsAccountStatus Junit Tests

--- test/unit/src/org/kuali/kfs/module/ld/businessobject/lookup/BaseFundsLookupableHelperServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/ld/businessobject/lookup/BaseFundsLookupableHelperServiceTest.java
@@ -21,15 +21,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.kuali.Constants;
 import org.kuali.PropertyConstants;
 import org.kuali.core.lookup.LookupableHelperService;
 import org.kuali.core.service.BusinessObjectService;
 import org.kuali.core.service.PersistenceService;
 import org.kuali.kfs.util.SpringServiceLocator;
+import org.kuali.module.budget.bo.CalculatedSalaryFoundationTracker;
 import org.kuali.module.gl.web.TestDataGenerator;
 import org.kuali.module.labor.bo.AccountStatusBaseFunds;
 import org.kuali.module.labor.bo.LedgerBalance;
-import org.kuali.module.labor.service.LaborNightlyOutService;
 import org.kuali.module.labor.util.ObjectUtil;
 import org.kuali.test.KualiTestBase;
 import org.kuali.test.WithTestSpringContext;
@@ -48,29 +49,14 @@ public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
     private Properties properties;
     private String fieldNames, documentFieldNames;
     private String deliminator;
-
+    private int csfNumberOfTestData;
+    private int csfExpectedInsertion;
+    private int baseFundsNumberOfTestData;
+    private int baseFundsExpectedInsertion;    
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-
-        String messageFileName = "test/src/org/kuali/module/labor/web/lookupable/testdata/message.properties";
-        String propertiesFileName = "test/src/org/kuali/module/labor/web/lookupable/testdata/accountStatusBaseFunds.properties";
-
-        properties = (new TestDataGenerator(propertiesFileName, messageFileName)).getProperties();
-        fieldNames = properties.getProperty("fieldNames");
-        documentFieldNames = properties.getProperty("fieldNames");
-        deliminator = properties.getProperty("deliminator");
-
-        TestDataGenerator testDataGenerator = new TestDataGenerator(propertiesFileName, messageFileName);
-
-        BeanFactory beanFactory = SpringServiceLocator.getBeanFactory();
-        businessObjectService = (BusinessObjectService) beanFactory.getBean("businessObjectService");
-        persistenceService = (PersistenceService) beanFactory.getBean("persistenceService");
-
-        int numberOfDocuments = Integer.valueOf(properties.getProperty("getAccountStatusBaseFunds.numOfData"));
-        List<LedgerBalance> inputDataList = this.getInputDocumentList("getAccountStatusBaseFunds.testData", numberOfDocuments);
-        businessObjectService.save(inputDataList);
 
         beanFactory = SpringServiceLocator.getBeanFactory();
         businessObjectService = (BusinessObjectService) beanFactory.getBean("businessObjectService");
@@ -78,16 +64,23 @@ public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
         lookupableHelperService = (LookupableHelperService) beanFactory.getBean("BaseFundsLookupableHelperService");
         lookupableHelperService.setBusinessObjectClass(AccountStatusBaseFunds.class);
 
-        // TODO: clear up the database so that any existing data cannot affact your test result
-        // businessObjectService.deleteMatching(clazz, fieldValues);
+        // Clear up the database so that any existing data cannot affact your test result
+        HashMap keys = new HashMap();
+        keys.put(PropertyConstants.ACCOUNT_NUMBER, "1031400");
+        keys.put(PropertyConstants.UNIVERSITY_FISCAL_YEAR, "2007");
+        keys.put(PropertyConstants.CHART_OF_ACCOUNTS_CODE, "BL");        
+        businessObjectService.deleteMatching(LedgerBalance.class, keys);
+        businessObjectService.deleteMatching(CalculatedSalaryFoundationTracker.class, keys);        
     }
 
     public void testGetSearchResults() throws Exception {
-        // insertNewRecord(ledgerBalance);
+        insertBaseFundsRecords();
+        insertCSFRecords();
 
         AccountStatusBaseFunds accountStatusBaseFunds = new AccountStatusBaseFunds();
         accountStatusBaseFunds.setAccountNumber("1031400");
         accountStatusBaseFunds.setUniversityFiscalYear(2007);
+        accountStatusBaseFunds.setChartOfAccountsCode("BL");
 
         // test the search results before the specified entry is inserted into the database
         Map fieldValues = buildFieldValues(accountStatusBaseFunds, this.getLookupFields(false));
@@ -97,11 +90,8 @@ public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
             System.out.println("Results Size:" + searchResults.size());
         }
 
-        // TODO: construct your expected results into a list of AccountStatusBaseFunds
-        List<AccountStatusBaseFunds> expectedAccountStatusBaseFunds = null;
-
-        // TODO: compare the search results with the expected and see if they match with each other
-        // assertTrue(!contains(expectedAccountStatusBaseFunds, searchResults));
+        // compare the search results with the expected and see if they match with each other        
+        assertEquals(this.baseFundsExpectedInsertion,searchResults.size());
     }
 
     private Map<String, String> buildFieldValues(AccountStatusBaseFunds accountStatusBaseFunds, List<String> lookupFields) {
@@ -119,40 +109,67 @@ public class BaseFundsLookupableHelperServiceTest extends KualiTestBase {
 
         lookupFields.add(PropertyConstants.UNIVERSITY_FISCAL_YEAR);
         lookupFields.add(PropertyConstants.ACCOUNT_NUMBER);
+        lookupFields.add(PropertyConstants.CHART_OF_ACCOUNTS_CODE);
 
         return lookupFields;
     }
 
-    /**
-     * This method inserts a new Base Funds account balance record into database
-     * 
-     * @param accounBalance the given account balance
-     */
-    protected void insertNewRecord(LedgerBalance ledgerBalance) {
-        try {
-            businessObjectService.save(ledgerBalance);
-        }
-        catch (Exception e) {
-        }
-    }
+    protected void insertCSFRecords() {
+        String messageFileName = "test/src/org/kuali/module/labor/web/testdata/message.properties";
+        String propertiesFileName = "test/src/org/kuali/module/labor/web/testdata/csfTracker.properties";
 
-    /**
-     * 
-     * This method generates ledger balance data objects and returns them for a temporary update.
-     * @param propertyKeyPrefix
-     * @param numberOfInputData
-     * @return
-     */
-    private List<LedgerBalance> getInputDocumentList(String propertyKeyPrefix, int numberOfInputData) {
+        properties = (new TestDataGenerator(propertiesFileName, messageFileName)).getProperties();
+        fieldNames = properties.getProperty("fieldNames");
+        documentFieldNames = properties.getProperty("fieldNames");
+        deliminator = properties.getProperty("deliminator");
+
+        TestDataGenerator testDataGenerator = new TestDataGenerator(propertiesFileName, messageFileName);
+
+        BeanFactory beanFactory = SpringServiceLocator.getBeanFactory();
+        businessObjectService = (BusinessObjectService) beanFactory.getBean("businessObjectService");
+        persistenceService = (PersistenceService) beanFactory.getBean("persistenceService");
+
+        int numberOfDocuments = Integer.valueOf(properties.getProperty("getCSFTracker.numOfData"));
+        List<CalculatedSalaryFoundationTracker> inputDataList = new ArrayList<CalculatedSalaryFoundationTracker>();
+        for (int i = 1; i <= numberOfDocuments; i++) {
+            String propertyKey = "getCSFTracker.testData" + i;
+            CalculatedSalaryFoundationTracker inputData = new CalculatedSalaryFoundationTracker();
+            ObjectUtil.populateBusinessObject(inputData, properties, propertyKey, documentFieldNames, deliminator);
+            inputDataList.add(inputData);
+        }
+        
+        String testTarget = "getCSFTracker.";
+        this.csfNumberOfTestData = Integer.valueOf(properties.getProperty(testTarget + "numOfData"));
+        this.csfExpectedInsertion = Integer.valueOf(properties.getProperty(testTarget + "expectedInsertion"));
+        businessObjectService.save(inputDataList);
+    }
+    
+    protected void insertBaseFundsRecords() {
+        String messageFileName = "test/src/org/kuali/module/labor/web/testdata/message.properties";
+        String propertiesFileName = "test/src/org/kuali/module/labor/web/testdata/accountStatusBaseFunds.properties";
+
+        properties = (new TestDataGenerator(propertiesFileName, messageFileName)).getProperties();
+        fieldNames = properties.getProperty("fieldNames");
+        documentFieldNames = properties.getProperty("fieldNames");
+        deliminator = properties.getProperty("deliminator");
+
+        TestDataGenerator testDataGenerator = new TestDataGenerator(propertiesFileName, messageFileName);
+
+        BeanFactory beanFactory = SpringServiceLocator.getBeanFactory();
+        businessObjectService = (BusinessObjectService) beanFactory.getBean("businessObjectService");
+        persistenceService = (PersistenceService) beanFactory.getBean("persistenceService");
+
+        int numberOfDocuments = Integer.valueOf(properties.getProperty("getAccountStatusBaseFunds.numOfData"));
         List<LedgerBalance> inputDataList = new ArrayList<LedgerBalance>();
-        for (int i = 1; i <= numberOfInputData; i++) {
-            String propertyKey = propertyKeyPrefix + i;
-            System.out.println("propertyKey:" + propertyKey);
-            System.out.println("documentFieldNames:" + documentFieldNames);
+        for (int i = 1; i <= numberOfDocuments; i++) {
+            String propertyKey = "getAccountStatusBaseFunds.testData" + i;
             LedgerBalance inputData = new LedgerBalance();
             ObjectUtil.populateBusinessObject(inputData, properties, propertyKey, documentFieldNames, deliminator);
             inputDataList.add(inputData);
         }
-        return inputDataList;
-    }
+        String testTarget = "getAccountStatusBaseFunds.";
+        this.baseFundsNumberOfTestData = Integer.valueOf(properties.getProperty(testTarget + "numOfData"));
+        this.baseFundsExpectedInsertion = Integer.valueOf(properties.getProperty(testTarget + "expectedInsertion"));
+        businessObjectService.save(inputDataList);
+    }  
 }

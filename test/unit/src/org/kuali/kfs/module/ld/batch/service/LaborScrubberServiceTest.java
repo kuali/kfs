ed2b50a0ab114f6ce3f531c582ea41f4f388544d
Shawn Choo <schoo@vivantech.com>
2007-04-18 19:32:52 +0000

    Update unit test

--- test/unit/src/org/kuali/kfs/module/ld/batch/service/LaborScrubberServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/ld/batch/service/LaborScrubberServiceTest.java
@@ -69,16 +69,16 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
                                        "2007BA6044900-----2400---ACEX07PAY PLM04013107     0000300015213----------KUALI TEST DESCRIPTION                  +000000000002716.89D2007-01-25                                                 2007-01-252007-01-31000092.942007070000683206 000REGS12PAE 13 M004013107                     IU IUBLA",
                                        "2007BA6044900-----2400---ACEX07PAY PLM04013107     0000400017659----------KUALI TEST DESCRIPTION                  +000000000001620.08D2007-01-25                                                 2007-01-252007-01-31000045.322007070001316908 000REGS12PAE 13 M004013107                     IU IUBLA"};
 
-        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[1]),
-                                             new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[2]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[3]),
-                                             new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[4]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[5]),
-                                             new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[6]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[7]),
-                                             new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[8]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[9]),
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[0]), new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[1]),
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[2]), new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[3]),
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[4]), new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[5]),
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[6]), new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[7]),
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[8]), new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[9]),
+        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[1]),
+                                             new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[2]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[3]),
+                                             new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[4]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[5]),
+                                             new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[6]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[7]),
+                                             new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[8]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[9]),
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[1]),
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[2]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[3]),
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[4]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[5]),
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[6]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[7]),
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[8]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[9]),
                                              };
         
         
@@ -92,7 +92,7 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
     
     public void testBlankFiscalYear() throws Exception {
         String[] inputTransactions = { "    BA6044900-----2400---ACEX06PAY PLBLANKFISC     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003493.50D2006-12-22                                                 2006-12-222006-12-31000168.002007060000149952 001REGS12PAE 11 M001010207                     IU IUBLA"};
-        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.SCRUBBER_VALID, inputTransactions[0])};
+        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_VALID, inputTransactions[0])};
         
         scrub(inputTransactions);
         assertOriginEntries(4, outputTransactions);
@@ -103,8 +103,8 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
     public void testInvalidObjectCode() throws Exception {
         String[] inputTransactions = { "2000BA6044906-----2400---ACEX06PAY PLCLOSEFISC     0000300015213----------KUALI TEST DESCRIPTION                  +000000000002716.89D2006-12-22                                                 2006-12-222006-12-31000084.862007060000683206 000REGS12PAE 13 M001010207                     IU IUBLA",
                                        "2000BA6044906-----2400---ACEX06PAY PLCLOSEFISC     0000400017659----------KUALI TEST DESCRIPTION                  +000000000001620.08D2006-12-22                                                 2006-12-222006-12-31000041.382007060001316908 000REGS12PAE 13 M001010207                     IU IUBLA"};
-        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[1]), 
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[1])};
+        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[1]), 
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[1])};
         
         scrub(inputTransactions);
         assertOriginEntries(4, outputTransactions);
@@ -114,8 +114,8 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
     public void testInvalidFiscalYear() throws Exception {
         String[] inputTransactions = { "2026BA6044913-----2400---ACEX06PAY PLINVALFISC     0000500019196----------KUALI TEST DESCRIPTION                  +000000000005106.15D2006-12-22                                                 2006-12-222006-12-31000104.932007060001368813 000REGS12PAE 19 M001010207                     IU IUBLA",
                                        "2026BA6044913-----2400---ACEX06PAY PLINVALFISC     0000600022120----------KUALI TEST DESCRIPTION                  +000000000003071.10D2006-12-22                                                 2006-12-222006-12-31000106.752007060001773996 000REGS12PAE 12 M001010207                     IU IUBLA"};
-        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[1]), 
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[1])};
+        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[1]), 
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[1])};
         
         scrub(inputTransactions);
         assertOriginEntries(4, outputTransactions);
@@ -125,14 +125,24 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
     public void testInvalid() throws Exception {
         String[] inputTransactions = { "2007  1031400-----2400---ACEX07PAY PLBLANKCHAR     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003493.50D2007-01-25                                                 2007-01-252007-01-31000184.002007070000149952 001REGS12PAE 11 M004013107                     IU IUBLA",
                                        "2007  1031400-----2400---ACEX07PAY PLBLANKCHAR     0000200014789----------KUALI TEST DESCRIPTION                  +000000000003329.27D2007-01-25                                                 2007-01-252007-01-31000090.162007070000649044 000REGS12PAE 16 M004013107                     IU IUBLA"};
-        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[1]), 
-                                             new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[1])};
+        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[1]), 
+                                             new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[1])};
         
         scrub(inputTransactions);
         assertOriginEntries(4, outputTransactions);
     
     }
 
+    public void testInvalidDebitCreditCode() throws Exception {
+        String[] inputTransactions = { "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003493.50X2006-12-22                                                 2006-12-222006-12-31000168.002007060000149952 001REGS12PAE 11 M001010207                     IU IUBLA"
+                                     , "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003495.50X2006-12-25                                                 2006-12-222006-12-31000168.002007060000149952 001REGS12PAE 11 M001010207                     IU IUBLA" };
+
+        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_BACKUP, inputTransactions[1]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.LABOR_SCRUBBER_ERROR, inputTransactions[1]) };
+
+        scrub(inputTransactions);
+        assertOriginEntries(4, outputTransactions);
+    }
+    
    
     
    /* @RelatesTo(RelatesTo.JiraIssue.KULLAB96)
@@ -1132,16 +1142,7 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
     
     */
     
-    @RelatesTo(RelatesTo.JiraIssue.KULLAB96)
-    public void testInvalidDebitCreditCode() throws Exception {
-        String[] inputTransactions = { "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003493.50X2006-12-22                                                 2006-12-222006-12-31000168.002007060000149952 001REGS12PAE 11 M001010207                     IU IUBLA"
-                                     , "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003495.50X2006-12-25                                                 2006-12-222006-12-31000168.002007060000149952 001REGS12PAE 11 M001010207                     IU IUBLA" };
-
-        EntryHolder[] outputTransactions = { new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[0]), new EntryHolder(OriginEntrySource.BACKUP, inputTransactions[1]), new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[0]), new EntryHolder(OriginEntrySource.SCRUBBER_ERROR, inputTransactions[1]) };
-
-        scrub(inputTransactions);
-        assertOriginEntries(4, outputTransactions);
-    }
+   
     
     /*
     @RelatesTo(RelatesTo.JiraIssue.KULLAB96)
@@ -1351,7 +1352,7 @@ public class LaborScrubberServiceTest extends LaborOriginEntryTestBase {
 
     private void scrub(String[] inputTransactions) {
         clearOriginEntryTables();
-        loadInputTransactions(OriginEntrySource.BACKUP, inputTransactions, date);
+        loadInputTransactions(OriginEntrySource.LABOR_BACKUP, inputTransactions, date);
         persistenceService.clearCache();
         laborScrubberService.scrubEntries();
     }

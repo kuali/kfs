877f00749617754f71fc3bf248fe84ef51ed8e1e
Gary	Palmer <gwpalmer@iu.edu>
2007-04-19 19:39:45 +0000

    to support duration code selection control

---
+++ work/src/org/kuali/kfs/module/bc/businessobject/options/BudgetConstructionDurationValuesFinder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.lookup.keyvalues;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.kuali.core.lookup.keyvalues.KeyValuesBase;
+import org.kuali.core.service.KeyValuesService;
+import org.kuali.core.web.ui.KeyLabelPair;
+import org.kuali.kfs.util.SpringServiceLocator;
+import org.kuali.module.budget.bo.BudgetConstructionDuration;
+
+/**
+ * This class...
+ */
+public class BudgetConstructionDurationValuesFinder extends KeyValuesBase {
+
+    /**
+     * @see org.kuali.core.lookup.keyvalues.KeyValuesFinder#getKeyValues()
+     */
+    public List getKeyValues() {
+        KeyValuesService boService = SpringServiceLocator.getKeyValuesService();
+        Collection budgetConstructionDurationCodes = boService.findAll(BudgetConstructionDuration.class);
+        List durationKeyLabels = new ArrayList();
+        durationKeyLabels.add(new KeyLabelPair("", ""));
+        for (Iterator iter = budgetConstructionDurationCodes.iterator(); iter.hasNext();) {
+            BudgetConstructionDuration element = (BudgetConstructionDuration) iter.next();
+            durationKeyLabels.add(new KeyLabelPair(element.getAppointmentDurationCode(), element.getAppointmentDurationCode()));
+        }
+
+        return durationKeyLabels;
+    }
+
+}

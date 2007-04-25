5ddcb84e02107fa95c7794761d6bbba5921289a5
Gary	Palmer <gwpalmer@iu.edu>
2007-04-25 19:28:59 +0000

    initial work to implement BC doc selection page

---
+++ work/src/org/kuali/kfs/module/bc/document/web/struts/BudgetConstructionSelectionAction.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.web.struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.Constants;
+import org.kuali.core.authorization.AuthorizationType;
+import org.kuali.core.exceptions.AuthorizationException;
+import org.kuali.core.exceptions.ModuleAuthorizationException;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.web.struts.action.KualiAction;
+import org.kuali.module.budget.document.authorization.BudgetConstructionDocumentAuthorizer;
+import org.kuali.module.budget.web.struts.form.BudgetConstructionSelectionForm;
+import org.kuali.rice.KNSServiceLocator;
+
+/**
+ * This class...
+ */
+public class BudgetConstructionSelectionAction extends KualiAction {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(BudgetConstructionSelectionAction.class);
+
+    /**
+     * @see org.kuali.core.web.struts.action.KualiAction#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ActionForward forward = super.execute(mapping, form, request, response);
+
+        BudgetConstructionSelectionForm budgetConstructionSelectionForm = (BudgetConstructionSelectionForm) form;  
+
+        //TODO should not need to handle optimistic lock exception here (like KualiDocumentActionBase)
+        //since BC sets locks up front, but need to verify this
+
+
+//TODO will eventually need to setup some sort of authorization for typical user versus BC root approver
+//root approvers have more controls present on the page
+        //TODO should probably use service locator and call
+        //DocumentAuthorizer documentAuthorizer = KNSServiceLocator.getDocumentAuthorizationService().getDocumentAuthorizer("<BCDoctype>");
+//        BudgetConstructionDocumentAuthorizer budgetConstructionDocumentAuthorizer = new BudgetConstructionDocumentAuthorizer();
+//        budgetConstructionSelectionForm.populateAuthorizationFields(budgetConstructionDocumentAuthorizer);
+
+        return forward;
+    }
+
+    /**
+     * @see org.kuali.core.web.struts.action.KualiAction#checkAuthorization(org.apache.struts.action.ActionForm, java.lang.String)
+     */
+    @Override
+    protected void checkAuthorization(ActionForm form, String methodToCall) throws AuthorizationException {
+ 
+        AuthorizationType bcAuthorizationType = new AuthorizationType.Default(this.getClass());
+        if ( !KNSServiceLocator.getKualiModuleService().isAuthorized( GlobalVariables.getUserSession().getUniversalUser(), bcAuthorizationType ) ){
+            LOG.error("User not authorized to use this action: " + this.getClass().getName() );
+            throw new ModuleAuthorizationException( GlobalVariables.getUserSession().getUniversalUser().getPersonUserIdentifier(), bcAuthorizationType, getKualiModuleService().getResponsibleModule(this.getClass()) );
+        }
+    }
+
+    public ActionForward loadExpansionScreen(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        BudgetConstructionSelectionForm budgetConstructionSelectionForm = (BudgetConstructionSelectionForm) form;
+        budgetConstructionSelectionForm.setUniversityFiscalYear(new Integer(2008));
+
+        return mapping.findForward(Constants.MAPPING_BASIC);
+    }
+
+    public ActionForward returnToCaller(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        BudgetConstructionSelectionForm budgetConstructionSelectionForm = (BudgetConstructionSelectionForm) form;  
+
+        return mapping.findForward(Constants.MAPPING_PORTAL);
+    }
+
+}
---
+++ work/src/org/kuali/kfs/module/bc/document/web/struts/BudgetConstructionSelectionForm.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.web.struts.form;
+
+import org.kuali.core.web.struts.form.KualiForm;
+import org.kuali.module.budget.bo.BudgetConstructionHeader;
+
+/**
+ * This class...
+ */
+public class BudgetConstructionSelectionForm extends KualiForm {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(BudgetConstructionSelectionForm.class);
+
+    private BudgetConstructionHeader budgetConstructionHeader;
+
+    //holds the BC fiscal year that is currently active 
+    private Integer universityFiscalYear;
+
+    public BudgetConstructionSelectionForm(){
+        super();
+        setBudgetConstructionHeader(new BudgetConstructionHeader());
+    }
+
+    /**
+     * Gets the budgetConstructionHeader attribute. 
+     * @return Returns the budgetConstructionHeader.
+     */
+    public BudgetConstructionHeader getBudgetConstructionHeader() {
+        return budgetConstructionHeader;
+    }
+
+    /**
+     * Sets the budgetConstructionHeader attribute value.
+     * @param budgetConstructionHeader The budgetConstructionHeader to set.
+     */
+    public void setBudgetConstructionHeader(BudgetConstructionHeader budgetConstructionHeader) {
+        this.budgetConstructionHeader = budgetConstructionHeader;
+    }
+
+    /**
+     * Gets the universityFiscalYear attribute. 
+     * @return Returns the universityFiscalYear.
+     */
+    public Integer getUniversityFiscalYear() {
+        return universityFiscalYear;
+    }
+
+    /**
+     * Sets the universityFiscalYear attribute value.
+     * @param universityFiscalYear The universityFiscalYear to set.
+     */
+    public void setUniversityFiscalYear(Integer universityFiscalYear) {
+        this.universityFiscalYear = universityFiscalYear;
+    }
+
+}

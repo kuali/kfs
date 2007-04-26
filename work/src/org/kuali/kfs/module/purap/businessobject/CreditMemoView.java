a7a17794edc7c1a936acca0237ba7a1322b46f8c
Teresa May <temay@indiana.edu>
2007-04-26 18:03:41 +0000

    KULDBA-752, -753:  added columns to AP_CRDT_MEMO_V and AP_PMT_RQST_V

--- work/src/org/kuali/kfs/module/purap/businessobject/CreditMemoView.java
+++ work/src/org/kuali/kfs/module/purap/businessobject/CreditMemoView.java
@@ -1,5 +1,7 @@
 package org.kuali.module.purap.bo;
 
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.util.List;
 
 import org.kuali.core.bo.Note;
@@ -9,6 +11,17 @@ import org.kuali.core.bo.Note;
  * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
  */
 public class CreditMemoView extends AbstractRelatedView {
+
+    private String creditMemoNumber;    
+    private Integer paymentRequestIdentifier;
+    private Integer purchaseOrderIdentifier;
+    private String creditMemoStatusCode;    
+    private boolean creditHoldIndicator;
+    private String vendorCustomerNumber;    
+    private Date accountsPayableApprovalDate;
+    private Date creditMemoExtractedDate;    
+    private BigDecimal totalAmount;    
+   
     public Integer getPurapDocumentIdentifier() {
         return super.getPurapDocumentIdentifier();
     }
@@ -19,5 +32,149 @@ public class CreditMemoView extends AbstractRelatedView {
     
     public String getUrl() {
         return super.getUrl();
+    }
+
+    /**
+     * Gets the accountsPayableApprovalDate attribute. 
+     * @return Returns the accountsPayableApprovalDate.
+     */
+    public Date getAccountsPayableApprovalDate() {
+        return accountsPayableApprovalDate;
+    }
+
+    /**
+     * Sets the accountsPayableApprovalDate attribute value.
+     * @param accountsPayableApprovalDate The accountsPayableApprovalDate to set.
+     */
+    public void setAccountsPayableApprovalDate(Date accountsPayableApprovalDate) {
+        this.accountsPayableApprovalDate = accountsPayableApprovalDate;
+    }
+
+    /**
+     * Gets the creditHoldIndicator attribute. 
+     * @return Returns the creditHoldIndicator.
+     */
+    public boolean isCreditHoldIndicator() {
+        return creditHoldIndicator;
+    }
+
+    /**
+     * Sets the creditHoldIndicator attribute value.
+     * @param creditHoldIndicator The creditHoldIndicator to set.
+     */
+    public void setCreditHoldIndicator(boolean creditHoldIndicator) {
+        this.creditHoldIndicator = creditHoldIndicator;
+    }
+
+    /**
+     * Gets the creditMemoExtractedDate attribute. 
+     * @return Returns the creditMemoExtractedDate.
+     */
+    public Date getCreditMemoExtractedDate() {
+        return creditMemoExtractedDate;
+    }
+
+    /**
+     * Sets the creditMemoExtractedDate attribute value.
+     * @param creditMemoExtractedDate The creditMemoExtractedDate to set.
+     */
+    public void setCreditMemoExtractedDate(Date creditMemoExtractedDate) {
+        this.creditMemoExtractedDate = creditMemoExtractedDate;
+    }
+
+    /**
+     * Gets the creditMemoNumber attribute. 
+     * @return Returns the creditMemoNumber.
+     */
+    public String getCreditMemoNumber() {
+        return creditMemoNumber;
+    }
+
+    /**
+     * Sets the creditMemoNumber attribute value.
+     * @param creditMemoNumber The creditMemoNumber to set.
+     */
+    public void setCreditMemoNumber(String creditMemoNumber) {
+        this.creditMemoNumber = creditMemoNumber;
+    }
+
+    /**
+     * Gets the creditMemoStatusCode attribute. 
+     * @return Returns the creditMemoStatusCode.
+     */
+    public String getCreditMemoStatusCode() {
+        return creditMemoStatusCode;
+    }
+
+    /**
+     * Sets the creditMemoStatusCode attribute value.
+     * @param creditMemoStatusCode The creditMemoStatusCode to set.
+     */
+    public void setCreditMemoStatusCode(String creditMemoStatusCode) {
+        this.creditMemoStatusCode = creditMemoStatusCode;
+    }
+
+    /**
+     * Gets the paymentRequestIdentifier attribute. 
+     * @return Returns the paymentRequestIdentifier.
+     */
+    public Integer getPaymentRequestIdentifier() {
+        return paymentRequestIdentifier;
+    }
+
+    /**
+     * Sets the paymentRequestIdentifier attribute value.
+     * @param paymentRequestIdentifier The paymentRequestIdentifier to set.
+     */
+    public void setPaymentRequestIdentifier(Integer paymentRequestIdentifier) {
+        this.paymentRequestIdentifier = paymentRequestIdentifier;
+    }
+
+    /**
+     * Gets the purchaseOrderIdentifier attribute. 
+     * @return Returns the purchaseOrderIdentifier.
+     */
+    public Integer getPurchaseOrderIdentifier() {
+        return purchaseOrderIdentifier;
+    }
+
+    /**
+     * Sets the purchaseOrderIdentifier attribute value.
+     * @param purchaseOrderIdentifier The purchaseOrderIdentifier to set.
+     */
+    public void setPurchaseOrderIdentifier(Integer purchaseOrderIdentifier) {
+        this.purchaseOrderIdentifier = purchaseOrderIdentifier;
+    }
+
+    /**
+     * Gets the totalAmount attribute. 
+     * @return Returns the totalAmount.
+     */
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    /**
+     * Sets the totalAmount attribute value.
+     * @param totalAmount The totalAmount to set.
+     */
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    /**
+     * Gets the vendorCustomerNumber attribute. 
+     * @return Returns the vendorCustomerNumber.
+     */
+    public String getVendorCustomerNumber() {
+        return vendorCustomerNumber;
+    }
+
+    /**
+     * Sets the vendorCustomerNumber attribute value.
+     * @param vendorCustomerNumber The vendorCustomerNumber to set.
+     */
+    public void setVendorCustomerNumber(String vendorCustomerNumber) {
+        this.vendorCustomerNumber = vendorCustomerNumber;
     }
 }
--- work/src/org/kuali/kfs/module/purap/businessobject/PaymentRequestView.java
+++ work/src/org/kuali/kfs/module/purap/businessobject/PaymentRequestView.java
@@ -1,16 +1,28 @@
 package org.kuali.module.purap.bo;
 
-import java.util.LinkedHashMap;
+import java.sql.Date;
 import java.util.List;
 
 import org.kuali.core.bo.Note;
-import org.kuali.core.bo.PersistableBusinessObjectBase;
-import org.kuali.module.purap.document.PaymentRequestDocument;
+import org.kuali.core.util.KualiDecimal;
 
 /**
  * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
  */
 public class PaymentRequestView extends AbstractRelatedView {
+
+    private String invoiceNumber;
+    private Integer purchaseOrderIdentifier;    
+    private String statusCode;
+    private boolean paymentHoldIndicator;    
+    private boolean paymentRequestedCancelIndicator;
+    private String vendorName;
+    private String vendorCustomerNumber; 
+    private Date paymentRequestPayDate;    
+    private Date paymentExtractedDate;
+    private Date paymentPaidDate;
+    private KualiDecimal totalAmount;    
+
     public List<Note> getNotes() {
         return super.getNotes();
     }
@@ -21,5 +33,181 @@ public class PaymentRequestView extends AbstractRelatedView {
     
     public String getUrl() {
         return super.getUrl();
+    }
+
+    /**
+     * Gets the invoiceNumber attribute. 
+     * @return Returns the invoiceNumber.
+     */
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    /**
+     * Sets the invoiceNumber attribute value.
+     * @param invoiceNumber The invoiceNumber to set.
+     */
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    /**
+     * Gets the paymentExtractedDate attribute. 
+     * @return Returns the paymentExtractedDate.
+     */
+    public Date getPaymentExtractedDate() {
+        return paymentExtractedDate;
+    }
+
+    /**
+     * Sets the paymentExtractedDate attribute value.
+     * @param paymentExtractedDate The paymentExtractedDate to set.
+     */
+    public void setPaymentExtractedDate(Date paymentExtractedDate) {
+        this.paymentExtractedDate = paymentExtractedDate;
+    }
+
+    /**
+     * Gets the paymentHoldIndicator attribute. 
+     * @return Returns the paymentHoldIndicator.
+     */
+    public boolean isPaymentHoldIndicator() {
+        return paymentHoldIndicator;
+    }
+
+    /**
+     * Sets the paymentHoldIndicator attribute value.
+     * @param paymentHoldIndicator The paymentHoldIndicator to set.
+     */
+    public void setPaymentHoldIndicator(boolean paymentHoldIndicator) {
+        this.paymentHoldIndicator = paymentHoldIndicator;
+    }
+
+    /**
+     * Gets the paymentPaidDate attribute. 
+     * @return Returns the paymentPaidDate.
+     */
+    public Date getPaymentPaidDate() {
+        return paymentPaidDate;
+    }
+
+    /**
+     * Sets the paymentPaidDate attribute value.
+     * @param paymentPaidDate The paymentPaidDate to set.
+     */
+    public void setPaymentPaidDate(Date paymentPaidDate) {
+        this.paymentPaidDate = paymentPaidDate;
+    }
+
+    /**
+     * Gets the paymentRequestedCancelIndicator attribute. 
+     * @return Returns the paymentRequestedCancelIndicator.
+     */
+    public boolean isPaymentRequestedCancelIndicator() {
+        return paymentRequestedCancelIndicator;
+    }
+
+    /**
+     * Sets the paymentRequestedCancelIndicator attribute value.
+     * @param paymentRequestedCancelIndicator The paymentRequestedCancelIndicator to set.
+     */
+    public void setPaymentRequestedCancelIndicator(boolean paymentRequestedCancelIndicator) {
+        this.paymentRequestedCancelIndicator = paymentRequestedCancelIndicator;
+    }
+
+    /**
+     * Gets the paymentRequestPayDate attribute. 
+     * @return Returns the paymentRequestPayDate.
+     */
+    public Date getPaymentRequestPayDate() {
+        return paymentRequestPayDate;
+    }
+
+    /**
+     * Sets the paymentRequestPayDate attribute value.
+     * @param paymentRequestPayDate The paymentRequestPayDate to set.
+     */
+    public void setPaymentRequestPayDate(Date paymentRequestPayDate) {
+        this.paymentRequestPayDate = paymentRequestPayDate;
+    }
+
+    /**
+     * Gets the purchaseOrderIdentifier attribute. 
+     * @return Returns the purchaseOrderIdentifier.
+     */
+    public Integer getPurchaseOrderIdentifier() {
+        return purchaseOrderIdentifier;
+    }
+
+    /**
+     * Sets the purchaseOrderIdentifier attribute value.
+     * @param purchaseOrderIdentifier The purchaseOrderIdentifier to set.
+     */
+    public void setPurchaseOrderIdentifier(Integer purchaseOrderIdentifier) {
+        this.purchaseOrderIdentifier = purchaseOrderIdentifier;
+    }
+
+    /**
+     * Gets the statusCode attribute. 
+     * @return Returns the statusCode.
+     */
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    /**
+     * Sets the statusCode attribute value.
+     * @param statusCode The statusCode to set.
+     */
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * Gets the totalAmount attribute. 
+     * @return Returns the totalAmount.
+     */
+    public KualiDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    /**
+     * Sets the totalAmount attribute value.
+     * @param totalAmount The totalAmount to set.
+     */
+    public void setTotalAmount(KualiDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    /**
+     * Gets the vendorCustomerNumber attribute. 
+     * @return Returns the vendorCustomerNumber.
+     */
+    public String getVendorCustomerNumber() {
+        return vendorCustomerNumber;
+    }
+
+    /**
+     * Sets the vendorCustomerNumber attribute value.
+     * @param vendorCustomerNumber The vendorCustomerNumber to set.
+     */
+    public void setVendorCustomerNumber(String vendorCustomerNumber) {
+        this.vendorCustomerNumber = vendorCustomerNumber;
+    }
+
+    /**
+     * Gets the vendorName attribute. 
+     * @return Returns the vendorName.
+     */
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    /**
+     * Sets the vendorName attribute value.
+     * @param vendorName The vendorName to set.
+     */
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
     }
 }

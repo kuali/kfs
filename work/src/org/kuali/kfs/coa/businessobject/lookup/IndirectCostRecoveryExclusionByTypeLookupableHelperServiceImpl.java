dc0d2c7422993792186c0c690a5d102252adc09c
Dennis Friends <dwf5@cornell.edu>
2007-04-27 15:09:55 +0000

    KULRNE-4672 : New custom lookupable helper service.

---
+++ work/src/org/kuali/kfs/coa/businessobject/lookup/IndirectCostRecoveryExclusionByTypeLookupableHelperServiceImpl.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.chart.lookup;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.Constants;
+import org.kuali.core.bo.BusinessObject;
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.lookup.KualiLookupableHelperServiceImpl;
+import org.kuali.core.lookup.LookupUtils;
+import org.kuali.core.util.BeanPropertyComparator;
+
+/**
+ * This class...
+ */
+public class IndirectCostRecoveryExclusionByTypeLookupableHelperServiceImpl extends KualiLookupableHelperServiceImpl {
+
+    /**
+     * @returns Empty string because we don't want any action links to be displayed on the lookups results page.
+     *
+     * @see org.kuali.core.lookup.LookupableHelperService#getActionUrls(org.kuali.core.bo.BusinessObject)
+     */
+    public String getActionUrls(BusinessObject businessObject) {
+        return "";
+    }
+
+}

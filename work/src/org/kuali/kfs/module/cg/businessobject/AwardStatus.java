eb3fa348e8c3d2fe2ff3b54404811d08f8b5ba60
Dennis Friends <dwf5@cornell.edu>
2007-04-30 15:12:40 +0000

    KULCG-281 : Added a simple getter and setter to complete the necessary components for a new attribute reference I added to the associated DD file.

--- work/src/org/kuali/kfs/module/cg/businessobject/AwardStatus.java
+++ work/src/org/kuali/kfs/module/cg/businessobject/AwardStatus.java
@@ -45,6 +45,7 @@ public class AwardStatus extends PersistableBusinessObjectBase {
         return awardStatusCode;
     }
 
+    
     /**
      * Sets the awardStatusCode attribute.
      * 
@@ -55,6 +56,28 @@ public class AwardStatus extends PersistableBusinessObjectBase {
     }
 
 
+    /**
+     * 
+     * This method is a dummy getter that simply returns the same value as getAwardStatusCode().
+     * I needed this method so I could add an attribute reference to the DD with a different
+     * name than awardStatusCode.  This allowed me to override the input behavior and define 
+     * two different methods of input based on what type of page we're displaying.
+     * 
+     * @return Returns the awardStatusCode
+     */
+    public String getAwardStatusCodeDropDown() {
+        return awardStatusCode;
+    }
+
+    /**
+     * @param awardStatusCode
+     * @deprecated Do not use this method, it is only here for DD mapping purposes and has 
+     * no defined functionality.  See KULCG-281 for further details.
+     */
+    public void setAwardStatusCodeDropDown(String awardStatusCode) {
+        this.awardStatusCode = awardStatusCode;
+    }
+    
     /**
      * Gets the awardStatusDescription attribute.
      * 

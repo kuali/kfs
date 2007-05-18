39066c38c65f0889fe27967ac0817dfa10f951b5
Leo Przybylski <leo@rsmart.com>
2007-05-18 03:46:11 +0000

    KULLAB-134 KULLAB-135 KULLAB-70 Adding Missing LaborExpenseTransferDocumentBase

---
+++ work/src/org/kuali/kfs/module/ld/document/LaborExpenseTransferDocumentBase.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.document;
+
+import org.kuali.core.document.Copyable;
+import org.kuali.core.document.Correctable;
+import org.kuali.kfs.KFSConstants;
+import org.kuali.kfs.bo.AccountingLineParser;
+import org.kuali.module.labor.bo.ExpenseTransferSourceAccountingLine;
+import org.kuali.module.labor.bo.ExpenseTransferTargetAccountingLine;
+import org.kuali.module.labor.bo.LaborLedgerAccountingLineParser;
+
+/**
+ * Base class for Expense Transfer Documents
+ */
+public abstract class LaborExpenseTransferDocumentBase extends LaborLedgerPostingDocumentBase implements Copyable, Correctable{
+
+    private String emplid;
+    
+    public String getEmplid() {
+        return emplid;
+    }
+
+    public void setEmplid(String emplid) {
+        this.emplid = emplid;
+    }
+
+    /**
+     * Overrides the base implementation to return "From".
+     * 
+     * @see org.kuali.core.document.AccountingDocument#getSourceAccountingLinesSectionTitle()
+     */
+    public String getSourceAccountingLinesSectionTitle() {
+        return KFSConstants.FROM;
+    }
+
+    /**
+     * Overrides the base implementation to return "To".
+     * 
+     * @see org.kuali.core.document.AccountingDocument#getTargetAccountingLinesSectionTitle()
+     */
+    public String getTargetAccountingLinesSectionTitle() {
+        return KFSConstants.TO;
+    }
+    
+    /**
+     * @see org.kuali.core.document.FinancialDocument#getAccountingLineParser()
+     */
+    @Override
+    public AccountingLineParser getAccountingLineParser() {
+        return new LaborLedgerAccountingLineParser();        
+    }
+    
+    /**
+     * @see org.kuali.kfs.document.AccountingDocumentBase#getSourceAccountingLineClass()
+     */
+    @Override
+    public Class getSourceAccountingLineClass() {
+        return ExpenseTransferSourceAccountingLine.class;
+    }
+
+    /**
+     * @see org.kuali.kfs.document.AccountingDocumentBase#getTargetAccountingLineClass()
+     */
+    @Override
+    public Class getTargetAccountingLineClass() {
+        return ExpenseTransferTargetAccountingLine.class;
+    }
+}
+

e28e1ccb1069b19a49b0806e2fa70b1610bb609c
Gary	Palmer <gwpalmer@iu.edu>
2007-05-25 19:50:31 +0000

    implement BudgetDocumentService

---
+++ work/src/org/kuali/kfs/module/bc/document/service/BudgetDocumentService.java
@@ -0,0 +1,28 @@
+package org.kuali.module.budget.service;
+
+import org.kuali.module.budget.bo.BudgetConstructionHeader;
+import org.kuali.module.budget.dao.BudgetConstructionDao;
+
+
+/**
+ * This defines the methods a BudgetDocumentService must implement 
+ */
+public interface BudgetDocumentService {
+
+    /**
+     * This method...
+     * @param chartOfAccountsCode
+     * @param accountNumber
+     * @param subAccountNumber
+     * @param fiscalYear
+     * @return
+     */
+    public BudgetConstructionHeader getByCandidateKey(String chartOfAccountsCode, String accountNumber, String subAccountNumber, Integer fiscalYear);
+
+    /**
+     * Sets the budgetConstructionDao attribute value.
+     * @param budgetConstructionDao The budgetConstructionDao to set.
+     */
+    public void setBudgetConstructionDao(BudgetConstructionDao budgetConstructionDao);
+
+}
---
+++ work/src/org/kuali/kfs/module/bc/document/service/impl/BudgetDocumentServiceImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.service.impl;
+
+import org.kuali.module.budget.bo.BudgetConstructionHeader;
+import org.kuali.module.budget.dao.BudgetConstructionDao;
+import org.kuali.module.budget.service.BudgetDocumentService;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * This class implements the BudgetDocumentService interface
+ * 
+ * Methods here operate on objects associated with the Budget Construction document such as
+ * BudgetConstructionHeader
+ */
+@Transactional
+public class BudgetDocumentServiceImpl implements BudgetDocumentService {
+    
+    private BudgetConstructionDao budgetConstructionDao;
+
+    /**
+     * @see org.kuali.module.budget.service.BudgetDocumentService#getByCandidateKey(java.lang.String, java.lang.String, java.lang.String, java.lang.Integer)
+     */
+    public BudgetConstructionHeader getByCandidateKey(String chartOfAccountsCode, String accountNumber, String subAccountNumber, Integer fiscalYear){
+        return budgetConstructionDao.getByCandidateKey(chartOfAccountsCode, accountNumber, subAccountNumber, fiscalYear);
+    }
+
+    /**
+     * Sets the budgetConstructionDao attribute value.
+     * @param budgetConstructionDao The budgetConstructionDao to set.
+     */
+    public void setBudgetConstructionDao(BudgetConstructionDao budgetConstructionDao) {
+        this.budgetConstructionDao = budgetConstructionDao;
+    }
+
+}

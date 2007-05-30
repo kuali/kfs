84c73523595080f86b61ec2c39b2757a3a783d9c
David Elyea <delyea@iu.edu>
2007-05-30 18:00:11 +0000

    purap workflow updates

---
+++ work/src/org/kuali/kfs/module/purap/document/routing/attribute/KualiInternalPurchasingRoleAttribute.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.workflow.module.purap.attribute;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.util.KualiDecimal;
+import org.kuali.core.util.ObjectUtils;
+import org.kuali.kfs.KFSPropertyConstants;
+import org.kuali.kfs.util.SpringServiceLocator;
+import org.kuali.module.purap.PurapConstants;
+import org.kuali.module.purap.PurapParameterConstants;
+import org.kuali.module.purap.document.PurchaseOrderDocument;
+import org.kuali.workflow.KualiWorkflowUtils;
+
+import edu.iu.uis.eden.Id;
+import edu.iu.uis.eden.engine.RouteContext;
+import edu.iu.uis.eden.exception.EdenUserNotFoundException;
+import edu.iu.uis.eden.routetemplate.ResolvedQualifiedRole;
+import edu.iu.uis.eden.routetemplate.Role;
+import edu.iu.uis.eden.routetemplate.UnqualifiedRoleAttribute;
+import edu.iu.uis.eden.routetemplate.xmlrouting.XPathHelper;
+import edu.iu.uis.eden.workgroup.GroupNameId;
+
+/**
+ * A document should stop in the Internal Purchasing Review route node if the following conditions are met:<br>
+ * <ol>
+ *   <li>The document is not an 'Automatic Purchase Order'
+ *   <li>The 'routing user' was not a contract manager
+ *   <li>The 'po internal amount limit' is not null and the document total amount is greater than the 'po internal amount limit'<br>
+ *       (see {@link org.kuali.module.purap.service.PurchaseOrderService#getInternalPurchasingDollarLimit(org.kuali.module.purap.document.PurchaseOrderDocument, String, String)}
+ *       for more details on the 'po internal amount limit')
+ * </ol>
+ */
+public class KualiInternalPurchasingRoleAttribute extends UnqualifiedRoleAttribute {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(KualiInternalPurchasingRoleAttribute.class);
+
+    private static final String INTERNAL_PURCHASING_ROLE_KEY = "INTERNAL_PURCHASING_REVIEWER";
+    private static final String INTERNAL_PURCHASING_ROLE_LABEL = "Internal Purchasing Reviewer";
+    private static final Role ROLE = new Role(KualiInternalPurchasingRoleAttribute.class, INTERNAL_PURCHASING_ROLE_KEY, INTERNAL_PURCHASING_ROLE_LABEL);
+    
+    private static final List ROLES;
+    static {
+        ArrayList list = new ArrayList(0);
+        list.add(ROLE);
+        ROLES = Collections.unmodifiableList(list);
+    }
+
+    /**
+     * @see edu.iu.uis.eden.routetemplate.UnqualifiedRoleAttribute#getRoleNames()
+     */
+    @Override
+    public List<Role> getRoleNames() {
+        return ROLES;
+    }
+
+    /**
+     * TODO delyea - documentation
+     * 
+     * @param routeContext the RouteContext
+     * @param roleName the role name
+     * @return a ResolvedQualifiedRole
+     */
+    @Override
+    public ResolvedQualifiedRole resolveRole(RouteContext routeContext, String roleName) throws EdenUserNotFoundException {
+        // TODO delyea - TESTING - delete the return below to fire the rule
+        return new ResolvedQualifiedRole(INTERNAL_PURCHASING_ROLE_KEY, new ArrayList());
+
+        //Select the class attribute of the document element to determine the doc type
+//        XPath xpath = XPathHelper.newXPath();
+//        String docType = "";
+//        String xPathExpression = "";
+//        try {
+//            xPathExpression = KualiWorkflowUtils.xstreamSafeXPath(KualiWorkflowUtils.XSTREAM_MATCH_ANYWHERE_PREFIX + "document/attribute::class");
+//            docType = (String) xpath.evaluate(xPathExpression, routeContext.getDocumentContent().getDocContent(), XPathConstants.STRING);
+//            xPathExpression = KualiWorkflowUtils.xstreamSafeXPath(KualiWorkflowUtils.XSTREAM_MATCH_ANYWHERE_PREFIX + "documentHeader/documentNumber");
+//            String documentNumber = (String) xpath.evaluate(xPathExpression, routeContext.getDocumentContent().getDocContent(), XPathConstants.STRING);
+//            Map queryMap = new HashMap();
+//            queryMap.put(KFSPropertyConstants.DOCUMENT_NUMBER, documentNumber);
+//            PurchaseOrderDocument poDocument = (PurchaseOrderDocument)SpringServiceLocator.getBusinessObjectService().findByPrimaryKey(Class.forName(docType), queryMap);
+//            KualiDecimal internalPurchasingLimit = SpringServiceLocator.getPurchaseOrderService().getInternalPurchasingDollarLimit(poDocument, poDocument.getChartOfAccountsCode(), poDocument.getOrganizationCode());
+//
+//            if ( (ObjectUtils.isNull(internalPurchasingLimit)) || (internalPurchasingLimit.compareTo(KualiWorkflowUtils.getFinancialDocumentTotalAmount(routeContext)) < 0) ) {
+//                KualiConfigurationService configService = SpringServiceLocator.getKualiConfigurationService();
+//                String workgroupName = configService.getApplicationParameterValue(PurapParameterConstants.PURAP_ADMIN_GROUP, PurapConstants.WorkflowConstants.PurchaseOrderDocument.INTERNAL_PURCHASING_WORKGROUP_NAME);
+//                return new ResolvedQualifiedRole(INTERNAL_PURCHASING_ROLE_LABEL, Arrays.asList(new Id[] { new GroupNameId(workgroupName) }));
+//            }
+//            return null;
+//        }
+//        catch (XPathExpressionException xe) {
+//            String errorMsg = "Error executing XPath expression - '" + xPathExpression + "'";
+//            LOG.error(errorMsg);
+//            throw new RuntimeException (errorMsg,xe);
+//        }
+//        catch (ClassNotFoundException cnfe) {
+//            String errorMsg = "Error trying to find document class '" + docType + "'";
+//            LOG.error(errorMsg);
+//            throw new RuntimeException (errorMsg,cnfe);
+//        }
+    }
+}
---
+++ work/src/org/kuali/kfs/module/purap/document/routing/attribute/KualiSeparationOfDutiesRoleAttribute.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.workflow.module.purap.attribute;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.util.KualiDecimal;
+import org.kuali.kfs.util.SpringServiceLocator;
+import org.kuali.module.purap.PurapConstants;
+import org.kuali.module.purap.PurapParameterConstants;
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.workflow.KualiWorkflowUtils;
+
+import edu.iu.uis.eden.EdenConstants;
+import edu.iu.uis.eden.Id;
+import edu.iu.uis.eden.actiontaken.ActionTakenValue;
+import edu.iu.uis.eden.engine.RouteContext;
+import edu.iu.uis.eden.exception.EdenUserNotFoundException;
+import edu.iu.uis.eden.routeheader.DocumentRouteHeaderValue;
+import edu.iu.uis.eden.routetemplate.ResolvedQualifiedRole;
+import edu.iu.uis.eden.routetemplate.Role;
+import edu.iu.uis.eden.routetemplate.UnqualifiedRoleAttribute;
+import edu.iu.uis.eden.workgroup.GroupNameId;
+
+/**
+ * A document should stop in the Separation of Duties Route Node if the following two conditions are met:
+ * <ol>
+ * <li>The total amount of the document is greater or equal to the Separation of Duties application parameter amount
+ * <li>The document was not routed for Approval or Completion to more than the user who routed the document
+ * </ol>
+ * In Workflow the rule above means any of the following scenarios do not require Separation of Duties routing (assuming that the
+ * routing of the document still applies as an Action Taken of COMPLETE):<br><br>
+ * <ol>
+ * <li>The total number of unique people who have had an APPROVE or COMPLETE action on the document is more than 1 (keep in mind
+ * that the 'Route' action on a document generates a COMPLETE action taken)
+ * <li>The total amount of the document is less than or equal to the Separation of Duties application parameter amount
+ * </ol>
+ * EPIC RULE (these cases will route to separation of duties group):<br>
+ *  - (totalCost > appSettingCost) && (0 approvals taken on the document)<br>
+ *  - (totalCost > appSettingCost) && (one approval taken on the document) && (approval is by initiator user)
+ */
+public class KualiSeparationOfDutiesRoleAttribute extends UnqualifiedRoleAttribute {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(KualiSeparationOfDutiesRoleAttribute.class);    
+    
+    private static final String SEPARATION_OF_DUTIES_ROLE_KEY = "SEPARATION_OF_DUTIES";
+    private static final String SEPARATION_OF_DUTIES_ROLE_LABEL = "Separation of Duties Reviewer";
+
+    private static final Role ROLE = new Role(KualiSeparationOfDutiesRoleAttribute.class, SEPARATION_OF_DUTIES_ROLE_KEY, SEPARATION_OF_DUTIES_ROLE_LABEL);
+    private static final List<Role> ROLES;
+    static {
+        ArrayList<Role> roles = new ArrayList<Role>(1);
+        roles.add(ROLE);
+        ROLES = Collections.unmodifiableList(roles);
+    }
+
+    /**
+     * @see edu.iu.uis.eden.routetemplate.UnqualifiedRoleAttribute#getRoleNames()
+     */
+    @Override
+    public List<Role> getRoleNames() {
+        return ROLES;
+    }
+
+    /**
+     * TODO delyea - documentation
+     * 
+     * @param routeContext the RouteContext
+     * @param roleName the role name
+     * @return a ResolvedQualifiedRole
+     */
+    @Override
+    public ResolvedQualifiedRole resolveRole(RouteContext routeContext, String roleName) throws EdenUserNotFoundException {
+        DocumentRouteHeaderValue document = routeContext.getDocument();
+        Set documentReviewers = new HashSet();
+        String workflowUserIdOfRouter = null;
+        // get a list of all people who have approved or completed this document in it's routing lifespan
+        // currently this will get the COMPLETE request that the router of the document
+        for (Iterator iter = document.getActionsTaken().iterator(); iter.hasNext();) {
+            ActionTakenValue actionTaken = (ActionTakenValue) iter.next();
+            if ( (actionTaken.getActionTaken().equals(EdenConstants.ACTION_TAKEN_APPROVED_CD)) || 
+                 (actionTaken.getActionTaken().equals(EdenConstants.ACTION_TAKEN_COMPLETED_CD)) ) {
+                documentReviewers.add(actionTaken.getWorkflowUser().getWorkflowUserId().getWorkflowId());
+            }
+        }
+        // if document has been approved or completed by more than one person... no need for separation of duties
+        if (documentReviewers.size() > 1) {
+            return null;
+        }
+        KualiConfigurationService configService = SpringServiceLocator.getKualiConfigurationService();
+        KualiDecimal maxAllowedAmount = new KualiDecimal(configService.getApplicationParameterValue(PurapParameterConstants.PURAP_ADMIN_GROUP,PurapConstants.WorkflowConstants.RequisitionDocument.SEPARATION_OF_DUTIES_DOLLAR_AMOUNT));
+        // if app param amount is greater than or equal to documentTotalAmount... no need for separation of duties
+        if (maxAllowedAmount.compareTo(KualiWorkflowUtils.getFinancialDocumentTotalAmount(routeContext)) >= 0) {
+            return null;
+        }
+        String workgroupName = configService.getApplicationParameterValue(PurapParameterConstants.PURAP_ADMIN_GROUP,PurapConstants.WorkflowConstants.RequisitionDocument.SEPARATION_OF_DUTIES_WORKGROUP_NAME);
+        return new ResolvedQualifiedRole(SEPARATION_OF_DUTIES_ROLE_LABEL, Arrays.asList(new Id[] {new GroupNameId(workgroupName)}));
+    }
+}

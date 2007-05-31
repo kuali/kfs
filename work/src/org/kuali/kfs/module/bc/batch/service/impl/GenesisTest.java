653a2d45ea2c118edf2bc7df1d577b35e98685e2
Fred Peterson <fpeterso@indiana.edu>
2007-05-31 22:25:30 +0000

    FBP

--- work/src/org/kuali/kfs/module/bc/batch/service/impl/GenesisTest.java
+++ work/src/org/kuali/kfs/module/bc/batch/service/impl/GenesisTest.java
@@ -105,10 +105,10 @@ public class GenesisTest {
       // update current positions
       //  genesisTestService.testPositionBuild(2007);
       LOG.warn("\nstarting fiscalYearMakers\n");
-      dateMakerTestService.fiscalYearMakers(2010,true);
+      //dateMakerTestService.fiscalYearMakers(2010,true);
       //dateMakerTestService.fiscalYearMakers(2009,false);
       //dateMakerTestService.fymkOrderProblem(2009,false);
-      //dateMakerTestService.testRoutine(); 
+      dateMakerTestService.testRoutine(); 
       LOG.warn("\nfiscalYearMakers finished\n");
       // create the proxy BC headers
       /*

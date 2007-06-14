124eea8080c56bf0f4a6d70ef5706e3c50999b06
Fred Peterson <fpeterso@indiana.edu>
2007-06-14 20:46:17 +0000

    fbp

--- work/src/org/kuali/kfs/module/bc/batch/service/impl/GenesisTest.java
+++ work/src/org/kuali/kfs/module/bc/batch/service/impl/GenesisTest.java
@@ -104,19 +104,15 @@ public class GenesisTest {
       //
       //
       // update current positions
-        genesisTestService.testPositionBuild(2011);
-//      LOG.warn("\nstarting fiscalYearMakers\n");
-//      dateMakerTestService.fiscalYearMakers(2010,true);
+//        genesisTestService.testPositionBuild(2011);
+      LOG.warn("\nstarting fiscalYearMakers\n");
+      dateMakerTestService.fiscalYearMakers(2010,true);
         //dateMakerTestService.fiscalYearMakers(2009,false);
-        //dateMakerTestService.fymkOrderProblem(2009,false);
         //dateMakerTestService.testRoutine(); 
-//      LOG.warn("\nfiscalYearMakers finished\n");
+      LOG.warn("\nfiscalYearMakers finished\n");
       // create the proxy BC headers
       /*
  //     genesisTestService.clearDBForGenesis(2009);
- //     LOG.info("\nDocument creation started: "+String.format("%tT",dateTimeService.getCurrentDate()));
- //     genesisTestService.createProxyBCHeadersTransactional(2009);
- //     LOG.info("\nProxy documents created: "+String.format("%tT",dateTimeService.getCurrentDate()));
       */
       // create the real BC documents based on the proxies
       /*

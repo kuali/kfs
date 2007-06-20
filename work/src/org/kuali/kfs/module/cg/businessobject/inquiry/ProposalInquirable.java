ea6b7fb429c6073373fd0c2683dc290244948eac
Dennis Friends <dwf5@cornell.edu>
2007-06-20 19:25:20 +0000

    KULCG-337 : Created custom inquirable class to check permissions and exclude research risk tab from inquiry when necessary.

---
+++ work/src/org/kuali/kfs/module/cg/businessobject/inquiry/ProposalInquirable.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.cg.maintenance;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.kuali.core.bo.BusinessObject;
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.datadictionary.InquirySectionDefinition;
+import org.kuali.core.inquiry.KualiInquirableImpl;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.web.ui.Section;
+import org.kuali.core.web.ui.SectionBridge;
+import org.kuali.module.cg.CGConstants;
+import org.kuali.rice.KNSServiceLocator;
+
+/**
+ * This class...
+ */
+public class ProposalInquirable extends KualiInquirableImpl {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ProposalInquirable.class);
+    
+    private transient static KualiConfigurationService configService;
+    private transient static String centralPreAwardWorkgroupName;
+    private transient static String centralPostAwardWorkgroupName;
+    private transient static String centralReviewWorkgroupName;
+
+    /**
+     * 
+     * @see org.kuali.core.inquiry.KualiInquirableImpl#getSections(org.kuali.core.bo.BusinessObject)
+     */
+    public List<Section> getSections(BusinessObject bo) {
+        
+        initStatics();
+        
+        List<Section> sections = new ArrayList<Section>();
+        if (getBusinessObjectClass() == null) {
+            LOG.error("Business object class not set in inquirable.");
+            throw new RuntimeException("Business object class not set in inquirable.");
+        }
+
+        Collection inquirySections = KNSServiceLocator.getBusinessObjectDictionaryService().getInquirySections(getBusinessObjectClass());
+        for (Iterator iter = inquirySections.iterator(); iter.hasNext();) {
+
+            UniversalUser user = GlobalVariables.getUserSession().getUniversalUser();
+            InquirySectionDefinition inquirySection = (InquirySectionDefinition) iter.next();
+            Section section = SectionBridge.toSection(inquirySection, bo);
+            if(inquirySection.getTitle().equals("Research Risks")) {
+                if ( user.isMember( centralPreAwardWorkgroupName ) || user.isMember( centralPostAwardWorkgroupName )) {
+                    sections.add(section);
+                }
+            }
+            else {
+                sections.add(section);
+            }
+        }
+
+        return sections;
+    }
+
+    private void initStatics() {
+        if ( configService == null ) {
+            configService = KNSServiceLocator.getKualiConfigurationService();
+        }
+        // get the group name that we need here
+        if ( centralPreAwardWorkgroupName == null ) {
+            centralPreAwardWorkgroupName = configService.getApplicationParameterValue(CGConstants.GROUP_CG_MAINT_EDOCS, "Kuali-Document.PreAward.Workgroup");
+        }
+        if ( centralPostAwardWorkgroupName == null ) {
+            centralPostAwardWorkgroupName = configService.getApplicationParameterValue(CGConstants.GROUP_CG_MAINT_EDOCS, "Kuali-Document.PostAward.Workgroup");
+        }
+//        if ( centralReviewWorkgroupName == null ) {
+//            centralReviewWorkgroupName = configService.getApplicationParameterValue(CGConstants.GROUP_CG_MAINT_EDOCS, "");
+//        }
+    }
+
+
+}

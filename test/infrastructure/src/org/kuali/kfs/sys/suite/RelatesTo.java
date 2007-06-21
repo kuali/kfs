787ad53a5b42536d4c8374562cb1cb1e610d5b4c
Eric Westfall <ewestfal@iu.edu>
2007-06-21 15:29:40 +0000

    Added a relatesTo for the KualiXmlRuleAttributeImplTest.  David will take a look at this when he returns from PTO.

--- test/infrastructure/src/org/kuali/kfs/sys/suite/RelatesTo.java
+++ test/infrastructure/src/org/kuali/kfs/sys/suite/RelatesTo.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006-2007 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,7 +33,7 @@ import org.kuali.core.util.AssertionUtils;
  * <p>
  * Another purpose of this annotation is for KualiTestBase to wrap any test errors or failures with a notice
  * that the annotated JIRA issues are related.
- * 
+ *
  * @see org.kuali.test.suite.InProgressSuite.Not
  * @see org.kuali.test.KualiTestBase
  */
@@ -49,14 +49,15 @@ public @interface RelatesTo {
      */
     public enum JiraIssue {
         KULRNE1612,
-        KULRNE4308, 
+        KULRNE4308,
         KULLAB54,
         KULLAB96,
         KULRNE4834,
         KULRNE34,
         KULRNE4797,
+        KULOWF255,
         ;
-        
+
         private final static Pattern PATTERN = Pattern.compile("(\\p{Alpha}+)(\\p{Digit}+)");
 
         /**

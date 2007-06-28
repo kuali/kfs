d5d6a5af3f67463d38703512fb224c68ef403a05
Ailish Bryne <abyrne@iu.edu>
2007-06-28 21:39:58 +0000

    test for data dump

--- work/src/org/kuali/kfs/sys/KFSConstants.java
+++ work/src/org/kuali/kfs/sys/KFSConstants.java
@@ -46,6 +46,7 @@ public class KFSConstants extends Constants {
     public static final String ATTACHMENTS_PENDING_DIRECTORY_KEY = "attachments.pending.directory";
     public static final String HTDOCS_LOGS_URL_KEY = "htdocs.logs.url";
     public static final String HTDOCS_STAGING_URL_KEY = "htdocs.staging.url";
+    public static final String STAGING_DIRECTORY_KEY = "staging.directory";
     public static final String EXTERNALIZABLE_HELP_URL_KEY = "externalizable.help.url";
     public static final String EXTERNALIZABLE_IMAGES_URL_KEY = "externalizable.images.url";
     public static final String REPORTS_DIRECTORY_KEY = "reports.directory";

34d006b73ddc1a541a2bc36c52d376fa773e2e7a
Bin Gao <gaob@msu.edu>
2007-07-27 14:51:57 +0000

    Backup the fix related to JIRA issue, KULLAB-341:  Labor Distribution Journal Voucher, lookup results for accounting lines don't have "return value"

--- work/src/org/kuali/kfs/module/ld/document/web/struts/JournalVoucherAction.java
+++ work/src/org/kuali/kfs/module/ld/document/web/struts/JournalVoucherAction.java
@@ -18,11 +18,14 @@ package org.kuali.module.labor.web.struts.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.kuali.RiceConstants;
 import org.kuali.kfs.KFSConstants;
 import org.kuali.module.labor.LaborConstants;
+import org.kuali.module.labor.bo.LaborLedgerPendingEntry;
 
 /**
  * This class piggy backs on all of the functionality in the KualiTransactionalDocumentActionBase but is necessary for this document
@@ -30,12 +33,22 @@ import org.kuali.module.labor.LaborConstants;
  * transaction processing eDocs (i.e. external system fields, object type override, credit and debit amounts).
  */
 public class JournalVoucherAction extends org.kuali.module.financial.web.struts.action.JournalVoucherAction {
-    
+
     /**
-     * @see org.kuali.core.web.struts.action.KualiAction#performLookup(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     * @see org.kuali.core.web.struts.action.KualiAction#performLookup(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
     @Override
     public ActionForward performLookup(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        // parse out the business object name from our methodToCall parameter
+        String fullParameter = (String) request.getAttribute(RiceConstants.METHOD_TO_CALL_ATTRIBUTE);
+        String boClassName = StringUtils.substringBetween(fullParameter, RiceConstants.METHOD_TO_CALL_BOPARM_LEFT_DEL, RiceConstants.METHOD_TO_CALL_BOPARM_RIGHT_DEL);
+
+        if (!StringUtils.equals(boClassName, LaborLedgerPendingEntry.class.getName())) {
+            return super.performLookup(mapping, form, request, response);
+        }
+
         String path = super.performLookup(mapping, form, request, response).getPath();
         path = path.replaceFirst(KFSConstants.LOOKUP_ACTION, LaborConstants.LONG_ROW_TABLE_INRUIRY_ACTION);
         return new ActionForward(path, true);

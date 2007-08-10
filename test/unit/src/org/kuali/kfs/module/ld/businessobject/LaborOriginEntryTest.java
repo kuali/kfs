cd723831727233d4a92b11d5b03f7218ade39ae9
Shawn Choo <schoo@vivantech.com>
2007-08-10 18:04:27 +0000

    Fixed an issue of unit test

--- test/unit/src/org/kuali/kfs/module/ld/businessobject/LaborOriginEntryTest.java
+++ test/unit/src/org/kuali/kfs/module/ld/businessobject/LaborOriginEntryTest.java
@@ -30,19 +30,22 @@ public class LaborOriginEntryTest extends KualiTestBase {
     public void testGetLine() throws Exception {
 
         String line = null;
-
-      /*  line = "2007BL4131407-----4100---ACEX07TOPSLGEXPIRCGAC          CONCERTO OFFICE PRODUCTS                            48.53C2006-01-05          ----------                                       ";
+/*
+        line = "2007BL4131407-----4100---ACEX07TOPSLGEXPIRCGAC          CONCERTO OFFICE PRODUCTS                            48.53C2006-01-05          ----------                                       ";
         String convertedLine = "2007BL4131407-----4100---ACEX07TOPSLGEXPIRCGAC     00000CONCERTO OFFICE PRODUCTS                            48.53C2006-01-05          ----------                                       ";
+        
         assertEquals(convertedLine, new LaborOriginEntry(line).getLine());
-*//*
+*/
+          
         line = "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                  +000000000003493.50D2006-12-22                                                 2006-12-222006-12-31000168.002007060000149952 001REGS12PAE 11 M001010207                     IU IUBLA";
         String convertedLine = "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                              3493.50D2006-12-22                                                 2006-12-222006-12-31168.00   2007060000149952 1  REGS12PAE 11 M001010207                     IU IUBLA";
         assertEquals(convertedLine, new LaborOriginEntry(line).getLine());
+        assertEquals(convertedLine, new LaborOriginEntry(convertedLine).getLine());
         
-        line = "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                              3493.50D2006-12-22                                                 2006-12-222006-12-31168.00   2007060000149952 1  REGS12PAE 11 M001010207                     IU IUBLA";
+        /*line = "2007BA6044900-----2400---ACEX06PAY PLM01010207     0000100009529----------KUALI TEST DESCRIPTION                              3493.50D2006-12-22                                                 2006-12-222006-12-31168.00   2007060000149952 1  REGS12PAE 11 M001010207                     IU IUBLA";
         assertEquals(line, new LaborOriginEntry(line).getLine());
-  */      
-       /* line = "2007BL4131407-----9041---ACLI07TOPSLGEXPIRCGAC     12345CONCERTO OFFICE PRODUCTS                            48.53D2006-01-05          ----------                                       ";
+ */
+        /* line = "2007BL4131407-----9041---ACLI07TOPSLGEXPIRCGAC     12345CONCERTO OFFICE PRODUCTS                            48.53D2006-01-05          ----------                                       ";
         assertEquals(line, new LaborOriginEntry(line).getLine());
 
         line = "2007BL4131407-----9041---ACLI07TOPSLGEXPIRCGAC     00045CONCERTO OFFICE PRODUCTS                            48.53D2006-01-05          ----------                                       ";

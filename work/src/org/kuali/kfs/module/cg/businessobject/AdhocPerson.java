e39ce7524bbf357e0e19e1eab6fda7cdf224a18f
Dennis Friends <dwf5@cornell.edu>
2007-08-10 13:57:49 +0000

    KULERABFDBCK-381 : Added new getters and setters and two new attributes to help resolve a problem with the interface refreshing properly on add.

--- work/src/org/kuali/kfs/module/cg/businessobject/AdhocPerson.java
+++ work/src/org/kuali/kfs/module/cg/businessobject/AdhocPerson.java
@@ -18,8 +18,11 @@ package org.kuali.module.kra.bo;
 import java.util.LinkedHashMap;
 
 import org.apache.commons.lang.StringUtils;
+import org.kuali.core.bo.user.AuthenticationUserId;
 import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.exceptions.UserNotFoundException;
 import org.kuali.kfs.KFSPropertyConstants;
+import org.kuali.rice.KNSServiceLocator;
 
 /**
  * This class represents an ad-hoc person.
@@ -31,6 +34,8 @@ public class AdhocPerson extends AbstractAdhoc {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AdhocPerson.class);
 
     private String personUniversalIdentifier;
+    private String personUserIdentifier;
+    private String name;
     private UniversalUser user;
     
     public AdhocPerson() {
@@ -77,6 +82,64 @@ public class AdhocPerson extends AbstractAdhoc {
      */
     public void setUser(UniversalUser user) {
         this.user = user;
+    }
+    
+    /**
+     * 
+     * This method retrieves the associated user id from the UniversalUser attribute.
+     * @return The user id of the associated user.
+     */
+    public String getPersonUserIdentifier() {
+        if ( user == null || user.getPersonUserIdentifier() == null ) {
+            user = null;
+            try {
+                user = KNSServiceLocator.getUniversalUserService().getUniversalUser( getPersonUniversalIdentifier() );
+            } catch (UserNotFoundException ex) {
+                // do nothing, leave user as null
+            }
+        }
+        if ( user == null ) {
+            return "";
+        }
+        return user.getPersonUserIdentifier();
+    }
+    
+    /**
+     * 
+     * This method has no function and is only here to satisfy Struts.
+     * @param userIdentifier User id to be passed in.
+     */
+    public void setPersonUserIdentifier(String userIdentifier) {
+        // do nothing, the getter will handle this
+    }
+    
+    /**
+     * 
+     * This method retrieves the associated user name from the UniversalUser attribute.
+     * @return The user name in the format of LAST, FIRST
+     */
+    public String getName() {
+        if ( user == null || user.getPersonName() == null  ) {
+            user = null;
+            try {
+                user = KNSServiceLocator.getUniversalUserService().getUniversalUser( getPersonUniversalIdentifier() );
+            } catch ( UserNotFoundException ex ) {
+                // do nothing, leave UU as null
+            }
+        }
+        if ( user == null ) {
+            return "";
+        }
+        return user.getPersonName();
+    }
+    
+    /**
+     * 
+     * This method has no function and is only here to satisfy Struts.
+     * @param name The name of the user.
+     */
+    public void setName(String name) {
+        // do nothing, the getter will handle this
     }
     
     /**

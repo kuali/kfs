5af2703c86bf93e4846009c5e83497f286e2b97c
Gary	Palmer <gwpalmer@iu.edu>
2007-08-13 18:37:11 +0000

    Initial work to implement BudgetOrganizationTreeService build and clean pullup

---
+++ test/unit/src/org/kuali/kfs/module/bc/document/service/BudgetOrganizationTreeServiceTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.service;
+
+import java.util.HashMap;
+
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.kfs.context.KualiTestBase;
+import org.kuali.kfs.context.SpringContext;
+import org.kuali.module.budget.bo.BudgetConstructionPullup;
+import org.kuali.test.ConfigureContext;
+
+/**
+ * This class...
+ */
+@ConfigureContext
+public class BudgetOrganizationTreeServiceTest extends KualiTestBase {
+    private BudgetOrganizationTreeService budgetOrganizationTreeService;
+    private BusinessObjectService businessObjectService;
+    
+    private String personUniversalIdentifier = "1111111111";
+    private String chartOfAccountsCode = "UA";
+    private String organizationCode = "UA";
+    
+    private boolean runTests() { // change this to return false to prevent running tests
+        return true;
+    }
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception {
+
+        super.setUp();
+        budgetOrganizationTreeService = SpringContext.getBean(BudgetOrganizationTreeService.class);
+        businessObjectService = SpringContext.getBean(BusinessObjectService.class);
+    }
+
+    @ConfigureContext(shouldCommitTransactions=true)
+    public void testBuildPullup() throws Exception {
+
+        if (!runTests()) return;
+
+        budgetOrganizationTreeService.buildPullup(personUniversalIdentifier, chartOfAccountsCode, organizationCode);
+        HashMap map = new HashMap();
+        map.put("personUniversalIdentifier", personUniversalIdentifier);
+        map.put("chartOfAccountsCode", chartOfAccountsCode);
+        map.put("organizationCode", organizationCode);
+
+        BudgetConstructionPullup bcPullup = (BudgetConstructionPullup) businessObjectService.findByPrimaryKey(BudgetConstructionPullup.class, map);
+        assertTrue(bcPullup.getChartOfAccountsCode().equalsIgnoreCase(chartOfAccountsCode));
+        assertTrue(bcPullup.getOrganizationCode().equalsIgnoreCase(organizationCode));
+        assertTrue(bcPullup.getPersonUniversalIdentifier().equalsIgnoreCase(personUniversalIdentifier));
+        
+
+    }
+
+}

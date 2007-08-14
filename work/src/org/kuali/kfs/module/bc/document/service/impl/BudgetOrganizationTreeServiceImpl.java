e851c28ea46b7c7db30e738e05d0835e77e7af29
Gary	Palmer <gwpalmer@iu.edu>
2007-08-14 18:56:16 +0000

    fleshed out buildSubTree

--- work/src/org/kuali/kfs/module/bc/document/service/impl/BudgetOrganizationTreeServiceImpl.java
+++ work/src/org/kuali/kfs/module/bc/document/service/impl/BudgetOrganizationTreeServiceImpl.java
@@ -15,6 +15,10 @@
  */
 package org.kuali.module.budget.service.impl;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.kuali.core.service.BusinessObjectService;
 import org.kuali.module.budget.bo.BudgetConstructionOrganizationReports;
 import org.kuali.module.budget.bo.BudgetConstructionPullup;
@@ -28,30 +32,56 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Transactional
 public class BudgetOrganizationTreeServiceImpl implements BudgetOrganizationTreeService {
+    private static Logger LOG = org.apache.log4j.Logger.getLogger(BudgetOrganizationTreeServiceImpl.class);
+
     private BudgetConstructionOrganizationReportsService budgetConstructionOrganizationReportsService;
     private BusinessObjectService businessObjectService;
     private BudgetConstructionDao budgetConstructionDao;
+    
+    // controls used to trap any runaways due to cycles in the reporting tree
+    private static final int MAXLEVEL = 50;
+    private int curLevel;
 
     /**
      * @see org.kuali.module.budget.service.BudgetOrganizationTreeService#buildPullup(java.lang.String, java.lang.String, java.lang.String)
      */
     public void buildPullup(String personUserIdentifier, String chartOfAccountsCode, String organizationCode) {
         cleanPullup(personUserIdentifier);
-        buildSubTree(personUserIdentifier, chartOfAccountsCode, organizationCode);
-    }
-    
-    private void buildSubTree(String personUserIdentifier, String chartOfAccountsCode, String organizationCode){
-        
         BudgetConstructionOrganizationReports bcOrgRpts = budgetConstructionOrganizationReportsService.getByPrimaryId(chartOfAccountsCode, organizationCode);
         if (bcOrgRpts != null){
-            BudgetConstructionPullup bcPullup = new BudgetConstructionPullup();
-            bcPullup.setPersonUniversalIdentifier(personUserIdentifier);
-            bcPullup.setChartOfAccountsCode(bcOrgRpts.getChartOfAccountsCode());
-            bcPullup.setOrganizationCode(bcOrgRpts.getOrganizationCode());
-            bcPullup.setReportsToChartOfAccountsCode(bcOrgRpts.getReportsToChartOfAccountsCode());
-            bcPullup.setReportsToOrganizationCode(bcOrgRpts.getReportsToOrganizationCode());
-            bcPullup.setPullFlag(new Integer(0));
-            businessObjectService.save(bcPullup);
+            if (bcOrgRpts.getOrganization().isOrganizationActiveIndicator()){
+                curLevel = 0;
+                buildSubTree(personUserIdentifier, bcOrgRpts, curLevel);
+            }
+        }
+    }
+    
+    private void buildSubTree(String personUserIdentifier, BudgetConstructionOrganizationReports bcOrgRpts, int curLevel){
+        
+        curLevel++;
+        BudgetConstructionPullup bcPullup = new BudgetConstructionPullup();
+        bcPullup.setPersonUniversalIdentifier(personUserIdentifier);
+        bcPullup.setChartOfAccountsCode(bcOrgRpts.getChartOfAccountsCode());
+        bcPullup.setOrganizationCode(bcOrgRpts.getOrganizationCode());
+        bcPullup.setReportsToChartOfAccountsCode(bcOrgRpts.getReportsToChartOfAccountsCode());
+        bcPullup.setReportsToOrganizationCode(bcOrgRpts.getReportsToOrganizationCode());
+        bcPullup.setPullFlag(new Integer(0));
+        businessObjectService.save(bcPullup);
+
+        if (curLevel <= MAXLEVEL){
+            // getActiveChildOrgs does not return orgs that report to themselves 
+            List childOrgs = budgetConstructionOrganizationReportsService.getActiveChildOrgs(bcOrgRpts.getChartOfAccountsCode(), bcOrgRpts.getOrganizationCode());
+            if (childOrgs.size() > 0){
+                for (Iterator iter = childOrgs.iterator(); iter.hasNext();){
+                    BudgetConstructionOrganizationReports bcOrg = (BudgetConstructionOrganizationReports) iter.next();
+                    buildSubTree(personUserIdentifier, bcOrg, curLevel);
+                }
+            }
+        } else {
+            LOG.warn(String.format("\n%s/%s reports to organization more than maxlevel of %d",
+                    bcOrgRpts.getChartOfAccountsCode(),
+                    bcOrgRpts.getOrganizationCode(),
+                    MAXLEVEL));
         }
     }
 

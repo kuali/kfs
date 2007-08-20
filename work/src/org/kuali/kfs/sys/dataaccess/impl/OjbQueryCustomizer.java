e833cf06dce17dfdcc1f8f0ef7a2c848d1304c53
Jeff	Morris <jbmorris@indiana.edu>
2007-08-20 20:05:32 +0000

    KULDBA-955

--- work/src/org/kuali/kfs/sys/dataaccess/impl/OjbQueryCustomizer.java
+++ work/src/org/kuali/kfs/sys/dataaccess/impl/OjbQueryCustomizer.java
@@ -28,6 +28,7 @@ import org.kuali.core.util.ObjectUtils;
 
 public class OjbQueryCustomizer extends QueryCustomizerDefaultImpl {
     // used to AND in additional criteria on a collection
+    private static final String FIELD_PREFIX = "parent.";
 
     @Override
     public Query customizeQuery(Object arg0, PersistenceBroker arg1, CollectionDescriptor arg2, QueryByCriteria arg3) {
@@ -47,17 +48,17 @@ public class OjbQueryCustomizer extends QueryCustomizerDefaultImpl {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        
+
         // now, do what we wanted to do to start with if we could've just gotten m_attributeList easily
         Criteria criteria = arg3.getCriteria();
         for (String key : m_attributeList.keySet()) {
-            //if beginning with "parent." is too hacky, or more flexibility is needed, another query customizer class can be made,
+            //if beginning with FIELD_PREFIX is too hacky, or more flexibility is needed, another query customizer class can be made,
             // and this method can be renamed to take a parameter to specify which we want to do
             // (and the customizeQuery method here made to call the new method with the parameter).
             // However, making another class would mean you couldn't intermix constants and field values,
             // since OJB won't use have multiple query-customizers per collection-descriptor.
-            if (this.getAttribute(key).startsWith("parent.")) {
-                criteria.addEqualTo(key, ObjectUtils.getPropertyValue(arg0, this.getAttribute(key).substring(5)));
+            if (this.getAttribute(key).startsWith(FIELD_PREFIX)) {
+                criteria.addEqualTo(key, ObjectUtils.getPropertyValue(arg0, this.getAttribute(key).substring(FIELD_PREFIX.length())));
             } else {
                 criteria.addEqualTo(key, this.getAttribute(key));
             }

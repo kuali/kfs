f5b37d34d1a02155fd0e59b679b95ba4f24cc7ee
Shawn Choo <schoo@vivantech.com>
2007-08-29 19:24:19 +0000

    KULLAB-402, create ClearLaborPendingStep

---
+++ work/src/org/kuali/kfs/module/ld/batch/ClearLaborPendingStep.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.batch;
+
+import org.kuali.kfs.batch.AbstractStep;
+import org.kuali.module.labor.service.LaborNightlyOutService;
+
+public class ClearLaborPendingStep extends AbstractStep {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ClearLaborPendingStep.class);
+    private LaborNightlyOutService laborNightlyOutService;
+
+    public boolean execute(String jobName) {
+        laborNightlyOutService.deleteCopiedPendingLedgerEntries();
+        return true;
+    }
+
+    /**
+     * Sets the laborNightlyOutService attribute value.
+     * 
+     * @param laborNightlyOutService The laborNightlyOutService to set.
+     */
+    public void setLaborNightlyOutService(LaborNightlyOutService laborNightlyOutService) {
+        this.laborNightlyOutService = laborNightlyOutService;
+    }
+
+}

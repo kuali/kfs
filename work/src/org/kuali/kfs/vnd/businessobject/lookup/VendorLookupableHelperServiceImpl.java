d34b3c0f214694342913fa25462bd989c68935e6
Tony Potts <tony@rsmart.com>
2007-09-07 17:37:29 +0000

    vendor search now properly including divisions and parents as designed
    [KULPURAP-877]

--- work/src/org/kuali/kfs/vnd/businessobject/lookup/VendorLookupableHelperServiceImpl.java
+++ work/src/org/kuali/kfs/vnd/businessobject/lookup/VendorLookupableHelperServiceImpl.java
@@ -17,6 +17,7 @@ package org.kuali.module.vendor.lookup;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +36,8 @@ import org.kuali.core.util.ObjectUtils;
 import org.kuali.core.util.UrlFactory;
 import org.kuali.core.web.format.Formatter;
 import org.kuali.kfs.KFSConstants;
+import org.kuali.kfs.context.SpringContext;
+import org.kuali.module.purap.service.CreditMemoCreateService;
 import org.kuali.module.vendor.VendorConstants;
 import org.kuali.module.vendor.VendorKeyConstants;
 import org.kuali.module.vendor.VendorPropertyConstants;
@@ -148,30 +151,50 @@ public class VendorLookupableHelperServiceImpl extends AbstractLookupableHelperS
         List<BusinessObject> processedSearchResults = new ArrayList();
         
         // loop through results
-        // if its a top level vendor, search for its divisions and add them to the appropriate list then add the vendor to the return results
-        // if its a division, see if we already have the parent and if not, retrieve it and its divisions then add the parent to the return results
-        
-        // loop through results
         for (BusinessObject bo : searchResults) {
             VendorDetail vendor = (VendorDetail) bo;
+
+            // if its a top level vendor, search for its divisions and add them to the appropriate list then add the vendor to the return results
+            // if its a division, see if we already have the parent and if not, retrieve it and its divisions then add the parent to the return results
+
+ 
             //If this vendor is not already in the processedSearchResults, let's do further processing (e.g. setting the state for lookup from default address, etc)
             //and then add it in the processedSearchResults.
             if (!processedSearchResults.contains(vendor)) {
-                // populate state from default address
+                Map<String, String> tmpValues = new HashMap<String, String>();
+                List<VendorDetail> relatedVendors = new ArrayList();
+                tmpValues.put(VendorPropertyConstants.VENDOR_HEADER_GENERATED_ID, vendor.getVendorHeaderGeneratedIdentifier().toString());
+                relatedVendors = (List) getLookupService().findCollectionBySearchHelper(getBusinessObjectClass(), tmpValues, unbounded);
 
-                if (!vendor.isVendorParentIndicator()) {
-                    for (BusinessObject tmpObject : searchResults) {
-                        VendorDetail tmpVendor = (VendorDetail) tmpObject;
-                        if (tmpVendor.getVendorHeaderGeneratedIdentifier().equals(vendor.getVendorHeaderGeneratedIdentifier()) && tmpVendor.getVendorDetailAssignedIdentifier() == 0) {
-                            vendor.setVendorName(tmpVendor.getVendorName() + " > " + vendor.getVendorName());
-                            break;
-                        }
+                for (VendorDetail tmpVendor : relatedVendors) {
+                    if (tmpVendor != null && !processedSearchResults.contains(tmpVendor)) {
+                        // populate state from default address
+                        updatedefaultVendorAddress(tmpVendor);
+                        processedSearchResults.add(tmpVendor);
                     }
                 }
-                updatedefaultVendorAddress(vendor);
-                processedSearchResults.add(vendor);
+
+                if (!processedSearchResults.contains(vendor)) {
+                    updatedefaultVendorAddress(vendor);
+                    processedSearchResults.add(vendor);
+                }
             }
         }
+
+        for (BusinessObject bo : processedSearchResults) {
+            VendorDetail vendor = (VendorDetail) bo;
+            if (!vendor.isVendorParentIndicator()) {
+                // find the parent object in the details collection and add that§
+                for (BusinessObject tmpObject : processedSearchResults) {
+                    VendorDetail tmpVendor = (VendorDetail) tmpObject;
+                    if (tmpVendor.getVendorHeaderGeneratedIdentifier().equals(vendor.getVendorHeaderGeneratedIdentifier()) && tmpVendor.getVendorDetailAssignedIdentifier() == 0) {
+                        vendor.setVendorName(tmpVendor.getVendorName() + " > " + vendor.getVendorName());
+                        break;
+                    }
+                }
+            }
+        }
+
         searchResults.clear();
         searchResults.addAll(processedSearchResults);
         

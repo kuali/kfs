0bb9c02cc80f0b5b15d82fa81b6cc7feabc66b50
Jerry Neal <jkneal@iu.edu>
2007-09-10 17:48:32 +0000

    KULLAB-448 created lookupable for position object benefit to supress edit and copy

---
+++ work/src/org/kuali/kfs/module/ld/businessobject/lookup/PositionObjectBenefitLookupableHelperServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.web.lookupable;
+
+import org.kuali.core.bo.BusinessObject;
+import org.kuali.core.lookup.KualiLookupableHelperServiceImpl;
+
+/**
+ * Custom lookupable to supress edit/copy actions on lookup screen.
+ */
+public class PositionObjectBenefitLookupableHelperServiceImpl extends KualiLookupableHelperServiceImpl {
+
+    /**
+     * @returns Empty string because we don't want any action links to be displayed on the lookups results page.
+     * @see org.kuali.core.lookup.LookupableHelperService#getActionUrls(org.kuali.core.bo.BusinessObject)
+     */
+    public String getActionUrls(BusinessObject businessObject) {
+        return "";
+    }
+}

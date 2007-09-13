0964c6556d2ac95f7026ef3d31217bdc90889ecc
Shawn Choo <schoo@vivantech.com>
2007-09-13 15:15:03 +0000

    KULLAB-464

--- work/src/org/kuali/kfs/module/ld/document/web/struts/JournalVoucherAction.java
+++ work/src/org/kuali/kfs/module/ld/document/web/struts/JournalVoucherAction.java
@@ -15,6 +15,8 @@
  */
 package org.kuali.module.labor.web.struts.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,8 +26,12 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.kuali.RiceConstants;
 import org.kuali.kfs.KFSConstants;
+import org.kuali.kfs.bo.AccountingLine;
 import org.kuali.module.labor.LaborConstants;
+import org.kuali.module.labor.bo.LaborJournalVoucherDetail;
 import org.kuali.module.labor.bo.LaborLedgerPendingEntry;
+import org.kuali.module.labor.document.LaborJournalVoucherDocument;
+import org.kuali.module.labor.web.struts.form.LaborJournalVoucherForm;
 
 /**
  * This class piggy backs on all of the functionality in the KualiTransactionalDocumentActionBase but is necessary for this document
@@ -53,4 +59,24 @@ public class JournalVoucherAction extends org.kuali.module.financial.web.struts.
         path = path.replaceFirst(KFSConstants.LOOKUP_ACTION, LaborConstants.LONG_ROW_TABLE_INRUIRY_ACTION);
         return new ActionForward(path, true);
     }
+
+    @Override
+    public ActionForward route(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        
+        //KULLAB-464
+        LaborJournalVoucherForm ljForm = (LaborJournalVoucherForm) form;
+        LaborJournalVoucherDocument ljvd = (LaborJournalVoucherDocument) ljForm.getDocument();
+        List<LaborJournalVoucherDetail> sourceAccountingLines = ljvd.getSourceAccountingLines(); 
+        
+        for (LaborJournalVoucherDetail al: sourceAccountingLines){
+           if (al.getEmplid() == null || al.getEmplid().equals("")){
+               al.setEmplid("-----------");
+           }
+        }
+        
+        return super.route(mapping, form, request, response);
+    }
+    
+    
+    
 }

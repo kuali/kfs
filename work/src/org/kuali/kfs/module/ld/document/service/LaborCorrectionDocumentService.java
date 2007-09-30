082686d277681ff590c53cda799ef6696301cf8a
Shawn Choo <schoo@vivantech.com>
2007-09-30 17:46:40 +0000

    Code Clean up

--- work/src/org/kuali/kfs/module/ld/document/service/LaborCorrectionDocumentService.java
+++ work/src/org/kuali/kfs/module/ld/document/service/LaborCorrectionDocumentService.java
@@ -34,30 +34,54 @@ public interface LaborCorrectionDocumentService {
 
     public final static String SYSTEM_DATABASE = "D";
     public final static String SYSTEM_UPLOAD = "U";
-    
+
     /**
      * When passed into {@link #retrievePersistedInputOriginEntries(CorrectionDocument, int)} and
-     * {@link #retrievePersistedOutputOriginEntries(CorrectionDocument, int)} as the int parameter, this will signify
-     * that there is no abort threshold (i.e. the methods should return all of the persisted rows, regardless
-     * of number of rows.
+     * {@link #retrievePersistedOutputOriginEntries(CorrectionDocument, int)} as the int parameter, this will signify that there is
+     * no abort threshold (i.e. the methods should return all of the persisted rows, regardless of number of rows.
      */
     public final static int UNLIMITED_ABORT_THRESHOLD = CorrectionDocumentUtils.RECORD_COUNT_FUNCTIONALITY_LIMIT_IS_UNLIMITED;
 
+    /**
+     * Find and return correctionChangeGroup with document number and Group Number
+     * 
+     * @param docId, i
+     * @return
+     */
     public CorrectionChangeGroup findByDocumentNumberAndCorrectionChangeGroupNumber(String docId, int i);
 
+    /**
+     * Find and return correctionChange with document number and Group Number
+     * 
+     * @param docId, i
+     * @return list of correctionChange
+     */
     public List findByDocumentHeaderIdAndCorrectionGroupNumber(String docId, int i);
 
+    /**
+     * Find and return list of correctionCriteria with document number and Group Number
+     * 
+     * @param docId, i
+     * @return
+     */
     public List findByDocumentNumberAndCorrectionGroupNumber(String docId, int i);
 
-    public LaborCorrectionDocument findByCorrectionDocumentHeaderId(String docId);
-    
     /**
-     * Returns metadata to help render columns in the GLCP.  Do not modify this list or the contents in this list.
+      * Find and return laborCorrectionDocument with document number
+     * 
+     * @param docId, i
+     * @return
+     */
+    public LaborCorrectionDocument findByCorrectionDocumentHeaderId(String docId);
+
+    /**
+     * Returns metadata to help render columns in the LLCP. Do not modify this list or the contents in this list.
+     * 
      * @param docId
      * @return
      */
     public List<Column> getTableRenderColumnMetadata(String docId);
-    
+
     /**
      * This method persists an Iterator of input origin entries for a document that is in the initiated or saved state
      * 
@@ -65,41 +89,49 @@ public interface LaborCorrectionDocumentService {
      * @param entries
      */
     public void persistInputOriginEntriesForInitiatedOrSavedDocument(LaborCorrectionDocument document, Iterator<LaborOriginEntry> entries);
-    
+
+    /**
+     * @see org.kuali.module.labor.service.LaborCorrectionDocumentService#removePersistedInputOriginEntriesForInitiatedOrSavedDocument(org.kuali.module.labor.document.LaborCorrectionDocument)
+     */
     public void removePersistedInputOriginEntries(LaborCorrectionDocument document);
-    
+
+    /**
+     * @see org.kuali.module.labor.service.LaborCorrectionDocumentService#removePersistedInputOriginEntriesForInitiatedOrSavedDocument(org.kuali.module.labor.document.LaborCorrectionDocument)
+     */
     public void removePersistedInputOriginEntries(String docId);
-    
+
     /**
      * Retrieves input origin entries that have been persisted for this document
+     * 
      * @param document the document
-     * @param abortThreshold if the file exceeds this number of rows, then null is returned.  
-     * {@link UNLIMITED_ABORT_THRESHOLD} signifies that there is no limit 
+     * @param abortThreshold if the file exceeds this number of rows, then null is returned. {@link UNLIMITED_ABORT_THRESHOLD}
+     *        signifies that there is no limit
      * @return the list, or null if there are too many origin entries
      * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems
      */
     public List<LaborOriginEntry> retrievePersistedInputOriginEntries(LaborCorrectionDocument document, int abortThreshold);
-    
+
     /**
-     * Returns true if the system is storing input origin entries for this class.  Note that this does not mean that there's at least one input origin
-     * entry record persisted for this document, but merely returns true if and only if the underlying persistence mechanism has a record of this document's
-     * origin entries.  See the docs for the implementations of this method for more implementation specific details.
-     *  
+     * Returns true if the system is storing input origin entries for this class. Note that this does not mean that there's at least
+     * one input origin entry record persisted for this document, but merely returns true if and only if the underlying persistence
+     * mechanism has a record of this document's origin entries. See the docs for the implementations of this method for more
+     * implementation specific details.
+     * 
      * @param document
      * @return
      */
     public boolean areInputOriginEntriesPersisted(LaborCorrectionDocument document);
-    
+
     /**
      * Writes out the persisted input origin entries in an {@link OutputStream} in a flat file format
      * 
-     * @param document 
+     * @param document
      * @param out an open and ready output stream
      * @throws IOException
      * @throws RuntimeException several reasons, including if the entries are not persisted
      */
     public void writePersistedInputOriginEntriesToStream(LaborCorrectionDocument document, OutputStream out) throws IOException;
-    
+
     /**
      * This method persists an Iterator of input origin entries for a document that is in the initiated or saved state
      * 
@@ -107,51 +139,59 @@ public interface LaborCorrectionDocumentService {
      * @param entries
      */
     public void persistOutputLaborOriginEntriesForInitiatedOrSavedDocument(LaborCorrectionDocument document, Iterator<LaborOriginEntry> entries);
-    
+
+    /**
+     * @see org.kuali.module.labor.service.LaborCorrectionDocumentService#removePersistedOutputOriginEntriesForInitiatedOrSavedDocument(org.kuali.module.labor.document.CorrectionDocument)
+     */
     public void removePersistedOutputOriginEntries(LaborCorrectionDocument document);
-    
+
+    /**
+     * @see org.kuali.module.labor.service.LaborCorrectionDocumentService#removePersistedOutputOriginEntriesForInitiatedOrSavedDocument(org.kuali.module.labor.document.CorrectionDocument)
+     */
     public void removePersistedOutputOriginEntries(String docId);
-    
+
     /**
      * Retrieves output origin entries that have been persisted for this document
+     * 
      * @param document the document
-     * @param abortThreshold if the file exceeds this number of rows, then null is returned.  
-     * {@link UNLIMITED_ABORT_THRESHOLD} signifies that there is no limit 
+     * @param abortThreshold if the file exceeds this number of rows, then null is returned. {@link UNLIMITED_ABORT_THRESHOLD}
+     *        signifies that there is no limit
      * @return the list, or null if there are too many origin entries
-     * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems 
+     * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems
      */
     public List<LaborOriginEntry> retrievePersistedOutputOriginEntries(LaborCorrectionDocument document, int abortThreshold);
-    
+
     /**
-     * Retrieves input origin entries that have been persisted for this document in an iterator.  Implementations of
-     * this method may choose to implement this method in a way that consumes very little memory.
+     * Retrieves input origin entries that have been persisted for this document in an iterator. Implementations of this method may
+     * choose to implement this method in a way that consumes very little memory.
      * 
      * @param document the document
      * @return the iterator
-     * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems 
+     * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems
      */
     public Iterator<LaborOriginEntry> retrievePersistedInputOriginEntriesAsIterator(LaborCorrectionDocument document);
-    
+
     /**
-     * Retrieves output origin entries that have been persisted for this document in an iterator.  Implementations of
-     * this method may choose to implement this method in a way that consumes very little memory.
+     * Retrieves output origin entries that have been persisted for this document in an iterator. Implementations of this method may
+     * choose to implement this method in a way that consumes very little memory.
      * 
      * @param document the document
      * @return the iterator
-     * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems 
+     * @throws RuntimeException several reasons, primarily relating to underlying persistence layer problems
      */
     public Iterator<LaborOriginEntry> retrievePersistedOutputOriginEntriesAsIterator(LaborCorrectionDocument document);
-    
+
     /**
-     * Returns true if the system is storing output origin entries for this class.  Note that this does not mean that there's at least one output origin
-     * entry record persisted for this document, but merely returns true if and only if the underlying persistence mechanism has a record of this document's
-     * origin entries.  See the docs for the implementations of this method for more implementation specific details.
-     *  
+     * Returns true if the system is storing output origin entries for this class. Note that this does not mean that there's at
+     * least one output origin entry record persisted for this document, but merely returns true if and only if the underlying
+     * persistence mechanism has a record of this document's origin entries. See the docs for the implementations of this method for
+     * more implementation specific details.
+     * 
      * @param document
      * @return
      */
     public boolean areOutputOriginEntriesPersisted(LaborCorrectionDocument document);
-    
+
     /**
      * Writes out the persisted output origin entries in an {@link OutputStream} in a flat file format\
      * 
@@ -161,9 +201,10 @@ public interface LaborCorrectionDocumentService {
      * @throws RuntimeException several reasons, including if the entries are not persisted
      */
     public void writePersistedOutputOriginEntriesToStream(LaborCorrectionDocument document, OutputStream out) throws IOException;
-    
+
     /**
      * Saves the input and output origin entry groups for a document prior to saving the document
+     * 
      * @param document
      * @param LaborCorrectionDocumentEntryMetadata
      */

764e078c7651ad2a0c2485374b984cd34a579bad
Gary	Palmer <gwpalmer@iu.edu>
2007-10-19 17:47:38 +0000

    new parent class for the BC SelectLookupables

---
+++ work/src/org/kuali/kfs/module/bc/businessobject/lookup/SelectLookupableHelperServiceImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.web.lookupable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.core.bo.BusinessObject;
+import org.kuali.core.lookup.AbstractLookupableHelperServiceImpl;
+import org.kuali.core.util.BeanPropertyComparator;
+import org.kuali.kfs.KFSConstants;
+
+/**
+ * This class...
+ */
+public class SelectLookupableHelperServiceImpl extends AbstractLookupableHelperServiceImpl {
+
+    /**
+     * @see org.kuali.core.lookup.AbstractLookupableHelperServiceImpl#getSearchResults(java.util.Map)
+     */
+    @Override
+    public List<? extends BusinessObject> getSearchResults(Map<String, String> fieldValues) {
+        //TODO may want to push this method to a parent class to be used by all BC temp table lookups
+
+        // We need to keep the personUniversalIdentifier hidden field in the criteria when
+        // operating against BC temp lookup tables that are built on the fly. This field is
+        // set behind the scenes so as to operate on only those rows associated with the current user.
+        //LookupUtils.removeHiddenCriteriaFields( getBusinessObjectClass(), fieldValues );
+
+        setBackLocation(fieldValues.get(KFSConstants.BACK_LOCATION));
+        setDocFormKey(fieldValues.get(KFSConstants.DOC_FORM_KEY));
+        setReferencesToRefresh(fieldValues.get(KFSConstants.REFERENCES_TO_REFRESH));
+        
+        List searchResults = (List) getLookupService().findCollectionBySearchHelper(getBusinessObjectClass(), fieldValues, false);
+        // sort list if default sort column given
+        List defaultSortColumns = getDefaultSortColumns();
+        if (defaultSortColumns.size() > 0) {
+            Collections.sort(searchResults, new BeanPropertyComparator(getDefaultSortColumns(), true));
+        }
+        return searchResults;
+    }
+
+}

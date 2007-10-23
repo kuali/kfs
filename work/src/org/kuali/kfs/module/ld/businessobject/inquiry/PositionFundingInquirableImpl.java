b5b1381ae7802fef1bc6dfbd12c59dd9e002280d
Gerardo Velasquez <gerardov@usc.edu>
2007-10-23 22:28:57 +0000

    KULLAB 542:  Problems on Position Info inquiry
    Two new classes were created in order to get the correct URL's of each record displayed on the screen.
    
    A.) New Classes are:
    PositionFundingInquirableImpl
    PositionFundingLookupableHelperServiceImpl
    
    B.) lookupableID section added in PositionFunding D.D.
    
    C.) New class  PositionFundingLookupableHelperServiceImpl added in KualiSpringBeanLabor.

---
+++ work/src/org/kuali/kfs/module/ld/businessobject/inquiry/PositionFundingInquirableImpl.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.web.inquirable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.kuali.kfs.KFSConstants;
+import org.kuali.kfs.KFSPropertyConstants;
+import org.kuali.module.gl.GLConstants;
+import org.kuali.module.gl.web.Constant;
+import org.kuali.module.labor.bo.PositionData;
+import org.kuali.module.labor.bo.PositionFunding;
+
+/**
+ * This class is used to generate the URL for the user-defined attributes for the Position Inquiry screen. It is entended the
+ * KualiInquirableImpl class, so it covers both the default implementation and customized implemetnation.
+ */
+public class PositionFundingInquirableImpl extends AbstractLaborInquirableImpl {
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#buildUserDefinedAttributeKeyList()
+     */
+    protected List buildUserDefinedAttributeKeyList() {
+        List keys = new ArrayList();
+        keys.add(KFSPropertyConstants.POSITION_NUMBER);
+        return keys;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getAttributeName(java.lang.String)
+     */
+    protected String getAttributeName(String attributeName) {
+        return attributeName;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getBaseUrl()
+     */
+    protected String getBaseUrl() {
+        return KFSConstants.GL_MODIFIED_INQUIRY_ACTION;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getInquiryBusinessObjectClass(java.lang.String)
+     */
+    protected Class getInquiryBusinessObjectClass(String attributeName) {
+        return PositionFunding.class;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getKeyName(java.lang.String)
+     */
+    protected String getKeyName(String keyName) {
+        return keyName;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getKeyValue(java.lang.String, java.lang.Object)
+     */
+    protected Object getKeyValue(String keyName, Object keyValue) {
+        return keyValue;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getLookupableImplAttributeName()
+     */
+    protected String getLookupableImplAttributeName() {
+        return null;
+    }
+
+    /**
+     * @see org.kuali.module.labor.web.inquirable.AbstractLaborInquirableImpl#getUserDefinedAttributeMap()
+     */
+    protected Map getUserDefinedAttributeMap() {
+        Map userDefinedAttributeMap = new HashMap();
+        userDefinedAttributeMap.put(GLConstants.DummyBusinessObject.LINK_BUTTON_OPTION, "");
+
+        return userDefinedAttributeMap;
+    }
+}
---
+++ work/src/org/kuali/kfs/module/ld/businessobject/lookup/PositionFundingLookupableHelperServiceImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.labor.web.lookupable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.core.bo.BusinessObject;
+import org.kuali.core.lookup.LookupUtils;
+import org.kuali.core.util.BeanPropertyComparator;
+import org.kuali.kfs.KFSConstants;
+import org.kuali.kfs.KFSPropertyConstants;
+import org.kuali.module.labor.web.inquirable.PositionFundingInquirableImpl;
+import org.kuali.core.lookup.AbstractLookupableHelperServiceImpl;
+
+public class PositionFundingLookupableHelperServiceImpl extends AbstractLookupableHelperServiceImpl {
+    /**
+     * @see org.kuali.core.lookup.Lookupable#getInquiryUrl(org.kuali.core.bo.BusinessObject, java.lang.String)
+     */
+    @Override
+    public String getInquiryUrl(BusinessObject businessObject, String propertyName) {
+        return (new PositionFundingInquirableImpl()).getInquiryUrl(businessObject, propertyName);
+    }
+
+    /**
+     * @see org.kuali.core.lookup.AbstractLookupableHelperServiceImpl#getSearchResults(java.util.Map)
+     */
+    @Override
+    public List<? extends BusinessObject> getSearchResults(Map<String, String> fieldValues) {
+        // remove hidden fields
+        LookupUtils.removeHiddenCriteriaFields(getBusinessObjectClass(), fieldValues);
+
+        setBackLocation(fieldValues.get(KFSConstants.BACK_LOCATION));
+        setDocFormKey(fieldValues.get(KFSConstants.DOC_FORM_KEY));
+        setReferencesToRefresh(fieldValues.get(KFSConstants.REFERENCES_TO_REFRESH));
+
+        List searchResults = (List) getLookupService().findCollectionBySearchHelper(getBusinessObjectClass(), fieldValues, false);
+        // sort list if default sort column given
+        List defaultSortColumns = getDefaultSortColumns();
+        if (defaultSortColumns.size() > 0) {
+            Collections.sort(searchResults, new BeanPropertyComparator(getDefaultSortColumns(), true));
+        }
+        return searchResults;
+    }
+}

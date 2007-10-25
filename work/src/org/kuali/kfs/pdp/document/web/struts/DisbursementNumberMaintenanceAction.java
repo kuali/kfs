1bfcaa72da3e751cfe221234a786d02531c80e0a
Jay Sissom <jsissom@jsissom.com>
2007-10-25 22:35:48 +0000

    KULPDP-169

--- work/src/org/kuali/kfs/pdp/document/web/struts/DisbursementNumberMaintenanceAction.java
+++ work/src/org/kuali/kfs/pdp/document/web/struts/DisbursementNumberMaintenanceAction.java
@@ -41,7 +41,7 @@ public class DisbursementNumberMaintenanceAction extends BaseAction {
 
     protected boolean isAuthorized(ActionMapping mapping, ActionForm form, HttpServletRequest request,HttpServletResponse response) {
         SecurityRecord sr = getSecurityRecord(request);
-        return sr.isRangesRole();
+        return sr.isRangesRole() || sr.isSysAdminRole();
     }
 
     protected ActionForward executeLogic(ActionMapping mapping, ActionForm form,HttpServletRequest request, HttpServletResponse response) throws Exception {

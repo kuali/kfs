54b544cdbf87233ea5a347e1d5031809d1959212
Olga Kizler <olga.kizler@usc.edu>
2007-10-27 00:04:46 +0000

    KULPDP-163

---
+++ work/src/org/kuali/kfs/pdp/document/authorization/PayeeAchAccountDocumentAuthorizer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.pdp.document.authorization;
+
+import java.util.Map;
+
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.Document;
+import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizerBase;
+import org.kuali.kfs.context.SpringContext;
+import org.kuali.kfs.service.ParameterService;
+import org.kuali.module.pdp.PdpAuthorizationConstants;
+import org.kuali.module.pdp.PdpConstants;
+import org.kuali.module.pdp.bo.PayeeAchAccount;
+
+
+
+public class PayeeAchAccountDocumentAuthorizer extends MaintenanceDocumentAuthorizerBase {
+
+    /**
+     * If the current user is a member of KUALI_PDP_VIEWALL then
+     * add entry to the editMode Map and set it true.
+     *   
+     * @see org.kuali.core.document.MaintenanceDocumentAuthorizerBase#getEditMode(org.kuali.core.document.Document,
+     *      org.kuali.core.bo.user.KualiUser)
+     */
+    @Override
+    public Map getEditMode(Document document, UniversalUser user) {
+        Map editMode = super.getEditMode(document, user);
+        PayeeAchAccount vendor = (PayeeAchAccount) ((MaintenanceDocument) document).getNewMaintainableObject().getBusinessObject();
+        String viewAllWorkgroup = SpringContext.getBean(ParameterService.class).getParameterValue(PayeeAchAccount.class, PdpConstants.Groups.VIEWALL_GROUP);
+
+        if (user.isMember(viewAllWorkgroup)) {
+            editMode.put(PdpAuthorizationConstants.PDPEditMode.ENTRY, "TRUE");
+        }
+        
+        return editMode;
+    }
+}

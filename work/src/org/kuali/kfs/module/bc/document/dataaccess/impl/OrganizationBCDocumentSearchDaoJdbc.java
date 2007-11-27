24b11ee6594163128137d21f5e48985757737c4a
Gary	Palmer <gwpalmer@iu.edu>
2007-11-27 15:13:32 +0000

    adjusted to use StringBuffer append

--- work/src/org/kuali/kfs/module/bc/document/dataaccess/impl/OrganizationBCDocumentSearchDaoJdbc.java
+++ work/src/org/kuali/kfs/module/bc/document/dataaccess/impl/OrganizationBCDocumentSearchDaoJdbc.java
@@ -32,11 +32,48 @@ public class OrganizationBCDocumentSearchDaoJdbc extends BudgetConstructionDaoJd
 
         LOG.debug("buildAccountSelectPullList() started");
 
-        getSimpleJdbcTemplate().update(
-                "INSERT INTO ld_bcn_acctsel_t " + " (PERSON_UNVL_ID,UNIV_FISCAL_YR,FIN_COA_CD,ACCOUNT_NBR,SUB_ACCT_NBR,FDOC_NBR, " + "  ORG_LEVEL_CD,ORG_FIN_COA_CD,ORG_CD,FDOC_STATUS_CD,FDOC_CREATE_DT) " + "SELECT pull.person_unvl_id, head.univ_fiscal_yr, head.fin_coa_cd, head.account_nbr, head.sub_acct_nbr,head.fdoc_nbr, " + " head.org_level_cd, hier2.org_fin_coa_cd, hier2.org_cd, fphd.fdoc_status_cd, fphd.temp_doc_fnl_dt " + "FROM ld_bcn_pullup_t pull, ld_bcn_acct_org_hier_t hier,  ld_bcn_acct_org_hier_t hier2, " + "     ld_bcnstr_hdr_t head, fp_doc_header_t fphd " + "WHERE pull.pull_flag > 0 " + "  AND pull.person_unvl_id = ? " + "  AND hier.univ_fiscal_yr = ? " + "  AND hier.org_fin_coa_cd = pull.fin_coa_cd " + "  AND hier.org_cd = pull.org_cd " + "  AND hier2.univ_fiscal_yr = hier.univ_fiscal_yr " + "  AND hier2.fin_coa_cd = hier.fin_coa_cd " + "  AND hier2.account_nbr = hier.account_nbr " + "  AND head.univ_fiscal_yr = hier2.univ_fiscal_yr "
-                        + "  AND head.fin_coa_cd = hier2.fin_coa_cd " + "  AND head.account_nbr = hier2.account_nbr " + "  AND head.org_level_cd = hier2.org_level_cd " + "  AND fphd.fdoc_nbr = head.fdoc_nbr " + "UNION " + "SELECT pull.person_unvl_id, head.univ_fiscal_yr, head.fin_coa_cd, head.account_nbr, head.sub_acct_nbr, head.fdoc_nbr, " + " head.org_level_cd, hier2.org_fin_coa_cd, hier2.org_cd, fphd.fdoc_status_cd, fphd.temp_doc_fnl_dt " + "FROM ld_bcn_pullup_t pull, ld_bcn_acct_org_hier_t hier, ld_bcn_acct_org_hier_t hier2, " + "     ld_bcnstr_hdr_t head, fp_doc_header_t fphd " + "WHERE pull.pull_flag > 0 " + "  AND pull.person_unvl_id = ? " + "  AND hier.univ_fiscal_yr = ? " + "  AND hier.org_fin_coa_cd = pull.fin_coa_cd " + "  AND hier.org_cd = pull.org_cd " + "  AND hier2.univ_fiscal_yr = hier.univ_fiscal_yr " + "  AND hier2.fin_coa_cd = hier.fin_coa_cd " + "  AND hier2.account_nbr = hier.account_nbr " + "  AND hier2.org_level_cd = 1 "
-                        + "  AND head.univ_fiscal_yr = hier2.univ_fiscal_yr " + "  AND head.fin_coa_cd = hier2.fin_coa_cd " + "  AND head.account_nbr = hier2.account_nbr " + "  AND head.org_level_cd = 0 " + "  AND fphd.fdoc_nbr = head.fdoc_nbr", personUserIdentifier, universityFiscalYear, personUserIdentifier, universityFiscalYear);
+        StringBuffer sqlText = new StringBuffer(50);
+        sqlText.append("INSERT INTO ld_bcn_acctsel_t \n");
+        sqlText.append(" (PERSON_UNVL_ID,UNIV_FISCAL_YR,FIN_COA_CD,ACCOUNT_NBR,SUB_ACCT_NBR,FDOC_NBR, \n");
+        sqlText.append("  ORG_LEVEL_CD,ORG_FIN_COA_CD,ORG_CD,FDOC_STATUS_CD,FDOC_CREATE_DT) \n");
+        sqlText.append("SELECT pull.person_unvl_id, head.univ_fiscal_yr, head.fin_coa_cd, head.account_nbr, head.sub_acct_nbr,head.fdoc_nbr, \n");
+        sqlText.append(" head.org_level_cd, hier2.org_fin_coa_cd, hier2.org_cd, fphd.fdoc_status_cd, fphd.temp_doc_fnl_dt \n");
+        sqlText.append("FROM ld_bcn_pullup_t pull, ld_bcn_acct_org_hier_t hier,  ld_bcn_acct_org_hier_t hier2, \n");
+        sqlText.append("     ld_bcnstr_hdr_t head, fp_doc_header_t fphd \n");
+        sqlText.append("WHERE pull.pull_flag > 0 \n");
+        sqlText.append("  AND pull.person_unvl_id = ? \n");
+        sqlText.append("  AND hier.univ_fiscal_yr = ? \n");
+        sqlText.append("  AND hier.org_fin_coa_cd = pull.fin_coa_cd \n");
+        sqlText.append("  AND hier.org_cd = pull.org_cd \n");
+        sqlText.append("  AND hier2.univ_fiscal_yr = hier.univ_fiscal_yr \n");
+        sqlText.append("  AND hier2.fin_coa_cd = hier.fin_coa_cd \n");
+        sqlText.append("  AND hier2.account_nbr = hier.account_nbr \n");
+        sqlText.append("  AND head.univ_fiscal_yr = hier2.univ_fiscal_yr \n");
+        sqlText.append("  AND head.fin_coa_cd = hier2.fin_coa_cd \n");
+        sqlText.append("  AND head.account_nbr = hier2.account_nbr \n");
+        sqlText.append("  AND head.org_level_cd = hier2.org_level_cd \n");
+        sqlText.append("  AND fphd.fdoc_nbr = head.fdoc_nbr \n");
+        sqlText.append("UNION \n");
+        sqlText.append("SELECT pull.person_unvl_id, head.univ_fiscal_yr, head.fin_coa_cd, head.account_nbr, head.sub_acct_nbr, head.fdoc_nbr, \n");
+        sqlText.append(" head.org_level_cd, hier2.org_fin_coa_cd, hier2.org_cd, fphd.fdoc_status_cd, fphd.temp_doc_fnl_dt \n");
+        sqlText.append("FROM ld_bcn_pullup_t pull, ld_bcn_acct_org_hier_t hier, ld_bcn_acct_org_hier_t hier2, \n");
+        sqlText.append("     ld_bcnstr_hdr_t head, fp_doc_header_t fphd \n");
+        sqlText.append("WHERE pull.pull_flag > 0 \n");
+        sqlText.append("  AND pull.person_unvl_id = ? \n");
+        sqlText.append("  AND hier.univ_fiscal_yr = ? \n");
+        sqlText.append("  AND hier.org_fin_coa_cd = pull.fin_coa_cd \n");
+        sqlText.append("  AND hier.org_cd = pull.org_cd \n");
+        sqlText.append("  AND hier2.univ_fiscal_yr = hier.univ_fiscal_yr \n");
+        sqlText.append("  AND hier2.fin_coa_cd = hier.fin_coa_cd \n");
+        sqlText.append("  AND hier2.account_nbr = hier.account_nbr \n");
+        sqlText.append("  AND hier2.org_level_cd = 1 \n");
+        sqlText.append("  AND head.univ_fiscal_yr = hier2.univ_fiscal_yr \n");
+        sqlText.append("  AND head.fin_coa_cd = hier2.fin_coa_cd \n");
+        sqlText.append("  AND head.account_nbr = hier2.account_nbr \n");
+        sqlText.append("  AND head.org_level_cd = 0 \n");
+        sqlText.append("  AND fphd.fdoc_nbr = head.fdoc_nbr\n");
 
+        getSimpleJdbcTemplate().update(sqlText.toString(), personUserIdentifier, universityFiscalYear, personUserIdentifier, universityFiscalYear);
     }
 
     /**

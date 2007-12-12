247b75f473699f0a74c9de4169885b31c1c730aa
Gary	Palmer <gwpalmer@iu.edu>
2007-12-12 16:23:21 +0000

    changed to use StringBuilder and class level static vars to hold the sql text

--- work/src/org/kuali/kfs/module/bc/document/dataaccess/impl/BudgetPullupDaoJdbc.java
+++ work/src/org/kuali/kfs/module/bc/document/dataaccess/impl/BudgetPullupDaoJdbc.java
@@ -26,6 +26,46 @@ public class BudgetPullupDaoJdbc extends BudgetConstructionDaoJdbcBase implement
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(BudgetPullupDaoJdbc.class);
     private static final int MAXLEVEL = 50;
     
+    private static String[] initPointOfViewTemplates = new String[1];
+    private static String[] insertChildOrgTemplates = new String[2];
+    
+    @RawSQL
+    public BudgetPullupDaoJdbc() {
+        
+        super();
+        
+        StringBuilder sqlText = new StringBuilder(500);
+        sqlText.append("INSERT INTO ld_bcn_pullup_t \n");
+        sqlText.append(" (PERSON_UNVL_ID, FIN_COA_CD, ORG_CD, OBJ_ID, RPTS_TO_FIN_COA_CD, RPTS_TO_ORG_CD, PULL_FLAG) \n");
+        sqlText.append("SELECT ?, r.fin_coa_cd, r.org_cd, sys_guid(), r.rpts_to_fin_coa_cd, r.rpts_to_org_cd, ? \n");
+        sqlText.append("FROM ld_bcn_org_rpts_t r \n");
+        sqlText.append("WHERE fin_coa_cd = ? \n");
+        sqlText.append("  AND org_cd = ? \n");
+        initPointOfViewTemplates[0] = sqlText.toString();
+        sqlText.delete(0, sqlText.length()-1);
+        
+        sqlText.append("INSERT INTO ld_bcn_pullup_t \n");
+        sqlText.append(" (PERSON_UNVL_ID, FIN_COA_CD, ORG_CD, OBJ_ID, RPTS_TO_FIN_COA_CD, RPTS_TO_ORG_CD, PULL_FLAG) \n");
+        sqlText.append("SELECT ?, r.fin_coa_cd, r.org_cd, sys_guid(), r.rpts_to_fin_coa_cd, r.rpts_to_org_cd, ? \n");
+        sqlText.append("FROM ld_bcn_org_rpts_t r, ld_bcn_pullup_t p, ca_org_t o \n");
+        sqlText.append("WHERE p.person_unvl_id = ? \n");
+        sqlText.append("  AND p.pull_flag = ? \n");
+        sqlText.append("  AND p.fin_coa_cd = r.rpts_to_fin_coa_cd \n");
+        sqlText.append("  AND p.org_cd = r.rpts_to_org_cd \n");
+        sqlText.append("  AND not (r.fin_coa_cd = r.rpts_to_fin_coa_cd and r.org_cd = r.rpts_to_org_cd)");
+        sqlText.append("  AND o.fin_coa_cd = r.fin_coa_cd \n");
+        sqlText.append("  AND o.org_cd = r.org_cd \n");
+        sqlText.append("  AND o.org_active_cd = 'Y' \n");
+        insertChildOrgTemplates[0] = sqlText.toString();
+        sqlText.delete(0, sqlText.length()-1);
+        
+        sqlText.append("UPDATE ld_bcn_pullup_t \n");
+        sqlText.append("SET pull_flag = 0 \n");
+        sqlText.append("WHERE person_unvl_id = ? \n");
+        insertChildOrgTemplates[1] = sqlText.toString();
+
+    }
+    
     /**
      * This method initializes and inserts the root organization using raw SQL.
      * 
@@ -33,19 +73,11 @@ public class BudgetPullupDaoJdbc extends BudgetConstructionDaoJdbcBase implement
      */
     @RawSQL
     public void initPointOfView(String personUserIdentifier, String chartOfAccountsCode, String organizationCode, int currentLevel) {
-    
+        //TODO remove OBJ_ID from the table and here before implementing any calls to this method
+   
         LOG.debug("initPointOfView() called");
         
-        //TODO remove OBJ_ID from the table and here before implementing any calls to this method
-        StringBuffer sqlText = new StringBuffer(50);
-        sqlText.append("INSERT INTO ld_bcn_pullup_t \n");
-        sqlText.append(" (PERSON_UNVL_ID, FIN_COA_CD, ORG_CD, OBJ_ID, RPTS_TO_FIN_COA_CD, RPTS_TO_ORG_CD, PULL_FLAG) \n");
-        sqlText.append("SELECT ?, r.fin_coa_cd, r.org_cd, sys_guid(), r.rpts_to_fin_coa_cd, r.rpts_to_org_cd, ? \n");
-        sqlText.append("FROM ld_bcn_org_rpts_t r \n");
-        sqlText.append("WHERE fin_coa_cd = ? \n");
-        sqlText.append("  AND org_cd = ? \n");
-        
-        getSimpleJdbcTemplate().update(sqlText.toString(), personUserIdentifier, currentLevel, chartOfAccountsCode, organizationCode);
+        getSimpleJdbcTemplate().update(initPointOfViewTemplates[0], personUserIdentifier, currentLevel, chartOfAccountsCode, organizationCode);
     }
 
     /**
@@ -55,40 +87,21 @@ public class BudgetPullupDaoJdbc extends BudgetConstructionDaoJdbcBase implement
      */
     @RawSQL
     public void insertChildOrgs(String personUserIdentifier, int previousLevel) {
+        //TODO remove OBJ_ID from the table and here before implementing any calls to this method
         
         LOG.debug("insertChildOrgs() called");
 
         if (previousLevel <= MAXLEVEL) {
             int currentLevel = previousLevel + 1;
 
-            //TODO remove OBJ_ID from the table and here before implementing any calls to this method
             // insert the children of the organizations at the current level for the user
             // and then recursively call on the new level
-            StringBuffer sqlText = new StringBuffer(50);
-            sqlText.append("INSERT INTO ld_bcn_pullup_t \n");
-            sqlText.append(" (PERSON_UNVL_ID, FIN_COA_CD, ORG_CD, OBJ_ID, RPTS_TO_FIN_COA_CD, RPTS_TO_ORG_CD, PULL_FLAG) \n");
-            sqlText.append("SELECT ?, r.fin_coa_cd, r.org_cd, sys_guid(), r.rpts_to_fin_coa_cd, r.rpts_to_org_cd, ? \n");
-            sqlText.append("FROM ld_bcn_org_rpts_t r, ld_bcn_pullup_t p, ca_org_t o \n");
-            sqlText.append("WHERE p.person_unvl_id = ? \n");
-            sqlText.append("  AND p.pull_flag = ? \n");
-            sqlText.append("  AND p.fin_coa_cd = r.rpts_to_fin_coa_cd \n");
-            sqlText.append("  AND p.org_cd = r.rpts_to_org_cd \n");
-            sqlText.append("  AND not (r.fin_coa_cd = r.rpts_to_fin_coa_cd and r.org_cd = r.rpts_to_org_cd)");
-            sqlText.append("  AND o.fin_coa_cd = r.fin_coa_cd \n");
-            sqlText.append("  AND o.org_cd = r.org_cd \n");
-            sqlText.append("  AND o.org_active_cd = 'Y' \n");
-
-            int rowsAffected = getSimpleJdbcTemplate().update(sqlText.toString(), personUserIdentifier, currentLevel, personUserIdentifier, previousLevel);
+            int rowsAffected = getSimpleJdbcTemplate().update(insertChildOrgTemplates[0], personUserIdentifier, currentLevel, personUserIdentifier, previousLevel);
             if (rowsAffected > 0) {
                 insertChildOrgs(personUserIdentifier, currentLevel);
             } else {
                 // cleanup by resetting the pull_flag to zero for all
-                sqlText = new StringBuffer(50);
-                sqlText.append("UPDATE ld_bcn_pullup_t \n");
-                sqlText.append("SET pull_flag = 0 \n");
-                sqlText.append("WHERE person_unvl_id = ? \n");
-                
-                getSimpleJdbcTemplate().update(sqlText.toString(), personUserIdentifier);
+                getSimpleJdbcTemplate().update(insertChildOrgTemplates[1], personUserIdentifier);
             }
         } else {
             // overrun problem

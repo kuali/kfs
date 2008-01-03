70e2a76fb2e64f87ad81752852a853eec7abe2b7
Venkat PremChandran <vpremcha@kuali.org>
2008-01-03 18:13:49 +0000

    Fixes for - KULPURAP-1516, 2215, 1519, 1520 and 1521

--- work/src/org/kuali/kfs/module/purap/businessobject/PurchaseOrderQuoteStatus.java
+++ work/src/org/kuali/kfs/module/purap/businessobject/PurchaseOrderQuoteStatus.java
@@ -23,54 +23,16 @@ import org.kuali.core.bo.PersistableBusinessObjectBase;
 /**
  * Purchase Order Quote Status Business Object.
  * 
- * THIS CODE IS NOT USED IN RELEASE 2 BUT THE CODE WAS LEFT IN TO
- * FACILITATE TURNING IT BACK ON EARLY IN THE DEVELOPMENT CYCLE OF RELEASE 3.
- * 
  */
-public class PurchaseOrderQuoteStatus extends PersistableBusinessObjectBase {
-
-    private String purchaseOrderQuoteStatusCode;
-    private String purchaseOrderQuoteStatusDescription;
-    private boolean active;
+public class PurchaseOrderQuoteStatus extends Status {
 
     /**
      * Default constructor.
      */
     public PurchaseOrderQuoteStatus() {
-
+        super();
+        super.ojbConcreteClass = this.getClass().getName();
     }
 
-    public String getPurchaseOrderQuoteStatusCode() {
-        return purchaseOrderQuoteStatusCode;
-    }
-
-    public void setPurchaseOrderQuoteStatusCode(String purchaseOrderQuoteStatusCode) {
-        this.purchaseOrderQuoteStatusCode = purchaseOrderQuoteStatusCode;
-    }
-
-    public String getPurchaseOrderQuoteStatusDescription() {
-        return purchaseOrderQuoteStatusDescription;
-    }
-
-    public void setPurchaseOrderQuoteStatusDescription(String purchaseOrderQuoteStatusDescription) {
-        this.purchaseOrderQuoteStatusDescription = purchaseOrderQuoteStatusDescription;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    /**
-     * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
-     */
-    protected LinkedHashMap toStringMapper() {
-        LinkedHashMap m = new LinkedHashMap();
-        m.put("purchaseOrderQuoteStatusCode", this.purchaseOrderQuoteStatusCode);
-        return m;
-    }
-
+    
 }

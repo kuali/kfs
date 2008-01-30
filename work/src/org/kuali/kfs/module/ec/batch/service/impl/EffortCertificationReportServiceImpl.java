53817695ff4a6aef84be4137b51b575413612ba0
Bin Gao <gaob@msu.edu>
2008-01-30 19:56:47 +0000

    Report generator with Jasper;

--- work/src/org/kuali/kfs/module/ec/batch/service/impl/EffortCertificationReportServiceImpl.java
+++ work/src/org/kuali/kfs/module/ec/batch/service/impl/EffortCertificationReportServiceImpl.java
@@ -18,6 +18,11 @@ package org.kuali.module.effort.service.impl;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import net.sf.jasperreports.engine.JRParameter;
 
 import org.kuali.module.effort.service.EffortCertificationReportService;
 import org.kuali.module.effort.util.EffortReportRegistry;
@@ -29,35 +34,33 @@ import org.kuali.module.effort.util.ReportGenerator;
  */
 public class EffortCertificationReportServiceImpl implements EffortCertificationReportService {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(EffortCertificationReportServiceImpl.class);
-    public static final String PDF_FILE_EXTENSION = "pdf";
 
     /**
      * @see org.kuali.module.effort.service.EffortCertificationReportService#generate(org.kuali.module.effort.util.ExtractProcessReportDataHolder,
      *      org.kuali.module.effort.util.EffortReportRegistry, java.lang.String, java.util.Date)
      */
     public void generateReportForExtractProcess(ExtractProcessReportDataHolder reportDataHolder, EffortReportRegistry reportInfo, String reportsDirectory, Date runDate) {
-        String fileNamePrefix = reportInfo.reportFilename();
         String template = "report/" + reportInfo.getReportTemplateName();
-        String fullReportFileName = this.buildFullFileName(reportsDirectory, fileNamePrefix, PDF_FILE_EXTENSION, runDate);
+        String fullReportFileName = this.buildFullFileName(reportsDirectory, reportInfo.reportFilename(), runDate);
+               
+        Map<String, Object> reportData = reportDataHolder.getReportData();
 
         LOG.info(reportDataHolder);
-
-        //ReportGenerator.generateReportToPdfFile(reportDataHolder.getReportData(), template, fullReportFileName);
+        //ReportGenerator.generateReportToPdfFile(reportData, template, fullReportFileName);
     }
 
     /**
      * build a full file name with the given information
      * 
      * @param directory the directory where the file would be located
-     * @param fileNamePrefix the prefix of the file name
-     * @param fileNameExtension the extension of the file name
+     * @param fileName the file name
      * @param runDate the run date
      * @return a full file name built from the given information
      */
-    private String buildFullFileName(String directory, String fileNamePrefix, String fileNameExtension, Date runDate) {
+    private String buildFullFileName(String directory, String fileName, Date runDate) {
         String sdf = new SimpleDateFormat("yyyyMMdd_HHmmss").format(runDate);
-        String fileNamePattern = "{0}/{1}.{2}";
+        String fileNamePattern = "{0}/{1}";
 
-        return MessageFormat.format(fileNamePattern, directory, fileNamePrefix + sdf, fileNameExtension);
+        return MessageFormat.format(fileNamePattern, directory, fileName + sdf);
     }
 }

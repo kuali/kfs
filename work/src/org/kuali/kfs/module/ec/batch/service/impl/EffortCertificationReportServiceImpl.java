afc63b1b00482cf77d67124558c1a9f9dfc93481
Bin Gao <gaob@msu.edu>
2008-02-08 18:07:45 +0000

    JasperReport for Effort Certification

--- work/src/org/kuali/kfs/module/ec/batch/service/impl/EffortCertificationReportServiceImpl.java
+++ work/src/org/kuali/kfs/module/ec/batch/service/impl/EffortCertificationReportServiceImpl.java
@@ -51,7 +51,7 @@ public class EffortCertificationReportServiceImpl implements EffortCertification
 
         Map<String, Object> reportData = reportDataHolder.getReportData();
         reportData.put(JRParameter.REPORT_RESOURCE_BUNDLE, this.getResourceBundle());
-        reportData.put("SUBREPORT_DIR", reportTemplateClassPath);
+        reportData.put(ReportGenerator.PARAMETER_NAME_SUBREPORT_DIR, reportTemplateClassPath);
 
         ReportGenerator.generateReportToPdfFile(reportData, template, fullReportFileName);
 

1e2d0bd365c25bad06f7ab7676dfb47b7f29565b
Sewit Adams <sewit.adams@colostate.edu>
2008-02-13 16:19:03 +0000

    KULEFR-42

---
+++ work/src/org/kuali/kfs/module/ec/businessobject/options/PeriodStatusCodeValuesFinder.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.effort.lookup.keyvalues;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.core.lookup.keyvalues.KeyValuesBase;
+import org.kuali.core.web.ui.KeyLabelPair;
+
+/**
+ * returns valid effort reporting fiscal periods 1 - 12
+ */
+public class PeriodStatusCodeValuesFinder extends KeyValuesBase {
+
+    /*
+     * @see org.kuali.keyvalues.KeyValuesFinder#getKeyValues()
+     */
+    public List getKeyValues() {
+        List keyValues = new ArrayList();
+        //keyValues.add(new KeyLabelPair(" ", " "));
+        keyValues.add(new KeyLabelPair("N", "N"));
+        keyValues.add(new KeyLabelPair("O", "O"));
+        keyValues.add(new KeyLabelPair("C", "C"));
+        
+        return keyValues;
+    }
+}

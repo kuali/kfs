d840b438498ed382a40c9f96dc7006dc3f84ee36
Sewit Adams <sewit.adams@colostate.edu>
2008-02-15 22:20:28 +0000

    KULEFR-44

--- work/src/org/kuali/kfs/module/ec/document/web/struts/EffortCertificationAction.java
+++ work/src/org/kuali/kfs/module/ec/document/web/struts/EffortCertificationAction.java
@@ -15,32 +15,33 @@
  */
 package org.kuali.module.effort.web.struts.action;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.kuali.core.service.BusinessObjectService;
 import org.kuali.core.web.struts.action.KualiTransactionalDocumentActionBase;
 import org.kuali.kfs.KFSConstants;
-import org.kuali.kfs.context.SpringContext;
 import org.kuali.module.effort.document.EffortCertificationDocument;
+import org.kuali.module.effort.web.struts.form.EffortCertificationForm;
 
 /**
- * This class handles Actions for InternalBilling.
+ * This class handles Actions for EffortCertification.
  */
 public class EffortCertificationAction extends KualiTransactionalDocumentActionBase {
     
     public ActionForward initiate(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         
+        
         return mapping.findForward(KFSConstants.MAPPING_BASIC);
     }
         
     public ActionForward recalculate(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        
+        EffortCertificationForm effortForm = (EffortCertificationForm) form;
+        EffortCertificationDocument document = (EffortCertificationDocument) effortForm.getDocument();
+        //document.getEffortCertificationDetailLines().get();
+        //EffortCertificationDetail document.g
         return mapping.findForward(KFSConstants.MAPPING_BASIC);
     }
     
@@ -53,4 +54,15 @@ public class EffortCertificationAction extends KualiTransactionalDocumentActionB
         
         return mapping.findForward(KFSConstants.MAPPING_BASIC);
     }
+
+    /*@Override
+    public ActionForward docHandler(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // TODO Auto-generated method stub
+        super.docHandler(mapping, form, request, response);
+        
+        
+        return mapping.findForward(KFSConstants.MAPPING_BASIC);
+    }*/
+    
+    
 }

4e8fb2ff5a8afab09178713230b2acc5872071f0
Dan Lemus <dlemus@vivantech.com>
2008-02-27 00:55:52 +0000

    KULPURAP-2306 carrier finder class added

---
+++ work/src/org/kuali/kfs/module/purap/businessobject/options/CarrierValuesFinder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.purap.lookup.keyvalues;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.kuali.core.lookup.keyvalues.KeyValuesBase;
+import org.kuali.core.service.KeyValuesService;
+import org.kuali.core.web.ui.KeyLabelPair;
+import org.kuali.kfs.context.SpringContext;
+import org.kuali.module.purap.bo.Carrier;
+
+/**
+ * Value Finder for Carrier.
+ */
+public class CarrierValuesFinder extends KeyValuesBase {
+
+    /**
+     * Returns code/description pairs of all Carriers.
+     * 
+     * @see org.kuali.core.lookup.keyvalues.KeyValuesFinder#getKeyValues()
+     */
+    public List getKeyValues() {
+        KeyValuesService boService = SpringContext.getBean(KeyValuesService.class);
+        Collection codes = boService.findAll(Carrier.class);
+        List labels = new ArrayList();
+        labels.add(new KeyLabelPair("", ""));
+        for (Iterator iter = codes.iterator(); iter.hasNext();) {
+            Carrier car = (Carrier) iter.next();
+            labels.add(new KeyLabelPair(car.getCarrierCode(), car.getCarrierDescription()));
+        }
+        return labels;
+    }
+}

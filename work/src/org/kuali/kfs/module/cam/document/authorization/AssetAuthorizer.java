c14f0e3f50a4f5bbda0fb80b05db82a4cbeb256e
Naser Alavi <alavinas@ais.msu.edu>
2008-03-05 19:40:27 +0000

    KULCAP-56 - Asset Authorization prrof of cocept basic code

--- work/src/org/kuali/kfs/module/cam/document/authorization/AssetAuthorizer.java
+++ work/src/org/kuali/kfs/module/cam/document/authorization/AssetAuthorizer.java
@@ -15,11 +15,55 @@
  */
 package org.kuali.module.cams.document.authorization;
 
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizations;
 import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizerBase;
+import org.kuali.kfs.context.SpringContext;
+import org.kuali.kfs.service.ParameterService;
+import org.kuali.kfs.service.impl.ParameterConstants;
+import org.kuali.module.cams.CamsConstants;
+import org.kuali.module.cams.bo.Asset;
 
 /**
  * AssetAuthorizer for Asset edit.
  */
 public class AssetAuthorizer extends MaintenanceDocumentAuthorizerBase {
 
+   // private static final Logger LOG = Logger.getLogger(AssetEditAuthorizer.class);
+
+    /**
+     * Returns the set of authorization restrictions (if any) that apply to this Asset in this context.
+     * 
+     * @param document
+     * @param user
+     * @return a new set of {@link MaintenanceDocumentAuthorizations} that marks certain fields as necessary
+     */
+    public MaintenanceDocumentAuthorizations getFieldAuthorizations(MaintenanceDocument document, UniversalUser user) {
+
+        MaintenanceDocumentAuthorizations auths = new MaintenanceDocumentAuthorizations();
+
+        // do addHiddenAuthField
+        Asset asset = (Asset) document.getNewMaintainableObject().getBusinessObject();
+        //TODO: we can break this nicely to three or more method calls  based on different workgroups, instad of doing it in one method: 
+        setAssetFieldsAuthorization(asset, auths, user);
+        return auths;
+    }
+    
+    /**
+     * Sets the asset fields to be read only if the current user is not a member of
+     * kuali-Assetmaintainers workgroup.
+     * 
+     * @param asset an instance of Asset document
+     * @param auths an instance of MaintenanceDocumentAuthorizations which is used to define the read only fields
+     * @param user current logged-in user
+     */
+    private void setAssetFieldsAuthorization(Asset asset, MaintenanceDocumentAuthorizations auths, UniversalUser user) {
+       // String camsAdministrotorsWorkgroup = SpringContext.getBean(ParameterService.class).getParameterValue(ParameterConstants.CAPITAL_ASSETS_DOCUMENT.class, CamsConstants.Workgroups.WORKGROUP_CM_ADMINISTRATORS);
+        if (user.isMember(CamsConstants.Workgroups.WORKGROUP_CM_ADMINISTRATORS)) {
+            //Just an Example for proof of concept
+            auths.addReadonlyAuthField("conditionCode");
+        }
+    }
+    
 }

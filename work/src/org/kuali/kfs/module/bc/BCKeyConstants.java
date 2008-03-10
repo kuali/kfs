37df4d6d1fdd1b59eb0399b8883adb4b97f181d8
Shawn Choo <schoo@vivantech.com>
2008-03-10 00:39:42 +0000

    Added some key constatns  for BC Report

--- work/src/org/kuali/kfs/module/bc/BCKeyConstants.java
+++ work/src/org/kuali/kfs/module/bc/BCKeyConstants.java
@@ -22,4 +22,25 @@ public class BCKeyConstants {
     public static final String MESSAGE_BUDGET_SUCCESSFUL_CLOSE = "message.budget.successfulClose";
     public static final String ERROR_BUDGET_ORG_NOT_SELECTED = "error.budget.orgNotSelected";
     public static final String ERROR_BUDGET_LINE_EXISTS = "error.budget.lineExists";
+    
+    public static final String ERROR_REPORT_GETTING_CHART_DESCRIPTION = "error.budget.report.gettingChartDescription";
+    public static final String ERROR_REPORT_GETTING_ACCOUNT_DESCRIPTION = "error.budget.report.gettingAccountDescription";
+    public static final String ERROR_REPORT_GETTING_SUB_ACCOUNT_DESCRIPTION = "error.budget.report.gettingSubAccountDescription";
+    public static final String ERROR_REPORT_GETTING_ORGANIZATION_NAME = "error.budget.report.gettingOrganizationName";
+    public static final String ERROR_REPORT_GETTING_FUNDGROUP_NAME = "error.budget.report.gettingFundGroupName";
+    public static final String ERROR_REPORT_GETTING_SUBFUNDGROUP_DESCRIPTION = "error.budget.report.gettingSubFundGroupDescription";
+    public static final String MSG_REPORT_HEADER_ACCOUNT_SUB = "message.budget.report.header.accountSub";
+    public static final String MSG_REPORT_HEADER_ACCOUNT_SUB_NAME = "message.budget.report.header.accountSubName"; 
+    public static final String MSG_REPORT_HEADER_BASE_AMOUNT = "message.budget.report.header.baseAmount";
+    public static final String MSG_REPORT_HEADER_REQ_AMOUNT = "message.budget.report.header.reqAmount";
+    public static final String MSG_REPORT_HEADER_CHANGE = "message.budget.report.header.change";
+    public static final String MSG_REPORT_INCOME_EXP_DESC_REVENUE = "message.budget.report.incomeExpDescRevenue";
+    public static final String MSG_REPORT_INCOME_EXP_DESC_EXP_GROSS = "message.budget.report.incomeExpDescExpGross";
+    public static final String MSG_REPORT_INCOME_EXP_DESC_TRNFR_IN = "message.budget.report.incomeExpDescTrnfrIn";
+    public static final String MSG_REPORT_INCOME_EXP_DESC_EXP_NET_TRNFR = "message.budget.report.incomeExpDescExpNetTrnfr";
+    public static final String MSG_REPORT_INCOME_EXP_DESC_EXPENDITURE = "message.budget.report.incomeExpDescExpenditure";
+    
+    public static final String MSG_REPORT_HEADER_SUBFUND = "message.budget.report.header.subFund";
+    public static final String MSG_REPORT_HEADER_SUBFUND_DESCRIPTION = "message.budget.report.header.subFundDescription";
 }
+

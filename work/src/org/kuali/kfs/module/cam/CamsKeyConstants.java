ee56aa1995ae3a1b86d560ceb908ca4aad762737
Harsha Ummerpillai <harsha07@msu.edu>
2008-03-20 14:25:35 +0000

    KULCAP-201 - Remove actions if asset is retired

--- work/src/org/kuali/kfs/module/cam/CamsKeyConstants.java
+++ work/src/org/kuali/kfs/module/cam/CamsKeyConstants.java
@@ -26,24 +26,24 @@ public class CamsKeyConstants {
     public static final String ERROR_PRE_TAG_NUMBER = "error.invalid.pre.tag.number";
     public static final String ERROR_PRE_TAG_DETAIL_EXCESS = "error.pre.tag.detail.excess";
     public static final String ERROR_NO_DETAIL_LINE = "error.invalid.no.detail.line";
-    
+
     public static final String MESSAGE_BATCH_UPLOAD_TITLE_PRE_ASSET_TAGGING = "message.batchUpload.title.pre.asset.tagging";
     public static final String ERROR_INVALID_ASSET_WARRANTY_NO = "error.invalid.asset.warranty.no";
-        
+
     public static class Depreciation {
-        public static final String NO_ELIGIBLE_FOR_DEPRECIATION_ASSETS_FOUND    = "error.batch.depreciation.assetsNotFound";
-        public static final String ERROR_WHEN_CALCULATING_BASE_AMOUNT           = "error.batch.depreciation.baseAmountCalculationError";
-        public static final String ERROR_WHEN_CALCULATING_DEPRECIATION          = "error.batch.depreciation.calculationError";
-        public static final String ERROR_WHEN_UPDATING_GL_PENDING_ENTRY_TABLE   = "error.batch.depreciation.glpeUpdateError";        
-        public static final String DEPRECIATION_DATE_PARAMETER_NOT_FOUND        = "error.batch.depreciation.depreciationDateNotFound";
-        public static final String INVALID_DEPRECIATION_DATE_FORMAT             = "error.batch.depreciation.invalidDepreciationDateFormat";
-        public static final String DEPRECIATION_ALREADY_RAN_MSG                 = "error.batch.depreciation.alreadyRan";
-        public static final String ERROR_WHEN_UPDATING_DOCUMENT_HEADER_TABLE    = "error.batch.depreciation.documentHeaderUpdateError";        
-                
-        public static final String MSG_REPORT_DEPRECIATION_HEADING1   = "message.batch.report.depreciation.heading.description";
-        public static final String MSG_REPORT_DEPRECIATION_HEADING2   = "message.batch.report.depreciation.heading.figures";        
+        public static final String NO_ELIGIBLE_FOR_DEPRECIATION_ASSETS_FOUND = "error.batch.depreciation.assetsNotFound";
+        public static final String ERROR_WHEN_CALCULATING_BASE_AMOUNT = "error.batch.depreciation.baseAmountCalculationError";
+        public static final String ERROR_WHEN_CALCULATING_DEPRECIATION = "error.batch.depreciation.calculationError";
+        public static final String ERROR_WHEN_UPDATING_GL_PENDING_ENTRY_TABLE = "error.batch.depreciation.glpeUpdateError";
+        public static final String DEPRECIATION_DATE_PARAMETER_NOT_FOUND = "error.batch.depreciation.depreciationDateNotFound";
+        public static final String INVALID_DEPRECIATION_DATE_FORMAT = "error.batch.depreciation.invalidDepreciationDateFormat";
+        public static final String DEPRECIATION_ALREADY_RAN_MSG = "error.batch.depreciation.alreadyRan";
+        public static final String ERROR_WHEN_UPDATING_DOCUMENT_HEADER_TABLE = "error.batch.depreciation.documentHeaderUpdateError";
+
+        public static final String MSG_REPORT_DEPRECIATION_HEADING1 = "message.batch.report.depreciation.heading.description";
+        public static final String MSG_REPORT_DEPRECIATION_HEADING2 = "message.batch.report.depreciation.heading.figures";
     }
-    
+
     public static final String ERROR_INVALID_ASSET_CAMPUS_CODE = "error.invalid.asset.campus.code";
     public static final String ERROR_INVALID_ASSET_BUILDING_CODE = "error.invalid.asset.building.code";
     public static final String ERROR_MANDATORY_ASSET_BUILDING_CODE = "error.mandatory.asset.building.code";
@@ -55,5 +55,7 @@ public class CamsKeyConstants {
     public static final String ERROR_ASSET_TYPE_CODE_RESTRICT_CHANGE = "error.asset.type.code.restrict.change";
     public static final String ERROR_ASSET_DESCRIPTION_RESTRICT_CHANGE = "error.asset.description.restrict.change";
     public static final String ERROR_INVALID_ASSET_STATUS_CHANGE = "error.invalid.asset.status.change";
-    
+    public static final String ERROR_ASSET_RETIRED_NOEDIT = "error.asset.retired.noedit";
+
+
 }
--- work/src/org/kuali/kfs/module/cam/document/authorization/AssetAuthorizer.java
+++ work/src/org/kuali/kfs/module/cam/document/authorization/AssetAuthorizer.java
@@ -15,8 +15,10 @@
  */
 package org.kuali.module.cams.document.authorization;
 
+import static org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase.MAINTAINABLE_ERROR_PREFIX;
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.AGENCY_NUMBER;
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.ASSET_DATE_OF_SERVICE;
+import static org.kuali.module.cams.CamsPropertyConstants.Asset.ASSET_INVENTORY_STATUS;
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.GOVERNMENT_TAG_NUMBER;
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.NATIONAL_STOCK_NUMBER;
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.OLD_TAG_NUMBER;
@@ -24,17 +26,23 @@ import static org.kuali.module.cams.CamsPropertyConstants.Asset.ORGANIZATION_OWN
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.ORGANIZATION_OWNER_CHART_OF_ACCOUNTS_CODE;
 import static org.kuali.module.cams.CamsPropertyConstants.Asset.VENDOR_NAME;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.Document;
 import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.document.authorization.DocumentActionFlags;
 import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizations;
 import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizerBase;
+import org.kuali.core.util.GlobalVariables;
 import org.kuali.module.cams.CamsConstants;
+import org.kuali.module.cams.CamsKeyConstants;
 import org.kuali.module.cams.bo.Asset;
 
 /**
  * AssetAuthorizer for Asset edit.
  */
 public class AssetAuthorizer extends MaintenanceDocumentAuthorizerBase {
+    private static final String[] RETIRED_INV_CODES = new String[] { "O", "R", "E" };
     private static String[] DEFAULT_READONLY_FIELDS = new String[] { ORGANIZATION_OWNER_ACCOUNT_NUMBER, ORGANIZATION_OWNER_CHART_OF_ACCOUNTS_CODE, AGENCY_NUMBER, VENDOR_NAME, OLD_TAG_NUMBER, GOVERNMENT_TAG_NUMBER, NATIONAL_STOCK_NUMBER, ASSET_DATE_OF_SERVICE };
     private static final String[] CM_ASSET_MERGE_SEPARATE_USERS_READONLY_FIELDS = new String[] { ORGANIZATION_OWNER_ACCOUNT_NUMBER, VENDOR_NAME, ASSET_DATE_OF_SERVICE };
     private static String[] CM_SUPER_USERS_READONLY_FIELDS = new String[] {};
@@ -48,7 +56,7 @@ public class AssetAuthorizer extends MaintenanceDocumentAuthorizerBase {
      * @return a new set of {@link MaintenanceDocumentAuthorizations} that marks certain fields as necessary
      */
     public MaintenanceDocumentAuthorizations getFieldAuthorizations(MaintenanceDocument document, UniversalUser user) {
-        MaintenanceDocumentAuthorizations auths = new MaintenanceDocumentAuthorizations();
+        MaintenanceDocumentAuthorizations auths = super.getFieldAuthorizations(document, user);
         Asset asset = (Asset) document.getNewMaintainableObject().getBusinessObject();
         if (user.isMember(CamsConstants.Workgroups.WORKGROUP_CM_SUPER_USERS)) {
             setReadonlyAuthorizations(auths, CM_SUPER_USERS_READONLY_FIELDS);
@@ -64,8 +72,25 @@ public class AssetAuthorizer extends MaintenanceDocumentAuthorizerBase {
 
     private void setReadonlyAuthorizations(MaintenanceDocumentAuthorizations auths, String[] readOnlyFields) {
         for (String field : readOnlyFields) {
-            auths.addReadonlyAuthField(field);
+            auths.addHiddenAuthField(field);
         }
+    }
+
+    @Override
+    public DocumentActionFlags getDocumentActionFlags(Document document, UniversalUser user) {
+        DocumentActionFlags actionFlags = super.getDocumentActionFlags(document, user);
+
+        // If asset is retired then deny "Save", "Submit" and "Approve"
+        Asset asset = (Asset) document.getDocumentBusinessObject();
+        if (ArrayUtils.contains(RETIRED_INV_CODES, asset.getInventoryStatusCode())) {
+            actionFlags.setCanAdHocRoute(false);
+            actionFlags.setCanApprove(false);
+            actionFlags.setCanBlanketApprove(false);
+            actionFlags.setCanRoute(false);
+            actionFlags.setCanSave(false);
+            GlobalVariables.getErrorMap().putError(MAINTAINABLE_ERROR_PREFIX + ASSET_INVENTORY_STATUS, CamsKeyConstants.ERROR_ASSET_RETIRED_NOEDIT, new String[] {});
+        }
+        return actionFlags;
     }
 
 }

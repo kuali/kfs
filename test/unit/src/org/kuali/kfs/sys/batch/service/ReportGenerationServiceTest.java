f53a2343f061cd6ef8d35bdd73a9eeca400036a6
Bin Gao <gaob@msu.edu>
2008-03-21 14:34:15 +0000

    1) backup the code for effort certification document; 2) KULEFR-85: Move Report Generator to KFS and make into a service

--- test/unit/src/org/kuali/kfs/sys/batch/service/ReportGenerationServiceTest.java
+++ test/unit/src/org/kuali/kfs/sys/batch/service/ReportGenerationServiceTest.java
@@ -15,108 +15,114 @@
  */
 package org.kuali.kfs.service;
 
-import static org.kuali.test.fixtures.AccountFixture.ACTIVE_ACCOUNT;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
 import net.sf.jasperreports.engine.JRParameter;
 
-import org.kuali.core.service.KualiConfigurationService;
-import org.kuali.kfs.KFSConstants;
 import org.kuali.kfs.KFSConstants.ReportGeneration;
 import org.kuali.kfs.context.KualiTestBase;
 import org.kuali.kfs.context.SpringContext;
+import org.kuali.kfs.util.ReportInfo;
 import org.kuali.module.chart.bo.Account;
 import org.kuali.module.chart.bo.SubAccount;
-import org.kuali.module.effort.bo.EffortCertificationReportDefinition;
-import org.kuali.module.effort.util.ExtractProcessReportDataHolder;
 import org.kuali.test.ConfigureContext;
 import org.kuali.test.fixtures.AccountFixture;
 import org.kuali.test.fixtures.SubAccountFixture;
-import org.springframework.core.io.ClassPathResource;
 
 @ConfigureContext
 public class ReportGenerationServiceTest extends KualiTestBase {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ReportGenerationServiceTest.class);
 
     private ReportGenerationService reportGenerationService;
-    private String reportDirectoty;
-    
-    private String reportTemplateClassPath = "org/kuali/kfs/report/";
-    private String resourceBundleBaseName = reportTemplateClassPath + "ReportGenerationService";
+    private ReportInfo infoForParameterMapReport;
+    private ReportInfo infoForDataSourceReport;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
         reportGenerationService = SpringContext.getBean(ReportGenerationService.class);
-        reportDirectoty = SpringContext.getBean(KualiConfigurationService.class).getPropertyString(KFSConstants.REPORTS_DIRECTORY_KEY);
+
+        Map<String, ReportInfo> reportInfoBeans = SpringContext.getBeansOfType(ReportInfo.class);
+        infoForParameterMapReport = reportInfoBeans.get("infoForParameterMapReport");
+        infoForDataSourceReport = reportInfoBeans.get("infoForDataSourceReport");
     }
-    
+
     // the report data are hold by data source, which can be an instance of java.util.Collection, an object array or JRDataSource
     public void testGenerateReportToPdfFile_WithDataSource() throws Exception {
-        String fullReportFileName = reportDirectoty + "/testGenerateReportToPdfFileWithDataSource";
-        String template = reportTemplateClassPath + "MasterReportWithDataSource";
-
-        Collection<Account> accounts = new ArrayList<Account>();
-        for(AccountFixture account : AccountFixture.values()) {  
-            accounts.add(account.createAccount());
-        }
-        
-        Map<String, String> subReports = new HashMap<String, String>();
-        subReports.put("accountReport", "AccountReport");
+        String reportFileName = infoForDataSourceReport.getReportFileName();
+        String reportDirectoty = infoForDataSourceReport.getReportsDirectory();
+        String reportTemplateClassPath = infoForDataSourceReport.getReportTemplateClassPath();
+        String reportTemplateName = infoForDataSourceReport.getReportTemplateName();
+        ResourceBundle resourceBundle = infoForDataSourceReport.getResourceBundle();
+        String subReportTemplateClassPath = infoForDataSourceReport.getSubReportTemplateClassPath();
+        Map<String, String> subReports = infoForDataSourceReport.getSubReports();
 
         Map<String, Object> reportData = new HashMap<String, Object>();
-        reportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR, reportTemplateClassPath);
-        reportData.put(JRParameter.REPORT_RESOURCE_BUNDLE, ResourceBundle.getBundle(resourceBundleBaseName));
+        reportData.put(JRParameter.REPORT_RESOURCE_BUNDLE, resourceBundle);
+        reportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR, subReportTemplateClassPath);
         reportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_TEMPLATE_NAME, subReports);
 
         try {
-            reportGenerationService.generateReportToPdfFile(reportData, accounts, template, fullReportFileName);
+            String template = reportTemplateClassPath + reportTemplateName;
+            String fullReportFileName = reportDirectoty + reportFileName;
+            Collection<Account> accountDataSource = this.getAccounts();
+            
+            reportGenerationService.generateReportToPdfFile(reportData, accountDataSource, template, fullReportFileName);
         }
         catch (Exception e) {
             fail("fail to generate PDF file." + e);
         }
     }
-    
+
     // the report data are managed by a parameter map so the multiple data source can be passed to report template.
     public void testGenerateReportToPdfFile_WithParameterMap() throws Exception {
-        String fullReportFileName = reportDirectoty + "/testGenerateReportToPdfFileWithParameterMap";
-        String template = reportTemplateClassPath + "MasterReportWithParamterMap";
-        
-        Collection<Account> accounts = new ArrayList<Account>();
-        for(AccountFixture account : AccountFixture.values()) {  
-            accounts.add(account.createAccount());
-        }
-                
-        Collection<SubAccount> subAccounts = new ArrayList<SubAccount>();
-        for(SubAccountFixture subAccount : SubAccountFixture.values()) {  
-            subAccounts.add(subAccount.createSubAccount());
-        }
-        
-        Map<String, String> subReports = new HashMap<String, String>();
-        subReports.put("accountReport", "AccountReport");
-        subReports.put("subAccountReport", "SubAccountReport");
-        
-        Map<String, Object> reportData = new HashMap<String, Object>(); 
-        reportData.put(JRParameter.REPORT_RESOURCE_BUNDLE, ResourceBundle.getBundle(resourceBundleBaseName));        
-        reportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR, reportTemplateClassPath);
-        
-        reportData.put("accounts", accounts);
-        reportData.put("subAccounts", subAccounts);
-        
+        String reportFileName = infoForParameterMapReport.getReportFileName();
+        String reportDirectoty = infoForParameterMapReport.getReportsDirectory();
+        String reportTemplateClassPath = infoForParameterMapReport.getReportTemplateClassPath();
+        String reportTemplateName = infoForParameterMapReport.getReportTemplateName();
+        ResourceBundle resourceBundle = infoForParameterMapReport.getResourceBundle();
+        String subReportTemplateClassPath = infoForParameterMapReport.getSubReportTemplateClassPath();
+        Map<String, String> subReports = infoForParameterMapReport.getSubReports();
+
+        Map<String, Object> reportData = new HashMap<String, Object>();
+        reportData.put(JRParameter.REPORT_RESOURCE_BUNDLE, resourceBundle);
+        reportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR, subReportTemplateClassPath);
         reportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_TEMPLATE_NAME, subReports);
 
+        reportData.put("accounts", this.getAccounts());
+        reportData.put("subAccounts", this.getSubAccounts());
+
         try {
+            String template = reportTemplateClassPath + reportTemplateName;
+            String fullReportFileName = reportDirectoty + reportFileName;
+            
             reportGenerationService.generateReportToPdfFile(reportData, template, fullReportFileName);
         }
         catch (Exception e) {
             fail("fail to generate PDF file." + e);
         }
+    }
+
+    // create a list of accounts that will be posted in report
+    private Collection<Account> getAccounts() {
+        Collection<Account> accounts = new ArrayList<Account>();
+        for (AccountFixture account : AccountFixture.values()) {
+            accounts.add(account.createAccount());
+        }
+        return accounts;
+    }
+
+    // create a list of sub accounts that will be posted in report 
+    private Collection<SubAccount> getSubAccounts() {
+        Collection<SubAccount> subAccounts = new ArrayList<SubAccount>();
+        for (SubAccountFixture subAccount : SubAccountFixture.values()) {
+            subAccounts.add(subAccount.createSubAccount());
+        }
+        return subAccounts;
     }
 }

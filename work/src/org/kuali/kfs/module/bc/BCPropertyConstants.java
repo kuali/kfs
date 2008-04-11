1913bab13476dc4d67e06047240ccadf815771b4
Gary	Palmer <gwpalmer@iu.edu>
2008-04-11 17:53:08 +0000

    added revenue and expenditure lines props

--- work/src/org/kuali/kfs/module/bc/BCPropertyConstants.java
+++ work/src/org/kuali/kfs/module/bc/BCPropertyConstants.java
@@ -21,4 +21,6 @@ package org.kuali.module.budget;
 public class BCPropertyConstants {
     public static final String BUDGET_CONSTRUCTION_MONTHLY = "budgetConstructionMonthly";
     public static final String BUDGET_CONSTRUCTION_ACCOUNT_REPORTS = "budgetConstructionAccountReports";
+    public static final String PENDING_BUDGET_CONSTRUCTION_GENERAL_LEDGER_REVENUE_LINES = "pendingBudgetConstructionGeneralLedgerRevenueLines";
+    public static final String PENDING_BUDGET_CONSTRUCTION_GENERAL_LEDGER_EXPENDITURE_LINES = "pendingBudgetConstructionGeneralLedgerExpenditureLines";
 }

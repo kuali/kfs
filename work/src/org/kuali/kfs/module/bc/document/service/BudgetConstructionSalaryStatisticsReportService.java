3e32d62292f54a5f8bbf16acbcec34cbb4bfc1d4
Shawn Choo <schoo@vivantech.com>
2008-05-03 23:12:04 +0000

    BCOrgSalaryStatisticsReport

---
+++ work/src/org/kuali/kfs/module/bc/document/service/BudgetConstructionSalaryStatisticsReportService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.module.budget.service;
+
+import java.util.Collection;
+
+import org.kuali.module.budget.bo.BudgetConstructionOrgSalaryStatisticsReport;
+
+/**
+ * defines the methods for BudgetConstructionAccountFundingDetailReports
+ */
+public interface BudgetConstructionSalaryStatisticsReportService {
+
+    /**
+     * updates account FundingDetail table.
+     * 
+     * @param personUserIdentifier
+     */
+    public void updateSalaryStatisticsReport(String personUserIdentifier, Integer universityFiscalYear);
+
+    
+    /**
+     * 
+     * builds BudgetConstructionAccountFundingDetailReports
+     * 
+     * @param universityFiscalYear
+     * @param accountFundingDetailList
+     */
+    public Collection<BudgetConstructionOrgSalaryStatisticsReport> buildReports(Integer universityFiscalYear, String personUserIdentifier);
+    
+}

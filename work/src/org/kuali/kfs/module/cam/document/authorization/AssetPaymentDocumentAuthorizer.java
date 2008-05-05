d90997ad57f181d4a7468dce9ff7fc3d2273e9e6
Gerardo Velasquez <gerardov@usc.edu>
2008-05-05 17:14:40 +0000

    Asset Payment
    Saving or deleting accounting lines after importing them was causing an error. This is now fixed.

--- work/src/org/kuali/kfs/module/cam/document/authorization/AssetPaymentDocumentAuthorizer.java
+++ work/src/org/kuali/kfs/module/cam/document/authorization/AssetPaymentDocumentAuthorizer.java
@@ -15,9 +15,36 @@
  */
 package org.kuali.module.cams.document.authorization;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.core.document.TransactionalDocument;
+import org.kuali.kfs.bo.AccountingLine;
 import org.kuali.kfs.document.authorization.AccountingDocumentAuthorizerBase;
+import org.kuali.module.chart.bo.ChartUser;
 
 
 public class AssetPaymentDocumentAuthorizer extends AccountingDocumentAuthorizerBase {
+    /**
+     * Overrides parent to return an empty Map since FO routing doesn't apply to the AD doc.
+     * 
+     * @see org.kuali.core.authorization.TransactionalDocumentAuthorizer#getEditableAccounts(org.kuali.core.document.TransactionalDocument,
+     *      org.kuali.core.bo.user.KualiUser)
+     */
+    @Override
+    public Map getEditableAccounts(TransactionalDocument document, ChartUser user) {
+        return new HashMap();
+    }
 
+    /**
+     * Overrides parent to return an empty Map since FO routing doesn't apply to the AD doc.
+     * 
+     * @see org.kuali.kfs.document.authorization.AccountingDocumentAuthorizerBase#getEditableAccounts(java.util.List,
+     *      org.kuali.module.chart.bo.ChartUser)
+     */
+    @Override
+    public Map getEditableAccounts(List<AccountingLine> lines, ChartUser user) {
+        return new HashMap();
+    }
 }

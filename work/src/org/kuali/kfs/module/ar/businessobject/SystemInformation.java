fe0669584e4c243546156533a207acbdb9647ef6
Matt	McQuarrie <matt.mcquarrie@colostate.edu>
2008-05-09 21:30:47 +0000

    KULAR-280 completed

--- work/src/org/kuali/kfs/module/ar/businessobject/SystemInformation.java
+++ work/src/org/kuali/kfs/module/ar/businessobject/SystemInformation.java
@@ -17,7 +17,6 @@ import org.kuali.module.chart.bo.SubObjCd;
 public class SystemInformation extends PersistableBusinessObjectBase {
 
 	private Integer universityFiscalYear;
-	private String universityName;
 	private String processingChartOfAccountCode;
 	private String processingOrganizationCode;
 	private String universityFederalEmployerIdentificationNumber;
@@ -93,26 +92,6 @@ public class SystemInformation extends PersistableBusinessObjectBase {
 	public void setUniversityFiscalYear(Integer universityFiscalYear) {
 		this.universityFiscalYear = universityFiscalYear;
 	}
-
-	   /**
-     * Gets the universityName attribute.
-     * 
-     * @return Returns the universityName
-     * 
-     */
-    public String getUniversityName() { 
-        return universityName;
-    }
-
-    /**
-     * Sets the universityName attribute.
-     * 
-     * @param universityName The universityName to set.
-     * 
-     */
-    public void setUniversityName(String universityName) {
-        this.universityName = universityName;
-    }
 
 	/**
 	 * Gets the processingChartOfAccountCode attribute.

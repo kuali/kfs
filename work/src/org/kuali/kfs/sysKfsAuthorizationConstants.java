95247c503ea513bd9456eae2426c76223aaecfd0
Gary	Palmer <gwpalmer@iu.edu>
2008-05-12 15:58:06 +0000

    added another BudgetConstructionEditMode constant

--- work/src/org/kuali/kfs/sysKfsAuthorizationConstants.java
+++ work/src/org/kuali/kfs/sysKfsAuthorizationConstants.java
@@ -46,6 +46,7 @@ public class KfsAuthorizationConstants extends JSTLConstants {
         public static final String SYSTEM_VIEW_ONLY = "systemViewOnly";
         public static final String USER_BELOW_DOC_LEVEL = "userBelowDocLevel";
         public static final String USER_NOT_ORG_APPROVER = "userNotOrgApprover";
+        public static final String USER_NOT_IN_ACCOUNT_HIER = "userNotInAccountHier";
     }
 
 }

0ba421f77ea10a4a04e83a5ea1ea0e7a6ba507eb
Shawn Choo <schoo@vivantech.com>
2008-06-11 00:26:40 +0000

    BCAccountSalaryDetailReport update

--- work/src/org/kuali/kfs/module/bc/document/service/impl/BudgetConstructionAccountSalaryDetailReportServiceImpl.java
+++ work/src/org/kuali/kfs/module/bc/document/service/impl/BudgetConstructionAccountSalaryDetailReportServiceImpl.java
@@ -15,6 +15,7 @@
  */
 package org.kuali.module.budget.service.impl;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -24,7 +25,11 @@ import java.util.Map;
 import org.kuali.core.service.KualiConfigurationService;
 import org.kuali.kfs.KFSPropertyConstants;
 import org.kuali.module.budget.bo.BudgetConstructionAccountSalaryDetailReport;
+import org.kuali.module.budget.bo.BudgetConstructionAccountSalaryDetailReportTotal;
+import org.kuali.module.budget.bo.BudgetConstructionCalculatedSalaryFoundationTracker;
 import org.kuali.module.budget.bo.BudgetConstructionMonthly;
+import org.kuali.module.budget.bo.BudgetConstructionOrgPositionFundingDetailReportTotal;
+import org.kuali.module.budget.bo.BudgetConstructionPositionFunding;
 import org.kuali.module.budget.bo.PendingBudgetConstructionAppointmentFunding;
 import org.kuali.module.budget.service.BudgetConstructionAccountSalaryDetailReportService;
 import org.kuali.module.budget.service.BudgetConstructionReportsServiceHelper;
@@ -49,16 +54,16 @@ public class BudgetConstructionAccountSalaryDetailReportServiceImpl implements B
 
         BudgetConstructionAccountSalaryDetailReport accountSalaryDetailReport;
         // build searchCriteria
-        Map searchCriteria = new HashMap();
-        searchCriteria.put(KFSPropertyConstants.UNIVERSITY_FISCAL_YEAR, universityFiscalYear);
-        searchCriteria.put(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE, chartOfAccountsCode);
-        searchCriteria.put(KFSPropertyConstants.ACCOUNT_NUMBER, accountNumber);
-        searchCriteria.put(KFSPropertyConstants.SUB_ACCOUNT_NUMBER, subAccountNumber);
+        Map searchCriteria = buildSearchCriteria(universityFiscalYear, chartOfAccountsCode, accountNumber, subAccountNumber);
         
-
         // build order list
         List<String> orderList = buildOrderByList();
         Collection<PendingBudgetConstructionAppointmentFunding> pendingAppointmentFundingList = budgetConstructionReportsServiceHelper.getDataForBuildingReports(PendingBudgetConstructionAppointmentFunding.class, searchCriteria, orderList);
+
+        List<PendingBudgetConstructionAppointmentFunding> listForTotal = BudgetConstructionReportHelper.deleteDuplicated((List) pendingAppointmentFundingList, fieldsForTotal());
+
+        // Calculate Total Section
+        Collection<BudgetConstructionAccountSalaryDetailReportTotal> fundingDetailTotalPerson = calculateTotal(pendingAppointmentFundingList, listForTotal);
 
 
 /*        for (BudgetConstructionMonthly bcMonthly : budgetConstructionMonthlyList) {
@@ -134,6 +139,67 @@ public class BudgetConstructionAccountSalaryDetailReportServiceImpl implements B
         accountMonthlyDetailReport.setFinancialDocumentMonth12LineAmount(financialDocumentMonth12LineAmount);
 */        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private Collection<BudgetConstructionAccountSalaryDetailReportTotal> calculateTotal(Collection<PendingBudgetConstructionAppointmentFunding> pendingAppointmentFundingList, List<PendingBudgetConstructionAppointmentFunding> listForTotal) {
+        Collection<BudgetConstructionAccountSalaryDetailReportTotal> returnCollection = new ArrayList();
+        Integer totalPersonPositionCsfAmount = new Integer(0);
+        Integer totalPersonAppointmentRequestedAmount = new Integer(0);
+        BigDecimal totalPersonPositionCsfFteQuantity = BigDecimal.ZERO;
+        BigDecimal totalPersonAppointmentRequestedFteQuantity = BigDecimal.ZERO;
+        Integer personSortCode = new Integer(0);
+        PendingBudgetConstructionAppointmentFunding pendingAppointmentFunding = null;
+        BudgetConstructionAccountSalaryDetailReportTotal budgetConstructionAccountSalaryDetailReportTotal = new BudgetConstructionAccountSalaryDetailReportTotal();
+        for (PendingBudgetConstructionAppointmentFunding totalEntry : listForTotal){
+            for (PendingBudgetConstructionAppointmentFunding appointmentFundingEntry : pendingAppointmentFundingList) {
+                if (BudgetConstructionReportHelper.isSameEntry(totalEntry, appointmentFundingEntry, fieldsForTotal())) {
+//                    pendingAppointmentFunding = appointmentFundingEntry.getPendingAppointmentFunding();
+//                    if (pendingAppointmentFunding.getBcnCalculatedSalaryFoundationTracker().size() > 0) {
+//                        BudgetConstructionCalculatedSalaryFoundationTracker calculatedSalaryFoundationTracker = pendingAppointmentFunding.getBcnCalculatedSalaryFoundationTracker().get(0);
+//                        totalPersonPositionCsfAmount = totalPersonPositionCsfAmount + new Integer(calculatedSalaryFoundationTracker.getCsfAmount().intValue());
+//                        totalPersonPositionCsfFteQuantity = totalPersonPositionCsfFteQuantity.add(calculatedSalaryFoundationTracker.getCsfFullTimeEmploymentQuantity());
+//                    }
+//                    if (pendingAppointmentFunding != null) {
+//                        totalPersonAppointmentRequestedAmount = totalPersonAppointmentRequestedAmount + new Integer(pendingAppointmentFunding.getAppointmentRequestedAmount().intValue());
+//                        totalPersonAppointmentRequestedFteQuantity = totalPersonAppointmentRequestedFteQuantity.add(pendingAppointmentFunding.getAppointmentRequestedFteQuantity());
+//                    }
+               }
+            }
+//            
+
+            returnCollection.add(budgetConstructionAccountSalaryDetailReportTotal);
+            // set all values to zero, after the entry was added to collection
+            totalPersonPositionCsfAmount = new Integer(0);
+            totalPersonAppointmentRequestedAmount = new Integer(0);
+            totalPersonPositionCsfFteQuantity = BigDecimal.ZERO;
+            totalPersonAppointmentRequestedFteQuantity = BigDecimal.ZERO;
+            personSortCode = new Integer(0);
+        }
+        return returnCollection;
+    }
+    
+    
+    
+    
+    
+    
+    private List<String> fieldsForTotal() {
+
+        List<String> fieldList = new ArrayList();
+        fieldList.add(KFSPropertyConstants.FINANCIAL_OBJECT_CODE);
+        return fieldList;
+    }
 
     /**
      * builds orderByList for sort order.
@@ -144,9 +210,20 @@ public class BudgetConstructionAccountSalaryDetailReportServiceImpl implements B
         List<String> returnList = new ArrayList();
         returnList.add(KFSPropertyConstants.FINANCIAL_OBJECT_CODE);
         returnList.add(KFSPropertyConstants.FINANCIAL_SUB_OBJECT_CODE);
+        returnList.add(KFSPropertyConstants.POSITION_NUMBER);
+        returnList.add(KFSPropertyConstants.EMPLID);
         return returnList;
     }
 
+    private Map buildSearchCriteria(Integer universityFiscalYear, String chartOfAccountsCode, String accountNumber, String subAccountNumber){
+        Map searchCriteria = new HashMap();
+        searchCriteria.put(KFSPropertyConstants.UNIVERSITY_FISCAL_YEAR, universityFiscalYear);
+        searchCriteria.put(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE, chartOfAccountsCode);
+        searchCriteria.put(KFSPropertyConstants.ACCOUNT_NUMBER, accountNumber);
+        searchCriteria.put(KFSPropertyConstants.SUB_ACCOUNT_NUMBER, subAccountNumber);
+        return searchCriteria;
+    }
+    
     public void setKualiConfigurationService(KualiConfigurationService kualiConfigurationService) {
         this.kualiConfigurationService = kualiConfigurationService;
     }

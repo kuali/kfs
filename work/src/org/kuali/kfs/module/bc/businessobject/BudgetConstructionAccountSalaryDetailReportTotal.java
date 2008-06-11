315b9daebf3c89adfd541e98c47b1f8a57a0a2ed
Shawn Choo <schoo@vivantech.com>
2008-06-11 00:26:22 +0000

    BO for BCAccountSalaryDetailReportTotal

---
+++ work/src/org/kuali/kfs/module/bc/businessobject/BudgetConstructionAccountSalaryDetailReportTotal.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kuali.module.budget.bo;
+
+import java.math.BigDecimal;
+
+import org.kuali.core.util.KualiInteger;
+
+/**
+ * 
+ */
+public class BudgetConstructionAccountSalaryDetailReportTotal{
+
+    // total
+    private String totalDescription;
+    private Integer totalBaseAmount;
+    private BigDecimal totalBaseFte;
+    private Integer totalRequestAmount;
+    private BigDecimal totalRequestFte;
+    private Integer totalAmountChange;
+    private BigDecimal totalPercentChange;
+    
+    private PendingBudgetConstructionAppointmentFunding pendingBudgetConstructionAppointmentFunding;
+    
+    
+    
+    /**
+     * Default constructor.
+     */
+    public BudgetConstructionAccountSalaryDetailReportTotal() {
+
+    }
+
+    public PendingBudgetConstructionAppointmentFunding getPendingBudgetConstructionAppointmentFunding() {
+        return pendingBudgetConstructionAppointmentFunding;
+    }
+
+    public void setPendingBudgetConstructionAppointmentFunding(PendingBudgetConstructionAppointmentFunding pendingBudgetConstructionAppointmentFunding) {
+        this.pendingBudgetConstructionAppointmentFunding = pendingBudgetConstructionAppointmentFunding;
+    }
+
+    public String getTotalDescription() {
+        return totalDescription;
+    }
+
+
+
+
+    public void setTotalDescription(String totalDescription) {
+        this.totalDescription = totalDescription;
+    }
+
+
+
+
+    public Integer getTotalBaseAmount() {
+        return totalBaseAmount;
+    }
+
+
+
+
+    public void setTotalBaseAmount(Integer totalBaseAmount) {
+        this.totalBaseAmount = totalBaseAmount;
+    }
+
+
+
+
+    public BigDecimal getTotalBaseFte() {
+        return totalBaseFte;
+    }
+
+
+
+
+    public void setTotalBaseFte(BigDecimal totalBaseFte) {
+        this.totalBaseFte = totalBaseFte;
+    }
+
+
+
+
+    public Integer getTotalRequestAmount() {
+        return totalRequestAmount;
+    }
+
+
+
+
+    public void setTotalRequestAmount(Integer totalRequestAmount) {
+        this.totalRequestAmount = totalRequestAmount;
+    }
+
+
+
+
+    public BigDecimal getTotalRequestFte() {
+        return totalRequestFte;
+    }
+
+
+
+
+    public void setTotalRequestFte(BigDecimal totalRequestFte) {
+        this.totalRequestFte = totalRequestFte;
+    }
+
+
+
+
+    public Integer getTotalAmountChange() {
+        return totalAmountChange;
+    }
+
+
+
+
+    public void setTotalAmountChange(Integer totalAmountChange) {
+        this.totalAmountChange = totalAmountChange;
+    }
+
+
+
+
+    public BigDecimal getTotalPercentChange() {
+        return totalPercentChange;
+    }
+
+
+
+
+    public void setTotalPercentChange(BigDecimal totalPercentChange) {
+        this.totalPercentChange = totalPercentChange;
+    }
+}

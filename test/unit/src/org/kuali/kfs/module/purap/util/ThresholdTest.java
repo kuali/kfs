400f3694bd97ab7f9c6c79402249af38159caacf
Venkat PremChandran <vpremcha@kuali.org>
2008-06-25 23:02:45 +0000

    Fix  - if PO.ITEMS_REQUIRE_COMMODITY_CODE_IND='Y', business rule evaluation failure in PO

--- test/unit/src/org/kuali/kfs/module/purap/util/ThresholdTest.java
+++ test/unit/src/org/kuali/kfs/module/purap/util/ThresholdTest.java
@@ -26,10 +26,12 @@ import org.kuali.core.service.DocumentService;
 import org.kuali.core.util.Guid;
 import org.kuali.core.util.KualiDecimal;
 import org.kuali.kfs.ConfigureContext;
+import org.kuali.kfs.module.purap.PurapRuleConstants;
 import org.kuali.kfs.module.purap.document.PurchaseOrderDocument;
 import org.kuali.kfs.module.purap.fixture.PurchaseOrderDocumentFixture;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.context.TestUtils;
 import org.kuali.kfs.sys.dataaccess.UnitTestSqlDao;
 import org.kuali.kfs.sys.document.AccountingDocumentTestUtils;
 import org.kuali.kfs.sys.document.workflow.WorkflowTestUtils;
@@ -210,13 +212,20 @@ public class ThresholdTest extends KualiTestBase {
         }else{
             insertThresholdRecord(thresholdCriteria,poDocument.getItem(0).getExtendedPrice().add(new KualiDecimal(1)));
         }
+        
         PurchaseOrderDocument result = routePO(poDocument);
         assertEquals("Receiving Flag test[" + thresholdCriteria + ",isPositiveTest=" + isPositiveTest + "]",isPositiveTest,result.isReceivingDocumentRequiredIndicator());
     }
     
     private PurchaseOrderDocument routePO(PurchaseOrderDocument poDocument) 
     throws Exception {
-        
+      /**
+       * FIXME: This is a temp fix if PO.ITEMS_REQUIRE_COMMODITY_CODE_IND='Y' since we're not using commodity code in the PO item fixture.
+       * Have to add CommodityCode in PO item fixture but facing user authorization issue since PARKE doesnt have permission to create vendormaintdoc
+       * for the given CC (Getting exception from POService.updateVendorCommodityCode(PurchaseOrderDocument po))   
+       */
+      TestUtils.setSystemParameter(PurchaseOrderDocument.class, PurapRuleConstants.ITEMS_REQUIRE_COMMODITY_CODE_IND,"N");
+      
       String docId = poDocument.getDocumentNumber();
       DocumentService documentService = SpringContext.getBean(DocumentService.class);
       poDocument.prepareForSave();       
@@ -228,7 +237,7 @@ public class ThresholdTest extends KualiTestBase {
       return result;
     }
     
-    public PurchaseOrderDocument buildSimplePODocument() throws Exception {
+    public PurchaseOrderDocument buildSimplePODocument() {
         PurchaseOrderDocument poDocument = PurchaseOrderDocumentFixture.PO_FOR_THRESHOLD_CHECK.createPurchaseOrderDocument();
         assertEquals("PO should have only one item for Threshold test",1,poDocument.getItems().size());
         assertEquals("PO Item should have only one account for Threshold test",1,poDocument.getItem(0).getSourceAccountingLines().size());

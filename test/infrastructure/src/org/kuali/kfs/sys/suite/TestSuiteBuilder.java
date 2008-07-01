d85e03dfeb43f725f37147949f7d7424004dc4e7
Jonathan Keller <keller.jonathan@gmail.com>
2008-07-01 17:35:06 +0000

    Updated TestSuiteBuilder for the new package depth.

--- test/infrastructure/src/org/kuali/kfs/sys/suite/TestSuiteBuilder.java
+++ test/infrastructure/src/org/kuali/kfs/sys/suite/TestSuiteBuilder.java
@@ -144,7 +144,7 @@ public class TestSuiteBuilder {
     private static File getTestRootPackageDir() {
         try {
             File thisClassFile = new File(THIS_CLASS.getResource(THIS_CLASS.getSimpleName() + ".class").toURI());
-            return thisClassFile.getParentFile().getParentFile().getParentFile();
+            return thisClassFile.getParentFile().getParentFile().getParentFile().getParentFile();
         }
         catch (URISyntaxException e) {
             throw new AssertionError(e); // if the classloader doesn't always return the "file:" protocol, then this method needs

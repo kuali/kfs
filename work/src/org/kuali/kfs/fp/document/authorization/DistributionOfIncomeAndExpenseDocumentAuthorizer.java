d5719e3f99fdd8b3b9f0cd9bddc6fdd6086dad9a
Dennis Friends <dwf5@cornell.edu>
2008-07-14 18:31:12 +0000

    KFSMI-768 : Built new custom authorizer class to handle the DI and YEDI.

---
+++ work/src/org/kuali/kfs/fp/document/authorization/DistributionOfIncomeAndExpenseDocumentAuthorizer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.fp.document.authorization;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.Document;
+import org.kuali.kfs.fp.document.DistributionOfIncomeAndExpenseDocument;
+import org.kuali.kfs.sys.KfsAuthorizationConstants;
+import org.kuali.kfs.sys.businessobject.ElectronicPaymentClaim;
+import org.kuali.kfs.sys.document.authorization.AccountingDocumentAuthorizerBase;
+
+/**
+ * This class...
+ */
+public class DistributionOfIncomeAndExpenseDocumentAuthorizer extends AccountingDocumentAuthorizerBase {
+
+    /**
+     * @see org.kuali.core.authorization.DocumentAuthorizer#getEditMode(org.kuali.core.document.Document,
+     *      org.kuali.core.bo.user.KualiUser)
+     */
+    @Override
+    public Map getEditMode(Document document, UniversalUser user, List sourceLines, List targetLines) {
+        DistributionOfIncomeAndExpenseDocument diDoc = (DistributionOfIncomeAndExpenseDocument) document;
+        
+        Map editModeMap = super.getEditMode(document, user, sourceLines, targetLines);
+        
+        List<ElectronicPaymentClaim> epcs = diDoc.getElectronicPaymentClaims();
+
+        if(epcs==null) {
+            diDoc.refreshReferenceObject("electronicPaymentClaims");
+            epcs = diDoc.getElectronicPaymentClaims();
+        }
+        
+        if(epcs!=null && epcs.size()>0) {
+            editModeMap.put(KfsAuthorizationConstants.DistributionOfIncomeAndExpenseEditMode.SOURCE_LINE_READ_ONLY_MODE, "TRUE");
+        }
+        
+        return editModeMap;
+    }
+}

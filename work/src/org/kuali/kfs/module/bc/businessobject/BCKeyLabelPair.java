426b3ca70cf89fd5486481feea7803eeeef05fc7
Gary	Palmer <gwpalmer@iu.edu>
2008-07-18 18:29:34 +0000

    initial commit to handle pullup/pushdown options selection on BC document

---
+++ work/src/org/kuali/kfs/module/bc/businessobject/BCKeyLabelPair.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.bc.businessobject;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.core.bo.TransientBusinessObjectBase;
+
+/**
+ * Used to represent a dynamic options selection using explicitly typed key/label pair
+ */
+public class BCKeyLabelPair extends TransientBusinessObjectBase {
+    public String key;
+    public String label;
+
+    public BCKeyLabelPair() {
+    }
+
+    public BCKeyLabelPair(String key, String label) {
+        this.key = key;
+        this.label = label;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
+     */
+    @Override
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap m = new LinkedHashMap();
+        m.put("useThreshold", this.key);
+        m.put("thresholdPercent", this.label);
+        return m;
+    }
+
+}

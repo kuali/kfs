87c3249eb3afb06d0e5fc7e0949cab8bd1d3cf6f
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2008-08-06 18:50:26 +0000

    KFSMI-1191
    Changes for new table/BO

---
+++ work/src/org/kuali/kfs/module/cam/businessobject/AssetPaymentAssetDetail.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.cam.businessobject;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.core.bo.PersistableBusinessObjectBase;
+import org.kuali.core.util.KualiDecimal;
+
+public class AssetPaymentAssetDetail extends PersistableBusinessObjectBase {
+    private String documentNumber;
+    private Long capitalAssetNumber;
+    private KualiDecimal previousTotalCostAmount;
+    
+    protected LinkedHashMap<String, String> toStringMapper() {
+        LinkedHashMap<String, String> m = new LinkedHashMap<String, String>();
+        if (this.documentNumber != null) {
+            m.put("paymentSequenceNumber", this.documentNumber.toString());
+        }
+        if (this.capitalAssetNumber != null) {
+            m.put("capitalAssetNumber", this.capitalAssetNumber.toString());
+        }
+        return m;
+    }
+
+    public Long getCapitalAssetNumber() {
+        return capitalAssetNumber;
+    }
+
+    public void setCapitalAssetNumber(Long capitalAssetNumber) {
+        this.capitalAssetNumber = capitalAssetNumber;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public KualiDecimal getPreviousTotalCostAmount() {
+        return previousTotalCostAmount;
+    }
+
+    public void setPreviousTotalCostAmount(KualiDecimal previousTotalCostAmount) {
+        this.previousTotalCostAmount = previousTotalCostAmount;
+    }
+
+}

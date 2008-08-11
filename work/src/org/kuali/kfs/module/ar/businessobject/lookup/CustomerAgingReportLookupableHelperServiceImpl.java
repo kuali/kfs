56dc087e6ca64d9167aef10cdcfb53c16a643342
Matt	McQuarrie <matt.mcquarrie@colostate.edu>
2008-08-11 17:56:18 +0000

    KULAR-307 Customer Aging Report - overiding performLookup isn't working yet

--- work/src/org/kuali/kfs/module/ar/businessobject/lookup/CustomerAgingReportLookupableHelperServiceImpl.java
+++ work/src/org/kuali/kfs/module/ar/businessobject/lookup/CustomerAgingReportLookupableHelperServiceImpl.java
@@ -14,34 +14,81 @@
  * limitations under the License.
  */
 package org.kuali.kfs.module.ar.businessobject.lookup;
+import java.security.GeneralSecurityException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+import org.kuali.RiceKeyConstants;
+import org.kuali.core.bo.BusinessObject;
+import org.kuali.core.bo.PersistableBusinessObject;
+import org.kuali.core.datadictionary.mask.Mask;
+import org.kuali.core.exceptions.ValidationException;
+import org.kuali.core.inquiry.Inquirable;
+import org.kuali.core.service.AuthorizationService;
+import org.kuali.core.service.BusinessObjectDictionaryService;
+import org.kuali.core.service.BusinessObjectMetaDataService;
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.service.DataDictionaryService;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.service.LookupService;
+import org.kuali.core.service.MaintenanceDocumentDictionaryService;
+import org.kuali.core.service.PersistenceStructureService;
+import org.kuali.core.service.SequenceAccessorService;
+import org.kuali.core.service.UniversalUserService;
+import org.kuali.core.util.FieldUtils;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.util.ObjectUtils;
+import org.kuali.core.util.UrlFactory;
+import org.kuali.core.util.cache.CopiedObject;
+import org.kuali.core.web.comparator.CellComparatorHelper;
+import org.kuali.core.web.format.BooleanFormatter;
+import org.kuali.core.web.format.CollectionFormatter;
+import org.kuali.core.web.format.DateFormatter;
+import org.kuali.core.web.format.Formatter;
+import org.kuali.core.web.struts.form.LookupForm;
+import org.kuali.core.web.ui.Column;
+import org.kuali.core.web.ui.ResultRow;
+import org.kuali.core.web.ui.Row;
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.rice.core.service.EncryptionService;
+import org.kuali.rice.kns.util.KNSConstants;
+import java.security.GeneralSecurityException;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.core.bo.BusinessObject;
 import org.kuali.core.lookup.CollectionIncomplete;
 import org.kuali.core.lookup.KualiLookupableHelperServiceImpl;
+import org.kuali.core.service.DataDictionaryService;
+import org.kuali.core.util.GlobalVariables;
 import org.kuali.core.util.KualiDecimal;
-import org.kuali.kfs.coa.businessobject.defaultvalue.ValueFinderUtil;
-//import org.kuali.kfs.gl.Constant;
-//import org.kuali.kfs.gl.GeneralLedgerConstants;
-//import org.kuali.kfs.gl.businessobject.AccountBalance;
-//import org.kuali.kfs.gl.businessobject.TransientBalanceInquiryAttributes;
-//import org.kuali.kfs.gl.businessobject.lookup.AccountBalanceByConsolidationLookupableHelperServiceImpl;
-//import org.kuali.kfs.gl.businessobject.lookup.BusinessObjectFieldConverter;
-//import org.kuali.kfs.gl.service.AccountBalanceService;
+import org.kuali.core.util.ObjectUtils;
+import org.kuali.core.web.format.DateFormatter;
+import org.kuali.core.web.struts.form.LookupForm;
 import org.kuali.kfs.module.ar.businessobject.CustomerAgingReportDetail;
-import org.kuali.kfs.module.ar.businessobject.lookup.CustomerLookupableHelperServiceImpl;
-import org.kuali.kfs.module.ar.util.ARUtil;
 import org.kuali.kfs.sys.KFSConstants;
-import org.kuali.kfs.sys.KFSPropertyConstants;
-import org.kuali.kfs.sys.businessobject.FinancialSystemUser;
+import org.kuali.rice.core.service.EncryptionService;
+import org.kuali.rice.kns.util.KNSConstants;
 
 public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupableHelperServiceImpl {
 
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CustomerAgingReportLookupableHelperServiceImpl.class);
+    private EncryptionService encryptionService;
+    private DataDictionaryService dataDictionaryService;
+    private Map fieldConversions;
+
 
     
     /**
@@ -102,16 +149,199 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
     /**
      * @return a List of the names of fields which are marked in data dictionary as return fields.
      */
+    @Override
     public List getReturnKeys() {
-//        List returnKeys;
+        List returnKeys;
 //        if (fieldConversions != null && !fieldConversions.isEmpty()) {
-//            returnKeys = new ArrayList(fieldConversions.keySet());
+            returnKeys = new ArrayList(fieldConversions.keySet());
 //        }
 //        else {
 //            returnKeys = getPersistenceStructureService().listPrimaryKeyFieldNames(getBusinessObjectClass());
 //        }
 //
-//        return returnKeys;
-        return null;
+        return returnKeys;
+
     }
+    
+    /**
+     * @return a List of the names of fields which are marked in data dictionary as return fields.
+     */
+    @Override
+    protected Properties getParameters(BusinessObject bo, Map fieldConversions, String lookupImpl) {
+        Properties parameters = new Properties();
+        parameters.put(KNSConstants.DISPATCH_REQUEST_PARAMETER, KNSConstants.RETURN_METHOD_TO_CALL);
+        parameters.put(KNSConstants.DOC_FORM_KEY, getDocFormKey());
+        parameters.put(KNSConstants.REFRESH_CALLER, lookupImpl);
+        if (getReferencesToRefresh() != null) {
+            parameters.put(KNSConstants.REFERENCES_TO_REFRESH, getReferencesToRefresh());
+        }
+        
+        String encryptedList = "";
+
+        Iterator returnKeys = getReturnKeys().iterator();
+        while (returnKeys.hasNext()) {
+            String fieldNm = (String) returnKeys.next();
+
+            Object fieldVal = ObjectUtils.getPropertyValue(bo, fieldNm);
+            if (fieldVal == null) {
+                fieldVal = KNSConstants.EMPTY_STRING;
+            }
+
+            // Encrypt value if it is a secure field
+            String displayWorkgroup = dataDictionaryService.getAttributeDisplayWorkgroup(bo.getClass(), fieldNm);
+
+            if (fieldConversions.containsKey(fieldNm)) {
+                fieldNm = (String) fieldConversions.get(fieldNm);
+            }
+
+            if (StringUtils.isNotBlank(displayWorkgroup) && !GlobalVariables.getUserSession().getUniversalUser().isMember( displayWorkgroup )) {
+//                try {
+//                    fieldVal = encryptionService.encrypt(fieldVal);
+//                }
+//                catch (GeneralSecurityException e) {
+//                    LOG.error("Exception while trying to encrypted value for inquiry framework.", e);
+//                    throw new RuntimeException(e);
+//                }
+
+                // add to parameter list so that KualiInquiryAction can identify which parameters are encrypted
+                if (encryptedList.equals("")) {
+                    encryptedList = fieldNm;
+                }
+                else {
+                    encryptedList = encryptedList + KNSConstants.FIELD_CONVERSIONS_SEPERATOR + fieldNm;
+                }
+            }
+            
+            //need to format date in url
+            if (fieldVal instanceof Date) {
+                DateFormatter dateFormatter = new DateFormatter();
+                fieldVal = dateFormatter.format(fieldVal);
+            }
+
+            parameters.put(fieldNm, fieldVal.toString());
+        }
+
+        // if we did encrypt a value (or values), add the list of those that are encrypted to the parameters
+        if (!encryptedList.equals("")) {
+            parameters.put(KNSConstants.ENCRYPTED_LIST_PREFIX, encryptedList);
+        }
+
+        return parameters;
+    }    
+    
+    /**
+     * 
+     * This method performs the lookup and returns a collection of lookup items
+     * @param lookupForm
+     * @param kualiLookupable
+     * @param resultTable
+     * @param bounded
+     * @return
+     */
+    @Override
+    public Collection performLookup(LookupForm lookupForm, Collection resultTable, boolean bounded) {
+        Collection displayList;
+        
+        // call search method to get results
+        if (bounded) {
+            displayList = getSearchResults(lookupForm.getFieldsForLookup());
+        }
+        else {
+            displayList = getSearchResultsUnbounded(lookupForm.getFieldsForLookup());
+        }
+// MJM get resultTable populated here
+        
+//        HashMap<String,Class> propertyTypes = new HashMap<String, Class>(); 
+//        
+//        boolean hasReturnableRow = false;
+//        
+//        // iterate through result list and wrap rows with return url and action urls
+//        for (Iterator iter = displayList.iterator(); iter.hasNext();) {
+//            BusinessObject element = (BusinessObject) iter.next();
+//
+//            String returnUrl = getReturnUrl(element, lookupForm.getFieldConversions(), lookupForm.getLookupableImplServiceName());
+//            String actionUrls = getActionUrls(element);
+//
+//            List<Column> columns = getColumns();
+//            for (Iterator iterator = columns.iterator(); iterator.hasNext();) {
+//                
+//                Column col = (Column) iterator.next();
+//                Formatter formatter = col.getFormatter();
+//
+//                // pick off result column from result list, do formatting
+//                String propValue = KNSConstants.EMPTY_STRING;
+//                Object prop = ObjectUtils.getPropertyValue(element, col.getPropertyName());
+//                
+//                // set comparator and formatter based on property type
+//                Class propClass = propertyTypes.get(col.getPropertyName());
+////                if ( propClass == null ) {
+////                    try {
+////                        propClass = ObjectUtils.getPropertyType( element, col.getPropertyName(), getPersistenceStructureService() );
+////                        propertyTypes.put( col.getPropertyName(), propClass );
+////                    } catch (Exception e) {
+////                        throw new RuntimeException("Cannot access PropertyType for property " + "'" + col.getPropertyName() + "' " + " on an instance of '" + element.getClass().getName() + "'.", e);
+////                    }
+////                }
+//
+//                // formatters
+//                if (prop != null) {
+//                    // for Booleans, always use BooleanFormatter
+//                    if (prop instanceof Boolean) {
+//                        formatter = new BooleanFormatter();
+//                    }
+//                    
+//                    // for Dates, always use DateFormatter
+//                    if (prop instanceof Date) {
+//                        formatter = new DateFormatter();
+//                    }
+//
+//                    // for collection, use the list formatter if a formatter hasn't been defined yet
+//                    if (prop instanceof Collection && formatter == null) {
+//                    formatter = new CollectionFormatter();
+//                    }
+//                    
+//                    if (formatter != null) {
+//                        propValue = (String) formatter.format(prop);
+//                    }
+//                    else {
+//                        propValue = prop.toString();
+//                    }
+//                }
+//
+//                // comparator
+////                col.setComparator(CellComparatorHelper.getAppropriateComparatorForPropertyClass(propClass));
+////                col.setValueComparator(CellComparatorHelper.getAppropriateValueComparatorForPropertyClass(propClass));
+//                
+//                // check security on field and do masking if necessary
+//                boolean viewAuthorized = getAuthorizationService().isAuthorizedToViewAttribute(GlobalVariables.getUserSession().getUniversalUser(), element.getClass().getName(), col.getPropertyName());
+//                if (!viewAuthorized) {
+//                    Mask displayMask = getDataDictionaryService().getAttributeDisplayMask(element.getClass().getName(), col.getPropertyName());
+//                    propValue = displayMask.maskValue(propValue);
+//                }
+//                col.setPropertyValue(propValue);
+//
+//
+//                if (StringUtils.isNotBlank(propValue)) {
+//                    col.setPropertyURL(getInquiryUrl(element, col.getPropertyName()));
+//                }
+//            }
+//
+//            ResultRow row = new ResultRow(columns, returnUrl, actionUrls);
+//            if ( element instanceof PersistableBusinessObject ) {
+//                row.setObjectId(((PersistableBusinessObject)element).getObjectId());
+//            }
+//            
+//            boolean rowReturnable = isResultReturnable(element);
+//            row.setRowReturnable(rowReturnable);
+//            if (rowReturnable) {
+//                hasReturnableRow = true;
+//            }
+//            resultTable.add(row);
+//        }
+//
+//        lookupForm.setHasReturnableRow(hasReturnableRow);
+
+        return displayList;
+    }
+
 }
--- work/src/org/kuali/kfs/module/ar/web/struts/CustomerAgingReportAction.java
+++ work/src/org/kuali/kfs/module/ar/web/struts/CustomerAgingReportAction.java
@@ -15,6 +15,8 @@
  */
 package org.kuali.kfs.module.ar.web.struts;
 
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -85,6 +87,10 @@ public class CustomerAgingReportAction extends KualiAction {
         Lookupable lookupable = lookupForm.getLookupable();
         LOG.info("\t\t\t\t\n\n lookupable is "+lookupable.toString());
 
+        if (LOG.isInfoEnabled()) {
+            LOG.info("\n\nsearch(ActionMapping, ActionForm, HttpServletRequest, HttpServletResponse) - testing my log - \nrequest=" + request + ", \nlookupForm=" + lookupForm);
+        }
+
         if (lookupable == null) {
             LOG.error("Lookupable is null.");
             throw new RuntimeException("Lookupable is null.");
@@ -98,7 +104,17 @@ public class CustomerAgingReportAction extends KualiAction {
         try {
             displayList = lookupable.performLookup(lookupForm, resultTable, true);
 
+            
+            
+            if (LOG.isInfoEnabled()) {
+                LOG.info("\n\n\n\n\t\t\tsearch(ActionMapping, ActionForm, HttpServletRequest, HttpServletResponse) - List<ResultRow> resultTable=" + resultTable.get(3));
+            }
+
             Object[] resultTableAsArray = resultTable.toArray();
+            
+            if (LOG.isInfoEnabled()) {
+                LOG.info("search(ActionMapping, ActionForm, HttpServletRequest, HttpServletResponse) - Object[] resultTableAsArray=" + resultTableAsArray);
+            }
 
             CollectionIncomplete incompleteDisplayList = (CollectionIncomplete) displayList;
             Long totalSize = ((CollectionIncomplete) displayList).getActualSizeIfTruncated();
@@ -106,49 +122,60 @@ public class CustomerAgingReportAction extends KualiAction {
             request.setAttribute(KFSConstants.REQUEST_SEARCH_RESULTS_SIZE, totalSize);
 
             // TODO: use inheritance instead of this if statement
-            if (lookupable.getLookupableHelperService() instanceof CustomerAgingReportLookupableHelperServiceImpl) {
+            //if (lookupable.getLookupableHelperService() instanceof CustomerAgingReportLookupableHelperServiceImpl) {
+            if (true) {
+//
+//                Collection totalsTable = new ArrayList();
+//
+//                int listIndex = 0;
+//                int arrayIndex = 0;
+//                int listSize = incompleteDisplayList.size();
+//
+//                for (; listIndex < listSize;) {
+//
+//                     
+//                    CustomerAgingReportDetail balance = (CustomerAgingReportDetail) incompleteDisplayList.get(listIndex);
+//
+//                    boolean ok = true;//ObjectHelper.isOneOf(balance.getTitle(), getTotalTitles());
+//                    if (ok) {
+//
+//                        if (totalSize > 7) {
+//                            totalsTable.add(resultTableAsArray[arrayIndex]);
+//                        }
+//                        resultTable.remove(resultTableAsArray[arrayIndex]);
+//
+//                        incompleteDisplayList.remove(balance);
+//                        // account for the removal of the balance which resizes the list
+//                        listIndex--;
+//                        listSize--;
+//
+//                    }
+//
+//                    listIndex++;
+//                    arrayIndex++;
+//
+//                }
 
+                // MJM figure out what resultTable is supposed to have in it
+               //Collection totalsTable = new ArrayList();
 
-                Collection totalsTable = new ArrayList();
-
-                int listIndex = 0;
-                int arrayIndex = 0;
-                int listSize = incompleteDisplayList.size();
-
-                for (; listIndex < listSize;) {
-
-                     
-                    CustomerAgingReportDetail balance = (CustomerAgingReportDetail) incompleteDisplayList.get(listIndex);
-
-                    boolean ok = true;//ObjectHelper.isOneOf(balance.getTitle(), getTotalTitles());
-                    if (ok) {
-
-                        if (totalSize > 7) {
-                            totalsTable.add(resultTableAsArray[arrayIndex]);
-                        }
-                        resultTable.remove(resultTableAsArray[arrayIndex]);
-
-                        incompleteDisplayList.remove(balance);
-                        // account for the removal of the balance which resizes the list
-                        listIndex--;
-                        listSize--;
-
-                    }
-
-                    listIndex++;
-                    arrayIndex++;
-
+                if (LOG.isInfoEnabled()) {
+                    LOG.info("search(ActionMapping, ActionForm, HttpServletRequest, HttpServletResponse) - List<ResultRow> resultTable=" + resultTable);
                 }
 
                 request.setAttribute(KFSConstants.REQUEST_SEARCH_RESULTS, resultTable);
 
-                request.setAttribute(TOTALS_TABLE_KEY, totalsTable);
-                GlobalVariables.getUserSession().addObject(TOTALS_TABLE_KEY, totalsTable);
+                
+//                request.setAttribute(TOTALS_TABLE_KEY, totalsTable);
+//                GlobalVariables.getUserSession().addObject(TOTALS_TABLE_KEY, totalsTable);
 
             }
             else {
 
-                request.setAttribute(KFSConstants.REQUEST_SEARCH_RESULTS, resultTable);
+                if (LOG.isInfoEnabled()) {
+                    LOG.info("search(ActionMapping, ActionForm, HttpServletRequest, HttpServletResponse) - in FIRST ELSE - lookupable=" + lookupable);
+                }
+ request.setAttribute(KFSConstants.REQUEST_SEARCH_RESULTS, resultTable);
 
             }
 

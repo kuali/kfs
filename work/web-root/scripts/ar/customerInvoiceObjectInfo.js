db5963639929912524c29998c09727667cb4e055
Vivek Saini <vsaini@ucdavis.edu>
2008-08-11 19:23:53 +0000

    InvoiceRecurrenceMaintenanceDocument. Populates CustomerNumber and Name after tabbing out of invoiceNumber attribute.

---
+++ work/web-root/scripts/ar/customerInvoiceObjectInfo.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+function loadCustomerNumberAndName( invoiceNumberFieldName ){
+	
+    var elPrefix = findElPrefix( invoiceNumberFieldName.name );
+    var customerNumberFieldName = elPrefix + ".customerNumber";
+    var customerNameFieldName = elPrefix + ".customer.customerName";
+	var invoiceNumber = DWRUtil.getValue( invoiceNumberFieldName );
+
+	if (invoiceNumber=='') {
+		clearRecipients(customerNumberFieldName);
+		clearRecipients(customerNameFieldName);
+	} else {
+		var dwrReply = {
+			callback:function(data) {
+			if ( data != null && typeof data == 'object' ) {
+				setRecipientValue( customerNumberFieldName, data.customer.customerNumber );
+				setRecipientValue( customerNameFieldName, data.customer.customerName );
+			} else {
+				setRecipientValue( customerNumberFieldName, wrapError( "invoice not found" ), true );			
+				clearRecipients( customerNameFieldName );
+			} },
+			errorHandler:function( errorMessage ) { 
+				window.status = errorMessage;
+				setRecipientValue( customerNumberFieldName, wrapError( "invoice not found" ), true );
+				clearRecipients( customerNameFieldName );
+			}
+		};
+		CustomerInvoiceDocumentService.getInvoiceByInvoiceDocumentNumber( invoiceNumber, dwrReply );
+	}
+}
+
+

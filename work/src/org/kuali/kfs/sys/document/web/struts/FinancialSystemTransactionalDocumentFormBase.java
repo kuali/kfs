8dc51a18c4911837323881dab33692161808eb25
Eric Westfall <ewestfal@iu.edu>
2008-08-14 16:35:50 +0000

    Fixed the corrected by and document in error header fields code to use the docInfo collection instead of getAdditionalDocId1.  Also updated OJB.properties to fix an incorrect package name.
    Rice jars had to be pulled in as well since there was an update to one rice file (see KULRICE-2170)

--- work/src/org/kuali/kfs/sys/document/web/struts/FinancialSystemTransactionalDocumentFormBase.java
+++ work/src/org/kuali/kfs/sys/document/web/struts/FinancialSystemTransactionalDocumentFormBase.java
@@ -15,8 +15,16 @@
  */
 package org.kuali.kfs.sys.document.web.struts;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.sys.businessobject.FinancialSystemDocumentHeader;
 import org.kuali.kfs.sys.document.authorization.FinancialSystemTransactionalDocumentActionFlags;
 import org.kuali.rice.kns.web.struts.form.KualiTransactionalDocumentFormBase;
+import org.kuali.rice.kns.web.ui.HeaderField;
+import org.kuali.rice.kns.web.ui.KeyLabelPair;
+import org.kuali.rice.kns.workflow.service.KualiWorkflowDocument;
 
 /**
  * This class is a Financial System specific transactional document form base
@@ -30,5 +38,55 @@ public class FinancialSystemTransactionalDocumentFormBase extends KualiTransacti
         super();
         setDocumentActionFlags(new FinancialSystemTransactionalDocumentActionFlags());
     }
+
+    /**
+     * @see org.kuali.rice.kns.web.struts.form.KualiDocumentFormBase#populateHeaderFields(org.kuali.rice.kns.workflow.service.KualiWorkflowDocument)
+     */
+    @Override
+    protected void populateHeaderFields(KualiWorkflowDocument workflowDocument) {
+        super.populateHeaderFields(workflowDocument);
+        if (getDocument().getDocumentHeader() instanceof FinancialSystemDocumentHeader) {
+            FinancialSystemDocumentHeader documentHeader = (FinancialSystemDocumentHeader)getDocument().getDocumentHeader();
+            if (StringUtils.isNotBlank(documentHeader.getFinancialDocumentInErrorNumber())) {
+                extendDocInfoToThreeColumns();
+                int insertIndex = 2;
+                getDocInfo().remove(insertIndex);
+                getDocInfo().add(insertIndex, new HeaderField("DataDictionary.FinancialSystemDocumentHeader.attributes.financialDocumentInErrorNumber", documentHeader.getFinancialDocumentInErrorNumber()));
+            }
+            if (StringUtils.isNotBlank(documentHeader.getCorrectedByDocumentId())) {
+                extendDocInfoToThreeColumns();
+                int insertIndex = getNumColumns() + 2;
+                getDocInfo().remove(insertIndex);
+                getDocInfo().add(insertIndex, new HeaderField("DataDictionary.FinancialSystemDocumentHeader.attributes.correctedByDocumentId", documentHeader.getCorrectedByDocumentId()));
+            }
+        }
+    }
+    
+    /**
+     * Extends the DocInfo on the form to 3 columns if it currently has less than 3 columns.
+     * If it has exactly 3 or more columns, no action will be taken.
+     */
+    protected void extendDocInfoToThreeColumns() {
+        List<HeaderField> newDocInfo = new ArrayList<HeaderField>();
+        int currentColumns = getNumColumns();
+        int targetColumns = 3;
+        if (getNumColumns() < targetColumns) {
+            int column = 0;
+            for (HeaderField headerField : getDocInfo()) {
+                if (column + 1 > currentColumns) {
+                    newDocInfo.add(HeaderField.EMPTY_FIELD);
+                    column = (column + 1) % targetColumns;
+                }
+                newDocInfo.add(headerField);
+                column = (column + 1) % targetColumns;
+            }
+            // fill out the final row with empty columns
+            while (newDocInfo.size() % targetColumns != 0) {
+                newDocInfo.add(HeaderField.EMPTY_FIELD);
+            }
+            setDocInfo(newDocInfo);
+            setNumColumns(3);
+        }
+    }    
 
 }

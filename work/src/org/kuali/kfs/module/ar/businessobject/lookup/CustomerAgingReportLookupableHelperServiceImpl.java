1098962f3fe6bcec93cc7318f6b6f92040a401c4
Matt	McQuarrie <matt.mcquarrie@colostate.edu>
2008-08-27 19:11:23 +0000

    IN PROGRESS - issue KULAR-373: Reports: Customer Aging Report - Service/View Development https://test.kuali.org/jira/browse/KULAR-373
    starting to use businessObjectService to get customerInvoiceDetails based on AcccountNumber

--- work/src/org/kuali/kfs/module/ar/businessobject/lookup/CustomerAgingReportLookupableHelperServiceImpl.java
+++ work/src/org/kuali/kfs/module/ar/businessobject/lookup/CustomerAgingReportLookupableHelperServiceImpl.java
@@ -15,9 +15,10 @@
  */
 package org.kuali.kfs.module.ar.businessobject.lookup;
 
-import java.util.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -25,8 +26,11 @@ import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.module.ar.businessobject.Customer;
 import org.kuali.kfs.module.ar.businessobject.CustomerAgingReportDetail;
+import org.kuali.kfs.module.ar.businessobject.CustomerInvoiceDetail;
 import org.kuali.kfs.module.ar.document.CustomerInvoiceDocument;
+import org.kuali.kfs.module.ar.document.service.CustomerInvoiceDetailService;
 import org.kuali.kfs.module.ar.document.service.CustomerInvoiceDocumentService;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.context.SpringContext;
@@ -36,6 +40,7 @@ import org.kuali.rice.kns.bo.PersistableBusinessObject;
 import org.kuali.rice.kns.datadictionary.mask.Mask;
 import org.kuali.rice.kns.lookup.CollectionIncomplete;
 import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
+import org.kuali.rice.kns.service.BusinessObjectService;
 import org.kuali.rice.kns.service.DataDictionaryService;
 import org.kuali.rice.kns.service.DateTimeService;
 import org.kuali.rice.kns.util.GlobalVariables;
@@ -57,9 +62,10 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
     private EncryptionService encryptionService;
     private DataDictionaryService dataDictionaryService;
     private Map fieldConversions;
-    private CustomerInvoiceDocumentService customerInvoiceDocumentService = SpringContext.getBean(CustomerInvoiceDocumentService.class);
-    
-    
+    private CustomerInvoiceDetailService customerInvoiceDetailService = SpringContext.getBean(CustomerInvoiceDetailService.class);
+    private CustomerInvoiceDocumentService customerInvoiceDocumentService = SpringContext.getBean(CustomerInvoiceDocumentService.class);   
+    private BusinessObjectService businessObjectService;
+
     /**
      * Get the search results that meet the input search criteria.
      * 
@@ -72,59 +78,60 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
 
         setBackLocation((String) fieldValues.get(KFSConstants.BACK_LOCATION));
         setDocFormKey((String) fieldValues.get(KFSConstants.DOC_FORM_KEY));
-        // MJM check what fieldValues returns; what is the propertyName for Account Number?
+
+        Collection<CustomerInvoiceDetail> invoiceDetails= getCustomerInvoiceDetailsByAccountNumber((String) fieldValues.get(KFSConstants.ACCOUNT_NUMBER_PROPERTY_NAME));
 
         Date today = SpringContext.getBean(DateTimeService.class).getCurrentDate();
         Date reportDate = today;
         Date reportDateCutoff1; // MJM need to add 4 more cutoff dates based on fieldValues.get...
-        
+
         LOG.debug("\n\n\n\n \t\t\t\t***********************    customerInvoiceDocumentService should not be null \n\n");
 
-       // List invoices = (List) customerInvoiceDocumentService.getAllCustomerInvoiceDocuments();
+        // List invoices = (List) customerInvoiceDocumentService.getAllCustomerInvoiceDocuments();
         Collection<CustomerInvoiceDocument> invoices = customerInvoiceDocumentService.getAllCustomerInvoiceDocuments();
         CustomerAgingReportDetail testcustomer1 = new CustomerAgingReportDetail();
-       
-        Map<String,Object> knownCustomers = new HashMap<String,Object>(invoices.size());
-      // EXAMPLE: fieldNamesValuesForParameter.put("parameterNamespaceCode",CustomerInvoiceDocumentBatchStep.RUN_INDICATOR_PARAMETER_NAMESPACE_CODE);       
-       
+
+        Map<String, Object> knownCustomers = new HashMap<String, Object>(invoices.size());
+        // EXAMPLE: fieldNamesValuesForParameter.put("parameterNamespaceCode",CustomerInvoiceDocumentBatchStep.
+        // RUN_INDICATOR_PARAMETER_NAMESPACE_CODE);
+
         KualiDecimal totalbalance = new KualiDecimal(0.00);
         CustomerAgingReportDetail custDetail = null;
-        
+
         // iterate over all invoices consolidating balances for each customer
-        for (CustomerInvoiceDocument cid : invoices) {
-           
-            Date approvalDate = cid.getCustomerPurchaseOrderDate();  
-        
-           
-            if (knownCustomers.containsKey(cid.getCustomer().getCustomerNumber()) && approvalDate.before(reportDate)) {
+        for (CustomerInvoiceDetail cid : invoiceDetails) {
+
+            Date approvalDate = today;//cid.getCustomerPurchaseOrderDate();
+            String customerNumber = cid.getCustomerInvoiceDocument().getCustomer().getCustomerNumber();
+            String customerName = cid.getCustomerInvoiceDocument().getCustomer().getCustomerName();
+
+            if (knownCustomers.containsKey(customerNumber) && approvalDate.before(reportDate) && (customerInvoiceDetailService.getOpenAmount(cid).isNonZero())) {
                 // not a new customer id and invoice approvalDate is valid
-                custDetail = (CustomerAgingReportDetail)knownCustomers.get(cid.getCustomer().getCustomerNumber());
+                custDetail = (CustomerAgingReportDetail) knownCustomers.get(customerNumber);
             }
-            else { //if (approvalDate.before(reportDate)) {
+            else { // if (approvalDate.before(reportDate)) {
                 // new customer id, so create a new CustomerAgingReportDetail
                 custDetail = new CustomerAgingReportDetail();
-                custDetail.setCustomerName(cid.getCustomer().getCustomerName());
-                custDetail.setCustomerNumber(cid.getCustomer().getCustomerNumber());
+                custDetail.setCustomerName(customerName);
+                custDetail.setCustomerNumber(customerNumber);
                 // if (approvalDate.
-                custDetail.setUnpaidBalance0to30(cid.getTotalDollarAmount());
-                knownCustomers.put(cid.getCustomer().getCustomerNumber(), custDetail);
+                custDetail.setUnpaidBalance0to30(cid.getAmount());
+                knownCustomers.put(customerNumber, custDetail);
             }
-              
-             
-       
+
 
             if (LOG.isInfoEnabled()) {
-                LOG.info("\t\tcustDetail=\t" + custDetail.getCustomerNumber() + "\t"+custDetail.getUnpaidBalance0to30());
+                LOG.info("\t\tcustDetail=\t" + custDetail.getCustomerNumber() + "\t" + custDetail.getUnpaidBalance0to30());
             }
         }
-           
 
 
-            if (LOG.isInfoEnabled()) {
-           //     LOG.info("CustomerInvoiceDocument cidgetTotalDollarAmount=" + cid.getTotalDollarAmount() + "\t\tCustomerName=" + cid.getCustomer().getCustomerNumber());
-            }
+        if (LOG.isInfoEnabled()) {
+            // LOG.info("CustomerInvoiceDocument cidgetTotalDollarAmount=" + cid.getTotalDollarAmount() + "\t\tCustomerName=" +
+            // cid.getCustomer().getCustomerNumber());
+        }
 
-  
+
         // create some fake entries to test with
         CustomerAgingReportDetail matt = new CustomerAgingReportDetail();
         matt.setCustomerName("Matt");
@@ -134,7 +141,7 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         matt.setUnpaidBalance61to90(KualiDecimal.ZERO);
         matt.setUnpaidBalance91toSYSPR(KualiDecimal.ZERO);
         matt.setUnpaidBalanceSYSPRplus1orMore(KualiDecimal.ZERO);
-        
+
         CustomerAgingReportDetail justin = new CustomerAgingReportDetail();
         justin.setCustomerName("Justin");
         justin.setCustomerNumber("LED12346");
@@ -143,7 +150,7 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         justin.setUnpaidBalance61to90(KualiDecimal.ZERO);
         justin.setUnpaidBalance91toSYSPR(KualiDecimal.ZERO);
         justin.setUnpaidBalanceSYSPRplus1orMore(KualiDecimal.ZERO);
-        
+
         CustomerAgingReportDetail patty = new CustomerAgingReportDetail();
         patty.setCustomerName("Patty");
         patty.setCustomerNumber("MGR12347");
@@ -152,8 +159,9 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         patty.setUnpaidBalance61to90(KualiDecimal.ZERO);
         patty.setUnpaidBalance91toSYSPR(KualiDecimal.ZERO);
         patty.setUnpaidBalanceSYSPRplus1orMore(KualiDecimal.ZERO);
-        
-        // List results = accountBalanceService.findAccountBalanceByConsolidation(universityFiscalYear, chartOfAccountsCode, accountNumber, subAccountNumber, isCostShareExcluded, isConsolidated, pendingEntryCode);
+
+        // List results = accountBalanceService.findAccountBalanceByConsolidation(universityFiscalYear, chartOfAccountsCode,
+        // accountNumber, subAccountNumber, isCostShareExcluded, isConsolidated, pendingEntryCode);
         // List<CustomerAgingReportDetail> results = new ArrayList<CustomerAgingReportDetail>(3);
         List results = new ArrayList();
         results.add(matt);
@@ -161,12 +169,24 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         results.add(patty);
         for (Object detail : knownCustomers.values()) {
             results.add(detail);
-        }   
-  
-        
+        }
+
+
         LOG.info("\t\t sending results back... \n\n\n");
         return new CollectionIncomplete(results, new Long(results.size()));
-    }    
+    }
+
+    /**
+     * @return a List of the CustomerInvoiceDetails associated with a given Account Number
+     */
+    @SuppressWarnings("unchecked")
+    public Collection<CustomerInvoiceDetail> getCustomerInvoiceDetailsByAccountNumber(String accountNumber) {
+        Map args = new HashMap();
+        args.put("accountNumber", accountNumber);
+        return businessObjectService.findMatching(CustomerInvoiceDetail.class, args);
+
+    }
+
 
     /**
      * @return a List of the names of fields which are marked in data dictionary as return fields.
@@ -174,18 +194,18 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
     @Override
     public List getReturnKeys() {
         List returnKeys;
-//        if (fieldConversions != null && !fieldConversions.isEmpty()) {
-            returnKeys = new ArrayList(fieldConversions.keySet());
-            LOG.info("\n\n\t\t THIS OVERRIDE IS WORKING (GETRETURNKEYS)... \n\n\n");
-//        }
-//        else {
-//            returnKeys = getPersistenceStructureService().listPrimaryKeyFieldNames(getBusinessObjectClass());
-//        }
-//
+        // if (fieldConversions != null && !fieldConversions.isEmpty()) {
+        returnKeys = new ArrayList(fieldConversions.keySet());
+        LOG.info("\n\n\t\t THIS OVERRIDE IS WORKING (GETRETURNKEYS)... \n\n\n");
+        // }
+        // else {
+        // returnKeys = getPersistenceStructureService().listPrimaryKeyFieldNames(getBusinessObjectClass());
+        // }
+        //
         return returnKeys;
 
     }
-    
+
     /**
      * @return a List of the names of fields which are marked in data dictionary as return fields.
      */
@@ -198,7 +218,7 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         if (getReferencesToRefresh() != null) {
             parameters.put(KNSConstants.REFERENCES_TO_REFRESH, getReferencesToRefresh());
         }
-        
+
         String encryptedList = "";
 
         Iterator returnKeys = getReturnKeys().iterator();
@@ -217,14 +237,14 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
                 fieldNm = (String) fieldConversions.get(fieldNm);
             }
 
-            if (StringUtils.isNotBlank(displayWorkgroup) && !GlobalVariables.getUserSession().getUniversalUser().isMember( displayWorkgroup )) {
-//                try {
-//                    fieldVal = encryptionService.encrypt(fieldVal);
-//                }
-//                catch (GeneralSecurityException e) {
-//                    LOG.error("Exception while trying to encrypted value for inquiry framework.", e);
-//                    throw new RuntimeException(e);
-//                }
+            if (StringUtils.isNotBlank(displayWorkgroup) && !GlobalVariables.getUserSession().getUniversalUser().isMember(displayWorkgroup)) {
+                // try {
+                // fieldVal = encryptionService.encrypt(fieldVal);
+                // }
+                // catch (GeneralSecurityException e) {
+                // LOG.error("Exception while trying to encrypted value for inquiry framework.", e);
+                // throw new RuntimeException(e);
+                // }
 
                 // add to parameter list so that KualiInquiryAction can identify which parameters are encrypted
                 if (encryptedList.equals("")) {
@@ -234,8 +254,8 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
                     encryptedList = encryptedList + KNSConstants.FIELD_CONVERSIONS_SEPERATOR + fieldNm;
                 }
             }
-            
-            //need to format date in url
+
+            // need to format date in url
             if (fieldVal instanceof Date) {
                 DateFormatter dateFormatter = new DateFormatter();
                 fieldVal = dateFormatter.format(fieldVal);
@@ -250,11 +270,11 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         }
 
         return parameters;
-    }    
-    
+    }
+
     /**
-     * 
      * This method performs the lookup and returns a collection of lookup items
+     * 
      * @param lookupForm
      * @param kualiLookupable
      * @param resultTable
@@ -272,41 +292,43 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
         else {
             displayList = getSearchResultsUnbounded(lookupForm.getFieldsForLookup());
         }
-// MJM get resultTable populated here
-        
-        HashMap<String,Class> propertyTypes = new HashMap<String, Class>(); 
-        
+        // MJM get resultTable populated here
+
+        HashMap<String, Class> propertyTypes = new HashMap<String, Class>();
+
         boolean hasReturnableRow = false;
-        
+
         // iterate through result list and wrap rows with return url and action urls
         for (Iterator iter = displayList.iterator(); iter.hasNext();) {
             BusinessObject element = (BusinessObject) iter.next();
 
-           // String returnUrl = getReturnUrl(element, lookupForm.getFieldConversions(), lookupForm.getLookupableImplServiceName());
-           // String actionUrls = getActionUrls(element);
+            // String returnUrl = getReturnUrl(element, lookupForm.getFieldConversions(),
+            // lookupForm.getLookupableImplServiceName());
+            // String actionUrls = getActionUrls(element);
             String returnUrl = "www.bigfrickenRETURNurl";
             String actionUrls = "www.someACTIONurl";
-            
+
             List<Column> columns = getColumns();
             for (Iterator iterator = columns.iterator(); iterator.hasNext();) {
-                
+
                 Column col = (Column) iterator.next();
                 Formatter formatter = col.getFormatter();
 
                 // pick off result column from result list, do formatting
                 String propValue = KNSConstants.EMPTY_STRING;
                 Object prop = ObjectUtils.getPropertyValue(element, col.getPropertyName());
-                
+
                 // set comparator and formatter based on property type
                 Class propClass = propertyTypes.get(col.getPropertyName());
-//                if ( propClass == null ) {
-//                    try {
-//                        propClass = ObjectUtils.getPropertyType( element, col.getPropertyName(), getPersistenceStructureService() );
-//                        propertyTypes.put( col.getPropertyName(), propClass );
-//                    } catch (Exception e) {
-//                        throw new RuntimeException("Cannot access PropertyType for property " + "'" + col.getPropertyName() + "' " + " on an instance of '" + element.getClass().getName() + "'.", e);
-//                    }
-//                }
+                // if ( propClass == null ) {
+                // try {
+                // propClass = ObjectUtils.getPropertyType( element, col.getPropertyName(), getPersistenceStructureService() );
+                // propertyTypes.put( col.getPropertyName(), propClass );
+                // } catch (Exception e) {
+                // throw new RuntimeException("Cannot access PropertyType for property " + "'" + col.getPropertyName() + "' " +
+                // " on an instance of '" + element.getClass().getName() + "'.", e);
+                // }
+                // }
 
                 // formatters
                 if (prop != null) {
@@ -314,7 +336,7 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
                     if (prop instanceof Boolean) {
                         formatter = new BooleanFormatter();
                     }
-                    
+
                     // for Dates, always use DateFormatter
                     if (prop instanceof Date) {
                         formatter = new DateFormatter();
@@ -322,9 +344,9 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
 
                     // for collection, use the list formatter if a formatter hasn't been defined yet
                     if (prop instanceof Collection && formatter == null) {
-                    formatter = new CollectionFormatter();
+                        formatter = new CollectionFormatter();
                     }
-                    
+
                     if (formatter != null) {
                         propValue = (String) formatter.format(prop);
                     }
@@ -336,7 +358,7 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
                 // comparator
                 col.setComparator(CellComparatorHelper.getAppropriateComparatorForPropertyClass(propClass));
                 col.setValueComparator(CellComparatorHelper.getAppropriateValueComparatorForPropertyClass(propClass));
-                
+
                 // check security on field and do masking if necessary
                 boolean viewAuthorized = getAuthorizationService().isAuthorizedToViewAttribute(GlobalVariables.getUserSession().getUniversalUser(), element.getClass().getName(), col.getPropertyName());
                 if (!viewAuthorized) {
@@ -352,10 +374,10 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
             }
 
             ResultRow row = new ResultRow(columns, returnUrl, actionUrls);
-            if ( element instanceof PersistableBusinessObject ) {
-                row.setObjectId(((PersistableBusinessObject)element).getObjectId());
+            if (element instanceof PersistableBusinessObject) {
+                row.setObjectId(((PersistableBusinessObject) element).getObjectId());
             }
-            
+
             boolean rowReturnable = isResultReturnable(element);
             row.setRowReturnable(rowReturnable);
             if (rowReturnable) {
@@ -368,5 +390,13 @@ public class CustomerAgingReportLookupableHelperServiceImpl extends KualiLookupa
 
         return displayList;
     }
+
+    /**
+     * @param businessObjectService
+     */
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+
 
 }

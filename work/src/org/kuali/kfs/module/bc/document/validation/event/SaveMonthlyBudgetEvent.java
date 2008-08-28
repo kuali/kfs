aec716130949197ccf5459c520e81bb4228d532a
Gary	Palmer <gwpalmer@iu.edu>
2008-08-28 15:46:01 +0000

    initial save monthly budget validation

---
+++ work/src/org/kuali/kfs/module/bc/document/validation/event/SaveMonthlyBudgetEvent.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.bc.document.validation.event;
+
+import org.kuali.kfs.module.bc.businessobject.BudgetConstructionMonthly;
+import org.kuali.kfs.module.bc.businessobject.PendingBudgetConstructionGeneralLedger;
+import org.kuali.kfs.module.bc.document.BudgetConstructionDocument;
+import org.kuali.kfs.module.bc.document.validation.SaveMonthlyBudgetRule;
+import org.kuali.rice.kns.document.Document;
+import org.kuali.rice.kns.rule.BusinessRule;
+import org.kuali.rice.kns.rule.event.KualiDocumentEventBase;
+
+/**
+ * Defines the save monthly budget event for the monthly budget screen.
+ */
+public class SaveMonthlyBudgetEvent extends KualiDocumentEventBase {
+    
+    private BudgetConstructionDocument budgetConstructionDocument;
+    private BudgetConstructionMonthly budgetConstructionMonthly;
+
+    public SaveMonthlyBudgetEvent (String errorPathPrefix, Document document, BudgetConstructionMonthly budgetConstructionMonthly){
+        super("", errorPathPrefix, document);
+        this.budgetConstructionDocument = (BudgetConstructionDocument) document;
+        this.budgetConstructionMonthly = budgetConstructionMonthly;
+    }
+
+    /**
+     * @see org.kuali.rice.kns.rule.event.KualiDocumentEvent#getRuleInterfaceClass()
+     */
+    public Class getRuleInterfaceClass() {
+
+        return SaveMonthlyBudgetRule.class;
+    }
+
+    /**
+     * @see org.kuali.rice.kns.rule.event.KualiDocumentEvent#invokeRuleMethod(org.kuali.rice.kns.rule.BusinessRule)
+     */
+    public boolean invokeRuleMethod(BusinessRule rule) {
+        // TODO Auto-generated method stub
+        return ((SaveMonthlyBudgetRule<BudgetConstructionDocument, BudgetConstructionMonthly>) rule).processSaveMonthlyBudgetRules(budgetConstructionDocument, budgetConstructionMonthly);
+    }
+
+    /**
+     * Gets the budgetConstructionDocument attribute. 
+     * @return Returns the budgetConstructionDocument.
+     */
+    public BudgetConstructionDocument getBudgetConstructionDocument() {
+        return budgetConstructionDocument;
+    }
+
+    /**
+     * Sets the budgetConstructionDocument attribute value.
+     * @param budgetConstructionDocument The budgetConstructionDocument to set.
+     */
+    public void setBudgetConstructionDocument(BudgetConstructionDocument budgetConstructionDocument) {
+        this.budgetConstructionDocument = budgetConstructionDocument;
+    }
+
+    /**
+     * Gets the budgetConstructionMonthly attribute. 
+     * @return Returns the budgetConstructionMonthly.
+     */
+    public BudgetConstructionMonthly getBudgetConstructionMonthly() {
+        return budgetConstructionMonthly;
+    }
+
+    /**
+     * Sets the budgetConstructionMonthly attribute value.
+     * @param budgetConstructionMonthly The budgetConstructionMonthly to set.
+     */
+    public void setBudgetConstructionMonthly(BudgetConstructionMonthly budgetConstructionMonthly) {
+        this.budgetConstructionMonthly = budgetConstructionMonthly;
+    }
+
+}

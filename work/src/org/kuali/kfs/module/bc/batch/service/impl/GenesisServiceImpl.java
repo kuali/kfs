6c480e510e722966604c58c73ab86f31c8598337
Fred Peterson <fpeterso@indiana.edu>
2008-09-03 20:12:22 +0000

    orphan accounts/Update Batch FBP

--- work/src/org/kuali/kfs/module/bc/batch/service/impl/GenesisServiceImpl.java
+++ work/src/org/kuali/kfs/module/bc/batch/service/impl/GenesisServiceImpl.java
@@ -23,6 +23,8 @@ import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSConstants.BudgetConstructionConstants;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
 
 @Transactional
 public class GenesisServiceImpl implements GenesisService {
@@ -80,22 +82,38 @@ public class GenesisServiceImpl implements GenesisService {
          *  from the budget construction position table
          *  the calling order is constrained further if an institution implements referential integrity
          */
+        LOG.warn(String.format("\n\nStarting BC Update:\n  clear hanging locks\n"));
         genesisDao.clearHangingBCLocks(BaseYear);
+        LOG.warn(String.format("\n  validate object classes\n"));
         genesisDao.ensureObjectClassRIForBudget(BaseYear);
+        LOG.warn(String.format("\n  create new documents\n"));
         genesisDao.createNewBCDocumentsFromGLCSF(BaseYear, GLUpdatesAllowed(BaseYear), CSFUpdatesAllowed(BaseYear));
         if (GLUpdatesAllowed(BaseYear)) {
+            LOG.warn(String.format("\n  add rows to pending general ledger\n"));
             genesisDao.updateToPBGL(BaseYear);
         }
         boolean CSFOK = CSFUpdatesAllowed(BaseYear);
         boolean PSSynchOK = BatchPositionSynchAllowed(BaseYear);
         boolean BCUpdatesAllowed = IsBudgetConstructionInUpdateMode(BaseYear);
+        LOG.warn(String.format("\n  update Budget Construction Position\n"));
         budgetConstructionHumanResourcesPayrollInterfaceService.refreshBudgetConstructionPosition(BaseYear,PSSynchOK,CSFOK);
         LOG.warn(String.format("\n  intended incumbent"));
         budgetConstructionHumanResourcesPayrollInterfaceService.refreshBudgetConstructionIntendedIncumbent(BaseYear,PSSynchOK,CSFOK,BCUpdatesAllowed);
         if (CSFOK) {
+            LOG.warn(String.format("\n  build appointment funding\n"));
             genesisDao.buildAppointmentFundingAndBCSF(BaseYear);
         }
+        LOG.warn(String.format("\n  rebuild the organization hierarchy\n"));
         genesisDao.rebuildOrganizationHierarchy(BaseYear);
+        // look for accounts coming from payroll or GL that have not been entered into the Budget Construction Accounting table
+        Integer requestFiscalYear = BaseYear + 1;
+        LOG.warn(String.format("\n  look for accounts missing from Budget Construction Accounting\n"));
+        HashMap<String,String[]> missingAccounts = (HashMap<String,String[]>) genesisDao.verifyAccountsAreAccessible(requestFiscalYear);
+        for (Map.Entry<String,String[]> missingAccount : missingAccounts.entrySet())
+        {
+            String[] missingValues = missingAccount.getValue();
+            LOG.warn(String.format("    (chart: %s, account: %s) not found in Budget Construction Accounting\n", missingValues[0], missingValues[1]));
+        }
     }
 
     public void genesisStep(Integer BaseYear) {
@@ -133,6 +151,17 @@ public class GenesisServiceImpl implements GenesisService {
         LOG.warn("\n  reset control flags");
         genesisDao.setControlFlagsAtTheEndOfGenesis(BaseYear);
         LOG.warn("\n  end of genesis");
+    }
+    
+    /*
+     *   look of accounts from the payroll (CSF) or GL that came into budget construction but are *not* in the budget construction accounting table.
+     *   this can be due to an oversight on the part of the chart manager, or to problems with the current year's budget control.
+     *   such accounts will not appear in the pull-up list, since they can't be added to the reporting hierarchy, which is built from budget construction accounting.
+     *   this method is provided for use by a report an institution might want to write.  such accounts will always appear in the log from bCUpdateStep above, whether this method is used or not.
+     */
+    public Map verifyAccountsAreAccessible(Integer requestFiscalYear)
+    {
+        return genesisDao.verifyAccountsAreAccessible(requestFiscalYear);
     }
 
     public void setGenesisDao(GenesisDao genesisDao) {

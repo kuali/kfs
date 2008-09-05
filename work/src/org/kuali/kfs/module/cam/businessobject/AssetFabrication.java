51738b37eba1b2fa25be5e6a8bed288af5d12a2d
Philip Berg <pcberg@iu.edu>
2008-09-05 21:29:09 +0000

    KULCAP-430: Fabrication needs its own document type code and title.

---
+++ work/src/org/kuali/kfs/module/cam/businessobject/AssetFabrication.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.cam.businessobject;
+
+/**
+ * This BO subclasses Asset and is the same table. We have it so that the maintenance document
+ * framework lets us set a different document type code and title for this document.
+ * 
+ * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
+ */
+public class AssetFabrication extends Asset {
+
+}

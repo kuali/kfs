63d8015c389415d65d491328b902fe2683553f47
Venkat PremChandran <vpremcha@kuali.org>
2008-09-16 21:20:22 +0000

    EInvoice - Refactored a lot and added xml namespace handling

---
+++ work/src/org/kuali/kfs/module/purap/service/ElectronicInvoiceHelperService.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.service;
+
+import java.util.List;
+
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoice;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceLoad;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceLoadSummary;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceOrder;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceRejectReason;
+import org.kuali.kfs.module.purap.document.ElectronicInvoiceRejectDocument;
+
+public interface ElectronicInvoiceHelperService {
+
+    public boolean loadElectronicInvoices();
+    
+    public boolean doMatchingProcess(ElectronicInvoiceRejectDocument rejectDocument);
+    
+    public boolean createPaymentRequest(ElectronicInvoiceRejectDocument rejectDocument);
+    
+}

24759d48117577f738157fa1dec3d911259d9e7e
Queing Lai <queing.lai@usc.edu>
2008-09-18 06:29:29 +0000

    KULCAP-607  Asset - submitting New asset returns server error.

--- work/src/org/kuali/kfs/module/cam/document/validation/impl/AssetGlobalRule.java
+++ work/src/org/kuali/kfs/module/cam/document/validation/impl/AssetGlobalRule.java
@@ -25,7 +25,6 @@ import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.coa.businessobject.Account;
 import org.kuali.kfs.coa.businessobject.ObjectCode;
-import org.kuali.kfs.gl.businessobject.UniversityDate;
 import org.kuali.kfs.module.cam.CamsConstants;
 import org.kuali.kfs.module.cam.CamsKeyConstants;
 import org.kuali.kfs.module.cam.CamsPropertyConstants;
@@ -42,6 +41,7 @@ import org.kuali.kfs.module.cam.document.service.AssetService;
 import org.kuali.kfs.module.cam.document.service.AssetLocationService.LocationField;
 import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.kfs.sys.businessobject.FinancialSystemDocumentHeader;
+import org.kuali.kfs.sys.businessobject.State;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.service.GeneralLedgerPendingEntryService;
 import org.kuali.kfs.sys.service.ParameterService;
@@ -166,13 +166,15 @@ public class AssetGlobalRule extends MaintenanceDocumentRuleBase {
                 valid &= false;
             }
         }
+
         if (StringUtils.isNotBlank(assetGlobalDetail.getOffCampusStateCode())) {
-            assetGlobalDetail.refreshReferenceObject(CamsPropertyConstants.AssetGlobalDetail.OFF_CAMPUS_STATE);
-            if (ObjectUtils.isNull(assetGlobalDetail.getOffCampusState())) {
+           State stateCode = assetGlobalDetail.getOffCampusState();
+            if (ObjectUtils.isNull(stateCode)) {
                 GlobalVariables.getErrorMap().putError(CamsPropertyConstants.AssetGlobalDetail.OFF_CAMPUS_STATE_CODE, CamsKeyConstants.AssetLocation.ERROR_INVALID_OFF_CAMPUS_STATE, assetGlobalDetail.getOffCampusStateCode());
                 valid &= false;
             }
         }
+
         return valid;
     }
 

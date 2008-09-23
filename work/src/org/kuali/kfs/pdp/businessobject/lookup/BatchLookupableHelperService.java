7d2c12830af687e796ae040365eea283eb75f760
Daniela Balmus <db483@cornell.edu>
2008-09-23 20:30:12 +0000

    KFSMI-1249 Add additional search criteria to get batches when a simpe date is specified for file creation time

--- work/src/org/kuali/kfs/pdp/businessobject/lookup/BatchLookupableHelperService.java
+++ work/src/org/kuali/kfs/pdp/businessobject/lookup/BatchLookupableHelperService.java
@@ -15,12 +15,16 @@
  */
 package org.kuali.kfs.pdp.businessobject.lookup;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.ojb.broker.query.Criteria;
 import org.kuali.kfs.pdp.PdpConstants;
 import org.kuali.kfs.pdp.PdpKeyConstants;
 import org.kuali.kfs.pdp.PdpParameterConstants;
@@ -29,12 +33,18 @@ import org.kuali.kfs.pdp.businessobject.Batch;
 import org.kuali.kfs.pdp.businessobject.PaymentDetail;
 import org.kuali.kfs.pdp.document.service.BatchMaintenanceService;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSKeyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.bo.PersistableBusinessObject;
 import org.kuali.rice.kns.bo.user.UniversalUser;
+import org.kuali.rice.kns.dao.LookupDao;
 import org.kuali.rice.kns.exception.ValidationException;
 import org.kuali.rice.kns.lookup.HtmlData;
 import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
 import org.kuali.rice.kns.lookup.HtmlData.AnchorHtmlData;
+import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.kns.service.DateTimeService;
 import org.kuali.rice.kns.service.KualiConfigurationService;
 import org.kuali.rice.kns.util.GlobalVariables;
 import org.kuali.rice.kns.util.KNSConstants;
@@ -44,13 +54,16 @@ import org.kuali.rice.kns.web.format.BooleanFormatter;
 public class BatchLookupableHelperService extends KualiLookupableHelperServiceImpl {
     private KualiConfigurationService configurationService;
     private BatchMaintenanceService batchMaintenanceService;
+    private LookupDao lookupDao;
+    private DateTimeService dateTimeService;
 
     /**
      * @see org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl#getSearchResults(java.util.Map)
      */
     @Override
-    public List<? extends BusinessObject> getSearchResults(Map<String, String> arg0) {
+    public List<? extends BusinessObject> getSearchResults(Map<String, String> fieldValues) {
         Map parameters = super.getParameters();
+
         if (parameters.containsKey(PdpParameterConstants.BatchConstants.ACTION_SUCCESSFUL_PARAM)) {
             String[] actionSuccessRequestParm = (String[]) parameters.get(PdpParameterConstants.BatchConstants.ACTION_SUCCESSFUL_PARAM);
             Boolean actionSuccess = (Boolean) (new BooleanFormatter()).convertFromPresentationFormat(actionSuccessRequestParm[0]);
@@ -69,7 +82,66 @@ public class BatchLookupableHelperService extends KualiLookupableHelperServiceIm
             }
         }
 
-        return super.getSearchResults(arg0);
+        if (fieldValues.containsKey(PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME)) {
+            String fileCreationTimeValue = fieldValues.get(PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME);
+            
+            // if file creation time value is not empty and does not contain wildcards we have to create additional search criteria
+            // to get the batches on that date;
+            // that is because the file creation timestamp is a Timestamp in the BO but comes as a date from the GUI - we don't want
+            // to have the user enter the time too
+            if (!StringUtils.isEmpty(fileCreationTimeValue)) {
+                String wildCards = "";
+                for (int i = 0; i < KFSConstants.QUERY_CHARACTERS.length; i++) {
+                    wildCards += KFSConstants.QUERY_CHARACTERS[i];
+                }
+                if (StringUtils.containsNone(fileCreationTimeValue, wildCards)) {
+
+                    fieldValues.remove(PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME);
+
+                    Criteria additionalCriteria = createAdditionalCriteria(fileCreationTimeValue);
+
+                    boolean unbounded = false;
+                    boolean usePrimaryKeyValuesOnly = getLookupService().allPrimaryKeyValuesPresentAndNotWildcard(getBusinessObjectClass(), fieldValues);
+                    List<PersistableBusinessObject> searchResults = (List) lookupDao.findCollectionBySearchHelper(getBusinessObjectClass(), fieldValues, unbounded, usePrimaryKeyValuesOnly, additionalCriteria);
+                    return searchResults;
+
+                }
+            }
+        }
+
+        // We ought to call the findCollectionBySearchHelper that would accept the additionalCriteria
+
+        List results = super.getSearchResults(fieldValues);
+        return results;
+    }
+    
+    /**
+     * This method creates additional criteria for the lookup when fileCreationTime is a single date (does not have any wildcards);
+     * The file creation timestamp is a Timestamp in the BO but comes as a date from the GUI - we don't want to have the user enter the time too.
+     * 
+     * @param fileCreationTimeValue
+     * @return
+     */
+    private Criteria createAdditionalCriteria(String fileCreationTimeValue) {
+        Criteria additionalCriteria = new Criteria();
+        try {
+            Date startDate = dateTimeService.convertToSqlDate(fileCreationTimeValue);
+            Timestamp startTime = new Timestamp(startDate.getTime());
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(startTime);
+            calendar.add(Calendar.HOUR, 24);
+            Timestamp endTime = new Timestamp(calendar.getTimeInMillis());
+
+            additionalCriteria.addLessThan(PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME, endTime);
+            additionalCriteria.addGreaterOrEqualThan(PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME, startTime);
+        }
+        catch (Exception e) {
+            GlobalVariables.getErrorMap().putError(PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME, KFSKeyConstants.ERROR_DATE_TIME, SpringContext.getBean(DataDictionaryService.class).getAttributeLabel(Batch.class, PdpPropertyConstants.BatchConstants.Fields.FILE_CREATION_TIME));
+            throw new ValidationException("errors in search criteria");
+        }
+
+        return additionalCriteria;
     }
 
     /**
@@ -206,6 +278,22 @@ public class BatchLookupableHelperService extends KualiLookupableHelperServiceIm
 
     public void setBatchMaintenanceService(BatchMaintenanceService batchMaintenanceService) {
         this.batchMaintenanceService = batchMaintenanceService;
+    }
+
+    public DateTimeService getDateTimeService() {
+        return dateTimeService;
+    }
+
+    public void setDateTimeService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+
+    public LookupDao getLookupDao() {
+        return lookupDao;
+    }
+
+    public void setLookupDao(LookupDao lookupDao) {
+        this.lookupDao = lookupDao;
     }
 
 }

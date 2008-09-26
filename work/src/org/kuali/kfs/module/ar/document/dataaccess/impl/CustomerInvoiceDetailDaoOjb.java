d3509d37a36b87e33772ffd375d3ac098e74bae7
Olga Kizler <olga.kizler@usc.edu>
2008-09-26 23:50:30 +0000

    KULAR-311

---
+++ work/src/org/kuali/kfs/module/ar/document/dataaccess/impl/CustomerInvoiceDetailDaoOjb.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.ar.document.dataaccess.impl;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.QueryFactory;
+import org.kuali.kfs.module.ar.businessobject.CustomerInvoiceDetail;
+import org.kuali.kfs.module.ar.document.dataaccess.CustomerInvoiceDetailDao;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.rice.kns.dao.impl.PlatformAwareDaoBaseOjb;
+import org.kuali.rice.kns.util.KNSPropertyConstants;
+
+public class CustomerInvoiceDetailDaoOjb extends PlatformAwareDaoBaseOjb implements CustomerInvoiceDetailDao{
+
+    /**
+     * This method retrieves all CustomerInvoiceDetail objects for the accountNumber if invoiceNumber is from the invoiceNumberList
+     * @return CustomerInvoiceDetail objects
+     */
+    public Collection getCustomerInvoiceDetailsByAccountNumberByInvoiceDocumentNumbers(String accountNumber,List documentNumbers) {
+        Criteria criteria = new Criteria();
+        criteria.addIn(KNSPropertyConstants.DOCUMENT_NUMBER, documentNumbers);
+        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.ACCOUNT_NUMBER, accountNumber);
+        
+        return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(CustomerInvoiceDetail.class, criteria));
+    }
+}

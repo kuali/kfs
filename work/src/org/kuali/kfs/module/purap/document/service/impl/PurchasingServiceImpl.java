653b3cfa98b8a2acca4279f0871551f1baf29a3d
Cathy Tan <tanc@ais.msu.edu>
2008-09-29 18:31:18 +0000

    KULPURAP-2926

--- work/src/org/kuali/kfs/module/purap/document/service/impl/PurchasingServiceImpl.java
+++ work/src/org/kuali/kfs/module/purap/document/service/impl/PurchasingServiceImpl.java
@@ -83,6 +83,10 @@ public class PurchasingServiceImpl extends PersistenceServiceStructureImplBase i
                         newCamsItemsList.add(newCamsItem);
                     }
                     else {
+                        //We need this next line so that the line item info is refreshed 
+                        //within the Capital Asset tab each time the document content has 
+                        //changed during editing to prevent KULPURAP-2926.
+                        camsItem.setPurchasingDocument(purDoc);
                         newCamsItemsList.add(camsItem);
                     }
                 }

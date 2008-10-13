10cd4566c0376460edf5b8d0aebb11e9cdb87560
Harsha Ummerpillai <harsha07@msu.edu>
2008-10-13 20:18:31 +0000

    KULCAP-316 - Junit fixes

--- test/unit/src/org/kuali/kfs/module/cab/sql/cleanup.sql
+++ test/unit/src/org/kuali/kfs/module/cab/sql/cleanup.sql
@@ -16,9 +16,10 @@ delete from ap_pmt_rqst_acct_t where pmt_rqst_acct_id < 1000;
 delete from ap_pmt_rqst_itm_t where pmt_rqst_itm_id < 1000;
 delete from ap_pmt_rqst_t where pmt_rqst_id < 1000;
 -----
-delete from pur_po_acct_t where po_acct_id < 1000;
-delete from pur_po_itm_t where po_itm_id < 1000;
-delete from pur_po_t where po_id < 1000;
+delete from pur_po_cptl_ast_itm_t where fdoc_nbr in ('11','12','13','21','22','23','31','32','33','34','35','36','41','51','52');
+delete from pur_po_acct_t where fdoc_nbr in ('11','12','13','21','22','23','31','32','33','34','35','36','41','51','52');
+delete from pur_po_itm_t where fdoc_nbr in ('11','12','13','21','22','23','31','32','33','34','35','36','41','51','52');
+delete from pur_po_t where fdoc_nbr in ('11','12','13','21','22','23','31','32','33','34','35','36','41','51','52');
 -------
 delete from pur_reqs_acct_t where reqs_acct_id < 1000;
 delete from pur_reqs_itm_t where reqs_itm_id < 1000;

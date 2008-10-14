8b6d0cdf407530e13d2f33943739c91d362a9cb5
Poonam Bhargava <bhargavp@kuali.org>
2008-10-14 21:37:33 +0000

    Kim Work - KFSMI-1657, KFSMI-1656 and KFSMI-1655.

---
+++ work/src/org/kuali/kfs/coa/service/impl/CampusRoleTypeServiceImpl.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.coa.service.impl;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.rice.kim.bo.types.dto.AttributeSet;
+import org.kuali.rice.kim.service.support.impl.KimRoleTypeServiceBase;
+
+public class CampusRoleTypeServiceImpl extends KimRoleTypeServiceBase {
+
+    @Override
+    protected boolean performRoleQualifierQualificationMatch(final AttributeSet qualification, final AttributeSet roleQualifier) {
+        if (!qualification.containsKey(KFSPropertyConstants.CAMPUS_CODE) || !roleQualifier.containsKey(KFSPropertyConstants.CAMPUS_CODE)) {
+            throw new RuntimeException("Campus code not found in qualifier.");
+        }
+        return StringUtils.equals(qualification.get(KFSPropertyConstants.CAMPUS_CODE), roleQualifier.get(KFSPropertyConstants.CAMPUS_CODE));
+    }
+
+    @Override
+    public AttributeSet validateAttributes(AttributeSet attributes) {
+        // TODO validate Campus code is present
+        return super.validateAttributes(attributes);
+    }
+}

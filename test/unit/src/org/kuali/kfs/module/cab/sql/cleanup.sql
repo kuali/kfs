09eca0a58ce6131c1b4e5e5337ae200a022a5c59
Harsha Ummerpillai <harsha07@msu.edu>
2008-10-15 20:03:17 +0000

    Purap history column dropped.

--- test/unit/src/org/kuali/kfs/module/cab/sql/cleanup.sql
+++ test/unit/src/org/kuali/kfs/module/cab/sql/cleanup.sql
@@ -6,12 +6,12 @@ delete from cb_gl_entry_t;
 delete from gl_entry_t where fdoc_nbr in ('11','12','13','21','22','23','31','32','33','34','35','36','41','51','52');
 delete from gl_pending_entry_t where fdoc_nbr in ('11','12','13','21','22','23','31','32','33','34','35','36','41','51','52');
 -----
-delete from ap_crdt_memo_acct_hist_t where crdt_memo_acct_id < 1000;
+delete from ap_crdt_memo_acct_hist_t where crdt_memo_itm_id < 1000;
 delete from ap_crdt_memo_acct_t where crdt_memo_acct_id < 1000;
 delete from ap_crdt_memo_itm_t where crdt_memo_itm_id < 1000;
 delete from ap_crdt_memo_t where crdt_memo_id < 1000;
 -----
-delete from ap_pmt_rqst_acct_hist_t where pmt_rqst_acct_id < 1000;
+delete from ap_pmt_rqst_acct_hist_t where pmt_rqst_itm_id < 1000;
 delete from ap_pmt_rqst_acct_t where pmt_rqst_acct_id < 1000;
 delete from ap_pmt_rqst_itm_t where pmt_rqst_itm_id < 1000;
 delete from ap_pmt_rqst_t where pmt_rqst_id < 1000;

fcc4ba88dfb443d58c2219f2b7b9e9a19808b381
Sewit Adams <sewit.adams@colostate.edu>
2008-10-17 17:14:52 +0000

    KFSMI-1468

---
+++ work/src/org/kuali/kfs/fp/document/validation/impl/PreEncumbranceRequiredAccountingLinesCountValidation.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.fp.document.validation.impl;
+
+import java.util.List;
+
+import org.kuali.kfs.fp.document.PreEncumbranceDocument;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kns.util.GlobalVariables;
+
+/**
+ * Validates that an accounting line does not have a capital object object code 
+ */
+public class PreEncumbranceRequiredAccountingLinesCountValidation extends GenericValidation {
+    private PreEncumbranceDocument accountingDocumentForValidation;
+
+    /**
+     * Validates that an accounting line does not have a capital object object code
+     * <strong>Expects an accounting line as the first a parameter</strong>
+     * @see org.kuali.kfs.sys.document.validation.Validation#validate(java.lang.Object[])
+     */
+    public boolean validate(AttributedDocumentEvent event) {
+        List sourceAccountingLineList = this.accountingDocumentForValidation.getSourceAccountingLines();
+        List targetAccountingLineList = this.accountingDocumentForValidation.getTargetAccountingLines();
+        if (sourceAccountingLineList.size() == 0 && targetAccountingLineList.size() == 0 ) {
+            GlobalVariables.getErrorMap().putError("sourceAccountingLines", "error.document.singleSectionNoAccountingLines", new String[] { null });
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Gets the accountingDocumentForValidation attribute. 
+     * @return Returns the accountingDocumentForValidation.
+     */
+    public PreEncumbranceDocument getAccountingDocumentForValidation() {
+        return accountingDocumentForValidation;
+    }
+
+    /**
+     * Sets the accountingDocumentForValidation attribute value.
+     * @param accountingDocumentForValidation The accountingDocumentForValidation to set.
+     */
+    public void setAccountingDocumentForValidation(PreEncumbranceDocument accountingDocumentForValidation) {
+        this.accountingDocumentForValidation = accountingDocumentForValidation;
+    }
+
+    
+   
+}

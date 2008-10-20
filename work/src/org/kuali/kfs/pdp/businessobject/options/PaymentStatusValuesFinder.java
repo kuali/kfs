6ba807806dde66756ef579af751ecb7e5754c071
Daniela Balmus <db483@cornell.edu>
2008-10-20 21:02:24 +0000

    KFSMI-1249 Add values finder.

---
+++ work/src/org/kuali/kfs/pdp/businessobject/options/PaymentStatusValuesFinder.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.pdp.businessobject.options;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kfs.module.ar.businessobject.CustomerType;
+import org.kuali.kfs.pdp.businessobject.PaymentStatus;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
+import org.kuali.rice.kns.service.KeyValuesService;
+import org.kuali.rice.kns.web.ui.KeyLabelPair;
+
+public class PaymentStatusValuesFinder extends KeyValuesBase {
+
+    /**
+     * @see org.kuali.rice.kns.lookup.keyvalues.KeyValuesFinder#getKeyValues()
+     */
+    @SuppressWarnings("unchecked")
+    public List<KeyLabelPair> getKeyValues() {
+        List<PaymentStatus> boList = (List) SpringContext.getBean(KeyValuesService.class).findAll(PaymentStatus.class);
+        List<KeyLabelPair> keyValues = new ArrayList();
+        keyValues.add(new KeyLabelPair("", ""));
+        for (PaymentStatus element : boList) {
+            keyValues.add(new KeyLabelPair(element.getCode(), element.getCode() + " - " +element.getName()));
+        }
+
+        return keyValues;
+    }
+
+}

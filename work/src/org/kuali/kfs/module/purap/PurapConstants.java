a95fae6637f50897511344432281afe182f694a6
Venkat PremChandran <vpremcha@kuali.org>
2008-10-22 18:58:13 +0000

    minor change for tax - changed postal code to zipcode in msg

--- work/src/org/kuali/kfs/module/purap/PurapConstants.java
+++ work/src/org/kuali/kfs/module/purap/PurapConstants.java
@@ -112,7 +112,7 @@ public class PurapConstants extends JSTLConstants {
     public static final String PDF_IMAGES_AVAILABLE_INDICATOR = "PDF_IMAGES_AVAILABLE_IND";
 
     public static final String TAX_RECALCULATION_INFO = "TaxRecalculationQuestion";
-    public static final String TAX_RECALCULATION_QUESTION = "The zip code of the delivery address has changed. Selecting \"Yes\" will submit the document without recalculating the taxes. Selecting \"No\" will return you to the document so that the taxes can be recalculated. The \"clear all\" button can be used to clear all tax amounts which will force taxes to be recalculated upon submission.";
+    public static final String TAX_RECALCULATION_QUESTION = "The postal code of the delivery address has changed. Selecting \"Yes\" will submit the document without recalculating the taxes. Selecting \"No\" will return you to the document so that the taxes can be recalculated. The \"clear all\" button can be used to clear all tax amounts which will force taxes to be recalculated upon submission.";
     
     public static class RequisitionStatuses {
         public static final String IN_PROCESS = "INPR";

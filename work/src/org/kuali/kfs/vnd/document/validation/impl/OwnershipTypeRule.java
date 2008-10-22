f1bec769a637d3c8a36baec960387f35575ed4bc
Cathy Tan <tanc@ais.msu.edu>
2008-10-22 13:38:35 +0000

    KULPURAP-2254

---
+++ work/src/org/kuali/kfs/vnd/document/validation/impl/OwnershipTypeRule.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.vnd.document.validation.impl;
+
+import java.util.List;
+
+import org.kuali.kfs.module.purap.PurapKeyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.service.ParameterService;
+import org.kuali.kfs.vnd.VendorParameterConstants;
+import org.kuali.kfs.vnd.businessobject.OwnershipType;
+import org.kuali.kfs.vnd.businessobject.VendorDetail;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.rice.kns.service.DataDictionaryService;
+
+/* 
+ * 
+*/
+public class OwnershipTypeRule extends MaintenanceDocumentRuleBase {
+
+    protected boolean processCustomApproveDocumentBusinessRules(MaintenanceDocument document) {
+        LOG.info("processCustomApproveDocumentBusinessRules called");
+        this.setupConvenienceObjects();
+        boolean success = this.checkForSystemParametersExistence();
+        return success && super.processCustomApproveDocumentBusinessRules(document);
+    }
+
+    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+        LOG.info("processCustomRouteDocumentBusinessRules called");
+        this.setupConvenienceObjects();
+        boolean success = this.checkForSystemParametersExistence();
+        return success && super.processCustomRouteDocumentBusinessRules(document);
+    }
+
+    protected boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
+        LOG.info("processCustomSaveDocumentBusinessRules called");
+        this.setupConvenienceObjects();
+        boolean success = this.checkForSystemParametersExistence();
+        return success && super.processCustomSaveDocumentBusinessRules(document);
+    }
+
+    protected boolean checkForSystemParametersExistence() {
+        LOG.info("checkForSystemParametersExistence called");
+        boolean success = true;
+        List<String> feinParameterValues = SpringContext.getBean(ParameterService.class).getParameterValues(VendorDetail.class, VendorParameterConstants.PURAP_FEIN_ALLOWED_OWNERSHIP_TYPES);
+        List<String> ssnParameterValues = SpringContext.getBean(ParameterService.class).getParameterValues(VendorDetail.class, VendorParameterConstants.PURAP_SSN_ALLOWED_OWNERSHIP_TYPES);
+        OwnershipType newBo = (OwnershipType)getNewBo();
+        OwnershipType oldBo= (OwnershipType)getOldBo();
+
+        if ((feinParameterValues.contains(newBo.getVendorOwnershipCode()) || ssnParameterValues.contains(newBo.getVendorOwnershipCode())) && ! newBo.isActive() && oldBo.isActive()) {
+            success = false;
+            String documentLabel = SpringContext.getBean(DataDictionaryService.class).getDocumentLabelByClass(newBo.getClass());
+            putGlobalError(PurapKeyConstants.ERROR_CANNOT_INACTIVATE_USED_IN_SYSTEM_PARAMETERS, documentLabel);
+        }
+        return success;
+    }
+}

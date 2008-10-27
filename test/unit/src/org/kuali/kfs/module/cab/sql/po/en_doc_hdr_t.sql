e0a2b053066c8c43a8748062e8c78952a0710b8d
Harsha Ummerpillai <harsha07@msu.edu>
2008-10-27 20:17:53 +0000

    junit fix related to kim data change

--- test/unit/src/org/kuali/kfs/module/cab/sql/po/en_doc_hdr_t.sql
+++ test/unit/src/org/kuali/kfs/module/cab/sql/po/en_doc_hdr_t.sql
@@ -1,9 +1,9 @@
 insert into en_doc_hdr_t (DOC_HDR_ID, DOC_TYP_ID, DOC_RTE_STAT_CD, DOC_RTE_LVL_NBR, DOC_STAT_MDFN_DT, DOC_CRTE_DT, DOC_APRV_DT, DOC_FNL_DT, DOC_RTE_STAT_MDFN_DT, DOC_RTE_LVL_MDFN_DT, DOC_TTL, DOC_APPL_DOC_ID, DOC_VER_NBR, DOC_INITR_PRSN_EN_ID, DOC_OVRD_IND, DOC_LOCK_CD, DB_LOCK_VER_NBR, DOC_RTE_USR_PRSN_EN_ID, DTYPE)
-values (21, 318281, 'F', 6, current_timestamp, current_timestamp, current_timestamp, current_timestamp, current_timestamp, null, 'Purchase Order - CAB Test Data 1', null, 1, '5621906847', null, null, 56, '5621906847', 'DocumentRouteHeaderValue');
+values (21, 318281, 'F', 6, current_timestamp, current_timestamp, current_timestamp, current_timestamp, current_timestamp, null, 'Purchase Order - CAB Test Data 1', null, 1, '6162502038', null, null, 56, '6162502038', 'DocumentRouteHeaderValue');
 
 insert into en_doc_hdr_t (DOC_HDR_ID, DOC_TYP_ID, DOC_RTE_STAT_CD, DOC_RTE_LVL_NBR, DOC_STAT_MDFN_DT, DOC_CRTE_DT, DOC_APRV_DT, DOC_FNL_DT, DOC_RTE_STAT_MDFN_DT, DOC_RTE_LVL_MDFN_DT, DOC_TTL, DOC_APPL_DOC_ID, DOC_VER_NBR, DOC_INITR_PRSN_EN_ID, DOC_OVRD_IND, DOC_LOCK_CD, DB_LOCK_VER_NBR, DOC_RTE_USR_PRSN_EN_ID, DTYPE)
-values (22, 318281, 'F', 6, current_timestamp, current_timestamp, current_timestamp, current_timestamp, current_timestamp, null, 'Purchase Order - CAB Test Data 1', null, 1, '5621906847', null, null, 56, '5621906847', 'DocumentRouteHeaderValue');
+values (22, 318281, 'F', 6, current_timestamp, current_timestamp, current_timestamp, current_timestamp, current_timestamp, null, 'Purchase Order - CAB Test Data 1', null, 1, '6162502038', null, null, 56, '6162502038', 'DocumentRouteHeaderValue');
 
 insert into en_doc_hdr_t (DOC_HDR_ID, DOC_TYP_ID, DOC_RTE_STAT_CD, DOC_RTE_LVL_NBR, DOC_STAT_MDFN_DT, DOC_CRTE_DT, DOC_APRV_DT, DOC_FNL_DT, DOC_RTE_STAT_MDFN_DT, DOC_RTE_LVL_MDFN_DT, DOC_TTL, DOC_APPL_DOC_ID, DOC_VER_NBR, DOC_INITR_PRSN_EN_ID, DOC_OVRD_IND, DOC_LOCK_CD, DB_LOCK_VER_NBR, DOC_RTE_USR_PRSN_EN_ID, DTYPE)
-values (23, 318281, 'F', 6, current_timestamp, current_timestamp, current_timestamp, current_timestamp, current_timestamp, null, 'Purchase Order - CAB Test Data 1', null, 1, '5621906847', null, null, 56, '5621906847', 'DocumentRouteHeaderValue');
+values (23, 318281, 'F', 6, current_timestamp, current_timestamp, current_timestamp, current_timestamp, current_timestamp, null, 'Purchase Order - CAB Test Data 1', null, 1, '6162502038', null, null, 56, '6162502038', 'DocumentRouteHeaderValue');
 

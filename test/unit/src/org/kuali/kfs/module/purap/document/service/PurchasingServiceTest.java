68c6f888e9f42d3abd01b5464b7ef8fabed80dcf
Venkat PremChandran <vpremcha@kuali.org>
2008-10-28 17:05:15 +0000

    Refactored my old problematic commit for sales tax unit test

--- test/unit/src/org/kuali/kfs/module/purap/document/service/PurchasingServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/purap/document/service/PurchasingServiceTest.java
@@ -49,69 +49,70 @@ public class PurchasingServiceTest extends KualiTestBase {
         super.tearDown();
     }
 
-//    public void testSetupCapitalAssetItems() {
-//        RequisitionDocument requisition = RequisitionDocumentWithCapitalAssetItemsFixture.REQ_VALID_IND_NEW_CAPITAL_ASSET_ITEM.createRequisitionDocument();
-//
-//        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);
-//        List<PurchasingCapitalAssetItem> afterFirstCall = requisition.getPurchasingCapitalAssetItems();
-//    
-//        RequisitionItem item2 = (RequisitionItem)ObjectUtils.deepCopy(requisition.getItem(0));
-//        item2.setItemIdentifier(null);
-//        requisition.addItem(item2);
-//        
-//        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);
-//        List<PurchasingCapitalAssetItem> afterSecondCall = requisition.getPurchasingCapitalAssetItems();
-//        assertTrue(afterSecondCall.size() == 2);
-//        
-//        for (PurchasingCapitalAssetItem camsItem : afterSecondCall) {
-//            assertTrue(camsItem.getPurchasingCapitalAssetSystem() != null);
-//        }
-//        
-//    }
-//    
-//    @ConfigureContext(session = khuntley, shouldCommitTransactions = true)
-//    public void testDeleteCapitalAssetItems() {
-//        RequisitionDocument requisition = RequisitionDocumentWithCapitalAssetItemsFixture.REQ_VALID_IND_NEW_CAPITAL_ASSET_ITEM.createRequisitionDocument();
-//        requisition.getDocumentHeader().setDocumentDescription("From PurchasingServiceTest)");
-//        
-//        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);    
-//
-//        PurchasingItem item2 = RequisitionItemFixture.REQ_ITEM_VALID_CAPITAL_ASSET.createRequisitionItemForCapitalAsset();
-//        
-//        requisition.addItem(item2);
-//
-//        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);
-//
-//        SpringContext.getBean(PurchasingService.class).saveDocumentWithoutValidation(requisition);
-//        
-//        //now do the deletion
-//        SpringContext.getBean(PurchasingService.class).deleteCapitalAssetItems(requisition, requisition.getItem(0).getItemIdentifier());
-//        
-//        List<PurchasingCapitalAssetItem> afterDeletion = requisition.getPurchasingCapitalAssetItems();
-//     
-//        SpringContext.getBean(PurchasingService.class).saveDocumentWithoutValidation(requisition);
-//        assertTrue(afterDeletion.size() == 1);
-//        
-//        for (PurchasingCapitalAssetItem camsItem : afterDeletion) {
-//            assertTrue(camsItem.getPurchasingCapitalAssetSystem() != null);
-//        }
-//    }
-//    
-//    public void testSetupCapitalAssetSystem() {
-//        RequisitionDocument requisition = RequisitionDocumentWithCapitalAssetItemsFixture.REQ_VALID_ONE_NEW_CAPITAL_ASSET_ITEM.createRequisitionDocument();
-//        SpringContext.getBean(PurchasingService.class).setupCapitalAssetSystem(requisition);
-//        assertTrue(requisition.getPurchasingCapitalAssetSystems().size() == 1);
-//    }
+    public void testSetupCapitalAssetItems() {
+        RequisitionDocument requisition = RequisitionDocumentWithCapitalAssetItemsFixture.REQ_VALID_IND_NEW_CAPITAL_ASSET_ITEM.createRequisitionDocument();
 
+        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);
+        List<PurchasingCapitalAssetItem> afterFirstCall = requisition.getPurchasingCapitalAssetItems();
+    
+        RequisitionItem item2 = (RequisitionItem)ObjectUtils.deepCopy(requisition.getItem(0));
+        item2.setItemIdentifier(null);
+        requisition.addItem(item2);
+        
+        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);
+        List<PurchasingCapitalAssetItem> afterSecondCall = requisition.getPurchasingCapitalAssetItems();
+        assertTrue(afterSecondCall.size() == 2);
+        
+        for (PurchasingCapitalAssetItem camsItem : afterSecondCall) {
+            assertTrue(camsItem.getPurchasingCapitalAssetSystem() != null);
+        }
+        
+    }
+    
+    @ConfigureContext(session = khuntley, shouldCommitTransactions = true)
+    public void testDeleteCapitalAssetItems() {
+        RequisitionDocument requisition = RequisitionDocumentWithCapitalAssetItemsFixture.REQ_VALID_IND_NEW_CAPITAL_ASSET_ITEM.createRequisitionDocument();
+        requisition.getDocumentHeader().setDocumentDescription("From PurchasingServiceTest)");
+        
+        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);    
+
+        PurchasingItem item2 = RequisitionItemFixture.REQ_ITEM_VALID_CAPITAL_ASSET.createRequisitionItemForCapitalAsset();
+        
+        requisition.addItem(item2);
+
+        SpringContext.getBean(PurchasingService.class).setupCapitalAssetItems(requisition);
+
+        SpringContext.getBean(PurchasingService.class).saveDocumentWithoutValidation(requisition);
+        
+        //now do the deletion
+        SpringContext.getBean(PurchasingService.class).deleteCapitalAssetItems(requisition, requisition.getItem(0).getItemIdentifier());
+        
+        List<PurchasingCapitalAssetItem> afterDeletion = requisition.getPurchasingCapitalAssetItems();
+     
+        SpringContext.getBean(PurchasingService.class).saveDocumentWithoutValidation(requisition);
+        assertTrue(afterDeletion.size() == 1);
+        
+        for (PurchasingCapitalAssetItem camsItem : afterDeletion) {
+            assertTrue(camsItem.getPurchasingCapitalAssetSystem() != null);
+        }
+    }
+    
+    public void testSetupCapitalAssetSystem() {
+        RequisitionDocument requisition = RequisitionDocumentWithCapitalAssetItemsFixture.REQ_VALID_ONE_NEW_CAPITAL_ASSET_ITEM.createRequisitionDocument();
+        SpringContext.getBean(PurchasingService.class).setupCapitalAssetSystem(requisition);
+        assertTrue(requisition.getPurchasingCapitalAssetSystems().size() == 1);
+    }
+    
     @ConfigureContext(session = khuntley, shouldCommitTransactions = false)
     public final void testDefaultUseTaxIndicatorValue(){
         
-        /*RequisitionDocument requisitionDocument = RequisitionDocumentFixture.REQ_TAX.createRequisitionDocument();
+        RequisitionDocument requisitionDocument = RequisitionDocumentFixture.REQ_TAX.createRequisitionDocument();
         requisitionDocument.refreshReferenceObject("vendorDetail");
         
         requisitionDocument.getVendorDetail().setDefaultAddressStateCode(requisitionDocument.getVendorStateCode());
         boolean defaultUseTaxIndicatorValue = SpringContext.getBean(PurchasingService.class).getDefaultUseTaxIndicatorValue(requisitionDocument);
-        assertFalse(defaultUseTaxIndicatorValue);*/
+        assertFalse(defaultUseTaxIndicatorValue);
         assertTrue(true);
     }
+
 }

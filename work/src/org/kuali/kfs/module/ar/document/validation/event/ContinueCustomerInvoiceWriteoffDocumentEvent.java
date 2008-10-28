4500cf94d5c0d74246b95935d06213dce41530d7
Olga Kizler <olga.kizler@usc.edu>
2008-10-28 20:02:50 +0000

    KULAR-382

---
+++ work/src/org/kuali/kfs/module/ar/document/validation/event/ContinueCustomerInvoiceWriteoffDocumentEvent.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.ar.document.validation.event;
+
+import org.kuali.kfs.module.ar.document.validation.ContinueCustomerInvoiceWriteoffDocumentRule;
+import org.kuali.rice.kns.document.Document;
+import org.kuali.rice.kns.document.TransactionalDocument;
+import org.kuali.rice.kns.rule.BusinessRule;
+import org.kuali.rice.kns.rule.event.KualiDocumentEventBase;
+
+public class ContinueCustomerInvoiceWriteoffDocumentEvent extends KualiDocumentEventBase {
+
+    public ContinueCustomerInvoiceWriteoffDocumentEvent(String errorPathPrefix, Document document) {
+        super("Continue customer invoice writeoff document " + getDocumentId(document), errorPathPrefix, document);
+    }
+    
+    public Class getRuleInterfaceClass() {
+        return ContinueCustomerInvoiceWriteoffDocumentRule.class;
+    }
+
+    public boolean invokeRuleMethod(BusinessRule rule) {
+        return ((ContinueCustomerInvoiceWriteoffDocumentRule) rule).processContinueCustomerInvoiceWriteoffDocumentRules((TransactionalDocument)getDocument());
+    }
+}

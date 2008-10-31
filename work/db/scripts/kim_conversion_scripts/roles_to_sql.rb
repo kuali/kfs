0dced3789cac880033cc33b843c4ae5638eb5904
Bin Gao <gaob@msu.edu>
2008-10-31 18:33:55 +0000

    KFSMI-1803: Updates for scripts based on database changes -- the table and column names have been updated

--- work/db/scripts/kim_conversion_scripts/roles_to_sql.rb
+++ work/db/scripts/kim_conversion_scripts/roles_to_sql.rb
@@ -17,7 +17,7 @@ end
 def generate_role_sql(role_id, role_namespace, role_name, role_description)
 	table_name = "KRIM_ROLE_T"
 	obj_id = generate_obj_id(table_name, "#{role_namespace} #{role_name}")
-	"insert into #{table_name} (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, ROLE_DESC, TYP_ID, ACTV_IND)\n\tvalues ('#{role_id}', '#{obj_id}', 0, '#{role_name.gsub("'", "''")}', '#{role_namespace}', '#{role_description.gsub("'", "''")}', '', 'Y')\n/\n"
+	"insert into #{table_name} (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, ROLE_DESC, KIM_TYP_ID, ACTV_IND)\n\tvalues ('#{role_id}', '#{obj_id}', 0, '#{role_name.gsub("'", "''")}', '#{role_namespace}', '#{role_description.gsub("'", "''")}', '', 'Y')\n/\n"
 end
 
 File.open(INPUT_FILE, "r") do |fin|

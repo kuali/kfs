f512c99a3c7ffb7e57b972362d70312f29bfbff7
Amanda Zhang <zhangma@msu.edu>
2008-11-04 20:46:59 +0000

    KULCAP-641: remove summary and description property for CAB and CAMS DD.

--- work/web-root/scripts/cab/selectCheckBox.js
+++ work/web-root/scripts/cab/selectCheckBox.js
@@ -15,39 +15,3 @@
 			trg.disabled = false;
 		}
 	}
-	function merge(src,trg){
-		var elms = document.getElementsByTagName("input");
-		var trgCount = 0;
-		var srcCount = 0;
-		for(var i=0; i< elms.length; i++){
-			if(elms[i].name.match('trg')== 'trg' && elms[i].checked ){
-			   trgCount = trgCount+1;
-			}else if((elms[i].name.match('src') == 'src' || elms[i].name.match('addl') == 'addl' )  && elms[i].checked){
-				srcCount = srcCount+1;
-			}
-		}
-		if(trgCount != 1 || srcCount == 0){
-		   alert('Merge action expects one target line and multiple source lines.');
-		}else{
-		   alert('Selection valid');
-		}
-	}
-	function allocate(src,trg){
-		var elms = document.getElementsByTagName("input");
-		var trgCount = 0;
-		var srcCount = 0;
-		for(var i=0; i< elms.length; i++){
-			if(elms[i].name.match('trg')== 'trg' && elms[i].checked ){
-			   trgCount = trgCount+1;
-			}else if((elms[i].name.match('src') == 'src' || elms[i].name.match('addl') == 'addl' )  && elms[i].checked){
-				srcCount = srcCount+1;
-			}
-		}		
-		if(srcCount != 1){
-		   alert('Allocate action expects one source line and multiple target lines');
-		}
-		else{
-		   alert('Selection valid');
-		}
-	}
-	

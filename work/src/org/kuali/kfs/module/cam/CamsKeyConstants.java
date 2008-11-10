070209856f34bd29f6bd53cc7b8a5d5c210b38f8
Queing Lai <queing.lai@usc.edu>
2008-11-10 18:36:10 +0000

    KULCAP-745  Asset created with inactive asset type code.

--- work/src/org/kuali/kfs/module/cam/CamsKeyConstants.java
+++ work/src/org/kuali/kfs/module/cam/CamsKeyConstants.java
@@ -180,6 +180,7 @@ public class CamsKeyConstants {
         public static final String ERROR_MFR_NAME_REQUIRED = "error.capital.asset.manufacturer.name.required";
         public static final String ERROR_ACQUISITION_TYPE_CODE_REQUIRED = "error.acquisition.code.required";
         public static final String ERROR_ACQUISITION_TYPE_CODE_NOT_ALLOWED = "error.acquisition.code.not.allowed";
+        public static final String ERROR_INACTIVE_ACQUISITION_TYPE_CODE = "error.inactive.acquisition.code";
         public static final String ERROR_OWNER_CHART_INVALID = "error.asset.owner.chart.code.invalid";
         public static final String ERROR_OWNER_ACCT_NUMBER_INVALID = "error.asset.owner.account.number.invalid";
         public static final String ERROR_CAMPUS_TAG_NUMBER_DUPLICATE = "error.asset.campus.tag.number.duplicate";

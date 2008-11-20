0b503ad9fac872268e66db56eaaad1346c15656d
Cathy Tan <tanc@ais.msu.edu>
2008-11-20 18:52:03 +0000

    KULPURAP-3091

--- test/unit/src/org/kuali/kfs/module/purap/document/service/PurchaseOrderServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/purap/document/service/PurchaseOrderServiceTest.java
@@ -17,21 +17,30 @@ package org.kuali.kfs.module.purap.document.service;
 
 import static org.kuali.kfs.sys.fixture.UserNameFixture.kuluser;
 
+import java.io.ByteArrayOutputStream;
+import java.sql.Timestamp;
+
 import org.apache.commons.lang.StringUtils;
-import org.kuali.rice.kns.service.DocumentService;
-import org.kuali.rice.kns.util.KualiDecimal;
-import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.module.purap.PurapConstants.PurchaseOrderDocTypes;
+import org.kuali.kfs.module.purap.PurapConstants.PurchaseOrderStatuses;
 import org.kuali.kfs.module.purap.businessobject.PurchaseOrderItem;
 import org.kuali.kfs.module.purap.document.PurchaseOrderDocument;
 import org.kuali.kfs.module.purap.fixture.PurchaseOrderDocumentFixture;
 import org.kuali.kfs.module.purap.fixture.PurchaseOrderDocumentWithCommodityCodeFixture;
+import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.AccountingDocumentTestUtils;
 import org.kuali.kfs.vnd.businessobject.ContractManager;
 import org.kuali.kfs.vnd.businessobject.VendorCommodityCode;
 import org.kuali.kfs.vnd.businessobject.VendorContract;
 import org.kuali.kfs.vnd.businessobject.VendorDetail;
 import org.kuali.kfs.vnd.document.service.VendorService;
+import org.kuali.rice.kns.exception.ValidationException;
+import org.kuali.rice.kns.service.DateTimeService;
+import org.kuali.rice.kns.service.DocumentService;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KualiDecimal;
 
 
 @ConfigureContext(session = kuluser, shouldCommitTransactions=true)
@@ -52,7 +61,7 @@ public class PurchaseOrderServiceTest extends KualiTestBase {
         }
     }
 
-    /*
+
     @ConfigureContext(session = kuluser, shouldCommitTransactions=true)
     public final void testPurchaseOrderRetransmit() throws Exception {   
         // Create and save a minimally-populated basic PO document for each test.
@@ -78,16 +87,15 @@ public class PurchaseOrderServiceTest extends KualiTestBase {
         ((PurchaseOrderItem)poRetrans.getItem(0)).setItemSelectedForRetransmitIndicator(true);
         ByteArrayOutputStream baosPDF = new ByteArrayOutputStream();
         try {
+            DateTimeService dtService = SpringContext.getBean(DateTimeService.class);
             StringBuffer sbFilename = new StringBuffer();
             sbFilename.append("PURAP_PO_");
             sbFilename.append(poRetrans.getPurapDocumentIdentifier());
             sbFilename.append("_");
-            sbFilename.append(System.currentTimeMillis());
+            sbFilename.append(dtService.getCurrentDate().getTime());
             sbFilename.append(".pdf");
             poService.retransmitPurchaseOrderPDF(poRetrans, baosPDF);
             assertTrue(baosPDF.size()>0);
-            DateTimeService dtService = SpringContext.getBean(DateTimeService.class);
-            assertEquals(poRetrans.getPurchaseOrderLastTransmitTimestamp(), dtService.getCurrentSqlDate());
         }
         catch (ValidationException e) {
             LOG.warn("Caught ValidationException while trying to retransmit PO with doc id " + po.getDocumentNumber());
@@ -103,13 +111,18 @@ public class PurchaseOrderServiceTest extends KualiTestBase {
     public final void testPurchaseOrderPrint() throws Exception {        
         PurchaseOrderDocument po = 
             PurchaseOrderDocumentFixture.PO_ONLY_REQUIRED_FIELDS_MULTI_ITEMS.createPurchaseOrderDocument();
+        po.setStatusCode(PurchaseOrderStatuses.OPEN);
+        po.refreshNonUpdateableReferences();
+        po.prepareForSave();
+        AccountingDocumentTestUtils.saveDocument(po, docService);
         ByteArrayOutputStream baosPDF = new ByteArrayOutputStream();
         try {
+            DateTimeService dtService = SpringContext.getBean(DateTimeService.class);
             StringBuffer sbFilename = new StringBuffer();
             sbFilename.append("PURAP_PO_");
             sbFilename.append(po.getPurapDocumentIdentifier());
             sbFilename.append("_");
-            sbFilename.append(System.currentTimeMillis());
+            sbFilename.append(dtService.getCurrentDate().getTime());
             sbFilename.append(".pdf");
             poService.performPrintPurchaseOrderPDFOnly(po.getDocumentNumber(), baosPDF);
             assertTrue(baosPDF.size()>0);
@@ -123,7 +136,7 @@ public class PurchaseOrderServiceTest extends KualiTestBase {
             }
         }
     }
-    */
+
     
     @ConfigureContext(session = kuluser, shouldCommitTransactions=true)
     public final void testGetInternalPurchasingDollarLimit() {

e9f1cdb0b50645250c78f3b3c061d68b6ef627eb
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2008-11-22 00:39:12 +0000

    KFSMI-1032
    Old perl scripts remvoed and replaced with java code.

---
+++ work/db/workflow/scripts/DocTypes.java
@@ -0,0 +1,5 @@
+public class DocTypes {
+	public static void main(String[] args){
+		MakeFragments myFragments = new MakeFragments("documentType");
+	}
+}
---
+++ work/db/workflow/scripts/Templates.java
@@ -0,0 +1,5 @@
+public class Templates {
+	public static void main(String[] args){
+		MakeFragments myFragments = new MakeFragments("ruleTemplate");
+	}
+}

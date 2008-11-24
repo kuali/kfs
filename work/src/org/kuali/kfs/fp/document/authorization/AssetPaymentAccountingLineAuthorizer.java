aca8e79e2d5cef1b4c5b33596362751d1fa30790
Amanda Zhang <zhangma@msu.edu>
2008-11-24 19:46:56 +0000

    kulcap-796:Add Payment - When document is populated from CAB payment lines should be read only.

--- work/src/org/kuali/kfs/fp/document/authorization/AssetPaymentAccountingLineAuthorizer.java
+++ work/src/org/kuali/kfs/fp/document/authorization/AssetPaymentAccountingLineAuthorizer.java
@@ -15,8 +15,41 @@
  */
 package org.kuali.kfs.fp.document.authorization;
 
+import java.util.Map;
+
+import org.kuali.kfs.module.cam.document.AssetPaymentDocument;
+import org.kuali.kfs.sys.businessobject.AccountingLine;
+import org.kuali.kfs.sys.document.AccountingDocument;
 import org.kuali.kfs.sys.document.authorization.AccountingLineAuthorizerBase;
+import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kns.authorization.AuthorizationConstants;
 
 public class AssetPaymentAccountingLineAuthorizer extends AccountingLineAuthorizerBase {
+    /**
+     * @see org.kuali.kfs.sys.document.authorization.AccountingLineAuthorizerBase#getEditModeForAccountingLine(org.kuali.kfs.sys.document.AccountingDocument,
+     *      org.kuali.kfs.sys.businessobject.AccountingLine, boolean, org.kuali.rice.kim.bo.Person, java.util.Map)
+     */
+    @Override
+    public String getEditModeForAccountingLine(AccountingDocument accountingDocument, AccountingLine accountingLine, boolean newLine, Person currentUser, Map<String, String> editModesForDocument) {
+        AssetPaymentDocument assetPaymentDocument = (AssetPaymentDocument) accountingDocument;
+        if (assetPaymentDocument.isCapitalAssetBuilderOriginIndicator()) {
+            return AuthorizationConstants.EditMode.VIEW_ONLY;
+        }
+        else {
+            return super.getEditModeForAccountingLine(accountingDocument, accountingLine, newLine, currentUser, editModesForDocument);
+        }
+    }
 
+    /**
+     * @see org.kuali.kfs.sys.document.authorization.AccountingLineAuthorizerBase#isGroupReadOnly(org.kuali.kfs.sys.document.AccountingDocument,
+     *      java.lang.String, org.kuali.rice.kim.bo.Person, java.util.Map)
+     */
+    @Override
+    public boolean isGroupReadOnly(AccountingDocument accountingDocument, String accountingLineCollectionProperty, Person currentUser, Map<String, String> editModesForDocument) {
+        AssetPaymentDocument assetPaymentDocument = (AssetPaymentDocument) accountingDocument;
+        if (assetPaymentDocument.isCapitalAssetBuilderOriginIndicator()) {
+            return true;
+        }
+        return super.isGroupReadOnly(accountingDocument, accountingLineCollectionProperty, currentUser, editModesForDocument);
+    }
 }

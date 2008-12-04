5178df9319f78c2825fa4e09b5295b3a67ba56c7
Laran Evans <laran@laranevans.com>
2008-12-04 22:40:00 +0000

    work on payment application unit tests.

--- test/integration/src/org/kuali/kfs/module/ar/document/PaymentApplicationDocumentTest.java
+++ test/integration/src/org/kuali/kfs/module/ar/document/PaymentApplicationDocumentTest.java
@@ -16,19 +16,74 @@
 package org.kuali.kfs.module.ar.document;
 
 import static org.kuali.kfs.sys.fixture.UserNameFixture.khuntley;
-import org.kuali.rice.kns.service.DocumentService;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.kuali.kfs.module.ar.fixture.CashControlDocumentFixture;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.service.DocumentService;
+import org.kuali.rice.kns.util.ErrorMap;
+import org.kuali.rice.kns.util.GlobalVariables;
 
 @ConfigureContext(session = khuntley)
 public class PaymentApplicationDocumentTest extends KualiTestBase {
+    static private String ANNOTATION = "PaymentApplicationDocument testing";
+    private DocumentService documentService;
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        documentService = SpringContext.getBean(DocumentService.class);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        // TODO Auto-generated method stub
+        super.tearDown();
+    }
 
     public void testCreatePaymentApplicationDocument() throws Exception {
-        DocumentService service = SpringContext.getBean(DocumentService.class);
-        PaymentApplicationDocument document = (PaymentApplicationDocument) service.getNewDocument(PaymentApplicationDocument.class);
+        PaymentApplicationDocument document = (PaymentApplicationDocument) documentService.getNewDocument(PaymentApplicationDocument.class);
         document.getDocumentHeader().setDocumentDescription("Testing");
-        service.saveDocument(document);
+        documentService.saveDocument(document);
+    }
+    
+    // Create a cash control document
+    // Approve it
+    // Check that a payapp doc was created with the same document number as the ref doc number on the cash control doc
+    public void testCreatedUponApprovingCashControlDocument() throws Exception {
+        CashControlDocument cashControlDocument = CashControlDocumentFixture.CASH_CONTROL_DOCUMENT.getNewCashControlDocumentWithDetails();
+        try {
+            documentService.blanketApproveDocument(cashControlDocument, ANNOTATION, new ArrayList());
+            assertNotNull(cashControlDocument.getReferenceFinancialDocument());
+        } catch(Throwable t) {
+            ErrorMap e = GlobalVariables.getErrorMap();
+            Set s = e.keySet();
+            if(0 != s.size()) {
+                boolean trap = true;
+            }
+        }
+    }
+    
+    public void testCreatedWithoutCashControlDocument() throws Exception {
+    }
+    
+    public void testPendingEntriesGeneratedCorrectly() throws Exception {
+    }
+    
+    public void testOverApplyingFails() throws Exception {
+        // TODO implement
+    }
+    
+    public void testUnderApplyingFails() throws Exception {
+        // TODO implement
+    }
+    
+    public void testExactlyApplyingSucceeds() throws Exception {
+        // TODO implement
     }
     
 }

37c9c34eeade7d2fc75758f6c435c7193398c480
Jonathan Keller <keller.jonathan@gmail.com>
2008-12-16 17:07:30 +0000

    Fixed up comparison of chart/org in KFS-SYS User role type service.

--- work/src/org/kuali/kfs/coa/identity/OrganizationAndOptionalNamespaceRoleTypeServiceImpl.java
+++ work/src/org/kuali/kfs/coa/identity/OrganizationAndOptionalNamespaceRoleTypeServiceImpl.java
@@ -19,13 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
-import org.kuali.kfs.sys.KFSPropertyConstants;
-import org.kuali.rice.kim.bo.impl.KimAttributes;
+import org.kuali.kfs.sys.identity.KimAttributes;
 import org.kuali.rice.kim.bo.role.dto.RoleMembershipInfo;
 import org.kuali.rice.kim.bo.types.dto.AttributeSet;
-import org.kuali.rice.kim.config.KIMConfigurer;
 import org.kuali.rice.kim.service.support.impl.KimRoleTypeServiceBase;
-import org.kuali.rice.kim.util.KimConstants;
+
 
 /**
  * This role type service was designed to be attached to the KFS-SYS User role.
@@ -36,12 +34,12 @@ public class OrganizationAndOptionalNamespaceRoleTypeServiceImpl extends KimRole
     protected List<String> roleQualifierRequiredAttributes = new ArrayList<String>();
     protected List<String> qualificationRequiredAttributes = new ArrayList<String>();
     {
-        roleQualifierRequiredAttributes.add(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE);
-        roleQualifierRequiredAttributes.add(KFSPropertyConstants.ORGANIZATION_CODE);        
+        roleQualifierRequiredAttributes.add(KimAttributes.CHART_OF_ACCOUNTS_CODE);
+        roleQualifierRequiredAttributes.add(KimAttributes.ORGANIZATION_CODE);        
         //roleQualifierRequiredAttributes.add(KfsCoreKimAttributes.NAMESPACE_CODE);
 
-        qualificationRequiredAttributes.add(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE);
-        qualificationRequiredAttributes.add(KFSPropertyConstants.ORGANIZATION_CODE);        
+        qualificationRequiredAttributes.add(KimAttributes.CHART_OF_ACCOUNTS_CODE);
+        qualificationRequiredAttributes.add(KimAttributes.ORGANIZATION_CODE);        
         //qualificationRequiredAttributes.add(KfsCoreKimAttributes.NAMESPACE_CODE);
     }
     
@@ -52,20 +50,20 @@ public class OrganizationAndOptionalNamespaceRoleTypeServiceImpl extends KimRole
         validateRequiredAttributesAgainstReceived(
                 qualificationRequiredAttributes, qualification, QUALIFICATION_RECEIVED_ATTIBUTES_NAME);
         
-        String chart = qualification.get(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE);
-        String orgCode = qualification.get(KFSPropertyConstants.ORGANIZATION_CODE);
+        String chart = qualification.get(KimAttributes.CHART_OF_ACCOUNTS_CODE);
+        String orgCode = qualification.get(KimAttributes.ORGANIZATION_CODE);
         String namespace = qualification.get(KimAttributes.NAMESPACE_CODE);
         
-        String roleChart = roleQualifier.get(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE);
-        String roleOrgCode = roleQualifier.get(KFSPropertyConstants.ORGANIZATION_CODE);
+        String roleChart = roleQualifier.get(KimAttributes.CHART_OF_ACCOUNTS_CODE);
+        String roleOrgCode = roleQualifier.get(KimAttributes.ORGANIZATION_CODE);
         String roleNamespace = roleQualifier.get(KimAttributes.NAMESPACE_CODE);
 
         // check if the chart/org match
         // if so, do an optional namespace match - if is specified on both, then must match
-        if ( StringUtils.equals(chart, roleChart) && StringUtils.equals(orgCode, roleOrgCode)) {
+        if ( StringUtils.equals(chart, roleChart) && StringUtils.equals(orgCode, roleOrgCode) || (StringUtils.isBlank(chart) && StringUtils.isBlank(orgCode)) ) {
             if ( StringUtils.isBlank(namespace) || StringUtils.isBlank(roleNamespace) ) {
                 return true;
-            } else if ( StringUtils.equals(namespace, roleNamespace) ) {
+            } else if ( StringUtils.equals(namespace, roleNamespace) || (StringUtils.isBlank(roleNamespace) && StringUtils.isBlank(namespace)) ) {
                 return true;
             }
         }
@@ -77,19 +75,20 @@ public class OrganizationAndOptionalNamespaceRoleTypeServiceImpl extends KimRole
         // perform special matching to make a match that includes the namespace take priority
         RoleMembershipInfo namespaceMatch = null;
         RoleMembershipInfo nonNamespaceMatch = null;
-        String chart = qualification.get(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE);
-        String orgCode = qualification.get(KFSPropertyConstants.ORGANIZATION_CODE);
+        String chart = qualification.get(KimAttributes.CHART_OF_ACCOUNTS_CODE);
+        String orgCode = qualification.get(KimAttributes.ORGANIZATION_CODE);
         String namespace = qualification.get(KimAttributes.NAMESPACE_CODE);
         // look for match
         for ( RoleMembershipInfo rmi : roleMemberList ) {
-            String roleChart = rmi.getQualifier().get(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE);
-            String roleOrgCode = rmi.getQualifier().get(KFSPropertyConstants.ORGANIZATION_CODE);
+            String roleChart = rmi.getQualifier().get(KimAttributes.CHART_OF_ACCOUNTS_CODE);
+            String roleOrgCode = rmi.getQualifier().get(KimAttributes.ORGANIZATION_CODE);
             String roleNamespace = rmi.getQualifier().get(KimAttributes.NAMESPACE_CODE);
-            
-            if ( StringUtils.equals(chart, roleChart) 
-                    && StringUtils.equals(orgCode, roleOrgCode) ) {
-                // check for an exact namespace match
-                if ( StringUtils.equals(namespace, roleNamespace) ) {
+            // compare the chart/org, but also match if none were passed
+            if ( (StringUtils.equals(chart, roleChart) 
+                    && StringUtils.equals(orgCode, roleOrgCode))
+                    || (StringUtils.isBlank(chart) && StringUtils.isBlank(orgCode)) ) {
+                // check for an exact namespace match (also match null and empty strings)
+                if ( StringUtils.equals(namespace, roleNamespace) || (StringUtils.isBlank(roleNamespace) && StringUtils.isBlank(namespace)) ) {
                     namespaceMatch = rmi;
                     break; // namespace matches will take precedence, so skip further checks 
                 // check for a blank namespace if the qualifier is not blank

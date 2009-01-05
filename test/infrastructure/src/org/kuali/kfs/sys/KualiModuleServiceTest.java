284fc6655a6dad5bb24fd0bd23ff7b80f664d0e8
Jonathan Keller <keller.jonathan@gmail.com>
2009-01-05 18:31:30 +0000

    KFSMI-2183 : added test for the module service to ensure that getInstalledModuleServices() is working correctly

---
+++ test/infrastructure/src/org/kuali/kfs/sys/KualiModuleServiceTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2009 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.sys;
+
+import java.util.List;
+
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kim.util.KimConstants;
+import org.kuali.rice.kns.service.KualiModuleService;
+import org.kuali.rice.kns.service.ModuleService;
+import org.kuali.rice.kns.util.KNSConstants;
+
+@ConfigureContext
+public class KualiModuleServiceTest extends KualiTestBase {
+
+    public void testGetInstalledModules() {
+        KualiModuleService kmi = SpringContext.getBean(KualiModuleService.class);
+        List<ModuleService> modules = kmi.getInstalledModuleServices();
+        assertTrue( "There must be more than one module", modules.size() > 1 );
+        boolean riceKnsFound = false;
+        boolean riceKimFound = false;
+        boolean riceKewFound = false;
+        boolean coaFound = false;
+        boolean sysFound = false;
+        boolean fpFound = false;
+        boolean glFound = false;
+        for ( ModuleService m : modules ) {
+            String namespaceCode = m.getModuleConfiguration().getNamespaceCode();
+            assertNotNull( "Namespace code must not be null in the ModuleConfiguration: " + m.getModuleConfiguration(), namespaceCode );
+            System.out.println( namespaceCode );
+            if ( namespaceCode.equals( KFSConstants.ParameterNamespaces.CHART ) ) {
+                coaFound = true;
+            }
+            if ( namespaceCode.equals( KFSConstants.ParameterNamespaces.FINANCIAL ) ) {
+                fpFound = true;
+            }
+            if ( namespaceCode.equals( KFSConstants.ParameterNamespaces.KFS ) ) {
+                sysFound = true;
+            }
+            if ( namespaceCode.equals( KFSConstants.ParameterNamespaces.GL ) ) {
+                glFound = true;
+            }
+            if ( namespaceCode.equals( KNSConstants.KNS_NAMESPACE ) ) {
+                riceKnsFound = true;
+            }
+            if ( namespaceCode.equals( KimConstants.NAMESPACE_CODE ) ) {
+                riceKimFound = true;
+            }
+            if ( namespaceCode.equals( KNSConstants.KUALI_RICE_WORKFLOW_NAMESPACE ) ) {
+                riceKewFound = true;
+            }
+        }
+        // TODO: constants in Rice do not match the namespaces used in the configuration
+        // so commenting out for now until the codes have stabilized
+//        assertTrue( "Unable to find " + KNSConstants.KNS_NAMESPACE + " module in installed modules list", riceKnsFound );
+//        assertTrue( "Unable to find " + KNSConstants.KUALI_RICE_WORKFLOW_NAMESPACE + " module in installed modules list", riceKewFound );
+//        assertTrue( "Unable to find " + KimConstants.NAMESPACE_CODE + " module in installed modules list", riceKimFound );
+        assertTrue( "Unable to find " + KFSConstants.ParameterNamespaces.CHART + " module in installed modules list", coaFound );
+        assertTrue( "Unable to find " + KFSConstants.ParameterNamespaces.KFS + " module in installed modules list", sysFound );
+        assertTrue( "Unable to find " + KFSConstants.ParameterNamespaces.FINANCIAL + " module in installed modules list", fpFound );
+        assertTrue( "Unable to find " + KFSConstants.ParameterNamespaces.GL + " module in installed modules list", glFound );
+    }
+}

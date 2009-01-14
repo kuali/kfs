b06f3518c2c950b3b9b855abf7681b4b3f182233
Bin Gao <gaob@msu.edu>
2009-01-14 20:12:07 +0000

    KFSMI-2234: Complete KIM conversion for CG

--- work/src/org/kuali/kfs/module/cg/document/authorization/RoutingFormDocumentPresentationController.java
+++ work/src/org/kuali/kfs/module/cg/document/authorization/RoutingFormDocumentPresentationController.java
@@ -15,14 +15,21 @@
  */
 package org.kuali.kfs.module.cg.document.authorization;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.module.cg.CGConstants;
 import org.kuali.kfs.module.cg.CGKeyConstants;
+import org.kuali.kfs.module.cg.businessobject.RoutingFormPersonnel;
 import org.kuali.kfs.module.cg.document.RoutingFormDocument;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.document.authorization.FinancialSystemTransactionalDocumentPresentationControllerBase;
+import org.kuali.rice.kim.bo.Person;
 import org.kuali.rice.kns.document.Document;
 import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.workflow.service.KualiWorkflowDocument;
 
 public class RoutingFormDocumentPresentationController extends FinancialSystemTransactionalDocumentPresentationControllerBase {
 
@@ -34,7 +41,7 @@ public class RoutingFormDocumentPresentationController extends FinancialSystemTr
         Set<String> editModes = super.getEditModes(document);
 
         RoutingFormDocument routingFormDocument = (RoutingFormDocument) document;
-        if (routingFormDocument.getRoutingFormBudgetNumber() != null) {
+        if (StringUtils.isNotBlank(routingFormDocument.getRoutingFormBudgetNumber())) {
             editModes.add(CGConstants.AuthorizationConstants.BUDGET_LINKED);
 
             // TODO: find a good place for the following code
@@ -44,6 +51,48 @@ public class RoutingFormDocumentPresentationController extends FinancialSystemTr
         }
 
         return editModes;
+    }
+
+    /**
+     * @see org.kuali.rice.kns.document.authorization.DocumentPresentationControllerBase#canEdit(org.kuali.rice.kns.document.Document)
+     */
+    @Override
+    protected boolean canEdit(Document document) {
+        RoutingFormDocument routingFormDocument = (RoutingFormDocument) document;
+        KualiWorkflowDocument workflowDocument = routingFormDocument.getDocumentHeader().getWorkflowDocument();
+        String documentStatusCode = routingFormDocument.getDocumentHeader().getFinancialDocumentStatusCode();
+
+        // Check initiator
+        Person user = GlobalVariables.getUserSession().getPerson();
+        if (workflowDocument.userIsInitiator(user)) {
+            return !(workflowDocument.stateIsEnroute() || this.isFinalStatusCode(documentStatusCode));
+        }
+
+        // Check personnel
+        for (RoutingFormPersonnel personnel : routingFormDocument.getRoutingFormPersonnel()) {
+            if (StringUtils.equals(user.getPrincipalId(), personnel.getPrincipalId())) {
+                personnel.refreshNonUpdateableReferences();
+                String role = personnel.getPersonRole().getPersonRoleCode();
+
+                if (CGConstants.PROJECT_DIRECTOR_CODE.equals(role) || CGConstants.CO_PROJECT_DIRECTOR_CODE.equals(role)) {
+                    Integer currentRouteLevel = workflowDocument.getRouteHeader().getDocRouteLevel();
+                    return (currentRouteLevel <= CGConstants.projectDirectorRouteLevel) && !this.isFinalStatusCode(documentStatusCode);
+                }
+
+                if (CGConstants.CONTACT_PERSON_ADMINISTRATIVE_CODE.equals(role) || CGConstants.CONTACT_PERSON_PROPOSAL_CODE.equals(role)) {
+                    return false;
+                }
+            }
+        }
+
+        return super.canEdit(document);
+    }
+
+    // determine whether the given document status is in a final state
+    private boolean isFinalStatusCode(String documentStatusCode) {
+        List<String> finalStatusCodes = Arrays.asList(KFSConstants.DocumentStatusCodes.APPROVED, KFSConstants.DocumentStatusCodes.DISAPPROVED, KFSConstants.DocumentStatusCodes.CANCELLED);
+
+        return finalStatusCodes.contains(documentStatusCode);
     }
 
     /**

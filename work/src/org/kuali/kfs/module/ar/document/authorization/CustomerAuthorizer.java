80aacba72fca54a4a99ac9533e88ca3245cdc1ca
Andrew Hollamon <andrew@dmentionsystems.com>
2009-01-15 09:07:16 +0000

    Some work on KIM integration for CustomerMaintenance. KULAR-516

--- work/src/org/kuali/kfs/module/ar/document/authorization/CustomerAuthorizer.java
+++ work/src/org/kuali/kfs/module/ar/document/authorization/CustomerAuthorizer.java
@@ -15,97 +15,56 @@
  */
 package org.kuali.kfs.module.ar.document.authorization;
 
-import java.util.Map;
-
-import org.kuali.kfs.module.ar.ArKeyConstants;
-import org.kuali.kfs.module.ar.util.ARUtil;
-import org.kuali.kfs.sys.KFSConstants;
-import org.kuali.kfs.sys.document.authorization.FinancialSystemDocumentActionFlags;
 import org.kuali.kfs.sys.document.authorization.FinancialSystemMaintenanceDocumentAuthorizerBase;
-import org.kuali.rice.kim.bo.Person;
-import org.kuali.rice.kns.authorization.AuthorizationConstants;
-import org.kuali.rice.kns.document.Document;
-import org.kuali.rice.kns.document.MaintenanceDocument;
-import org.kuali.rice.kns.exception.DocumentInitiationAuthorizationException;
-import org.kuali.rice.kns.util.KNSConstants;
 
 public class CustomerAuthorizer extends FinancialSystemMaintenanceDocumentAuthorizerBase {
 
-    // TODO remove - replaced by kim
-//    @Override
-//    public void canInitiate(String documentTypeName, Person user) {
-//
-//        super.canInitiate(documentTypeName, user);
-//        
-//        // if organization doesn't exist
-//        if (!ARUtil.isUserInArBillingOrg(user)) {
-//            throw new DocumentInitiationAuthorizationException(ArKeyConstants.ERROR_ORGANIZATION_OPTIONS_MUST_BE_SET_FOR_USER_ORG, 
-//                    new String[] { "(Users in an AR Billing Org)", "Customer Maintenance" });
-//
-//        }
-//    }
+    // TODO this is commented out because the old logic doesnt match up the new logic at 
+    //      https://test.kuali.org/confluence/display/KULAR/Document+Types%2C+Approvals 
 
-// TODO remove - replaced by kim create / maintain template
-    /**
-     * @see org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizerBase#getEditMode(org.kuali.rice.kns.document.Document,
-     *      org.kuali.rice.kim.bo.Person)
-     */
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public Map getEditMode(Document document, Person user) {
-//        Map editModes = super.getEditMode(document, user);
+    // the rules should be:  persons belonging to AR Biller or AR Processor roles can 
+    // initiate these documents, both Create and Maintain (ie, New and Edit).
+    //
+    // All approvals is done by AR_ROLE_MAINTAINERS (or whatever the KIM group/role equiv of htat is)
+    //
+    // Furthermore, System Super Users should be able to do everything, including BlanketApprove.
+    
+///**
+// * @see org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizerBase#getDocumentActionFlags(org.kuali.rice.kns.document.Document, org.kuali.rice.kim.bo.Person)
+// */
+//@Override
+//public FinancialSystemDocumentActionFlags getDocumentActions(Document document) {
+//    FinancialSystemDocumentActionFlags actionFlags = super.getDocumentActionFlags(document);
 //
-//        MaintenanceDocument maintDocument = (MaintenanceDocument) document;
-//        String maintenanceAction = maintDocument.getNewMaintainableObject().getMaintenanceAction();
-//        if (maintenanceAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_EDIT_ACTION) || maintenanceAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_COPY_ACTION)) {
-//            if (!ARUtil.isUserInArSupervisorGroup(user)) {
-//                editModes.clear();
-//                editModes.put(AuthorizationConstants.EditMode.UNVIEWABLE, "TRUE");
-//            }
-//        }
+//    MaintenanceDocument maintDocument = (MaintenanceDocument) document;
+//    String maintenanceAction = maintDocument.getNewMaintainableObject().getMaintenanceAction();
 //
-//        return editModes;
-//    }
-
-
- // TODO fix for kim - and let's let permissions take care of what the system user can do
-//     /**
-//     * @see org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizerBase#getDocumentActionFlags(org.kuali.rice.kns.document.Document, org.kuali.rice.kim.bo.Person)
-//     */
-//    @Override
-//    public FinancialSystemDocumentActionFlags getDocumentActionFlags(Document document, Person user) {
-//        FinancialSystemDocumentActionFlags actionFlags = super.getDocumentActionFlags(document, user);
-//
-//        MaintenanceDocument maintDocument = (MaintenanceDocument) document;
-//        String maintenanceAction = maintDocument.getNewMaintainableObject().getMaintenanceAction();
-//
-//        //  this is used for batch processing of customer records
-//        if (KFSConstants.SYSTEM_USER.equalsIgnoreCase(user.getPrincipalName())) {
-//           actionFlags.setCanApprove(true);
-//           actionFlags.setCanBlanketApprove(true);
-//           actionFlags.setCanAdHocRoute(true);
-//           actionFlags.setCanRoute(true);
-//           return actionFlags;
-//        }
-//        
-//        // if user is not AR SUPERVISOR he cannot approve the customer creation document
-//        if (KNSConstants.MAINTENANCE_NEW_ACTION.equalsIgnoreCase(maintenanceAction) && !ARUtil.isUserInArSupervisorGroup(user)) {
-//
-//            actionFlags.setCanApprove(false);
-//            actionFlags.setCanBlanketApprove(false);
-//
-//        }
-//
-//        // if ((maintenanceAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_EDIT_ACTION) ||
-//        // maintenanceAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_COPY_ACTION)) && !isUserInArSupervisorGroup(user)) {
-//        //
-//        // actionFlags.setCanRoute(false);
-//        // actionFlags.setCanSave(false);
-//        // actionFlags.setCanCancel(false);
-//        //
-//        //        }
+//    //  this is used for batch processing of customer records
+//    if (KFSConstants.SYSTEM_USER.equalsIgnoreCase(user.getPrincipalName())) {
+//        actionFlags.setCanApprove(true);
+//        actionFlags.setCanBlanketApprove(true);
+//        actionFlags.setCanAdHocRoute(true);
+//        actionFlags.setCanRoute(true);
 //        return actionFlags;
 //    }
-
+//
+//    // if user is not AR SUPERVISOR he cannot approve the customer creation document
+//    if (KNSConstants.MAINTENANCE_NEW_ACTION.equalsIgnoreCase(maintenanceAction) && !ARUtil.isUserInArSupervisorGroup(user)) {
+//
+//        actionFlags.setCanApprove(false);
+//        actionFlags.setCanBlanketApprove(false);
+//
+//    }
+//
+//    // if ((maintenanceAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_EDIT_ACTION) ||
+//    // maintenanceAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_COPY_ACTION)) && !isUserInArSupervisorGroup(user)) {
+//    //
+//    // actionFlags.setCanRoute(false);
+//    // actionFlags.setCanSave(false);
+//    // actionFlags.setCanCancel(false);
+//    //
+//    //        }
+//    return actionFlags;
+//}
 }
 

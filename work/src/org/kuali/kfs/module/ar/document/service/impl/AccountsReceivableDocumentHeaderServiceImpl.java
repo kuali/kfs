0ab420f1f552c1849c5db756915f19c9d87caf3c
Andrew Hollamon <andrew@dmentionsystems.com>
2009-01-22 05:21:31 +0000

    Fixed a bug where an AR Processor whose home org wasnt in BOTH billing-org and processing-org would fail to pull a valid processing org for the AR Document Header.  In other words, fixed a bug where the system only worked for KHUNTLEY. KULAR-515

--- work/src/org/kuali/kfs/module/ar/document/service/impl/AccountsReceivableDocumentHeaderServiceImpl.java
+++ work/src/org/kuali/kfs/module/ar/document/service/impl/AccountsReceivableDocumentHeaderServiceImpl.java
@@ -21,16 +21,21 @@ import java.util.Map;
 import org.kuali.kfs.module.ar.ArConstants;
 import org.kuali.kfs.module.ar.businessobject.AccountsReceivableDocumentHeader;
 import org.kuali.kfs.module.ar.businessobject.OrganizationOptions;
+import org.kuali.kfs.module.ar.businessobject.SystemInformation;
 import org.kuali.kfs.module.ar.document.service.AccountsReceivableDocumentHeaderService;
+import org.kuali.kfs.module.ar.document.service.SystemInformationService;
 import org.kuali.kfs.sys.businessobject.ChartOrgHolder;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.service.FinancialSystemUserService;
+import org.kuali.kfs.sys.service.UniversityDateService;
 import org.kuali.rice.kns.service.BusinessObjectService;
 import org.kuali.rice.kns.util.GlobalVariables;
 
 public class AccountsReceivableDocumentHeaderServiceImpl implements AccountsReceivableDocumentHeaderService {
 
     private BusinessObjectService businessObjectService;
+    private UniversityDateService universityDateService;
+    private SystemInformationService sysInfoService;
 
     /**
      * @see org.kuali.kfs.module.ar.document.service.AccountsReceivableDocumentHeaderService#getNewAccountsReceivableDocumentHeader(java.lang.String,
@@ -39,19 +44,38 @@ public class AccountsReceivableDocumentHeaderServiceImpl implements AccountsRece
     public AccountsReceivableDocumentHeader getNewAccountsReceivableDocumentHeader(String chartOfAccountsCode, String organizationCode) {
         AccountsReceivableDocumentHeader accountsReceivableDocumentHeader = new AccountsReceivableDocumentHeader();
 
+        //  we try to get the processing org directly, which we'll get if the initiating user is an AR Processor
+        SystemInformation processingOrg = getProcessingOrgIfExists(chartOfAccountsCode, organizationCode);
+        if (processingOrg != null) {
+            accountsReceivableDocumentHeader.setProcessingChartOfAccountCode(processingOrg.getProcessingChartOfAccountCode());
+            accountsReceivableDocumentHeader.setProcessingOrganizationCode(processingOrg.getProcessingOrganizationCode());
+            return accountsReceivableDocumentHeader;
+        }
+        
+        //  next we try to get the processing org through the initiating user's billing org, if that exists
+        OrganizationOptions orgOptions = getOrgOptionsIfExists(chartOfAccountsCode, organizationCode);
+        if (orgOptions != null) {
+            accountsReceivableDocumentHeader.setProcessingChartOfAccountCode(orgOptions.getProcessingChartOfAccountCode());
+            accountsReceivableDocumentHeader.setProcessingOrganizationCode(orgOptions.getProcessingOrganizationCode());
+            return accountsReceivableDocumentHeader;
+        }
+        
+        //  If we get here, then we didnt find a matching BillingOrg or ProcessingOrg, so we have 
+        // no way to retrieve the document processing org.  This should never happen if the authorization 
+        // is working right, so we're going to puke and die right here.
+        throw new UnsupportedOperationException("");
+    }
+
+    private OrganizationOptions getOrgOptionsIfExists(String chartOfAccountsCode, String organizationCode) {
         Map<String, String> criteria = new HashMap<String, String>();
         criteria.put("chartOfAccountsCode", chartOfAccountsCode);
         criteria.put("organizationCode", organizationCode);
-        OrganizationOptions organizationOptions = (OrganizationOptions) SpringContext.getBean(BusinessObjectService.class).findByPrimaryKey(OrganizationOptions.class, criteria);
-
-        if (organizationOptions != null) {
-            accountsReceivableDocumentHeader.setProcessingChartOfAccountCode(organizationOptions.getProcessingChartOfAccountCode());
-            accountsReceivableDocumentHeader.setProcessingOrganizationCode(organizationOptions.getProcessingOrganizationCode());
-        }
-
-        return accountsReceivableDocumentHeader;
+        return (OrganizationOptions) SpringContext.getBean(BusinessObjectService.class).findByPrimaryKey(OrganizationOptions.class, criteria);
     }
-
+    
+    private SystemInformation getProcessingOrgIfExists(String chartOfAccountsCode, String organizationCode) {
+        return sysInfoService.getByProcessingChartOrgAndFiscalYear(chartOfAccountsCode, organizationCode, universityDateService.getCurrentFiscalYear());
+    }
     /**
      * @see org.kuali.kfs.module.ar.document.service.AccountsReceivableDocumentHeaderService#getNewAccountsReceivableDocumentHeaderForCurrentUser()
      */
@@ -66,6 +90,14 @@ public class AccountsReceivableDocumentHeaderServiceImpl implements AccountsRece
 
     public void setBusinessObjectService(BusinessObjectService businessObjectService) {
         this.businessObjectService = businessObjectService;
+    }
+
+    public void setUniversityDateService(UniversityDateService universityDateService) {
+        this.universityDateService = universityDateService;
+    }
+
+    public void setSysInfoService(SystemInformationService sysInfoService) {
+        this.sysInfoService = sysInfoService;
     }
 
 }

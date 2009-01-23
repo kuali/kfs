c857774281a2da8e5c1dc2fc28b81eecb995b066
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-01-23 20:04:04 +0000

    Weekly Purge

---
+++ work/db/weekly-changes/1-Friday/sql.sql
@@ -0,0 +1,37 @@
+delete from krew_doc_typ_t
+where doc_typ_nm ='UniversalUserMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EDENSERVICE-DOCS.DocumentType'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm ='PayeeMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'KualiBaseDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EmployeeStatusMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EmployeeTypeMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'KualiEmployeeStatusMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'KualiEmployeeTypeMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'BankAccountMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EDENSERVICE-DOCS'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EDENSERVICE-DOCS.RuleDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm ='NonpersonnelSubCategoryMaintenanceDocument'
+/
+

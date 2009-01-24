b8fb92b63d21ff6a74001f07266c1255cfe31b08
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-01-24 01:14:33 +0000

    DAILY COMMIT

--- work/db/weekly-changes/1-Friday/sql.sql
+++ work/db/weekly-changes/1-Friday/sql.sql
@@ -34,4 +34,49 @@ where doc_typ_nm = 'EDENSERVICE-DOCS.RuleDocument'
 delete from krew_doc_typ_t
 where doc_typ_nm ='NonpersonnelSubCategoryMaintenanceDocument'
 /
-
+delete from krew_doc_typ_t
+where doc_typ_nm ='UniversalUserMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EDENSERVICE-DOCS.DocumentType'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm ='PayeeMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'KualiBaseDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EmployeeStatusMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EmployeeTypeMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'KualiEmployeeStatusMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'KualiEmployeeTypeMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'BankAccountMaintenanceDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EDENSERVICE-DOCS'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm = 'EDENSERVICE-DOCS.RuleDocument'
+/
+delete from krew_doc_typ_t
+where doc_typ_nm ='NonpersonnelSubCategoryMaintenanceDocument'
+/
+insert into krns_parm_t 
+(SELECT 'KFS-AR', 'CustomerInvoiceWriteoff',
+'APPROVAL_THRESHOLD', sys_guid(),1,
+'CONFG', '50',
+'Writing off invoices of this value or higher requires Fiscal Officer approval.',
+'A'
+FROM dual)
+/
+alter table CG_AWD_T drop column WRKGRP_NM
+/ 

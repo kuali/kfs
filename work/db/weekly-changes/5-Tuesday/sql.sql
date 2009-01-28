43be374a31bb23821032ec3409fe62b8ca9d6181
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-01-28 01:10:48 +0000

    DAILY COMMIT

--- work/db/weekly-changes/5-Tuesday/sql.sql
+++ work/db/weekly-changes/5-Tuesday/sql.sql
@@ -1,9 +1,74 @@
+drop table ER_ADHOC_WKGRP_T
+/
+insert into krns_parm_t
+(SELECT 'KFS-PURAP', 'PaymentRequest',
+'NON_RESIDENT_ALIEN_TAX_FEDERAL_CHART', sys_guid(),1,
+'CONFG', 'UA',
+'Allowed Chart on the Non-resident Alien Federal',
+'A'
+FROM dual)
+/
+insert into krns_parm_t
+(SELECT 'KFS-PURAP', 'PaymentRequest',
+'NON_RESIDENT_ALIEN_TAX_FEDERAL_ACCOUNT', sys_guid(),1,
+'CONFG', '9612729',
+'Allowed Account on the Non-resident Alien Federal',
+'A'
+FROM dual)
+/
+insert into krns_parm_t
+(SELECT 'KFS-PURAP', 'PaymentRequest',
+'NON_RESIDENT_ALIEN_TAX_FEDERAL_OBJECT_CODE_BY_INCOME_CLASS', sys_guid(),1,
+'CONFG', 'F=9021;I=9022;R=9022',
+'Allowed Object Code(s) by income class on the',
+'A'
+FROM dual)
+/
+insert into krns_parm_t
+(SELECT 'KFS-PURAP', 'PaymentRequest',
+'NON_RESIDENT_ALIEN_TAX_STATE_CHART', sys_guid(),1,
+'CONFG', 'UA',
+'Allowed Chart on the Non-resident Alien State Tax',
+'A'
+FROM dual)
+/
+insert into krns_parm_t
+(SELECT 'KFS-PURAP', 'PaymentRequest',
+'NON_RESIDENT_ALIEN_TAX_STATE_ACCOUNT', sys_guid(),1,
+'CONFG', '9612732',
+'Allowed Account on the Non-resident Alien State',
+'A'
+FROM dual)
+/
+insert into krns_parm_t
+(SELECT 'KFS-PURAP', 'PaymentRequest',
+'NON_RESIDENT_ALIEN_TAX_STATE_OBJECT_CODE_BY_INCOME_CLASS', sys_guid(),1,
+'CONFG', 'F=9021;I=9022;R=9022',
+'Allowed Object Code(s) by income class on the',
+'A'
+FROM dual)
+/ 
+delete krew_doc_typ_t
+where doc_typ_nm = 'DocumentTypeMaintenanceDocument'
+/
 update krim_perm_attr_data_t set attr_val = 'org.kuali.kfs.sys.web.struts.ElectronicFundTransferAction' where attr_data_id = '395'
 /
-
 update krim_role_mbr_attr_data_t set target_primary_key = '1290', attr_val = 'UA' where attr_data_id = '2912'
 /
 update krim_role_mbr_attr_data_t set target_primary_key = '1290', attr_val = 'VPIT' where attr_data_id = '2913'
+/
+insert into krns_parm_t
+SELECT 'KFS-SYS', a.parm_dtl_typ_cd, a.parm_nm, sys_guid(), 1,
+       a.parm_typ_cd, a.txt, a.parm_desc_txt, a.cons_cd
+  FROM krns_parm_t a
+  where nmspc_cd = 'KFS-GL'
+  and parm_dtl_typ_cd = 'UniversityDate'
+  and parm_nm = 'UNIVERSITY_DATE'
+/
+delete from krns_parm_t
+where nmspc_cd = 'KFS-GL'
+  and parm_dtl_typ_cd = 'UniversityDate'
+  and parm_nm = 'UNIVERSITY_DATE'
 /
 
 INSERT INTO KRIM_ROLE_T(ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT) 

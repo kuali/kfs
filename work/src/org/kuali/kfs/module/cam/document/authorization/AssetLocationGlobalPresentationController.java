c1c94d5a10fb3a5e52174f10ba98432d03937765
Queing Lai <queing.lai@usc.edu>
2009-01-29 23:25:28 +0000

    KULCAP-448  Global Retirement - why do we see the blank assets fields before asset is retrieved.

---
+++ work/src/org/kuali/kfs/module/cam/document/authorization/AssetLocationGlobalPresentationController.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2009 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.cam.document.authorization;
+
+import java.util.Set;
+
+import org.kuali.kfs.module.cam.CamsPropertyConstants;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.document.authorization.FinancialSystemMaintenanceDocumentPresentationControllerBase;
+import org.kuali.rice.kns.bo.BusinessObject;
+
+public class AssetLocationGlobalPresentationController extends FinancialSystemMaintenanceDocumentPresentationControllerBase {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AssetLocationGlobalPresentationController.class);
+
+    @Override
+    public Set<String> getConditionallyHiddenPropertyNames(BusinessObject businessObject) {
+        Set<String> fields = super.getConditionallyHiddenPropertyNames(businessObject);
+
+        // only include campitalAssetNumber in the add section within the asset location details collection
+        fields.add(KFSConstants.ADD_PREFIX + "." + CamsPropertyConstants.AssetLocationGlobal.ASSET_LOCATION_GLOBAL_DETAILS + "." + CamsPropertyConstants.AssetLocationGlobal.CAMPUS_CODE);
+        fields.add(KFSConstants.ADD_PREFIX + "." + CamsPropertyConstants.AssetLocationGlobal.ASSET_LOCATION_GLOBAL_DETAILS + "." + CamsPropertyConstants.AssetLocationGlobal.BUILDING_CODE);
+        fields.add(KFSConstants.ADD_PREFIX + "." + CamsPropertyConstants.AssetLocationGlobal.ASSET_LOCATION_GLOBAL_DETAILS + "." + CamsPropertyConstants.AssetLocationGlobal.BUILDING_ROOM_NUMBER);
+        fields.add(KFSConstants.ADD_PREFIX + "." + CamsPropertyConstants.AssetLocationGlobal.ASSET_LOCATION_GLOBAL_DETAILS + "." + CamsPropertyConstants.AssetLocationGlobal.BUILDING_SUB_ROOM_NUMBER);
+        fields.add(KFSConstants.ADD_PREFIX + "." + CamsPropertyConstants.AssetLocationGlobal.ASSET_LOCATION_GLOBAL_DETAILS + "." + CamsPropertyConstants.AssetLocationGlobal.CAMPUS_TAG_NUMBER);
+
+        return fields;
+    }
+
+}

588410328875c5f5385e31282af983d91d5b8c11
Gerardo Velasquez <gerardov@usc.edu>
2009-02-19 22:58:36 +0000

    KULCAP-880 	 Review and improve unit test coverage: cam.util

---
+++ test/unit/src/org/kuali/kfs/module/cam/fixture/BarcodeInventoryErrorDetailPredicateFixture.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.cam.fixture;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.kuali.kfs.module.cam.businessobject.BarcodeInventoryErrorDetail;
+import org.kuali.kfs.module.cam.document.BarcodeInventoryErrorDocument;
+import org.kuali.rice.kns.service.BusinessObjectService;
+
+public enum BarcodeInventoryErrorDetailPredicateFixture {
+    DATA();
+
+    private BusinessObjectService businessObjectService;    
+    private int testDataPos;
+    private static Properties properties;
+    static {
+        String propertiesFileName = "org/kuali/kfs/module/cam/document/service/barcode_inventory_predicate.properties";
+        properties = new Properties();
+        try {
+            properties.load(ClassLoader.getSystemResourceAsStream(propertiesFileName));
+        }
+        catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    static String TEST_RECORD="testRecord";
+    static String RESULT="result";
+    static String DOCUMENT="document";
+    static String BCIE="bcie";
+    static String FIELD_NAMES="fieldNames";
+    static String NUM_OF_REC="numOfRecords";
+    static String DELIMINATOR="deliminator";
+
+    private BarcodeInventoryErrorDetailPredicateFixture() {  
+    }
+
+    public List<BarcodeInventoryErrorDetail> getBarcodeInventoryDetail() {
+        Integer numOfRecords = new Integer(properties.getProperty(BCIE+"."+NUM_OF_REC));                        
+        List<BarcodeInventoryErrorDetail> details = new ArrayList<BarcodeInventoryErrorDetail>();
+
+        String deliminator = properties.getProperty(DELIMINATOR);
+        String fieldNames = properties.getProperty(BCIE+"."+FIELD_NAMES);
+
+        for(int i=1;i<=numOfRecords.intValue();i++) {
+            String propertyKey = BCIE+"."+TEST_RECORD + i;
+            details.add(CamsFixture.DATA_POPULATOR.buildTestDataObject(BarcodeInventoryErrorDetail.class, properties, propertyKey, fieldNames, deliminator));
+        }
+        return details;
+    }
+
+
+    public List<BarcodeInventoryErrorDetail> getExpectedResults() {
+        Integer numOfRecords = new Integer(properties.getProperty(BCIE+"."+NUM_OF_REC));                        
+        List<BarcodeInventoryErrorDetail> details = new ArrayList<BarcodeInventoryErrorDetail>();
+
+        String deliminator = properties.getProperty(DELIMINATOR);
+        String fieldNames = properties.getProperty(BCIE+"."+FIELD_NAMES);
+
+        for(int i=1;i<=numOfRecords.intValue();i++) {
+            String propertyKey = RESULT+"."+TEST_RECORD + i;
+            details.add(CamsFixture.DATA_POPULATOR.buildTestDataObject(BarcodeInventoryErrorDetail.class, properties, propertyKey, fieldNames, deliminator));
+        }
+        return details;
+    }
+
+
+    public BarcodeInventoryErrorDocument getBarcodeInventoryErrorDocument() {
+        BarcodeInventoryErrorDocument barcodeInventoryErrorDocument;
+
+        String deliminator = properties.getProperty(DELIMINATOR);
+        String fieldNames = properties.getProperty(DOCUMENT+"."+FIELD_NAMES);
+
+        String propertyKey = DOCUMENT+"."+TEST_RECORD+"1";
+        barcodeInventoryErrorDocument = CamsFixture.DATA_POPULATOR.buildTestDataObject(BarcodeInventoryErrorDocument.class, properties, propertyKey, fieldNames, deliminator);
+ 
+        return barcodeInventoryErrorDocument;        
+    }
+}
---
+++ test/unit/src/org/kuali/kfs/module/cam/utils/BarcodeInventoryErrorDetailPredicateTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2009 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.cam.utils;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.log4j.Logger;
+import org.kuali.kfs.module.cam.batch.service.AssetBarcodeInventoryLoadService;
+import org.kuali.kfs.module.cam.businessobject.BarcodeInventoryErrorDetail;
+import org.kuali.kfs.module.cam.document.BarcodeInventoryErrorDocument;
+import org.kuali.kfs.module.cam.document.service.AssetBarcodeInventoryLoadServiceTest;
+import org.kuali.kfs.module.cam.fixture.BarcodeInventoryErrorDetailPredicateFixture;
+import org.kuali.kfs.module.cam.util.BarcodeInventoryErrorDetailPredicate;
+import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.fixture.UserNameFixture;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.service.DateTimeService;
+
+@ConfigureContext(session = UserNameFixture.kfs)
+public class BarcodeInventoryErrorDetailPredicateTest extends KualiTestBase {
+    private static Logger LOG = Logger.getLogger(BarcodeInventoryErrorDetailPredicateTest.class);
+
+    /**
+     *     
+     * test the UpdateAssetInformation
+     */
+    public void testUpdateAssetInformation() {
+        BarcodeInventoryErrorDetail barcodeInventoryErrorDetail;
+        BarcodeInventoryErrorDetail barcodeInventoryErrorExpectedDetail;
+
+        BarcodeInventoryErrorDocument barcodeInventoryErrorDocument = BarcodeInventoryErrorDetailPredicateFixture.DATA.getBarcodeInventoryErrorDocument();
+        List<BarcodeInventoryErrorDetail> barcodeInventoryErrorDetails=BarcodeInventoryErrorDetailPredicateFixture.DATA.getBarcodeInventoryDetail();
+        List<BarcodeInventoryErrorDetail> barcodeInventoryErrorExpectedDetails=BarcodeInventoryErrorDetailPredicateFixture.DATA.getExpectedResults();
+
+        BarcodeInventoryErrorDetailPredicate predicatedClosure = new BarcodeInventoryErrorDetailPredicate(barcodeInventoryErrorDocument);
+
+        // searches and replaces
+        CollectionUtils.forAllDo(barcodeInventoryErrorDetails, predicatedClosure);
+
+        for(int row=0;row < barcodeInventoryErrorDetails.size();row++) {
+            barcodeInventoryErrorDetail = barcodeInventoryErrorDetails.get(row);
+            barcodeInventoryErrorExpectedDetail = barcodeInventoryErrorExpectedDetails.get(row);
+
+            assertTrue("Replacement failed.",                    
+                    barcodeInventoryErrorDetail.getCampusCode().equals(barcodeInventoryErrorExpectedDetail.getCampusCode()) &&  
+                    barcodeInventoryErrorDetail.getBuildingCode().equals(barcodeInventoryErrorExpectedDetail.getBuildingCode()) &&
+                    barcodeInventoryErrorDetail.getBuildingRoomNumber().equals(barcodeInventoryErrorExpectedDetail.getBuildingRoomNumber()) && 
+                    barcodeInventoryErrorDetail.getBuildingSubRoomNumber().equals(barcodeInventoryErrorExpectedDetail.getBuildingSubRoomNumber()) &&
+                    barcodeInventoryErrorDetail.getAssetConditionCode().equals(barcodeInventoryErrorExpectedDetail.getAssetConditionCode()));
+        }
+    }
+}

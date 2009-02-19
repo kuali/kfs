2a708302b4c99e45ac112f197bdc904a48925f9d
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-02-19 20:42:17 +0000

    Interim Commit

--- work/db/weekly-changes/7-Thursday/sql.sql
+++ work/db/weekly-changes/7-Thursday/sql.sql
@@ -1,2 +1,14 @@
+insert into pdp_disb_nbr_rng_t 
+(SELECT sys_guid(), 1, 'BL', 100000,
+       100095, 999999, to_date('01/01/2009','MM/DD/YYYY'),
+       'TEST', 'CHCK', 'Y'
+  FROM  dual)
+/
+insert into pdp_disb_nbr_rng_t
+(SELECT sys_guid(), 1, 'BL', 100000,
+       100002, 999999, to_date('01/01/2009','MM/DD/YYYY'),
+       '1010', 'CHCK', 'Y'
+  FROM  dual)
+/
 update krim_entity_emp_info_t set base_slry_amt = '50000'
 /

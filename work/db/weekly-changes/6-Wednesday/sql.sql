3b17f183aeb88bc1b8022b2e2783773c5b9e7caf
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-02-27 18:23:55 +0000

    Weekly Reset/Delete

--- work/db/weekly-changes/6-Wednesday/sql.sql
+++
@@ -1,6 +0,0 @@
-update krns_parm_t
-set txt = 'FRHT;SPHD;MNOR;ORDS;TRDI;FDGR;STGR;MISC'
-where parm_nm = 'ITEM_TYPES_ALLOWING_POSITIVE'
-and nmspc_cd = 'KFS-PURAP'
-and parm_dtl_typ_cd = 'PaymentRequest'
-/
--- work/db/weekly-changes/7-Thursday/sql.sql
+++
@@ -1,6 +0,0 @@
-update krns_parm_t
-set txt = 'MISC;ORDS;TRDI'
-where nmspc_cd = 'KFS-PURAP'
-and parm_dtl_typ_cd = 'PaymentRequest'
-and parm_nm = 'ITEM_TYPES_REQUIRING_USER_ENTERED_DESCRIPTION'
-/

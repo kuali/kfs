5df97e8cc0909ad70c4b1e1d6f80c833aa24fb2f
Harsha Ummerpillai <harsha07@msu.edu>
2009-03-03 21:55:28 +0000

    Minor fix to prevent error from Webutils class

--- work/src/org/kuali/kfs/module/cam/document/web/struts/AssetTransferAction.java
+++ work/src/org/kuali/kfs/module/cam/document/web/struts/AssetTransferAction.java
@@ -36,6 +36,7 @@ import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.document.web.struts.FinancialSystemTransactionalDocumentActionBase;
 import org.kuali.rice.kew.util.KEWConstants;
 import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kns.util.ErrorMap;
 import org.kuali.rice.kns.util.ErrorMessage;
 import org.kuali.rice.kns.util.GlobalVariables;
 import org.kuali.rice.kns.util.WebUtils;
@@ -53,7 +54,7 @@ public class AssetTransferAction extends FinancialSystemTransactionalDocumentAct
     @Override
     public ActionForward docHandler(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         ActionForward docHandlerForward = super.docHandler(mapping, form, request, response);
-        
+
         // refresh asset information
         AssetTransferForm assetTransferForm = (AssetTransferForm) form;
         AssetTransferDocument assetTransferDocument = (AssetTransferDocument) assetTransferForm.getDocument();
@@ -64,16 +65,16 @@ public class AssetTransferAction extends FinancialSystemTransactionalDocumentAct
         asset.refreshReferenceObject(CamsPropertyConstants.Asset.ASSET_PAYMENTS);
         SpringContext.getBean(AssetLocationService.class).setOffCampusLocation(asset);
         SpringContext.getBean(PaymentSummaryService.class).calculateAndSetPaymentSummary(asset);
-        
+
         // populate old asset fields for historic retaining on document
         String command = assetTransferForm.getCommand();
         if (KEWConstants.INITIATE_COMMAND.equals(command)) {
             assetTransferDocument.setOldOrganizationOwnerChartOfAccountsCode(asset.getOrganizationOwnerChartOfAccountsCode());
             assetTransferDocument.setOldOrganizationOwnerAccountNumber(asset.getOrganizationOwnerAccountNumber());
         }
-        
+
         this.refresh(mapping, form, request, response);
-        
+
         return docHandlerForward;
     }
 
@@ -93,7 +94,8 @@ public class AssetTransferAction extends FinancialSystemTransactionalDocumentAct
             Person person = personService.getPerson(assetTransferDocument.getRepresentativeUniversalIdentifier());
             if (person != null) {
                 assetTransferDocument.setAssetRepresentative(person);
-            } else {
+            }
+            else {
                 LOG.error("org.kuali.rice.kim.service.PersonService returned null for uuid " + assetTransferDocument.getRepresentativeUniversalIdentifier());
             }
         }
@@ -117,22 +119,24 @@ public class AssetTransferAction extends FinancialSystemTransactionalDocumentAct
             assetTransferDocument.refreshReferenceObject(CamsPropertyConstants.AssetTransferDocument.ASSET);
         }
     }
-    
+
 
     /**
      * Since the organization fields are view only we need to make sure they are in sync with the data entry fields.
      * 
-     * @see org.kuali.rice.kns.web.struts.action.KualiDocumentActionBase#refresh(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     * @see org.kuali.rice.kns.web.struts.action.KualiDocumentActionBase#refresh(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
     @Override
     public ActionForward refresh(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ((KualiDocumentFormBase) form).setErrorMapFromPreviousRequest(new ErrorMap());
         ActionForward actionForward = super.refresh(mapping, form, request, response);
-        
+
         AssetTransferDocument assetTransferDocument = ((AssetTransferForm) form).getAssetTransferDocument();
-        
+
         assetTransferDocument.refreshReferenceObject(CamsPropertyConstants.AssetTransferDocument.ORGANIZATION_OWNER_ACCOUNT);
         assetTransferDocument.refreshReferenceObject(CamsPropertyConstants.AssetTransferDocument.OLD_ORGANIZATION_OWNER_ACCOUNT);
-        
+
         return actionForward;
     }
 
@@ -148,27 +152,26 @@ public class AssetTransferAction extends FinancialSystemTransactionalDocumentAct
 
         return actionForward;
     }
-    
-    private void allPaymentsFederalOwnedMessage(ActionForm form){
+
+    private void allPaymentsFederalOwnedMessage(ActionForm form) {
         boolean allPaymentsFederalOwned = true;
-        
+
         AssetTransferDocument assetTransferDocument = ((AssetTransferForm) form).getAssetTransferDocument();
 
         for (AssetPayment assetPayment : assetTransferDocument.getAsset().getAssetPayments()) {
             if (!getAssetPaymentService().isPaymentFederalOwned(assetPayment)) {
-                allPaymentsFederalOwned =  false;
+                allPaymentsFederalOwned = false;
             }
         }
-        
-        //display a message for asset not generating ledger entries when it is federally owned 
+
+        // display a message for asset not generating ledger entries when it is federally owned
         if (allPaymentsFederalOwned) {
-            GlobalVariables.getMessageList().add(0, new ErrorMessage(CamsKeyConstants.Transfer.MESSAGE_NO_LEDGER_ENTRY_REQUIRED_TRANSFER));        
+            GlobalVariables.getMessageList().add(0, new ErrorMessage(CamsKeyConstants.Transfer.MESSAGE_NO_LEDGER_ENTRY_REQUIRED_TRANSFER));
         }
     }
-    
+
     private AssetPaymentService getAssetPaymentService() {
         return SpringContext.getBean(AssetPaymentService.class);
     }
-        
-}
 
+}

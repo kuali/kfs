6a143ea775e63fcebd2db2c6313dbfaadf568f74
Venkat PremChandran <vpremcha@kuali.org>
2009-03-05 16:46:29 +0000

    KULPURAP-3430 - PREQ - unable to submit only non-qty line item payment requests

--- work/src/org/kuali/kfs/module/purap/businessobject/AccountsPayableItemBase.java
+++ work/src/org/kuali/kfs/module/purap/businessobject/AccountsPayableItemBase.java
@@ -97,7 +97,9 @@ public abstract class AccountsPayableItemBase extends PurApItemBase implements A
     public KualiDecimal getExtendedPrice() {
         if (ObjectUtils.isNotNull(this.getItemUnitPrice()) && this.getItemType().isAmountBasedGeneralLedgerIndicator()) {
             extendedPrice = new KualiDecimal(this.getItemUnitPrice().toString());
-        }else if (ObjectUtils.isNull(this.getItemUnitPrice()) && this.getItemType().isAmountBasedGeneralLedgerIndicator()){
+        }else if (ObjectUtils.isNull(this.getItemUnitPrice()) && 
+                  this.getItemType().isAmountBasedGeneralLedgerIndicator() &&
+                  this.getItemType().isAdditionalChargeIndicator()){ // This additional charges check is needed since non qty items also dont have unit price - KULPURAP-3430
             // extendedPrice should be null if the unit price is null - KULPURAP-3274
             extendedPrice = null;
         }

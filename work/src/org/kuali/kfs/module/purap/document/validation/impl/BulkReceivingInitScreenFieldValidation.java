cbda0af2780aaeb0948304ad0fc288b745699333
Venkat PremChandran <vpremcha@kuali.org>
2009-03-05 13:07:46 +0000

    KULPURAP-3434 - Bulk Receiving document is giving errors about the format of fields. - Added validation xml

---
+++ work/src/org/kuali/kfs/module/purap/document/validation/impl/BulkReceivingInitScreenFieldValidation.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.document.validation.impl;
+
+import org.kuali.kfs.module.purap.document.BulkReceivingDocument;
+import org.kuali.kfs.sys.KFSKeyConstants;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kns.service.DictionaryValidationService;
+import org.kuali.rice.kns.util.GlobalVariables;
+
+public class BulkReceivingInitScreenFieldValidation extends GenericValidation {
+
+    private DictionaryValidationService dictionaryValidationService;
+    
+    public boolean validate(AttributedDocumentEvent event) {
+        
+        BulkReceivingDocument bulkReceivingDocument = (BulkReceivingDocument)event.getDocument();
+        
+        GlobalVariables.getErrorMap().clearErrorPath();
+        GlobalVariables.getErrorMap().addToErrorPath(KFSPropertyConstants.DOCUMENT);
+        
+        dictionaryValidationService.validateAttributeFormat("BulkReceivingDocument","shipmentPackingSlipNumber",bulkReceivingDocument.getShipmentPackingSlipNumber(),KFSKeyConstants.ERROR_INVALID_FORMAT);
+        dictionaryValidationService.validateAttributeFormat("BulkReceivingDocument","shipmentBillOfLadingNumber",bulkReceivingDocument.getShipmentBillOfLadingNumber(),KFSKeyConstants.ERROR_INVALID_FORMAT);
+        
+        return true;
+    }
+
+    public DictionaryValidationService getDictionaryValidationService() {
+        return dictionaryValidationService;
+    }
+
+    public void setDictionaryValidationService(DictionaryValidationService dictionaryValidationService) {
+        this.dictionaryValidationService = dictionaryValidationService;
+    }
+
+}
---
+++ work/src/org/kuali/kfs/module/purap/document/validation/impl/BulkReceivingPurchaseOrderIdValidation.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.document.validation.impl;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.module.purap.PurapConstants;
+import org.kuali.kfs.module.purap.PurapKeyConstants;
+import org.kuali.kfs.module.purap.PurapPropertyConstants;
+import org.kuali.kfs.module.purap.document.BulkReceivingDocument;
+import org.kuali.kfs.module.purap.document.PurchaseOrderDocument;
+import org.kuali.kfs.module.purap.document.service.BulkReceivingService;
+import org.kuali.kfs.module.purap.document.service.PurchaseOrderService;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.ObjectUtils;
+
+public class BulkReceivingPurchaseOrderIdValidation extends GenericValidation {
+
+    private PurchaseOrderService purchaseOrderService;
+    
+    public boolean validate(AttributedDocumentEvent event) {
+        
+        boolean valid = true;
+        
+        BulkReceivingDocument bulkReceivingDocument = (BulkReceivingDocument)event.getDocument();
+        
+        GlobalVariables.getErrorMap().clearErrorPath();
+        GlobalVariables.getErrorMap().addToErrorPath(KFSPropertyConstants.DOCUMENT);
+        
+        return canCreateBulkReceivingDocument(bulkReceivingDocument);
+
+    }
+
+    /**
+     * Determines if it is valid to create a bulk receiving document.  
+     * 
+     * @param bulkReceivingDocument
+     * @return
+     */
+    private boolean canCreateBulkReceivingDocument(BulkReceivingDocument bulkReceivingDocument){
+        
+        boolean valid = true;
+        
+        if (bulkReceivingDocument.getPurchaseOrderIdentifier() != null){
+            PurchaseOrderDocument po = SpringContext.getBean(PurchaseOrderService.class).getCurrentPurchaseOrder(bulkReceivingDocument.getPurchaseOrderIdentifier());
+            
+            if (ObjectUtils.isNull(po)){
+                GlobalVariables.getErrorMap().putError(PurapPropertyConstants.PURCHASE_ORDER_IDENTIFIER, PurapKeyConstants.ERROR_BULK_RECEIVING_DOCUMENT_INVALID_PO, bulkReceivingDocument.getDocumentNumber(), bulkReceivingDocument.getPurchaseOrderIdentifier().toString());
+                valid = false;
+            }else{
+                if (!(po.getStatusCode().equals(PurapConstants.PurchaseOrderStatuses.OPEN) || 
+                    po.getStatusCode().equals(PurapConstants.PurchaseOrderStatuses.CLOSED))){
+                    valid &= false;
+                    GlobalVariables.getErrorMap().putError(PurapPropertyConstants.PURCHASE_ORDER_IDENTIFIER, PurapKeyConstants.ERROR_BULK_RECEIVING_PO_NOT_OPEN, bulkReceivingDocument.getDocumentNumber(), bulkReceivingDocument.getPurchaseOrderIdentifier().toString());
+                }else{
+                    String docNumberInProcess = SpringContext.getBean(BulkReceivingService.class).getBulkReceivingDocumentNumberInProcessForPurchaseOrder(po.getPurapDocumentIdentifier(), bulkReceivingDocument.getDocumentNumber());
+                    if (StringUtils.isNotEmpty(docNumberInProcess)){
+                        valid &= false;
+                        GlobalVariables.getErrorMap().putError(PurapPropertyConstants.PURCHASE_ORDER_IDENTIFIER, PurapKeyConstants.ERROR_BULK_RECEIVING_DOCUMENT_ACTIVE_FOR_PO, docNumberInProcess, bulkReceivingDocument.getPurchaseOrderIdentifier().toString());
+                    }
+                }
+            }
+        }
+         
+        return valid;
+    }
+
+    public PurchaseOrderService getPurchaseOrderService() {
+        return purchaseOrderService;
+    }
+
+    public void setPurchaseOrderService(PurchaseOrderService purchaseOrderService) {
+        this.purchaseOrderService = purchaseOrderService;
+    }
+
+}

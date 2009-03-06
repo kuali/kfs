0631e05faea6baec2afa551b9c9a3413e1c080db
Bin Gao <gaob@msu.edu>
2009-03-06 16:20:20 +0000

    KULLAB-655: LJV submit - sub-account, project code and sub-object-code are required error messages

--- work/src/org/kuali/kfs/module/ld/document/validation/impl/LaborJournalVoucherPositionCodeExistenceCheckValidation.java
+++ work/src/org/kuali/kfs/module/ld/document/validation/impl/LaborJournalVoucherPositionCodeExistenceCheckValidation.java
@@ -21,44 +21,43 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.module.ld.LaborConstants;
-import org.kuali.kfs.module.ld.LaborKeyConstants;
 import org.kuali.kfs.module.ld.businessobject.LaborJournalVoucherDetail;
 import org.kuali.kfs.module.ld.businessobject.PositionData;
-import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSKeyConstants;
 import org.kuali.kfs.sys.KFSPropertyConstants;
-import org.kuali.kfs.sys.ObjectUtil;
 import org.kuali.kfs.sys.businessobject.AccountingLine;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
 import org.kuali.rice.kns.service.BusinessObjectService;
 import org.kuali.rice.kns.service.DataDictionaryService;
 import org.kuali.rice.kns.util.GlobalVariables;
-import org.kuali.kfs.sys.document.validation.GenericValidation;
-import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
 
 /**
- * Validates that a labor journal voucher document's accounting lines have valid Position Code 
+ * Validates that a labor journal voucher document's accounting lines have valid Position Code
  */
 public class LaborJournalVoucherPositionCodeExistenceCheckValidation extends GenericValidation {
-    private AccountingLine accountingLineForValidation;   
-    
+    private AccountingLine accountingLineForValidation;
+
     /**
-     * Validates that the accounting line in the labor journal voucher document for valid position code 
+     * Validates that the accounting line in the labor journal voucher document for valid position code
+     * 
      * @see org.kuali.kfs.validation.Validation#validate(java.lang.Object[])
      */
     public boolean validate(AttributedDocumentEvent event) {
         boolean result = true;
-        
+
         LaborJournalVoucherDetail laborJournalVoucherDetail = (LaborJournalVoucherDetail) getAccountingLineForValidation();
         String positionNumber = laborJournalVoucherDetail.getPositionNumber();
-        
-        if (StringUtils.isBlank(positionNumber) || LaborConstants.getDashEmplId().equals(positionNumber)) {
-            return true ;
+
+        if (StringUtils.isBlank(positionNumber) || LaborConstants.getDashPositionNumber().equals(positionNumber)) {
+            return true;
         }
+
         if (!positionCodeExistenceCheck(positionNumber)) {
-            result = false ;
+            result = false;
         }
-        return result ;    
+        return result;
     }
 
     /**
@@ -66,26 +65,27 @@ public class LaborJournalVoucherPositionCodeExistenceCheckValidation extends Gen
      * 
      * @param positionNumber positionNumber is checked against the collection of position number matches
      * @return True if the given position number exists, false otherwise.
-     */ 
+     */
     private boolean positionCodeExistenceCheck(String positionNumber) {
-        
-        boolean positionNumberExists  = true ;
-        
-        Map criteria = new HashMap();
+
+        boolean positionNumberExists = true;
+
+        Map<String, Object> criteria = new HashMap<String, Object>();
         criteria.put(KFSPropertyConstants.POSITION_NUMBER, positionNumber);
-        
-        Collection positionNumberMatches = SpringContext.getBean(BusinessObjectService.class).findMatching(PositionData.class, criteria);
+
+        Collection<PositionData> positionNumberMatches = SpringContext.getBean(BusinessObjectService.class).findMatching(PositionData.class, criteria);
         if (positionNumberMatches == null || positionNumberMatches.isEmpty()) {
             String label = SpringContext.getBean(DataDictionaryService.class).getDataDictionary().getBusinessObjectEntry(PositionData.class.getName()).getAttributeDefinition(KFSPropertyConstants.POSITION_NUMBER).getLabel();
-            GlobalVariables.getErrorMap().putError(KFSPropertyConstants.POSITION_NUMBER, KFSKeyConstants.ERROR_EXISTENCE, label) ;
-            positionNumberExists = false ;
+            GlobalVariables.getErrorMap().putError(KFSPropertyConstants.POSITION_NUMBER, KFSKeyConstants.ERROR_EXISTENCE, label);
+            positionNumberExists = false;
         }
-            
-        return positionNumberExists ;
+
+        return positionNumberExists;
     }
 
     /**
-     * Gets the accountingLineForValidation attribute. 
+     * Gets the accountingLineForValidation attribute.
+     * 
      * @return Returns the accountingLineForValidation.
      */
     public AccountingLine getAccountingLineForValidation() {
@@ -94,6 +94,7 @@ public class LaborJournalVoucherPositionCodeExistenceCheckValidation extends Gen
 
     /**
      * Sets the accountingLineForValidation attribute value.
+     * 
      * @param accountingLineForValidation The accountingLineForValidation to set.
      */
     public void setAccountingLineForValidation(AccountingLine accountingLineForValidation) {

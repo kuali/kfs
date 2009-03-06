75c10754a24be09b210d75a42fc5a0420e5ae370
Dennis Friends <dwf5@cornell.edu>
2009-03-06 20:37:39 +0000

    Replaced the DateUtlis.getDifferenceInDays() method call with a custom method because the DateUtlis method did not support enough precision in it's calculations and was leading to incorrect rounding errors.

--- work/src/org/kuali/kfs/module/ar/document/validation/impl/CustomerInvoiceDueDateValidation.java
+++ work/src/org/kuali/kfs/module/ar/document/validation/impl/CustomerInvoiceDueDateValidation.java
@@ -46,7 +46,7 @@ public class CustomerInvoiceDueDateValidation extends GenericValidation {
             return false;
         }
         else {
-            double diffInDays = DateUtils.getDifferenceInDays(billingDateTimestamp, dueDateTimestamp);
+            long diffInDays = getDifferenceInDays(billingDateTimestamp, dueDateTimestamp); //DateUtils.getDifferenceInDays(billingDateTimestamp, dueDateTimestamp);
             int maxNumOfDaysAfterCurrentDateForInvoiceDueDate = Integer.parseInt(parameterService.getParameterValue(CustomerInvoiceDocument.class, ArConstants.MAXIMUM_NUMBER_OF_DAYS_AFTER_CURRENT_DATE_FOR_INVOICE_DUE_DATE));
             if (diffInDays >= maxNumOfDaysAfterCurrentDateForInvoiceDueDate) {
                 GlobalVariables.getErrorMap().putError(DOCUMENT_ERROR_PREFIX + ArPropertyConstants.CustomerInvoiceDocumentFields.INVOICE_DUE_DATE, ArKeyConstants.ERROR_CUSTOMER_INVOICE_DOCUMENT_INVALID_INVOICE_DUE_DATE_MORE_THAN_X_DAYS, maxNumOfDaysAfterCurrentDateForInvoiceDueDate + "");
@@ -58,6 +58,51 @@ public class CustomerInvoiceDueDateValidation extends GenericValidation {
         return true;
     }
 
+    /**
+     * 
+     * This method calculates the difference in days between the two timestamps provided.
+     * 
+     * This method is used instead of DateUtils.getDifferenceInDays() because there is a rounding issue within the timestamp that exists which must be dealt with to 
+     * prevent improper calculations.  This issue is similar to the problems that exist with adding and subtracting doubles and the inherently bad way that Java handles
+     * numbers.  
+     * 
+     * The approach used within DateUtils does not offer enough accuracy to calculate the difference consistently and accurately.
+     * 
+     * @param t1
+     * @param t2
+     * @return The difference in days between the two given timestamps.  
+     */
+    public static long getDifferenceInDays (Timestamp t1, Timestamp t2) {
+        // Make sure the result is always > 0
+        if (t1.compareTo (t2) < 0) {
+            Timestamp tmp = t1;
+            t1 = t2;
+            t2 = tmp;
+        }
+
+        // Timestamps mix milli and nanoseconds in the API, so we have to separate the two
+        long diffSeconds = (t1.getTime () / 1000) - (t2.getTime () / 1000);
+        // For normals dates, we have millisecond precision
+        int nano1 = ((int) t1.getTime () % 1000) * 1000000;
+        nano1 = t1.getNanos ();
+        int nano2 = ((int) t2.getTime () % 1000) * 1000000;
+        nano2 = t2.getNanos ();
+
+        int diffNanos = nano1 - nano2;
+        if (diffNanos < 0) {
+            // Borrow one second
+            diffSeconds --;
+            diffNanos += 1000000000;
+        }
+
+        // mix nanos and millis again
+        Timestamp result = new Timestamp ((diffSeconds * 1000) + (diffNanos / 1000000));
+        // setNanos() with a value of in the millisecond range doesn't affect the value of the time field
+        // while milliseconds in the time field will modify nanos! Damn, this API is a *mess*
+        result.setNanos (diffNanos);
+        return result.getDate();
+    }
+    
     public CustomerInvoiceDocument getCustomerInvoiceDocument() {
         return customerInvoiceDocument;
     }

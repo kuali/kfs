67f6dff22e49a064babc19515e4c923a84f8d0bd
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-03-06 07:01:39 +0000

    DAILY COMMIT

--- work/db/weekly-changes/7-Thursday/sql.sql
+++ work/db/weekly-changes/7-Thursday/sql.sql
@@ -4,3 +4,5 @@ delete from krim_perm_attr_data_t where target_primary_key in ('309', '310', '31
 /
 delete from krim_perm_t where perm_id in ('309', '310', '311', '312')
 /
+alter table AR_INV_DOC_T add (RECURRED_INV_IND varchar(1))
+/

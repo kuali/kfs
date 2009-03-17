342d317a6fed5b4d6d94f28093f8fc03f9987eeb
Shawn Choo <schoo@vivantech.com>
2009-03-17 17:35:37 +0000

    Changed for DB vs File - pdp feeds gl entry

--- work/src/org/kuali/kfs/pdp/batch/service/impl/ExtractTransactionsServiceImpl.java
+++ work/src/org/kuali/kfs/pdp/batch/service/impl/ExtractTransactionsServiceImpl.java
@@ -15,12 +15,17 @@
  */
 package org.kuali.kfs.pdp.batch.service.impl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.sql.Date;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.kuali.kfs.gl.GeneralLedgerConstants;
 import org.kuali.kfs.gl.businessobject.LedgerEntryHolder;
 import org.kuali.kfs.gl.businessobject.OriginEntryGroup;
 import org.kuali.kfs.gl.businessobject.OriginEntrySource;
@@ -45,6 +50,7 @@ public class ExtractTransactionsServiceImpl implements ExtractTransactionsServic
     private DateTimeService dateTimeService;
     private String reportsDirectory;
     private KualiConfigurationService kualiConfigurationService;
+    private String batchFileDirectoryName;
 
     /**
      * @see org.kuali.kfs.pdp.batch.service.ExtractTransactionsService#extractGlTransactions()
@@ -53,23 +59,64 @@ public class ExtractTransactionsServiceImpl implements ExtractTransactionsServic
         LOG.debug("extractGlTransactions() started");
 
         Date processDate = dateTimeService.getCurrentSqlDate();
-        OriginEntryGroup oeg = null;
+        
+        // TODO - shawn: this should be moved to some kind of util class
+        java.util.Date jobRunDate =  dateTimeService.getCurrentDate();
+        String timeString = jobRunDate.toString();
+        String year = timeString.substring(timeString.length() - 4, timeString.length());
+        String month = timeString.substring(4, 7);
+        String day = timeString.substring(8, 10);
+        String hour = timeString.substring(11, 13);
+        String min = timeString.substring(14, 16);
+        String sec = timeString.substring(17, 19);
+        String fileTimeInfo =  "." + year + "-" + month + "-" + day + "." + hour + "-" + min + "-" + sec;
+        //OriginEntryGroup oeg = null;
+        
+        String extractTGlTransactionFileName = GeneralLedgerConstants.BatchFileSystem.EXTRACT_TRANSACTION_FILE + fileTimeInfo + GeneralLedgerConstants.BatchFileSystem.EXTENSION;
+        File extractTGlTransactionFile = new File(batchFileDirectoryName + File.separator + extractTGlTransactionFileName);
+        PrintStream  extractTGlTransactionPS = null; 
+        
         Iterator transactions = glPendingTransactionService.getUnextractedTransactions();
         while (transactions.hasNext()) {
             GlPendingTransaction tran = (GlPendingTransaction) transactions.next();
 
             // We only want to create the group if there are transactions in the group
-            if (oeg == null) {
-                oeg = originEntryGroupService.createGroup(processDate, OriginEntrySource.PDP, true, true, true);
+//            if (oeg == null) {
+//                oeg = originEntryGroupService.createGroup(processDate, OriginEntrySource.PDP, true, true, true);
+//            }
+            
+            if (extractTGlTransactionPS == null){
+                try {
+                    extractTGlTransactionPS = new PrintStream(extractTGlTransactionFile);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException("enterpriseFeedFile doesn't exist " + extractTGlTransactionFileName);
+                }
             }
-
-            originEntryService.createEntry(tran.getOriginEntry(), oeg);
+            
+           // originEntryService.createEntry(tran.getOriginEntry(), oeg);
+            extractTGlTransactionPS.printf("%s\n", tran.getOriginEntry().getLine());
 
             tran.setProcessInd(true);
             glPendingTransactionService.save(tran);
         }
-
-        if (oeg != null) {
+        
+        
+        
+        if (extractTGlTransactionPS != null) {
+            extractTGlTransactionPS.close();
+            
+            // TODO - shawn: this should be moved to some kind of util class
+            // create done file
+            String extractTGlTransactionDoneFileName = extractTGlTransactionFileName.replace(GeneralLedgerConstants.BatchFileSystem.EXTENSION, GeneralLedgerConstants.BatchFileSystem.DONE_FILE_EXTENSION);
+            File extractTGlTransactionDoneFile = new File (batchFileDirectoryName + File.separator + extractTGlTransactionDoneFileName);
+            if (!extractTGlTransactionDoneFile.exists()){
+                try {
+                    extractTGlTransactionDoneFile.createNewFile();
+                } catch (IOException e) {
+                    throw new RuntimeException();
+                }
+            }
+            
             String reportTitle = this.kualiConfigurationService.getPropertyString(PdpKeyConstants.EXTRACT_TRANSACTION_SERVICE_REPORT_TITLE);
             reportTitle = MessageFormat.format(reportTitle, new Object[]{ null });
             
@@ -78,7 +125,7 @@ public class ExtractTransactionsServiceImpl implements ExtractTransactionsServic
             
             // Run a report
             Collection groups = new ArrayList();
-            groups.add(oeg);
+           // groups.add(oeg);
             LedgerEntryHolder ledgerEntries = originEntryService.getSummaryByGroupId(groups);
 
             LedgerReport ledgerReport = new LedgerReport();
@@ -108,5 +155,9 @@ public class ExtractTransactionsServiceImpl implements ExtractTransactionsServic
 
     public void setKualiConfigurationService(KualiConfigurationService kualiConfigurationService) {
         this.kualiConfigurationService = kualiConfigurationService;
+    }
+
+    public void setBatchFileDirectoryName(String batchFileDirectoryName) {
+        this.batchFileDirectoryName = batchFileDirectoryName;
     }
 }

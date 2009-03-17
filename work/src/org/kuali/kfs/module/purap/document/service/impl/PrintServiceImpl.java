71e884eb45584a273b2daf4d06550bdcf050e3e2
Cathy Tan <tanc@ais.msu.edu>
2009-03-17 21:02:33 +0000

    KULPURAP-3475

--- work/src/org/kuali/kfs/module/purap/document/service/impl/PrintServiceImpl.java
+++ work/src/org/kuali/kfs/module/purap/document/service/impl/PrintServiceImpl.java
@@ -157,7 +157,7 @@ public class PrintServiceImpl implements PrintService {
         ContractManager contractManager = po.getContractManager();
         String contractManagerCampusCode = "N/A";
         if (contractManager != null && contractManager.getContractManagerUserIdentifier() != null) {
-            Person contractManagerUser = KIMServiceLocator.getPersonService().getPersonByPrincipalName(contractManager.getContractManagerUserIdentifier().toLowerCase());
+            Person contractManagerUser = KIMServiceLocator.getPersonService().getPerson(contractManager.getContractManagerUserIdentifier().toLowerCase());
             contractManagerCampusCode = contractManagerUser.getCampusCode();
         }
 

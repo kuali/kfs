9e4716d584c1bd8a6bd7eccb98d7517b37ca4795
Gary	Palmer <gwpalmer@iu.edu>
2009-03-23 20:40:50 +0000

    Implement FTE and Percent Time conversions

---
+++ work/src/org/kuali/kfs/module/bc/util/OjbBudgetConstructionPercentTimeConversion.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2009 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.bc.util;
+
+import java.math.BigDecimal;
+
+import org.apache.ojb.broker.accesslayer.conversions.ConversionException;
+import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
+
+/**
+ * Ensures that the database Percent Time value is rounded to 2 decimal places when reading. Any saved Percent Time value will
+ * automatically be forced to 2 decimals during validation.
+ */
+public class OjbBudgetConstructionPercentTimeConversion implements FieldConversion {
+
+    /**
+     * @see org.apache.ojb.broker.accesslayer.conversions.FieldConversion#javaToSql(java.lang.Object)
+     */
+    public Object javaToSql(Object source) throws ConversionException {
+        if (source != null && source instanceof BigDecimal) {
+            BigDecimal converted = (BigDecimal) source;
+            return converted;
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * @see org.apache.ojb.broker.accesslayer.conversions.FieldConversion#sqlToJava(java.lang.Object)
+     */
+    public Object sqlToJava(Object source) throws ConversionException {
+        // Check for null, and verify object type.
+        // Do conversion if our type is correct (BigDecimal).
+        if (source != null && source instanceof BigDecimal) {
+            BigDecimal converted = (BigDecimal) source;
+
+            return converted.setScale(2, BigDecimal.ROUND_HALF_UP);
+        }
+        else {
+            return null;
+        }
+    }
+
+}

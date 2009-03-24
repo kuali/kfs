90ecc875576056f2ebf3b2b3a3e046b2e4101caa
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-03-24 18:45:15 +0000

    Recommitting to remove artifact from parm_desc_txt (get rid of carriage return).

--- work/db/weekly-changes/4-Monday/sql.sql
+++ work/db/weekly-changes/4-Monday/sql.sql
@@ -2,9 +2,7 @@ insert into krns_parm_t
 (SELECT 'KFS-LD', 'LaborBalancingStep',
 'NUMBER_OF_PAST_FISCAL_YEARS_TO_CONSIDER', sys_guid(),1,
 'CONFG', '1',
-'Number of fiscal years to subtract from current fiscal year to represent what t
-he start range on the balancing batch job is. This is used for laborBalancingSte
-p.',
+'Number of fiscal years to subtract from current fiscal year to represent what the start range on the balancing batch job is. This is used for laborBalancingStep.',
 'A'
 FROM dual)
 ;
@@ -12,8 +10,7 @@ insert into krns_parm_t
 (SELECT 'KFS-LD', 'LaborBalancingStep',
 'NUMBER_OF_COMPARISON_FAILURES_TO_PRINT_PER_REPORT', sys_guid(),1,
 'CONFG', '10',
-'Total number of failures to print on the report for each category of balancing
-failures on the balancing batch job. This is used for laborBalancingStep.',
+'Total number of failures to print on the report for each category of balancing failures on the balancing batch job. This is used for laborBalancingStep.',
 'A'
 FROM dual)
 ;
@@ -21,9 +18,7 @@ insert into krns_parm_t
 (SELECT 'KFS-GL', 'PosterBalancingStep',
 'NUMBER_OF_PAST_FISCAL_YEARS_TO_CONSIDER', sys_guid(),1,
 'CONFG', '1',
-'Number of fiscal years to subtract from current fiscal year to represent what t
-he start range on the balancing batch job is. This is used for posterBalancingSt
-ep.',
+'Number of fiscal years to subtract from current fiscal year to represent what the start range on the balancing batch job is. This is used for posterBalancingStep.',
 'A'
 FROM dual)
 ;
@@ -31,8 +26,7 @@ insert into krns_parm_t
 (SELECT 'KFS-GL', 'PosterBalancingStep',
 'NUMBER_OF_COMPARISON_FAILURES_TO_PRINT_PER_REPORT', sys_guid(),1,
 'CONFG', '10',
-'Total number of failures to print on the report for each category of balancing
-failures on the balancing batch job. This is used for posterBalancingStep.',
+'Total number of failures to print on the report for each category of balancing failures on the balancing batch job. This is used for posterBalancingStep.',
 'A'
 FROM dual)
 ;

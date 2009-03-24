9408622bc4c56d35826d3ad4ddcae7ff47172592
Cathy Tan <tanc@ais.msu.edu>
2009-03-24 20:43:41 +0000

    KULPURAP-3140 so that unit test won't fail

--- test/unit/src/org/kuali/kfs/module/purap/document/service/PurchaseOrderServiceTest.java
+++ test/unit/src/org/kuali/kfs/module/purap/document/service/PurchaseOrderServiceTest.java
@@ -332,7 +332,7 @@ public class PurchaseOrderServiceTest extends KualiTestBase {
      * 
      * @throws Exception
      */
-    
+    @RelatesTo(JiraIssue.KULPURAP3140)
     public void testPrintPurchaseOrderQuoteRequestsListPDF() throws Exception {
         PurchaseOrderDocument po = 
             PurchaseOrderDocumentFixture.PO_ONLY_REQUIRED_FIELDS_MULTI_ITEMS.createPurchaseOrderDocument();
@@ -366,7 +366,7 @@ public class PurchaseOrderServiceTest extends KualiTestBase {
      * 
      * @throws Exception
      */
-   
+    @RelatesTo(JiraIssue.KULPURAP3140)
     public void testPrintPurchaseOrderQuotePDF() throws Exception {
         PurchaseOrderDocument po = 
             PurchaseOrderDocumentFixture.PO_ONLY_REQUIRED_FIELDS_MULTI_ITEMS.createPurchaseOrderDocument();

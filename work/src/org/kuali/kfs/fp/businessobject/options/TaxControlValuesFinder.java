acf93a9f04214a872d8185a161fe48354fbb9ac0
James Smith <smith750@iu.edu>
2009-04-03 19:55:35 +0000

    heal the broken build...

--- work/src/org/kuali/kfs/fp/businessobject/options/TaxControlValuesFinder.java
+++ work/src/org/kuali/kfs/fp/businessobject/options/TaxControlValuesFinder.java
@@ -16,13 +16,9 @@
 package org.kuali.kfs.fp.businessobject.options;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.kuali.kfs.fp.businessobject.TaxControlCode;
-import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
-import org.kuali.rice.kns.service.KeyValuesService;
 import org.kuali.rice.kns.web.ui.KeyLabelPair;
 
 /**
@@ -34,14 +30,14 @@ public class TaxControlValuesFinder extends KeyValuesBase {
      * @see org.kuali.keyvalues.KeyValuesFinder#getKeyValues()
      */
     public List getKeyValues() {
-        List<TaxControlCode> boList = (List<TaxControlCode>) SpringContext.getBean(KeyValuesService.class).findAll(TaxControlCode.class);
+        //List<TaxControlCode> boList = (List<TaxControlCode>) SpringContext.getBean(KeyValuesService.class).findAll(TaxControlCode.class);
         List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
         keyValues.add(new KeyLabelPair("", ""));
-        for (TaxControlCode element : boList) {
+        /*for (TaxControlCode element : boList) {
             if(element.isActive()) {
                 keyValues.add(new KeyLabelPair(element.getCode(), element.getName()));
             }
-        }
+        }*/
 
         return keyValues;
     }

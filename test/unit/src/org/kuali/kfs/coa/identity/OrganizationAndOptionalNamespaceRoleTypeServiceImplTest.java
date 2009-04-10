49b3363bdc686ebdd87a84752a8027b56838e54a
James Smith <smith750@iu.edu>
2009-04-10 13:57:10 +0000

    fix unit test with bad column names...

--- test/unit/src/org/kuali/kfs/coa/identity/OrganizationAndOptionalNamespaceRoleTypeServiceImplTest.java
+++ test/unit/src/org/kuali/kfs/coa/identity/OrganizationAndOptionalNamespaceRoleTypeServiceImplTest.java
@@ -41,13 +41,13 @@ public class OrganizationAndOptionalNamespaceRoleTypeServiceImplTest extends Kua
     protected static final String SQL_ADD_COA_ORG_1 = "INSERT INTO KRIM_ROLE_MBR_T(ROLE_MBR_ID, OBJ_ID, ver_nbr, ROLE_ID, MBR_ID, MBR_TYP_CD) VALUES (\n" + 
     		"'" + TEST_ROLE_MEMBER_ID + "', '" + TEST_ROLE_MEMBER_ID + "', 1, (SELECT role_id FROM krim_role_t WHERE nmspc_cd = 'KFS-SYS' AND role_nm = 'User')" +
     				", (SELECT prncpl_id FROM krim_prncpl_t WHERE prncpl_nm = '" + USER + "' ), 'P' )";
-    protected static final String SQL_ADD_COA_ORG_2 = "INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, ver_nbr, TARGET_PRIMARY_KEY, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) \n" + 
+    protected static final String SQL_ADD_COA_ORG_2 = "INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, ver_nbr, role_mbr_id, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) \n" + 
     "    VALUES('"+TEST_ROLE_MEMBER_ID+"_1', '"+TEST_ROLE_MEMBER_ID+"_1', 1, '" + TEST_ROLE_MEMBER_ID + "', (SELECT kim_typ_id FROM krim_role_t WHERE nmspc_cd = \'KFS-SYS\' AND role_nm = \'User\')" +
             ", (SELECT kim_attr_defn_id FROM krim_attr_defn_t WHERE nm = '"+KfsKimAttributes.CHART_OF_ACCOUNTS_CODE+"'), '" + USER_COA_CHART + "' )";
-    protected static final String SQL_ADD_COA_ORG_3 = "INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, ver_nbr, TARGET_PRIMARY_KEY, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) \n" + 
+    protected static final String SQL_ADD_COA_ORG_3 = "INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, ver_nbr, role_mbr_id, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) \n" + 
     "    VALUES('"+TEST_ROLE_MEMBER_ID+"_2', '"+TEST_ROLE_MEMBER_ID+"_2', 1, '" + TEST_ROLE_MEMBER_ID + "', (SELECT kim_typ_id FROM krim_role_t WHERE nmspc_cd = \'KFS-SYS\' AND role_nm = \'User\')" +
             ", (SELECT kim_attr_defn_id FROM krim_attr_defn_t WHERE nm = '"+KfsKimAttributes.ORGANIZATION_CODE+"'), '" + USER_COA_ORG + "' )";
-    protected static final String SQL_ADD_COA_ORG_4 = "INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, ver_nbr, TARGET_PRIMARY_KEY, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) \n" + 
+    protected static final String SQL_ADD_COA_ORG_4 = "INSERT INTO KRIM_ROLE_MBR_ATTR_DATA_T(ATTR_DATA_ID, OBJ_ID, ver_nbr, role_mbr_id, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) \n" + 
     "    VALUES('"+TEST_ROLE_MEMBER_ID+"_3', '"+TEST_ROLE_MEMBER_ID+"_3', 1, '" + TEST_ROLE_MEMBER_ID + "', (SELECT kim_typ_id FROM krim_role_t WHERE nmspc_cd = \'KFS-SYS\' AND role_nm = \'User\')" +
             ", (SELECT kim_attr_defn_id FROM krim_attr_defn_t WHERE nm = '"+KfsKimAttributes.NAMESPACE_CODE+"'), '" + KFSConstants.ParameterNamespaces.CHART + "' )";
     

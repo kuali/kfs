12a5995c4dd30a0ad8d9942183238b8755e9276e
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-04-24 18:11:00 +0000

    Weekly Reset/Delete

--- work/db/weekly-changes/1-Friday/sql.sql
+++
@@ -1,7 +0,0 @@
-insert into krns_parm_t
-(select nmspc_cd, parm_dtl_typ_cd, 'COST_SHARE_DOCUMENT_TYPES',
-sys_guid(), 1, parm_typ_cd, txt, parm_desc_txt, cons_cd
-from krns_parm_t
-where parm_nm = 'COST_SHARE_ENCUMBRANCE_DOCUMENT_TYPES')
-;
-commit;

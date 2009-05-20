c966807babbd6c4b79dac03a9bc4ed4cfd0f0c5c
Poonam Bhargava <bhargavp@kuali.org>
2009-05-20 15:27:43 +0000

    KFSMI-3298

--- work/src/org/kuali/kfs/coa/identity/AccountingOrganizationHierarchyReviewRoleTypeServiceImpl.java
+++ work/src/org/kuali/kfs/coa/identity/AccountingOrganizationHierarchyReviewRoleTypeServiceImpl.java
@@ -84,10 +84,27 @@ public class AccountingOrganizationHierarchyReviewRoleTypeServiceImpl extends Or
     public AttributeSet validateUnmodifiableAttributes(
             String kimTypeId, AttributeSet originalAttributeSet, AttributeSet newAttributeSet){
         AttributeSet validationErrors = super.validateUnmodifiableAttributes(kimTypeId, originalAttributeSet, newAttributeSet);
-        String toAmountRoleMember = getAttributeValue(originalAttributeSet, KfsKimAttributes.TO_AMOUNT);
-        String toAmountDelegationMember = getAttributeValue(newAttributeSet, KfsKimAttributes.TO_AMOUNT);
         List<String> attributeErrors = null;
         KimAttributeImpl attributeImpl;
+
+        String fromAmountRoleMember = getAttributeValue(originalAttributeSet, KfsKimAttributes.FROM_AMOUNT);
+        String fromAmountDelegationMember = getAttributeValue(newAttributeSet, KfsKimAttributes.FROM_AMOUNT);
+        if(!isGreaterNumber(fromAmountDelegationMember, fromAmountRoleMember)){
+            attributeImpl = getAttributeImpl(KfsKimAttributes.FROM_AMOUNT);
+            GlobalVariables.getErrorMap().putError(
+                    KfsKimAttributes.FROM_AMOUNT, RiceKeyConstants.ERROR_DELEGATION_FROM_AMOUNT_LESSER, 
+                    dataDictionaryService.getAttributeLabel(attributeImpl.getComponentName(), KfsKimAttributes.FROM_AMOUNT));
+            attributeErrors = extractErrorsFromGlobalVariablesErrorMap(KfsKimAttributes.FROM_AMOUNT);
+        }
+        if(attributeErrors!=null){
+            for(String err: attributeErrors){
+                validationErrors.put(KfsKimAttributes.FROM_AMOUNT, err);
+            }
+            attributeErrors = null;
+        }
+        
+        String toAmountRoleMember = getAttributeValue(originalAttributeSet, KfsKimAttributes.TO_AMOUNT);
+        String toAmountDelegationMember = getAttributeValue(newAttributeSet, KfsKimAttributes.TO_AMOUNT);
         if(isGreaterNumber(toAmountDelegationMember, toAmountRoleMember)){
             attributeImpl = getAttributeImpl(KfsKimAttributes.TO_AMOUNT);
             GlobalVariables.getErrorMap().putError(
@@ -96,25 +113,12 @@ public class AccountingOrganizationHierarchyReviewRoleTypeServiceImpl extends Or
             attributeErrors = extractErrorsFromGlobalVariablesErrorMap(KfsKimAttributes.TO_AMOUNT);
         }
         if(attributeErrors!=null){
-            for(String err:attributeErrors){
+            for(String err: attributeErrors){
                 validationErrors.put(KfsKimAttributes.TO_AMOUNT, err);
             }
+            attributeErrors = null;
         }
-        
-        String fromAmountRoleMember = getAttributeValue(originalAttributeSet, KfsKimAttributes.FROM_AMOUNT);
-        String fromAmountDelegationMember = getAttributeValue(newAttributeSet, KfsKimAttributes.FROM_AMOUNT);
-        if(isGreaterNumber(fromAmountDelegationMember, fromAmountRoleMember)){
-            attributeImpl = getAttributeImpl(KfsKimAttributes.FROM_AMOUNT);
-            GlobalVariables.getErrorMap().putError(
-                    KfsKimAttributes.FROM_AMOUNT, RiceKeyConstants.ERROR_DELEGATION_FROM_AMOUNT_LESSER, 
-                    dataDictionaryService.getAttributeLabel(attributeImpl.getComponentName(), KfsKimAttributes.FROM_AMOUNT));
-            attributeErrors = extractErrorsFromGlobalVariablesErrorMap(KfsKimAttributes.FROM_AMOUNT);
-        }
-        if(attributeErrors!=null){
-            for(String err:attributeErrors){
-                validationErrors.put(KfsKimAttributes.FROM_AMOUNT, err);
-            }
-        }
+
         return validationErrors;
     }
 

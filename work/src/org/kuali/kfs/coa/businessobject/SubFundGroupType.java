10b1ca241b1f3782d93385cf7b32a3db762c74d8
Gary	McGlinn <gzmcglinn@ucdavis.edu>
2009-05-28 00:12:44 +0000

    KFSMI-3385
    modified class to extend Inactivatable.

--- work/src/org/kuali/kfs/coa/businessobject/SubFundGroupType.java
+++ work/src/org/kuali/kfs/coa/businessobject/SubFundGroupType.java
@@ -18,12 +18,13 @@ package org.kuali.kfs.coa.businessobject;
 
 import java.util.LinkedHashMap;
 
+import org.kuali.rice.kns.bo.Inactivateable;
 import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
 
 /**
  * 
  */
-public class SubFundGroupType extends PersistableBusinessObjectBase {
+public class SubFundGroupType extends PersistableBusinessObjectBase implements Inactivateable {
 
     private String subFundGroupTypeCode;
     private String subFundGroupTypeDescription;

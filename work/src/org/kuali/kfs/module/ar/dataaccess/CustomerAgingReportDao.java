46c3d94cf1ebf3917176cfeb99fc97998231c32c
Dennis Friends <dwf5@cornell.edu>
2009-07-17 12:37:26 +0000

    KULAR-830 : Add some simple javadocs.

--- work/src/org/kuali/kfs/module/ar/dataaccess/CustomerAgingReportDao.java
+++ work/src/org/kuali/kfs/module/ar/dataaccess/CustomerAgingReportDao.java
@@ -20,21 +20,110 @@ import java.util.HashMap;
 import org.kuali.rice.kns.util.KualiDecimal;
 
 public interface CustomerAgingReportDao {
+
+    // PROCESSING CHART AND ORG METHODS
+    
+    /**
+     * This method generates a database query and retrieves the total of all invoices for the given processing chart and org codes, 
+     * which have billing dates within the given date range, for which there is an outstanding balance due.  
+     * 
+     * @param chart Processing chart code for the invoices being retrieved.
+     * @param org Processing org code for the invoices being retrieved.
+     * @param begin Beginning date of the date range used to find invoice balances due.
+     * @param end Ending date of the date range used to find invoice balances due.
+     * @return 
+     */
     HashMap<String, KualiDecimal> findInvoiceAmountByProcessingChartAndOrg(String chart, String org, java.sql.Date begin, java.sql.Date end);
 
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param org
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findAppliedAmountByProcessingChartAndOrg(String chart, String org, java.sql.Date begin, java.sql.Date end);
 
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param org
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findDiscountAmountByProcessingChartAndOrg(String chart, String org, java.sql.Date begin, java.sql.Date end);
 
+    // BILLING CHART AND ORG METHODS
+    
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param org
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findInvoiceAmountByBillingChartAndOrg(String chart, String org, java.sql.Date begin, java.sql.Date end);
 
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param org
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findAppliedAmountByBillingChartAndOrg(String chart, String org, java.sql.Date begin, java.sql.Date end);
 
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param org
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findDiscountAmountByBillingChartAndOrg(String chart, String org, java.sql.Date begin, java.sql.Date end);
 
+    // ACCOUNT METHODS
+    
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param account
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findInvoiceAmountByAccount(String chart, String account, java.sql.Date begin, java.sql.Date end);
 
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param account
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findAppliedAmountByAccount(String chart, String account, java.sql.Date begin, java.sql.Date end);
 
+    /**
+     * 
+     * This method...
+     * @param chart
+     * @param account
+     * @param begin
+     * @param end
+     * @return
+     */
     HashMap<String, KualiDecimal> findDiscountAmountByAccount(String chart, String account, java.sql.Date begin, java.sql.Date end);
 }

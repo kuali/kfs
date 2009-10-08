0419ea97bc158781a93826470c0b891922c31477
James Smith <smith750@iu.edu>
2009-10-08 18:01:09 +0000

    more updates to data dictionary based origin entry parsing

--- work/src/org/kuali/kfs/sys/businessobject/BusinessObjectStringParserFieldUtils.java
+++ work/src/org/kuali/kfs/sys/businessobject/BusinessObjectStringParserFieldUtils.java
@@ -29,22 +29,34 @@ import org.kuali.rice.kns.service.DataDictionaryService;
  * An abstract class which provides help in determining field lengths of business objects being parsed from Strings
  */
 public abstract class BusinessObjectStringParserFieldUtils {
+    protected org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(getClass());
+    private Map<String, Integer> fieldLengthMap;
+    private Map<String, Integer> fieldBeginningPositionMap;
+    
+    /**
+     * @return a Map with attribute names as keys and lengths of how long those fields are specified to be in the DataDictionary as values
+     */
+    public Map<String, Integer> getFieldLengthMap() {
+        if (fieldLengthMap == null) {
+            initializeFieldLengthMap();
+        }
+        return fieldLengthMap;
+    }
+    
     /**
      * Calculates a map with the field length of all of the attributes of the class given by the
      * getBusinessObjectClass method
-     * @return a Map with attribute names as keys and lengths as values
      */
-    public Map<String, Integer> getFieldLengthMap() {
-        Map<String, Integer> fieldLengthMap = new HashMap<String, Integer>();
+    protected void initializeFieldLengthMap() {
+        fieldLengthMap = new HashMap<String, Integer>();
         DataDictionaryService dataDictionaryService = SpringContext.getBean(DataDictionaryService.class);
         List<AttributeDefinition> attributes = dataDictionaryService.getDataDictionary().getBusinessObjectEntry(getBusinessObjectClass().getName()).getAttributes();
 
         for (AttributeDefinition attributeDefinition : attributes) {
             Integer fieldLength;
-            fieldLength = dataDictionaryService.getAttributeMaxLength(OriginEntryFull.class, attributeDefinition.getName());
+            fieldLength = dataDictionaryService.getAttributeMaxLength(getBusinessObjectClass(), attributeDefinition.getName());
             fieldLengthMap.put(attributeDefinition.getName(), fieldLength);
         }
-        return fieldLengthMap;
     }
     
     /**
@@ -53,21 +65,32 @@ public abstract class BusinessObjectStringParserFieldUtils {
     public abstract Class<? extends BusinessObject> getBusinessObjectClass();
 
     /**
-     * Calculates the beginning positions of each field in the array returned by getOrderedProperties, based on
-     * the length map calculated by getFieldLengthMap()
-     * @return a Map with business object field names as keys and starting positions as values
+     * @return a Map with business object field names as keys and starting positions of each field in the String as values
      */
     public Map<String, Integer> getFieldBeginningPositionMap() {
-        Map<String, Integer> positionMap = new HashMap<String, Integer>();
+        if (fieldBeginningPositionMap == null) {
+            initializeFieldBeginningPositionMap();
+        }
+        return fieldBeginningPositionMap;
+    }
+    
+    /**
+     * Calculates the beginning positions of each field in the array returned by getOrderedProperties, based on
+     * the length map calculated by getFieldLengthMap().
+     */
+    protected void initializeFieldBeginningPositionMap() {
+        fieldBeginningPositionMap = new HashMap<String, Integer>();
         Map<String, Integer> lengthMap = getFieldLengthMap();
         
         int lengthTracker = 0;
    
         for (String property : getOrderedProperties()) {
-            positionMap.put(property, new Integer(lengthTracker));
+            fieldBeginningPositionMap.put(property, new Integer(lengthTracker));
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Finding position for property: "+property+"; length = "+lengthMap.get(property));
+            }
             lengthTracker += lengthMap.get(property).intValue();
         }
-        return positionMap;
     }
     
     /**

9f64115605c6312f3b54240ce50d69ec61b71b77
HeatherStapleton <hstaplet@iu.edu>
2009-10-28 14:38:34 +0000

    KFSMI-5113: adding method that is duplicated in PurchasingAccountsPayableFormBase, but at this date too much risk to attempt to share the code.

--- work/src/org/kuali/kfs/module/purap/document/web/struts/ElectronicInvoiceRejectForm.java
+++ work/src/org/kuali/kfs/module/purap/document/web/struts/ElectronicInvoiceRejectForm.java
@@ -16,20 +16,26 @@
 package org.kuali.kfs.module.purap.document.web.struts;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.kuali.kfs.module.purap.document.ElectronicInvoiceRejectDocument;
+import org.kuali.kfs.pdp.PdpPropertyConstants;
+import org.kuali.kfs.pdp.businessobject.PurchasingPaymentDetail;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSParameterKeyConstants;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.document.web.struts.FinancialSystemTransactionalDocumentFormBase;
+import org.kuali.kfs.sys.service.impl.KfsParameterConstants;
 import org.kuali.rice.kns.service.KualiConfigurationService;
+import org.kuali.rice.kns.service.ParameterService;
+import org.kuali.rice.kns.util.KNSConstants;
+import org.kuali.rice.kns.util.UrlFactory;
 import org.kuali.rice.kns.web.ui.ExtraButton;
 
 /**
- * Struts Action Form for Electroinc Invoice Reject document.
+ * Struts Action Form for Electronic Invoice Reject document.
  */
 public class ElectronicInvoiceRejectForm extends FinancialSystemTransactionalDocumentFormBase {
-
-//    private PurchaseOrderVendorStipulation newPurchaseOrderVendorStipulationLine;
 
     /**
      * Constructs a PaymentRequestForm instance and sets up the appropriately casted document.
@@ -95,4 +101,30 @@ public class ElectronicInvoiceRejectForm extends FinancialSystemTransactionalDoc
         extraButtons.add(newButton);
     }
     
+    /**
+     * This method builds the url for the disbursement info on the purap documents.
+     * @return the disbursement info url
+     */
+    public String getDisbursementInfoUrl() {
+        String basePath = SpringContext.getBean(KualiConfigurationService.class).getPropertyString(KFSConstants.APPLICATION_URL_KEY);
+        ParameterService parameterService = SpringContext.getBean(ParameterService.class);
+
+        String orgCode = parameterService.getParameterValue(KfsParameterConstants.PURCHASING_BATCH.class, KFSParameterKeyConstants.PurapPdpParameterConstants.PURAP_PDP_ORG_CODE);
+        String subUnitCode = parameterService.getParameterValue(KfsParameterConstants.PURCHASING_BATCH.class, KFSParameterKeyConstants.PurapPdpParameterConstants.PURAP_PDP_SUB_UNIT_CODE);
+
+        Properties parameters = new Properties();
+        parameters.put(KFSConstants.DISPATCH_REQUEST_PARAMETER, KFSConstants.SEARCH_METHOD);
+        parameters.put(KFSConstants.BACK_LOCATION, basePath + "/" + KFSConstants.MAPPING_PORTAL + ".do");
+        parameters.put(KNSConstants.DOC_FORM_KEY, "88888888");
+        parameters.put(KFSConstants.BUSINESS_OBJECT_CLASS_ATTRIBUTE, PurchasingPaymentDetail.class.getName());
+        parameters.put(KFSConstants.HIDE_LOOKUP_RETURN_LINK, "true");
+        parameters.put(KFSConstants.SUPPRESS_ACTIONS, "false");
+        parameters.put(PdpPropertyConstants.PaymentDetail.PAYMENT_UNIT_CODE, orgCode);
+        parameters.put(PdpPropertyConstants.PaymentDetail.PAYMENT_SUBUNIT_CODE, subUnitCode);
+
+        String lookupUrl = UrlFactory.parameterizeUrl(basePath + "/" + KFSConstants.LOOKUP_ACTION, parameters);
+
+        return lookupUrl;
+    }
+
 }

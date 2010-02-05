4def3437eca4b6dac3747c5dbff8e29c6cbcdc44
Winston Dang <wkd@hawaii.edu>
2010-02-05 19:00:23 +0000

    KFSMI-5330  Created a maintainableImpl to handle clearing of chart and subunit on copy and making those fields readonly if editing

---
+++ work/src/org/kuali/kfs/pdp/document/datadictionary/CustomerProfileMaintenanceDocumentMaintainableImpl.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.pdp.document.datadictionary;
+
+import java.security.GeneralSecurityException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.coa.businessobject.AccountDelegate;
+import org.kuali.kfs.coa.businessobject.AccountDelegateGlobal;
+import org.kuali.kfs.coa.identity.OrgReviewRole;
+import org.kuali.kfs.coa.identity.OrgReviewRoleLookupableHelperServiceImpl;
+import org.kuali.kfs.coa.service.AccountDelegateService;
+import org.kuali.kfs.coa.service.OrganizationReversionService;
+import org.kuali.kfs.pdp.businessobject.CustomerProfile;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.FinancialSystemMaintainable;
+import org.kuali.rice.core.service.EncryptionService;
+import org.kuali.rice.kim.service.RoleManagementService;
+import org.kuali.rice.kns.bo.DocumentHeader;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.document.MaintenanceLock;
+import org.kuali.rice.kns.maintenance.Maintainable;
+import org.kuali.rice.kns.service.BusinessObjectAuthorizationService;
+import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.kns.service.DateTimeService;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+import org.kuali.rice.kns.util.ObjectUtils;
+import org.kuali.rice.kns.web.ui.Field;
+import org.kuali.rice.kns.web.ui.Row;
+import org.kuali.rice.kns.web.ui.Section;
+
+/**
+ * This class is a special implementation of Maintainable specifically for Account Delegates. It was created to correctly update the
+ * default Start Date on edits and copies, ala JIRA #KULRNE-62.
+ */
+public class CustomerProfileMaintenanceDocumentMaintainableImpl extends FinancialSystemMaintainable {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CustomerProfileMaintenanceDocumentMaintainableImpl.class);
+
+    /**
+     * This method will reset AccountDelegate's Start Date to the current timestamp on edits and copies
+     * 
+     * @see org.kuali.rice.kns.maintenance.KualiMaintainableImpl#processAfterRetrieve()
+     */
+    @Override
+    public void processAfterCopy( MaintenanceDocument document, Map<String,String[]> parameters ) {
+        
+        super.processAfterCopy( document, parameters );
+        CustomerProfile customerProfile = (CustomerProfile) document.getNewMaintainableObject().getBusinessObject();
+        customerProfile.setChartCode(null);
+        customerProfile.setSubUnitCode(null);
+    }
+    
+    
+    public List getSections(MaintenanceDocument document, Maintainable oldMaintainable) {
+        List<Section> sections = super.getSections(document, oldMaintainable);
+        //If oldMaintainable is null, it means we are trying to get sections for the old part
+        //If oldMaintainable is not null, it means we are trying to get sections for the new part
+        //Refer to KualiMaintenanceForm lines 288-294
+        CustomerProfile customerProfile = (CustomerProfile) document.getNewMaintainableObject().getBusinessObject();
+        if(oldMaintainable==null) return sections;
+        if (shouldReviewTypesFieldBeReadOnly(document) == false) return sections;
+
+        for (Section section : sections) {
+            for (Row row : section.getRows()) {
+                for (Field field : row.getFields()) {
+                    if (customerProfile.CHART_OF_ACCOUNTS_FIELD_NAME.equals(field.getPropertyName())) {
+                        field.setReadOnly(true);
+                     }
+                    if (customerProfile.SUB_UNIT_CODE_FIELD_NAME.equals(field.getPropertyName())) {
+                        field.setReadOnly(true);
+                    }
+                }
+            }
+        }
+        return sections;
+    }
+
+
+    protected boolean shouldReviewTypesFieldBeReadOnly(MaintenanceDocument document){
+        CustomerProfile  customerProfile = (CustomerProfile)document.getNewMaintainableObject().getBusinessObject();
+        if(StringUtils.isEmpty(customerProfile.getChartCode())) return false;
+        if(StringUtils.isEmpty(customerProfile.getSubUnitCode())) return false;
+        return true;
+    }
+
+
+  
+}

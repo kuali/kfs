fb9b127659b46d267fad07c825cc53e38dd8b7a0
Winston Dang <wkd@hawaii.edu>
2010-03-10 21:58:31 +0000

    KFSMI-5488

--- work/db/upgrades/4.0/oracle/kfs/03-10-2010.sql
+++ work/db/upgrades/4.0/oracle/kfs/03-10-2010.sql
@@ -1,6 +1,6 @@
 ALTER TABLE AR_ORG_OPTION_T ADD org_postal_cntry_cd varchar2(2);
 
-INSERT INTO AR_ORG_OPTION_T (org_postal_cntry_cd) VALUES ('US');
+UPDATE AR_ORG_OPTION_T SET org_postal_cntry_cd = 'US';
 
 alter table AR_ORG_OPTION_T
 add constraint

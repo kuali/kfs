abf661f131d9bdc2689c8d7d009de5e90b555665
Winston Dang <wkd@hawaii.edu>
2010-03-11 17:47:47 +0000

    KFSMI-5488

--- work/db/upgrades/4.0/oracle/kfs/03-10-2010.sql
+++ work/db/upgrades/4.0/oracle/kfs/03-10-2010.sql
@@ -3,6 +3,3 @@ ALTER TABLE AR_ORG_OPTION_T ADD org_postal_cntry_cd varchar2(2);
 UPDATE AR_ORG_OPTION_T SET org_postal_cntry_cd = 'US';
 
 alter table AR_ORG_OPTION_T
-add constraint
-  AR_ORG_OPTION_TR5 FOREIGN KEY (org_postal_zip_cd, org_postal_cntry_cd)
-  references KR_POSTAL_CODE_T (postal_cd, postal_cntry_cd);

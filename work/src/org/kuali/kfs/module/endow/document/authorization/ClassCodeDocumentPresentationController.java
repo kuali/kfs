36326ffd2d1d6095f6d1b16da888dedde2137fed
Daniela Balmus <db483@cornell.edu>
2010-03-17 20:37:13 +0000

    KULENDOW-252 Make class code type read only in the edit maintenance document

---
+++ work/src/org/kuali/kfs/module/endow/document/authorization/ClassCodeDocumentPresentationController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.document.authorization;
+
+import java.util.Set;
+
+import org.kuali.kfs.module.endow.EndowPropertyConstants;
+import org.kuali.kfs.sys.document.authorization.FinancialSystemMaintenanceDocumentPresentationControllerBase;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.util.KNSConstants;
+
+public class ClassCodeDocumentPresentationController extends FinancialSystemMaintenanceDocumentPresentationControllerBase {
+
+    /**
+     * @see org.kuali.rice.kns.document.authorization.MaintenanceDocumentPresentationControllerBase#getConditionallyReadOnlyPropertyNames(org.kuali.rice.kns.document.MaintenanceDocument)
+     */
+    @Override
+    public Set<String> getConditionallyReadOnlyPropertyNames(MaintenanceDocument document) {
+
+        Set<String> fields = super.getConditionallyReadOnlyPropertyNames(document);
+
+        if (KNSConstants.MAINTENANCE_EDIT_ACTION.equals(document.getNewMaintainableObject().getMaintenanceAction())) {
+            // a class code, once established as a certain class code type cannot change to a different class code type
+            fields.add(EndowPropertyConstants.CLASS_CODE_TYPE);
+
+        }
+        return fields;
+    }
+
+}

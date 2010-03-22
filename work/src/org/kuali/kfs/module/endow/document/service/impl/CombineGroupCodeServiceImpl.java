37e3fa41773af98fb1577c1360fff4c599b98fb4
Daniela Balmus <db483@cornell.edu>
2010-03-22 15:06:04 +0000

    KULENDOW-281 Add combine group code - code and description

---
+++ work/src/org/kuali/kfs/module/endow/document/service/impl/CombineGroupCodeServiceImpl.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.document.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.module.endow.EndowPropertyConstants;
+import org.kuali.kfs.module.endow.businessobject.CombineGroupCode;
+import org.kuali.kfs.module.endow.document.service.CombineGroupCodeService;
+import org.kuali.rice.kns.service.BusinessObjectService;
+
+public class CombineGroupCodeServiceImpl implements CombineGroupCodeService {
+
+    private BusinessObjectService businessObjectService;
+
+    /**
+     * @see org.kuali.kfs.module.endow.document.service.CombineGroupCodeService#getByPrimaryKey(java.lang.String)
+     */
+    public CombineGroupCode getByPrimaryKey(String combineGroupCode) {
+        CombineGroupCode combineGroup = null;
+        if (StringUtils.isNotBlank(combineGroupCode)) {
+            Map<String, String> criteria = new HashMap<String, String>();
+            criteria.put(EndowPropertyConstants.KUALICODEBASE_CODE, combineGroupCode);
+
+            combineGroup = (CombineGroupCode) businessObjectService.findByPrimaryKey(CombineGroupCode.class, criteria);
+        }
+        return combineGroup;
+    }
+
+    /**
+     * This method gets the businessObjectService.
+     * 
+     * @return businessObjectService
+     */
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+
+    /**
+     * This method sets the businessObjectService
+     * 
+     * @param businessObjectService
+     */
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+
+}
---
+++ work/web-root/scripts/module/endow/kemidCombineGroupCode.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ function loadCombineGroupCodeDesc(combineGroupCodeFieldName){
+	var elPrefix = findElPrefix( combineGroupCodeFieldName.name );
+	var combineGroupCodeDescriptionFieldName = elPrefix + ".combineGroup.name";
+ 	setCombineGroupCodeDescription(combineGroupCodeFieldName, combineGroupCodeDescriptionFieldName);
+ }
+  
+ function setCombineGroupCodeDescription(combineGroupCodeFieldName, combineGroupCodeDescriptionFieldName){
+ 
+	var combineGroupCode = DWRUtil.getValue(combineGroupCodeFieldName);
+    
+	if (combineGroupCode =='') {
+		clearRecipients(combineGroupCodeDescriptionFieldName);
+	} else {
+		combineGroupCode = combineGroupCode.toUpperCase();
+		
+		var dwrReply = {
+			callback:function(data) {
+			if (data != null && typeof data == 'object') {
+				setRecipientValue( combineGroupCodeDescriptionFieldName, data.name);
+			} else {
+				setRecipientValue(combineGroupCodeDescriptionFieldName, wrapError("combine group code not found" ), true);			
+			} },
+			errorHandler:function(errorMessage ) { 
+				setRecipientValue(combineGroupCodeDescriptionFieldName, wrapError("combine group code not found" ), true);
+			}
+		};
+		CombineGroupCodeService.getByPrimaryKey(combineGroupCode, dwrReply);
+	}
+}

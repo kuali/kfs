f64a88afbcdfe9afb7e333678ce5205ce53d93e7
Daniela Balmus <db483@cornell.edu>
2010-03-22 16:06:58 +0000

    KULENDOW-280 Add javascript to set descriptions

--- work/web-root/scripts/module/endow/kemidFee.js
+++ work/web-root/scripts/module/endow/kemidFee.js
@@ -14,32 +14,103 @@
  * limitations under the License.
  */
 
- function loadFeeStartDate(feeMethodCodeFieldName){
-	var elPrefix = findElPrefix( feeMethodCodeFieldName.name );
+function setRelatedFields(feeMethodCodeFieldName) {
+	var elPrefix = findElPrefix(feeMethodCodeFieldName.name);
 	var feeStartDateFieldName = elPrefix + ".feeStartDate";
- 	setFeeStartDate(feeMethodCodeFieldName, feeStartDateFieldName);
- }
-  
- function setFeeStartDate( feeMethodCodeFieldName, feeStartDateFieldName ){
- 
-	var feeMethodCode = DWRUtil.getValue( feeMethodCodeFieldName );
-    
-	if (feeMethodCode =='') {
+	var feeMethodDescriptionFieldName = elPrefix + ".feeMethod.name";
+
+	setFeeStartDate(feeMethodCodeFieldName, feeStartDateFieldName);
+
+	setFeeMethodDescription(feeMethodCodeFieldName,
+			feeMethodDescriptionFieldName);
+}
+
+function setFeeStartDate(feeMethodCodeFieldName, feeStartDateFieldName) {
+
+	var feeMethodCode = DWRUtil.getValue(feeMethodCodeFieldName);
+
+	if (feeMethodCode == '') {
 		clearRecipients(feeStartDateFieldName);
 	} else {
 		feeMethodCode = feeMethodCode.toUpperCase();
-		
+
 		var dwrReply = {
-			callback:function(data) {
-			if ( data != null ) {
-				setRecipientValue( feeStartDateFieldName, data );
-			} else {
-				setRecipientValue( feeStartDateFieldName, wrapError( "failed to display fee start date" ), true );			
-			} },
-			errorHandler:function( errorMessage ) { 
-				setRecipientValue( feeStartDateFieldName, wrapError( "failed to display fee start date" ), true );
+			callback : function(data) {
+				if (data != null) {
+					setRecipientValue(feeStartDateFieldName, data);
+				} else {
+					setRecipientValue(feeStartDateFieldName,
+							wrapError("failed to display fee start date"), true);
+				}
+			},
+			errorHandler : function(errorMessage) {
+				setRecipientValue(feeStartDateFieldName,
+						wrapError("failed to display fee start date"), true);
 			}
 		};
-		FeeMethodService.getFeeMethodNextProcessDateForAjax( feeMethodCode, dwrReply );
+		FeeMethodService.getFeeMethodNextProcessDateForAjax(feeMethodCode,
+				dwrReply);
+	}
+}
+
+function setFeeMethodDescription(feeMethodFieldName,
+		feeMethodDescriptionFieldName) {
+
+	var feeMethod = DWRUtil.getValue(feeMethodFieldName);
+
+	if (feeMethod == '') {
+		clearRecipients(feeMethodDescriptionFieldName);
+	} else {
+		feeMethod = feeMethod.toUpperCase();
+
+		var dwrReply = {
+			callback : function(data) {
+				if (data != null && typeof data == 'object') {
+					setRecipientValue(feeMethodDescriptionFieldName, data.name);
+				} else {
+					setRecipientValue(feeMethodDescriptionFieldName,
+							wrapError("fee method not found"), true);
+				}
+			},
+			errorHandler : function(errorMessage) {
+				setRecipientValue(feeMethodDescriptionFieldName,
+						wrapError("fee method not found"), true);
+			}
+		};
+		FeeMethodService.getByPrimaryKey(feeMethod, dwrReply);
+	}
+}
+
+function loadKEMIDShortTitle(kemidFieldName) {
+	var elPrefix = findElPrefix(kemidFieldName.name);
+	var kemidShortTitleFieldName = elPrefix
+			+ ".chargeFeeToKemidObjRef.shortTitle";
+
+	setKEMIDShortTitle(kemidFieldName, kemidShortTitleFieldName);
+}
+
+function setKEMIDShortTitle(kemidFieldName, kemidShortTitleFieldName) {
+
+	var kemid = DWRUtil.getValue(kemidFieldName);
+
+	if (kemid == '') {
+		clearRecipients(kemidShortTitleFieldName, "");
+	} else {
+		kemid = kemid.toUpperCase();
+		var dwrReply = {
+			callback : function(data) {
+				if (data != null && typeof data == 'object') {
+					setRecipientValue(kemidShortTitleFieldName, data.shortTitle);
+				} else {
+					setRecipientValue(kemidShortTitleFieldName,
+							wrapError("kemid not found"), true);
+				}
+			},
+			errorHandler : function(errorMessage) {
+				setRecipientValue(kemidShortTitleFieldName,
+						wrapError("kemid not found"), true);
+			}
+		};
+		KEMIDService.getByPrimaryKey(kemid, dwrReply);
 	}
 }

4e5ff52b75762d5f7fcc0abc15b7aeaca08d6c6f
Tapan Mokha <tmokha@indiana.edu>
2010-03-23 18:53:14 +0000

    KULENDOW-274 In KEMID --> Source of Funds Tab, Fund Source Cod requires Code-CodeDescription display.

---
+++ work/web-root/scripts/module/endow/fundSourceCode.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ function loadFundSourceDesc(fundSourceCodeFieldName)
+ { 
+	var elPrefix = findElPrefix( fundSourceCodeFieldName.name );
+	var fundSourceCodeDescFieldName = elPrefix + ".fundSource.name";
+	setFundSourceDesc(fundSourceCodeFieldName, fundSourceCodeDescFieldName);
+ }
+ 
+ function setFundSourceDesc( fundSourceCodeFieldName, fundSourceCodeDescFieldName ){
+	 
+	var fundSourceCode = DWRUtil.getValue( fundSourceCodeFieldName );
+
+	if (fundSourceCode =='') {
+		clearRecipients(fundSourceCodeDescFieldName, "");
+	} else {
+		fundSourceCode = fundSourceCode.toUpperCase();
+		var dwrReply = {
+			callback:function(data) {
+			if ( data != null && typeof data == 'object' ) {
+				setRecipientValue( fundSourceCodeDescFieldName, data.name );
+			} else {
+				setRecipientValue( fundSourceCodeDescFieldName, wrapError( "Fund source code not found" ), true );			
+			} },
+			errorHandler:function( errorMessage ) { 
+				setRecipientValue( fundSourceCodeDescFieldName, wrapError( "Fund source code not found" ), true );
+			}
+		};
+		FundSourceService.getByPrimaryKey(fundSourceCode, dwrReply );
+	}
+}

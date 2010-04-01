837b12243c1297366f80915149b56a9b2ede92a2
Weixia (Bonnie) Huang <huangb@indiana.edu>
2010-04-01 19:26:52 +0000

    Initial version

---
+++ work/src/org/kuali/kfs/module/endow/businessobject/EndowmentTransactionLineBase.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.businessobject;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerException;
+import org.kuali.kfs.sys.businessobject.AccountingLineOverride;
+import org.kuali.rice.kns.bo.Note;
+import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
+import org.kuali.rice.kns.bo.PersistableBusinessObjectExtension;
+import org.kuali.rice.kns.util.KualiDecimal;
+
+public abstract class EndowmentTransactionLineBase extends PersistableBusinessObjectBase implements EndowmentTransactionLine {
+//[Bonnie] not 100% if we should use protected or private  for the following fields. please check.
+    
+    protected String documentNumber;
+    protected Integer sequenceNumber; // relative to the grouping of acctng lines
+    protected String financialDocumentLineTypeCode;
+    
+    protected String kemid;
+    protected String etranCode;
+    protected String transactionLineDescription;
+    protected String ipIndicator;
+    protected KualiDecimal transactionAmount;
+    protected boolean corpusIndicator;
+    protected KualiDecimal transactionUnits;
+    protected boolean linePosted;
+    
+    protected KEMID kemidObj;
+    protected EndowmentTransactionCode etranCodeObj;
+    
+    
+    
+    
+    
+    
+
+}

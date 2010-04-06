cbe40db33b61f76ee4ea75d75e664b9624d0e20e
Jerry Neal <jkneal@iu.edu>
2010-04-06 16:06:18 +0000

    Endow sprint action class, form, add line event

---
+++ work/src/org/kuali/kfs/module/endow/document/validation/event/AddTransactionLineEvent.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.document.validation.event;
+
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
+import org.kuali.kfs.module.endow.document.EndowmentTransactionLinesDocument;
+import org.kuali.kfs.module.endow.document.validation.AddTransactionLineRule;
+import org.kuali.rice.kns.rule.BusinessRule;
+import org.kuali.rice.kns.rule.event.KualiDocumentEventBase;
+
+public class AddTransactionLineEvent extends KualiDocumentEventBase {
+    private EndowmentTransactionLine line;
+
+    /**
+     * @param description
+     * @param errorPathPrefix
+     * @param document
+     */
+    public AddTransactionLineEvent(String description, String errorPathPrefix, EndowmentTransactionLinesDocument document, EndowmentTransactionLine line) {
+        super(description, errorPathPrefix, document);
+        this.document = document;
+        this.line = line;        
+    }
+
+    /**
+     * @see org.kuali.rice.kns.rule.event.KualiDocumentEvent#getRuleInterfaceClass()
+     */
+    @SuppressWarnings("unchecked")
+    public Class getRuleInterfaceClass() {
+        return AddTransactionLineRule.class;
+    }
+
+    /**
+     * @see org.kuali.rice.kns.rule.event.KualiDocumentEvent#invokeRuleMethod(org.kuali.rice.kns.rule.BusinessRule)
+     */
+    public boolean invokeRuleMethod(BusinessRule rule) {
+        return ((AddTransactionLineRule<EndowmentTransactionLinesDocument, EndowmentTransactionLine>) rule).processAddTransactionLineRules((EndowmentTransactionLinesDocument) document, line);
+    }
+}
--- work/src/org/kuali/kfs/module/endow/document/web/struts/AssetIncreaseDocumentAction.java
+++ work/src/org/kuali/kfs/module/endow/document/web/struts/AssetIncreaseDocumentAction.java
@@ -15,8 +15,65 @@
  */
 package org.kuali.kfs.module.endow.document.web.struts;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kfs.module.endow.businessobject.EndowmentSourceTransactionLine;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
+import org.kuali.kfs.module.endow.document.EndowmentTransactionLinesDocument;
+import org.kuali.kfs.module.endow.document.validation.event.AddTransactionLineEvent;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.businessobject.AccountingLine;
+import org.kuali.kfs.sys.businessobject.FinancialSystemDocumentHeader;
+import org.kuali.kfs.sys.businessobject.SourceAccountingLine;
+import org.kuali.kfs.sys.businessobject.TargetAccountingLine;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.AccountingDocument;
+import org.kuali.kfs.sys.document.AmountTotaling;
+import org.kuali.kfs.sys.document.validation.event.AddAccountingLineEvent;
 import org.kuali.kfs.sys.document.web.struts.FinancialSystemTransactionalDocumentActionBase;
+import org.kuali.kfs.sys.web.struts.KualiAccountingDocumentFormBase;
+import org.kuali.rice.kns.service.KualiRuleService;
+import org.kuali.rice.kns.service.PersistenceService;
 
 public class AssetIncreaseDocumentAction extends FinancialSystemTransactionalDocumentActionBase {
+ 
 
+    /**
+     * This action executes an insert of a SourceAccountingLine into a document only after validating the accounting line and
+     * checking any appropriate business rules.
+     * 
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return ActionForward
+     * @throws Exception
+     */
+    public ActionForward insertSourceTransactionLine(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        AssetIncreaseDocumentForm documentForm = (AssetIncreaseDocumentForm) form;
+        EndowmentTransactionLinesDocument endowmentDocument =  (EndowmentTransactionLinesDocument) documentForm.getDocument();
+        
+        EndowmentTransactionLine transLine = documentForm.getNewSourceTransactionLine();
+
+        boolean rulePassed = true;
+
+        // check any business rules
+        rulePassed &= SpringContext.getBean(KualiRuleService.class).applyRules(new AddTransactionLineEvent("", KFSConstants.NEW_SOURCE_ACCT_LINE_PROPERTY_NAME, endowmentDocument ,transLine));
+
+        if (rulePassed) {
+            // add accountingLine
+            SpringContext.getBean(PersistenceService.class).refreshAllNonUpdatingReferences(transLine);
+         //   insertAccountingLine(true, financialDocumentForm, line);
+            endowmentDocument.getSourceTransactionLines().add(transLine);
+
+            // clear the used newTargetLine
+            documentForm.setNewSourceTransactionLine(new EndowmentSourceTransactionLine());
+        }
+
+        return mapping.findForward(KFSConstants.MAPPING_BASIC);
+    }
 }

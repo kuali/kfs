1c85e786057c4097f2be0dd60983ef9d3380ee6b
Tapan Mokha <tmokha@indiana.edu>
2010-04-08 19:51:37 +0000

    KULENDOW 296, Added Liability Increase file.

---
+++ work/src/org/kuali/kfs/module/endow/document/LiabilityIncreaseDocument.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kfs.module.endow.businessobject.EndowmentSourceTransactionLine;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTargetTransactionLine;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLineBase;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLineParser;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionTaxLotLine;
+import org.kuali.kfs.sys.document.AmountTotaling;
+import org.kuali.kfs.sys.document.Correctable;
+import org.kuali.kfs.sys.document.FinancialSystemTransactionalDocumentBase;
+import org.kuali.rice.kns.util.KualiDecimal;
+
+public class LiabilityIncreaseDocument extends EndowmentTaxLotLinesDocumentBase implements Correctable {
+
+
+    public LiabilityIncreaseDocument() {
+        super();
+    }
+
+}

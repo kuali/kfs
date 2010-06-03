460ce27f41e5cca6eccd5717b40bdb8a6ee45949
Tapan Mokha <tmokha@indiana.edu>
2010-06-03 20:06:46 +0000

    KULENDOW 325 Reverse a posted transaction (Error Correction) Asset Increase & Decrease implementation.

--- work/src/org/kuali/kfs/module/endow/document/AssetDecreaseDocument.java
+++ work/src/org/kuali/kfs/module/endow/document/AssetDecreaseDocument.java
@@ -20,8 +20,9 @@ import java.util.List;
 import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
 import org.kuali.kfs.sys.document.AmountTotaling;
 import org.kuali.kfs.sys.document.Correctable;
+import org.kuali.rice.kns.document.Copyable;
 
-public class AssetDecreaseDocument extends EndowmentTaxLotLinesDocumentBase implements Correctable, UnitsTotaling, AmountTotaling {
+public class AssetDecreaseDocument extends EndowmentTaxLotLinesDocumentBase implements Correctable, Copyable, UnitsTotaling, AmountTotaling {
 
     public AssetDecreaseDocument() {
         super();

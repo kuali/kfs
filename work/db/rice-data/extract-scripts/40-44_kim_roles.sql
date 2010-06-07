e5c33ef40e6fb2ec29ae7160df0f33072c5294ba
Jonathan Keller <keller.jonathan@gmail.com>
2010-06-07 17:43:57 +0000

    Fixed up KIM exports to avoid key collisions with Rice base dataset.

--- work/db/rice-data/extract-scripts/40-44_kim_roles.sql
+++ work/db/rice-data/extract-scripts/40-44_kim_roles.sql
@@ -1,22 +1,22 @@
 -- KFS Roles
-SELECT ROLE_ID, 'KFS'||ROLE_ID AS OBJ_ID, 1 AS VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND 
+SELECT ROLE_ID, 'KFS'||ROLE_ID AS OBJ_ID, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND 
     FROM KRIM_ROLE_T
     WHERE nmspc_cd LIKE 'KFS%'
     ORDER BY NMSPC_CD, ROLE_NM
 /
 
 -- KFS Role Members and KFS Members of Rice Roles
-SELECT ROLE_MBR_ID, 1 AS VER_NBR, 'KFS'||ROLE_MBR_ID AS OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD
+SELECT 'KFS'||ROLE_MBR_ID AS ROLE_MBR_ID, 'KFS'||ROLE_MBR_ID AS OBJ_ID, ROLE_ID, CASE WHEN mbr_typ_cd = 'P' THEN 'KFS'||MBR_ID ELSE mbr_id END AS MBR_ID, MBR_TYP_CD
     FROM KRIM_ROLE_MBR_T
     WHERE role_id IN (
         SELECT ROLE_ID
             FROM KRIM_ROLE_T
             WHERE nmspc_cd LIKE 'KFS%'
     )
-    AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' ) -- exclude the "admin" user
+    --AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' ) -- exclude the "admin" user
     --AND role_id IN ( '81', '28', '18' ) -- bootstrap data that should not be there
 UNION ALL -- KFS roles (as members) assigned to rice roles
-    SELECT ROLE_MBR_ID, 1 AS VER_NBR, 'KFS'||ROLE_MBR_ID AS OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD
+    SELECT 'KFS'||ROLE_MBR_ID AS ROLE_MBR_ID, 'KFS'||ROLE_MBR_ID AS OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD
         FROM KRIM_ROLE_MBR_T m
         WHERE role_id NOT IN (
             SELECT ROLE_ID
@@ -27,7 +27,7 @@ UNION ALL -- KFS roles (as members) assigned to rice roles
  --       AND role_id NOT IN ( '81', '28', '18' ) -- bootstrap data that should not be there
         AND EXISTS ( SELECT 'x' FROM krim_role_t r WHERE r.ROLE_ID = m.mbr_id AND r.NMSPC_CD LIKE 'KFS%' )
 UNION ALL -- KFS groups assigned to rice roles (none at present)
-    SELECT ROLE_MBR_ID, 1 AS VER_NBR, 'KFS'||ROLE_MBR_ID AS OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD
+    SELECT 'KFS'||ROLE_MBR_ID AS ROLE_MBR_ID, 'KFS'||ROLE_MBR_ID AS OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD
         FROM KRIM_ROLE_MBR_T m
         WHERE role_id NOT IN (
             SELECT ROLE_ID
@@ -41,7 +41,7 @@ ORDER BY ROLE_ID, ROLE_MBR_ID
 /
 
 -- Delegations
-SELECT DLGN_ID, 1 AS VER_NBR, 'KFS'||DLGN_ID AS OBJ_ID, ROLE_ID, ACTV_IND, KIM_TYP_ID, DLGN_TYP_CD 
+SELECT 'KFS'||DLGN_ID AS DLGN_ID, 1 AS VER_NBR, 'KFS'||DLGN_ID AS OBJ_ID, ROLE_ID, ACTV_IND, KIM_TYP_ID, DLGN_TYP_CD 
     FROM KRIM_DLGN_T
     WHERE role_id IN (
         SELECT ROLE_ID
@@ -52,7 +52,7 @@ SELECT DLGN_ID, 1 AS VER_NBR, 'KFS'||DLGN_ID AS OBJ_ID, ROLE_ID, ACTV_IND, KIM_T
 /
 
 -- Role member attribute data
-SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 1 AS VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL 
+SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 1 AS VER_NBR, 'KFS'||ROLE_MBR_ID AS ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL 
     FROM KRIM_ROLE_MBR_ATTR_DATA_T
     WHERE role_mbr_id IN (
         SELECT ROLE_MBR_ID
@@ -63,7 +63,7 @@ SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 1 AS 
                     WHERE nmspc_cd LIKE 'KFS%'
             )
 --            AND role_id NOT IN ( '81', '28', '18' ) -- bootstrap data that should not be there
-		    AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' )
+		    --AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' )
         UNION ALL
             SELECT ROLE_MBR_ID
                 FROM KRIM_ROLE_MBR_T m
@@ -91,7 +91,7 @@ SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 1 AS 
 /
 
 -- Delegation Members
-SELECT DLGN_MBR_ID, 1 AS VER_NBR, 'KFS'||DLGN_MBR_ID AS OBJ_ID, DLGN_ID, MBR_ID, MBR_TYP_CD, ROLE_MBR_ID 
+SELECT 'KFS'||DLGN_MBR_ID AS DLGN_MBR_ID, 'KFS'||DLGN_MBR_ID AS OBJ_ID, 'KFS'||DLGN_ID AS DLGN_ID, CASE WHEN mbr_typ_cd = 'P' THEN 'KFS'||MBR_ID ELSE mbr_id END AS MBR_ID, MBR_TYP_CD, 'KFS'||ROLE_MBR_ID AS ROLE_MBR_ID 
     FROM KRIM_DLGN_MBR_T
     WHERE dlgn_id IN (
         SELECT DLGN_ID
@@ -103,12 +103,12 @@ SELECT DLGN_MBR_ID, 1 AS VER_NBR, 'KFS'||DLGN_MBR_ID AS OBJ_ID, DLGN_ID, MBR_ID,
             )
 --            AND role_id NOT IN ( '7', '28', '37', '45', '54', '48', 'KFS5014' ) -- dynamically loaded roles, only at migration-time
     )
-    AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' )
+    --AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' )
     ORDER BY DLGN_ID, DLGN_MBR_ID
 /
 
 
-SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 1 AS VER_NBR, DLGN_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL 
+SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 'KFS'||DLGN_MBR_ID AS DLGN_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL 
     FROM KRIM_DLGN_MBR_ATTR_DATA_T
     WHERE DLGN_MBR_ID IN (
         SELECT DLGN_MBR_ID
@@ -123,7 +123,7 @@ SELECT 'KFS'||ATTR_DATA_ID AS ATTR_DATA_ID, 'KFS'||ATTR_DATA_ID AS OBJ_ID, 1 AS 
                     )
                     --AND role_id NOT IN ( '7', '28', '37', '45', '54', '48', 'KFS5014' ) -- dynamically loaded roles, only at migration-time
             )
-            AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' )
+            --AND NOT ( mbr_typ_cd = 'P' AND MBR_ID = '3' )
         )
     ORDER BY DLGN_MBR_ID, ATTR_DATA_ID
 /

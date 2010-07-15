45f3c4ccd3852dcb28410f7e5e645483cadaed70
Daniela Balmus <db483@cornell.edu>
2010-07-15 20:30:58 +0000

    KULENDOW-373 Add transaction line specific BRs

--- work/src/org/kuali/kfs/module/endow/document/validation/impl/EndowmentToGLTransferOfFundsDocumentRules.java
+++ work/src/org/kuali/kfs/module/endow/document/validation/impl/EndowmentToGLTransferOfFundsDocumentRules.java
@@ -15,6 +15,59 @@
  */
 package org.kuali.kfs.module.endow.document.validation.impl;
 
+import org.kuali.kfs.module.endow.EndowConstants;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
+import org.kuali.kfs.module.endow.document.EndowmentTransactionLinesDocument;
+import org.kuali.kfs.module.endow.document.service.EndowmentTransactionLinesDocumentService;
+import org.kuali.kfs.sys.context.SpringContext;
+
 public class EndowmentToGLTransferOfFundsDocumentRules extends EndowmentTransactionLinesDocumentBaseRules {
+
+    /**
+     * @see org.kuali.kfs.module.endow.document.validation.impl.EndowmentTransactionLinesDocumentBaseRules#validateTransactionLine(org.kuali.kfs.module.endow.document.EndowmentTransactionLinesDocument,
+     *      org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine, int)
+     */
+    @Override
+    protected boolean validateTransactionLine(EndowmentTransactionLinesDocument endowmentTransactionLinesDocument, EndowmentTransactionLine line, int index) {
+        boolean isValid = super.validateTransactionLine(endowmentTransactionLinesDocument, line, index);
+        // Obtain Prefix for Error fields in UI.
+        String ERROR_PREFIX = getErrorPrefix(line, index);
+
+        if (isValid) {
+
+            // Is Etran code empty
+            if (isEndowmentTransactionCodeEmpty(line, ERROR_PREFIX))
+                return false;
+
+            // Validate ETran code
+            if (!validateEndowmentTransactionCode(line, ERROR_PREFIX))
+                return false;
+
+            // Validate ETran code as E or I
+            isValid &= validateEndowmentTransactionTypeCode(line, ERROR_PREFIX);
+
+            // Validate if a KEMID can have a principal transaction when IP indicator is P
+            if (!canKEMIDHaveAPrincipalTransaction(line, ERROR_PREFIX))
+                return false;
+
+            // Validate if the chart is matched between the KEMID and EtranCode
+            isValid &= validateChartMatch(line, ERROR_PREFIX);
+
+            // Validate Amount is Greater than Zero.
+            isValid &= validateTransactionAmountGreaterThanZero(line, ERROR_PREFIX);
+
+            // Set Corpus Indicator
+            line.setCorpusIndicator(SpringContext.getBean(EndowmentTransactionLinesDocumentService.class).getCorpusIndicatorValueforAnEndowmentTransactionLine(line.getKemid(), line.getEtranCode(), line.getTransactionIPIndicatorCode()));
+
+            if (isValid) {
+                if (EndowConstants.TRANSACTION_LINE_TYPE_SOURCE.equalsIgnoreCase(line.getTransactionLineTypeCode())) {
+                    checkWhetherReducePermanentlyRestrictedFund(line, ERROR_PREFIX);
+                    checkWhetherHaveSufficientFundsForCashBasedTransaction(line, ERROR_PREFIX);
+                }
+            }
+        }
+
+        return isValid;
+    }
 
 }

e546006386266490da17bf264fb77f2047b4eaa3
Tariq El-Atrache <tatrache@usc.edu>
2010-07-22 19:38:37 +0000

    KFSMI-5354: Allowed for sales tax in convertMoneyToPercent() for trade in line

--- work/src/org/kuali/kfs/module/purap/service/impl/PurapAccountingServiceImpl.java
+++ work/src/org/kuali/kfs/module/purap/service/impl/PurapAccountingServiceImpl.java
@@ -801,6 +801,9 @@ public class PurapAccountingServiceImpl implements PurapAccountingService {
                     KualiDecimal accountAmount = KualiDecimal.ZERO;
                     if(item.getItemTypeCode().equals(PurapConstants.ItemTypeCodes.ITEM_TYPE_TRADE_IN_CODE)){
                         accountAmount = item.getExtendedPrice();
+                        if(!item.getItemSalesTaxAmount().isZero()){
+                            accountAmount = accountAmount.add(item.getItemSalesTaxAmount());
+                        }
                     }else{
                         accountAmount = account.getAmount();
                     }

022a593c67ca24d43d46dc4a125e4837b7924dd3
Weixia (Bonnie) Huang <huangb@indiana.edu>
2010-08-12 18:56:15 +0000

    KULENDOW-376: initial update.

---
+++ work/src/org/kuali/kfs/module/endow/batch/RollProcessDateStep.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.batch;
+
+import java.util.Date;
+
+import org.kuali.kfs.fp.batch.service.DisbursementVoucherExtractService;
+import org.kuali.kfs.module.ec.batch.EffortCertificationCreateStep;
+import org.kuali.kfs.module.ec.batch.service.EffortCertificationCreateService;
+import org.kuali.kfs.module.endow.batch.service.RollProcessDateService;
+import org.kuali.kfs.sys.batch.AbstractStep;
+
+public class RollProcessDateStep extends AbstractStep {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(RollProcessDateStep.class);
+
+    private RollProcessDateService rollProcessDateService;
+    /**
+     * @see org.kuali.kfs.sys.batch.Step#execute(java.lang.String, java.util.Date)
+     */
+    public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
+        // TODO Auto-generated method stub
+        return rollProcessDateService.rollDate();
+    }
+
+    public void setRollProcessDateService(RollProcessDateService rollProcessDateService) {
+        this.rollProcessDateService = rollProcessDateService;
+    }
+}
---
+++ work/src/org/kuali/kfs/module/endow/batch/service/impl/RollProcessDateServiceImpl.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.batch.service.impl;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.service.impl.KfsParameterConstants;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.service.ParameterService;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.bo.Parameter;
+
+import org.kuali.kfs.module.endow.EndowConstants;
+import org.kuali.kfs.module.endow.batch.service.RollProcessDateService;
+import org.kuali.kfs.module.endow.document.service.KEMService;
+
+public class RollProcessDateServiceImpl implements RollProcessDateService {
+
+    private ParameterService parameterService;
+    private KEMService kemService;
+    
+    /**
+     * @see org.kuali.kfs.module.endow.batch.service.RollProcessDateService#rollDate()
+     */
+    public boolean rollDate() {
+        int MILLIS_IN_DAY = 1000 * 60 * 60 * 24;
+        
+        boolean success = true;
+        kemService = SpringContext.getBean(KEMService.class);
+        parameterService = SpringContext.getBean(ParameterService.class);
+           
+        Date currentProcessDate = kemService.getCurrentSystemProcessDateObject();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        String nextDate = dateFormat.format(currentProcessDate.getTime() + MILLIS_IN_DAY);
+        System.out.println(">>>next date is "+nextDate);
+        Parameter theCurrentProcessDate = parameterService.retrieveParameter("KFS-ENDOW", "All", EndowConstants.EndowmentSystemParameter.CURRENT_PROCESS_DATE);
+        theCurrentProcessDate.setParameterValue(nextDate);        
+       
+        return success;
+    }
+    
+    /**
+     * Gets the kemService.
+     * 
+     * @return kemService
+     */
+    public KEMService getKemService() {
+        return kemService;
+    }
+
+    /**
+     * Sets the kemService.
+     * 
+     * @param kemService
+     */
+    public void setKemService(KEMService kemService) {
+        this.kemService = kemService;
+    }
+    /**
+     * Gets the parameterService.
+     * 
+     * @return parameterService
+     */
+    public ParameterService getParameterService() {
+        return parameterService;
+    }
+
+    /**
+     * Sets the parameterService.
+     * 
+     * @param parameterService
+     */
+    public void setParameterService(ParameterService parameterService) {
+        this.parameterService = parameterService;
+    }
+
+}

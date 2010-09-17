05b8a97e970f935e6fb7b14de092b0894dc6d6b7
Bob Lemm <rlemm@rsmart.com>
2010-09-17 23:39:38 +0000

    KFSMI-5979 Remove module dependencies from the core classes - about half of the changes are as the result of merging in a UCD split of GL and Labor. The remainder are due to the separation of community module code from the main core code.

---
+++ work/src/org/kuali/kfs/module/ld/service/impl/LaborOriginEntryGroupServiceImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2005 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.ld.service.impl;
+
+import org.kuali.kfs.gl.service.impl.OriginEntryGroupServiceImpl;
+import org.kuali.kfs.module.ld.service.LaborOriginEntryGroupService;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @see org.kuali.kfs.gl.service.OriginEntryGroupService
+ */
+@Transactional
+public class LaborOriginEntryGroupServiceImpl extends OriginEntryGroupServiceImpl implements LaborOriginEntryGroupService {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(LaborOriginEntryGroupServiceImpl.class);
+
+   
+    
+}

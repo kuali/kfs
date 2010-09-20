675dfac9cfbb21383da5baee8aef2062e2653ff8
David Yun <davidyun@umd.edu>
2010-09-20 15:15:20 +0000

    KULENDOW-444

--- work/src/org/kuali/kfs/module/endow/EndowConstants.java
+++ work/src/org/kuali/kfs/module/endow/EndowConstants.java
@@ -156,13 +156,13 @@ public class EndowConstants extends JSTLConstants {
         public static final String TAX_LOTS_ACCOUNTING_METHOD = "TAX_LOTS_ACCOUNTING_METHOD";
         public static final String FISCAL_YEAR_END_DAY_AND_MONTH = "FISCAL_YEAR_END_MONTH_AND_DAY";
         public static final String TRANSACTION_ARCHIVE_DOCUMENT_TYPE_NAMES = "DOCUMENT_TYPES";
-        public static final String GAIN_LOSS_BLANKET_APPROVAL = "GAIN_LOSS_NO_ROUTE_IND";
+        public static final String GAIN_LOSS_NO_ROUTE_IND = "GAIN_LOSS_NO_ROUTE_IND";
         public static final String GAIN_LOSS_DESCRIPTION = "GAIN_LOSS_DESCRIPTION";
-        public static final String INCOME_BLANKET_APPROVAL = "INCOME_NO_ROUTE_IND";
+        public static final String INCOME_NO_ROUTE_IND = "INCOME_NO_ROUTE_IND";
         public static final String INCOME_DESCRIPTION = "INCOME_DESCRIPTION";
-        public static final String PURCHASE_BLANKET_APPROVAL = "PURCHASE_NO_ROUTE_IND";
+        public static final String PURCHASE_NO_ROUTE_IND = "PURCHASE_NO_ROUTE_IND";
         public static final String PURCHASE_DESCRIPTION = "PURCHASE_DESCRIPTION";
-        public static final String SALE_BLANKET_APPROVAL = "SALE_NO_ROUTE_IND";
+        public static final String SALE_NO_ROUTE_IND = "SALE_NO_ROUTE_IND";
         public static final String SALE_DESCRIPTION = "SALE_DESCRIPTION";
         public static final String DESCRIPTION = "DESCRIPTION";
         public static final String BLANKET_APPROVAL_IND = "NO_ROUTE_IND";
@@ -170,8 +170,8 @@ public class EndowConstants extends JSTLConstants {
         public static final String MAXIMUM_TRANSACTION_LINES = "MAXIMUM_TRANSACTION_LINES";
         public static final String SHORT_TERM_GAIN_LOSS_DESCRIPTION = "SHORT_TERM_GAIN_LOSS_DESCRIPTION";
         public static final String LONG_TERM_GAIN_LOSS_DESCRIPTION = "LONG_TERM_GAIN_LOSS_DESCRIPTION";
-        public static final String GAIN_LOSS_BLANKET_APPROVAL_IND = "GAIN_LOSS_NO_ROUTE_IND";
         public static final String ALLOW_NEGATIVE_BALANCE = "ALLOW_NEGATIVE_BALANCE";
+        public static final String INCOME_TRANSFER_DESCRIPTION = "INCOME_TRANSFER_DESCRIPTION";
         
     }
 

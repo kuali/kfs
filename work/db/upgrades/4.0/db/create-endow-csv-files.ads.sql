2c113e3aaba273fc7b190a467e28108316caaf2f
Jonathan Keller <keller.jonathan@gmail.com>
2010-10-19 17:54:44 +0000

    Added endowment bootstrap data to KFS Upgrade scripts.

---
+++ work/db/upgrades/4.0/db/create-endow-csv-files.ads.sql
@@ -0,0 +1,274 @@
+SELECT * FROM END_ACRL_MTHD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_ACRL_MTHD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_CAE_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_CAE_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_CLS_CD_TYP_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_CLS_CD_TYP_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_DONR_LBL_SEL_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_DONR_LBL_SEL_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_ETRAN_TYP_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_ETRAN_TYP_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_FEE_BAL_TYP_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_FEE_BAL_TYP_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_FEE_BASE_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_FEE_BASE_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_FEE_RT_DEF_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_FEE_RT_DEF_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_FEE_TYP_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_FEE_TYP_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_FREQ_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_FREQ_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+
+SELECT * FROM END_IP_IND_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_IP_IND_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_PMT_TYP_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_PMT_TYP_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_SEC_VLTN_MTHD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_SEC_VLTN_MTHD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_TRAN_TYP_CD_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_TRAN_TYP_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_TRAN_SUB_TYP_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_TRAN_SUB_TYP_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_TRAN_SRC_TYP_T
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_TRAN_SRC_TYP_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_SEC_RPT_GRP_T WHERE SEC_RPT_GRP = 'CSHEQ' 
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_SEC_RPT_GRP_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_AGRMNT_SPCL_INSTRC_CD_T WHERE AGRMNT_SPCL_INSTRC_CD = '0'
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_AGRMNT_SPCL_INSTRC_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_AGRMNT_STAT_CD_T WHERE AGRMNT_STAT_CD IN ( 'COMP', 'NONE', 'PEND' )
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_AGRMNT_STAT_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_TRAN_RESTR_CD_T WHERE TRAN_RESTR_CD IN ( 'NDISB', 'NONE', 'NTRAN' )
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_TRAN_RESTR_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/
+SELECT * FROM END_TYP_RESTR_CD_T WHERE TYP_RESTR_CD IN ( 'U', 'NA', 'P', 'T' )
+/
+.saveLastResult 'Format=CSV,
+	File="/Users/jonathan/dev/projects/kfs/work/db/upgrades/4.0/db/02_data/END_TYP_RESTR_CD_T.csv",
+	Overwrite=true,
+	Platform=Unix (LF) - EOL,
+	QuoteIdentifier="\"",
+	Delimiter=",",
+	Encoding=UTF-8,
+	IncludeHeader=true,
+	IncludeRowCount=false,
+	IncludeNullText=false' 
+/

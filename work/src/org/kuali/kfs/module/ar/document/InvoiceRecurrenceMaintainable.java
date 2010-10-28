6709b12098f783713c540b6f33a2ecc48bdfbc09
Jonathan Keller <keller.jonathan@gmail.com>
2010-10-28 19:58:10 +0000

    KFSMI-6077 : cleaned up split node code to not be sensitive to nulls in various places

--- work/src/org/kuali/kfs/module/ar/document/InvoiceRecurrenceMaintainable.java
+++ work/src/org/kuali/kfs/module/ar/document/InvoiceRecurrenceMaintainable.java
@@ -15,6 +15,7 @@
  */
 package org.kuali.kfs.module.ar.document;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.module.ar.businessobject.InvoiceRecurrence;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.context.SpringContext;
@@ -22,53 +23,65 @@ import org.kuali.kfs.sys.document.FinancialSystemMaintainable;
 import org.kuali.kfs.sys.document.FinancialSystemMaintenanceDocument;
 import org.kuali.rice.kew.exception.WorkflowException;
 import org.kuali.rice.kim.bo.Person;
-import org.kuali.rice.kim.service.PersonService;
+import org.kuali.rice.kim.bo.entity.KimPrincipal;
+import org.kuali.rice.kim.service.IdentityManagementService;
 import org.kuali.rice.kns.service.DocumentService;
 import org.kuali.rice.kns.workflow.service.KualiWorkflowDocument;
 
 public class InvoiceRecurrenceMaintainable extends FinancialSystemMaintainable {
-
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(InvoiceRecurrenceMaintainable.class);
+    
     private static final String INACTIVATING_NODE_NAME = "InvoiceRecurrenceIsInactivating";
     private static final String INITIATED_BY_SYSTEM_USER = "InitiatedBySystemUser";
     
     @Override
     protected boolean answerSplitNodeQuestion(String nodeName) throws UnsupportedOperationException {
-        FinancialSystemMaintenanceDocument maintDoc = getParentMaintDoc();
-        
         //  return true if the doc is flipping form Active to Inactive, false otherwise
-        if (INACTIVATING_NODE_NAME.equalsIgnoreCase(nodeName)) { 
+        if ( StringUtils.equalsIgnoreCase( INACTIVATING_NODE_NAME, nodeName) ) { 
             //  go through some contortions to get the oldMaintainable to compare against
+            FinancialSystemMaintenanceDocument maintDoc = getParentMaintDoc();
+            // make sure all the needed objects are there
+            if ( maintDoc == null
+                    || maintDoc.getOldMaintainableObject() == null
+                    || maintDoc.getOldMaintainableObject().getBusinessObject() == null ) {
+                return false;
+            }
             boolean oldIsActive = ((InvoiceRecurrence)maintDoc.getOldMaintainableObject().getBusinessObject()).isActive();
-            boolean newIsActive = ((InvoiceRecurrence)this.getBusinessObject()).isActive();
+            boolean newIsActive = ((InvoiceRecurrence)getBusinessObject()).isActive();
             
             //  return true if the invoicerecurrence is being deactivated, otherwise return false
             return oldIsActive && !newIsActive;
         }
 
         //  return true if the document was initiated by the SYSTEM_USER, false otherwise
-        if (INITIATED_BY_SYSTEM_USER.equalsIgnoreCase(nodeName)) {
-            KualiWorkflowDocument workflowDoc = maintDoc.getDocumentHeader().getWorkflowDocument();
-            String initiatorPrincipalId = workflowDoc.getInitiatorPrincipalId();
-            PersonService<Person> personService = SpringContext.getBean(PersonService.class);
-            Person initiatorPerson = personService.getPerson(initiatorPrincipalId);
-            return (KFSConstants.SYSTEM_USER.equalsIgnoreCase(initiatorPerson.getPrincipalName()));
+        if ( StringUtils.equalsIgnoreCase( INITIATED_BY_SYSTEM_USER, nodeName) ) {
+            FinancialSystemMaintenanceDocument maintDoc = getParentMaintDoc();
+            if ( maintDoc == null
+                    || maintDoc.getDocumentHeader() == null 
+                    || maintDoc.getDocumentHeader().getWorkflowDocument() == null
+                    || StringUtils.isBlank( maintDoc.getDocumentHeader().getWorkflowDocument().getInitiatorPrincipalId() ) ) {
+                return false;
+            }
+            
+            String initiatorPrincipalId = maintDoc.getDocumentHeader().getWorkflowDocument().getInitiatorPrincipalId();
+            KimPrincipal principal = SpringContext.getBean(IdentityManagementService.class).getPrincipal(initiatorPrincipalId);
+            
+            return principal != null && StringUtils.equalsIgnoreCase(principal.getPrincipalName(), KFSConstants.SYSTEM_USER);
         }
 
         throw new UnsupportedOperationException("InvoiceRecurrenceMaintainable does not implement the answerSplitNodeQuestion method. Node name specified was: " + nodeName);
         
     }
 
-    private FinancialSystemMaintenanceDocument getParentMaintDoc() {
+    protected FinancialSystemMaintenanceDocument getParentMaintDoc() {
         //  how I wish for the ability to directly access the parent object
         DocumentService documentService = SpringContext.getBean(DocumentService.class);
-        FinancialSystemMaintenanceDocument maintDoc = null;
         try {
-            maintDoc =(FinancialSystemMaintenanceDocument) documentService.getByDocumentHeaderId(this.documentNumber);
+            return (FinancialSystemMaintenanceDocument) documentService.getByDocumentHeaderId(this.documentNumber);
+        } catch (WorkflowException e) {
+            LOG.error( "Unable to retrieve maintenance document for use in split node routing - returning null",e);
         }
-        catch (WorkflowException e) {
-            throw new RuntimeException(e);
-        }
-        return maintDoc;
+        return null;
     }
     
 }

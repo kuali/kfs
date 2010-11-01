219862f725d92b28b8b5cb7ab4ec4baa574ebd67
Winston Dang <wkd@hawaii.edu>
2010-11-01 23:23:11 +0000

    KFSMI-6147 Inserted ObjectCodeInquirable to provide three Research Attributes.  Their display is controlled byif they appear on the doc by parameter ENABLE_RESEARCH_ADMIN_OBJECT_CODE_ATTRIBUTE_IND .

---
+++ work/src/org/kuali/kfs/coa/businessobject/inquiry/ObjectCodeInquirable.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2006 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.coa.businessobject.inquiry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.kuali.kfs.coa.businessobject.Account;
+import org.kuali.kfs.coa.businessobject.ObjectCode;
+import org.kuali.kfs.coa.businessobject.Organization;
+import org.kuali.kfs.coa.identity.OrgReviewRole;
+import org.kuali.kfs.coa.identity.OrgReviewRoleLookupableHelperServiceImpl;
+import org.kuali.kfs.integration.kc.businessobject.BudgetCategoryDTO;
+import org.kuali.kfs.module.external.kc.KcConstants;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.businessobject.inquiry.KfsInquirableImpl;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kim.util.KimConstants;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.lookup.HtmlData;
+import org.kuali.rice.kns.lookup.HtmlData.AnchorHtmlData;
+import org.kuali.rice.kns.service.ParameterService;
+import org.kuali.rice.kns.util.KNSConstants;
+import org.kuali.rice.kns.util.UrlFactory;
+import org.kuali.rice.kns.web.ui.Field;
+import org.kuali.rice.kns.web.ui.Row;
+import org.kuali.rice.kns.web.ui.Section;
+
+/**
+ * This class adds in some new sections for {@link ObjectCode} inquiries, specifically Research object code fields
+ */
+public class ObjectCodeInquirable extends KfsInquirableImpl {
+
+    public void addAdditionalSections(List sections, BusinessObject bo) {
+        ParameterService parameterService = SpringContext.getBean(ParameterService.class);
+        String enableResearchAdminObjectCodeAttributeInd = parameterService.getParameterValue(ObjectCode.class, KFSConstants.ObjectCodeConstants.PARAMETER_KC_ENABLE_RESEARCH_ADMIN_OBJECT_CODE_ATTRIBUTE_IND);
+
+        if ((bo instanceof ObjectCode) && (enableResearchAdminObjectCodeAttributeInd.equals("Y")) ) {
+            ObjectCode objectCode = (ObjectCode) bo;
+            
+            List rows = new ArrayList();
+
+            Field f = new Field();
+            f.setPropertyName("Budget Category Code");
+            f.setFieldLabel("Budget Category Code");
+            f.setPropertyValue(objectCode.getRschBudgetCategoryCode());
+            f.setFieldType(Field.TEXT);
+            rows.add(new Row(f));
+            
+            f = new Field();
+            f.setPropertyName("On Campus?");
+            f.setFieldLabel("On Campus?");
+            f.setPropertyValue(objectCode.isRschOnCampusIndicator());
+            f.setFieldType(Field.CHECKBOX);
+            rows.add(new Row(f));
+            
+            f = new Field();
+            f.setPropertyName("Object Code Description");
+            f.setFieldLabel("Object Code Description");
+            f.setPropertyValue(objectCode.getRschObjectCodeDescription());
+            f.setFieldType(Field.TEXT);
+            rows.add(new Row(f));
+           
+            Section section = new Section();
+            section.setRows(rows);
+            section.setSectionTitle("Research Admin Attributes");
+            sections.add(section);
+        }
+    }
+
+
+}

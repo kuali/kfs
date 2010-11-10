7b513061e7783d3266538e5fb4c362d5e621eb8b
Daniela Balmus <db483@cornell.edu>
2010-11-10 18:17:27 +0000

    KULENDOW-548 Create new ValuesFinder for currentProcessDate

---
+++ work/src/org/kuali/kfs/module/endow/businessobject/options/CurrentProcessDateValuesFinder.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.businessobject.options;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang.time.DateUtils;
+import org.kuali.kfs.module.endow.document.service.KEMService;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.lookup.valueFinder.ValueFinder;
+
+public class CurrentProcessDateValuesFinder implements ValueFinder {
+
+    public String getValue() {
+        // get the current date from the service
+        Date date = SpringContext.getBean(KEMService.class).getCurrentDate();
+
+        // remove the time component
+        date = DateUtils.truncate(date, Calendar.DAY_OF_MONTH);
+
+        // format it as expected
+        return DateFormatUtils.format(date, "MM/dd/yyyy");
+    }
+
+}

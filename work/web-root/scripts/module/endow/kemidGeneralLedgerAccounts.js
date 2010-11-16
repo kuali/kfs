b9e007d4a8e2d09f02c949a41d3b692ff79d3792
Daniela Balmus <db483@cornell.edu>
2010-11-16 16:47:52 +0000

    KULENDOW-276 Fix chart code description display in General Ledger Accounts tab on the KEMID

---
+++ work/web-root/scripts/module/endow/kemidGeneralLedgerAccounts.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
+
+function loadChartInfo(codeFieldName ) {
+	var elPrefix = findElPrefix(codeFieldName.name);
+	var codeDescriptionFieldName = elPrefix + ".chart.finChartOfAccountDescription";
+	var code = DWRUtil.getValue(codeFieldName);
+	
+	setChartDescription(code, codeDescriptionFieldName);
+}
+
+function setChartDescription(code, codeDescriptionFieldName){	 
+
+ 	if (code == '') {
+ 		clearRecipients(codeDescriptionFieldName);
+ 	} else {
+ 		var dwrReply = {
+ 			callback:function(data) {
+ 				if ( data != null && typeof data == 'object') {
+ 					setRecipientValue(codeDescriptionFieldName, data.finChartOfAccountDescription);
+ 				} else {
+ 					setRecipientValue(codeDescriptionFieldName, wrapError("Chart not found"), true);			
+ 				} },
+ 			errorHandler:function(errorMessage) { 
+ 				setRecipientValue(codeDescriptionFieldName, wrapError("Chart not found"), true);
+ 			}
+ 		};
+ 		ChartService.getByPrimaryId(code.toUpperCase(), dwrReply);
+ 	}
+}
+ 

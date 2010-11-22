be1656a173639e802f1a6349f6d755a5749d54bc
David Yun <davidyun@umd.edu>
2010-11-22 16:26:24 +0000

    Removed kemService

--- work/src/org/kuali/kfs/module/endow/dataaccess/impl/CashSweepModelDaoOjb.java
+++ work/src/org/kuali/kfs/module/endow/dataaccess/impl/CashSweepModelDaoOjb.java
@@ -15,6 +15,7 @@
  */
 package org.kuali.kfs.module.endow.dataaccess.impl;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.apache.ojb.broker.query.Criteria;
@@ -22,29 +23,19 @@ import org.apache.ojb.broker.query.QueryFactory;
 import org.kuali.kfs.module.endow.EndowPropertyConstants;
 import org.kuali.kfs.module.endow.businessobject.CashSweepModel;
 import org.kuali.kfs.module.endow.dataaccess.CashSweepModelDao;
-import org.kuali.kfs.module.endow.document.service.KEMService;
 import org.kuali.rice.kns.dao.impl.PlatformAwareDaoBaseOjb;
 
 public class CashSweepModelDaoOjb extends PlatformAwareDaoBaseOjb implements CashSweepModelDao {
 
-    protected KEMService kemService;
-    
     /**
      * @see org.kuali.kfs.module.endow.dataaccess.CashSweepModelDao#getCashSweepModelWithNextPayDateEqualToCurrentDate()
      */
-    public List<CashSweepModel> getCashSweepModelWithNextPayDateEqualToCurrentDate() {
+    public List<CashSweepModel> getCashSweepModelWithNextPayDateEqualToCurrentDate(Date currentDate) {
         Criteria criteria = new Criteria();
-        criteria.addEqualTo(EndowPropertyConstants.CASH_SWEEP_MODEL_NEXT_DUE_DATE, kemService.getCurrentDate());
+        criteria.addEqualTo(EndowPropertyConstants.CASH_SWEEP_MODEL_NEXT_DUE_DATE, currentDate);
         criteria.addNotNull(EndowPropertyConstants.CASH_SWEEP_MODEL_FREQUENCY_CDOE);
         criteria.addEqualTo(EndowPropertyConstants.CASH_SWEEP_MODEL_ACTIVE_INDICATOR, "Y");
         return (List<CashSweepModel>) getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(CashSweepModel.class, criteria));
     }
 
-    /**
-     * Sets the kemService attribute value.
-     * @param kemService The kemService to set.
-     */
-    public void setKemService(KEMService kemService) {
-        this.kemService = kemService;
-    }
 }

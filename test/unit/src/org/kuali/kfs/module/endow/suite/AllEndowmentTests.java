57fb3807a7e0b3a7b85a2da1a12ef97cfa896987
Muddu	Salem <mksalem@umd.edu>
2010-11-24 19:09:16 +0000

    Added my unit tests to the suite that was created by Daniela.

--- test/unit/src/org/kuali/kfs/module/endow/suite/AllEndowmentTests.java
+++ test/unit/src/org/kuali/kfs/module/endow/suite/AllEndowmentTests.java
@@ -18,7 +18,14 @@ package org.kuali.kfs.module.endow.suite;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.kuali.kfs.module.endow.batch.service.impl.AvailableCashUpdateServiceImplTest;
+import org.kuali.kfs.module.endow.batch.service.impl.CurrentTaxLotBalanceUpdateServiceImplTest;
+import org.kuali.kfs.module.endow.batch.service.impl.HoldingHistoryMarketValuesUpdateServiceImplTest;
+import org.kuali.kfs.module.endow.document.service.HoldingHistoryServiceTest;
 import org.kuali.kfs.module.endow.document.validation.impl.AssetIncreaseDocumentRulesTest;
+import org.kuali.kfs.module.endow.document.validation.impl.HoldingAdjustmentDocumentRulesTest;
+import org.kuali.kfs.module.endow.document.validation.impl.HoldingHistoryValueAdjustmentDocumentRuleTest;
+import org.kuali.kfs.module.endow.document.validation.impl.LiabilityDecreaseDocumentRulesTest;
 
 /**
  * Runs all endowment tests.
@@ -33,8 +40,18 @@ public class AllEndowmentTests {
     public static Test suite() {
         TestSuite suite = new TestSuite();
 
+        // Daniela's unit tests....
         suite.addTestSuite(AssetIncreaseDocumentRulesTest.class);
-
+        
+        // Muddu's unit tests....
+        suite.addTestSuite(HoldingAdjustmentDocumentRulesTest.class);
+        suite.addTestSuite(HoldingHistoryValueAdjustmentDocumentRuleTest.class);
+        suite.addTestSuite(LiabilityDecreaseDocumentRulesTest.class);
+        suite.addTestSuite(AvailableCashUpdateServiceImplTest.class);
+        suite.addTestSuite(CurrentTaxLotBalanceUpdateServiceImplTest.class);
+        suite.addTestSuite(HoldingHistoryMarketValuesUpdateServiceImplTest.class);
+        suite.addTestSuite(HoldingHistoryServiceTest.class);
+        
         return suite;
     }
 

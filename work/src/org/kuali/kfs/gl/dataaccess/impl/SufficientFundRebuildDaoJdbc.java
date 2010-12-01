b89bf571eb5233fd3b4b4dbdf67af3aacdbecc17
Jonathan Keller <keller.jonathan@gmail.com>
2010-12-01 21:49:42 +0000

    KFSMI-6283 : fixed MySQL incompatible SQL

--- work/src/org/kuali/kfs/gl/dataaccess/impl/SufficientFundRebuildDaoJdbc.java
+++ work/src/org/kuali/kfs/gl/dataaccess/impl/SufficientFundRebuildDaoJdbc.java
@@ -28,7 +28,7 @@ public class SufficientFundRebuildDaoJdbc extends PlatformAwareDaoBaseJdbc imple
      */
     public void populateSufficientFundRebuild() {
        getSimpleJdbcTemplate().update("INSERT INTO GL_SF_REBUILD_T (FIN_COA_CD, ACCT_NBR_FOBJ_CD, acct_fobj_typ_cd, obj_id, ver_nbr) "+ 
-               "(SELECT FIN_COA_CD, ACCOUNT_NBR, 'A', sys_guid(), 1 FROM CA_ACCOUNT_T WHERE ACCT_CLOSED_IND = 'N')");
+               "(SELECT FIN_COA_CD, ACCOUNT_NBR, 'A', obj_id, 1 FROM CA_ACCOUNT_T WHERE ACCT_CLOSED_IND = 'N')");
 
     }
 

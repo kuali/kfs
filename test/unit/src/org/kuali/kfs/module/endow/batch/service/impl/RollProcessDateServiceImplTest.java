ea0df55919faa889298f4a8d77c6d7bdd9f23e31
David Yun <davidyun@umd.edu>
2010-12-02 21:36:50 +0000

    KFSMI-6271: renamed

---
+++ test/unit/src/org/kuali/kfs/module/endow/batch/service/impl/RollProcessDateServiceImplTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2010 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.batch.service.impl;
+
+import static org.kuali.kfs.sys.fixture.UserNameFixture.khuntley;
+
+import java.util.Date;
+
+import org.kuali.kfs.module.endow.EndowParameterKeyConstants;
+import org.kuali.kfs.module.endow.batch.service.RollProcessDateService;
+import org.kuali.kfs.module.endow.document.service.KEMService;
+import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.bo.Parameter;
+import org.kuali.rice.kns.service.ParameterService;
+
+@ConfigureContext(session = khuntley)
+public class RollProcessDateServiceImplTest extends KualiTestBase {
+   
+    protected ParameterService parameterService;
+    protected KEMService kemService;
+
+    protected RollProcessDateService rollProcessDateService;
+    
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception { 
+    
+        // Initialize service objects.
+        rollProcessDateService = SpringContext.getBean(RollProcessDateService.class);        
+        kemService = SpringContext.getBean(KEMService.class);
+        parameterService = SpringContext.getBean(ParameterService.class);
+                        
+        super.setUp();
+    }
+    
+    /**
+     * 
+     * @see junit.framework.TestCase#tearDown()
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        rollProcessDateService = null;        
+        kemService = null;
+        parameterService = null;
+        super.tearDown();
+    }
+    
+    public void testProcessDate() {
+        Date currentProcessDate = kemService.getCurrentDate();
+        System.out.println(currentProcessDate.toString());
+        rollProcessDateService.rollDate();
+    }
+}

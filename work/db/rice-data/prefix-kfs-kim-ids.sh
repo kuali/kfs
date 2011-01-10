9f3d1ca11bc2649826e1e5115799a135c1d2a463
Jonathan Keller <keller.jonathan@gmail.com>
2011-01-10 22:02:07 +0000

    Script cleanup

--- work/db/rice-data/prefix-kfs-kim-ids.sh
+++ work/db/rice-data/prefix-kfs-kim-ids.sh
@@ -7,37 +7,40 @@
 
 # Role-Permission Table
 
-#cp bootstrap/krim_role_perm_t.csv bootstrap/krim_role_perm_t.backup
+mv bootstrap/krim_role_perm_t.csv bootstrap/krim_role_perm_t.backup
 # copy the header line
-#head -1 bootstrap/krim_role_perm_t.backup > bootstrap/krim_role_perm_t.csv
-#for LINE in $(cat bootstrap/krim_role_perm_t.backup); do
-#	#echo $LINE
-#	ROLE_ID=`echo "$LINE" | cut -d ',' -f 3`
-#	PERM_ID=`echo "$LINE" | cut -d ',' -f 4`
-#	#echo $PERM_ID 
-#
-#	grep "^$PERM_ID," bootstrap/krim_perm_t.csv > /dev/null
-#	RESULT=$?
-#	#echo Perm: $PERM_ID : $RESULT
-#	if [[ $RESULT == 0 ]]; then
-#		PERM_ID=KFS$PERM_ID
-#	fi
-#	grep "^$ROLE_ID," bootstrap/krim_role_t.csv > /dev/null
-#	RESULT=$?
-#	#echo Role: $ROLE_ID : $RESULT
-#	if [[ $RESULT == 0 ]]; then
-#		ROLE_ID=KFS$ROLE_ID
-#	fi
-#	echo $LINE | sed "s/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),/\1,\2,$ROLE_ID,$PERM_ID,/" >> bootstrap/krim_role_perm_t.csv
-#done
+touch bootstrap/krim_role_perm_t.csv
+for LINE in $(cat bootstrap/krim_role_perm_t.backup); do
+	#echo $LINE
+	ROLE_ID=`echo "$LINE" | cut -d ',' -f 3`
+	PERM_ID=`echo "$LINE" | cut -d ',' -f 4`
+	#echo $PERM_ID
+
+	if [[ "$ROLE_ID" != "ROLE_ID" ]]; then
+
+		grep "^$PERM_ID," bootstrap/krim_perm_t.csv > /dev/null
+		RESULT=$?
+		#echo Perm: $PERM_ID : $RESULT
+		if [[ $RESULT == 0 ]]; then
+			PERM_ID=KFS$PERM_ID
+		fi
+		grep "^$ROLE_ID," bootstrap/krim_role_t.csv > /dev/null
+		RESULT=$?
+		#echo Role: $ROLE_ID : $RESULT
+		if [[ $RESULT == 0 ]]; then
+			ROLE_ID=KFS$ROLE_ID
+		fi
+	fi
+	echo $LINE | sed "s/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),/\1,\2,$ROLE_ID,$PERM_ID,/" >> bootstrap/krim_role_perm_t.csv
+done
 
 # Role-Responsibility
   # change role only - resp still integer
 
 # Role Member (role member IDs)
 
-cp bootstrap/krim_role_mbr_t.csv bootstrap/krim_role_mbr_t.backup
-head -1 bootstrap/krim_role_mbr_t.backup > bootstrap/krim_role_mbr_t.csv
+mv bootstrap/krim_role_mbr_t.csv bootstrap/krim_role_mbr_t.backup
+touch bootstrap/krim_role_mbr_t.csv
 for LINE in $(cat bootstrap/krim_role_mbr_t.backup); do
 	#echo $LINE
 	ROLE_ID=`echo "$LINE" | cut -d ',' -f 3`
@@ -48,18 +51,20 @@ for LINE in $(cat bootstrap/krim_role_mbr_t.backup); do
 	MBR_TYP_CD=${MBR_TYP_CD//\"/}
 	#echo $ROLE_ID 
 
-	grep "^$ROLE_ID," bootstrap/krim_role_t.csv > /dev/null
-	RESULT=$?
-	#echo Role: $ROLE_ID : $RESULT
-	if [[ $RESULT == 0 ]]; then
-		ROLE_ID=KFS$ROLE_ID
-	fi
-	if [[ "$MBR_TYP_CD" == "R" ]]; then
-		grep "^$MBR_ID," bootstrap/krim_role_t.csv > /dev/null
+	if [[ "$ROLE_ID" != "ROLE_ID" ]]; then
+		grep "^$ROLE_ID," bootstrap/krim_role_t.csv > /dev/null
 		RESULT=$?
 		#echo Role: $ROLE_ID : $RESULT
 		if [[ $RESULT == 0 ]]; then
-			MBR_ID=KFS$MBR_ID
+			ROLE_ID=KFS$ROLE_ID
+		fi
+		if [[ "$MBR_TYP_CD" == "R" ]]; then
+			grep "^$MBR_ID," bootstrap/krim_role_t.csv > /dev/null
+			RESULT=$?
+			#echo Role: $ROLE_ID : $RESULT
+			if [[ $RESULT == 0 ]]; then
+				MBR_ID=KFS$MBR_ID
+			fi
 		fi
 	fi
 	echo $LINE | sed "s/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),/\1,\2,\"$ROLE_ID\",\"$MBR_ID\",/" >> bootstrap/krim_role_mbr_t.csv

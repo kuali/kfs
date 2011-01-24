aa553f616981ff21fc81471a1f8c58f55caaf1a0
David Yun <davidyun@umd.edu>
2011-01-24 21:52:29 +0000

    KFSMI-6264: added security, class code, etc

--- test/unit/src/org/kuali/kfs/module/endow/batch/service/impl/PooledFundControlTransactionsServiceImplTest.java
+++ test/unit/src/org/kuali/kfs/module/endow/batch/service/impl/PooledFundControlTransactionsServiceImplTest.java
@@ -17,9 +17,23 @@ package org.kuali.kfs.module.endow.batch.service.impl;
 
 import static org.kuali.kfs.sys.fixture.UserNameFixture.kfs;
 
-import org.kuali.kfs.module.endow.batch.service.PooledFundControlTransactionsService;
+import org.kuali.kfs.module.endow.businessobject.ClassCode;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionCode;
+import org.kuali.kfs.module.endow.businessobject.GLLink;
+import org.kuali.kfs.module.endow.businessobject.KEMID;
+import org.kuali.kfs.module.endow.businessobject.KemidGeneralLedgerAccount;
 import org.kuali.kfs.module.endow.businessobject.PooledFundControl;
+import org.kuali.kfs.module.endow.businessobject.Security;
+import org.kuali.kfs.module.endow.businessobject.SecurityReportingGroup;
+import org.kuali.kfs.module.endow.fixture.ClassCodeFixture;
+import org.kuali.kfs.module.endow.fixture.EndowmentTransactionCodeFixture;
+import org.kuali.kfs.module.endow.fixture.GLLinkFixture;
+import org.kuali.kfs.module.endow.fixture.KemIdFixture;
+import org.kuali.kfs.module.endow.fixture.KemidGeneralLedgerAccountFixture;
 import org.kuali.kfs.module.endow.fixture.PooledFundControlTransactionFixture;
+import org.kuali.kfs.module.endow.fixture.RegistrationCodeFixture;
+import org.kuali.kfs.module.endow.fixture.SecurityFixture;
+import org.kuali.kfs.module.endow.fixture.SecurityReportingGroupFixture;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.TestUtils;
@@ -38,6 +52,26 @@ public class PooledFundControlTransactionsServiceImplTest extends KualiTestBase 
         super.setUp();        
         pooledFundControlTransactionsService = (PooledFundControlTransactionsServiceImpl) TestUtils.getUnproxiedService("mockPooledFundControlTransactionsService");
         pooledFundControlTransactionsService.initializeReports();
+        
+        SecurityReportingGroup reportingGroup = SecurityReportingGroupFixture.REPORTING_GROUP.createSecurityReportingGroup();
+        EndowmentTransactionCode endowmentTransactionCode = EndowmentTransactionCodeFixture.INCOME_TRANSACTION_CODE.createEndowmentTransactionCode();                
+        ClassCode classCode = ClassCodeFixture.ASSET_CLASS_CODE.createClassCodeRecord();
+        
+        Security security1 = SecurityFixture.ENDOWMENT_ASSET_INCOME_SECURITY_RECORD.createSecurityRecord();
+        Security security2 = SecurityFixture.ENDOWMENT_ASSET_PRINCIPAL_SECURITY_RECORD.createSecurityRecord();
+        RegistrationCodeFixture.ASSET_REGISTRATION_CODE_RECORD_1.createRegistrationCode();
+        RegistrationCodeFixture.ASSET_REGISTRATION_CODE_RECORD_2.createRegistrationCode();
+
+        KEMID kemid1 = KemIdFixture.CSM_KEMID_RECORD_1.createKemidRecord();
+        KEMID kemid2 = KemIdFixture.CSM_KEMID_RECORD_2.createKemidRecord();
+      
+        GLLink glLink = GLLinkFixture.GL_LINK_BL_CHART.createGLLink();
+        KemidGeneralLedgerAccount generalLedgerAccount1 = KemidGeneralLedgerAccountFixture.KEMID_GL_ACCOUNT_FOR_ASSET1.createKemidGeneralLedgerAccount();
+        KemidGeneralLedgerAccount generalLedgerAccount2 = KemidGeneralLedgerAccountFixture.KEMID_GL_ACCOUNT_FOR_ASSET2.createKemidGeneralLedgerAccount();
+        kemid1.getKemidGeneralLedgerAccounts().add(generalLedgerAccount1);
+        kemid2.getKemidGeneralLedgerAccounts().add(generalLedgerAccount2);
+        
+        endowmentTransactionCode.getGlLinks().add(glLink);
     }
     
     /**
@@ -54,7 +88,7 @@ public class PooledFundControlTransactionsServiceImplTest extends KualiTestBase 
      * Validates CreateECDD()
      */
     public void testCreateECDD() {
-        PooledFundControlTransactionFixture pooledFundControlTransactionFixture = PooledFundControlTransactionFixture.ECDD_DATA;
+        PooledFundControlTransactionFixture pooledFundControlTransactionFixture = PooledFundControlTransactionFixture.ASSET_PURCHASE_DATA;
         PooledFundControl pooledFundControl = pooledFundControlTransactionFixture.createPooledFundControl();
         KualiDecimal totalAmount = pooledFundControlTransactionFixture.getTotalAmount();
         String paramDescriptionName = pooledFundControlTransactionFixture.getParamDescriptionName();
@@ -68,7 +102,7 @@ public class PooledFundControlTransactionsServiceImplTest extends KualiTestBase 
      * Validates CreateECI()
      */
     public void testCreateECI() {
-        PooledFundControlTransactionFixture pooledFundControlTransactionFixture = PooledFundControlTransactionFixture.ECI_DATA;
+        PooledFundControlTransactionFixture pooledFundControlTransactionFixture = PooledFundControlTransactionFixture.ASSET_INCOME_DATA;
         PooledFundControl pooledFundControl = pooledFundControlTransactionFixture.createPooledFundControl();
         KualiDecimal totalAmount = pooledFundControlTransactionFixture.getTotalAmount();
         String paramDescriptionName = pooledFundControlTransactionFixture.getParamDescriptionName();

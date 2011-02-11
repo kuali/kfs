03469b2e21525aa662b5ec2738d7bc42eb68cafd
Winston Dang <wkd@hawaii.edu>
2011-02-11 20:51:29 +0000

    KFSMI-6442 Effort Reporting Integration. add web service

---
+++ work/src/org/kuali/kfs/module/external/kc/service/impl/ContractsAndGrantsModuleServiceImpl.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2007-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.external.kc.service.impl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.kuali.kfs.coa.businessobject.Account;
+import org.kuali.kfs.coa.service.AccountService;
+import org.kuali.kfs.fp.document.BudgetAdjustmentDocument;
+import org.kuali.kfs.integration.cg.ContractsAndGrantsModuleService;
+import org.kuali.kfs.integration.kc.KcConstants;
+import org.kuali.kfs.module.cg.CGConstants;
+import org.kuali.kfs.module.cg.businessobject.AwardAccount;
+import org.kuali.kfs.module.cg.service.AgencyService;
+import org.kuali.kfs.module.cg.service.CfdaService;
+import org.kuali.kfs.module.ec.EffortConstants.SystemParameters;
+import org.kuali.kfs.module.ec.document.EffortCertificationDocument;
+import org.kuali.kfs.module.ec.util.EffortCertificationParameterFinder;
+import org.kuali.kfs.module.external.kc.webService.BudgetCategory.InstitutionalBudgetCategorySoapService;
+import org.kuali.kfs.module.external.kc.webService.EffortReporting.EffortReportingService;
+import org.kuali.kfs.module.external.kc.webService.EffortReporting.EffortReportingServiceSoapService;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.service.NonTransactional;
+import org.kuali.kfs.sys.service.impl.KfsParameterConstants;
+import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.service.ParameterService;
+
+@NonTransactional
+public class ContractsAndGrantsModuleServiceImpl implements ContractsAndGrantsModuleService {
+    private org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ContractsAndGrantsModuleServiceImpl.class);
+    private String wsdlLocation;
+    private URL wsdlURL = EffortReportingServiceSoapService.WSDL_LOCATION;
+    private static final QName SERVICE_NAME = new QName(KFSConstants.Research.KC_NAMESPACE_URI, "effortReportingServiceSoapService");   
+
+    /**
+     * @see org.kuali.kfs.integration.cg.ContractsAndGrantsModuleService#getProjectDirectorForAccount(java.lang.String,
+     *      java.lang.String)
+     */
+    public Person getProjectDirectorForAccount(String chartOfAccountsCode, String accountNumber) {     
+        EffortReportingServiceSoapService ss = new EffortReportingServiceSoapService(wsdlURL, SERVICE_NAME);
+        EffortReportingService port = ss.getEffortReportingServicePort();  
+        
+        String projectDirectorAcctNumber = port.getProjectDirector(accountNumber);
+
+        AccountService accountService = SpringContext.getBean(AccountService.class);
+        Account projectDirectorAccount = accountService.getByPrimaryId(chartOfAccountsCode, projectDirectorAcctNumber);
+        
+        if (projectDirectorAccount != null) {
+            Person projectDirector = SpringContext.getBean(org.kuali.rice.kim.service.PersonService.class).getPersonByEmployeeId(projectDirectorAccount.getAccountFiscalOfficerSystemIdentifier());
+              return projectDirector;
+        }
+
+        return null;
+    }
+
+    /**
+     * @see org.kuali.kfs.integration.service.ContractsAndGrantsModuleService#getProjectDirectorForAccount(org.kuali.kfs.coa.businessobject.Account)
+     */
+    public Person getProjectDirectorForAccount(Account account) {
+        if (account != null) {
+            String chartOfAccountsCode = account.getChartOfAccountsCode();
+            String accountNumber = account.getAccountNumber();
+            return this.getProjectDirectorForAccount(chartOfAccountsCode, accountNumber);
+        }
+        return null;
+    }
+
+    /**
+     * @see org.kuali.kfs.integration.service.ContractsAndGrantsModuleService#isAwardedByFederalAgency(java.lang.String,
+     *      java.lang.String, java.util.List)
+     */
+    public boolean isAwardedByFederalAgency(String chartOfAccountsCode, String accountNumber, List<String> federalAgencyTypeCodes) {       
+        EffortReportingServiceSoapService ss = new EffortReportingServiceSoapService(wsdlURL, SERVICE_NAME);
+        EffortReportingService port = ss.getEffortReportingServicePort();  
+        boolean _isFederalSponsor__return = port.isFederalSponsor(accountNumber);
+        if ( _isFederalSponsor__return) return true;
+        
+        String federalSponsorTypeCode = getParameterService().getParameterValue(EffortCertificationDocument.class, "FEDERAL_SPONSOR_TYPE_CODES");
+        if (federalAgencyTypeCodes.contains(federalSponsorTypeCode)) return true;
+
+        return false;
+        /*
+        AwardAccount primaryAward = getPrimaryAwardAccount(chartOfAccountsCode, accountNumber);
+        if (primaryAward == null) {
+            return false;
+        }
+
+        String agencyTypeCode = primaryAward.getAward().getAgency().getAgencyTypeCode();
+        if (federalAgencyTypeCodes.contains(agencyTypeCode) || primaryAward.getAward().getFederalPassThroughIndicator()) {
+            return true;
+        }
+
+        return false;
+        */
+    }
+
+    /**
+     * get the primary award account for the given account
+     * 
+     * @param account the given account
+     * @return the primary award account for the given account
+     */
+    protected AwardAccount getPrimaryAwardAccount(String chartOfAccountsCode, String accountNumber) {
+        AwardAccount primaryAwardAccount = null;
+        long highestProposalNumber = 0;
+
+        Map<String, Object> accountKeyValues = new HashMap<String, Object>();
+        accountKeyValues.put(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE, chartOfAccountsCode);
+        accountKeyValues.put(KFSPropertyConstants.ACCOUNT_NUMBER, accountNumber);
+
+        for (Object awardAccountAsObject : getBusinessObjectService().findMatching(AwardAccount.class, accountKeyValues)) {
+            AwardAccount awardAccount = (AwardAccount) awardAccountAsObject;
+            Long proposalNumber = awardAccount.getProposalNumber();
+
+            if (proposalNumber >= highestProposalNumber) {
+                highestProposalNumber = proposalNumber;
+                primaryAwardAccount = awardAccount;
+            }
+        }
+
+        return primaryAwardAccount;
+    }
+
+    /**
+     * @see org.kuali.kfs.integration.cg.ContractsAndGrantsModuleService#getAllAccountReponsiblityIds()
+     */
+    public List<Integer> getAllAccountReponsiblityIds() {
+        int maxResponsibilityId = this.getMaxiumAccountResponsibilityId();
+
+        List<Integer> contractsAndGrantsReponsiblityIds = new ArrayList<Integer>();
+        for (int id = 1; id <= maxResponsibilityId; id++) {
+            contractsAndGrantsReponsiblityIds.add(id);
+        }
+
+        return contractsAndGrantsReponsiblityIds;
+    }
+
+    /**
+     * @see org.kuali.kfs.integration.cg.ContractsAndGrantsModuleService#hasValidAccountReponsiblityIdIfExists(org.kuali.kfs.coa.businessobject.Account)
+     */
+    public boolean hasValidAccountReponsiblityIdIfNotNull(Account account) {
+        Integer accountResponsiblityId = account.getContractsAndGrantsAccountResponsibilityId();
+
+        if (accountResponsiblityId == null) {
+            return true;
+        }
+
+        return accountResponsiblityId >= 1 && accountResponsiblityId <= this.getMaxiumAccountResponsibilityId();
+    }
+
+    /**
+     * retieve the maxium account responsiblity id from system parameter
+     * 
+     * @return the maxium account responsiblity id from system parameter
+     */
+    protected int getMaxiumAccountResponsibilityId() {
+        String maxResponsibilityId = getParameterService().getParameterValue(KfsParameterConstants.CONTRACTS_AND_GRANTS_ALL.class, CGConstants.MAXIMUM_ACCOUNT_RESPONSIBILITY_ID);
+
+        return Integer.valueOf(maxResponsibilityId);
+    }
+
+    /**
+     * Returns an implementation of the parameterService
+     * 
+     * @return an implementation of the parameterService
+     */
+    public ParameterService getParameterService() {
+        return SpringContext.getBean(ParameterService.class);
+    }
+
+    /**
+     * Returns the default implementation of the C&G AgencyService
+     * 
+     * @return an implementation of AgencyService
+     */
+    public AgencyService getAgencyService() {
+        return SpringContext.getBean(AgencyService.class);
+    }
+
+    /**
+     * Returns an implementation of the CfdaService
+     * 
+     * @return an implementation of the CfdaService
+     */
+    public CfdaService getCfdaService() {
+        return SpringContext.getBean(CfdaService.class);
+    }
+
+    /**
+     * Returns an implementation of the BusinessObjectService
+     * 
+     * @return an implementation of the BusinessObjectService
+     */
+    public BusinessObjectService getBusinessObjectService() {
+        return SpringContext.getBean(BusinessObjectService.class);
+    }
+    public String getWsdlLocation() {
+        return wsdlLocation;
+    }
+
+    public void setWsdlLocation(String wsdlLocation) {
+        this.wsdlLocation = wsdlLocation;
+        try {
+            wsdlURL = new URL(wsdlLocation);
+        }
+        catch (MalformedURLException ex) {
+          
+            LOG.error(KcConstants.BudgetAdjustmentService.ERROR_KC_ACCOUNT_PARAMS_UNIT_NOTFOUND +  ex.getMessage()); 
+            //ex.printStackTrace();
+        }
+    }
+}
+

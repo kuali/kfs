df2924b6447620904c9527af1164e36a56dbf609
Dan Lemus <dlemus@vivantech.com>
2011-02-11 19:30:16 +0000

    KFSMI-6185 Tickler Delivery job implemented.

---
+++ work/src/org/kuali/kfs/module/endow/batch/TicklerDeliveryStep.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2011 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.endow.batch;
+
+import java.util.Date;
+
+import org.kuali.kfs.module.endow.batch.service.TicklerDeliveryService;
+import org.kuali.kfs.sys.batch.AbstractStep;
+
+public class TicklerDeliveryStep extends AbstractStep {
+
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(TicklerDeliveryStep.class);
+    
+    private TicklerDeliveryService ticklerDeliveryService;
+    
+    public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
+        return ticklerDeliveryService.generateTicklerNotices();
+    }
+
+    public void setTicklerDeliveryService(TicklerDeliveryService ticklerDeliveryService){
+        this.ticklerDeliveryService = ticklerDeliveryService;
+    }
+}

cbf0cd55c8cfeb3bdc5bc6f4476da805b3cb15e3
Dan Lemus <dlemus@vivantech.com>
2011-03-03 18:51:37 +0000

    KFSMI-6450 More fixes to KFS/KC framework, reduced methods overridden in the module service.

--- work/src/org/kuali/kfs/module/external/kc/service/impl/KcKfsModuleServiceImpl.java
+++ work/src/org/kuali/kfs/module/external/kc/service/impl/KcKfsModuleServiceImpl.java
@@ -39,81 +39,9 @@ public class KcKfsModuleServiceImpl  extends KfsModuleServiceImpl  {
     
     protected static final Logger LOG = Logger.getLogger(KcKfsModuleServiceImpl.class);
     private static Map<Class, Class> externalizedWebBOs = new HashMap<Class, Class>();
-    //private static Map<Class, String> externalWebBusinessObjectPrimaryKeys = new HashMap<Class,String>();
-    
-    public <T extends ExternalizableBusinessObject> T retrieveExternalizableBusinessObjectIfNecessary(BusinessObject businessObject, T currentInstanceExternalizableBO, String externalizableRelationshipName) {
-                
-        if(businessObject==null) return null;
-        
-        Class<org.kuali.rice.kns.bo.ExternalizableBusinessObject> clazz;
-        
-        try{
-            clazz = getExternalizableBusinessObjectImplementation(
-                    PropertyUtils.getPropertyType(businessObject, externalizableRelationshipName));
-        } catch(Exception iex){
-            LOG.warn("Exception:"+iex+" thrown while trying to get property type for property:"+externalizableRelationshipName+
-                    " from business object:"+businessObject);
-            return null;
-        }
-        
-        //Get the business object entry for this business object from data dictionary
-        //using the class name (without the package) as key
-        BusinessObjectEntry entry =
-            KNSServiceLocator.getDataDictionaryService().getDataDictionary().getBusinessObjectEntries().get(
-                    businessObject.getClass().getSimpleName());
-        RelationshipDefinition relationshipDefinition = entry.getRelationshipDefinition(externalizableRelationshipName);
-        List<PrimitiveAttributeDefinition> primitiveAttributeDefinitions = relationshipDefinition.getPrimitiveAttributes();
-
-        Map<String, Object> fieldValuesInEBO = new HashMap<String, Object>();
-        Object sourcePropertyValue;
-        Object targetPropertyValue = null;
-        boolean sourceTargetPropertyValuesSame = true;
-        for(PrimitiveAttributeDefinition primitiveAttributeDefinition: primitiveAttributeDefinitions){
-            sourcePropertyValue = ObjectUtils.getPropertyValue(
-                    businessObject, primitiveAttributeDefinition.getSourceName());
-            if(currentInstanceExternalizableBO!=null)
-                targetPropertyValue = ObjectUtils.getPropertyValue(currentInstanceExternalizableBO, primitiveAttributeDefinition.getTargetName());
-            if(sourcePropertyValue==null){
-                return null;
-            } else if(targetPropertyValue==null || (targetPropertyValue!=null && !targetPropertyValue.equals(sourcePropertyValue))){
-                sourceTargetPropertyValuesSame = false;
-            }
-            fieldValuesInEBO.put(primitiveAttributeDefinition.getTargetName(), sourcePropertyValue);
-        }
-
-        if(!sourceTargetPropertyValuesSame)
-            return (T) getExternalizableBusinessObject(clazz, fieldValuesInEBO);
-        return currentInstanceExternalizableBO;        
-    };
 
     public <T extends ExternalizableBusinessObject> T getExternalizableBusinessObject(Class<T> businessObjectClass, Map<String, Object> fieldValues) {        
         return (T) getExternalizableBusinessObjectService(businessObjectClass).findByPrimaryKey(fieldValues);
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<? extends ExternalizableBusinessObject> retrieveExternalizableBusinessObjectsList(
-            BusinessObject businessObject, String externalizableRelationshipName, Class externalizableClazz) {        
-        if(businessObject==null) return null;
-        //Get the business object entry for this business object from data dictionary
-        //using the class name (without the package) as key
-        String className = businessObject.getClass().getName();
-        String key = className.substring(className.lastIndexOf(".")+1);
-        BusinessObjectEntry entry =
-            KNSServiceLocator.getDataDictionaryService().getDataDictionary().getBusinessObjectEntries().get(key);
-        RelationshipDefinition relationshipDefinition = entry.getRelationshipDefinition(externalizableRelationshipName);
-        List<PrimitiveAttributeDefinition> primitiveAttributeDefinitions = relationshipDefinition.getPrimitiveAttributes();
-        Map<String, Object> fieldValuesInEBO = new HashMap<String, Object>();
-        Object sourcePropertyValue;
-        for(PrimitiveAttributeDefinition primitiveAttributeDefinition: primitiveAttributeDefinitions){
-            sourcePropertyValue = ObjectUtils.getPropertyValue(
-                    businessObject, primitiveAttributeDefinition.getSourceName());
-            if(sourcePropertyValue==null){
-                return null;
-            }
-            fieldValuesInEBO.put(primitiveAttributeDefinition.getTargetName(), sourcePropertyValue);
-        }
-        return getExternalizableBusinessObjectsList(
-                getExternalizableBusinessObjectImplementation(externalizableClazz), fieldValuesInEBO);
     }
 
     public <T extends ExternalizableBusinessObject> List<T> getExternalizableBusinessObjectsListForLookup(Class<T> businessObjectClass, Map<String, Object> fieldValues, boolean unbounded) {
@@ -140,78 +68,11 @@ public class KcKfsModuleServiceImpl  extends KfsModuleServiceImpl  {
         return eboService;
     }
 
-/*    protected Class getInterfaceToObj(Class ebo) {
-        if (ebo.isInterface()) return ebo;
-        return ebo.getInterfaces()[0];
-    }
-*/  
-
     public List listPrimaryKeyFieldNames(Class businessObjectInterfaceClass){
         Class clazz = getExternalizableBusinessObjectImplementation(businessObjectInterfaceClass);
-        List primaryKeyFieldNames = new ArrayList();
-        
+        List primaryKeyFieldNames = new ArrayList();        
         return primaryKeyFieldNames;
     }
-
-  /*
-    public BusinessObjectEntry getExternalizableBusinessObjectDictionaryEntry(Class businessObjectInterfaceClass) {
-            Class boClass = businessObjectInterfaceClass;
-            if(businessObjectInterfaceClass.isInterface()) {
-                boClass = getExternalizableBusinessObjectImplementation(businessObjectInterfaceClass);
-                if (boClass == null) return null;
-                   DataDictionary dataDictionary = KNSServiceLocator.getDataDictionaryService().getDataDictionary();
-                   Map<String, BusinessObjectEntry> boEntries = dataDictionary.getBusinessObjectEntries();
-                   BusinessObjectEntry businessObjectEntry = boEntries.get(boClass.getName());
-                   if (businessObjectEntry != null) return businessObjectEntry;
-                   // try again but look for the simple name
-                   businessObjectEntry = boEntries.get(boClass.getSimpleName());
-                   return businessObjectEntry;
-             }
-            DataDictionary dataDictionary = KNSServiceLocator.getDataDictionaryService().getDataDictionary();
-            Map<String, BusinessObjectEntry> boEntries = dataDictionary.getBusinessObjectEntries();
-            BusinessObjectEntry businessObjectEntry = boEntries.get(boClass.getName());
-            if (businessObjectEntry != null) return businessObjectEntry;
-            businessObjectEntry = boEntries.get(boClass.getSimpleName());
-            return businessObjectEntry;
-        }
-*/
-/*  
-    @Override
-    public boolean isExternalizableBusinessObjectLookupable(Class boClass) {
-        // TODO Auto-generated method stub
-        Class boClassInterface = getInterfaceToObj(boClass);
-        if (externalWebBusinessObjectPrimaryKeys.containsKey(boClassInterface)) {
-            String primaryKey = externalWebBusinessObjectPrimaryKeys.get(boClassInterface);
-            if (primaryKey.length() > 1) return true;
-            return false;
-         }
-        return super.isExternalizableBusinessObjectLookupable(boClass);
-    }
-    
-   public String getExternalizableBusinessObjectLookupUrl(Class inquiryBusinessObjectClass, Map<String, String> parameters) {
-        Properties urlParameters = new Properties();
-
-        String riceBaseUrl = KNSServiceLocator.getKualiConfigurationService().getPropertyString(KNSConstants.APPLICATION_URL_KEY);
-        String lookupUrl = riceBaseUrl;
-        if (!lookupUrl.endsWith("/")) {
-            lookupUrl = lookupUrl + "/";
-        }
-        if (parameters.containsKey(KNSConstants.MULTIPLE_VALUE)) {
-            lookupUrl = lookupUrl + "kr/" + KNSConstants.MULTIPLE_VALUE_LOOKUP_ACTION;
-        }
-        else {
-            lookupUrl = lookupUrl + "kr/" + KNSConstants.LOOKUP_ACTION;
-        }
-        for (String paramName : parameters.keySet()) {
-            urlParameters.put(paramName, parameters.get(paramName));
-        }
-
-        Class clazz = getExternalizableBusinessObjectImplementation(inquiryBusinessObjectClass);
-        urlParameters.put(KNSConstants.BUSINESS_OBJECT_CLASS_ATTRIBUTE, clazz==null?"":clazz.getName());
-
-        return UrlFactory.parameterizeUrl(lookupUrl, urlParameters);
-    }
-*/
     
     @Override
     public void setModuleConfiguration(ModuleConfiguration moduleConfiguration) {   
@@ -226,7 +87,6 @@ public class KcKfsModuleServiceImpl  extends KfsModuleServiceImpl  {
                   if (ebos.containsKey(webo)) {
                       externalizedWebBOs.put(webo, ebos.get(webo));
                       externalizedWebBOs.put(ebos.get(webo), ebos.get(webo));
-                      //externalWebBusinessObjectPrimaryKeys.put(webo, externalWebBusinessObjects.get(webo));
                    }
               }
           }

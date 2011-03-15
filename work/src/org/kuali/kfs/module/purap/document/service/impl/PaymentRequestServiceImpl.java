512a1fe3e5d92047c335738754df0073fa9a8f70
Jonathan Keller <keller.jonathan@gmail.com>
2011-03-15 14:37:20 +0000

    KFSMI-6434 create deep copy when auto-approving

--- work/src/org/kuali/kfs/module/purap/document/service/impl/PaymentRequestServiceImpl.java
+++ work/src/org/kuali/kfs/module/purap/document/service/impl/PaymentRequestServiceImpl.java
@@ -311,6 +311,23 @@ public class PaymentRequestServiceImpl implements PaymentRequestService {
     public boolean autoApprovePaymentRequest(PaymentRequestDocument doc, KualiDecimal defaultMinimumLimit) {
         if (isEligibleForAutoApproval(doc, defaultMinimumLimit)) {
             try {
+                // Much of the rice frameworks assumes that document instances that are saved via DocumentService.saveDocument are those
+                // that were dynamically created by PojoFormBase (i.e., the Document instance wasn't created from OJB).  We need to make
+                // a deep copy and materialize collections to fulfill that assumption so that collection elements will delete properly
+                
+                // TODO: maybe rewriting PurapService.calculateItemTax could be rewritten so that the a deep copy doesn't need to be made
+                // by taking advantage of OJB's managed array lists
+                try {
+                    ObjectUtils.materializeUpdateableCollections(doc);
+                    for (PaymentRequestItem item : (List<PaymentRequestItem>) doc.getItems()) {
+                        ObjectUtils.materializeUpdateableCollections(item);
+                    }
+                }
+                catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+                doc = (PaymentRequestDocument) ObjectUtils.deepCopy(doc);
+                
                 purapService.updateStatus(doc, PaymentRequestStatuses.AUTO_APPROVED);
                 documentService.blanketApproveDocument(doc, "auto-approving: Total is below threshold.", null);
             }

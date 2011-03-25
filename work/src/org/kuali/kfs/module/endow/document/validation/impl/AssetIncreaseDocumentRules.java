1d2ffa0a2b76f0532b72f3c11c5ce6edbbc2a304
David Yun <davidyun@umd.edu>
2011-03-25 17:29:23 +0000

    KFSMI-6518: changes in validateTaxLotsCostAndTransactionAmountLessOrEqualToSecurityCommitment

--- work/src/org/kuali/kfs/module/endow/document/validation/impl/AssetIncreaseDocumentRules.java
+++ work/src/org/kuali/kfs/module/endow/document/validation/impl/AssetIncreaseDocumentRules.java
@@ -165,9 +165,9 @@ public class AssetIncreaseDocumentRules extends EndowmentTransactionLinesDocumen
     }
 
     /**
-     * Validates that If the SECURITY_ID has a class code type of A (Alternative Investments), the system must validate that the
-     * total END_HLDG_TAX_LOT_T: HLDG_COST for the SECURITY_ID plus the END_TRAN_LN_T: TRAN_AMT does not exceed the value in
-     * END_SEC_T: CMTMNT_AMT for the Security. If it does, the transaction should not be allowed.
+     * If the SECURITY_ID has a class code type A (Alternative Investments), the system must validate that the total
+     * END_SEC_T: SEC_CVAL for the SECURITY_ID plus the END_TRAN_LN_T:TRAN_AMT does not exceed the value in END_SEC_T: CMTMNT_AMT
+     * for the Security. If it does, the transaction should not be allowed.
      * 
      * @return true if valid, false otherwise
      */
@@ -179,28 +179,26 @@ public class AssetIncreaseDocumentRules extends EndowmentTransactionLinesDocumen
             BigDecimal totalHoldingTaxLotsCost = BigDecimal.ZERO;
             BigDecimal totalTransactionLinesAmt = BigDecimal.ZERO;
             BigDecimal securityCommitmentAmt = BigDecimal.ZERO;
+            BigDecimal securityCarryValue = BigDecimal.ZERO;
 
             if (assetIncreaseDocument.getTargetTransactionLines() != null) {
                 for (EndowmentTransactionLine transactionLine : assetIncreaseDocument.getTargetTransactionLines()) {
 
                     // add to total amount
                     totalTransactionLinesAmt = totalTransactionLinesAmt.add(transactionLine.getTransactionAmount().bigDecimalValue());
-
-                    if (transactionLine.getTaxLotLines() != null) {
-                        for (EndowmentTransactionTaxLotLine taxLotLine : transactionLine.getTaxLotLines()) {
-                            // add to total holding cost
-                            totalHoldingTaxLotsCost = totalHoldingTaxLotsCost.add(taxLotLine.getLotHoldingCost());
-
-                        }
-                    }
                 }
             }
 
-            if (assetIncreaseDocument.getTargetTransactionSecurity() != null && assetIncreaseDocument.getTargetTransactionSecurity().getSecurity() != null && assetIncreaseDocument.getTargetTransactionSecurity().getSecurity().getCommitmentAmount() != null) {
-                securityCommitmentAmt = assetIncreaseDocument.getTargetTransactionSecurity().getSecurity().getCommitmentAmount();
+            if (assetIncreaseDocument.getTargetTransactionSecurity() != null && assetIncreaseDocument.getTargetTransactionSecurity().getSecurity() != null) {
+                if (assetIncreaseDocument.getTargetTransactionSecurity().getSecurity().getCommitmentAmount() != null) {
+                    securityCommitmentAmt = assetIncreaseDocument.getTargetTransactionSecurity().getSecurity().getCommitmentAmount();
+                }
+                if (assetIncreaseDocument.getTargetTransactionSecurity().getSecurity().getCarryValue() != null) {
+                    securityCarryValue = assetIncreaseDocument.getTargetTransactionSecurity().getSecurity().getCarryValue();
+                }
             }
 
-            isValid = (totalHoldingTaxLotsCost.add(totalTransactionLinesAmt)).compareTo(securityCommitmentAmt) <= 0;
+            isValid = (securityCarryValue.add(totalTransactionLinesAmt)).compareTo(securityCommitmentAmt) <= 0;
 
             if (!isValid) {
                 putFieldError(getErrorPrefix(assetIncreaseDocument.getTargetTransactionLine(0), 0) + EndowPropertyConstants.TRANSACTION_LINE_TRANSACTION_AMOUNT, EndowKeyConstants.EndowmentTransactionDocumentConstants.ERROR_TRANSACTION_SECURITY_COMMITMENT_AMT);

95fa2e79d402602fea7369cd45ee2c63a63ffbcf
Daniela Balmus <db483@cornell.edu>
2011-05-03 20:36:54 +0000

    KFSMI-6632 Remove unused service import.

--- work/src/org/kuali/kfs/module/ar/document/dataaccess/impl/AccountsReceivableDocumentHeaderDaoOjb.java
+++ work/src/org/kuali/kfs/module/ar/document/dataaccess/impl/AccountsReceivableDocumentHeaderDaoOjb.java
@@ -15,36 +15,30 @@
  */
 package org.kuali.kfs.module.ar.document.dataaccess.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.ojb.broker.query.Criteria;
-import org.apache.ojb.broker.query.QueryByCriteria;
 import org.apache.ojb.broker.query.QueryFactory;
 import org.kuali.kfs.module.ar.businessobject.AccountsReceivableDocumentHeader;
 import org.kuali.kfs.module.ar.document.dataaccess.AccountsReceivableDocumentHeaderDao;
 import org.kuali.kfs.sys.KFSConstants;
-import org.kuali.rice.kew.exception.WorkflowException;
 import org.kuali.rice.kns.dao.impl.PlatformAwareDaoBaseOjb;
-import org.kuali.rice.kns.exception.InfrastructureException;
-import org.kuali.rice.kns.service.DocumentService;
 
 public class AccountsReceivableDocumentHeaderDaoOjb extends PlatformAwareDaoBaseOjb implements AccountsReceivableDocumentHeaderDao {
-    
-    public Collection getARDocumentHeadersByCustomerNumber(String customerNumber){
+
+    public Collection getARDocumentHeadersByCustomerNumber(String customerNumber) {
         Criteria criteria = new Criteria();
-        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.CUSTOMER_NUMBER,customerNumber);
- 
+        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.CUSTOMER_NUMBER, customerNumber);
+
         return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(AccountsReceivableDocumentHeader.class, criteria));
     }
-    
+
     public Collection getARDocumentHeadersByCustomerNumberByProcessingOrgCodeAndChartCode(String customerNumber, String processingChartOfAccountCode, String processingOrganizationCode) {
         Criteria criteria = new Criteria();
-        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.CUSTOMER_NUMBER,customerNumber);
-        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.PROCESSING_COA_CODE,processingChartOfAccountCode);
-        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.PROCESSING_ORGANIZATION_CODE,processingOrganizationCode);
-        
+        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.CUSTOMER_NUMBER, customerNumber);
+        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.PROCESSING_COA_CODE, processingChartOfAccountCode);
+        criteria.addEqualTo(KFSConstants.CustomerOpenItemReport.PROCESSING_ORGANIZATION_CODE, processingOrganizationCode);
+
         return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(AccountsReceivableDocumentHeader.class, criteria));
     }
 }

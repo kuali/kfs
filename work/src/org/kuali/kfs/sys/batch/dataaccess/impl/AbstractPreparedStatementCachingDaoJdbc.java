c2f632347081db5e43ca58e6b421f00ad9ae65c6
Daniela Balmus <db483@cornell.edu>
2011-05-09 15:48:09 +0000

    KFSMI-6632 Remove unused service import.

--- work/src/org/kuali/kfs/sys/batch/dataaccess/impl/AbstractPreparedStatementCachingDaoJdbc.java
+++ work/src/org/kuali/kfs/sys/batch/dataaccess/impl/AbstractPreparedStatementCachingDaoJdbc.java
@@ -23,7 +23,6 @@ import java.util.Map;
 
 import org.kuali.kfs.sys.batch.dataaccess.PreparedStatementCachingDao;
 import org.kuali.rice.kns.dao.jdbc.PlatformAwareDaoBaseJdbc;
-import org.kuali.rice.kns.service.DateTimeService;
 
 public abstract class AbstractPreparedStatementCachingDaoJdbc extends PlatformAwareDaoBaseJdbc implements PreparedStatementCachingDao {
     protected static final String RETRIEVE_PREFIX = "retrieve-";

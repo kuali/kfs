f2e93d0def22bd0d598bda85624aed58dd54079f
Daniela Balmus <db483@cornell.edu>
2011-05-27 17:53:53 +0000

    KFSMI-4000 Remove active indicator from TaxRegionType

--- work/src/org/kuali/kfs/sys/businessobject/TaxRegionType.java
+++ work/src/org/kuali/kfs/sys/businessobject/TaxRegionType.java
@@ -17,40 +17,30 @@ package org.kuali.kfs.sys.businessobject;
 
 import java.util.LinkedHashMap;
 
-import org.kuali.rice.kns.bo.Inactivateable;
 import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
 
-public class TaxRegionType extends PersistableBusinessObjectBase implements Inactivateable {
-	
-	private String taxRegionTypeCode;
-	private String taxRegionTypeName;
-	private boolean active;
-	
-	public boolean isActive() {
-		return active;
-	}
+public class TaxRegionType extends PersistableBusinessObjectBase {
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+    private String taxRegionTypeCode;
+    private String taxRegionTypeName;
 
-	public String getTaxRegionTypeCode() {
-		return taxRegionTypeCode;
-	}
+    public String getTaxRegionTypeCode() {
+        return taxRegionTypeCode;
+    }
 
-	public void setTaxRegionTypeCode(String taxRegionTypeCode) {
-		this.taxRegionTypeCode = taxRegionTypeCode;
-	}
+    public void setTaxRegionTypeCode(String taxRegionTypeCode) {
+        this.taxRegionTypeCode = taxRegionTypeCode;
+    }
 
-	public String getTaxRegionTypeName() {
-		return taxRegionTypeName;
-	}
+    public String getTaxRegionTypeName() {
+        return taxRegionTypeName;
+    }
 
-	public void setTaxRegionTypeName(String taxRegionTypeName) {
-		this.taxRegionTypeName = taxRegionTypeName;
-	}
+    public void setTaxRegionTypeName(String taxRegionTypeName) {
+        this.taxRegionTypeName = taxRegionTypeName;
+    }
 
-	@Override
+    @Override
     protected LinkedHashMap toStringMapper() {
         // TODO Auto-generated method stub
         return null;

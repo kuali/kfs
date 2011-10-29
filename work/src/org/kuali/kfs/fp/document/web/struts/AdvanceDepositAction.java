f8d29017d90d6111e145f2b56f7c5300805a3f9e
Ying	Feng <yingfeng@iu.edu>
2011-10-29 04:45:29 +0000

    KFSMI-6396

--- work/src/org/kuali/kfs/fp/document/web/struts/AdvanceDepositAction.java
+++ work/src/org/kuali/kfs/fp/document/web/struts/AdvanceDepositAction.java
@@ -16,6 +16,7 @@
 package org.kuali.kfs.fp.document.web.struts;
 
 import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,11 +26,13 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.kuali.kfs.fp.businessobject.AdvanceDepositDetail;
 import org.kuali.kfs.fp.document.AdvanceDepositDocument;
+import org.kuali.kfs.fp.document.CapitalAccountingLinesDocumentBase;
 import org.kuali.kfs.fp.document.validation.impl.AdvanceDepositDocumentRuleUtil;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.rice.kns.util.GlobalVariables;
 import org.kuali.rice.kns.util.KualiDecimal;
+import org.kuali.rice.kns.util.ObjectUtils;
 
 /**
  * This is the action class for the Advance Deposit document.
@@ -56,6 +59,30 @@ public class AdvanceDepositAction extends CapitalAccountingLinesActionBase {
         return super.execute(mapping, form, request, response);
     }
 
+    /**
+     * Overridden to ensure that the nested AdvanceDepositDocuments in AdvanceDepositDetails are populated.
+     *  
+     * @see org.kuali.rice.kns.web.struts.action.KualiTransactionalDocumentActionBase#copy(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward copy(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        AdvanceDepositForm adForm = (AdvanceDepositForm) form;
+        AdvanceDepositDocument adDoc = adForm.getAdvanceDepositDocument();
+        List<AdvanceDepositDetail> advanceDeposits = adDoc.getAdvanceDeposits();
+        
+        for (AdvanceDepositDetail advanceDeposit: advanceDeposits) {
+            /* somehow ObjectUtils.isNull(nestAdDoc) returns false here
+            AdvanceDepositDocument nestAdDoc = advanceDeposit.getAdvanceDepositDocument();
+            if (ObjectUtils.isNull(nestAdDoc)) {
+                nestAdDoc = null;
+            }
+            */
+            // need to populate the referred document, otherwise there will be NPE when copying
+            advanceDeposit.setAdvanceDepositDocument(adDoc);
+        }
+        return super.copy(mapping, form, request, response);
+    }
+    
     /**
      * Adds a AdvanceDepositDetail instance created from the current "new advanceDeposit" line to the document
      * 

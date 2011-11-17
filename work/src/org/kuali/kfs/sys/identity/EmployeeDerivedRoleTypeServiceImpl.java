828f42044bd2d126a631be98ed6e2ba927adb14c
Winston Dang <wkd@hawaii.edu>
2011-11-17 21:39:32 +0000

    KFSMI-7110

--- work/src/org/kuali/kfs/sys/identity/EmployeeDerivedRoleTypeServiceImpl.java
+++ work/src/org/kuali/kfs/sys/identity/EmployeeDerivedRoleTypeServiceImpl.java
@@ -63,14 +63,15 @@ public class EmployeeDerivedRoleTypeServiceImpl extends DerivedRoleTypeServiceBa
         if (StringUtils.isBlank(principalId)) {
             return false;
         }
+        
         EntityDefault entity = getIdentityManagementService().getEntityDefaultInfoByPrincipalId(principalId);
         if ((entity == null) || (entity.getEmployment() == null)) {
             return false;
         }
-        if (!entity.isActive() || !entity.getEmployment().isActive() || !ACTIVE_EMPLOYEE_STATUSES.contains(entity.getPrimaryEmployment().getEmployeeStatusCode())) {
+        if (!entity.isActive() || !entity.getEmployment().isActive() || !ACTIVE_EMPLOYEE_STATUSES.contains(entity.getEmployment().getEmployeeStatus().toString())) {
             return false;
         }
-        if ((KFSConstants.SysKimApiConstants.ACTIVE_PROFESSIONAL_EMPLOYEE_KIM_ROLE_NAME.equals(roleName) || KFSConstants.SysKimApiConstants.ACTIVE_PROFESSIONAL_EMPLOYEE_AND_KFS_USER_KIM_ROLE_NAME.equals(roleName)) && !PROFESSIONAL_EMPLOYEE_TYPE_CODE.equals(entity.getPrimaryEmployment().getEmployeeTypeCode())) {
+        if ((KFSConstants.SysKimApiConstants.ACTIVE_PROFESSIONAL_EMPLOYEE_KIM_ROLE_NAME.equals(roleName) || KFSConstants.SysKimApiConstants.ACTIVE_PROFESSIONAL_EMPLOYEE_AND_KFS_USER_KIM_ROLE_NAME.equals(roleName)) && !PROFESSIONAL_EMPLOYEE_TYPE_CODE.equals(entity.getEmployment().getEmployeeType().toString())) {
             return false;
         }
         if ((KFSConstants.SysKimApiConstants.ACTIVE_PROFESSIONAL_EMPLOYEE_AND_KFS_USER_KIM_ROLE_NAME.equals(roleName) || KFSConstants.SysKimApiConstants.ACTIVE_EMPLOYEE_AND_KFS_USER_KIM_ROLE_NAME.equals(roleName))) {

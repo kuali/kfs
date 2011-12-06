3964bb012a025c20e967a15fe17e06e8a6da914c
Jonathan Keller <keller.jonathan@gmail.com>
2011-12-06 00:10:26 +0000

    Final cleanup to get KFS Running.

--- work/src/org/kuali/kfs/sys/document/dataaccess/impl/FinancialSystemDocumentHeaderDaoOjb.java
+++ work/src/org/kuali/kfs/sys/document/dataaccess/impl/FinancialSystemDocumentHeaderDaoOjb.java
@@ -25,21 +25,17 @@ import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.kfs.sys.businessobject.FinancialSystemDocumentHeader;
 import org.kuali.kfs.sys.document.dataaccess.FinancialSystemDocumentHeaderDao;
+import org.kuali.rice.core.api.util.RiceConstants;
 import org.kuali.rice.krad.bo.DocumentHeader;
 import org.kuali.rice.krad.dao.impl.DocumentHeaderDaoOjb;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.rice.krad.util.KRADPropertyConstants;
 
 /**
  * This class is the financial system document header dao ojb implementation
  */
 public class FinancialSystemDocumentHeaderDaoOjb extends DocumentHeaderDaoOjb implements FinancialSystemDocumentHeaderDao {
-    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(FinancialSystemDocumentHeaderDaoOjb.class);
 
-    /**
-     * Constructs a FinancialSystemDocumentHeaderDaoOjb.java.
-     */
-    public FinancialSystemDocumentHeaderDaoOjb() {
-        super();
-    }
 
     /*
      * (non-Javadoc)
@@ -53,26 +49,4 @@ public class FinancialSystemDocumentHeaderDaoOjb extends DocumentHeaderDaoOjb im
         return (DocumentHeader) getPersistenceBrokerTemplate().getObjectByQuery(QueryFactory.newQuery(getDocumentHeaderBaseClass(), correctedByCriteria));
     }
 
-    /**
-     * @see org.kuali.rice.krad.dao.DocumentHeaderDao#getByDocumentFinalDate(Date documentFinalDate)
-     */
-    public Collection getByDocumentFinalDate(Date documentFinalDate) {
-        Criteria criteria = new Criteria();
-        criteria.addEqualTo(KFSPropertyConstants.DOCUMENT_FINAL_DATE, documentFinalDate);
-        return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(getDocumentHeaderBaseClass(), criteria));
-    }
-    
-    public Class getDocumentHeaderBaseClass() {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Method getDocumentHeaderBaseClass() returning class " + FinancialSystemDocumentHeader.class.getName());
-        }
-        return FinancialSystemDocumentHeader.class;
-    }
-    
-    public Collection getByDocumentNumbers(List documentNumbers) {
-        Criteria criteria = new Criteria();
-        criteria.addIn(KFSConstants.CustomerOpenItemReport.DOCUMENT_NUMBER, documentNumbers);
-        
-        return getPersistenceBrokerTemplate().getCollectionByQuery(QueryFactory.newQuery(getDocumentHeaderBaseClass(), criteria));   
-    }
 }

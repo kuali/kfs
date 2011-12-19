f1e85070e767426f5984c69c392594cf53e30f27
David Yun <davidyun@umd.edu>
2011-12-19 20:37:02 +0000

    KFSMI-4417: added more columns

--- work/src/org/kuali/kfs/module/ar/businessobject/CustomerInvoiceLookup.java
+++ work/src/org/kuali/kfs/module/ar/businessobject/CustomerInvoiceLookup.java
@@ -15,6 +15,7 @@
  */
 package org.kuali.kfs.module.ar.businessobject;
 
+import java.sql.Date;
 import java.util.LinkedHashMap;
 
 import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
@@ -22,64 +23,11 @@ import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
 public class CustomerInvoiceLookup extends PersistableBusinessObjectBase {
     
     protected String invoiceNumber;
-    protected String customerName;
-    
-    /*
-    protected FinancialSystemDocumentHeader documentHeader;
-    
-    protected String invoiceHeaderText;
-    protected String invoiceAttentionLineText;
+    protected String customerName;    
     protected Date invoiceDueDate;
     protected Date billingDate;
-    protected Date closedDate;
-    protected Date billingDateForDisplay;
-    protected String invoiceTermsText;
-    protected String organizationInvoiceNumber;
-    protected String customerPurchaseOrderNumber;
-    protected String printInvoiceIndicator;
-    protected Date customerPurchaseOrderDate;
     protected String billByChartOfAccountCode;
     protected String billedByOrganizationCode;
-    protected Integer customerShipToAddressIdentifier;
-    protected Integer customerBillToAddressIdentifier;
-    protected String customerSpecialProcessingCode;
-    protected boolean customerRecordAttachmentIndicator;
-    protected boolean openInvoiceIndicator;
-    protected String paymentChartOfAccountsCode;
-    protected String paymentAccountNumber;
-    protected String paymentSubAccountNumber;
-    protected String paymentFinancialObjectCode;
-    protected String paymentFinancialSubObjectCode;
-    protected String paymentProjectCode;
-    protected String paymentOrganizationReferenceIdentifier;
-    protected Date printDate;
-    protected Integer age;
-    
-    protected String billingAddressName;
-    protected String billingCityName;
-    protected String billingStateCode;
-    protected String billingZipCode;
-    protected String billingCountryCode;
-    protected String billingAddressInternationalProvinceName;
-    protected String billingInternationalMailCode;
-    protected String billingEmailAddress;
-    protected String billingAddressTypeCode;
-    protected String billingLine1StreetAddress;
-    protected String billingLine2StreetAddress;
-    protected String shippingLine1StreetAddress;
-    protected String shippingLine2StreetAddress;
-    protected String shippingAddressName;
-    protected String shippingCityName;
-    protected String shippingStateCode;
-    protected String shippingZipCode;
-    protected String shippingCountryCode;
-    protected String shippingAddressInternationalProvinceName;
-    protected String shippingInternationalMailCode;
-    protected String shippingEmailAddress;
-    protected String shippingAddressTypeCode;
-    protected boolean recurredInvoiceIndicator;
-    protected Date reportedDate;
-    */
     
     /**
      * @see org.kuali.rice.kns.bo.BusinessObjectBase#toStringMapper()
@@ -91,16 +39,6 @@ public class CustomerInvoiceLookup extends PersistableBusinessObjectBase {
         return m;
     }
     
-    public int hashCode() {
-        int hashCode = 0;
-
-        if (getInvoiceNumber() != null) {
-            hashCode = getInvoiceNumber().hashCode();
-        }
-
-        return hashCode;
-    }
-
     /**
      * 
      */
@@ -129,6 +67,60 @@ public class CustomerInvoiceLookup extends PersistableBusinessObjectBase {
         this.invoiceNumber = invoiceNumber;
     }
 
+    /**
+     * 
+     */
+    public Date getInvoiceDueDate() {
+        return invoiceDueDate;
+    }
 
+    /**
+     * 
+     */
+    public void setInvoiceDueDate(Date invoiceDueDate) {
+        this.invoiceDueDate = invoiceDueDate;
+    }
+
+    /**
+     * 
+     */
+    public Date getBillingDate() {
+        return billingDate;
+    }
+
+    /**
+     * 
+     */
+    public void setBillingDate(Date billingDate) {
+        this.billingDate = billingDate;
+    }
+
+    /**
+     * 
+     */
+    public String getBillByChartOfAccountCode() {
+        return billByChartOfAccountCode;
+    }
+
+    /**
+     * 
+     */
+    public void setBillByChartOfAccountCode(String billByChartOfAccountCode) {
+        this.billByChartOfAccountCode = billByChartOfAccountCode;
+    }
+
+    /**
+     * 
+     */
+    public String getBilledByOrganizationCode() {
+        return billedByOrganizationCode;
+    }
+
+    /**
+     * 
+     */
+    public void setBilledByOrganizationCode(String billedByOrganizationCode) {
+        this.billedByOrganizationCode = billedByOrganizationCode;
+    }
     
 }

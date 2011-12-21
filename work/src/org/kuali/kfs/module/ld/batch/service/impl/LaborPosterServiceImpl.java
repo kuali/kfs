c41ae240106ed91c4fd1cfb1c3b44a1d06b8a581
Bob	Campbell <rcampbell@rsmart.com>
2011-12-21 18:47:50 +0000

    KFSMI-5761 fixing labor poster record bypass issue

--- work/src/org/kuali/kfs/module/ld/batch/service/impl/LaborPosterServiceImpl.java
+++ work/src/org/kuali/kfs/module/ld/batch/service/impl/LaborPosterServiceImpl.java
@@ -353,7 +353,7 @@ public class LaborPosterServiceImpl implements LaborPosterService {
 
     protected void writeLaborGLEntry(LaborOriginEntry laborOriginEntry, LaborLedgerUnitOfWork laborLedgerUnitOfWork, Date runDate, int lineNumber, Map<String, Integer> glEntryReportSummary) {
         LaborOriginEntry summarizedEntry = summarizeLaborGLEntries(laborOriginEntry, laborLedgerUnitOfWork, runDate, lineNumber, glEntryReportSummary);
-        if (summarizedEntry == null) {
+        if (summarizedEntry == null || laborOriginEntry == null) { 
             return;
         }
 

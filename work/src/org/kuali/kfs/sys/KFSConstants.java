df846d0c61efe71b96138c16358a6173c5683226
Muddu	Salem <mksalem@umd.edu>
2012-01-04 16:51:58 +0000

    KFSMI-7106: FP: 1 => Distribute cost evenly but in CAM it is 2 => Distribute cost evenly 2 => Distribute cost by amount but in CAM it is 1 => Distribute cost by amount.
    The codes have been now matched from FP to CAM side.

--- work/src/org/kuali/kfs/sys/KFSConstants.java
+++ work/src/org/kuali/kfs/sys/KFSConstants.java
@@ -1266,9 +1266,9 @@ public class KFSConstants extends JSTLConstants implements ParameterKeyConstants
         public static final String CREATE_CAPITAL_ASSETS_TAB_TITLE = "Create Capital Assets";
         public static final String MODIFY_CAPITAL_ASSETS_TAB_TITLE = "Modify Capital Assets";
         
-        public static final String DISTRIBUTE_COST_EQUALLY_CODE = "1";
+        public static final String DISTRIBUTE_COST_EQUALLY_CODE = "2";
         public static final String DISTRIBUTE_COST_EQUALLY_DESCRIPTION = "Distribute cost evenly";
-        public static final String DISTRIBUTE_COST_BY_INDIVIDUAL_ASSET_AMOUNT_CODE = "2";
+        public static final String DISTRIBUTE_COST_BY_INDIVIDUAL_ASSET_AMOUNT_CODE = "1";
         public static final String DISTRIBUTE_COST_BY_INDIVIDUAL_ASSET_AMOUNT_DESCRIPTION = "Distribute cost by amount";
         
         public static final Integer PERCENT_SCALE = new Integer(2);

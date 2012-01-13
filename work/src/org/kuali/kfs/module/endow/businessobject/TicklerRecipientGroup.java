2687424b368b992bafa8343b9c873ee3ae8bbe0a
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-13 21:44:15 +0000

    JIRA KFSMI-7195 Endow Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/endow/businessobject/TicklerRecipientGroup.java
+++ work/src/org/kuali/kfs/module/endow/businessobject/TicklerRecipientGroup.java
@@ -20,6 +20,7 @@ import java.util.LinkedHashMap;
 import org.kuali.kfs.module.endow.EndowPropertyConstants;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.core.api.mo.common.active.Inactivatable;
+import org.kuali.rice.kim.api.group.Group;
 import org.kuali.rice.kim.api.group.GroupService;
 import org.kuali.rice.kim.api.services.IdentityManagementService;
 import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;

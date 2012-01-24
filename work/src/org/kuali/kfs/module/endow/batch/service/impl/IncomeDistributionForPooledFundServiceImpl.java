53e06a233f9f29a4498678db43b46526f012eef7
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-24 17:20:26 +0000

    JIRA KFSMI-7195 Endow Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/endow/batch/service/impl/IncomeDistributionForPooledFundServiceImpl.java
+++ work/src/org/kuali/kfs/module/endow/batch/service/impl/IncomeDistributionForPooledFundServiceImpl.java
@@ -50,7 +50,7 @@ import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.coreservice.framework.parameter.ParameterService;
 import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.kns.service.TransactionalDocumentDictionaryService;
-import org.kuali.rice.krad.rule.event.RouteDocumentEvent;
+import org.kuali.rice.krad.rules.rule.event.RouteDocumentEvent;
 import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.service.DocumentService;
 import org.kuali.rice.krad.service.KualiRuleService;

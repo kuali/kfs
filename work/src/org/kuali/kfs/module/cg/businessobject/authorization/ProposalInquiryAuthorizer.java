e3915d2d4f1e1b444c1f1c714f93b4dc2aefaa6d
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-24 17:12:21 +0000

    JIRA KFSMI-7176 CG Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/cg/businessobject/authorization/ProposalInquiryAuthorizer.java
+++ work/src/org/kuali/kfs/module/cg/businessobject/authorization/ProposalInquiryAuthorizer.java
@@ -18,7 +18,7 @@ package org.kuali.kfs.module.cg.businessobject.authorization;
 import java.util.Set;
 
 import org.kuali.kfs.module.cg.CGConstants;
-import org.kuali.rice.krad.inquiry.InquiryAuthorizerBase;
+import org.kuali.rice.kns.inquiry.InquiryAuthorizerBase;
 
 public class ProposalInquiryAuthorizer extends InquiryAuthorizerBase{
 

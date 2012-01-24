de5e4253cd6739767e9e071164b6163afb9d2df0
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-24 19:28:16 +0000

    JIRA KFSMI-7208 Unit Test Rice 2.0 Conversion

--- test/unit/src/org/kuali/kfs/module/cab/batch/service/BatchTestBase.java
+++ test/unit/src/org/kuali/kfs/module/cab/batch/service/BatchTestBase.java
@@ -42,7 +42,7 @@ import org.kuali.kfs.module.cab.fixture.RequisitionItemFixture;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.core.api.datetime.DateTimeService;
-import org.kuali.rice.core.api.parameter.Parameter;
+import org.kuali.rice.coreservice.api.parameter.Parameter;
 import org.kuali.rice.coreservice.framework.parameter.ParameterService;
 import org.kuali.rice.kns.util.DateUtils;
 import org.kuali.rice.krad.util.ObjectUtils;

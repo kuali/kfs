291b5dd1a02328b6531dda2f7624820e3af30bfa
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-25 19:27:12 +0000

    JIRA KFSMI-7195 Endow Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/endow/document/EndowmentToGLTransferOfFundsDocument.java
+++ work/src/org/kuali/kfs/module/endow/document/EndowmentToGLTransferOfFundsDocument.java
@@ -15,7 +15,10 @@
  */
 package org.kuali.kfs.module.endow.document;
 
+import java.util.List;
+
 import org.kuali.kfs.module.endow.EndowConstants;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
 import org.kuali.kfs.sys.document.AmountTotaling;
 
 public class EndowmentToGLTransferOfFundsDocument extends EndowmentAccountingLinesDocumentBase implements AmountTotaling {
@@ -27,5 +30,17 @@ public class EndowmentToGLTransferOfFundsDocument extends EndowmentAccountingLin
         super();
         setTransactionSubTypeCode(EndowConstants.TransactionSubTypeCode.CASH);
         initializeSubType();
+    }
+
+    @Override
+    public void setSourceTransactionLines(List<EndowmentTransactionLine> sourceLines) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setTargetTransactionLines(List<EndowmentTransactionLine> targetLines) {
+        // TODO Auto-generated method stub
+        
     }
 }

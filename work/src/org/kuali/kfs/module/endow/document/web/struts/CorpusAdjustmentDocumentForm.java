4408d7c3cba830830363915ae012427b41906524
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-25 19:24:04 +0000

    JIRA KFSMI-7195 Endow Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/endow/document/web/struts/CorpusAdjustmentDocumentForm.java
+++ work/src/org/kuali/kfs/module/endow/document/web/struts/CorpusAdjustmentDocumentForm.java
@@ -21,7 +21,7 @@ import org.kuali.kfs.module.endow.EndowConstants;
 import org.kuali.kfs.module.endow.document.CorpusAdjustmentDocument;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kns.service.DataDictionaryService;
-import org.kuali.rice.krad.service.BusinessObjectDictionaryService;
+import org.kuali.rice.kns.service.BusinessObjectDictionaryService;
 
 public class CorpusAdjustmentDocumentForm extends EndowmentTransactionLinesDocumentFormBase {
 

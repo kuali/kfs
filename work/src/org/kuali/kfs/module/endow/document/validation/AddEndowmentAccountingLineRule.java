1964509fb30eb89afd052bb89624cadab29c187e
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-25 19:19:51 +0000

    JIRA KFSMI-7195 Endow Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/endow/document/validation/AddEndowmentAccountingLineRule.java
+++ work/src/org/kuali/kfs/module/endow/document/validation/AddEndowmentAccountingLineRule.java
@@ -17,7 +17,7 @@ package org.kuali.kfs.module.endow.document.validation;
 
 import org.kuali.kfs.module.endow.businessobject.EndowmentAccountingLine;
 import org.kuali.kfs.module.endow.document.EndowmentAccountingLinesDocument;
-import org.kuali.rice.krad.rule.BusinessRule;
+import org.kuali.rice.krad.rules.rule.BusinessRule;
 
 public interface AddEndowmentAccountingLineRule<E extends EndowmentAccountingLinesDocument, D extends EndowmentAccountingLine> extends BusinessRule {
 

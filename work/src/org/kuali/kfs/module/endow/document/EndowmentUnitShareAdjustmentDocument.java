568fdcc86ea20925614b0aabc914e69fa4917ce7
Sravani Gogineni <sravani@ais.msu.edu>
2012-01-25 19:31:41 +0000

    JIRA KFSMI-7195 Endow Module Rice 2.0 Conversion

--- work/src/org/kuali/kfs/module/endow/document/EndowmentUnitShareAdjustmentDocument.java
+++ work/src/org/kuali/kfs/module/endow/document/EndowmentUnitShareAdjustmentDocument.java
@@ -15,10 +15,13 @@
  */
 package org.kuali.kfs.module.endow.document;
 
+import java.util.List;
+
 import org.apache.cxf.common.util.StringUtils;
 import org.kuali.kfs.module.endow.EndowConstants.TransactionSourceTypeCode;
 import org.kuali.kfs.module.endow.EndowConstants.TransactionSubTypeCode;
 import org.kuali.kfs.module.endow.businessobject.EndowmentSourceTransactionSecurity;
+import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionLine;
 import org.kuali.kfs.module.endow.businessobject.EndowmentTransactionSecurity;
 import org.kuali.kfs.sys.document.AmountTotaling;
 
@@ -91,6 +94,18 @@ public class EndowmentUnitShareAdjustmentDocument extends EndowmentTaxLotLinesDo
         }
         return this.sourceTransactionSecurity;
 
+    }
+
+    @Override
+    public void setSourceTransactionLines(List<EndowmentTransactionLine> sourceLines) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setTargetTransactionLines(List<EndowmentTransactionLine> targetLines) {
+        // TODO Auto-generated method stub
+        
     }
 
 }

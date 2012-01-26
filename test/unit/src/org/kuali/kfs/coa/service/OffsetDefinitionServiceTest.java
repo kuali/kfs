6124e7903e211b2022685ccd31fc2f292fc15809
Jonathan Keller <keller.jonathan@gmail.com>
2012-01-26 21:02:41 +0000

    Updating test to find problem.

--- test/unit/src/org/kuali/kfs/coa/service/OffsetDefinitionServiceTest.java
+++ test/unit/src/org/kuali/kfs/coa/service/OffsetDefinitionServiceTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2005 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,11 +30,11 @@ public class OffsetDefinitionServiceTest extends KualiTestBase {
     public void testValidateAccount() {
         OffsetDefinition offsetDefinition = null;
         offsetDefinition = SpringContext.getBean(OffsetDefinitionService.class).getByPrimaryId(TestUtils.getFiscalYearForTesting(), "BA", "IB", "AC");
-        assertNotNull("offset object code not found", offsetDefinition.getFinancialObject());
-        assertEquals("offset object code should have been 8000", "8000", offsetDefinition.getFinancialObject().getFinancialObjectCode());
+        assertNotNull("offset object code not found in FY: " + TestUtils.getFiscalYearForTesting(), offsetDefinition.getFinancialObject());
+        assertEquals("offset object code should have been 8000 in FY: " + TestUtils.getFiscalYearForTesting(), "8000", offsetDefinition.getFinancialObject().getFinancialObjectCode());
 
         offsetDefinition = null;
         offsetDefinition = SpringContext.getBean(OffsetDefinitionService.class).getByPrimaryId(TestUtils.getFiscalYearForTesting(), "XX", "XX", "XX");
-        assertNull(offsetDefinition);
+        assertNull("No offset definition should have been retrieved: " + offsetDefinition,offsetDefinition);
     }
 }

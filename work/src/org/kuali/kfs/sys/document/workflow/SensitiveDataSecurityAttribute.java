ca262d78fca94cb8e07a9bcd4516959388778ac7
Jonathan Keller <keller.jonathan@gmail.com>
2012-02-02 00:48:40 +0000

    Fix for sensitive data workflow attribute.

--- work/src/org/kuali/kfs/sys/document/workflow/SensitiveDataSecurityAttribute.java
+++ work/src/org/kuali/kfs/sys/document/workflow/SensitiveDataSecurityAttribute.java
@@ -20,19 +20,22 @@ import java.util.List;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.document.datadictionary.FinancialSystemTransactionalDocumentEntry;
 import org.kuali.rice.kew.api.document.WorkflowDocumentService;
+import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.kew.framework.document.security.DocumentSecurityAttribute;
 import org.kuali.rice.kim.api.services.KimApiServiceLocator;
 import org.kuali.rice.kns.service.DataDictionaryService;
 import org.kuali.rice.kns.service.DocumentHelperService;
 import org.kuali.rice.krad.datadictionary.DocumentEntry;
 import org.kuali.rice.krad.document.DocumentAuthorizer;
+import org.kuali.rice.krad.service.KRADServiceLocator;
+import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
 
 /**
  * This class...
  */
 public class SensitiveDataSecurityAttribute implements DocumentSecurityAttribute {
-
-    private DocumentHelperService docHelperService = SpringContext.getBean(DocumentHelperService.class);
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(SensitiveDataSecurityAttribute.class);
+    
 
     @Override
     public boolean isAuthorizedForDocument(String principalId, org.kuali.rice.kew.api.document.Document document) {
@@ -47,8 +50,14 @@ public class SensitiveDataSecurityAttribute implements DocumentSecurityAttribute
                     List<String> sensitiveDataCode = sensitiveDataCodeArray;
                     if ( sensitiveDataCode != null && sensitiveDataCode.contains("Y")) {
 
-                        DocumentAuthorizer docAuthorizer = docHelperService.getDocumentAuthorizer(docTypeName);
-                        return docAuthorizer.canOpen(document, KimApiServiceLocator.getPersonService().getPerson(principalId));
+                        DocumentAuthorizer docAuthorizer = SpringContext.getBean(DocumentHelperService.class).getDocumentAuthorizer(docTypeName);
+                        try {
+                            return docAuthorizer.canOpen(KRADServiceLocatorWeb.getDocumentService().getByDocumentHeaderIdSessionless(document.getDocumentId()), KimApiServiceLocator.getPersonService().getPerson(principalId));
+                        }
+                        catch (WorkflowException ex) {
+                            LOG.error( "Exception while testing if user can open document: " + document, ex);
+                            return false;
+                        }
                     }
                 }
             }

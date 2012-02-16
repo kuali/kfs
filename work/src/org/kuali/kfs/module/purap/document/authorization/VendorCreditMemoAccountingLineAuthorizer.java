1e9bd931c9fcffe2a6a335a023ee0c041c71245d
Muddu	Salem <mksalem@umd.edu>
2012-02-16 21:07:29 +0000

    KFSMI-4522: Yes, it is that jira again.  Amount field needs to be visible on accounting lines in credit memo document.

---
+++ work/src/org/kuali/kfs/module/purap/document/authorization/VendorCreditMemoAccountingLineAuthorizer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2009 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.document.authorization;
+
+import java.util.Set;
+
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.businessobject.AccountingLine;
+import org.kuali.kfs.sys.document.AccountingDocument;
+import org.kuali.rice.kim.bo.Person;
+
+/**
+ * Accounting line authorizer for Requisition document which allows adding accounting lines at specified nodes
+ */
+public class VendorCreditMemoAccountingLineAuthorizer extends PurapAccountingLineAuthorizer {
+    private static final String INITIATOR_NODE = "Initiator";
+    private static final String CONTENT_REVIEW_NODE = "Organization";
+
+    /**
+     * Allow new lines to be rendered at Initiator node
+     * @see org.kuali.kfs.sys.document.authorization.AccountingLineAuthorizerBase#renderNewLine(org.kuali.kfs.sys.document.AccountingDocument, java.lang.String)
+     */
+    @Override
+    public boolean renderNewLine(AccountingDocument accountingDocument, String accountingGroupProperty) {
+        if (accountingDocument.getDocumentHeader().getWorkflowDocument().getCurrentRouteNodeNames().equals(VendorCreditMemoAccountingLineAuthorizer.INITIATOR_NODE) || accountingDocument.getDocumentHeader().getWorkflowDocument().getCurrentRouteNodeNames().equals(VendorCreditMemoAccountingLineAuthorizer.CONTENT_REVIEW_NODE)) return true;
+        return super.renderNewLine(accountingDocument, accountingGroupProperty);
+    }
+    
+    /**
+     * @see org.kuali.kfs.sys.document.authorization.AccountingLineAuthorizerBase#getUnviewableBlocks(org.kuali.kfs.sys.document.AccountingDocument, org.kuali.kfs.sys.businessobject.AccountingLine, boolean, org.kuali.rice.kim.bo.Person)
+     */
+    @Override
+    public Set<String> getUnviewableBlocks(AccountingDocument accountingDocument, AccountingLine accountingLine, boolean newLine, Person currentUser) {
+        Set<String> unviewableBlocks = super.getUnviewableBlocks(accountingDocument, accountingLine, newLine, currentUser);
+        unviewableBlocks.remove(KFSPropertyConstants.PERCENT);
+        unviewableBlocks.remove(KFSPropertyConstants.AMOUNT);
+
+        return unviewableBlocks;
+    }
+    
+}

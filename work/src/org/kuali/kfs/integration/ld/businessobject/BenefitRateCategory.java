e80c323876873a808750f5596f8df6ff88647457
Ryan Suematsu <rms@usc.edu>
2012-02-17 23:38:17 +0000

    KFSMI-7255

--- work/src/org/kuali/kfs/integration/ld/businessobject/BenefitRateCategory.java
+++ work/src/org/kuali/kfs/integration/ld/businessobject/BenefitRateCategory.java
@@ -98,7 +98,6 @@ public class BenefitRateCategory implements LaborBenefitRateCategory {
         
     }
 
-    @Override
     public void prepareForWorkflow() {
         // TODO Auto-generated method stub
         

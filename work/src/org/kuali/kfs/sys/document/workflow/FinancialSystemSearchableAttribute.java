e82c476bd277d1ef88fc6eb35a0f02d7d16a61db
Jonathan Keller <keller.jonathan@gmail.com>
2012-03-02 00:14:03 +0000

    Removed customization - not needed.

--- work/src/org/kuali/kfs/sys/document/workflow/FinancialSystemSearchableAttribute.java
+++ work/src/org/kuali/kfs/sys/document/workflow/FinancialSystemSearchableAttribute.java
@@ -15,45 +15,34 @@
  */
 package org.kuali.kfs.sys.document.workflow;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.kuali.kfs.coa.businessobject.Chart;
-import org.kuali.kfs.sys.KFSPropertyConstants;
-import org.kuali.rice.core.api.uif.RemotableAbstractWidget;
-import org.kuali.rice.core.api.uif.RemotableAttributeField;
-import org.kuali.rice.core.api.uif.RemotableQuickFinder;
-import org.kuali.rice.kew.api.extension.ExtensionDefinition;
-import org.kuali.rice.kns.lookup.LookupUtils;
 import org.kuali.rice.krad.workflow.attribute.DataDictionarySearchableAttribute;
 
 //RICE20 This class needs to be fixed to support pre-rice2.0 features
 public class FinancialSystemSearchableAttribute extends DataDictionarySearchableAttribute {
 
-    @Override
-    public List<RemotableAttributeField> getSearchFields(ExtensionDefinition extensionDefinition, String documentTypeName) {
-        // TODO Auto-generated method stub
-        List<RemotableAttributeField> orignalFields = super.getSearchFields(extensionDefinition, documentTypeName);
-        List<RemotableAttributeField> fields = new ArrayList<RemotableAttributeField>( orignalFields.size() );
-        for ( RemotableAttributeField field : orignalFields ) {
-            if ( field.getName().equals(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE) ) {
-                RemotableAttributeField.Builder newField = RemotableAttributeField.Builder.create(field);
-                RemotableQuickFinder.Builder qf = RemotableQuickFinder.Builder.create(LookupUtils.getBaseLookupUrl(false), Chart.class.getName() );
-                Collection<RemotableAbstractWidget.Builder> widgets = new ArrayList<RemotableAbstractWidget.Builder>(1);
-                widgets.add(qf);
-                newField.setWidgets( widgets );
-                fields.add( newField.build() );
-            } else {
-                fields.add(field);
-            }
-
-            // RICE20: other fields
-            // RICE20: add field conversions to widget
-
-        }
-        return fields;
-    }
+//    @Override
+//    public List<RemotableAttributeField> getSearchFields(ExtensionDefinition extensionDefinition, String documentTypeName) {
+//        // TODO Auto-generated method stub
+//        List<RemotableAttributeField> orignalFields = super.getSearchFields(extensionDefinition, documentTypeName);
+//        List<RemotableAttributeField> fields = new ArrayList<RemotableAttributeField>( orignalFields.size() );
+//        for ( RemotableAttributeField field : orignalFields ) {
+//            if ( field.getName().equals(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE) ) {
+//                RemotableAttributeField.Builder newField = RemotableAttributeField.Builder.create(field);
+//                RemotableQuickFinder.Builder qf = RemotableQuickFinder.Builder.create(LookupUtils.getBaseLookupUrl(false), Chart.class.getName() );
+//                Collection<RemotableAbstractWidget.Builder> widgets = new ArrayList<RemotableAbstractWidget.Builder>(1);
+//                widgets.add(qf);
+//                newField.setWidgets( widgets );
+//                fields.add( newField.build() );
+//            } else {
+//                fields.add(field);
+//            }
+//
+//            // RICE20: other fields
+//            // RICE20: add field conversions to widget
+//
+//        }
+//        return fields;
+//    }
 
 
 //    @Override

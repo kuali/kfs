ef5b7efff855c9b4fa3a811f98422f27d1ebc0f4
Jonathan Keller <keller.jonathan@gmail.com>
2012-03-13 17:24:50 +0000

    Moved DevelopmentLoginFilter into a KFS package.

--- work/src/org/kuali/kfs/sys/web/filter/DevelopmentLoginFilter.java
+++ work/src/org/kuali/kfs/sys/web/filter/DevelopmentLoginFilter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.kns.web.filter;
+package org.kuali.kfs.sys.web.filter;
 
 import java.io.IOException;
 

50015f684f663a747330b1a5b779a5c9fdd458de
Jonathan Keller <keller.jonathan@gmail.com>
2012-03-22 20:07:31 +0000

    Fixed remoting service URL

--- start-dev2-server.sh
+++ start-dev2-server.sh
@@ -2,9 +2,14 @@ pushd build/tomcat
 mkdir -p -v conf/Catalina/localhost
 rm conf/Catalina/localhost/*.xml
 cp conf/kr-dev2.xml conf/Catalina/localhost
-export CATALINA_OPTS="-Xmx3g -Xms512m -XX:MaxPermSize=256m -Djava.awt.headless=true -Dadditional.config.locations=${PWD}/conf/rice-config.xml"
+rm -rf logs/*
+export JAVA_TOOL_OPTIONS=
+export CATALINA_OPTS="-Dxxxxxxx=kfs-dev2 -Xmx3g -Xms512m -XX:MaxPermSize=256m -Djava.awt.headless=true -Dadditional.config.locations=${PWD}/conf/rice-config.xml"
 ./bin/startup.sh
 sleep 10
 cp conf/kfs-dev2.xml conf/Catalina/localhost
+set -x
 tail -F logs/catalina.out
+set +x
+ps -Af | grep "Dxxxxxxx=kfs-dev2" | grep -v grep | cut -c 7-12 | xargs kill -9
 popd

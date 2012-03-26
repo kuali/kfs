b90fb6e9acd4ad41404321a8b56be32e88727392
Jonathan Keller <keller.jonathan@gmail.com>
2012-03-26 19:46:36 +0000

    Added debugging mode and correct server stop upon ctrl-C.

--- start-dev2-server.sh
+++ start-dev2-server.sh
@@ -10,14 +10,22 @@ cp -v conf/kr-dev2.xml conf/Catalina/localhost
 rm -rf logs/*
 export JAVA_TOOL_OPTIONS=
 export CATALINA_OPTS="-Dxxxxxxx=kfs-dev2 -DTOMCAT_DIR=$PWD -Xmx3g -Xms512m -XX:MaxPermSize=256m -Djava.awt.headless=true -Dadditional.config.locations=${PWD}/conf/rice-config.xml"
+if [[ "$1" == "-debug" ]]; then
+	export CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,suspend=n,server=y"
+fi
+
+trap stop_tomcat 1 2 3 15
+function stop_tomcat() {
+	echo "Attempting to Kill Tomcat server"
+	ps -Af | grep "Dxxxxxxx=kfs-dev2" | grep -v grep | cut -c 7-12 | xargs kill -9
+} # catch_sig()
+
+
 echo "Starting up Tomcat"
 ./bin/startup.sh
 sleep 10
 echo "Moving KFS descriptor into path"
 cp -v conf/kfs-dev2.xml conf/Catalina/localhost
-set +x
 tail -F logs/catalina.out
-set -x
-echo "Attempting to Kill Tomcat server"
-ps -Af | grep "Dxxxxxxx=kfs-dev2" | grep -v grep | cut -c 7-12 | xargs kill -9
+stop_tomcat
 popd

c3c6a39da91b81a0e7e7179f854d5a1f3a10bd30
Jonathan Keller <keller.jonathan@gmail.com>
2012-04-25 03:09:22 +0000

    Updated cache service to use new caching mechanisms.

--- work/src/org/kuali/kfs/sys/batch/service/impl/CacheServiceImpl.java
+++ work/src/org/kuali/kfs/sys/batch/service/impl/CacheServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,44 +17,25 @@ package org.kuali.kfs.sys.batch.service.impl;
 
 import org.kuali.kfs.sys.batch.service.CacheService;
 import org.kuali.kfs.sys.service.NonTransactional;
-import org.kuali.rice.coreservice.api.parameter.Parameter;
-import org.kuali.rice.coreservice.framework.parameter.ParameterService;
-import org.kuali.rice.kim.api.common.template.Template;
-import org.kuali.rice.kim.api.responsibility.Responsibility;
-import org.kuali.rice.kim.api.role.RoleService;
-import org.kuali.rice.kim.api.services.IdentityManagementService;
-import org.kuali.rice.location.framework.campus.CampusValuesFinder;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Caching;
+import org.kuali.rice.core.impl.services.CoreImplServiceLocator;
+import org.springframework.cache.CacheManager;
 
 /**
  * @see org.kuali.kfs.sys.batch.service.CacheService
  */
 @NonTransactional
 public class CacheServiceImpl implements CacheService {
-    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CacheServiceImpl.class);
 
     /**
      * @see org.kuali.kfs.sys.batch.service.CacheService#clearSystemCache()
      */
+    @Override
     public void clearSystemCache() {
-        clearMethodCache();
+        for ( CacheManager cm :  CoreImplServiceLocator.getCacheManagerRegistry().getCacheManagers() ) {
+            for ( String cacheName : cm.getCacheNames() ) {
+                cm.getCache(cacheName).clear();
+            }
+        }
     }
 
-    /**
-     * Clears out service methods cache by calling adminstrators flushAll
-     */
-    @CacheEvict(allEntries=true, value = { "" })
-    protected void clearMethodCache() {
-        LOG.info("clearing spring method cache ...");
-    }
-
-    /**
-     * Clears out parameter cache by calling flush method on parameter service
-     */
-    @CacheEvict(value={Parameter.Cache.NAME}, allEntries=true)
-    protected void clearParameterCache() {
-        LOG.info("clearing parameter cache ...");
-    }
-    
 }

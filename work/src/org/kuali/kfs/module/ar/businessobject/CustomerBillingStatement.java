5b62d183b3e02edd7282dedab1b78cb4d2ad0552
Jonathan Keller <keller.jonathan@gmail.com>
2012-05-05 00:05:54 +0000

    Code Cleanup.

--- work/src/org/kuali/kfs/module/ar/businessobject/CustomerBillingStatement.java
+++ work/src/org/kuali/kfs/module/ar/businessobject/CustomerBillingStatement.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@
 package org.kuali.kfs.module.ar.businessobject;
 
 import java.sql.Date;
-import java.util.LinkedHashMap;
 
 import org.kuali.kfs.module.ar.ArConstants;
 import org.kuali.kfs.sys.context.SpringContext;
@@ -26,19 +25,9 @@ import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
 
 public class CustomerBillingStatement extends PersistableBusinessObjectBase {
 
-    private String customerNumber;
-    private KualiDecimal previouslyBilledAmount;
-    private Date reportedDate;
-    
-    /**
-     * @see org.kuali.rice.krad.bo.BusinessObjectBase#toStringMapper()
-     */
-    
-    protected LinkedHashMap toStringMapper_RICE20_REFACTORME() {
-        LinkedHashMap<String, String> m = new LinkedHashMap<String, String>();
-        m.put("CUSTOMERNUMBER", customerNumber);
-        return m;
-    }
+    protected String customerNumber;
+    protected KualiDecimal previouslyBilledAmount;
+    protected Date reportedDate;
 
     /**
      * @return the customerNumber
@@ -81,23 +70,23 @@ public class CustomerBillingStatement extends PersistableBusinessObjectBase {
     public void setReportedDate(Date reportedDate) {
         this.reportedDate = reportedDate;
     }
- 
+
     /**
      * Gets the default statement format value
      * @return
      */
-    public String getStatementFormat() {        
+    public String getStatementFormat() {
         ParameterService parameterService = SpringContext.getBean(ParameterService.class);
-        return parameterService.getParameterValueAsString(CustomerBillingStatement.class, ArConstants.DEFAULT_FORMAT);       
+        return parameterService.getParameterValueAsString(CustomerBillingStatement.class, ArConstants.DEFAULT_FORMAT);
     }
-    
+
     /**
      * Gets the default include zero balance customer value
      * @return
      */
     public String getIncludeZeroBalanceCustomers() {
         ParameterService parameterService = SpringContext.getBean(ParameterService.class);
-        return parameterService.getParameterValueAsString(CustomerBillingStatement.class, ArConstants.INCLUDE_ZERO_BALANCE_CUSTOMERS);       
+        return parameterService.getParameterValueAsString(CustomerBillingStatement.class, ArConstants.INCLUDE_ZERO_BALANCE_CUSTOMERS);
     }
-    
+
 }

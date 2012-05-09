d57fe42eb2cbc8767dce19168b4cab04bfc6e85b
Cathy Tan <tanc@ais.msu.edu>
2012-05-09 18:17:12 +0000

    KFSMI-8056 : The document overview fields on an unapproved PO are not editable to the Purchasing Processor.

---
+++ work/src/org/kuali/kfs/module/purap/document/authorization/PurchaseOrderDocumentAuthorizerBase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2009 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.document.authorization;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.kfs.module.purap.businessobject.SensitiveData;
+import org.kuali.kfs.module.purap.document.PurchasingAccountsPayableDocument;
+import org.kuali.kfs.module.purap.identity.PurapKimAttributes;
+import org.kuali.kfs.module.purap.service.SensitiveDataService;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.authorization.AccountingDocumentAuthorizerBase;
+import org.kuali.rice.kim.api.KimConstants;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.krad.document.Document;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.rice.krad.util.ObjectUtils;
+
+public class PurchaseOrderDocumentAuthorizerBase extends PurchasingAccountsPayableTransactionalDocumentAuthorizerBase {
+
+    @Override
+    public boolean canEditDocumentOverview(Document document, Person user) {
+        // According to the requirement in KFSMI-8056, for PurchaseOrderDocument, the way it should work is :
+        // "any person who has edit ability should be able to edit the entire Document Overview tab".
+        // So I'm going to return the same value that is returned by canEdit method of the superclass.
+        return canEdit(document, user);
+    }
+
+}

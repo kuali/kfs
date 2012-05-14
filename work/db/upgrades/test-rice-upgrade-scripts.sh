aa37b5f2916ec44f2492fbc1272e3ae3b7728f4c
Jonathan Keller <keller.jonathan@gmail.com>
2012-05-14 21:49:52 +0000

    Script cleanup.

--- work/db/upgrades/test-rice-upgrade-scripts.sh
+++ work/db/upgrades/test-rice-upgrade-scripts.sh
@@ -12,30 +12,17 @@ if [[ -z "$WORKSPACE" ]]; then
 	ABSPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
 	WORKSPACE=`dirname $ABSPATH`
 	echo Working Directory: $WORKSPACE
-	PROJECT_DIR=$WORKSPACE/../../..
-	TEMP_DIR=$WORKSPACE/temp
-else
-	PROJECT_DIR=$WORKSPACE/${PROJECT_NAME:-kfs}
-	TEMP_DIR=$PROJECT_DIR/build/temp
 fi
+
 ORACLE_TEST_DB_URL=${ORACLE_TEST_DB_URL:-jdbc:oracle:thin:@oraclerds.kfs.kuali.org:1521:KFS}
 #MYSQL_TEST_DB_URL=jdbc:mysql://test.db.kfs.kuali.org:3306
 MYSQL_TEST_DB_URL=${MYSQL_TEST_DB_URL:-jdbc:mysql://localhost:3306}
-
-mkdir $TEMP_DIR
 
 # Ensure we are in the right directory
 cd $WORKSPACE
 
 # Parameters to job
-OLD_BRANCH_PATH=${OLD_BRANCH_PATH:-branches/release-4-1-1}
-UPGRADE_SCRIPT_DIR=$PROJECT_DIR/work/db/upgrades/${UPGRADE_SCRIPT_DIR:-4.1.1_5.0}
-
-# Other parameters
-SVNREPO=${SVNREPO:-https://svn.kuali.org/repos}
-BASE_SVN_DATA_PATH=$SVNREPO/kfs/legacy/cfg-dbs
-PRIOR_SVN_DATA_PATH=$BASE_SVN_DATA_PATH/$OLD_BRANCH_PATH
-PRIOR_SVN_KFS_PATH=$SVNREPO/kfs/$OLD_BRANCH_PATH
+UPGRADE_SCRIPT_DIR=$WORKSPACE/kfs/work/db/upgrades/${UPGRADE_SCRIPT_DIR:-4.1.1_5.0}
 
 DB_TYPE=${DB_TYPE:-MYSQL}
 DB_USER=${DB_USER:-dbtest}
@@ -74,23 +61,13 @@ EOF
 
 if [[ "$IMPORT_OLD_PROJECT" == "true" ]]; then
 	
-#	echo Obtaining OLD Data Project from $PRIOR_SVN_DATA_PATH
-	# Check out the old data project
-#	if [[ -d $WORKSPACE/old_data ]]; then
-#		svn -q revert -R $WORKSPACE/old_data
-#		svn -q switch $PRIOR_SVN_DATA_PATH $WORKSPACE/old_data
-#		svn -q update --non-interactive $WORKSPACE/old_data
-#	else
-#		svn -q co $PRIOR_SVN_DATA_PATH $WORKSPACE/old_data
-#	fi
-	
 	# Prepare a tomcat directory that can be written to
 	rm -rf $WORKSPACE/tomcat
 	mkdir -p $WORKSPACE/tomcat/common/lib
 	mkdir -p $WORKSPACE/tomcat/common/classes
 	
 	# Lower-case the table names in case we are running against MySQL on Amazon RDS
-	perl -pi -e 's/dbTable="([^"]*)"/dbTable="\U\1"/g' $TEMP_DIR/old_data/development/graphs/*.xml
+	perl -pi -e 's/dbTable="([^"]*)"/dbTable="\U\1"/g' $WORKSPACE/old_data/development/graphs/*.xml
 
 	(
 	cat <<-EOF
@@ -127,14 +104,15 @@ if [[ "$IMPORT_OLD_PROJECT" == "true" ]]; then
 		post.import.workflow.ingester.username.property=datasource.username
 		post.import.workflow.ingester.password.property=datasource.password
 		post.import.workflow.ingester.additional.command.line=-Ddatasource.ojb.platform=$OJB_PLATFORM \
-		-Dbase.directory=\${torque.output.dir} \
+		-Dbase.directory=$WORKSPACE \
+		-Dappserver.home=$WORKSPACE/tomcat
+		-Dexternal.config.directory=$WORKSPACE/opt		
 		-Dis.local.build= \
 		-Ddev.mode= \
 		-Drice.dev.mode=true \
 		-Drice.ksb.batch.mode=true \
 		-Ddont.filter.project.rice= \
-		-Ddont.filter.project.spring.ide=
-		
+		-Ddont.filter.project.spring.ide=		
 	EOF
 	) > $WORKSPACE/impex-build.properties
 
@@ -151,7 +129,7 @@ if [[ "$IMPORT_OLD_PROJECT" == "true" ]]; then
 	
 fi
 
-set $PROJECT_DIR/build/drivers/*.jar
+set $WORKSPACE/kfs/build/drivers/*.jar
 DRIVER_CLASSPATH=$(IFS=:; echo "$*")
 cd $WORKSPACE
 
@@ -174,7 +152,7 @@ fi
 
 # run the export target
 if [[ "$EXPORT_UPGRADED_PROJECT" == "true" ]]; then
-	mkdir -p $TEMP_DIR/upgraded_data
+	mkdir -p $WORKSPACE/upgraded_data
 	(
 	cat <<-EOF
 		export.torque.database.user=$DB_USER
@@ -182,7 +160,7 @@ if [[ "$EXPORT_UPGRADED_PROJECT" == "true" ]]; then
 		export.torque.database.password=$DB_PASSWORD
 
 		torque.project=kfs
-		torque.schema.dir=$TEMP_DIR/upgraded_data
+		torque.schema.dir=$WORKSPACE/upgraded_data
 		torque.sql.dir=\${torque.schema.dir}/sql
 		torque.output.dir=\${torque.schema.dir}/sql
 
@@ -192,20 +170,18 @@ if [[ "$EXPORT_UPGRADED_PROJECT" == "true" ]]; then
 		
 		export.table.name.filter=^(?!.*(_BKUP)).*\$
 	EOF
-	) > $TEMP_DIR/impex-build.properties
-	pushd $PROJECT_DIR/work/db/kfs-db/db-impex/impex
-	ant "-Dimpex.properties.file=$TEMP_DIR/impex-build.properties" jdbc-to-xml
+	) > $WORKSPACE/impex-build.properties
+	pushd $WORKSPACE/kfs/work/db/kfs-db/db-impex/impex
+	ant "-Dimpex.properties.file=$WORKSPACE/impex-build.properties" jdbc-to-xml
 	popd
-	cp $TEMP_DIR/upgraded_data/schema.xml $WORKSPACE/upgraded_schema.xml
+	cp $WORKSPACE/upgraded_data/schema.xml $WORKSPACE/upgraded_schema.xml
 fi
 
 # Compare the schema.xml files
 
 if [[ "$PERFORM_COMPARISON" == "true" ]]; then
 	cd $WORKSPACE
-	pushd $TEMP_DIR
-	cp $PROJECT_DIR/work/db/kfs-db/rice/schema.xml $WORKSPACE/new_schema.xml
-	popd
+	cp $WORKSPACE/kfs/work/db/kfs-db/rice/schema.xml $WORKSPACE/new_schema.xml
 	# Sanitize the sequence next values
 	perl -pi -e 's/nextval="[^"]*"/nextval="0"/g' upgraded_schema.xml
 	perl -pi -e 's/nextval="[^"]*"/nextval="0"/g' new_schema.xml

3383dfab1b6fd0cdc54db876e918aad1dd0065a4
Jonathan Keller <keller.jonathan@gmail.com>
2012-05-15 18:51:35 +0000

    Fix for Mysql?

--- work/db/upgrades/4.1.1_5.0/rice_server/update_final_mysql.sql
+++ work/db/upgrades/4.1.1_5.0/rice_server/update_final_mysql.sql
@@ -2185,11 +2185,11 @@ alter table KREW_PPL_FLW_DLGT_T add column RSP_ID VARCHAR(40) NOT NULL;
 -- KULRICE-5348
 alter table KREW_RULE_T change PREV_RULE_VER_NBR PREV_VER_RULE_ID varchar(40);
 
---KULRICE-4589
+-- KULRICE-4589
 UPDATE KRCR_PARM_T
-SET PARM_NM='NOTIFY_GROUPS',
-    PARM_DESC_TXT='Defines a group name (in the format "namespace:name") which contains members who should never receive "notifications" action requests from KEW.',
-    EVAL_OPRTR_CD='D'
+SET PARM_NM = 'NOTIFY_GROUPS',
+    PARM_DESC_TXT = 'Defines a group name (in the format "namespace:name") which contains members who should never receive "notifications" action requests from KEW.',
+    EVAL_OPRTR_CD = 'D'
 WHERE NMSPC_CD = 'KR-WKFLW'
   AND CMPNT_CD = 'Notification'
   AND PARM_NM = 'NOTIFY_EXCLUDED_USERS_IND';

12e35fbd30dfaaf77fe1bcac3c8a85fe4b6c8029
Jonathan Keller <keller.jonathan@gmail.com>
2012-05-16 16:33:17 +0000

    Fixed property file name

--- work/db/refresh-local-mysql-kuldev.sh
+++ work/db/refresh-local-mysql-kuldev.sh
@@ -2,7 +2,7 @@ TARGETS="${1:-create-schema empty-schema import}"
 pushd kfs-db/db-impex/impex
 
 set -x
-ant $TARGETS -Dimpex.properties.file=../../../impex-local-mysql-kuldev.properties.properties
+ant $TARGETS -Dimpex.properties.file=../../../impex-local-mysql-kuldev.properties
 set +x
 
 popd

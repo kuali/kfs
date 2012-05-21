285405b49bfc1a9da0dbfd83cc8d848942c462e8
Jonathan Keller <keller.jonathan@gmail.com>
2012-05-21 20:10:48 +0000

    Updating app doc status method to not attempt to save workflow data if the document has never been saved.

--- work/src/org/kuali/kfs/sys/businessobject/FinancialSystemDocumentHeader.java
+++ work/src/org/kuali/kfs/sys/businessobject/FinancialSystemDocumentHeader.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,15 +31,15 @@ import org.kuali.rice.krad.workflow.service.WorkflowDocumentService;
  * This class is a custom {@link DocumentHeader} class used by KFS to facilitate custom data fields and a few UI fields
  */
 public class FinancialSystemDocumentHeader extends DocumentHeader {
-    
+
     protected KualiDecimal financialDocumentTotalAmount;
     protected String correctedByDocumentId;
     protected String financialDocumentInErrorNumber;
     protected String financialDocumentStatusCode;
-    
+
     /**
      * Constructor - creates empty instances of dependent objects
-     * 
+     *
      */
     public FinancialSystemDocumentHeader() {
         super();
@@ -47,7 +47,7 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
     }
 
     /**
-     * Gets the financialDocumentTotalAmount attribute. 
+     * Gets the financialDocumentTotalAmount attribute.
      * @return Returns the financialDocumentTotalAmount.
      */
     public KualiDecimal getFinancialDocumentTotalAmount() {
@@ -63,7 +63,7 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
     }
 
     /**
-     * Gets the correctedByDocumentId attribute. 
+     * Gets the correctedByDocumentId attribute.
      * @return Returns the correctedByDocumentId.
      */
     public String getCorrectedByDocumentId() {
@@ -79,7 +79,7 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
     }
 
     /**
-     * Gets the financialDocumentInErrorNumber attribute. 
+     * Gets the financialDocumentInErrorNumber attribute.
      * @return Returns the financialDocumentInErrorNumber.
      */
     public String getFinancialDocumentInErrorNumber() {
@@ -95,7 +95,7 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
     }
 
     /**
-     * Gets the financialDocumentStatusCode attribute. 
+     * Gets the financialDocumentStatusCode attribute.
      * @return Returns the financialDocumentStatusCode.
      */
     public String getFinancialDocumentStatusCode() {
@@ -111,29 +111,31 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
     }
 
     /**
-     * Gets the documentFinalDate attribute. 
+     * Gets the documentFinalDate attribute.
      * @return Returns the documentFinalDate.
      */
     public Date getDocumentFinalDate() {
         WorkflowDocument workflowDoc = this.getWorkflowDocument();
-        if (workflowDoc == null || (workflowDoc.getDateFinalized() == null)) return null;
+        if (workflowDoc == null || (workflowDoc.getDateFinalized() == null)) {
+            return null;
+        }
         return  new java.sql.Date(workflowDoc.getDateFinalized().getMillis());
     }
 
     /**
      * Gets the applicationDocumentStatus attribute.
-     * 
+     *
      * @return Returns the applicationDocumentStatus
      */
-    
+
     public String getApplicationDocumentStatus() {
         WorkflowDocument document = getWorkflowDocument();
         return StringUtils.trimToEmpty(document.getApplicationDocumentStatus());
     }
 
-    /** 
+    /**
      * Sets the applicationDocumentStatus attribute.
-     * 
+     *
      * @param applicationDocumentStatus The applicationDocumentStatus to set.
      */
     public void setApplicationDocumentStatus(String applicationDocumentStatus) {
@@ -141,20 +143,21 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
 
         document.setApplicationDocumentStatus(applicationDocumentStatus);
     }
-    
+
     /**
      * method to retrieve the workflow document for the given documentHeader.
      *
      * @return workflowDocument
      */
+    @Override
     public WorkflowDocument getWorkflowDocument() {
-        
+
         WorkflowDocument workflowDocument = null;
-        
+
         if (hasWorkflowDocument()) {
             workflowDocument = super.getWorkflowDocument();
         }
-        
+
         try {
             if (workflowDocument != null) {
                 return workflowDocument;
@@ -168,7 +171,7 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
 
         return workflowDocument;
     }
-    
+
     /**
      * Updates status of this document and saves the workflow data
      *
@@ -177,7 +180,12 @@ public class FinancialSystemDocumentHeader extends DocumentHeader {
      */
     public void updateAndSaveAppDocStatus(String applicationDocumentStatus) throws WorkflowException {
        setApplicationDocumentStatus(applicationDocumentStatus);
-       SpringContext.getBean(WorkflowDocumentService.class).saveRoutingData(getWorkflowDocument());
+       // if the document is Initiated, then it has not been saved yet
+       // So, we don't need to save the routing data since the document
+       // has not been submitted to the workflow engine yet
+       if ( !getWorkflowDocument().isInitiated() ) {
+           SpringContext.getBean(WorkflowDocumentService.class).saveRoutingData(getWorkflowDocument());
+       }
     }
-    
+
 }

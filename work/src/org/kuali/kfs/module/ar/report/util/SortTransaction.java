a5e0badb2eade89c17a6dec235f239ed7d035a73
Winston Dang <wkd@hawaii.edu>
2012-06-05 02:32:19 +0000

    KFSMI-8166 test for null in java comparision sort

--- work/src/org/kuali/kfs/module/ar/report/util/SortTransaction.java
+++ work/src/org/kuali/kfs/module/ar/report/util/SortTransaction.java
@@ -1,11 +1,20 @@
 package org.kuali.kfs.module.ar.report.util;
 
+import java.sql.Date;
 import java.util.Comparator;
 
 public class SortTransaction implements Comparator<CustomerStatementDetailReportDataHolder> {
 
     public int compare(CustomerStatementDetailReportDataHolder c1, CustomerStatementDetailReportDataHolder c2) {
-        int result = c1.getDocumentFinalDate().compareTo(c2.getDocumentFinalDate());
+        Date c1Date = c1.getDocumentFinalDate();
+        Date c2Date = c2.getDocumentFinalDate();
+        int result = 0;
+        if (c1Date == null) {
+            result = (c2Date == null)? 0 : -1;
+        } else {
+            result = (c2Date == null) ? 1 : c1Date.compareTo(c2Date);
+        }
+        
         if (result == 0) {
             result = c1.getDocType().compareTo(c2.getDocType());
         }        

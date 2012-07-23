3447dad36ca1f932bd39e0a22c2ad515e66b5515
Muddu	Salem <mksalem@umd.edu>
2012-07-23 15:17:12 +0000

    KFSMI-8771: check for principal object and continue to check for authorizataion if it exists else return false.  This should prevent the NPE.

--- work/src/org/kuali/kfs/module/purap/document/workflow/KFSPurapDocumentSearchCustomizer.java
+++ work/src/org/kuali/kfs/module/purap/document/workflow/KFSPurapDocumentSearchCustomizer.java
@@ -93,6 +93,12 @@ public class KFSPurapDocumentSearchCustomizer extends KFSDocumentSearchCustomize
     }
 
     protected boolean isAuthorizedToViewPurapDocId() {
+        //prevent a NPE when user has not logged in yet..
+        //KFSMI-8771
+        if (GlobalVariables.getUserSession().getPerson() == null) {
+            return false;
+        }
+        
         String principalId = GlobalVariables.getUserSession().getPerson().getPrincipalId();
         String namespaceCode = KFSConstants.CoreModuleNamespaces.KNS;
         String permissionTemplateName = KimConstants.PermissionTemplateNames.FULL_UNMASK_FIELD;

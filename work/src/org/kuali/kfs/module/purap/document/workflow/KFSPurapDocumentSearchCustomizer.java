f302ee95d683927fb475d74f8ed821f7db0bff1d
Muddu	Salem <mksalem@umd.edu>
2012-07-30 13:17:17 +0000

    KFSMI-8956.  Commenting out the fix that was done for KFSMI-8771

--- work/src/org/kuali/kfs/module/purap/document/workflow/KFSPurapDocumentSearchCustomizer.java
+++ work/src/org/kuali/kfs/module/purap/document/workflow/KFSPurapDocumentSearchCustomizer.java
@@ -95,9 +95,9 @@ public class KFSPurapDocumentSearchCustomizer extends KFSDocumentSearchCustomize
     protected boolean isAuthorizedToViewPurapDocId() {
         //prevent a NPE when user has not logged in yet..
         //KFSMI-8771
-        if (GlobalVariables.getUserSession().getPerson() == null) {
-            return false;
-        }
+     //   if (GlobalVariables.getUserSession().getPerson() == null) {
+    //        return false;
+    //    }
         
         String principalId = GlobalVariables.getUserSession().getPerson().getPrincipalId();
         String namespaceCode = KFSConstants.CoreModuleNamespaces.KNS;

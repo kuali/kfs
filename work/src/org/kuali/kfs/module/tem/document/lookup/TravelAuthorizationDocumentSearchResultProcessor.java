3178786bc9d82c79d1496180219675c90aadd146
Samuel Wong <samuel.wong@uci.edu>
2012-08-02 23:11:17 +0000

    KFSCNTRB-1012
    TA- Vendor Payment Links missing when trip date is prior to system date
    
    Other payment method can be initiated from TA (even without the indicator) when trip has not started

--- work/src/org/kuali/kfs/module/tem/document/lookup/TravelAuthorizationDocumentSearchResultProcessor.java
+++ work/src/org/kuali/kfs/module/tem/document/lookup/TravelAuthorizationDocumentSearchResultProcessor.java
@@ -17,14 +17,19 @@ package org.kuali.kfs.module.tem.document.lookup;
 
 import static org.kuali.kfs.module.tem.TemPropertyConstants.TRVL_IDENTIFIER_PROPERTY;
 
+import java.util.Date;
+
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.kuali.kfs.module.tem.TemConstants;
 import org.kuali.kfs.module.tem.TemConstants.TravelAuthorizationStatusCodeKeys;
 import org.kuali.kfs.module.tem.TemPropertyConstants;
+import org.kuali.kfs.module.tem.document.TravelDocument;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kew.docsearch.DocSearchDTO;
 import org.kuali.rice.kew.docsearch.DocumentSearchResult;
 import org.kuali.rice.kew.doctype.service.DocumentTypeService;
+import org.kuali.rice.kew.exception.WorkflowException;
 import org.kuali.rice.kew.util.KEWConstants;
 import org.kuali.rice.kew.web.KeyValueSort;
 import org.kuali.rice.kns.service.ParameterService;
@@ -36,6 +41,8 @@ import org.kuali.rice.kns.service.ParameterService;
  */
 public class TravelAuthorizationDocumentSearchResultProcessor extends AbstractDocumentSearchResultProcessor implements TravelDocumentSearchResultsProcessor {
 
+    protected static Logger LOG = Logger.getLogger(TravelAuthorizationDocumentSearchResultProcessor.class);
+    
     /**
      * Determines if the url column for actions should be rendered. This is done for {@link TravelAuthorizationDocument} instances
      * in the search results that have a workflow document status of FINAL or PROCESSED and on documents that do not have a workflow
@@ -58,13 +65,45 @@ public class TravelAuthorizationDocumentSearchResultProcessor extends AbstractDo
                 && (!appDocStatus.equals(TravelAuthorizationStatusCodeKeys.RETIRED_VERSION));
     }
 
+    /**
+     * Other payment is allowed
+     * 
+     * @param docCriteriaDTO
+     * @return
+     */
     private boolean otherPaymentMethodsAllowed(DocSearchDTO docCriteriaDTO) {
         final String documentStatus = docCriteriaDTO.getDocRouteStatusCode();
         return (getParameterService().getIndicatorParameter(TemConstants.PARAM_NAMESPACE, TemConstants.TravelAuthorizationParameters.PARAM_DTL_TYPE, TemConstants.TravelAuthorizationParameters.ENABLE_VENDOR_PAYMENT_BEFORE_TA_FINAL_APPROVAL_IND) 
                 || documentStatus.equals(KEWConstants.ROUTE_HEADER_FINAL_CD) 
                 || documentStatus.equals(KEWConstants.ROUTE_HEADER_PROCESSED_CD));
     }
+    
+    /**
+     * check if the trip has already started
+     * 
+     * @param docCriteriaDTO
+     * @return
+     */
+    private boolean hasTripStarted(DocSearchDTO docCriteriaDTO) {
+        
+        //default that the trip has started
+        boolean tripStarted = true;
+        try {
+            TravelDocument document = getDocument(docCriteriaDTO.getRouteHeaderId().toString());
+            tripStarted = document.getTripBegin().before(new Date());
+        }
+        catch (WorkflowException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+        return tripStarted;
+    }
 
+    /**
+     * 
+     * @param docCriteriaDTO
+     * @param tripID
+     * @return
+     */
     private String createPaymentsURL(DocSearchDTO docCriteriaDTO, String tripID) {
         String links = createDisbursementVoucherLink(docCriteriaDTO, TemConstants.TravelCustomSearchLinks.PRE_TRIP_VENDOR_PAYMENT);
         links += "<br />" + createRequisitionLink(docCriteriaDTO, TemConstants.TravelCustomSearchLinks.REQUISITION);
@@ -73,6 +112,9 @@ public class TravelAuthorizationDocumentSearchResultProcessor extends AbstractDo
         return links;
     }
 
+    /**
+     * @see org.kuali.kfs.module.tem.document.lookup.TravelDocumentSearchResultsProcessor#addActionsColumn(org.kuali.rice.kew.docsearch.DocSearchDTO, org.kuali.rice.kew.docsearch.DocumentSearchResult)
+     */
     @Override
     public DocumentSearchResult addActionsColumn(DocSearchDTO docCriteriaDTO, DocumentSearchResult result) {
         final String columnName = TemPropertyConstants.TRVL_DOC_SEARCH_RESULT_PROPERTY_NAME_ACTIONS;
@@ -81,7 +123,7 @@ public class TravelAuthorizationDocumentSearchResultProcessor extends AbstractDo
         if (showReimbursementURL(docCriteriaDTO)) {
             actionsHTML += createTravelReimbursementLink(tripID);
         }
-        if (otherPaymentMethodsAllowed(docCriteriaDTO)) {
+        if (otherPaymentMethodsAllowed(docCriteriaDTO) || !hasTripStarted(docCriteriaDTO)) {
             if (!StringUtils.isBlank(actionsHTML)) {
                 actionsHTML += "<br />";
             }

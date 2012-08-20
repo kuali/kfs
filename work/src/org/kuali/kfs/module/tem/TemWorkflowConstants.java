5453f085cb03f30088bfb0d39f6e3d3373a4b262
Samuel Wong <samuel.wong@uci.edu>
2012-08-20 17:45:10 +0000

    KFSCNTRB-1057
    Cleanup the TEM document workflow setup
    
    1. Use TEMDocumentWorkflow as a base for TEM document SearchingTypeDefinition
    2. TA and TR also is using the shared TravelDocument-workflowAttributes as the parent bean for its worklfow attributes

--- work/src/org/kuali/kfs/module/tem/TemWorkflowConstants.java
+++ work/src/org/kuali/kfs/module/tem/TemWorkflowConstants.java
@@ -16,45 +16,41 @@
 package org.kuali.kfs.module.tem;
 
 public class TemWorkflowConstants {
-    public static final String SPECIAL_REQUEST              = "TravelHasSpecialRequest";
-    public static final String INTL_TRAVEL                  = "TravelIsInternationalTravel";
-    public static final String RISK_MANAGEMENT              = "TravelNeedsRiskManagement";
-    public static final String TRVL_ADV_REQUESTED           = "TravelAdvanceRequested";
-    public static final String ACCTG_APPROVAL_REQUIRED      = "TravelRequiresAccountingReviewer";
-    public static final String DIVISION_APPROVAL_REQUIRED   = "TravelRequiresDivisionApproval";
-    public static final String ACCOUNT_APPROVAL_REQUIRED    = "TravelRequiresAccountApproval";
-    public static final String TAX_MANAGER                  = "TaxManager";
-    public static final String ENTERTAINMENT_MANAGER        = "EntertainmentManager";
-    public static final String SEPARATION_OF_DUTIES         = "SeparationOfDuties";
-    public static final String REQUIRES_SUB_FUND            = "RequiresSubFund";
-    public static final String REQUIRES_AWARD               = "RequiresAward";
-    public static final String REQUIRES_AP_TRAVEL           = "RequiresAPTravel";
-    public static final String REQUIRES_TRAVELER_REVIEW     = "TravelRequiresTravelerApproval";
+    public static final String SPECIAL_REQUEST = "TravelHasSpecialRequest";
+    public static final String INTL_TRAVEL = "TravelIsInternationalTravel";
+    public static final String RISK_MANAGEMENT = "TravelNeedsRiskManagement";
+    public static final String TRVL_ADV_REQUESTED = "TravelAdvanceRequested";
+    public static final String ACCTG_APPROVAL_REQUIRED = "TravelRequiresAccountingReviewer";
+    public static final String DIVISION_APPROVAL_REQUIRED = "TravelRequiresDivisionApproval";
+    public static final String ACCOUNT_APPROVAL_REQUIRED = "TravelRequiresAccountApproval";
+    public static final String TAX_MANAGER = "TaxManager";
+    public static final String ENTERTAINMENT_MANAGER = "EntertainmentManager";
+    public static final String SEPARATION_OF_DUTIES = "SeparationOfDuties";
+    public static final String REQUIRES_SUB_FUND = "RequiresSubFund";
+    public static final String REQUIRES_AWARD = "RequiresAward";
+    public static final String REQUIRES_AP_TRAVEL = "RequiresAPTravel";
+    public static final String REQUIRES_TRAVELER_REVIEW = "TravelRequiresTravelerApproval";
+    
     // TEMProfile constants
     public static final String TAX_MANAGER_REQUIRED = "ProfileIsTaxManagerRequired";
-    
-    //RELO constants
-    
+    // RELO constants
     public static final String TAX_MANAGER_APPROVAL_REQUIRED = "TaxManagerApproval";
-    
-    
-    public static class RouteNodeNames{
-        public static final String AP_TRAVEL                        = "APTravel";
-        public static final String TRAVELER_REVIEW                  = "TravelerReview";
-        public static final String ACCOUNT                          = "Account";
-        public static final String ACCOUNTING_REVIEWER              = "AccountingReviewer"; 
-        public static final String DIVISION_REVIEWER                = "DivisionReviewer"; 
-        public static final String SPECIAL_REQUEST_REVIEWER         = "SpecialRequestReviewer"; 
-        public static final String INTERNATIONAL_TRAVEL_REVIEWER    = "InternationalTravelReviewer";
-        public static final String SUB_FUND                         = "SubFund";
-        public static final String AWARD                            = "Award";
-        public static final String TAX                              = "Tax";
-        public static final String SEPARATION_OF_DUTIES_REVIEWER    = "SeparationOfDutiesReviewer";
-        public static final String ORGANIZATION_HIERARCHY           = "OrganizationHierarchy";
-        public static final String RISK_MANAGEMENT                  = "RiskManagement";
-        public static final String EXECUTIVE_APPROVAL               = "ExecutiveApproval";
-        public static final String MOVING_AND_RELOCATION_MANAGER    = "MovingAndRelocationManager";
-        
-        
+
+    public static class RouteNodeNames {
+        public static final String AP_TRAVEL = "APTravel";
+        public static final String TRAVELER_REVIEW = "TravelerReview";
+        public static final String ACCOUNT = "Account";
+        public static final String ACCOUNTING_REVIEWER = "AccountingReviewer";
+        public static final String DIVISION_REVIEWER = "DivisionReviewer";
+        public static final String SPECIAL_REQUEST_REVIEWER = "SpecialRequestReviewer";
+        public static final String INTERNATIONAL_TRAVEL_REVIEWER = "InternationalTravelReviewer";
+        public static final String SUB_FUND = "SubFund";
+        public static final String AWARD = "Award";
+        public static final String TAX = "Tax";
+        public static final String SEPARATION_OF_DUTIES_REVIEWER = "SeparationOfDutiesReviewer";
+        public static final String ORGANIZATION_HIERARCHY = "OrganizationHierarchy";
+        public static final String RISK_MANAGEMENT = "RiskManagement";
+        public static final String EXECUTIVE_APPROVAL = "ExecutiveApproval";
+        public static final String MOVING_AND_RELOCATION_MANAGER = "MovingAndRelocationManager";
     }
 }

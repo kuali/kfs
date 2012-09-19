d0cbab3d68a94cf6bf5ea501ee6462e9013f7a7f
Jonathan Keller <keller.jonathan@gmail.com>
2012-09-19 16:58:39 +0000

    Upgraded liquibase to 2.0 as other parts of the project.

--- work/db/rice-data/run_bootstrap.sh
+++ work/db/rice-data/run_bootstrap.sh
@@ -1,1 +1,1 @@
-java -jar liquibase-1.9.5.jar --logLevel=finest --contexts=bootstrap --changeLogFile=$1 $2 $3
+java -jar liquibase*.jar --logLevel=finest --contexts=bootstrap --changeLogFile=$1 $2 $3

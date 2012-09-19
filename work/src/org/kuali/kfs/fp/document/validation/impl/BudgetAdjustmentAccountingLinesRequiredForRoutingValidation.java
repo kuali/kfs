eb8200c5ea3feda4981953050abe56d40220c1fb
Harsha Ummerpillai <harsha07@msu.edu>
2012-09-19 20:10:10 +0000

    KFSCNTRB-1084

--- work/src/org/kuali/kfs/fp/document/validation/impl/BudgetAdjustmentAccountingLinesRequiredForRoutingValidation.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/BudgetAdjustmentAccountingLinesRequiredForRoutingValidation.java
@@ -17,9 +17,14 @@ package org.kuali.kfs.fp.document.validation.impl;
 
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSKeyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.document.AccountingDocument;
 import org.kuali.kfs.sys.document.validation.GenericValidation;
 import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kew.api.doctype.DocumentTypeService;
+import org.kuali.rice.kew.api.exception.WorkflowException;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.krad.util.GlobalVariables;
 
 /**
@@ -35,13 +40,42 @@ public class BudgetAdjustmentAccountingLinesRequiredForRoutingValidation extends
      * @see org.kuali.kfs.sys.document.validation.Validation#validate(org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent)
      */
     public boolean validate(AttributedDocumentEvent event) {
+        boolean sucess = true;
+        
         // check that both source and target are not empty, in which case is an error
         if (getAccountingDocumentForValidation().getSourceAccountingLines().isEmpty() && getAccountingDocumentForValidation().getTargetAccountingLines().isEmpty()) {
             GlobalVariables.getMessageMap().putError(KFSConstants.ACCOUNTING_LINE_ERRORS, KFSKeyConstants.ERROR_DOCUMENT_NO_ACCOUNTING_LINES);
-            return false;
+            return sucess&= false;
+        }
+        
+        Document document = event.getDocument();
+        int sourceSectionSize = getAccountingDocumentForValidation().getSourceAccountingLines().size();
+        int targetSectionSize = getAccountingDocumentForValidation().getTargetAccountingLines().size();
+
+        
+        String documentTypeLabel= SpringContext.getBean(DocumentTypeService.class).getDocumentTypeByName(document.getDocumentHeader().getWorkflowDocument().getDocumentTypeName()).getLabel();
+         if(documentTypeLabel.equals(KFSConstants.YEAR_END_BUDGET_ADJUSTMENT_LABEL)){
+            if ((sourceSectionSize == 0 && targetSectionSize < 2) || (targetSectionSize == 0 && sourceSectionSize < 2)) {
+                GlobalVariables.getMessageMap().putError(KFSConstants.ACCOUNTING_LINE_ERRORS, KFSKeyConstants.ERROR_DOCUMENT_OPTIONAL_ONE_SIDED_DOCUMENT_REQUIRED_NUMBER_OF_ACCOUNTING_LINES_NOT_MET);
+                sucess&= false;
+            }
+        }
+        else{
+            if(documentTypeLabel.equals(KFSConstants.BUDGET_REALLOCATION_LABEL)){
+                if (getAccountingDocumentForValidation().getSourceAccountingLines().isEmpty()) {
+                    GlobalVariables.getMessageMap().putError(KFSConstants.SOURCE_ACCOUNTING_LINE_ERRORS, KFSKeyConstants.ERROR_DOCUMENT_SOURCE_SECTION_NO_ACCOUNTING_LINES,KFSConstants.FROM);
+                    sucess&=false;
+                }
+                if (getAccountingDocumentForValidation().getTargetAccountingLines().isEmpty()) {
+                    GlobalVariables.getMessageMap().putError(KFSConstants.TARGET_ACCOUNTING_LINE_ERRORS, KFSKeyConstants.ERROR_DOCUMENT_TARGET_SECTION_NO_ACCOUNTING_LINES,KFSConstants.TO);
+                    sucess&=false;
+                }
+            }
         }
 
-        return true;
+        return sucess;
+
+       
     }
 
     /**

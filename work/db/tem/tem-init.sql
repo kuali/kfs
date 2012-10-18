ce30b3a3c78254a70020aa15ed8c9307f010a2ac
Samuel Wong <samuel.wong@uci.edu>
2012-10-18 23:31:02 +0000

    KFSCNTRB-1243 KFMSI-9630
    New Special Circumstance document hit Exception Routing
    
    1. Removing the tem-init function that creates the special circumstances question.  This is moved into the DBA base line setup.  Having the code in here requiring an update to the TEM_SPCL_CRCMSNCS_Q_ID_SEQ to reset the appropriate sequence value.

--- work/db/tem/tem-init.sql
+++ work/db/tem/tem-init.sql
@@ -4,29 +4,6 @@ SET FS_MOTD_TXT = 'Welcome to TEM Contribution Team Test Site (5.0 Pre-Rice 2.0)
 WHERE FS_ORIGIN_CD = '01'
 /
 
--- Insert Special Circumstances Questions for TEM
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-values (1,'TA','Question for TA - is anyone traveling with you?','Y','Y','KFS-TEM-SPCM-001')
-/
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-values (2,'TR','Question for TR documents - did you stay overnight with family or friends?:','Y','Y','KFS-TEM-SPCM-002')
-/
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-VALUES (3,'ENT','Question for Entertainment documents - was alcohol served?:','Y','Y','KFS-TEM-SPCM-003')
-/
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-values (4,'RELO','Question for Relocation documents - do you have pets moving with you?:','Y','Y','KFS-TEM-SPCM-004')
-/
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-values (5,'TA','Question for TA documents - not free form: Carrying Fruit?','N','Y','KFS-TEM-SPCM-005')
-/
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-VALUES (6,'TA','Inactive Question for search testing','N','N','KFS-TEM-SPCM-006')
-/
-INSERT INTO TEM_SPCL_CRCMSNCS_Q_T (ID,DOCUMENT_TYPE,TEXT,TXT_IND,ACTV_IND,OBJ_ID) 
-VALUES (7,'TA','Do you have large pets traveling with you?:','Y','Y','KFS-TEM-SPCM-007')
-/
-
 -- Update max file upload size to 200M 
 UPDATE KRNS_PARM_T
 SET TXT = '200M'

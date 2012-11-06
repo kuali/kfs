6d0d43a5c6ac967d48df91119c6c60069e782428
Jonathan Keller <keller.jonathan@gmail.com>
2012-11-06 19:34:57 +0000

    KFSMI-9702 : fixed null checking of the bank and continuation bank on the NCD document

--- work/src/org/kuali/kfs/fp/document/validation/impl/NonCheckDisbursementDocumentPreRules.java
+++ work/src/org/kuali/kfs/fp/document/validation/impl/NonCheckDisbursementDocumentPreRules.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008-2009 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,6 @@ import java.text.MessageFormat;
 import org.kuali.kfs.fp.document.NonCheckDisbursementDocument;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSKeyConstants;
-import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.kfs.sys.businessobject.Bank;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.service.BankService;
@@ -36,45 +35,44 @@ public class NonCheckDisbursementDocumentPreRules extends PromptBeforeValidation
     @Override
     public boolean doPrompts(Document document) {
         boolean preRulesOK = true;
-        
+
         NonCheckDisbursementDocument nonCheckDocument = (NonCheckDisbursementDocument) document;
-        
+
         preRulesOK &= checkBankCodeActive(nonCheckDocument);
-        
+
         return preRulesOK;
     }
-    
+
     /**
      * If bank specification is enabled, prompts user to use the continuation bank code when the
      * given bank code is inactive
-     * 
+     *
      * @param nonCheckDocument document containing bank code
      * @return true
      */
     protected boolean checkBankCodeActive(NonCheckDisbursementDocument nonCheckDocument) {
-        boolean continueRules = true;
-        
+        BankService bankService = SpringContext.getBean(BankService.class);
+
         // if bank specification is not enabled, no need to validate bank
-        if (!SpringContext.getBean(BankService.class).isBankSpecificationEnabled()) {
-            return continueRules;
-        }
+        if ( bankService.isBankSpecificationEnabled() ) {
+            Bank bank = bankService.getByPrimaryId(nonCheckDocument.getFinancialDocumentBankCode());
 
-        // refresh bank reference so continuation bank can be checked for active status
-        nonCheckDocument.refreshReferenceObject(KFSPropertyConstants.BANK);
-        Bank bank = nonCheckDocument.getBank();
+            // if bank is inactive and continuation is active, prompt user to use continuation bank
+            if ( bank != null  && !bank.isActive() ) {
+                Bank continuationBank = bankService.getByPrimaryId(bank.getContinuationBankCode());
+                if ( continuationBank != null && continuationBank.isActive() ) {
+                    String questionText = SpringContext.getBean(ConfigurationService.class).getPropertyValueAsString(KFSKeyConstants.QUESTION_BANK_INACTIVE);
+                    questionText = MessageFormat.format(questionText, bank.getBankCode(), continuationBank.getBankCode());
 
-        // if bank is inactive and continuation is active, prompt user to use continuation bank
-        if (bank != null && !bank.isActive() && bank.getContinuationBank().isActive()) {
-            String questionText = SpringContext.getBean(ConfigurationService.class).getPropertyValueAsString(KFSKeyConstants.QUESTION_BANK_INACTIVE);
-            questionText = MessageFormat.format(questionText, nonCheckDocument.getFinancialDocumentBankCode(), bank.getContinuationBankCode());
-
-            boolean useContinuation = super.askOrAnalyzeYesNoQuestion(KFSConstants.USE_CONTINUATION_BANK_QUESTION, questionText);
-            if (useContinuation) {
-                nonCheckDocument.setFinancialDocumentBankCode(bank.getContinuationBankCode());
+                    boolean useContinuation = askOrAnalyzeYesNoQuestion(KFSConstants.USE_CONTINUATION_BANK_QUESTION, questionText);
+                    if (useContinuation) {
+                        nonCheckDocument.setFinancialDocumentBankCode(continuationBank.getBankCode());
+                    }
+                }
             }
         }
 
-        return continueRules;
+        return true;
     }
 
 }

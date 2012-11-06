c1c23b7c66729ca55e0139f79d4187034f64068d
Jonathan Keller <keller.jonathan@gmail.com>
2012-11-06 18:59:15 +0000

    KFSMI-9133 : restored ability to use expired accounts on SET document

--- work/src/org/kuali/kfs/module/ld/businessobject/LaborAccountingLineOverride.java
+++ work/src/org/kuali/kfs/module/ld/businessobject/LaborAccountingLineOverride.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ import java.util.Set;
 import org.kuali.kfs.sys.businessobject.AccountingLine;
 import org.kuali.kfs.sys.businessobject.AccountingLineOverride;
 import org.kuali.kfs.sys.businessobject.AccountingLineOverride.COMPONENT;
+import org.kuali.kfs.sys.businessobject.SourceAccountingLine;
 
 /**
  * Labor business object for Labor Accounting Line Override
@@ -29,7 +30,7 @@ public class LaborAccountingLineOverride {
 
     /**
      * On the given AccountingLine, converts override input checkboxes from a Struts Form into a persistable override code.
-     * 
+     *
      * @param line
      */
     public static void populateFromInput(AccountingLine line) {
@@ -54,14 +55,23 @@ public class LaborAccountingLineOverride {
      * Prepares the given AccountingLine in a Struts Action for display by a JSP. This means converting the override code to
      * checkboxes for display and input, as well as analyzing the accounting line and determining which override checkboxes are
      * needed.
-     * 
+     *
      * @param line
      */
     public static void processForOutput(AccountingLine line) {
         AccountingLineOverride fromCurrentCode = AccountingLineOverride.valueOf(line.getOverrideCode());
         AccountingLineOverride needed = determineNeededOverrides(line);
-        line.setAccountExpiredOverride(fromCurrentCode.hasComponent(COMPONENT.EXPIRED_ACCOUNT));
+        // KFSMI-9133 : updating system to automatically check expired account boxes on the source side
+        // of the transaction, since those are read only.  Otherwise, amounts in expired accounts
+        // could never be transferred
         line.setAccountExpiredOverrideNeeded(needed.hasComponent(COMPONENT.EXPIRED_ACCOUNT));
+        if ( line.getAccountExpiredOverrideNeeded() ) {
+            if ( line instanceof SourceAccountingLine ) {
+                line.setAccountExpiredOverride(true);
+            } else {
+                line.setAccountExpiredOverride(fromCurrentCode.hasComponent(COMPONENT.EXPIRED_ACCOUNT));
+            }
+        }
         line.setObjectBudgetOverride(fromCurrentCode.hasComponent(COMPONENT.NON_BUDGETED_OBJECT));
         line.setObjectBudgetOverrideNeeded(needed.hasComponent(COMPONENT.NON_BUDGETED_OBJECT));
         line.setNonFringeAccountOverride(fromCurrentCode.hasComponent(COMPONENT.NON_FRINGE_ACCOUNT_USED));
@@ -70,7 +80,7 @@ public class LaborAccountingLineOverride {
 
     /**
      * Determines what overrides the given line needs.
-     * 
+     *
      * @param line
      * @return what overrides the given line needs.
      */

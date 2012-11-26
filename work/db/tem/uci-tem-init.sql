08b4767d4742c7b6cee27c49cc76cd5942ff588b
Samuel Wong <samuel.wong@uci.edu>
2012-11-26 19:41:27 +0000

    1. Update parameters to the correct csv (krcr_parm_t.csv) 2. krns_parm_dtl_typ_t converted to krcr_cmpnt_t.csv 3. kr_state_t converted to krlc_st_t 4. updated liquibase loading scripts 5. removed all rsmart sqls (will only be used for reference)

--- work/db/tem/uci-tem-init.sql
+++ work/db/tem/uci-tem-init.sql
@@ -1,17 +1,17 @@
 
 -- update parameters for UCI institution names and travel policy URL
-UPDATE KRNS_PARM_T
+UPDATE KRCR_PARM_T
 SET TXT = 'University of California, Irvine'
 WHERE NMSPC_CD = 'KFS-TEM' AND PARM_NM = 'TRAVEL_REPORT_INSTITUTION_NAME'
 /
 
-UPDATE KRNS_PARM_T
+UPDATE KRCR_PARM_T
 SET TXT = 'University of California, Irvine',
 PARM_DESC_TXT = 'University of California, Irvine'
 WHERE NMSPC_CD = 'KFS-AR' AND PARM_NM = 'INSTITUTION_NAME'
 /
 
-UPDATE KRNS_PARM_T
+UPDATE KRCR_PARM_T
 SET TXT = 'http://www.policies.uci.edu/adm/procs/700/715-22.html'
 WHERE NMSPC_CD = 'KFS-TEM' AND PARM_NM = 'TRAVEL_ADVANCES_POLICY_URL'
 /

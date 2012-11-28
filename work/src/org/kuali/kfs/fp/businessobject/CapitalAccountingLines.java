d18d0f05432b8d80f2520262001d671815f9c9a5
Jonathan Keller <keller.jonathan@gmail.com>
2012-11-28 20:29:57 +0000

    KFSMI-9645 : Completion of CAB changes for accounting line uniqueness

--- work/src/org/kuali/kfs/fp/businessobject/CapitalAccountingLines.java
+++ work/src/org/kuali/kfs/fp/businessobject/CapitalAccountingLines.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,6 @@
 
 package org.kuali.kfs.fp.businessobject;
 
-import java.util.LinkedHashMap;
-
-import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.krad.bo.TransientBusinessObjectBase;
 
@@ -42,178 +39,94 @@ public class CapitalAccountingLines extends TransientBusinessObjectBase {
     protected boolean selectLine;
     protected String distributionAmountCode;
     protected boolean amountDistributed;
-    
+
     //need to show the percentage of the accounts.
     protected KualiDecimal accountLinePercent;
-    
-    /**
-     * Default constructor.
-     */
-    public CapitalAccountingLines() {
-    }
-    
-    /**
-     * Gets the documentNumber attribute.
-     * 
-     * @return Returns the documentNumber
-     */
+
     public String getDocumentNumber() {
         return documentNumber;
     }
 
-    /**
-     * Sets the documentNumber attribute.
-     * 
-     * @param documentNumber The documentNumber to set.
-     */
     public void setDocumentNumber(String documentNumber) {
         this.documentNumber = documentNumber;
     }
 
-    /**
-     * Gets the sequenceNumber attribute.
-     * 
-     * @return Returns the sequenceNumber
-     */
     public Integer getSequenceNumber() {
         return sequenceNumber;
     }
 
-    /** 
-     * Sets the sequenceNumber attribute.
-     * 
-     * @param sequenceNumber The sequenceNumber to set.
-     */
     public void setSequenceNumber(Integer sequenceNumber) {
         this.sequenceNumber = sequenceNumber;
     }
 
-    /**
-     * Gets the lineType attribute.
-     * 
-     * @return Returns the lineType
-     */
     public String getLineType() {
         return lineType;
     }
 
-    /** 
-     * Sets the lineType attribute.
-     * 
-     * @param lineType The lineType to set.
-     */
     public void setLineType(String lineType) {
         this.lineType = lineType;
     }
 
-    /**
-     * Gets the chartOfAccountsCode attribute.
-     * 
-     * @return Returns the chartOfAccountsCode
-     */
+
     public String getChartOfAccountsCode() {
         return chartOfAccountsCode;
     }
 
-    /** 
-     * Sets the chartOfAccountsCode attribute.
-     * 
-     * @param chartOfAccountsCode The chartOfAccountsCode to set.
-     */
+
     public void setChartOfAccountsCode(String chartOfAccountsCode) {
         this.chartOfAccountsCode = chartOfAccountsCode;
     }
 
-    /**
-     * Gets the accountNumber attribute.
-     * 
-     * @return Returns the accountNumber
-     */
+
     public String getAccountNumber() {
         return accountNumber;
     }
 
-    /** 
-     * Sets the accountNumber attribute.
-     * 
-     * @param accountNumber The accountNumber to set.
-     */
+
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-    /**
-     * Gets the financialObjectCode attribute.
-     * 
-     * @return Returns the financialObjectCode
-     */
+
     public String getFinancialObjectCode() {
         return financialObjectCode;
     }
 
-    /** 
-     * Sets the financialObjectCode attribute.
-     * 
-     * @param financialObjectCode The financialObjectCode to set.
-     */
+
     public void setFinancialObjectCode(String financialObjectCode) {
         this.financialObjectCode = financialObjectCode;
     }
 
-    /**
-     * Gets the subAccountNumber attribute.
-     * 
-     * @return Returns the subAccountNumber
-     */
+
     public String getSubAccountNumber() {
         return subAccountNumber;
     }
 
-    /** 
-     * Sets the subAccountNumber attribute.
-     * 
-     * @param subAccountNumber The subAccountNumber to set.
-     */
+
     public void setSubAccountNumber(String subAccountNumber) {
         this.subAccountNumber = subAccountNumber;
     }
 
-    /**
-     * Gets the financialSubObjectCode attribute.
-     * 
-     * @return Returns the financialSubObjectCode
-     */
+
     public String getFinancialSubObjectCode() {
         return financialSubObjectCode;
     }
 
-    /** 
-     * Sets the financialSubObjectCode attribute.
-     * 
-     * @param financialSubObjectCode The financialSubObjectCode to set.
-     */
+
     public void setFinancialSubObjectCode(String financialSubObjectCode) {
         this.financialSubObjectCode = financialSubObjectCode;
     }
 
-    /**
-     * Gets the projectCode attribute.
-     * 
-     * @return Returns the projectCode
-     */
+
     public String getProjectCode() {
         return projectCode;
     }
 
-    /** 
-     * Sets the projectCode attribute.
-     * 
-     * @param projectCode The projectCode to set.
-     */
+
     public void setProjectCode(String projectCode) {
         this.projectCode = projectCode;
     }
-    
+
     public String getOrganizationReferenceId() {
         return organizationReferenceId;
     }
@@ -222,141 +135,53 @@ public class CapitalAccountingLines extends TransientBusinessObjectBase {
         this.organizationReferenceId = organizationReferenceId;
     }
 
-    /**
-     * Gets the financialDocumentLineDescription attribute.
-     * 
-     * @return Returns the financialDocumentLineDescription
-     */
+
     public String getFinancialDocumentLineDescription() {
         return financialDocumentLineDescription;
     }
 
-    /** 
-     * Sets the financialDocumentLineDescription attribute.
-     * 
-     * @param financialDocumentLineDescription The financialDocumentLineDescription to set.
-     */
+
     public void setFinancialDocumentLineDescription(String financialDocumentLineDescription) {
         this.financialDocumentLineDescription = financialDocumentLineDescription;
     }
 
-    /**
-     * Gets the amount attribute.
-     * 
-     * @return Returns the amount
-     */
     public KualiDecimal getAmount() {
         return amount;
     }
 
-    /** 
-     * Sets the amount attribute.
-     * 
-     * @param amount The amount to set.
-     */
     public void setAmount(KualiDecimal amount) {
         this.amount = amount;
     }
 
-    /**
-     * Gets the selectLine attribute.
-     * 
-     * @return Returns the selectLine
-     */
     public boolean isSelectLine() {
         return selectLine;
     }
 
-    /** 
-     * Sets the selectLine attribute.
-     * 
-     * @param selectLine The selectLine to set.
-     */
     public void setSelectLine(boolean selectLine) {
         this.selectLine = selectLine;
     }
 
-    /**
-     * Gets the distributionAmountCode attribute.
-     * 
-     * @return Returns the distributionAmountCode
-     */
-    
     public String getDistributionAmountCode() {
         return distributionAmountCode;
     }
 
-    /** 
-     * Sets the distributionAmountCode attribute.
-     * 
-     * @param distributionAmountCode The distributionAmountCode to set.
-     */
     public void setDistributionAmountCode(String distributionAmountCode) {
         this.distributionAmountCode = distributionAmountCode;
     }
 
-    /**
-     * Gets the amountDistributed attribute.
-     * 
-     * @return Returns the amountDistributed
-     */
-    
     public boolean isAmountDistributed() {
         return amountDistributed;
     }
 
-    /** 
-     * Sets the amountDistributed attribute.
-     * 
-     * @param amountDistributed The amountDistributed to set.
-     */
     public void setAmountDistributed(boolean amountDistributed) {
         this.amountDistributed = amountDistributed;
     }
 
-    /**
-     * Gets the accountLinePercent attribute.
-     * 
-     * @return Returns the accountLinePercent
-     */
-    
     public KualiDecimal getAccountLinePercent() {
         return accountLinePercent;
     }
 
-    /** 
-     * Sets the accountLinePercent attribute.
-     * 
-     * @param accountLinePercent The accountLinePercent to set.
-     */
     public void setAccountLinePercent(KualiDecimal accountLinePercent) {
         this.accountLinePercent = accountLinePercent;
-    }
-
-    /**
-     * @see org.kuali.rice.kns.bo.BusinessObjectBase#toStringMapper()
-     */
-    
-    public LinkedHashMap toStringMapper_RICE20_REFACTORME() {
-        LinkedHashMap m = new LinkedHashMap();
-        
-        m.put(KFSPropertyConstants.DOCUMENT_NUMBER, this.getDocumentNumber());
-        m.put(KFSPropertyConstants.SEQUENCE_NUMBER, this.getSequenceNumber());
-        m.put(KFSPropertyConstants.LINE_TYPE, this.getLineType());
-        m.put(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE, this.getChartOfAccountsCode());
-        m.put(KFSPropertyConstants.ACCOUNT_NUMBER, this.getAccountNumber());
-        m.put(KFSPropertyConstants.FINANCIAL_OBJECT_CODE, this.getFinancialObjectCode());
-        m.put(KFSPropertyConstants.SUB_ACCOUNT_NUMBER, this.getSubAccountNumber());
-        m.put(KFSPropertyConstants.FINANCIAL_SUB_OBJECT_CODE, this.getFinancialSubObjectCode());
-        m.put(KFSPropertyConstants.PROJECT_CODE, this.getProjectCode());
-
-  //      m.put(KFSPropertyConstants.FINANCIAL_DOCUMENT_LINE_DESCRIPTION, this.getFinancialDocumentLineDescription());
-  //      m.put(KFSPropertyConstants.AMOUNT, this.getAmount());
-  //      m.put(KFSPropertyConstants.SELECT_LINE, this.isSelectLine());
-  //      m.put(KFSPropertyConstants.DISTRIBUTION_AMOUNT_CODE, this.getDistributionAmountCode());
-  //      m.put(KFSPropertyConstants.AMOUNT_DISTRIBUTED, this.isAmountDistributed());
-  //      m.put(KFSPropertyConstants.ACCOUNT_LINE_PERCENT, this.getAccountLinePercent());
-        
-        return m;
     }
 }
--- work/src/org/kuali/kfs/fp/businessobject/CapitalAssetAccountsGroupDetails.java
+++ work/src/org/kuali/kfs/fp/businessobject/CapitalAssetAccountsGroupDetails.java
@@ -16,7 +16,6 @@
 package org.kuali.kfs.fp.businessobject;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.kuali.kfs.sys.KFSPropertyConstants;
@@ -27,193 +26,118 @@ import org.kuali.rice.krad.util.ObjectUtils;
 public class CapitalAssetAccountsGroupDetails extends PersistableBusinessObjectBase {
 
     //primary key fields..
-    private String documentNumber;
-    private Integer capitalAssetLineNumber;
-    private Integer capitalAssetAccountLineNumber;
-    private Integer sequenceNumber;
+    protected String documentNumber;
+    protected Integer capitalAssetLineNumber;
+    protected Integer capitalAssetAccountLineNumber;
+    protected Integer sequenceNumber;
 
     // accounting line info
-    private String financialDocumentLineTypeCode;
-    private String chartOfAccountsCode;
-    private String accountNumber;
-    private String financialObjectCode;
-    private String subAccountNumber;
-    private String financialSubObjectCode;
-    private String projectCode;
-    private String organizationReferenceId;
-    private KualiDecimal amount;
+    protected String financialDocumentLineTypeCode;
+    protected String chartOfAccountsCode;
+    protected String accountNumber;
+    protected String financialObjectCode;
+    protected String subAccountNumber;
+    protected String financialSubObjectCode;
+    protected String projectCode;
+    protected String organizationReferenceId;
+    protected KualiDecimal amount;
 
-    private CapitalAssetInformation capitalAssetInformation;
+    protected CapitalAssetInformation capitalAssetInformation;
 
-    /**
-     * Gets the documentNumber attribute.
-     *
-     * @return Returns the documentNumber
-     */
+
 
     public String getDocumentNumber() {
         return documentNumber;
     }
 
-    /**
-     * Gets the capitalAssetAccountLineNumber attribute.
-     *
-     * @return Returns the capitalAssetAccountLineNumber
-     */
+
 
     public Integer getCapitalAssetAccountLineNumber() {
         return capitalAssetAccountLineNumber;
     }
 
-    /**
-     * Gets the financialDocumentLineTypeCode attribute.
-     *
-     * @return Returns the financialDocumentLineTypeCode
-     */
+
 
     public String getFinancialDocumentLineTypeCode() {
         return financialDocumentLineTypeCode;
     }
 
-    /**
-     * Gets the chartOfAccountsCode attribute.
-     *
-     * @return Returns the chartOfAccountsCode
-     */
+
 
     public String getChartOfAccountsCode() {
         return chartOfAccountsCode;
     }
 
-    /**
-     * Gets the accountNumber attribute.
-     *
-     * @return Returns the accountNumber
-     */
+
 
     public String getAccountNumber() {
         return accountNumber;
     }
 
-    /**
-     * Gets the financialObjectCode attribute.
-     *
-     * @return Returns the financialObjectCode
-     */
+
 
     public String getFinancialObjectCode() {
         return financialObjectCode;
     }
 
-    /**
-     * Gets the capitalAssetInformation attribute.
-     *
-     * @return Returns the capitalAssetInformation.
-     */
+
     public CapitalAssetInformation getCapitalAssetInformation() {
         return capitalAssetInformation;
     }
 
-    /**
-     * Sets the capitalAssetInformation attribute value.
-     *
-     * @param capitalAssetInformation The capitalAssetInformation to set.
-     */
+
     public void setCapitalAssetInformation(CapitalAssetInformation capitalAssetInformation) {
         this.capitalAssetInformation = capitalAssetInformation;
     }
 
 
-    /**
-     * Sets the documentNumber attribute.
-     *
-     * @param documentNumber The documentNumber to set.
-     */
+
     public void setDocumentNumber(String documentNumber) {
         this.documentNumber = documentNumber;
     }
 
-    /**
-     * Sets the capitalAssetAccountLineNumber attribute.
-     *
-     * @param capitalAssetAccountLineNumber The capitalAssetAccountLineNumber to set.
-     */
+
     public void setCapitalAssetAccountLineNumber(Integer capitalAssetAccountLineNumber) {
         this.capitalAssetAccountLineNumber = capitalAssetAccountLineNumber;
     }
 
-    /**
-     * Sets the financialDocumentLineTypeCode attribute.
-     *
-     * @param financialDocumentLineTypeCode The financialDocumentLineTypeCode to set.
-     */
+
     public void setFinancialDocumentLineTypeCode(String financialDocumentLineTypeCode) {
         this.financialDocumentLineTypeCode = financialDocumentLineTypeCode;
     }
 
-    /**
-     * Sets the chartOfAccountsCode attribute.
-     *
-     * @param chartOfAccountsCode The chartOfAccountsCode to set.
-     */
+
     public void setChartOfAccountsCode(String chartOfAccountsCode) {
         this.chartOfAccountsCode = chartOfAccountsCode;
     }
 
-    /**
-     * Sets the accountNumber attribute.
-     *
-     * @param accountNumber The accountNumber to set.
-     */
+
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-    /**
-     * Sets the financialObjectCode attribute.
-     *
-     * @param financialObjectCode The financialObjectCode to set.
-     */
+
     public void setFinancialObjectCode(String financialObjectCode) {
         this.financialObjectCode = financialObjectCode;
     }
 
-    /**
-     * Constructs a CapitalAssetInformation.java.
-     */
+
     public CapitalAssetAccountsGroupDetails() {
         super();
         setAmount(KualiDecimal.ZERO);
     }
 
-    /**
-     * Gets the sequenceNumber attribute.
-     *
-     * @return Returns the sequenceNumber
-     */
+
 
     public Integer getSequenceNumber() {
         return sequenceNumber;
     }
 
-    /**
-     * Sets the sequenceNumber attribute.
-     *
-     * @param sequenceNumber The sequenceNumber to set.
-     */
+
     public void setSequenceNumber(Integer sequenceNumber) {
         this.sequenceNumber = sequenceNumber;
     }
 
-    protected LinkedHashMap toStringMapper() {
-        LinkedHashMap m = new LinkedHashMap();
-        m.put(KFSPropertyConstants.DOCUMENT_NUMBER, this.documentNumber);
-        m.put(KFSPropertyConstants.CAPITAL_ASSET_LINE_NUMBER, this.getCapitalAssetLineNumber());
-        m.put(KFSPropertyConstants.CAPITAL_ASSET_ACCOUNT_LINE_NUMBER, this.getCapitalAssetAccountLineNumber());
-        m.put(KFSPropertyConstants.SEQUENCE_NUMBER, this.getSequenceNumber());
-
-        return m;
-    }
 
     /**
      * Returns a map with the primitive field names as the key and the primitive values as the map value.
@@ -229,32 +153,28 @@ public class CapitalAssetAccountsGroupDetails extends PersistableBusinessObjectB
         simpleValues.put(KFSPropertyConstants.FINANCIAL_DOCUMENT_LINE_TYPE_CODE, this.getFinancialDocumentLineTypeCode());
         simpleValues.put(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE, this.getChartOfAccountsCode());
         simpleValues.put(KFSPropertyConstants.ACCOUNT_NUMBER, this.getAccountNumber());
+        simpleValues.put(KFSPropertyConstants.SUB_ACCOUNT_NUMBER, this.getSubAccountNumber());
         simpleValues.put(KFSPropertyConstants.FINANCIAL_OBJECT_CODE, this.getFinancialObjectCode());
+        simpleValues.put(KFSPropertyConstants.FINANCIAL_SUB_OBJECT_CODE, this.getFinancialSubObjectCode());
+        simpleValues.put(KFSPropertyConstants.PROJECT_CODE, this.getProjectCode());
+        simpleValues.put(KFSPropertyConstants.ORGANIZATION_REFERENCE_ID, this.getOrganizationReferenceId());
         simpleValues.put(KFSPropertyConstants.AMOUNT, this.getAmount());
         simpleValues.put(KFSPropertyConstants.SEQUENCE_NUMBER, this.getSequenceNumber());
 
         return simpleValues;
     }
 
-    /**
-     * Gets the capitalAssetLineNumber attribute.
-     * @return Returns the capitalAssetLineNumber.
-     */
+
     public Integer getCapitalAssetLineNumber() {
         return capitalAssetLineNumber;
     }
 
-    /**
-     * Sets the capitalAssetLineNumber attribute value.
-     * @param capitalAssetLineNumber The capitalAssetLineNumber to set.
-     */
+
     public void setCapitalAssetLineNumber(Integer capitalAssetLineNumber) {
         this.capitalAssetLineNumber = capitalAssetLineNumber;
     }
 
-    /**
-     * @return Returns the amount.
-     */
+
     public KualiDecimal getAmount() {
         if (ObjectUtils.isNull(amount)) {
             return KualiDecimal.ZERO;
@@ -264,9 +184,7 @@ public class CapitalAssetAccountsGroupDetails extends PersistableBusinessObjectB
         }
     }
 
-    /**
-     * @param amount The amount to set.
-     */
+
     public void setAmount(KualiDecimal amount) {
         if (ObjectUtils.isNull(amount)) {
             this.amount = KualiDecimal.ZERO;
@@ -309,5 +227,5 @@ public class CapitalAssetAccountsGroupDetails extends PersistableBusinessObjectB
     public void setOrganizationReferenceId(String organizationReferenceId) {
         this.organizationReferenceId = organizationReferenceId;
     }
-        
+
 }

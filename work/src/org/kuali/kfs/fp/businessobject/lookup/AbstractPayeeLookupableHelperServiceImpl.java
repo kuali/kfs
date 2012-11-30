c3097f1859ed43a9876a2bf38925166f1486cfd2
Kevin Kronenbitter <kevin.kronenbitter@colostate.edu>
2012-11-30 00:19:25 +0000

    KFSCNTRB-1327 reverting enhancement made in KFSCNTRB-668

--- work/src/org/kuali/kfs/fp/businessobject/lookup/AbstractPayeeLookupableHelperServiceImpl.java
+++ work/src/org/kuali/kfs/fp/businessobject/lookup/AbstractPayeeLookupableHelperServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 package org.kuali.kfs.fp.businessobject.lookup;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +24,7 @@ import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.fp.businessobject.DisbursementPayee;
 import org.kuali.kfs.fp.document.service.DisbursementVoucherPayeeService;
+import org.kuali.kfs.fp.document.service.DisbursementVoucherPaymentReasonService;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.kfs.sys.context.SpringContext;
@@ -35,6 +37,7 @@ import org.kuali.rice.kim.impl.KIMPropertyConstants;
 import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
 import org.kuali.rice.kns.lookup.Lookupable;
 import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.kns.web.ui.ResultRow;
 import org.kuali.rice.krad.bo.BusinessObject;
 import org.kuali.rice.krad.datadictionary.AttributeSecurity;
 import org.kuali.rice.krad.util.ObjectUtils;
@@ -43,14 +46,32 @@ public class AbstractPayeeLookupableHelperServiceImpl extends KualiLookupableHel
 
     protected Lookupable vendorLookupable;
     protected DisbursementVoucherPayeeService disbursementVoucherPayeeService;
+    protected DisbursementVoucherPaymentReasonService disbursementVoucherPaymentReasonService;
 
     // get the label for the given attribute of the current business object
     protected String getAttributeLabel(String attributeName) {
         return this.getDataDictionaryService().getAttributeLabel(getBusinessObjectClass(), attributeName);
-    }    
+    }
+
+    // remove its return URLs if a row is not qualified for returning
+    protected void filterReturnUrl(List<ResultRow> resultRowList, List<DisbursementPayee> payeeList, String paymentReasonCode) {
+        Collection<String> payeeTypeCodes = disbursementVoucherPaymentReasonService.getPayeeTypesByPaymentReason(paymentReasonCode);
+        if (payeeTypeCodes == null || payeeTypeCodes.isEmpty()) {
+            return;
+        }
+
+        for (int index = 0; index < payeeList.size(); index++) {
+            DisbursementPayee payee = payeeList.get(index);
+
+            boolean isQualified = disbursementVoucherPaymentReasonService.isPayeeQualifiedForPayment(payee, paymentReasonCode, payeeTypeCodes);
+            if (!isQualified) {
+                resultRowList.get(index).setReturnUrl(StringUtils.EMPTY);
+            }
+        }
+    }
 
     // perform vendor search
-    protected List<DisbursementPayee> getVendorsAsPayees(Map<String, String> fieldValues) {               
+    protected List<DisbursementPayee> getVendorsAsPayees(Map<String, String> fieldValues) {
         List<DisbursementPayee> payeeList = new ArrayList<DisbursementPayee>();
 
         Map<String, String> fieldsForLookup = this.getVendorFieldValues(fieldValues);
@@ -63,13 +84,13 @@ public class AbstractPayeeLookupableHelperServiceImpl extends KualiLookupableHel
             DisbursementPayee payee = getPayeeFromVendor(vendorDetail, fieldValues);
             payeeList.add(payee);
         }
-        
+
         return payeeList;
     }
 
     // get the search criteria valid for vendor lookup
     protected Map<String, String> getVendorFieldValues(Map<String, String> fieldValues) {
-        Map<String, String> vendorFieldValues = new HashMap<String, String>();        
+        Map<String, String> vendorFieldValues = new HashMap<String, String>();
         vendorFieldValues.put(KFSPropertyConstants.TAX_NUMBER, fieldValues.get(KFSPropertyConstants.TAX_NUMBER));
         vendorFieldValues.put(KFSPropertyConstants.VENDOR_NAME, fieldValues.get(KFSPropertyConstants.VENDOR_NAME));
         vendorFieldValues.put(KFSPropertyConstants.VENDOR_NUMBER, fieldValues.get(KFSPropertyConstants.VENDOR_NUMBER));
@@ -95,8 +116,8 @@ public class AbstractPayeeLookupableHelperServiceImpl extends KualiLookupableHel
     protected String getVendorName(Map<String, String> vendorFieldValues) {
         String firstName = vendorFieldValues.get(VendorPropertyConstants.VENDOR_FIRST_NAME);
         String lastName = vendorFieldValues.get(VendorPropertyConstants.VENDOR_LAST_NAME);
-       
-        // TODO The following code has a bug: it doesn't handle blank first name right. 
+
+        // TODO The following code has a bug: it doesn't handle blank first name right.
         if (StringUtils.isNotBlank(lastName)) {
             return lastName + VendorConstants.NAME_DELIM + firstName;
         }
@@ -110,33 +131,33 @@ public class AbstractPayeeLookupableHelperServiceImpl extends KualiLookupableHel
     protected DisbursementPayee getPayeeFromVendor(VendorDetail vendorDetail, Map<String, String> fieldValues) {
         DisbursementPayee payee = disbursementVoucherPayeeService.getPayeeFromVendor(vendorDetail);
         payee.setPaymentReasonCode(fieldValues.get(KFSPropertyConstants.PAYMENT_REASON_CODE));
-        
+
         //KFSMI-5497
-        //get the attributeSecurity property and mask the field so that on results screen will be shown masked.        
+        //get the attributeSecurity property and mask the field so that on results screen will be shown masked.
         DataDictionaryService dataDictionaryService = SpringContext.getBean(DataDictionaryService.class);
         AttributeSecurity attributeSecurity =  dataDictionaryService.getAttributeSecurity(DisbursementPayee.class.getName(), KFSPropertyConstants.TAX_NUMBER);
         if (ObjectUtils.isNotNull(attributeSecurity)) {
             attributeSecurity.setMask(true);
         }
-        
+
         return payee;
     }
 
     // perform person search
     protected List<DisbursementPayee> getPersonAsPayees(Map<String, String> fieldValues) {
         List<DisbursementPayee> payeeList = new ArrayList<DisbursementPayee>();
-        
-        Map<String, String> fieldsForLookup = this.getPersonFieldValues(fieldValues);                
-        List<Person> persons = SpringContext.getBean(PersonService.class).findPeople(fieldsForLookup);   
-        
-        for (Person personDetail : persons) {            
+
+        Map<String, String> fieldsForLookup = this.getPersonFieldValues(fieldValues);
+        List<Person> persons = SpringContext.getBean(PersonService.class).findPeople(fieldsForLookup);
+
+        for (Person personDetail : persons) {
             DisbursementPayee payee = getPayeeFromPerson(personDetail, fieldValues);
             payeeList.add(payee);
         }
-        
+
         return payeeList;
     }
-    
+
     // get the search criteria valid for person lookup
     protected Map<String, String> getPersonFieldValues(Map<String, String> fieldValues) {
         Map<String, String> personFieldValues = new HashMap<String, String>();
@@ -181,7 +202,7 @@ public class AbstractPayeeLookupableHelperServiceImpl extends KualiLookupableHel
 
     /**
      * Sets the vendorLookupable attribute value.
-     * 
+     *
      * @param vendorLookupable The vendorLookupable to set.
      */
     public void setVendorLookupable(Lookupable vendorLookupable) {
@@ -190,11 +211,20 @@ public class AbstractPayeeLookupableHelperServiceImpl extends KualiLookupableHel
 
     /**
      * Sets the disbursementVoucherPayeeService attribute value.
-     * 
+     *
      * @param disbursementVoucherPayeeService The disbursementVoucherPayeeService to set.
      */
     public void setDisbursementVoucherPayeeService(DisbursementVoucherPayeeService disbursementVoucherPayeeService) {
         this.disbursementVoucherPayeeService = disbursementVoucherPayeeService;
+    }
+
+    /**
+     * Sets the disbursementVoucherPaymentReasonService attribute.
+     *
+     * @param disbursementVoucherPaymentReasonService The disbursementVoucherPaymentReasonService to set.
+     */
+    public void setDisbursementVoucherPaymentReasonService(DisbursementVoucherPaymentReasonService disbursementVoucherPaymentReasonService) {
+        this.disbursementVoucherPaymentReasonService = disbursementVoucherPaymentReasonService;
     }
 
 }
--- work/src/org/kuali/kfs/sys/KFSKeyConstants.java
+++ work/src/org/kuali/kfs/sys/KFSKeyConstants.java
@@ -917,9 +917,9 @@ public class KFSKeyConstants {
 
     public static final String ERROR_DV_LOOKUP_NEEDS_SOME_FIELD = "error.dv.lookupNeedsSomeField";
     public static final String ERROR_DV_VENDOR_EMPLOYEE_CONFUSION = "error.dv.vendorEmployeeConfusion";
-    public static final String ERROR_DV_LAST_FIRST_NAME_NOT_FILLED = "error.dv.lastFirstNameNotFilled";
+    //public static final String ERROR_DV_LAST_FIRST_NAME_NOT_FILLED = "error.dv.lastFirstNameNotFilled";
     public static final String ERROR_DV_NAME_NOT_FILLED_ENOUGH = "error.dv.nameNotFilledEnough";
-    public static final String WARNING_DV_PAYEE_ID_UNIQUE = "warning.dv.payeeIdUnique";
+    //public static final String WARNING_DV_PAYEE_ID_UNIQUE = "warning.dv.payeeIdUnique";
     public static final String ERROR_DV_VENDOR_NAME_PERSON_NAME_CONFUSION = "error.dv.vendorNamePersonNameConfusion";
     public static final String ERROR_DV_LOOKUP_TAX_NUMBER_EMPLOYEE_DETAILS_CONFUSION = "error.dv.vendorTaxNumberEmployeeDetailsConfusion";
 

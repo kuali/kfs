db3e98dd1550a771c117e45150a25c7ab1902eb8
Muddu	Salem <mksalem@umd.edu>
2012-12-02 11:13:06 +0000

    KFSMI-9760: Gl pending entries are retrieved using dao and summed up the totals and set the amount cab gl entry transaction amount for positive and negative entries whenever there is FO update.  The purpose of this method is to combine the same account string cab gl lines into one for positive and one for negative entry.

--- work/src/org/kuali/kfs/module/cab/batch/dataaccess/ExtractDao.java
+++ work/src/org/kuali/kfs/module/cab/batch/dataaccess/ExtractDao.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,14 +19,16 @@ import java.util.Collection;
 
 import org.kuali.kfs.gl.businessobject.Entry;
 import org.kuali.kfs.module.cab.businessobject.BatchParameters;
+import org.kuali.kfs.module.cab.businessobject.GeneralLedgerEntry;
 import org.kuali.kfs.module.purap.businessobject.CreditMemoAccountRevision;
 import org.kuali.kfs.module.purap.businessobject.PaymentRequestAccountRevision;
 import org.kuali.kfs.module.purap.businessobject.PurchaseOrderAccount;
+import org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntry;
 
 public interface ExtractDao {
     /**
      * This method implementation should find all GL Entries matching the parameters listed in the batch parameters
-     * 
+     *
      * @param batchParameters Batch Parameters
      * @return GL Entries
      */
@@ -35,7 +37,7 @@ public interface ExtractDao {
 
     /**
      * This method implementation should find valid Credit Memo account line changes as per the batch parameters
-     * 
+     *
      * @param batchParameters Batch Parameters
      * @return List of Credit memo account history records
      */
@@ -43,7 +45,7 @@ public interface ExtractDao {
 
     /**
      * This method implementation should find valid Payment Request account line changes as per the batch parameters
-     * 
+     *
      * @param batchParameters Batch Parameters
      * @return List of Payment Request account history records
      */
@@ -51,10 +53,17 @@ public interface ExtractDao {
 
     /**
      * This method implementation should retrieve all eligible pretaggable PO account lines from Purchasing module
-     * 
+     *
      * @param batchParameters Batch Parameters
      * @return List of pre-taggable purchase order account lines
      */
     Collection<PurchaseOrderAccount> findPreTaggablePOAccounts(BatchParameters batchParameters);
 
+    /**
+     * This method finds the matching gl pending entry for the given current entry and returns the entry.
+     *
+     * @param currentEntry
+     * @return List of matching gl pending entry
+     */
+    Collection<GeneralLedgerPendingEntry> findMatchingGlPendingEntries(GeneralLedgerEntry currentEntry);
 }

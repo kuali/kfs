98673a9b49ab487f673ec0411768e289ae5cd0da
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2012-12-14 22:50:38 +0000

    KFSMI-9923 Put in a fix so that logic this logic to prevent an account maint. doc without a C&G Account Respsonsibilty Id to route to all C&G Processors will not interfere with the ability of C&G Processors to edit object codes on target accounting lines on ST docs

--- work/src/org/kuali/kfs/coa/identity/ContractsAndGrantsResponsibilityRoleTypeServiceImpl.java
+++ work/src/org/kuali/kfs/coa/identity/ContractsAndGrantsResponsibilityRoleTypeServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.identity.KfsKimAttributes;
 import org.kuali.rice.kim.api.KimConstants;
 import org.kuali.rice.kim.api.role.RoleMembership;
@@ -33,9 +34,13 @@ public class ContractsAndGrantsResponsibilityRoleTypeServiceImpl extends RoleTyp
         // special handling for where the code is being called for a particular route node
         // and the code is blank
         if ( qualification != null ) {
-            if ( StringUtils.equals( qualification.get(KimConstants.AttributeConstants.ROUTE_NODE_NAME), AWARD_ROUTE_NODE_NAME) ) {
-                if ( StringUtils.isBlank(qualification.get(KfsKimAttributes.CONTRACTS_AND_GRANTS_ACCOUNT_RESPONSIBILITY_ID)) ) {
-                    return new ArrayList<RoleMembership>(0);
+            // this logic is here for Account docs, not ST docs where it interferes with the ability of C&G Processors
+            // to edit object codes on target accounting lines
+            if ( !StringUtils.equals( qualification.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME), KFSConstants.FinancialDocumentTypeCodes.SALARY_EXPENSE_TRANSFER) ) {
+                if ( StringUtils.equals( qualification.get(KimConstants.AttributeConstants.ROUTE_NODE_NAME), AWARD_ROUTE_NODE_NAME) ) {
+                    if ( StringUtils.isBlank(qualification.get(KfsKimAttributes.CONTRACTS_AND_GRANTS_ACCOUNT_RESPONSIBILITY_ID)) ) {
+                        return new ArrayList<RoleMembership>(0);
+                    }
                 }
             }
         }

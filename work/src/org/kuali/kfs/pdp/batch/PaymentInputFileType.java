a547ae5f7fc4a6c39f0cb9bb4b40a249c3796789
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2013-01-03 14:03:19 +0000

    KFSMI-10001 Modified code to consistently send parsing errors regardless of whether PDP payment file is loaded via batch or online Payment File Batch Upload screen.

--- work/src/org/kuali/kfs/pdp/batch/PaymentInputFileType.java
+++ work/src/org/kuali/kfs/pdp/batch/PaymentInputFileType.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,10 +29,12 @@ import org.kuali.kfs.pdp.businessobject.PaymentDetail;
 import org.kuali.kfs.pdp.businessobject.PaymentFileLoad;
 import org.kuali.kfs.pdp.businessobject.PaymentGroup;
 import org.kuali.kfs.pdp.service.PaymentFileService;
+import org.kuali.kfs.pdp.service.PdpEmailService;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSKeyConstants;
 import org.kuali.kfs.sys.batch.XmlBatchInputFileTypeBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.exception.ParseException;
 import org.kuali.rice.core.api.datetime.DateTimeService;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.util.MessageMap;
@@ -41,13 +43,17 @@ import org.kuali.rice.krad.util.MessageMap;
  * Batch input type for the PDP payment file.
  */
 public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PaymentInputFileType.class);
+
     private DateTimeService dateTimeService;
     private PaymentFileService paymentFileService;
+    private PdpEmailService paymentFileEmailService;
 
     /**
      * @see org.kuali.kfs.sys.batch.BatchInputFileType#getFileName(org.kuali.rice.kim.api.identity.Person, java.lang.Object,
      *      java.lang.String)
      */
+    @Override
     public String getFileName(String principalName, Object parsedFileContents, String fileUserIdentifer) {
         Timestamp currentTimestamp = dateTimeService.getCurrentTimestamp();
 
@@ -56,13 +62,14 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
             fileName += "_" + StringUtils.remove(fileUserIdentifer, " ");
         }
         fileName += "_" + dateTimeService.toString(currentTimestamp, "yyyyMMdd_HHmmss");
-        
+
         // remove spaces in filename
         fileName = StringUtils.remove(fileName, " ");
 
         return fileName;
     }
-    
+
+    @Override
     public String getAuthorPrincipalName(File file) {
         String[] fileNameParts = StringUtils.split(file.getName(), "_");
         if (fileNameParts.length > 3) {
@@ -74,6 +81,7 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
     /**
      * @see org.kuali.kfs.sys.batch.BatchInputFileType#getFileTypeIdentifer()
      */
+    @Override
     public String getFileTypeIdentifer() {
         return PdpConstants.PAYMENT_FILE_TYPE_INDENTIFIER;
     }
@@ -81,17 +89,22 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
     /**
      * @see org.kuali.kfs.sys.batch.BatchInputFileType#validate(java.lang.Object)
      */
+    @Override
     public boolean validate(Object parsedFileContents) {
         PaymentFileLoad paymentFile = (PaymentFileLoad) parsedFileContents;
-        
-        // add validation for chartCode-accountNumber, as chartCode is not required in xsd due to accounts-cant-cross-charts option        
-        AccountService acctserv = SpringContext.getBean(AccountService.class);        
+
+        // add validation for chartCode-accountNumber, as chartCode is not required in xsd due to accounts-cant-cross-charts option
+        AccountService acctserv = SpringContext.getBean(AccountService.class);
         boolean validAccounts = true;
         if (paymentFile.getPaymentGroups() != null) {
             for (PaymentGroup payGroup : paymentFile.getPaymentGroups()) {
-                if (payGroup.getPaymentDetails() == null) continue;
+                if (payGroup.getPaymentDetails() == null) {
+                    continue;
+                }
                 for (PaymentDetail payDetail : payGroup.getPaymentDetails()) {
-                    if (payDetail.getAccountDetail() == null) continue;
+                    if (payDetail.getAccountDetail() == null) {
+                        continue;
+                    }
                     for (PaymentAccountDetail acctDetail : payDetail.getAccountDetail()) {
                         // if chart code is empty while accounts cannot cross charts, then derive chart code from account number
                         if (StringUtils.isEmpty(acctDetail.getFinChartCode())) {
@@ -104,7 +117,7 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
                                 Account account = acctserv.getUniqueAccountForAccountNumber(acctDetail.getAccountNbr());
                                 if (account != null) {
                                     acctDetail.setFinChartCode(account.getChartOfAccountsCode());
-                                }       
+                                }
                                 else {
                                     GlobalVariables.getMessageMap().putError(KFSConstants.GLOBAL_ERRORS, KFSKeyConstants.ERROR_BATCH_UPLOAD_FILE_INVALID_ACCOUNT, acctDetail.getAccountNbr());
                                     validAccounts = false;
@@ -123,6 +136,7 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
     /**
      * @see org.kuali.kfs.sys.batch.BatchInputType#getTitleKey()
      */
+    @Override
     public String getTitleKey() {
         return PdpKeyConstants.MESSAGE_BATCH_UPLOAD_TITLE_PAYMENT;
     }
@@ -144,8 +158,33 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
     }
 
     /**
+     * Overridden so we can send the error email here. (keep it consistent between the batch processing and the
+     * online file upload web processing)
+     *
+     * @see org.kuali.kfs.sys.batch.XmlBatchInputFileTypeBase#parse(byte[])
+     */
+    @Override
+    public Object parse(byte[] fileByteContent) throws ParseException {
+        PaymentFileLoad paymentFile = null;
+
+        try {
+            paymentFile = (PaymentFileLoad) super.parse(fileByteContent);
+        } catch (ParseException e1) {
+            LOG.error("Error parsing xml contents: " + e1.getMessage());
+            MessageMap errorMap = new MessageMap();
+            errorMap.putError(KFSConstants.GLOBAL_ERRORS, KFSKeyConstants.ERROR_BATCH_UPLOAD_PARSING_XML, new String[] { "Error parsing xml contents: " + e1.getMessage() });
+
+            // Send error email
+            paymentFileEmailService.sendErrorEmail(paymentFile, errorMap);
+            throw new ParseException("Error parsing xml contents: " + e1.getMessage(), e1);
+        }
+
+        return paymentFile;
+    }
+
+    /**
      * Sets the paymentFileService attribute value.
-     * 
+     *
      * @param paymentFileService The paymentFileService to set.
      */
     public void setPaymentFileService(PaymentFileService paymentFileService) {
@@ -154,11 +193,20 @@ public class PaymentInputFileType extends XmlBatchInputFileTypeBase {
 
     /**
      * Sets the dateTimeService attribute value.
-     * 
+     *
      * @param dateTimeService The dateTimeService to set.
      */
     public void setDateTimeService(DateTimeService dateTimeService) {
         this.dateTimeService = dateTimeService;
+    }
+
+    /**
+     * Sets the paymentFileEmailService attribute value.
+     *
+     * @param paymentFileEmailService The paymentFileEmailService to set.
+     */
+    public void setPaymentFileEmailService(PdpEmailService paymentFileEmailService) {
+        this.paymentFileEmailService = paymentFileEmailService;
     }
 }
 

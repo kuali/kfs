4a8a95793bc509177a37913b849c1da987238a90
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2013-03-14 14:36:53 +0000

    changed account delegate id to user in role 54 (eligible for delegation) to fix failing tests

--- test/unit/src/org/kuali/kfs/coa/document/GlobalDocumentTest.java
+++ test/unit/src/org/kuali/kfs/coa/document/GlobalDocumentTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -119,7 +119,7 @@ public class GlobalDocumentTest extends KualiTestBase {
         AccountDelegateGlobalDetail change = new AccountDelegateGlobalDetail();
         change.setAccountDelegatePrimaryRoutingIndicator(false);
         change.setAccountDelegateStartDate(KfsDateUtils.newDate(2006, 6, 1));
-        change.setAccountDelegateUniversalId("6137600107");
+        change.setAccountDelegateUniversalId("1183109030");
         change.setApprovalFromThisAmount(KualiDecimal.ZERO);
         change.setApprovalToThisAmount(KualiDecimal.ZERO);
         change.setFinancialDocumentTypeCode(KFSConstants.ROOT_DOCUMENT_TYPE);
@@ -147,7 +147,7 @@ public class GlobalDocumentTest extends KualiTestBase {
         account.setAccountNumber("1031467");
         bo.addAccount(account);
         SpringContext.getBean(DocumentService.class).saveDocument(document);
-        
+
         document = (MaintenanceDocument)SpringContext.getBean(DocumentService.class).getByDocumentHeaderId(document.getDocumentNumber());
 
         // now that it worked, lets cancel the doc so it doesnt lock for others
@@ -172,7 +172,7 @@ public class GlobalDocumentTest extends KualiTestBase {
         AccountDelegateGlobalDetail change = new AccountDelegateGlobalDetail();
         change.setAccountDelegatePrimaryRoutingIndicator(false);
         change.setAccountDelegateStartDate(KfsDateUtils.newDate(2006, 6, 1));
-        change.setAccountDelegateUniversalId("6137600107");
+        change.setAccountDelegateUniversalId("1183109030");
         change.setApprovalFromThisAmount(KualiDecimal.ZERO);
         change.setApprovalToThisAmount(KualiDecimal.ZERO);
         change.setFinancialDocumentTypeCode(KFSConstants.ROOT_DOCUMENT_TYPE);

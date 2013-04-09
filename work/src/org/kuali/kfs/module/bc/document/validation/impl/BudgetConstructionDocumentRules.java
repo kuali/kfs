29407c3cd59237b78e8f3a0b846a0559ee254228
Gary	Palmer <gwpalmer@iu.edu>
2013-04-09 11:49:25 +0000

    KFSCNTRB-1464 added validation checks for object and sub-object and refresh monthly budget references and skip check for sub-object dashes value

--- work/src/org/kuali/kfs/module/bc/document/validation/impl/BudgetConstructionDocumentRules.java
+++ work/src/org/kuali/kfs/module/bc/document/validation/impl/BudgetConstructionDocumentRules.java
@@ -363,7 +363,7 @@ public class BudgetConstructionDocumentRules extends TransactionalDocumentRuleBa
     public boolean processSaveMonthlyBudgetRules(BudgetConstructionDocument budgetConstructionDocument, BudgetConstructionMonthly budgetConstructionMonthly) {
         LOG.debug("processSaveMonthlyBudgetRules() start");
 
-        budgetConstructionMonthly.refreshReferenceObject("pendingBudgetConstructionGeneralLedger");
+        budgetConstructionMonthly.refreshNonUpdateableReferences();
         PendingBudgetConstructionGeneralLedger pbgl = budgetConstructionMonthly.getPendingBudgetConstructionGeneralLedger();
         MessageMap errors = GlobalVariables.getMessageMap();
         boolean isValid = true;
@@ -382,6 +382,18 @@ public class BudgetConstructionDocumentRules extends TransactionalDocumentRuleBa
         if (!budgetConstructionDocument.isBudgetableDocument()) {
             isValid &= Boolean.FALSE;
             errors.putError(BCPropertyConstants.FINANCIAL_DOCUMENT_MONTH1_LINE_AMOUNT, BCKeyConstants.ERROR_BUDGET_DOCUMENT_NOT_BUDGETABLE, budgetConstructionDocument.getAccountNumber() + ";" + budgetConstructionDocument.getSubAccountNumber());
+        }
+
+        DataDictionary dd = dataDictionaryService.getDataDictionary();
+        if (isValid) {
+
+            ObjectCode objectCode = budgetConstructionMonthly.getFinancialObject();
+            isValid &= isValidObjectCode(objectCode, budgetConstructionMonthly.getFinancialObjectCode(), dd, KFSPropertyConstants.FINANCIAL_OBJECT_CODE);
+
+            if (StringUtils.isNotBlank(budgetConstructionMonthly.getFinancialSubObjectCode()) && !budgetConstructionMonthly.getFinancialSubObjectCode().equalsIgnoreCase(KFSConstants.getDashFinancialSubObjectCode())){
+                SubObjectCode subObjectCode = budgetConstructionMonthly.getFinancialSubObject();
+                isValid &= isValidSubObjectCode(subObjectCode, budgetConstructionMonthly.getFinancialSubObjectCode(), dd, KFSPropertyConstants.FINANCIAL_SUB_OBJECT_CODE);
+            }
         }
 
         if (isValid) {

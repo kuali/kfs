aa1aeaa5bd35a397d14f05de354264cab7b1fee8
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2013-04-23 18:08:44 +0000

    KFSMI-9886 KFS/KC integration security work - updating namespace URIs

--- work/src/org/kuali/kfs/module/external/kc/service/AccountCreationService.java
+++ work/src/org/kuali/kfs/module/external/kc/service/AccountCreationService.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,29 +19,28 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
-import org.kuali.kfs.integration.cg.ContractsAndGrantsConstants;
 import org.kuali.kfs.integration.cg.dto.AccountCreationStatusDTO;
 import org.kuali.kfs.integration.cg.dto.AccountParametersDTO;
 import org.kuali.kfs.module.external.kc.KcConstants;
 
-@WebService(name = KcConstants.AccountCreationService.WEB_SERVICE_NAME, 
-            targetNamespace = ContractsAndGrantsConstants.MODULE_TARGET_NAMESPACE)
-@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, 
+@WebService(name = KcConstants.AccountCreationService.WEB_SERVICE_NAME,
+            targetNamespace = KcConstants.KFS_NAMESPACE_URI)
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL,
              parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface AccountCreationService {
 
     public AccountCreationStatusDTO createAccount(
             @WebParam(name="accountParametersDTO")AccountParametersDTO accountParametersDTO);
-    
+
     public boolean isValidAccount(
             @WebParam(name="accountNumber")  String accountNumber);
-    
+
     public boolean isValidChartAccount(
             @WebParam(name="chartOfAccountCode") String chartOfAccountsCode, @WebParam(name="accountNumber")  String accountNumber);
-    
+
     public boolean isValidChartCode(
             @WebParam(name="chartOfAccountsCode")  String chartOfAccountsCode);
-    
-    public boolean accountsCanCrossCharts(); 
+
+    public boolean accountsCanCrossCharts();
 
 }

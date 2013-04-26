b846f74db9d7288ebc3736b223896cdb97a0e728
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2013-04-26 18:39:48 +0000

    KFSMI-9151 Reverting changes for this JIRA since it was fixed by KFSCNTRB-1433

--- work/src/org/kuali/kfs/module/purap/document/validation/impl/PurchaseOrderAmendmentAccountingLineAccessibleValidation.java
+++ work/src/org/kuali/kfs/module/purap/document/validation/impl/PurchaseOrderAmendmentAccountingLineAccessibleValidation.java
@@ -15,19 +15,11 @@
  */
 package org.kuali.kfs.module.purap.document.validation.impl;
 
-import java.util.Iterator;
-import java.util.Map;
-
 import org.kuali.kfs.module.purap.businessobject.PurApAccountingLine;
 import org.kuali.kfs.module.purap.document.PurchasingAccountsPayableDocument;
 import org.kuali.kfs.module.purap.document.service.PurapService;
-import org.kuali.kfs.sys.KFSPropertyConstants;
-import org.kuali.kfs.sys.businessobject.AccountingLine;
-import org.kuali.kfs.sys.businessobject.SourceAccountingLine;
-import org.kuali.kfs.sys.businessobject.TargetAccountingLine;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
-import org.kuali.kfs.sys.document.validation.event.UpdateAccountingLineEvent;
 import org.kuali.kfs.sys.service.FinancialSystemWorkflowHelperService;
 import org.kuali.rice.krad.util.GlobalVariables;
 
@@ -52,25 +44,21 @@ public class PurchaseOrderAmendmentAccountingLineAccessibleValidation extends Pu
 
         } else if (SpringContext.getBean(FinancialSystemWorkflowHelperService.class).isAdhocApprovalRequestedForPrincipal(event.getDocument().getDocumentHeader().getWorkflowDocument(), GlobalVariables.getUserSession().getPrincipalId())) {
             return true;
-        } else if (event instanceof UpdateAccountingLineEvent && onlyAmountChanged(((UpdateAccountingLineEvent) event).getAccountingLine(), ((UpdateAccountingLineEvent) event).getUpdatedAccountingLine())) {
-            //  if only amount is changed, return true...
-            //  This condition is added later which will return true only if amount is changed, it won't affect any other functionality of amending the PO document.
-            return true;
         } else {
             boolean result = false;
             boolean setDummyAccountIdentifier = false;
-
+            
             if (needsDummyAccountIdentifier()) {
                 ((PurApAccountingLine)getAccountingLineForValidation()).setAccountIdentifier(Integer.MAX_VALUE);  // avoid conflicts with any accouting identifier on any other accounting lines in the doc because, you know, you never know...
                 setDummyAccountIdentifier = true;
             }
-
+            
             result = super.validate(event);
-
+            
             if (setDummyAccountIdentifier) {
                 ((PurApAccountingLine)getAccountingLineForValidation()).setAccountIdentifier(null);
             }
-
+            
             return result;
         }
     }
@@ -79,55 +67,5 @@ public class PurchaseOrderAmendmentAccountingLineAccessibleValidation extends Pu
         this.purapService = purapService;
     }
 
-    /**
-     * Checks to see if the object code is the only difference between the original accounting line and the updated accounting line.
-     *
-     * @param accountingLine
-     * @param updatedAccountingLine
-     * @return true if only the object code has changed on the accounting line, false otherwise
-     */
-    private boolean onlyAmountChanged(AccountingLine accountingLine, AccountingLine updatedAccountingLine) {
-        // no changes, return false
-        if (accountingLine.isLike(updatedAccountingLine)) {
-            return false;
-        }
-
-        // copy the updatedAccountLine so we can set the object code on the copy of the updated accounting line
-        // to be the original value for comparison purposes
-        AccountingLine updatedLine = null;
-        if (updatedAccountingLine.isSourceAccountingLine()) {
-            updatedLine = new SourceAccountingLine();
-        } else {
-            updatedLine = new TargetAccountingLine();
-        }
-
-        updatedLine.copyFrom(updatedAccountingLine);
-        updatedLine.setAmount(accountingLine.getAmount());
-
-        boolean isLike = false;
-        Map thisValues = accountingLine.getValuesMap();
-        Map otherValues = updatedLine.getValuesMap();
-        isLike = thisValues.equals(otherValues);
-
-        //  if both are not same, check for document number, as document number gets omitted in values map for updated line
-        if (!isLike) {
-            int countInequalities = 0;
-
-            for (Iterator i = otherValues.keySet().iterator(); i.hasNext();) {
-                String key = (String) i.next();
-
-                Object thisValue = thisValues.get(key);
-                Object otherValue = otherValues.get(key);
-                //  ignore if document number and sequence number found, as document number is omitted from one map and sequence number might change so ignore considering them
-                if (!org.apache.commons.lang.ObjectUtils.equals(key, KFSPropertyConstants.DOCUMENT_NUMBER) && !org.apache.commons.lang.ObjectUtils.equals(key, KFSPropertyConstants.SEQUENCE_NUMBER) && !org.apache.commons.lang.ObjectUtils.equals(thisValue, otherValue)) {
-                    countInequalities++;
-                }
-            }
-            isLike = (countInequalities == 0);
-        }
-
-        // if they're the same, the only change was the amount
-        return isLike;
-    }
 }
 

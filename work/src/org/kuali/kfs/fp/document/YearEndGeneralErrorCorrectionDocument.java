404556af54a2e9692a3125c5cc7058b54264c260
Cathy Tan <tanc@ais.msu.edu>
2013-05-03 20:55:01 +0000

    KFSCNTRB-1124 : Tables not populating correctly for FP Period 13 FP Transactions

--- work/src/org/kuali/kfs/fp/document/YearEndGeneralErrorCorrectionDocument.java
+++ work/src/org/kuali/kfs/fp/document/YearEndGeneralErrorCorrectionDocument.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2005 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 package org.kuali.kfs.fp.document;
 
+import org.kuali.kfs.coa.businessobject.AccountingPeriod;
 import org.kuali.kfs.fp.document.service.YearEndPendingEntryService;
 import org.kuali.kfs.sys.businessobject.AccountingLine;
 import org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntry;
@@ -40,15 +41,15 @@ public class YearEndGeneralErrorCorrectionDocument extends GeneralErrorCorrectio
     public YearEndGeneralErrorCorrectionDocument() {
         super();
     }
-    
+
     /**
      * Set attributes of an explicit pending entry according to rules specific to GeneralErrorCorrectionDocument.<br/> <br/> Uses
      * <code>{@link YearEndDocumentUtil#customizeExplicitGeneralLedgerPendingEntry(AccountingDocument, AccountingLine, GeneralLedgerPendingEntry)}</code>
-     * 
+     *
      * @param accountingDocument The accounting document containing the general ledger pending entries being customized.
      * @param accountingLine The accounting line the explicit general ledger pending entry was generated from.
      * @param explicitEntry The explicit general ledger pending entry to be customized.
-     * 
+     *
      * @see org.kuali.kfs.fp.document.validation.impl.GeneralErrorCorrectionDocumentRule#customizeExplicitGeneralLedgerPendingEntry(org.kuali.kfs.sys.document.AccountingDocument,
      *      org.kuali.rice.krad.bo.AccountingLine, org.kuali.module.gl.bo.GeneralLedgerPendingEntry)
      * @see YearEndDocumentUtil#customizeExplicitGeneralLedgerPendingEntry(TransactionalDocument, AccountingLine,
@@ -59,7 +60,7 @@ public class YearEndGeneralErrorCorrectionDocument extends GeneralErrorCorrectio
         super.customizeExplicitGeneralLedgerPendingEntry(postable, explicitEntry);
         SpringContext.getBean(YearEndPendingEntryService.class).customizeExplicitGeneralLedgerPendingEntry(this, (AccountingLine)postable, explicitEntry);
     }
-    
+
     /**
      * Overridden to populate object code from last year's offset definition
      * @see org.kuali.kfs.sys.document.AccountingDocumentBase#customizeOffsetGeneralLedgerPendingEntry(org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntrySourceDetail, org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntry, org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntry)
@@ -74,5 +75,16 @@ public class YearEndGeneralErrorCorrectionDocument extends GeneralErrorCorrectio
     @Override
     public Class<? extends AccountingDocument> getDocumentClassForAccountingLineValueAllowedValidation() {
         return GeneralErrorCorrectionDocument.class;
+    }
+
+    @Override
+    public void setAccountingPeriod(AccountingPeriod accountingPeriod) {
+        //Need to override this method to set the posting year and
+        //posting period code so that we can have period 13 in the document's
+        //table in the database so that it's consistent with the GL Entry's
+        //posting year and posting period.
+        YearEndPendingEntryService yearEndPendingEntryService = SpringContext.getBean(YearEndPendingEntryService.class);
+        setPostingYear(yearEndPendingEntryService.getPreviousFiscalYear());
+        this.setPostingPeriodCode(yearEndPendingEntryService.getFinalAccountingPeriod());
     }
 }

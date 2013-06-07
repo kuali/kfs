1d0231c482aab1d213909f7cf0f0efe6f421d09f
Harsha Ummerpillai <harsha07@msu.edu>
2013-06-07 20:19:52 +0000

    KFSCNTRB-716

--- test/unit/src/org/kuali/kfs/vnd/fixture/VendorDefaultAddressFixture.java
+++ test/unit/src/org/kuali/kfs/vnd/fixture/VendorDefaultAddressFixture.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,20 @@ import org.kuali.kfs.vnd.businessobject.VendorDefaultAddress;
 
 public enum VendorDefaultAddressFixture {
 
-    campusBL("BL"), campusKO("KO"), campusIN("IN"), campusSB("SB"), ;
+    campusBL("BL",false), campusKO("KO",false), campusIN("IN",true), campusSB("SB",true), ;
 
     public final String vendorCampusCode;
+    public final boolean active;
 
-    private VendorDefaultAddressFixture(String vendorCampusCode) {
+    private VendorDefaultAddressFixture(String vendorCampusCode,boolean active) {
         this.vendorCampusCode = vendorCampusCode;
+        this.active=active;
     }
 
     public VendorDefaultAddress createAddress() {
         VendorDefaultAddress defaultAddress = new VendorDefaultAddress();
         defaultAddress.setVendorCampusCode(this.vendorCampusCode);
+        defaultAddress.setActive(this.active);
         return defaultAddress;
     }
 

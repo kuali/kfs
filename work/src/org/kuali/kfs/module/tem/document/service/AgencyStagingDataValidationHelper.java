b284627a313687beeefd8b98ff4c33363b484a66
Megha Doshi <mramawat@iu.edu>
2013-06-11 17:46:12 +0000

    KFSMI-9545

--- work/src/org/kuali/kfs/module/tem/document/service/AgencyStagingDataValidationHelper.java
+++ work/src/org/kuali/kfs/module/tem/document/service/AgencyStagingDataValidationHelper.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,11 +19,11 @@ import org.kuali.rice.kns.document.MaintenanceDocument;
 
 /**
  * Helper interface to bridge between trip and traveler implementations of travel agency auditing
- *  
+ *
  * @author Leo Przybylski (leo [at] rsmart.com)
  */
-public interface TravelAgencyAuditValidationHelper  {
-    
+public interface AgencyStagingDataValidationHelper  {
+
     /**
      * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#processCustomSaveDocumentBusinessRules(org.kuali.rice.kns.document.MaintenanceDocument)
      */

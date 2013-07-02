b721c8bafb44c621ebe3bb574ae1b7f84a1c69e9
Kevin Kronenbitter <kevin.kronenbitter@colostate.edu>
2013-07-02 14:03:46 +0000

    KFSMI-10409 Do not show the create profile buttons from a lookup within a document.

--- work/src/org/kuali/kfs/module/tem/businessobject/lookup/TEMProfileLookupableHelperServiceImpl.java
+++ work/src/org/kuali/kfs/module/tem/businessobject/lookup/TEMProfileLookupableHelperServiceImpl.java
@@ -179,6 +179,10 @@ public class TEMProfileLookupableHelperServiceImpl extends KualiLookupableHelper
     @Override
     public String getSupplementalMenuBar() {
 
+        // Do not show the extra actions
+        if (ObjectUtils.isNotNull(parameters.get("docNum")))  {
+            return KFSConstants.EMPTY_STRING;
+        }
         // next we need to get the current user's info
         UserSession user = GlobalVariables.getUserSession();
 

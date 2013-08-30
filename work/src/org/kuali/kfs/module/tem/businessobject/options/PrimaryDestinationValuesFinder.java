06b1908f4133c17bdb3ea3b29c084ba1f86be610
Kevin Kronenbitter <kevin.kronenbitter@colostate.edu>
2013-08-30 14:58:31 +0000

    KFSMI-11320 second pass at fixing.

--- work/src/org/kuali/kfs/module/tem/businessobject/options/PrimaryDestinationValuesFinder.java
+++ work/src/org/kuali/kfs/module/tem/businessobject/options/PrimaryDestinationValuesFinder.java
@@ -16,19 +16,10 @@
 package org.kuali.kfs.module.tem.businessobject.options;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
-import org.kuali.kfs.module.tem.TemConstants;
-import org.kuali.kfs.module.tem.businessobject.PrimaryDestination;
-import org.kuali.kfs.module.tem.service.TravelService;
-import org.kuali.kfs.sys.context.SpringContext;
-import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.keyvalues.KeyValuesBase;
-import org.kuali.rice.krad.service.BusinessObjectService;
 
 public class PrimaryDestinationValuesFinder extends KeyValuesBase {
 
@@ -39,40 +30,6 @@ public class PrimaryDestinationValuesFinder extends KeyValuesBase {
     @Override
     public List<KeyValue> getKeyValues() {
         List<KeyValue> keyValues = new ArrayList<KeyValue>();
-        keyValues.add(new ConcreteKeyValue("", ""));
-        BusinessObjectService service = SpringContext.getBean(BusinessObjectService.class);
-        TravelService travelService = SpringContext.getBean(TravelService.class);
-        Map<String,String> fieldValues = new HashMap<String, String>();
-
-        List<PrimaryDestination> primaryDestinationsInternational = travelService.findAllDistinctPrimaryDestinations(TemConstants.TEMTripTypes.INTERNATIONAL);
-
-        List<PrimaryDestination> primaryDestinationsDomestic = travelService.findAllDistinctPrimaryDestinations(TemConstants.TEMTripTypes.DOMESTIC);
-
-
-        Iterator<PrimaryDestination> it = primaryDestinationsDomestic.iterator();
-
-        String key = "";
-        while (it.hasNext()){
-            PrimaryDestination primaryDestination = it.next();
-
-            String tempKey = primaryDestination.getPrimaryDestinationName();
-            if (!tempKey.equals(key)){
-                keyValues.add(new ConcreteKeyValue(primaryDestination.getPrimaryDestinationName().toUpperCase(), primaryDestination.getPrimaryDestinationName().toUpperCase()));
-            }
-            key = tempKey;
-        }
-        keyValues.add(new ConcreteKeyValue("---", "------------------------------------------"));
-        it =primaryDestinationsInternational.iterator();
-        while (it.hasNext()){
-            PrimaryDestination primaryDestination = it.next();
-            //skip dummy value for custom expenses
-
-            String tempKey = primaryDestination.getPrimaryDestinationName();
-            if (!tempKey.equals(key)){
-                keyValues.add(new ConcreteKeyValue(primaryDestination.getPrimaryDestinationName().toUpperCase(), primaryDestination.getPrimaryDestinationName().toUpperCase()));
-            }
-            key = tempKey;
-        }
 
 
         return keyValues;

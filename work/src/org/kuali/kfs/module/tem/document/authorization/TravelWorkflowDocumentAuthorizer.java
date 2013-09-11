1c8119afec978c9fd75447440b8cab2cc5a28284
Kevin Kronenbitter <kevin.kronenbitter@colostate.edu>
2013-09-11 19:58:22 +0000

    KFSMI-11419 Added a workflow authorizer to TravelAuth to pass in qualifiers for initiate.

---
+++ work/src/org/kuali/kfs/module/tem/document/authorization/TravelWorkflowDocumentAuthorizer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013 The Kuali Foundation.
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.tem.document.authorization;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kuali.kfs.module.tem.identity.TemKimAttributes;
+import org.kuali.rice.kew.api.KewApiConstants;
+import org.kuali.rice.kew.doctype.bo.DocumentType;
+import org.kuali.rice.kew.doctype.service.impl.KimDocumentTypeAuthorizer;
+import org.kuali.rice.krad.util.KRADConstants;
+
+/**
+ *
+ */
+public class TravelWorkflowDocumentAuthorizer extends KimDocumentTypeAuthorizer {
+
+    @Override
+    public boolean canInitiate(String principalId, DocumentType documentType) {
+        validatePrincipalId(principalId);
+        validateDocumentType(documentType);
+
+        Map<String, String> permissionDetails = buildDocumentTypePermissionDetails(documentType, null, null, null);
+        Map<String, String> qualificationDetails = new HashMap<String, String>();
+        qualificationDetails.put(TemKimAttributes.PROFILE_PRINCIPAL_ID, principalId);
+
+        if (useKimPermission(KRADConstants.KUALI_RICE_SYSTEM_NAMESPACE, KewApiConstants.INITIATE_PERMISSION, permissionDetails, true)) {
+            return getPermissionService().isAuthorizedByTemplate(principalId, KRADConstants.KUALI_RICE_SYSTEM_NAMESPACE,
+                    KewApiConstants.INITIATE_PERMISSION, permissionDetails, qualificationDetails);
+        }
+        return true;
+
+
+
+
+    }
+
+}

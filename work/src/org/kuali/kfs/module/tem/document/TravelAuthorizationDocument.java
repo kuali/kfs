4e0ea44ea0f6cacde645ac84e8ccd27b888e2c6c
James Smith <smith750@iu.edu>
2013-09-12 21:05:04 +0000

    KFSMI-11438: when creating a new TA, set the due date on the travel payment to today's date so it validates

--- work/src/org/kuali/kfs/module/tem/document/TravelAuthorizationDocument.java
+++ work/src/org/kuali/kfs/module/tem/document/TravelAuthorizationDocument.java
@@ -151,6 +151,8 @@ public class TravelAuthorizationDocument extends TravelDocumentBase implements P
         getAdvanceTravelPayment().setDocumentationLocationCode(getParameterService().getParameterValueAsString(TravelAuthorizationDocument.class, TravelParameters.DOCUMENTATION_LOCATION_CODE,
                 getParameterService().getParameterValueAsString(TemParameterConstants.TEM_DOCUMENT.class,TravelParameters.DOCUMENTATION_LOCATION_CODE)));
         getAdvanceTravelPayment().setCheckStubText(getConfigurationService().getPropertyValueAsString(TemKeyConstants.MESSAGE_TA_ADVANCE_PAYMENT_HOLD_TEXT));
+        final java.sql.Date currentDate = getDateTimeService().getCurrentSqlDate();
+        getAdvanceTravelPayment().setDueDate(currentDate);
         updatePayeeTypeForAuthorization(); // if the traveler is already initialized, set up payee type on advance travel payment
         setWireTransfer(new PaymentSourceWireTransfer());
         getWireTransfer().setDocumentNumber(getDocumentNumber());

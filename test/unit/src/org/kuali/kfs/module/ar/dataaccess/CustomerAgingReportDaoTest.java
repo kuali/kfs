48382c3c8b47f6843ea7ed3372a2878a600d858c
Mital Pritmani <mpritmani@vivantech.com>
2013-10-14 11:47:48 +0000

    KFSMI-10824: Collector Hierarchy MD removed.

--- test/unit/src/org/kuali/kfs/module/ar/dataaccess/CustomerAgingReportDaoTest.java
+++ test/unit/src/org/kuali/kfs/module/ar/dataaccess/CustomerAgingReportDaoTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ojb.broker.query.Criteria;
-import org.kuali.kfs.module.ar.businessobject.CollectorHierarchy;
 import org.kuali.kfs.module.ar.businessobject.Event;
 import org.kuali.kfs.module.ar.fixture.CustomerInvoiceDetailFixture;
 import org.kuali.kfs.module.ar.fixture.CustomerInvoiceDocumentFixture;
@@ -33,7 +32,7 @@ import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 
 /**
- * This class tests the methods of CollectorHierarchyDao.
+ * This class tests the methods of CustomerAgingReportDao.
  */
 @ConfigureContext(session = khuntley)
 public class CustomerAgingReportDaoTest extends KualiTestBase {

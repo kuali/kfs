f25c445e5e51b0c2f67a5f03680a42cc54017310
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2013-11-21 03:14:44 +0000

    KFSMI-11826 fix doc type and bo setup to address AgencyAddressTypeTest unit test failures

--- test/unit/src/org/kuali/kfs/module/cg/document/AgencyAddressTypeTest.java
+++ test/unit/src/org/kuali/kfs/module/cg/document/AgencyAddressTypeTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,13 +21,12 @@ import static org.kuali.kfs.sys.fixture.UserNameFixture.khuntley;
 import org.kuali.kfs.module.cg.businessobject.AgencyAddressType;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
-import org.kuali.kfs.sys.context.SpringContext; import org.kuali.rice.krad.service.DocumentService;
+import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.document.MaintenanceDocumentTestUtils;
 import org.kuali.kfs.sys.fixture.UserNameFixture;
-import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.krad.service.DocumentService;
-import org.kuali.kfs.sys.context.SpringContext; import org.kuali.rice.krad.service.DocumentService;
 
 /**
  * This class is used to test the AgencyAddressType MaintenanceDocument
@@ -46,15 +45,17 @@ public class AgencyAddressTypeTest extends KualiTestBase {
     /**
      * @see junit.framework.TestCase#setUp()
      */
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
-        document = (MaintenanceDocument) SpringContext.getBean(DocumentService.class).getNewDocument("CGAA");
+        document = (MaintenanceDocument) SpringContext.getBean(DocumentService.class).getNewDocument("AATY");
         document.getDocumentHeader().setDocumentDescription("Test Document");
         documentService = SpringContext.getBean(DocumentService.class);
         agencyAddressType = new AgencyAddressType();
         agencyAddressType.setAgencyAddressTypeCode(AGENCY_ADDRESS_TYPE_CODE);
         agencyAddressType.setAgencyAddressTypeDescription(AGENCY_ADDRESS_TYPE_DESCRIPTION);
         agencyAddressType.setActive(ACTIVE);
+        agencyAddressType.setVersionNumber(new Long(1));
         document.getNewMaintainableObject().setBusinessObject(agencyAddressType);
         document.getNewMaintainableObject().setBoClass(agencyAddressType.getClass());
 
@@ -67,7 +68,7 @@ public class AgencyAddressTypeTest extends KualiTestBase {
     }
 
     public void testGetNewDocument() throws Exception {
-        Document document = (Document) documentService.getNewDocument("CGAA");
+        Document document = documentService.getNewDocument("AATY");
         // verify document was created
         assertNotNull(document);
         assertNotNull(document.getDocumentHeader());

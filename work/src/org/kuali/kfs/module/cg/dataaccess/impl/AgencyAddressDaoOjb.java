36dacb9105c1dc0ba55018b2171340856c2fd12a
Nishant Contractor <ncontractor@vivantech.com>
2013-12-08 05:21:31 +0000

    KFSMI-11303 Tried to fix unit test issues

--- work/src/org/kuali/kfs/module/cg/dataaccess/impl/AgencyAddressDaoOjb.java
+++ work/src/org/kuali/kfs/module/cg/dataaccess/impl/AgencyAddressDaoOjb.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 The Kuali Foundation
- *
+ * 
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.opensource.org/licenses/ecl2.php
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,16 +15,18 @@
  */
 package org.kuali.kfs.module.cg.dataaccess.impl;
 
+import java.math.BigDecimal;
+import java.util.Iterator;
+
 import org.apache.ojb.broker.query.Criteria;
 import org.apache.ojb.broker.query.QueryFactory;
 import org.apache.ojb.broker.query.ReportQueryByCriteria;
-import org.apache.xpath.operations.String;
 import org.kuali.kfs.module.cg.CGKeyConstants;
 import org.kuali.kfs.module.cg.CGPropertyConstants;
 import org.kuali.kfs.module.cg.businessobject.AgencyAddress;
 import org.kuali.kfs.module.cg.dataaccess.AgencyAddressDao;
-import org.kuali.kfs.sys.util.TransactionalServiceUtils;
 import org.kuali.rice.core.framework.persistence.ojb.dao.PlatformAwareDaoBaseOjb;
+import org.kuali.kfs.sys.util.TransactionalServiceUtils;
 
 /**
  * Agency Address Dao OJB object that implements AgencyAddressDao

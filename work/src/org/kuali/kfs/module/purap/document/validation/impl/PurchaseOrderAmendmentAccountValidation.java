ef456b253c6125196323bab7c61543a66487ad93
Irina Naydich <irina.naydich@cornell.edu>
2013-12-11 21:30:31 +0000

    KFSCNTRB-1811- fixed Exception when submit POA with empty qty or unit cost

--- work/src/org/kuali/kfs/module/purap/document/validation/impl/PurchaseOrderAmendmentAccountValidation.java
+++ work/src/org/kuali/kfs/module/purap/document/validation/impl/PurchaseOrderAmendmentAccountValidation.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008-2009 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ import org.kuali.rice.krad.util.GlobalVariables;
 
 public class PurchaseOrderAmendmentAccountValidation extends GenericValidation {
 
+    @Override
     public boolean validate(AttributedDocumentEvent event) {
 
         boolean valid = true;
@@ -38,17 +39,17 @@ public class PurchaseOrderAmendmentAccountValidation extends GenericValidation {
 
         PurchaseOrderDocument po = SpringContext.getBean(PurchaseOrderService.class).getCurrentPurchaseOrder(poaDocument.getPurapDocumentIdentifier());
         List<PurApItem> poItems = po.getItems();
-                
+
         for (PurApItem item : items) {
             String identifierString = item.getItemIdentifierString();
-            
+
             // check to see if the account has been expired or inactive
-            if (item.getItemTypeCode().equals(PurapConstants.ItemTypeCodes.ITEM_TYPE_ITEM_CODE) && item.getSourceAccountingLines() != null && item.getSourceAccountingLines().size() > 0) {            
+            if (item.getItemTypeCode().equals(PurapConstants.ItemTypeCodes.ITEM_TYPE_ITEM_CODE) && item.getSourceAccountingLines() != null && item.getSourceAccountingLines().size() > 0) {
 
                 // check only if there are changes on the item
                 if (isItemChanged(item, poItems)) {
                     List<PurApAccountingLine> accountingLines = item.getSourceAccountingLines();
-                    for (PurApAccountingLine accountingLine : accountingLines) {                
+                    for (PurApAccountingLine accountingLine : accountingLines) {
                         if (accountingLine.getAccount().isExpired()) {
                             valid = false;
                             GlobalVariables.getMessageMap().putError(PurapConstants.ITEM_TAB_ERROR_PROPERTY, PurapKeyConstants.ERROR_ITEM_ACCOUNT_EXPIRED, accountingLine.getAccount().getAccountNumber());
@@ -60,32 +61,33 @@ public class PurchaseOrderAmendmentAccountValidation extends GenericValidation {
                             break;
                         }
                     }
-                } 
-            }            
-        }   
-        
+                }
+            }
+        }
+
         return valid;
     }
 
     private boolean isItemChanged(PurApItem poaItem, List<PurApItem> poItems) {
-        
+
         boolean changed = false;
-        
+
         int poaItemId = poaItem.getItemLineNumber().intValue();
-        		
+
         for (PurApItem poItem : poItems) {
-        
+
             if (poItem.getItemTypeCode().equals(PurapConstants.ItemTypeCodes.ITEM_TYPE_ITEM_CODE) && poaItemId == poItem.getItemLineNumber().intValue()) {
                 // required fields so they cannot be null
-                if (poaItem.getItemQuantity().intValue() != poItem.getItemQuantity().intValue()) {
+                if (poaItem.getItemQuantity() == null ||  poaItem.getItemQuantity().intValue() != poItem.getItemQuantity().intValue()) {
                     changed = true;
                 }
                 if (!poaItem.getItemUnitOfMeasureCode().equals(poItem.getItemUnitOfMeasureCode())) {
                     changed = true;
                 }
-                if  (poaItem.getItemUnitPrice().floatValue() != poItem.getItemUnitPrice().floatValue()) {
+                if  (poaItem.getItemUnitPrice() == null || poaItem.getItemUnitPrice().floatValue() != poItem.getItemUnitPrice().floatValue()) {
                     changed = true;
                 }
+
                 if (poaItem.getTotalAmount().floatValue() != poItem.getTotalAmount().floatValue()) {
                     changed = true;
                 }
@@ -97,7 +99,7 @@ public class PurchaseOrderAmendmentAccountValidation extends GenericValidation {
                     (poItem.getItemCatalogNumber() != null && !poItem.getItemCatalogNumber().equals(poaItem.getItemCatalogNumber()))) {
                     changed = true;
                 }
-                if ((poaItem.getItemDescription() != null && !poaItem.getItemDescription().equals(poItem.getItemDescription())) || 
+                if ((poaItem.getItemDescription() != null && !poaItem.getItemDescription().equals(poItem.getItemDescription())) ||
                     (poItem.getItemDescription() != null && !poItem.getItemDescription().equals(poaItem.getItemDescription()))) {
                     changed = true;
                 }
@@ -111,11 +113,11 @@ public class PurchaseOrderAmendmentAccountValidation extends GenericValidation {
                     (poaItem.getItemTaxAmount() == null && poItem.getItemTaxAmount() != null && poItem.getItemTaxAmount().floatValue() != 0)) {
                     changed = true;
                 }
-                
+
                 break;
             }
         }
-        
+
         return changed;
     }
 }

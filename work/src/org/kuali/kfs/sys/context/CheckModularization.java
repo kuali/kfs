927de8efda1b4b4fec9fcf6f036177febc9e3b38
James Smith <smith750@iu.edu>
2014-01-27 16:03:23 +0000

    merged from release-5-1-2

--- work/src/org/kuali/kfs/sys/context/CheckModularization.java
+++ work/src/org/kuali/kfs/sys/context/CheckModularization.java
@@ -433,7 +433,13 @@ public class CheckModularization {
         InputStream in = resourceLoader.getResource(fileName).getInputStream();
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setValidating(true);
+        dbf.setValidating(false);
+        dbf.setNamespaceAware(true);
+        dbf.setFeature("http://xml.org/sax/features/namespaces", false);
+        dbf.setFeature("http://xml.org/sax/features/validation", false);
+        dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+        dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
 
         DocumentBuilder db = dbf.newDocumentBuilder();
         db.setErrorHandler(new LogErrorHandler());

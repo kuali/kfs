4c0de7c95473bfa29aaf236eb0434382588818f4
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2014-01-29 21:23:30 +0000

    KFSTI-77 refactor one more service to use dependency injection

--- work/src/org/kuali/kfs/module/cg/identity/ResearchRiskReviewRoleTypeServiceImpl.java
+++ work/src/org/kuali/kfs/module/cg/identity/ResearchRiskReviewRoleTypeServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,15 +19,14 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kew.api.doctype.DocumentTypeService;
 import org.kuali.rice.kim.api.KimConstants;
 import org.kuali.rice.kim.util.KimCommonUtils;
 import org.kuali.rice.kns.kim.role.RoleTypeServiceBase;
 
 public class ResearchRiskReviewRoleTypeServiceImpl extends RoleTypeServiceBase {
-    DocumentTypeService documentTypeService;
-    
+    protected DocumentTypeService documentTypeService;
+
     @Override
     protected boolean performMatch(Map<String,String> qualification, Map<String,String> roleQualifier) {
         if (KimCommonUtils.storedValueNotSpecifiedOrInputValueMatches(roleQualifier, qualification, CgKimAttributes.RESEARCH_RISK_TYPE_CODE)) {
@@ -35,15 +34,17 @@ public class ResearchRiskReviewRoleTypeServiceImpl extends RoleTypeServiceBase {
             if (roleQualifier.containsKey(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)) {
                 potentialParentDocumentTypeNames.add(roleQualifier.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME));
             }
-            return potentialParentDocumentTypeNames.isEmpty() || qualification.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME).equalsIgnoreCase(roleQualifier.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)) || (KimCommonUtils.getClosestParentDocumentTypeName(getDocumentTypeService().getDocumentTypeByName(qualification.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)), potentialParentDocumentTypeNames) != null);
+            return potentialParentDocumentTypeNames.isEmpty() || qualification.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME).equalsIgnoreCase(roleQualifier.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)) || (KimCommonUtils.getClosestParentDocumentTypeName(documentTypeService.getDocumentTypeByName(qualification.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)), potentialParentDocumentTypeNames) != null);
         }
         return false;
     }
-    
-    protected DocumentTypeService getDocumentTypeService() {
-        if (documentTypeService == null) {
-            documentTypeService = SpringContext.getBean(DocumentTypeService.class);
-        }
-        return this.documentTypeService;
+
+    @Override
+    public DocumentTypeService getDocumentTypeService() {
+        return documentTypeService;
+    }
+
+    public void setDocumentTypeService(DocumentTypeService documentTypeService) {
+        this.documentTypeService = documentTypeService;
     }
 }

cc597a83ff3ceb220e40724f078e20020c12bdbd
Ramya	Rajesh <rrajesh@vivantech.com>
2014-02-03 22:02:16 +0000

    KFSTP-907 - "Account Invoice Template" vs "Invoice Template" - make labels consistent

--- test/unit/src/org/kuali/kfs/module/ar/document/InvoiceTemplateTest.java
+++ test/unit/src/org/kuali/kfs/module/ar/document/InvoiceTemplateTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,7 +48,7 @@ public class InvoiceTemplateTest extends KualiTestBase {
      */
     protected void setUp() throws Exception {
         super.setUp();
-        document = (MaintenanceDocument) SpringContext.getBean(DocumentService.class).getNewDocument("AITM");
+        document = (MaintenanceDocument) SpringContext.getBean(DocumentService.class).getNewDocument("ITM");
         document.getDocumentHeader().setDocumentDescription("Test Document");
         documentService = SpringContext.getBean(DocumentService.class);
         invoiceTemplate = new InvoiceTemplate();
@@ -66,7 +66,7 @@ public class InvoiceTemplateTest extends KualiTestBase {
 
 
     public void testGetNewDocument() throws Exception {
-        Document document = (Document) documentService.getNewDocument("AITM");
+        Document document = documentService.getNewDocument("ITM");
         // verify document was created
         assertNotNull(document);
         assertNotNull(document.getDocumentHeader());

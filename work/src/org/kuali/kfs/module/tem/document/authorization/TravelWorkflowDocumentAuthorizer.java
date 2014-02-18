b8710f7cf5e423a2eb8fdc7ad7b1105a7d8673e7
James Smith <smith750@iu.edu>
2014-02-18 23:39:29 +0000

    KFSTP-1055: make sure that route permission check works with KFS-TEM TEM Profile permission

--- work/src/org/kuali/kfs/module/tem/document/authorization/TravelWorkflowDocumentAuthorizer.java
+++ work/src/org/kuali/kfs/module/tem/document/authorization/TravelWorkflowDocumentAuthorizer.java
@@ -22,6 +22,7 @@ import org.kuali.kfs.module.tem.identity.TemKimAttributes;
 import org.kuali.rice.kew.api.KewApiConstants;
 import org.kuali.rice.kew.doctype.bo.DocumentType;
 import org.kuali.rice.kew.doctype.service.impl.KimDocumentTypeAuthorizer;
+import org.kuali.rice.kew.routeheader.DocumentRouteHeaderValue;
 import org.kuali.rice.krad.util.KRADConstants;
 
 /**
@@ -35,8 +36,7 @@ public class TravelWorkflowDocumentAuthorizer extends KimDocumentTypeAuthorizer 
         validateDocumentType(documentType);
 
         Map<String, String> permissionDetails = buildDocumentTypePermissionDetails(documentType, null, null, null);
-        Map<String, String> qualificationDetails = new HashMap<String, String>();
-        qualificationDetails.put(TemKimAttributes.PROFILE_PRINCIPAL_ID, principalId);
+        Map<String, String> qualificationDetails = getRoleQualifiers(principalId);
 
         if (useKimPermission(KRADConstants.KUALI_RICE_SYSTEM_NAMESPACE, KewApiConstants.INITIATE_PERMISSION, permissionDetails, true)) {
             return getPermissionService().isAuthorizedByTemplate(principalId, KRADConstants.KUALI_RICE_SYSTEM_NAMESPACE,
@@ -45,4 +45,44 @@ public class TravelWorkflowDocumentAuthorizer extends KimDocumentTypeAuthorizer 
         return true;
     }
 
+    @Override
+    public boolean canRoute(String principalId, DocumentRouteHeaderValue document) {
+        validatePrincipalId(principalId);
+        validateDocument(document);
+        String documentId = document.getDocumentId();
+        DocumentType documentType = document.getDocumentType();
+        String documentStatus = document.getDocRouteStatus();
+        String initiatorPrincipalId = document.getInitiatorWorkflowId();
+        validateDocumentType(documentType);
+        validateDocumentStatus(documentStatus);
+        validatePrincipalId(initiatorPrincipalId);
+
+        if (!documentType.isPolicyDefined(org.kuali.rice.kew.api.doctype.DocumentTypePolicy.INITIATOR_MUST_ROUTE)) {
+            Map<String, String> permissionDetails = buildDocumentTypePermissionDetails(documentType, documentStatus, null, null);
+            Map<String, String> roleQualifiers = buildDocumentRoleQualifiers(document, permissionDetails.get(KewApiConstants.ROUTE_NODE_NAME_DETAIL));
+            roleQualifiers.put(TemKimAttributes.PROFILE_PRINCIPAL_ID, principalId);
+
+            if (useKimPermission(KewApiConstants.KEW_NAMESPACE, KewApiConstants.ROUTE_PERMISSION, permissionDetails, true)) {
+                return getPermissionService().isAuthorizedByTemplate(principalId, KewApiConstants.KEW_NAMESPACE,
+                        KewApiConstants.ROUTE_PERMISSION, permissionDetails, roleQualifiers);
+            }
+        }
+
+        if (documentType.getInitiatorMustRoutePolicy().getPolicyValue()) {
+            return super.canRoute(principalId, document);
+        }
+        return true;
+    }
+
+    /**
+     * Creates a role qualifiers map that will work with the KFS-TEM TEM Profile role
+     *
+     * @param principalId
+     * @return
+     */
+    protected Map<String, String> getRoleQualifiers(String principalId) {
+        Map<String, String> qualificationDetails = new HashMap<String, String>();
+        qualificationDetails.put(TemKimAttributes.PROFILE_PRINCIPAL_ID, principalId);
+        return qualificationDetails;
+    }
 }

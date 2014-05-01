add665be83905e0ef247132af0f24dcc8feac8a9
James Smith <smith750@iu.edu>
2014-05-01 14:54:25 +0000

    merged from release-5-3

--- test/unit/src/org/kuali/kfs/fp/document/validation/impl/AdvanceDepositDocumentRuleTest.java
+++ test/unit/src/org/kuali/kfs/fp/document/validation/impl/AdvanceDepositDocumentRuleTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,19 +40,20 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * test that an <code>IllegalStateException</code> gets thrown for an error correction document
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_errorCorrection() throws Exception {
         AccountingDocument accountingDocument = IsDebitTestUtils.getErrorCorrectionDocument(SpringContext.getBean(DocumentService.class), AdvanceDepositDocument.class);
         AccountingLine accountingLine = IsDebitTestUtils.getIncomeLine(accountingDocument, SourceAccountingLine.class, POSITIVE);
 
-        assertTrue(IsDebitTestUtils.isErrorCorrectionIllegalStateException(SpringContext.getBean(DataDictionaryService.class), SpringContext.getBean(DataDictionaryService.class), accountingDocument, accountingLine));
+        // now that we allow error correct on AD, we shall not throw any IllegalStateException
+        assertFalse(IsDebitTestUtils.isErrorCorrectionIllegalStateException(SpringContext.getBean(DataDictionaryService.class), SpringContext.getBean(DataDictionaryService.class), accountingDocument, accountingLine));
     }
 
     /**
      * tests false is returned for a positive income
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_income_positveAmount() throws Exception {
@@ -64,7 +65,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests true is returned for a negative income
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_income_negativeAmount() throws Exception {
@@ -77,7 +78,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests an <code>IllegalStateException</code> is thrown for a zero income
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_income_zeroAmount() throws Exception {
@@ -89,7 +90,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests true is returned for a positive expense
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_expense_positveAmount() throws Exception {
@@ -101,7 +102,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests false is returned for a negative expense
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_expense_negativeAmount() throws Exception {
@@ -114,7 +115,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests an <code>IllegalStateException</code> is thrown for a zero expense
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_expense_zeroAmount() throws Exception {
@@ -126,7 +127,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests true is returned for a positive asset
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_asset_positveAmount() throws Exception {
@@ -138,7 +139,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests false is returned for a negative asset
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_asset_negativeAmount() throws Exception {
@@ -151,7 +152,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests an <code>IllegalStateException</code> is thrown for a zero asset
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_asset_zeroAmount() throws Exception {
@@ -163,7 +164,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests false is returned for a positive liability
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_liability_positveAmount() throws Exception {
@@ -175,7 +176,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests true is returned for a negative liability
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_liability_negativeAmount() throws Exception {
@@ -188,7 +189,7 @@ public class AdvanceDepositDocumentRuleTest extends KualiTestBase {
 
     /**
      * tests an <code>IllegalStateException</code> is thrown for a zero liability
-     * 
+     *
      * @throws Exception
      */
     public void testIsDebit_liability_zeroAmount() throws Exception {

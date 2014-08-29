bc0332b599e34fcb7f0d2180d34374ba6ee1bc97
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2014-08-29 13:27:27 -0600

    if there are no valid awards, don't try to create CINV docs (and avoid an unnecessary error message)

--- work/src/org/kuali/kfs/module/ar/web/struts/ContractsGrantsInvoiceSummaryAction.java
+++ work/src/org/kuali/kfs/module/ar/web/struts/ContractsGrantsInvoiceSummaryAction.java
@@ -144,7 +144,9 @@ public class ContractsGrantsInvoiceSummaryAction extends KualiAction {
             awards = cgInvoiceDocumentCreateService.validateAwards(awards, contractsGrantsInvoiceDocumentErrorLogs, null, ArConstants.ContractsAndGrantsInvoiceDocumentCreationProcessType.MANUAL.getCode());
             validationErrors += contractsGrantsInvoiceDocumentErrorLogs.size();
             validAwards += awards.size();
-            errorMessages = cgInvoiceDocumentCreateService.createCGInvoiceDocumentsByAwards(awards, ArConstants.ContractsAndGrantsInvoiceDocumentCreationProcessType.MANUAL);
+            if (awards.size() > 0) {
+                errorMessages = cgInvoiceDocumentCreateService.createCGInvoiceDocumentsByAwards(awards, ArConstants.ContractsAndGrantsInvoiceDocumentCreationProcessType.MANUAL);
+            }
         }
         if (validationErrors > 0) {
             // At a minimum, show users a message that errors occurred, check report for details.

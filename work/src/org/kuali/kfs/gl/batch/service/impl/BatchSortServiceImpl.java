3aa88045ade6f88620ca6cf634ffdeecf5455188
James Smith <smith750@iu.edu>
2014-08-29 13:31:07 -0600

    KFSTP-1306: remove unnecessary catch of IOException

--- work/src/org/kuali/kfs/gl/batch/service/impl/BatchSortServiceImpl.java
+++ work/src/org/kuali/kfs/gl/batch/service/impl/BatchSortServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008-2009 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +32,8 @@ import org.kuali.kfs.gl.batch.service.BatchSortService;
  */
 public class BatchSortServiceImpl implements BatchSortService {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(BatchSortServiceImpl.class);
-    
+
+    @Override
     public void sortTextFileWithFields(String inputFileName, String outputFileName, Comparator comparator){
         FileReader inputFile = null;
         PrintStream outputFileStream = null;
@@ -43,34 +44,31 @@ public class BatchSortServiceImpl implements BatchSortService {
         catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
         List<String> lineList = new ArrayList();
         BufferedReader inputBufferedReader = new BufferedReader(inputFile);
-        
+
         try {
             String currentLine = inputBufferedReader.readLine();
             while (currentLine != null) {
                 lineList.add(currentLine);
             }
-            inputBufferedReader.close();    
+            inputBufferedReader.close();
             outputFileStream.close();
         } catch (IOException e) {
             // FIXME: do whatever should be done here
             LOG.error("performDemerger Stopped: " + e.getMessage());
             throw new RuntimeException("sortTextFileWithFields() Stopped: " + e.getMessage(), e);
         }
-        
+
         Collections.sort(lineList, comparator);
-        
+
         for (String line: lineList){
             outputFileStream.printf("%s\n", line);
         }
         outputFileStream.close();
-             
+
     }
-    
-    
+
+
 }

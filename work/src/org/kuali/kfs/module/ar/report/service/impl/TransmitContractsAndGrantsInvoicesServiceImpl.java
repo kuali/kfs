493366957af15674f2699723ea3bbd86a780607c
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2014-09-08 13:03:19 -0600

    KFSTP-1318,don't include error correction CINV docs in transmission process

--- work/src/org/kuali/kfs/module/ar/report/service/impl/TransmitContractsAndGrantsInvoicesServiceImpl.java
+++ work/src/org/kuali/kfs/module/ar/report/service/impl/TransmitContractsAndGrantsInvoicesServiceImpl.java
@@ -118,10 +118,12 @@ public class TransmitContractsAndGrantsInvoicesServiceImpl implements TransmitCo
         for (ContractsGrantsInvoiceDocument item : list) {
             ContractsGrantsInvoiceDocument invoice = (ContractsGrantsInvoiceDocument)getDocumentService().getByDocumentHeaderId(item.getDocumentNumber());
 
-            if (isInvoiceBetween(invoice, fromDate, toDate)) {
-                if ((StringUtils.equals(ArConstants.InvoiceTransmissionMethod.EMAIL, invoiceTransmissionMethodCode) && isInvoiceValidToEmail(invoice)) ||
-                    (StringUtils.equals(ArConstants.InvoiceTransmissionMethod.MAIL, invoiceTransmissionMethodCode) && isInvoiceValidToMail(invoice))) {
-                    finalList.add(invoice);
+            if (!invoice.isInvoiceReversal()) {
+                if (isInvoiceBetween(invoice, fromDate, toDate)) {
+                    if ((StringUtils.equals(ArConstants.InvoiceTransmissionMethod.EMAIL, invoiceTransmissionMethodCode) && isInvoiceValidToEmail(invoice)) ||
+                        (StringUtils.equals(ArConstants.InvoiceTransmissionMethod.MAIL, invoiceTransmissionMethodCode) && isInvoiceValidToMail(invoice))) {
+                        finalList.add(invoice);
+                    }
                 }
             }
         }

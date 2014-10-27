2ebe94a57dd5c7072d76e302031e6edd404dcde7
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2014-10-27 10:48:25 -0600

    KFSTI-299,fix unit test failure

--- test/unit/src/org/kuali/kfs/module/ar/fixture/ContractsGrantsInvoiceDocumentFixture.java
+++ test/unit/src/org/kuali/kfs/module/ar/fixture/ContractsGrantsInvoiceDocumentFixture.java
@@ -17,6 +17,7 @@ package org.kuali.kfs.module.ar.fixture;
 
 import java.sql.Date;
 
+import org.kuali.kfs.module.ar.businessobject.InvoiceGeneralDetail;
 import org.kuali.kfs.module.ar.document.ContractsGrantsInvoiceDocument;
 import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.krad.service.DocumentService;
@@ -34,12 +35,13 @@ public enum ContractsGrantsInvoiceDocumentFixture {
     private ContractsGrantsInvoiceDocumentFixture(Long proposalNumber, String billingDate) {
         this.billingDate = billingDate;
         this.proposalNumber = proposalNumber;
-
     }
 
     private void setValues(ContractsGrantsInvoiceDocument contractsGrantsInvoiceDocument) {
-        contractsGrantsInvoiceDocument.getInvoiceGeneralDetail().setProposalNumber(this.proposalNumber);
-        contractsGrantsInvoiceDocument.setBillingDate(Date.valueOf(this.billingDate));
+        InvoiceGeneralDetail invoiceGeneralDetail = new InvoiceGeneralDetail();
+        invoiceGeneralDetail.setProposalNumber(proposalNumber);
+        contractsGrantsInvoiceDocument.setInvoiceGeneralDetail(invoiceGeneralDetail);
+        contractsGrantsInvoiceDocument.setBillingDate(Date.valueOf(billingDate));
     }
 
 

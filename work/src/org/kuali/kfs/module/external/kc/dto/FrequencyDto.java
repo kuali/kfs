bc933219d872a38ae087900b662a584cd1d4f4f0
smith750 <smith750@iu.edu>
2014-12-29 11:04:44 -0500

    KFSTP-1657: make sure that serializability matches KC serializability

--- work/src/org/kuali/kfs/module/external/kc/dto/FrequencyDto.java
+++ work/src/org/kuali/kfs/module/external/kc/dto/FrequencyDto.java
@@ -1,28 +1,32 @@
 /*
  * The Kuali Financial System, a comprehensive financial management system for higher education.
- * 
+ *
  * Copyright 2005-2014 The Kuali Foundation
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.kuali.kfs.module.external.kc.dto;
 
-public class FrequencyDto {
+import java.io.Serializable;
+
+public class FrequencyDto implements Serializable {
+
+    private static final long serialVersionUID = -115792438972874644L;
 
 	private String frequencyCode;
 	private String description;
-	
+
 	public String getFrequencyCode() {
 		return frequencyCode;
 	}

3b72b2ffea7d0b6412f31da85bfd5e602a84779f
smith750 <smith750@iu.edu>
2015-01-22 15:42:52 -0500

    KFSTP-1548

--- work/src/org/kuali/kfs/module/ar/document/service/impl/ContractsGrantsLetterOfCreditReviewDocumentServiceImpl.java
+++ work/src/org/kuali/kfs/module/ar/document/service/impl/ContractsGrantsLetterOfCreditReviewDocumentServiceImpl.java
@@ -1,18 +1,18 @@
 /*
  * The Kuali Financial System, a comprehensive financial management system for higher education.
- * 
+ *
  * Copyright 2005-2014 The Kuali Foundation
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,7 +24,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.gl.businessobject.Balance;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAwardAccount;
@@ -33,7 +32,6 @@ import org.kuali.kfs.module.ar.businessobject.AwardAccountObjectCodeTotalBilled;
 import org.kuali.kfs.module.ar.dataaccess.AwardAccountObjectCodeTotalBilledDao;
 import org.kuali.kfs.module.ar.document.service.ContractsGrantsInvoiceDocumentService;
 import org.kuali.kfs.module.ar.document.service.ContractsGrantsLetterOfCreditReviewDocumentService;
-import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.kfs.sys.businessobject.SystemOptions;
 import org.kuali.kfs.sys.service.OptionsService;
@@ -41,7 +39,6 @@ import org.kuali.kfs.sys.service.UniversityDateService;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.service.KualiModuleService;
-import org.kuali.rice.krad.util.ObjectUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -98,45 +95,6 @@ public class ContractsGrantsLetterOfCreditReviewDocumentServiceImpl implements C
     }
 
     /**
-     * This method calculates the claim on cash balance for every award account.
-     *
-     * @param awardaccount
-     * @return
-     */
-    @Override
-    public KualiDecimal getClaimOnCashforAwardAccount(ContractsAndGrantsBillingAwardAccount awardAccount, java.sql.Date awardBeginningDate) {
-        KualiDecimal balAmt = KualiDecimal.ZERO;
-        KualiDecimal expAmt = KualiDecimal.ZERO;
-        KualiDecimal incAmt = KualiDecimal.ZERO;
-        KualiDecimal claimOnCash = KualiDecimal.ZERO;
-        Integer currentYear = getUniversityDateService().getCurrentFiscalYear();
-
-        Integer fiscalYear = getUniversityDateService().getFiscalYear(awardBeginningDate);
-
-        for (Integer currFiscalYear = fiscalYear; currFiscalYear <= currentYear; currFiscalYear++) {
-            final List<String> objectTypeCodeList = getIncomeAndExpenseObjectTypesForFiscalYear(currFiscalYear);
-            final Collection<Balance> glBalances = retrieveBalancesForAwardAccount(awardAccount, currFiscalYear, objectTypeCodeList);
-            final SystemOptions systemOptions = optionsService.getCurrentYearOptions();
-
-            for (Balance bal : glBalances) {
-                if (ObjectUtils.isNull(bal.getSubAccount()) || ObjectUtils.isNull(bal.getSubAccount().getA21SubAccount()) || !StringUtils.equalsIgnoreCase(bal.getSubAccount().getA21SubAccount().getSubAccountTypeCode(), KFSConstants.SubAccountType.COST_SHARE)) {
-                    if (bal.getObjectTypeCode().equalsIgnoreCase(systemOptions.getFinancialObjectTypeTransferExpenseCd())) {
-                        balAmt = bal.getContractsGrantsBeginningBalanceAmount().add(bal.getAccountLineAnnualBalanceAmount());
-
-                        expAmt = expAmt.add(balAmt);
-                    }
-                    else if (bal.getObjectTypeCode().equalsIgnoreCase(systemOptions.getFinancialObjectTypeTransferIncomeCd())) {
-                        balAmt = bal.getContractsGrantsBeginningBalanceAmount().add(bal.getAccountLineAnnualBalanceAmount());
-
-                        incAmt = incAmt.add(balAmt);
-                    }
-                }
-            }
-        }
-        return claimOnCash = incAmt.subtract(expAmt);
-    }
-
-    /**
      * Looks up a Collection of Balances for a given award Account
      * @param awardAccount award account to find balances for
      * @param fiscalYear the fiscal year to find balances for
@@ -161,11 +119,11 @@ public class ContractsGrantsLetterOfCreditReviewDocumentServiceImpl implements C
      * @param fiscalYear the fiscal year to find income and expense object types for
      * @return a List of the object type codes for the income and expense object types for the given fiscal year
      */
-    protected List<String> getIncomeAndExpenseObjectTypesForFiscalYear(Integer fiscalYear) {
+    protected List<String> getTransferIncomeAndExpenseObjectTypesForFiscalYear(Integer fiscalYear) {
         final SystemOptions systemOptions = optionsService.getCurrentYearOptions();
         List<String> expenseTypes = new ArrayList<>();
-        expenseTypes.add(systemOptions.getFinObjectTypeIncomecashCode());
-        expenseTypes.add(systemOptions.getFinObjTypeExpenditureexpCd());
+        expenseTypes.add(systemOptions.getFinancialObjectTypeTransferExpenseCd());
+        expenseTypes.add(systemOptions.getFinancialObjectTypeTransferIncomeCd());
         return expenseTypes;
     }
 

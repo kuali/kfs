35305be217f31cefe21e12efb51079ef54997223
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2015-01-29 09:18:04 -0500

    KFSTP-1798 - add award account validation to prevent creation of -bash invoices

--- work/src/org/kuali/kfs/module/ar/batch/service/VerifyBillingFrequencyService.java
+++ work/src/org/kuali/kfs/module/ar/batch/service/VerifyBillingFrequencyService.java
@@ -1,18 +1,18 @@
 /*
  * The Kuali Financial System, a comprehensive financial management system for higher education.
- * 
+ *
  * Copyright 2005-2014 The Kuali Foundation
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 
 import org.kuali.kfs.coa.businessobject.AccountingPeriod;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
+import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAwardAccount;
 
 /**
  * Interface class for Billing Frequency validation.
@@ -36,6 +37,15 @@ public interface VerifyBillingFrequencyService {
      * @return true if valid else false.
      */
     public boolean validateBillingFrequency(ContractsAndGrantsBillingAward award);
+
+    /**
+     * This method checks if the award account is within the grace period.
+     *
+     * @param award ContractsAndGrantsBillingAward to validate billing frequency for
+     * @param award ContractsAndGrantsBillingAwardAccount to validate billing frequency for
+     * @return true if valid else false.
+     */
+    public boolean validateBillingFrequency(ContractsAndGrantsBillingAward award, ContractsAndGrantsBillingAwardAccount awardAccount);
 
     /**
      * This method returns the start and end date of previous billing period.

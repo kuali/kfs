95c8ff718093e5eec2481a2576ae2d67d669f3ae
smith750 <smith750@iu.edu>
2015-02-25 11:01:00 -0500

    KFSTP-1951

--- src/main/java/org/kuali/kfs/module/purap/document/service/impl/PaymentRequestServiceImpl.java
+++ src/main/java/org/kuali/kfs/module/purap/document/service/impl/PaymentRequestServiceImpl.java
@@ -395,7 +395,8 @@ public class PaymentRequestServiceImpl implements PaymentRequestService {
         // Iterate all source accounting lines on the document, deriving a
         // minimum limit from each according to chart, chart and account, and
         // chart and organization.
-        for (SourceAccountingLine line : purapAccountingService.generateSummary(document.getItems())) {
+        final List<SourceAccountingLine> summaryLines = purapAccountingService.generateSummary(document.getItems());
+        for (SourceAccountingLine line : summaryLines) {
             // check to make sure the account is in the auto approve exclusion list
             Map<String, Object> autoApproveMap = new HashMap<String, Object>();
             autoApproveMap.put("chartOfAccountsCode", line.getChartOfAccountsCode());

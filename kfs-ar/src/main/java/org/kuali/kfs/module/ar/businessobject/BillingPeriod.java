2270c3aef150fffa54b3e02cf6134f9f9d1bc7dc
Travis Foxley <travis.foxley@kuali.co>
2015-04-22 11:49:26 -0400

    KFSTI-2080

--- kfs-ar/src/main/java/org/kuali/kfs/module/ar/businessobject/BillingPeriod.java
+++ kfs-ar/src/main/java/org/kuali/kfs/module/ar/businessobject/BillingPeriod.java
@@ -2,8 +2,6 @@ package org.kuali.kfs.module.ar.businessobject;
 
 
 import org.apache.commons.lang.time.DateUtils;
-import org.kuali.kfs.coa.businessobject.AccountingPeriod;
-import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
 import org.kuali.kfs.sys.util.KfsDateUtils;
 
 import java.sql.Date;
@@ -23,14 +21,32 @@ public class BillingPeriod {
 
     public BillingPeriod determineBillingPeriodPriorTo(Date awardStartDate, Date currentDate, Date lastBilledDate) {
         BillingPeriod billingPeriod = new BillingPeriod();
-        if (lastBilledDate == null) {
-            billingPeriod.startDate = awardStartDate;
-        } else {
-            billingPeriod.startDate = lastBilledDate;
+        if (canThisBeBilled(lastBilledDate, currentDate)) {
+            billingPeriod.startDate = determineStartDate(awardStartDate, lastBilledDate);
+            billingPeriod.endDate = calculatePreviousDate(currentDate);
         }
 
-        billingPeriod.endDate = calculatePreviousDate(currentDate);
         return billingPeriod;
+    }
+
+    protected boolean canThisBeBilled(Date lastBilledDate, Date currentDate) {
+        if (lastBilledDate == null) {
+            return true;
+        }
+
+        if (KfsDateUtils.isSameDay(currentDate, lastBilledDate) ||
+                KfsDateUtils.isSameDay(lastBilledDate, calculatePreviousDate(currentDate))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected Date determineStartDate(Date awardStartDate, Date lastBilledDate) {
+        if (lastBilledDate == null) {
+            return awardStartDate;
+        }
+        return lastBilledDate;
     }
 
     protected Date calculatePreviousDate(Date currentDate) {

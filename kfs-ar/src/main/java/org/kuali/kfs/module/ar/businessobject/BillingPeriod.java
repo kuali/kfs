97640dab256406af91d827cbce7d5a82c1b1012f
Travis Foxley <travis.foxley@kuali.co>
2015-04-22 10:52:32 -0400

    KFSTI-2080

--- kfs-ar/src/main/java/org/kuali/kfs/module/ar/businessobject/BillingPeriod.java
+++ kfs-ar/src/main/java/org/kuali/kfs/module/ar/businessobject/BillingPeriod.java
@@ -1,23 +1,39 @@
 package org.kuali.kfs.module.ar.businessobject;
 
 
+import org.apache.commons.lang.time.DateUtils;
 import org.kuali.kfs.coa.businessobject.AccountingPeriod;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
+import org.kuali.kfs.sys.util.KfsDateUtils;
 
 import java.sql.Date;
 
 public class BillingPeriod {
 
+    private Date startDate;
+    private Date endDate;
 
     public Date getStartDate() {
-        return Date.valueOf("2014-07-01");
+        return startDate;
     }
 
     public Date getEndDate() {
-        return Date.valueOf("2015-04-20");
+        return endDate;
     }
 
-    public BillingPeriod determineBillingPeriodPriorTo(AccountingPeriod currentPeriod, ContractsAndGrantsBillingAward award) {
-        return new BillingPeriod();
+    public BillingPeriod determineBillingPeriodPriorTo(Date awardStartDate, Date currentDate, Date lastBilledDate) {
+        BillingPeriod billingPeriod = new BillingPeriod();
+        if (lastBilledDate == null) {
+            billingPeriod.startDate = awardStartDate;
+        } else {
+            billingPeriod.startDate = lastBilledDate;
+        }
+
+        billingPeriod.endDate = calculatePreviousDate(currentDate);
+        return billingPeriod;
+    }
+
+    protected Date calculatePreviousDate(Date currentDate) {
+        return new Date(DateUtils.addDays(currentDate, -1).getTime());
     }
 }

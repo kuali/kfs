26cdbf0f2baa9d5cbffd8cef33837b581da366b8
Travis Foxley <travis.foxley@kuali.co>
2015-04-22 11:49:26 -0400

    KFSTI-2080

--- kfs-ar/src/test/java/org/kuali/kfs/module/ar/businessobject/BillingPeriodTest.java
+++ kfs-ar/src/test/java/org/kuali/kfs/module/ar/businessobject/BillingPeriodTest.java
@@ -50,13 +50,93 @@ public class BillingPeriodTest {
         verifyBillingPeriodPriorTo(awardStartDate, currentDate, lastBilledDate, expectedBillingPeriodStart, expectedBillingPeriodEnd);
     }
 
+    @Test
+    public void testDetermineBillingPeriodPriorTo_LOC_BillSeveralMonths() {
+        String awardStartDate = "2014-07-01";
+        String currentDate = "2015-04-21";
+        String lastBilledDate = "2014-11-15";
+        String expectedBillingPeriodStart = "2014-11-15";
+        String expectedBillingPeriodEnd = "2015-04-20";
+
+        verifyBillingPeriodPriorTo(awardStartDate, currentDate, lastBilledDate, expectedBillingPeriodStart, expectedBillingPeriodEnd);
+    }
+
+    @Test
+    public void testDetermineBillingPeriodPriorTo_LOC_BillAcrossFiscalYears() {
+        String awardStartDate = "2014-07-01";
+        String currentDate = "2015-04-21";
+        String lastBilledDate = "2014-06-15";
+        String expectedBillingPeriodStart = "2014-06-15";
+        String expectedBillingPeriodEnd = "2015-04-20";
+
+        verifyBillingPeriodPriorTo(awardStartDate, currentDate, lastBilledDate, expectedBillingPeriodStart, expectedBillingPeriodEnd);
+    }
+
+    @Test
+    public void testDetermineBillingPeriodPriorTo_LOC_MayNotBillNow() {
+        String awardStartDate = "2014-07-01";
+        String currentDate = "2015-04-21";
+        String lastBilledDate = "2015-04-20";
+        String expectedBillingPeriodStart = null;
+        String expectedBillingPeriodEnd = null;
+
+        verifyBillingPeriodPriorTo(awardStartDate, currentDate, lastBilledDate, expectedBillingPeriodStart, expectedBillingPeriodEnd);
+    }
+
     protected void verifyBillingPeriodPriorTo(String awardStartDate, String currentDate, String lastBilledDate, String expectedBillingPeriodStart, String expectedBillingPeriodEnd) {
         BillingPeriod billingPeriod = new BillingPeriod();
-        Date lastBilledDateAsDate = (StringUtils.isBlank(lastBilledDate)) ? null : Date.valueOf(lastBilledDate);
+        Date lastBilledDateAsDate = nullSafeDateFromString(lastBilledDate);
         BillingPeriod priorBillingPeriod = billingPeriod.determineBillingPeriodPriorTo(Date.valueOf(awardStartDate), Date.valueOf(currentDate), lastBilledDateAsDate);
 
-        Assert.assertEquals("Billing period start wasn't what we thought it was going to be", Date.valueOf(expectedBillingPeriodStart), priorBillingPeriod.getStartDate());
-        Assert.assertEquals("Billing period end wasn't what we thought it was going to be", Date.valueOf(expectedBillingPeriodEnd), priorBillingPeriod.getEndDate());
+        Date expectedStartDate = nullSafeDateFromString(expectedBillingPeriodStart);
+        Assert.assertEquals("Billing period start wasn't what we thought it was going to be", expectedStartDate, priorBillingPeriod.getStartDate());
+        Date expectedEndDate = nullSafeDateFromString(expectedBillingPeriodEnd);
+        Assert.assertEquals("Billing period end wasn't what we thought it was going to be", expectedEndDate, priorBillingPeriod.getEndDate());
+    }
+
+    private Date nullSafeDateFromString(String date) {
+        return (StringUtils.isBlank(date)) ? null : Date.valueOf(date);
+    }
+
+    @Test
+    public void canThisBeBilledLastBilledDateIsNull() {
+        boolean expectedCanThisBeBilled = true;
+
+        String lastBilledDate = null;
+        String currentDate = "2015-04-21";
+        validateCanThisBeBilled(expectedCanThisBeBilled, lastBilledDate, currentDate);
+    }
+
+    @Test
+    public void canThisBeBilledLastBilledDateIsToday() {
+        boolean expectedCanThisBeBilled = false;
+
+        String lastBilledDate = "2015-04-21";
+        String currentDate = "2015-04-21";
+        validateCanThisBeBilled(expectedCanThisBeBilled, lastBilledDate, currentDate);
+    }
+
+    @Test
+    public void canThisBeBilledLastBilledDateIsYesterday() {
+        boolean expectedCanThisBeBilled = false;
+
+        String lastBilledDate = "2015-04-20";
+        String currentDate = "2015-04-21";
+        validateCanThisBeBilled(expectedCanThisBeBilled, lastBilledDate, currentDate);
+    }
+
+    @Test
+    public void canThisBeBilledLastBilledDateIsBeforeYesterday() {
+        boolean expectedCanThisBeBilled = true;
+
+        String lastBilledDate = "2015-04-19";
+        String currentDate = "2015-04-21";
+        validateCanThisBeBilled(expectedCanThisBeBilled, lastBilledDate, currentDate);
+    }
+
+    protected void validateCanThisBeBilled(boolean expectedCanThisBeBilled, String lastBilledDate, String currentDate) {
+        BillingPeriod billingPeriod = new BillingPeriod();
+        Assert.assertEquals(expectedCanThisBeBilled, billingPeriod.canThisBeBilled(nullSafeDateFromString(lastBilledDate), Date.valueOf(currentDate)));
     }
 
 

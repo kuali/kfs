5cf6390391765ab1981fd247026b00178b370f13
Bryan Hutchinson <bryan@kuali.co>
2015-04-23 14:31:05 -0400

    KFSTI-2080

--- kfs-ar/src/main/java/org/kuali/kfs/module/ar/businessobject/BillingPeriod.java
+++ kfs-ar/src/main/java/org/kuali/kfs/module/ar/businessobject/BillingPeriod.java
@@ -2,10 +2,12 @@ package org.kuali.kfs.module.ar.businessobject;
 
 
 import org.apache.commons.lang.time.DateUtils;
+import org.kuali.kfs.coa.businessobject.AccountingPeriod;
 import org.kuali.kfs.module.ar.ArConstants;
 import org.kuali.kfs.sys.util.KfsDateUtils;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 public class BillingPeriod {
 
@@ -37,8 +39,23 @@ public class BillingPeriod {
     }
 
     protected Date findEndDate(Date currentDate) {
+        final AccountingPeriod accountingPeriod = findPreviousAccountingPeriod(currentDate);
+        return accountingPeriod.getUniversityFiscalPeriodEndDate();
+    }
 
-        return Date.valueOf("2015-03-31");
+    protected AccountingPeriod findPreviousAccountingPeriod(final Date currentDate) {
+        return new AccountingPeriod() {
+            @Override
+            public Date getUniversityFiscalPeriodEndDate() {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(currentDate);
+                if (cal.get(Calendar.MONTH) == Calendar.APRIL) {
+                    return Date.valueOf("2015-03-31");
+                } else {
+                    return Date.valueOf("2015-02-28");
+                }
+            }
+        };
     }
 
     protected boolean canThisBeBilled(Date lastBilledDate, Date currentDate) {

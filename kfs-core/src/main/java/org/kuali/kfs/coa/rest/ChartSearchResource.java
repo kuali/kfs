e303c965cf7f13d00f026ac6fc5fdbc519e958dc
smith750 <jksmith@kuali.co>
2015-05-12 17:00:59 -0400

    KFSTI-2218 start a REST endpoint for ChartSearch

---
+++ kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartSearchResource.java
@@ -0,0 +1,12 @@
+package org.kuali.kfs.coa.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/lookup/coa/chart")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ChartSearchResource {
+}

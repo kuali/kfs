79b927db1540da3d3c99db3b98f1db6c861fce45
kkronenb <kevin.kronenbitter@colostate.edu>
2015-05-13 14:32:56 -0600

    KFSTI-2243

---
+++ kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartController.java
@@ -0,0 +1,24 @@
+package org.kuali.kfs.coa.rest;
+
+
+import org.kuali.kfs.coa.businessobject.Chart;
+import org.kuali.kfs.coa.businessobject.lookup.ChartSearch;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.krad.service.LookupService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ChartController {
+
+    @RequestMapping(value="lookup/coa/chart/{chartofAccountsCode}", method= RequestMethod.GET, produces="application/json")
+    @ResponseBody
+    public Chart lookupChartByPrimaryKey(@PathVariable String chartOfAccountsCode) {
+        final LookupService lookupService = SpringContext.getBean(LookupService.class);
+        final ChartSearch chartSearch = new ChartSearch(lookupService);
+        return chartSearch.retrieveByChartCode(chartOfAccountsCode);
+    }
+}

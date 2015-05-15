1bf18b857cc5be804ad3daa28bc044b9f9c9da1f
kkronenb <kevin.kronenbitter@colostate.edu>
2015-05-15 11:02:10 -0600

    KFSTI-2243

--- kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartController.java
+++ kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartController.java
@@ -6,17 +6,14 @@ import org.kuali.kfs.coa.businessobject.lookup.ChartSearch;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.krad.service.LookupService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(value="/coa")
+@RequestMapping(value="/kfs-dev/lookup")
 public class ChartController {
 
-    @RequestMapping(value="/chart/{chartofAccountsCode}", method= RequestMethod.GET, produces="application/json")
-    public @ResponseBody Chart lookupChartByPrimaryKey(@PathVariable String chartOfAccountsCode) {
+    @RequestMapping(value="/coa/chart", method= RequestMethod.GET, produces="application/json")
+    public @ResponseBody Chart lookupChartByPrimaryKey(@RequestParam("chartOfAccountsCode") String chartOfAccountsCode) {
         final LookupService lookupService = SpringContext.getBean(LookupService.class);
         final ChartSearch chartSearch = new ChartSearch(lookupService);
         return chartSearch.retrieveByChartCode(chartOfAccountsCode);

e07c6ac86831a2fe7eef1a934f744ced592c0e66
smith750 <jksmith@kuali.co>
2015-06-08 14:22:13 -0400

    fixup! KFSTI-2218

--- kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartSearchSerializer.java
+++ kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartSearchSerializer.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import javafx.beans.property.ObjectProperty;
 import org.kuali.kfs.coa.businessobject.Chart;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kns.datadictionary.BusinessObjectEntry;
-import org.kuali.rice.krad.datadictionary.DataDictionary;
 import org.kuali.rice.krad.service.DataDictionaryService;
+import org.kuali.rice.krad.service.PersistenceStructureService;
 import org.kuali.rice.krad.uif.util.ObjectPropertyUtils;
 import org.kuali.rice.krad.util.ObjectUtils;
 
@@ -17,29 +16,49 @@ import java.io.IOException;
 
 public class ChartSearchSerializer extends JsonSerializer<Chart> {
     private DataDictionaryService dataDictionaryService;
+    private PersistenceStructureService persistenceStructureService;
 
     public ChartSearchSerializer() {}
 
-    public ChartSearchSerializer(DataDictionaryService dataDictionaryService) {
+    public ChartSearchSerializer(DataDictionaryService dataDictionaryService, PersistenceStructureService persistenceStructureService) {
         this.dataDictionaryService = dataDictionaryService;
+        this.persistenceStructureService = persistenceStructureService;
     }
 
     @Override
     public void serialize(Chart value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
         gen.writeStartObject();
         final BusinessObjectEntry entry = (BusinessObjectEntry)getDataDictionaryService().getDataDictionary().getBusinessObjectEntryForConcreteClass(Chart.class.getName());
-        for (String resultFieldName : entry.getLookupDefinition().getResultFieldNames()) {
+        entry.getLookupDefinition().getResultFieldNames().stream().forEach(resultFieldName -> {
             final Object resultFieldValue = ObjectPropertyUtils.getPropertyValue(value, resultFieldName);
             if (!ObjectUtils.isNull(resultFieldValue)) {
-                final Class<?> resultFieldType = ObjectPropertyUtils.getPropertyType(value, resultFieldName);
-                if (resultFieldType.isAssignableFrom(Number.class)) {
-                    gen.writeNumberField(resultFieldName, ((Number) resultFieldValue).doubleValue());
-                } else {
-                    gen.writeStringField(resultFieldName, resultFieldValue.toString());
+                try {
+                    if (getPersistenceStructureService().hasReference(value.getClass(), resultFieldName)) {
+                        writeLinkedValue(gen, value, resultFieldName, resultFieldValue);
+                    } else {
+                        writeValue(gen, resultFieldName, resultFieldValue);
+                    }
+                } catch (IOException ioe) {
+                    throw new RuntimeException(ioe);
                 }
             }
-        }
+        });
         gen.writeEndObject();
+    }
+
+    protected void writeLinkedValue(JsonGenerator gen, Chart value, String resultFieldName, Object resultFieldValue) throws IOException {
+        gen.writeFieldName(resultFieldName);
+        gen.writeStartObject();
+        writeValue(gen, "value", resultFieldValue);
+        gen.writeEndObject();
+    }
+
+    protected void writeValue(JsonGenerator gen, String propertyName, Object value) throws IOException {
+        if (value.getClass().isAssignableFrom(Number.class)) {
+            gen.writeNumberField(propertyName, ((Number) value).doubleValue());
+        } else {
+            gen.writeStringField(propertyName, value.toString());
+        }
     }
 
     public DataDictionaryService getDataDictionaryService() {
@@ -47,5 +66,12 @@ public class ChartSearchSerializer extends JsonSerializer<Chart> {
             dataDictionaryService = SpringContext.getBean(DataDictionaryService.class);
         }
         return dataDictionaryService;
+    }
+
+    public PersistenceStructureService getPersistenceStructureService() {
+        if (persistenceStructureService == null) {
+            persistenceStructureService = SpringContext.getBean(PersistenceStructureService.class);
+        }
+        return persistenceStructureService;
     }
 }

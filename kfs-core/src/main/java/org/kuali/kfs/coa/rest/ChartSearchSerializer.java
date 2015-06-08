32af6157e634a876b2b80286816ea816a340475b
smith750 <jksmith@kuali.co>
2015-06-08 14:00:15 -0400

    fixup! KFSTI-2218

--- kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartSearchSerializer.java
+++ kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartSearchSerializer.java
@@ -8,6 +8,7 @@ import javafx.beans.property.ObjectProperty;
 import org.kuali.kfs.coa.businessobject.Chart;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kns.datadictionary.BusinessObjectEntry;
+import org.kuali.rice.krad.datadictionary.DataDictionary;
 import org.kuali.rice.krad.service.DataDictionaryService;
 import org.kuali.rice.krad.uif.util.ObjectPropertyUtils;
 import org.kuali.rice.krad.util.ObjectUtils;
@@ -17,6 +18,8 @@ import java.io.IOException;
 public class ChartSearchSerializer extends JsonSerializer<Chart> {
     private DataDictionaryService dataDictionaryService;
 
+    public ChartSearchSerializer() {}
+
     public ChartSearchSerializer(DataDictionaryService dataDictionaryService) {
         this.dataDictionaryService = dataDictionaryService;
     }
@@ -24,18 +27,25 @@ public class ChartSearchSerializer extends JsonSerializer<Chart> {
     @Override
     public void serialize(Chart value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
         gen.writeStartObject();
-        final BusinessObjectEntry entry = (BusinessObjectEntry)dataDictionaryService.getDataDictionary().getBusinessObjectEntryForConcreteClass(Chart.class.getName());
+        final BusinessObjectEntry entry = (BusinessObjectEntry)getDataDictionaryService().getDataDictionary().getBusinessObjectEntryForConcreteClass(Chart.class.getName());
         for (String resultFieldName : entry.getLookupDefinition().getResultFieldNames()) {
             final Object resultFieldValue = ObjectPropertyUtils.getPropertyValue(value, resultFieldName);
             if (!ObjectUtils.isNull(resultFieldValue)) {
                 final Class<?> resultFieldType = ObjectPropertyUtils.getPropertyType(value, resultFieldName);
                 if (resultFieldType.isAssignableFrom(Number.class)) {
-                    gen.writeNumberField(resultFieldName, ((Number)resultFieldValue).doubleValue());
+                    gen.writeNumberField(resultFieldName, ((Number) resultFieldValue).doubleValue());
                 } else {
                     gen.writeStringField(resultFieldName, resultFieldValue.toString());
                 }
             }
         }
         gen.writeEndObject();
+    }
+
+    public DataDictionaryService getDataDictionaryService() {
+        if (dataDictionaryService == null) {
+            dataDictionaryService = SpringContext.getBean(DataDictionaryService.class);
+        }
+        return dataDictionaryService;
     }
 }

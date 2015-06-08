d0cb7b60bfec30bc0c4ee5c4321b7b0223563efd
kkronenb <kevin.kronenbitter@colostate.edu>
2015-06-08 11:37:34 -0600

    KFSTI-2218 Adding ObjectCode search

---
+++ kfs-core/src/main/java/org/kuali/kfs/coa/businessobject/lookup/ObjectCodeSearch.java
@@ -0,0 +1,34 @@
+package org.kuali.kfs.coa.businessobject.lookup;
+
+import org.kuali.kfs.coa.businessobject.Chart;
+import org.kuali.kfs.coa.businessobject.ObjectCode;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.rice.krad.service.LookupService;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ObjectCodeSearch {
+
+    protected LookupService lookupService;
+
+    public ObjectCodeSearch(LookupService lookupService) {
+        this.lookupService = lookupService;
+    }
+
+    public ObjectCode retrieveByPK(Integer fiscalYear, String chartCode, String objectCode) {
+        ObjectCode objCode = findObjectCode(fiscalYear, chartCode, objectCode);
+        return objCode;
+    }
+
+    protected ObjectCode findObjectCode(Integer fiscalYear, String chartCode, String objectCode) {
+        Map<String, String> searchProperties = new ConcurrentHashMap<>();
+        searchProperties.put(KFSPropertyConstants.FISCAL_YEAR, fiscalYear+"");
+        searchProperties.put(KFSPropertyConstants.CHART_OF_ACCOUNTS_CODE, chartCode);
+        searchProperties.put(KFSPropertyConstants.FINANCIAL_OBJECT_CODE, objectCode);
+        Collection objectCodes = lookupService.findCollectionBySearch(ObjectCode.class, searchProperties);
+        return (ObjectCode) objectCodes.iterator().next();
+    }
+
+}

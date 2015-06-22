f146ef1c4709e286f98cad5d9292cb0ebef6183e
smith750 <jksmith@kuali.co>
2015-06-22 13:56:45 -0400

    KFSTI-2396 get a bit farther with serializer

--- kfs-core/src/main/java/org/kuali/kfs/sys/businessobject/datadictionary/FinancialSystemBusinessObjectEntrySerializer.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/businessobject/datadictionary/FinancialSystemBusinessObjectEntrySerializer.java
@@ -1,11 +1,15 @@
 package org.kuali.kfs.sys.businessobject.datadictionary;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializerProvider;
+import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
+import org.kuali.rice.krad.datadictionary.AttributeDefinition;
+import org.kuali.rice.krad.util.ObjectUtils;
 
 import java.io.IOException;
 
@@ -18,6 +22,39 @@ public class FinancialSystemBusinessObjectEntrySerializer extends JsonSerializer
         node.put("businessObjectClass", entry.getBusinessObjectClass().getName());
         node.put("titleAttribute", entry.getTitleAttribute());
         node.put("objectLabel", entry.getObjectLabel());
+
+        ArrayNode attributes = mapper.createArrayNode();
+        for (AttributeDefinition attributeDefinition : entry.getAttributes()) {
+            attributes.add(mapAttributeDefinition(attributeDefinition, mapper));
+        }
+        node.put("attributes", attributes);
         jsonGenerator.writeTree(node);
     }
+
+    protected ObjectNode mapAttributeDefinition(AttributeDefinition attributeDefinition, ObjectMapper mapper) {
+        ObjectNode attributeNode = mapper.createObjectNode();
+        attributeNode.put("name", attributeDefinition.getName());
+        attributeNode.put("label", attributeDefinition.getLabel());
+        attributeNode.put("shortLabel", attributeDefinition.getShortLabel());
+        attributeNode.put("constraintText", attributeDefinition.getConstraintText());
+        attributeNode.put("required", attributeDefinition.isRequired());
+        attributeNode.put("forceUppercase", attributeDefinition.getForceUppercase());
+        if (!ObjectUtils.isNull(attributeDefinition.getMinLength())) {
+            attributeNode.put("minLength", attributeDefinition.getMinLength());
+        }
+        if (!ObjectUtils.isNull(attributeDefinition.getMaxLength())) {
+            attributeNode.put("maxLength", attributeDefinition.getMaxLength());
+        }
+        if (!ObjectUtils.isNull(attributeDefinition.getUnique())) {
+            attributeNode.put("unique", attributeDefinition.getUnique());
+        }
+        if (!StringUtils.isBlank(attributeDefinition.getExclusiveMin())) {
+            attributeNode.put("exclusiveMin", attributeDefinition.getExclusiveMin());
+        }
+        if (!StringUtils.isBlank(attributeDefinition.getInclusiveMax())) {
+            attributeNode.put("inclusiveMax", attributeDefinition.getInclusiveMax());
+        }
+        return attributeNode;
+    }
+
 }

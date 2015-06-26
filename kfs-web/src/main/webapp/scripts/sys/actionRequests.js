29597c9347835f59ccda6edbde2a534e918dad66
smith750 <jksmith@kuali.co>
2015-06-26 16:37:25 -0400

    KFSTI-2405 got charts to change - but only if the number of results goes up...

--- kfs-web/src/main/webapp/scripts/sys/actionRequests.js
+++ kfs-web/src/main/webapp/scripts/sys/actionRequests.js
@@ -1,8 +1,13 @@
-function setupPrincipalChart(data, idName) {
-    var cols = data.map(function(obj, i){
+function principalsToArray(prncpls) {
+    var ary = prncpls.map(function(obj, i){
         var principalName = Object.keys(obj)[0]
         return [principalName, obj[principalName]]
     })
+    return ary
+}
+
+function setupPrincipalChart(data, idName) {
+    var cols = principalsToArray(data)
 
     var chart = c3.generate({
         data: {
@@ -15,11 +20,39 @@ function setupPrincipalChart(data, idName) {
     return chart;
 }
 
-$.get("/kfs-snd/docStats/uncompletedActionRequestsByPrincipalName", function (response) {
-    var uncompletedActionRequests = setupPrincipalChart(response, "uncompleted-requests");
+var uncompletedLimit = 5;
+var completedLimit = 5;
+var uncompletedActionRequests;
+var completedActionRequests;
+
+$.get("/kfs-snd/docStats/uncompletedActionRequestsByPrincipalName?limit="+uncompletedLimit, function (response) {
+    uncompletedActionRequests = setupPrincipalChart(response, "uncompleted-requests");
 });
 
-$.get("/kfs-snd/docStats/completedActionRequestsByPrincipalName", function (response) {
-    var completedActionRequests = setupPrincipalChart(response, "completed-requests");
+$.get("/kfs-snd/docStats/completedActionRequestsByPrincipalName?limit="+completedLimit, function (response) {
+    completedActionRequests = setupPrincipalChart(response, "completed-requests");
 });
 
+$(document).ready(function () {
+    $("#completed-limit-label").html(completedLimit)
+    $("#completed-limit").val(completedLimit)
+    $("#completed-limit").change(function (ele) {
+        completedLimit = $( this ).val()
+        $.get("/kfs-snd/docStats/completedActionRequestsByPrincipalName?limit="+completedLimit, function (response) {
+            var cols = principalsToArray(response)
+            completedActionRequests.load({columns: cols})
+            $("#completed-limit-label").html(completedLimit)
+        });
+    });
+
+    $("#uncompleted-limit-label").html(uncompletedLimit)
+    $("#uncompleted-limit").val(uncompletedLimit)
+    $("#uncompleted-limit").change(function (ele) {
+        uncompletedLimit = $( this ).val()
+        $.get("/kfs-snd/docStats/uncompletedActionRequestsByPrincipalName?limit="+uncompletedLimit, function (response) {
+            var cols = principalsToArray(response)
+            uncompletedActionRequests.load({columns: cols})
+            $("#uncompleted-limit-label").html(uncompletedLimit)
+        });
+    });
+})

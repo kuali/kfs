602fd71e51ce5af71b2f2fccb2e0ca17fc938215
smith750 <jksmith@kuali.co>
2015-06-26 17:36:07 -0400

    KFSTI-2405 add donut charts to for action requests by type

--- kfs-web/src/main/webapp/scripts/sys/actionRequests.js
+++ kfs-web/src/main/webapp/scripts/sys/actionRequests.js
@@ -34,6 +34,20 @@ function setupChangeResponse(divName, chart, startLimit, sourceUrl) {
     })
 }
 
+function setupTypeChart(data, divName) {
+    var cols = Object.keys(data).map(function (ele, idx) {
+        return [ele, data[ele]]
+    })
+    var chart = c3.generate({
+        bindto: "#"+divName,
+        data: {
+            type: 'donut',
+            columns: cols
+        }
+    })
+    return chart;
+}
+
 $.get("/kfs-snd/docStats/uncompletedActionRequestsByPrincipalName?limit=5", function (response) {
     var uncompletedActionRequests = setupPrincipalChart(response, "uncompleted-requests")
     setupChangeResponse("uncompleted-limit", uncompletedActionRequests, 5, "/kfs-snd/docStats/uncompletedActionRequestsByPrincipalName")
@@ -42,4 +56,11 @@ $.get("/kfs-snd/docStats/uncompletedActionRequestsByPrincipalName?limit=5", func
 $.get("/kfs-snd/docStats/completedActionRequestsByPrincipalName?limit=5", function (response) {
     var completedActionRequests = setupPrincipalChart(response, "completed-requests")
     setupChangeResponse("completed-limit",completedActionRequests, 5,"/kfs-snd/docStats/completedActionRequestsByPrincipalName")
+})
+
+$.get("/kfs-snd/docStats/uncompletedActionRequestsByType", function (response) {
+    var uncompletedActionRequestsByType = setupTypeChart(response, "uncompleted-requests-by-type")
+})
+$.get("/kfs-snd/docStats/completedActionRequestsByType", function (response) {
+    var completedActionRequestsbyType = setupTypeChart(response, "completed-requests-by-type")
 })

750c2fe7942c77144b1d968ed77b42dbb7fa8fb7
smith750 <jksmith@kuali.co>
2015-06-29 16:20:28 -0400

    KFSTI-2404 add reactable to project (thank you, Travis!)

--- kfs-web/src/main/webapp/scripts/sys/batchSchedule.js
+++ kfs-web/src/main/webapp/scripts/sys/batchSchedule.js
@@ -1,3 +1,5 @@
+var Table = Reactable.Table;
+
 var Parent = React.createClass({
     handleSearchSubmit: function(code) {
         $.ajax({
@@ -31,54 +33,24 @@ var Parent = React.createClass({
 var JobList = React.createClass({
     render: function() {
         console.log("render JobList");
-        var rows = [];
         var scheduled = this.props.searchResults.scheduled;
         var unscheduled = this.props.searchResults.unscheduled;
-        scheduled.forEach(function(result) {
-            rows.push(<ResultsRow job={result} />);
-        }.bind(this));
-        unscheduled.forEach(function(result) {
-            rows.push(<ResultsRow job={result} />);
-        }.bind(this));
+        var rows = scheduled.concat(unscheduled)
+        rows = rows.map(function(row) {
+            var formattedNextRunDate = (row.nextRunDate ? moment(row.nextRunDate).format('M/DD/YYYY, h:mm:ss a') : '');
+            row['formattedNextRunDate'] = formattedNextRunDate
+            return row
+        })
         return (
-            <table>
-                <thead>
-                <tr>
-                    <th>Actions</th>
-                    <th>Namespace</th>
-                    <th>Job Name</th>
-                    <th>Job Group</th>
-                    <th>Job Status</th>
-                    <th>Next Runtime</th>
-                    <th>Steps</th>
-                    <th>Dependencies</th>
-                </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </table>
-        );
-    }
-});
-
-var ResultsRow = React.createClass({
-    render: function() {
-        var nextRunDate = (this.props.job.nextRunDate ? moment(this.props.job.nextRunDate).format('M/DD/YYYY, h:mm:ss a') : '');
-        return (
-            <tr>
-                <td>modify</td>
-                <td>{this.props.job.namespaceCode}</td>
-                <td>{this.props.job.name}</td>
-                <td>{this.props.job.group}</td>
-                <td>{this.props.job.status}</td>
-                <td>{nextRunDate}</td>
-                <td>{this.props.job.stepList}</td>
-                <td>{this.props.job.dependencyList}</td>
-            </tr>
+            <Table data={rows}
+                   columns={[{key: 'actions', label: 'Actions'},{key: 'namespaceCode', label: 'Namespace'},{key: 'name', label: 'Name'}, {key: 'group', label: 'Group'},{key: 'formattedNextRunDate', label: 'Next Run Date'},{ key: 'stepList', label: 'Steps'},{key: 'dependencyList',label: 'Dependencies'}]}
+                   sortable={true}
+                   filterable={['namespaceCode','name','group']}/>
         );
     }
 });
 
 React.render(
-    <Parent url="/kfs-dev/batch/jobs" />,
+    <Parent url="/kfs-snd/batch/jobs" />,
     document.getElementById('main')
 );

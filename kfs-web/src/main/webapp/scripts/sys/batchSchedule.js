ecac10ec0dc470f92cf29f96b068d7004ad53f79
Travis Foxley <tfoxley@kuali.co>
2015-06-29 15:18:10 -0600

    KFSTI-2404 add detail page and start adding react-router

--- kfs-web/src/main/webapp/scripts/sys/batchSchedule.js
+++ kfs-web/src/main/webapp/scripts/sys/batchSchedule.js
@@ -1,9 +1,13 @@
+
+
+var url = "/kfs-dev/batch/jobs";
+
 var Table = Reactable.Table;
 
-var Parent = React.createClass({
-    handleSearchSubmit: function(code) {
+var JobTable = React.createClass({
+    handleSearchSubmit: function() {
         $.ajax({
-            url: this.props.url,
+            url: url,
             dataType: 'json',
             type: 'GET',
             success: function(searchResults) {
@@ -39,18 +43,89 @@ var JobList = React.createClass({
         rows = rows.map(function(row) {
             var formattedNextRunDate = (row.nextRunDate ? moment(row.nextRunDate).format('M/DD/YYYY, h:mm:ss a') : '');
             row['formattedNextRunDate'] = formattedNextRunDate
+            row['modifyUrl'] = "<a href='/kfs-dev/batch/jobs/'" + row.name +  "></a>";
             return row
         })
         return (
             <Table data={rows}
-                   columns={[{key: 'actions', label: 'Actions'},{key: 'namespaceCode', label: 'Namespace'},{key: 'name', label: 'Name'}, {key: 'group', label: 'Group'},{key: 'formattedNextRunDate', label: 'Next Run Date'},{ key: 'stepList', label: 'Steps'},{key: 'dependencyList',label: 'Dependencies'}]}
+                   columns={[{key: 'modifyUrl', label: 'Actions'},{key: 'namespaceCode', label: 'Namespace'},{key: 'name', label: 'Name'}, {key: 'group', label: 'Group'},{key: 'formattedNextRunDate', label: 'Next Run Date'},{ key: 'stepList', label: 'Steps'},{key: 'dependencyList',label: 'Dependencies'}]}
                    sortable={true}
                    filterable={['namespaceCode','name','group']}/>
         );
     }
 });
 
+var JobDetail = React.createClass({
+    componentDidMount: function() {
+        //$.ajax({
+        //    url: this.props.url,
+        //    dataType: 'json',
+        //    type: 'GET',
+        //    success: function(job) {
+        //        this.setState({job: job});
+        //    }.bind(this),
+        //    error: function(xhr, status, err) {
+        //        console.error(this.props.url, status, err.toString());
+        //    }.bind(this)
+        //});
+        this.setState({job: {
+            "name": "pdpSendAchAdviceNotificationsJob",
+            "group": "unscheduled",
+            "status": null,
+            "running": false,
+            "fullName": "unscheduled.pdpSendAchAdviceNotificationsJob",
+            "namespaceCode": "KFS-PDP",
+            "scheduled": true,
+            "dependencies": {},
+            "dependencyList": "",
+            "stepList": "pdpSendAchAdviceNotificationsStep \n",
+            "numSteps": 1,
+            "nextRunDate": null
+        }})
+    },
+    getInitialState: function () {
+        return {job: {}};
+    },
+    render: function() {
+        return (
+            <JobActionForm/>,
+            <JobInfo job={this.state.job}/>
+        )
+    }
+})
+
+var JobActionForm = React.createClass({
+    render: function() {
+        return (
+            <div></div>
+        )
+    }
+})
+
+var JobInfo = React.createClass({
+    render: function() {
+        var formattedNextRunDate = (this.props.job.nextRunDate ? moment(this.props.job.nextRunDate).format('M/DD/YYYY, h:mm:ss a') : '');
+        return (
+            <ul>
+                <li>Namespace: {this.props.job.namespaceCode}</li>
+                <li>Name: {this.props.job.name}</li>
+                <li>Group: {this.props.job.group}</li>
+                <li>Status: {this.props.job.status}</li>
+                <li>Next Run Date: {formattedNextRunDate}</li>
+                <li>Steps: {this.props.job.stepList}</li>
+                <li>Number of Steps: {this.props.job.numSteps}</li>
+                <li>Dependencies: {this.props.job.dependencyList}</li>
+            </ul>
+        )
+    }
+})
+
 React.render(
-    <Parent url="/kfs-snd/batch/jobs" />,
+    <Router history={new HashHistory}>
+        <Route component={JobTable}>
+            <Route path="/" component={Index}/>
+            <Route path="/job/:jobName/group/:groupId" component={JobDetail}/>
+        </Route>
+    </Router>,
     document.getElementById('main')
 );

d78968911f9ce2063d1d18e9b9c276248dc7bcf2
Travis Foxley <tfoxley@kuali.co>
2015-06-30 15:13:48 -0600

    KFSTI-2404 add endpoint for (un)scheduling jobs

--- kfs-core/src/main/java/org/kuali/kfs/sys/web/SchedulerResource.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/web/SchedulerResource.java
@@ -1,11 +1,18 @@
 package org.kuali.kfs.sys.web;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.kuali.kfs.sys.batch.BatchJobStatus;
 import org.kuali.kfs.sys.batch.service.SchedulerService;
 import org.kuali.kfs.sys.context.SpringContext;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +35,28 @@ public class SchedulerResource {
     @Path("job/{groupId}/{jobName}")
     public BatchJobStatus getJobDetail(@PathParam("groupId") String groupId, @PathParam("jobName") String jobName) {
         return getSchedulerService().getJob(groupId, jobName);
+    }
+
+    @POST
+    @Path("job/{groupId}/{jobName}")
+    public Response modifyJob(@PathParam("groupId") String groupId,
+                              @PathParam("jobName") String jobName,
+                              ObjectNode body) {
+        if (body.has("command")) {
+            String command = body.get("command").asText();
+            BatchJobStatus job = getSchedulerService().getJob(groupId, jobName);
+            switch (command) {
+                case "schedule":
+                    job.schedule();
+                    return Response.ok().build();
+                case "unschedule" :
+                    job.unschedule();
+                    return Response.ok().build();
+                default: return Response.status(400).entity("Command not recognized").build();
+            }
+
+        }
+        return Response.status(400).entity("Command not recognized").build();
     }
 
     protected SchedulerService getSchedulerService() {

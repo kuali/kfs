ca598ae80927aec1bc72e070b2feb7fa8df20f05
smith750 <jksmith@kuali.co>
2015-07-07 17:41:28 -0400

    KFSTI-2243 RequestMapping path is relative to url-pattern

--- kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartController.java
+++ kfs-core/src/main/java/org/kuali/kfs/coa/rest/ChartController.java
@@ -6,7 +6,13 @@ import org.kuali.kfs.coa.businessobject.lookup.ChartSearch;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.krad.service.LookupService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class ChartController {
@@ -14,11 +20,14 @@ public class ChartController {
     // Lines commented out here and line 378 of the web.xml render a different result that partially works in a different way
 
 //    @RequestMapping(value="/lookup/coa/chart/{code}", method= RequestMethod.GET, produces="application/json")
-    @RequestMapping(value="/lookup/coa/chart/BA", method= RequestMethod.GET, produces="application/json")
-    public @ResponseBody Chart lookupChartByPrimaryKey(/**@PathVariable String code*/) {
+    @RequestMapping(value="/coa/chart/{code}", method= RequestMethod.GET, produces="application/json")
+    public @ResponseBody Map<String, String> lookupChartByPrimaryKey(@PathVariable String code) {
         final LookupService lookupService = SpringContext.getBean(LookupService.class);
         final ChartSearch chartSearch = new ChartSearch(lookupService);
-        return chartSearch.retrieveByChartCode("BA");
-//        return chartSearch.retrieveByChartCode(code);
+//        return chartSearch.retrieveByChartCode("BA");
+        final Chart chart = chartSearch.retrieveByChartCode("BL");
+        Map<String, String> m = new ConcurrentHashMap<>();
+        m.put("name", chart.getName());
+        return m;
     }
 }

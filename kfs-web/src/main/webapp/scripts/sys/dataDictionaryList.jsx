da48b650ae35547f8deea5b1fd3ead5fd4dbea2e
tfoxley <tfoxley@kuali.co>
2015-07-10 12:44:01 -0600

    KFSTI-2324 add detail page and begin editability

--- kfs-web/src/main/webapp/scripts/sys/dataDictionaryList.jsx
+++ kfs-web/src/main/webapp/scripts/sys/dataDictionaryList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Griddle from 'griddle-react';
 import $ from 'jQuery';
+import Router from 'react-router';
+import { DefaultRoute, HashHistory, Link, Route, RouteHandler, Navigation } from 'react-router';
 
 var Root = React.createClass({
     getInitialState: function() {
@@ -8,7 +10,7 @@ var Root = React.createClass({
     },
     componentWillMount: function() {
         $.ajax({
-            url: this.props.url,
+            url: getUrlPathPrefix("/sys/DataDictionaryList.html") + "/core/datadictionary/businessObjectEntry",
             dataType: 'json',
             type: 'GET',
             success: function(entries) {
@@ -41,13 +43,13 @@ var Root = React.createClass({
 
 var LinkComponent = React.createClass({
     render: function(){
-        return <a href={this.props.data.link}>view more</a>
+        return <Link to="detail" params={{name: this.props.rowData.key, editable: false}}>view more</Link>
     }
 });
 
 var EditComponent = React.createClass({
     render: function(){
-        return <a href="">edit</a>
+        return <Link to="detail" params={{name: this.props.rowData.key, editable: true}}>edit</Link>
     }
 });
 
@@ -80,7 +82,74 @@ var columnMeta = [
     }
 ];
 
-React.render(
-    <Root url="/kfs-dev/core/datadictionary/businessObjectEntry" />,
-    document.getElementById('main')
-)
+var Detail = React.createClass({
+    getInitialState: function() {
+        return {entry: {}}
+    },
+    componentWillMount: function() {
+        $.ajax({
+            url: getUrlPathPrefix("/sys/DataDictionaryList.html") + "/core/datadictionary/businessObjectEntry/" + this.props.params.name,
+            dataType: 'json',
+            type: 'GET',
+            success: function(entry) {
+                this.setState({entry: entry});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        })
+    },
+    render: function() {
+        var fields = [];
+        for (var key in this.state.entry) {
+            if (this.state.entry.hasOwnProperty(key)) {
+                fields.push(<FormField name={key} value={this.state.entry[key]} editable={this.props.params.editable}/>)
+            } else {
+                console.log(key)
+            }
+        }
+        return (
+            <table>
+                {fields}
+            </table>
+        )
+    }
+})
+
+var FormField = React.createClass({
+    render: function() {
+        return (
+            <tr>
+                <td><label>{this.props.name}</label></td>
+                <td>{this.props.editable && this.props.editable === 'true' ? <input type="text" value={this.props.value}/> : this.props.value}</td>
+            </tr>
+        )
+    }
+})
+
+function getUrlPathPrefix(page) {
+    var path = new URL(window.location.href).pathname;
+    var index = path.indexOf(page);
+    return path.substring(0, index);
+}
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <RouteHandler/>
+            </div>
+        )
+    }
+});
+
+let routes = (
+    <Route handler={App}>
+        <Route name="table" path="/" handler={Root}/>
+        <Route name="detail" path="/businessObjectEntry/:name/:editable" handler={Detail}/>
+    </Route>
+);
+
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.getElementById('main'));
+});

afb9008354389701e71c017e035f05f8226385d2
tfoxley <tfoxley@kuali.co>
2015-07-13 13:19:59 -0600

    KFSTI-2324 begin making form elements editable

--- kfs-web/src/main/webapp/scripts/sys/dataDictionaryList.jsx
+++ kfs-web/src/main/webapp/scripts/sys/dataDictionaryList.jsx
@@ -81,24 +81,54 @@ var Detail = React.createClass({
             }.bind(this)
         })
     },
+    updateFieldValue: function(name, event) {
+        console.log("updating state")
+        console.log(name)
+        console.log(event.target.value)
+        var s = {}
+        s.entry = this.state.entry
+        s.entry[name] = event.target.value
+        this.setState(s)
+    },
+    updateBusinessObjectEntry: function() {
+        console.log("updating boe")
+        $.ajax({
+            url: getUrlPathPrefix("/sys/DataDictionaryList.html") + "/core/datadictionary/businessObjectEntry/" + this.props.params.name,
+            dataType: 'json',
+            type: 'PUT',
+            data: JSON.stringify(this.state.entry),
+            success: function() {
+                alert('Congrats')
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        })
+
+    },
     render: function() {
-        var fields = buildFieldArray(this.state.entry, this.props.params.editable)
+        var fields = buildFieldArray(this.state.entry, this.props.params.editable, this.updateFieldValue)
+        var updateButton;
+        if (this.props.params.editable && this.props.params.editable  === "true") {
+            updateButton = <button type="button" onClick={this.updateBusinessObjectEntry.bind(this)}>Update</button>;
+        }
         return (
             <div>
                 <Link to="table">go back</Link>
                 <table>
                     {fields}
                 </table>
+                {updateButton}
             </div>
         )
     }
 })
 
-function buildFieldArray(map, editable) {
+function buildFieldArray(map, editable, updateFieldValue) {
     var fields = [];
     for (var key in map) {
         if (map.hasOwnProperty(key)) {
-            fields.push(<FormField name={key} value={map[key]} editable={editable}/>)
+            fields.push(<FormField name={key} value={map[key]} editable={editable} updateFieldValue={updateFieldValue}/>)
         }
     }
     return fields;
@@ -108,22 +138,23 @@ var FormField = React.createClass({
     render: function() {
         var attributeValue
         if (this.props.name === "attributes" || this.props.name === "inquiryFields" || this.props.name === "lookupFields" || this.props.name === "resultFields") {
-            attributeValue = <AttributeTable attributes={this.props.value} editable={this.props.editable}/>
+            attributeValue = <AttributeTable attributes={this.props.value} editable={this.props.editable} updateFieldValue={this.props.updateFieldValue}/>
         } else if (this.props.name === "defaultSort") {
-            var fields = buildFieldArray(this.props.value, this.props.editable)
+            var fields = buildFieldArray(this.props.value, this.props.editable, this.props.updateFieldValue)
             attributeValue = <table>{fields}</table>
         } else if (this.props.name === "inquirySections") {
             var attributeValues = [];
             for (var i=0;i<this.props.value.length;i++) {
-                var fields = buildFieldArray(this.props.value[i], this.props.editable)
+                var fields = buildFieldArray(this.props.value[i], this.props.editable, this.props.updateFieldValue)
                 attributeValues.push(<tr><td><table>{fields}</table></td></tr>)
             }
             attributeValue = <table>{attributeValues}</table>
         } else if (this.props.name === "inquiryDefinition" || this.props.name === "lookupDefinition") {
-            var fields = buildFieldArray(this.props.value, this.props.editable)
+            var fields = buildFieldArray(this.props.value, this.props.editable, this.props.updateFieldValue)
             attributeValue = <table>{fields}</table>
         } else {
-            attributeValue = determineFieldValue(this.props.editable, this.props.value)
+            attributeValue = <InputField editable={this.props.editable} value={this.props.value} name={this.props.name} updateFieldValue={this.props.updateFieldValue}/>
+                //determineFieldValue(this.props.editable, this.props.value, this.props.name, this.props.updateFieldValue)
         }
         return (
             <tr>
@@ -142,7 +173,7 @@ var AttributeTable = React.createClass({
             fields.push(<AttributeLabelField attribute={this.props.attributes[0]}/>)
         }
         for (var i=0; i<this.props.attributes.length; i++) {
-            fields.push(<AttributeFormField attribute={this.props.attributes[i]} editable={this.props.editable}/>)
+            fields.push(<AttributeFormField attribute={this.props.attributes[i]} editable={this.props.editable} updateFieldValue={this.props.updateFieldValue}/>)
         }
         return (
             <table>
@@ -170,14 +201,16 @@ var AttributeLabelField = React.createClass({
 
 var AttributeFormField = React.createClass({
     render: function() {
+        console.log('rendering attribute form field')
         var fields = [];
         for (var key in this.props.attribute) {
             if (this.props.attribute.hasOwnProperty(key)) {
                 if (key === "control") {
-                    fields.push(<td><AttributeTable editable={this.props.editable} attributes={[this.props.attribute[key]]}/></td>)
+                    fields.push(<td><AttributeTable editable={this.props.editable} attributes={[this.props.attribute[key]]} updateFieldValue={this.props.updateFieldValue}/></td>)
 
                 } else {
-                    var attributeValue = determineFieldValue(this.props.editable, this.props.attribute[key])
+                    var attributeValue = <InputField editable={this.props.editable} value={this.props.attribute[key]} name={key} updateFieldValue={this.props.updateFieldValue}/>
+                        //determineFieldValue(this.props.editable, this.props.attribute[key], key, this.props.updateFieldValue)
                     fields.push(<td>{attributeValue}</td>)
                 }
             }
@@ -190,7 +223,21 @@ var AttributeFormField = React.createClass({
     }
 })
 
-function determineFieldValue(editable, value) {
+var InputField = React.createClass({
+    render: function() {
+        if (!this.props.editable || this.props.editable === 'false') {
+            return typeof this.props.value === "boolean" ? this.props.value.toString() : this.props.value
+        } else {
+            var type = "text"
+            if (typeof this.props.value === "boolean") {
+                type = "checkbox"
+            }
+            return <input type={type} value={this.props.value} checked={this.props.value} onChange={this.props.updateFieldValue.bind(this, this.props.name)}/>
+        }
+    }
+})
+
+function determineFieldValue(editable, value, name, onBlurFunction) {
     if (!editable || editable === 'false') {
         return typeof value === "boolean" ? value.toString() : value
     } else {
@@ -198,7 +245,7 @@ function determineFieldValue(editable, value) {
         if (typeof value === "boolean") {
             type = "checkbox"
         }
-        return <input type={type} value={value} checked={value}/>
+        return <input type={type} value={value} checked={value} onBlur={onBlurFunction.bind(this, name)}/>
     }
 }
 

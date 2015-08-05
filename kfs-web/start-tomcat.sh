529f867655efe4fb3c74cb96d89492cb918b65f6
heather-lo <hlo@email.arizona.edu>
2015-08-05 16:14:50 -0700

    UAF-174 Modifying variable name to be more specific

--- kfs-web/start-tomcat.sh
+++ kfs-web/start-tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export EXTERNAL_CONFIG="-Dadditional.kfs.config.locations=$EXTERNAL_CONFIG_DIR/kfs-config.properties,$EXTERNAL_CONFIG_DIR/kfs-rice-config.properties,$EXTERNAL_CONFIG_DIR/kfs-security-config.properties"
+export EXTERNAL_CONFIG="-Dadditional.kfs.config.locations=$UA_KFS_EXTERNAL_CONFIG_DIR/kfs-config.properties,$UA_KFS_EXTERNAL_CONFIG_DIR/kfs-rice-config.properties,$UA_KFS_EXTERNAL_CONFIG_DIR/kfs-security-config.properties"
 
 mvn $EXTERNAL_CONFIG -DskipTests=true -P oracle -Dserver.name="http://localhost:8080" tomcat7:run-war

7ec7a6d5c4bc0f2162ca461eebc0f63d8468559c
Adam Kost <akost@eaglecrk.com>
2015-10-08 14:14:32 -0500

    KFSCD-14 Add additional search parameters. Includes:  Deposit Date From  Deposit Date To  Line Description  Amount From  Amount To

--- kfs-core/src/main/java/org/kuali/kfs/sys/businessobject/ElectronicPaymentClaim.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/businessobject/ElectronicPaymentClaim.java
@@ -27,6 +27,7 @@ import org.kuali.kfs.fp.businessobject.AdvanceDepositDetail;
 import org.kuali.kfs.fp.document.AdvanceDepositDocument;
 import org.kuali.kfs.sys.KFSPropertyConstants;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.krad.bo.DocumentHeader;
 import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
@@ -55,6 +56,8 @@ public class ElectronicPaymentClaim extends PersistableBusinessObjectBase {
     private AccountingPeriod financialDocumentPostingPeriod;
     private DocumentHeader generatingDocumentHeader;
 
+    private transient KualiDecimal amountFrom;
+    private transient KualiDecimal amountTo;
     /**
      * Default constructor.  It constructs.
      */
@@ -285,5 +288,21 @@ public class ElectronicPaymentClaim extends PersistableBusinessObjectBase {
      */
     public String getPaymentClaimStatus() {
         return getPaymentClaimStatusCode().equals("C") ? "Claimed" : "Unclaimed";
+    }
+
+    public KualiDecimal getAmountFrom() {
+        return amountFrom;
+    }
+
+    public void setAmountFrom(KualiDecimal amountFrom) {
+        this.amountFrom = amountFrom;
+    }
+
+    public KualiDecimal getAmountTo() {
+        return amountTo;
+    }
+
+    public void setAmountTo(KualiDecimal amountTo) {
+        this.amountTo = amountTo;
     }
 }

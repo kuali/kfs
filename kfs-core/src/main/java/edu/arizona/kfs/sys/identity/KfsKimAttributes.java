f78c105f81ee72da7f2eb63d700dc4ee3fb8602f
Mark Moen <mmoen@eaglecrk.com>
2016-01-18 14:48:38 -0600

    UAF-43 made the changes requested from the internal code review

--- kfs-core/src/main/java/edu/arizona/kfs/sys/identity/KfsKimAttributes.java
+++ kfs-core/src/main/java/edu/arizona/kfs/sys/identity/KfsKimAttributes.java
@@ -1,24 +1,10 @@
-/*
- * Copyright 2009 The Kuali Foundation.
- * 
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.opensource.org/licenses/ecl1.php
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package edu.arizona.kfs.sys.identity;
 
 import org.kuali.kfs.coa.businessobject.ObjectSubType;
+import org.kuali.kfs.sys.KFSPropertyConstants;
 
-public class ArizonaKfsKimAttributes extends org.kuali.kfs.sys.identity.KfsKimAttributes {
-    public static final String OBJECT_SUB_TYPE_CODE = "financialObjectSubTypeCode";
+public class KfsKimAttributes extends org.kuali.kfs.sys.identity.KfsKimAttributes {
+    public static final String OBJECT_SUB_TYPE_CODE = KFSPropertyConstants.FINANCIAL_OBJECT_SUB_TYPE_CODE;
         
     protected String financialObjectSubTypeCode;
     protected ObjectSubType objectSubType;

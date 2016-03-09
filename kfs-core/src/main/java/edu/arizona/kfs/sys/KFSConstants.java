342b855b02806023c6015073c74e31eee064b371
Mark Moen <mmoen@eaglecrk.com>
2016-03-09 15:08:53 -0600

    UAF-2219 Remove ability for AP Specialist to EDIT PREQ vendor address

--- kfs-core/src/main/java/edu/arizona/kfs/sys/KFSConstants.java
+++ kfs-core/src/main/java/edu/arizona/kfs/sys/KFSConstants.java
@@ -4,5 +4,13 @@ public class KFSConstants extends org.kuali.kfs.sys.KFSConstants {
 
     public static final String INVOICE_NUMBER = "Invoice Number";
     public static final String DUPLICATE_INVOICE_QUESTION_ID = "DVDuplicateInvoice";
+    
+
+
+    public static final class Authorization {
+    	
+        //Remove ability for AP Specialist to EDIT PREQ vendor address.
+        public static String EDIT_VENDOR_ADDR_EDIT_MODE = "editVendorAddress";
+    }
 
 }

8d3bf92ba7e6fdee47526dcfe967ba0f15aa5ad1
Mark Moen <mmoen@eaglecrk.com>
2016-03-18 09:26:07 -0500

    UAF-2228 made the changes requested in the code review

--- kfs-core/src/main/java/edu/arizona/kfs/coa/businessobject/FACostSubCategory.java
+++ kfs-core/src/main/java/edu/arizona/kfs/coa/businessobject/FACostSubCategory.java
@@ -1,27 +1,13 @@
-/*
- * Copyright 2009 The Kuali Foundation.
- * 
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.opensource.org/licenses/ecl1.php
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package edu.arizona.kfs.coa.businessobject;
 
 import java.util.LinkedHashMap;
 
+import org.kuali.rice.core.api.mo.common.active.MutableInactivatable;
 import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
 /**
  * FA Cost Subclass...
  */
-public class FACostSubCategory extends PersistableBusinessObjectBase{
+public class FACostSubCategory extends PersistableBusinessObjectBase implements MutableInactivatable {
     private String faCostSubCatCode;              // Subcategory Code 
     private boolean active;                       // Active Indicator
     private String faCostSubCatDesc;              // Subcategory Description
@@ -51,6 +37,7 @@ public class FACostSubCategory extends PersistableBusinessObjectBase{
      * Gets the active attribute. 
      * @return Returns the active.
      */
+    @Override
     public boolean isActive() {
         return active;
     }
@@ -58,6 +45,7 @@ public class FACostSubCategory extends PersistableBusinessObjectBase{
      * Sets the active attribute value.
      * @param active The active to set.
      */
+    @Override
     public void setActive(boolean active) {
         this.active = active;
     }

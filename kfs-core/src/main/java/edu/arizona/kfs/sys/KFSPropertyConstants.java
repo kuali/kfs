0b6356cb864c53c8d8cbd2f0895973afa9556836
Adam Kost <akost@eaglecrk.com>
2016-04-20 14:24:18 -0500

    UAF-66 / UAF-1971 / UAF-1972 MOD-PA7000-02 US Export Controls Process Update - eCustoms (Step 2) This is the second step of the modification for MOD-PA7000-02 US Export Controls Process Update - eCustoms, to create the daily batch job of creating a list of vendors that meet one of the following requirements: 1) The vendor is new since the last time the batch job was ran. 2) The vendor's name has changed since the last time the batch job was ran. 3) The vendor's Export Controls Flag is set to No. If a vendor meets any of these requirements, it is added to the .data file to be sent
    included is a minor fix discovered for Step 1, which was that the Additional Information tab on the Vendor Inquiry page that contains the Export Controls Flag was showing at the bottom, rather than as the second tab.

--- kfs-core/src/main/java/edu/arizona/kfs/sys/KFSPropertyConstants.java
+++ kfs-core/src/main/java/edu/arizona/kfs/sys/KFSPropertyConstants.java
@@ -7,5 +7,9 @@ public class KFSPropertyConstants extends org.kuali.kfs.sys.KFSPropertyConstants
     public static final String DV_PAYEE_DETAIL_PAYEE_TYPE_CODE = DV_PAYEE_DETAIL + ".disbursementVoucherPayeeTypeCode";
     public static final String UPPER_SOURCE_ACCOUNTING_LINES_EXTENSION_INVOICE_NUMBER = "upper("+SOURCE_ACCOUNTING_LINES + "." + EXTENSION + ".invoiceNumber)";
     public static final String UPPER_INVOICE_NUMBER = "upper(invoiceNumber)";
+    public static final String DOC_ROUTE_STATUS = "docRouteStatus"; //This should really be a Rice property constant.
+    public static final String FINALIZED_DATE = "finalizedDate"; //This should really be a Rice property constant.
+    public static final String BATCH_FILE_UPLOADS_BATCH_FILE_NAME = "batchFileName";
+    public static final String BATCH_FILE_UPLOADS_FILE_PROCESS_TIMESTAMP = "fileProcessTimestamp";
 
 }

ee6bbf999b6ddb3716171375cef3fea39228dc2e
Mark Moen <mmoen@eaglecrk.com>
2016-11-08 14:40:41 -0600

    UAF-3571 Maintenance doc Income Type. Added new file IncomeType.java to model the business object. Added new files IncomeType.xml and IncomeTypeMaintenanceDocument.xml to define how the maintenance document looks and works. Added new file  ojb-tax.xml so the object pulls from the correct table in the database. Added new file spring-tax.xml to set up the spring configurations. Modified institutional-config.properties to include the newly created spring-tax.xml file. Modified financialProcessing.tag to add the 1099 Income Type link on the maintenance tab.

---
+++ kfs-core/src/main/java/edu/arizona/kfs/tax/businessobject/IncomeType.java
@@ -0,0 +1,66 @@
+package edu.arizona.kfs.tax.businessobject;
+
+import java.util.LinkedHashMap;
+import org.kuali.rice.core.api.mo.common.active.MutableInactivatable;
+import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
+
+public class IncomeType extends PersistableBusinessObjectBase implements MutableInactivatable {
+    private String incomeTypeCode;
+    private String objectId;
+    private String incomeTypeDescription;
+    private String incomeTypeBox;
+    private boolean active;
+    
+    public String getIncomeTypeCode() {
+        return incomeTypeCode;
+    }
+
+    public void setIncomeTypeCode(String incomeTypeCode) {
+        this.incomeTypeCode = incomeTypeCode;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+
+    public String getIncomeTypeBox() {
+        return incomeTypeBox;
+    }
+
+    public void setIncomeTypeBox(String incomeTypeBox) {
+        this.incomeTypeBox = incomeTypeBox;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    protected LinkedHashMap<String, String> toStringMapper() {
+        LinkedHashMap<String, String> retval = new LinkedHashMap<String, String>();
+        retval.put("incomeTypeCode", this.incomeTypeCode);
+        retval.put("incomeTypeDescription", this.incomeTypeDescription);
+        return retval;
+    }
+
+    public String getIncomeTypeDescription() {
+        return incomeTypeDescription;
+    }
+
+    public void setIncomeTypeDescription(String incomeTypeDescription) {
+        this.incomeTypeDescription = incomeTypeDescription;
+    }
+    
+    public String getFullDescription() {
+        return getIncomeTypeCode() + " - " + getIncomeTypeDescription();
+    }
+}
+

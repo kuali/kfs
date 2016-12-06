194b2e1e3b1f19ec748b7faec3c96aa2487c2da8
Mark Moen <mmoen@eaglecrk.com>
2016-12-06 17:59:12 -0600

    UAF-3586 1099 Reporting, 1099 Tab on CM & Custom Searches. Modified tax-resources.properties to add an error message Added a constant to PurapConstants.java for use in other files Modified VendorCreditMemoDocument.java to include 1099 Modified VendorCreditMemoDocumentPresentationController.java so the 1099 tab will be visible Added new file IncomeTypeTotalsValidation.java to validate Income Types (1099 Classification tab) total against document total Added new file VendorCreditMemoAction.java to handle new income type, delete income type, and refresh income types from account lines Modified VendorCreditMemoDocument.xml to include 1099 Modified VendorCreditMemoValidation.xml to include income type totals validation Modified ojb-purap.xml to add paymentPaidYear and payment1099Indicator fields to the AP_CRDT_MEMO_T table. Added collection-descriptor for income types Modified struts-config.xml to map to the correct jsp (VendorCreditMemo.jsp) and action (VendorCreditMemoAction) Modified VendorCreditMemo.jsp to add the 1099 tab

--- kfs-purap/src/main/java/edu/arizona/kfs/module/purap/PurapConstants.java
+++ kfs-purap/src/main/java/edu/arizona/kfs/module/purap/PurapConstants.java
@@ -3,5 +3,8 @@ package edu.arizona.kfs.module.purap;
 public class PurapConstants extends org.kuali.kfs.module.purap.PurapConstants {
 
     public static final String PREQ_DUPLICATE_DV_QUESTION = "PREQ_DUPLICATE_DV_QUESTION";
+    
+    // Add New Search Fields to DV, PREQ and CM Documents 
+    public static final String INCOME_TYPE_NON_REPORTABLE_CODE = "NR";
 
 }

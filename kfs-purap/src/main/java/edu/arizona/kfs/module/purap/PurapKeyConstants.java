e1d1fade646699bcc9a3305e3abceb2ed5e2f7aa
Lin-Long Shyu <lshyu@email.arizona.edu>
2017-03-22 08:26:05 -0400

    UAF-3794 : Fixed Calculate PREQ from unencumbered PO with quantity but zero dollars

--- kfs-purap/src/main/java/edu/arizona/kfs/module/purap/PurapKeyConstants.java
+++ kfs-purap/src/main/java/edu/arizona/kfs/module/purap/PurapKeyConstants.java
@@ -4,4 +4,6 @@ public class PurapKeyConstants extends org.kuali.kfs.module.purap.PurapKeyConsta
 
     public static final String MESSAGE_PREQ_DUPLICATE_DV_INVOICE = "message.preq.duplicate.dv.invoice";
     public static final String ERROR_PAYMENTMETHODCODE_MUSTMATCHPREQ = "error.document.creditmemo.paymentmethodcode.mustmatchpreq";
+    public static final String WARNING_ADDITIONAL_CHARGES_ACCOUNT_DEFAULTED = "warning.additional.charges.account.defaulted";
+
 }
---
+++ kfs-purap/src/main/java/edu/arizona/kfs/module/purap/service/impl/PurapAccountingServiceImpl.java
@@ -0,0 +1,81 @@
+package edu.arizona.kfs.module.purap.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kfs.module.purap.PurapConstants;
+import org.kuali.kfs.module.purap.businessobject.PurApAccountingLine;
+import org.kuali.kfs.module.purap.document.PaymentRequestDocument;
+import org.kuali.kfs.sys.businessobject.SourceAccountingLine;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.rice.krad.util.MessageMap;
+
+import edu.arizona.kfs.module.purap.PurapKeyConstants;
+import edu.arizona.kfs.module.purap.PurapParameterConstants;
+
+
+public class PurapAccountingServiceImpl extends org.kuali.kfs.module.purap.service.impl.PurapAccountingServiceImpl {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PurapAccountingServiceImpl.class);
+    
+    @Override
+    public List<PurApAccountingLine> generateAccountDistributionForProration(List<SourceAccountingLine> accounts, KualiDecimal totalAmount, Integer percentScale, Class clazz) {
+        String methodName = "generateAccountDistributionForProration()";
+        if ( LOG.isDebugEnabled() ) {
+            LOG.debug(methodName + " started");
+        }
+        
+        List<PurApAccountingLine> newAccounts = new ArrayList<PurApAccountingLine>();
+        
+        if(totalAmount.isZero()) {
+            PurApAccountingLine newAccountingLine = null;
+            try {
+                newAccountingLine = (PurApAccountingLine) clazz.newInstance();
+            }
+            catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+            catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            newAccountingLine.setChartOfAccountsCode(getDefaultAdditionalChargesChartOfAccountsCode());
+            newAccountingLine.setAccountNumber(getDefaultAdditionalChargesAccount());
+            newAccountingLine.setFinancialObjectCode(getDefaultAdditionalChargesFinancialObjectCode());
+            
+            newAccountingLine.setAccountLinePercent(ONE_HUNDRED);
+            if ( LOG.isDebugEnabled() ) {
+                LOG.debug(methodName + " adding " + newAccountingLine.getAccountLinePercent());
+            }
+            newAccounts.add(newAccountingLine);
+            if ( LOG.isDebugEnabled() ) {
+                LOG.debug(methodName + " total = " + newAccountingLine.getAccountLinePercent());
+            }
+            
+            generateWarningsOnDocument();
+            
+            return newAccounts;
+        } else {
+            return super.generateAccountDistributionForProration(accounts, totalAmount, percentScale, clazz);
+        }
+    }
+    
+    private void generateWarningsOnDocument() {
+        MessageMap messages = GlobalVariables.getMessageMap();
+        messages.putWarning(PurapConstants.ITEM_TAB_ERROR_PROPERTY, PurapKeyConstants.WARNING_ADDITIONAL_CHARGES_ACCOUNT_DEFAULTED);
+        messages.putWarning(KRADConstants.GLOBAL_ERRORS, PurapKeyConstants.WARNING_ADDITIONAL_CHARGES_ACCOUNT_DEFAULTED);
+    }
+
+    private String getDefaultAdditionalChargesAccount() {
+        return parameterService.getParameterValueAsString(PaymentRequestDocument.class, PurapParameterConstants.ADDITIONAL_CHARGES_CLEARING_ACCOUNT);
+    }
+    
+    private String getDefaultAdditionalChargesChartOfAccountsCode() {
+        return parameterService.getParameterValueAsString(PaymentRequestDocument.class, PurapParameterConstants.ADDITIONAL_CHARGES_CLEARING_CHART_OF_ACCOUNTS);
+    }
+    
+    private String getDefaultAdditionalChargesFinancialObjectCode() {
+        return parameterService.getParameterValueAsString(PaymentRequestDocument.class, PurapParameterConstants.ADDITIONAL_CHARGES_CLEARING_OBJECT_CODE);
+    }
+}

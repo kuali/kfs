804bd839b02c4bc91434fea1e82532f5a1a69c3a
Lin-Long Shyu <lshyu@email.arizona.edu>
2017-03-22 17:19:00 -0400

    UAF-4101 : Added use tax offset for PREQ. initial commit to fix other jira issue

--- kfs-purap/src/main/java/edu/arizona/kfs/module/purap/PurapConstants.java
+++ kfs-purap/src/main/java/edu/arizona/kfs/module/purap/PurapConstants.java
@@ -5,5 +5,6 @@ public class PurapConstants extends org.kuali.kfs.module.purap.PurapConstants {
     public static final String PREQ_DUPLICATE_DV_QUESTION = "PREQ_DUPLICATE_DV_QUESTION";
 
     public static final String B2B_DIRECT_SHIP_ROUTE_CODES_PARM = "B2B_DIRECT_SHIP_ROUTE_CODE";
+    public static final String GLPE_USE_TAX_GENERATED_OFFSET_DESCRIPTION = "USE TAX GENERATED OFFSET";
 
 }
